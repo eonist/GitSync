ff8e347a73249090db9db8a6848e3791bc0347cc
Eonist <unicornhive@gmail.com>
2015-09-26 18:15:41 +0200

    Files modified: 1
    Modified 1 file:test/XMLTest.swift

--- test/XMLTest.swift
+++ test/XMLTest.swift
@@ -7,3 +7,67 @@ let path = "//Users/<path>/someFile.xml"
 
 var err: NSError?
 let content = String.stringWithContentsOfFile(path, encoding: NSUTF8StringEncoding, error: &err)
+
+
+//try the bellow:
+
+class CategoryParser: NSObject, NSXMLParserDelegate {
+
+    var subcategories = [[String : String]]()
+    var currentSubcategory: [String : String]?
+    var currentElementName: String?
+
+    var completion: (([[String : String]]) -> ())?
+
+    func parseXML( string: String, completion: (([[String : String]]) -> ())? ) {
+        guard let data = string.dataUsingEncoding( NSUTF8StringEncoding ) else {
+            fatalError( "Base XML data" )
+        }
+        self.completion = completion
+        let parser = NSXMLParser(data: data )
+        parser.delegate = self
+        parser.parse()
+    }
+
+    func parserDidEndDocument(parser: NSXMLParser) {
+        self.completion?( self.subcategories )
+    }
+
+    func parser(parser: NSXMLParser, parseErrorOccurred parseError: NSError) {
+        print( "Parse error: \(parseError)" )
+    }
+
+    func parser(parser: NSXMLParser, foundCharacters string: String) {
+
+        if let elementName = self.currentElementName {
+            if [ "id", "name" ].contains( elementName ) {
+                self.currentSubcategory?[ elementName ] = string
+            }
+        }
+    }
+
+    func parser(parser: NSXMLParser, didEndElement elementName: String, namespaceURI: String?, qualifiedName qName: String?) {
+        if elementName == "category", let subcategory = self.currentSubcategory {
+            self.subcategories.append( subcategory )
+            self.currentSubcategory = nil
+        }
+    }
+
+    func parser(parser: NSXMLParser, didStartElement elementName: String, namespaceURI: String?, qualifiedName qName: String?, attributes attributeDict: [String : String]) {
+
+        if elementName == "category" {
+            self.currentSubcategory = [String : String]()
+        }
+        else {
+            self.currentElementName = elementName
+        }
+    }
+}
+
+
+
+let categoryParser = CategoryParser()
+let xmlString = "<subCategories><category><id>someId</id><name>someName</name></category></subCategories>"
+categoryParser.parseXML( xmlString ) { (categories) in
+    print( categories )
+}
