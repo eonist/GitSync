daff74987f6942488ddd42e543b2891670107a4a
Eonist <unicornhive@gmail.com>
2015-09-26 18:23:46 +0200

    Files modified: 1
    Modified 1 file:test/XMLTest.swift

--- test/XMLTest.swift
+++ test/XMLTest.swift
@@ -1,4 +1,5 @@
 //xml parsing test
+//https://developer.apple.com/library/prerelease/ios/documentation/Cocoa/Reference/NSXMLParserDelegate_Protocol/index.html#//apple_ref/occ/intfm/NSXMLParserDelegate/parser:foundCharacters:
 
 import Foundation
 import Cocoa
