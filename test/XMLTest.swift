444d6fb9b716544e18f954af8fba63c8eb5659f7
Eonist <unicornhive@gmail.com>
2015-09-27 12:06:07 +0200

    Files modified: 1
    Modified 1 file:test/XMLTest.swift

--- test/XMLTest.swift
+++ test/XMLTest.swift
@@ -14,8 +14,8 @@
 
 
 //if you have an attr named value and content needs to be inside value then to differentiate the two you need to:
-categories["category"][0]
-
+categories["category"][0]["color"]//green
+categories["category"][0]//{color:green,name:"tinits",value:
 
 var dict:Dictionary = [String:String]()
 
