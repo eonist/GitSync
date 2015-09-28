65c91840e0dfa6e201867de1675edff396f8a713
Eonist <unicornhive@gmail.com>
2015-09-28 09:21:52 +0200

    Files modified: 1
    Modified 1 file:test/XMLTest.swift

--- test/XMLTest.swift
+++ test/XMLTest.swift
@@ -100,15 +100,16 @@ curIndexDepth = []
 var hasClosed = true
 var prevEnteredNodeName:String?
 var parent:Dictionary = [:]
-var prevEnteredParents:Array = []//flat list of previous entered parents
+var prevEnteredParents:Array = [parent]//flat list of previous entered parents
 func enter(nodeName:String,attributes:Dictionary){
 	if(hasClosed){//means the item is an sibling
-	
-	}else{//means you stepped into a subnode
 		
+	}else{//means you stepped into a subnode
+		prevEnteredParents.last[]
 		prevEnteredParents.append(parent)
 	}
 	prevEnteredNodeName = nodeName
+	hasClosed = false
 }
 func read(){
 	
@@ -119,7 +120,7 @@ func exit(nodeName:String){
 	}else{//means you exit an elemnt back one level (had children)
 		prevEnteredParents.removeLast()
 	}
-	
+	hasClosed = true
 }
 
 		
@@ -129,10 +130,10 @@ func exit(nodeName:String){
 
 //NOTE: if you have an attr named content and the child value needs to be inside content then to differentiate the two you need to rename the attr to somethin unique, this is out of the scope for this method though, so in that case just dont parse xmls with attr named content, if you do have to do it then just wrap this method into another with this extended functionality.
 //this is how you should navigate the result:
-root["categories"][0]["content"]["category"][0]["color"]//"green" that is an attribute value of color
-root["categories"][0]["content"]["category"][0]//{color:green,name:"tinits",content:{item:[{auther:john,age:2,content:"well designed car"},{},{}]}
+root["content"]["categories"][0]["content"]["category"][0]["color"]//"green" that is an attribute value of color
+root["content"]["categories"][0]["content"]["category"][0]//{color:green,name:"tinits",content:{item:[{auther:john,age:2,content:"well designed car"},{},{}]}
 //i guess optional chaining would suit the bellow line well:
-root["categories"][0]["content"]["category"][0]["content"]["item"][0]["content"]//"well designed car"
+root["content"]["categories"][0]["content"]["category"][0]["content"]["item"][0]["content"]//"well designed car"
 
 //here is how it works:
 //1. dictionaries store arrays of xml nodes of the same name
