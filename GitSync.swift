00438ada71155aedc1dc8379fb2622101cdc699e
Eonist <unicornhive@gmail.com>
2015-09-29 22:29:06 +0200

    Files modified: 1
    Modified 1 file:GitSync.swift

--- GitSync.swift
+++ GitSync.swift
@@ -10,7 +10,7 @@ class RepoUtils{
 	/**
 	 * 
 	 */
-	func compileRepoList(filePath:String){
+	func compileRepoList(filePath:String)
 		let xml:String = XMLParser.data(filePath)
 		let children:Array = xml["."]["repositories"][0]["."]["repository"]
 		let numChildren:Int = children.count //number of xml children in xml root element
@@ -21,17 +21,16 @@ class RepoUtils{
 			//continue here:
 			let local_path = ShellUtils.run("echo " + "'" + local_path + "'" + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths. 
 			"remote-path"
-			let remote_path: String = child["@"]["remote_path"]
+			let remotePath: String = child["@"]["remote_path"]
 			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
 			print( remote_path)
 			
-			set keychain_item_name to XMLParser's attribute_value_by_name(theXMLChild, "keychain-item-name")
-			--set commit_int to XMLParser's attribute_value_by_name(theXMLChild, "commit-interval-in-minutes") --defualt is 5min
-			--set push_int to XMLParser's attribute_value_by_name(theXMLChild, "push-interval-in-minutes") --defualt is 10min
-			--set pull_int to XMLParser's attribute_value_by_name(theXMLChild, "pull-interval-in-minutes") --default is 30min
-			set interval to XMLParser's attribute_value_by_name(theXMLChild, "interval") --default is 30min
-			--set remote_account_name to XMLParser's attribute_value_by_name(theXMLChild, "remote-account-name")
-			--TODO: use only 1 interval
+			let keychainItemName: String = child["@"]["keychain-item-name"]
+			//set commit_int to XMLParser's attribute_value_by_name(theXMLChild, "commit-interval-in-minutes") --defualt is 5min
+			//set push_int to XMLParser's attribute_value_by_name(theXMLChild, "push-interval-in-minutes") --defualt is 10min
+			//set pull_int to XMLParser's attribute_value_by_name(theXMLChild, "pull-interval-in-minutes") --default is 30min
+			let interval: String = child["@"]["interval"]//default is 1min
+			let remoteAccountName: String = child["@"]["remote-account-name"]
 			set key_value_pairs to {local_path:local_path, remote_path:remote_path, keychain_item_name:keychain_item_name, interval:interval} --remote_account_name:remote_account_name,commit_int:commit_int, push_int:push_int--TODO: shouldnt the line bellow be sudo acociative list? or does the record style list work as is?, if you dont need to iterate over the values, you may use record
 			set the_repo_list to ListModifier's add_list(the_repo_list, key_value_pairs)
 		}
