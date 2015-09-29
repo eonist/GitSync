076d055503d57cd6b72b82fd6a8b98b94b2544d1
Eonist <unicornhive@gmail.com>
2015-09-29 22:37:38 +0200

    Files modified: 1
    Modified 1 file:GitSync.swift

--- GitSync.swift
+++ GitSync.swift
@@ -23,12 +23,8 @@ class RepoUtils{
 			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
 			//print(remotePath)
 			let keychainItemName: String = child["@"]["keychain-item-name"]
-			//set commit_int to XMLParser's attribute_value_by_name(theXMLChild, "commit-interval-in-minutes") --defualt is 5min
-			//set push_int to XMLParser's attribute_value_by_name(theXMLChild, "push-interval-in-minutes") --defualt is 10min
-			//set pull_int to XMLParser's attribute_value_by_name(theXMLChild, "pull-interval-in-minutes") --default is 30min
 			let interval: String = child["@"]["interval"]//default is 1min
 			let remoteAccountName: String = child["@"]["remote-account-name"]
-			let key_value_pairs to {local_path:local_path, remote_path:remote_path, keychain_item_name:keychain_item_name, interval:interval} //remote_account_name:remote_account_name,commit_int:commit_int, push_int:push_int--TODO: shouldnt the line bellow be sudo acociative list? or does the record style list work as is?, if you dont need to iterate over the values, you may use record
 			theRepoList += ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval,"remoteAccountName":remoteAccountName]
 		}
 		return theRepoList
