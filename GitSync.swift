8b67db9d90caeef10cf6e7e50f284d2d9caa199c
Eonist <unicornhive@gmail.com>
2015-09-29 22:33:14 +0200

    Files modified: 1
    Modified 1 file:GitSync.swift

--- GitSync.swift
+++ GitSync.swift
@@ -18,21 +18,18 @@ class RepoUtils{
 		for (var i:Int; i++; i < numChildren){
 			let child = children[i]
 			let localPath to child["@"]["local-path"] //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
-			//continue here:
 			let localPath = ShellUtils.run("echo " + "'" + localPath + "'" + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths. 
-			"remote-path"
 			let remotePath: String = child["@"]["remote_path"]
 			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
-			print( remote_path)
-			
+			//print(remotePath)
 			let keychainItemName: String = child["@"]["keychain-item-name"]
 			//set commit_int to XMLParser's attribute_value_by_name(theXMLChild, "commit-interval-in-minutes") --defualt is 5min
 			//set push_int to XMLParser's attribute_value_by_name(theXMLChild, "push-interval-in-minutes") --defualt is 10min
 			//set pull_int to XMLParser's attribute_value_by_name(theXMLChild, "pull-interval-in-minutes") --default is 30min
 			let interval: String = child["@"]["interval"]//default is 1min
 			let remoteAccountName: String = child["@"]["remote-account-name"]
-			set key_value_pairs to {local_path:local_path, remote_path:remote_path, keychain_item_name:keychain_item_name, interval:interval} --remote_account_name:remote_account_name,commit_int:commit_int, push_int:push_int--TODO: shouldnt the line bellow be sudo acociative list? or does the record style list work as is?, if you dont need to iterate over the values, you may use record
-			set the_repo_list += []
+			set key_value_pairs to {local_path:local_path, remote_path:remote_path, keychain_item_name:keychain_item_name, interval:interval} //remote_account_name:remote_account_name,commit_int:commit_int, push_int:push_int--TODO: shouldnt the line bellow be sudo acociative list? or does the record style list work as is?, if you dont need to iterate over the values, you may use record
+			set theRepoList += ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName]
 		}
 		return theRepoList
 	}
