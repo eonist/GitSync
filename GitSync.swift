e51056a13759bfedaef91f3b0fa5a39204bc716c
Eonist <unicornhive@gmail.com>
2015-09-30 08:34:55 +0200

    Files modified: 1
    Modified 1 file:GitSync.swift

--- GitSync.swift
+++ GitSync.swift
@@ -97,13 +97,13 @@ class MergeUtils{
  	 * Promts the user with a list of options to aid in resolving merge conflicts
  	 * @param branch: the branch you tried to merge into
  	 */
-	on resolve_merge_conflicts(local_repo_path, branch, unmerged_files)
+	on resolve_merge_conflicts(localRepoPath, branch, unMergedFiles)
 		//log "resolve_merge_conflicts()"
 		//log ("MergeUtil's resolve_merge_conflicts()")
-		repeat with unmerged_file in unmerged_files
+		for( unMergedFile in unMergedFiles){;
 			set last_selected_action to first item in options --you may want to make this a "property" to store the last item more permenantly
 			set the_action to choose from list options with title "Resolve merge conflict in:" with prompt unmerged_file & ":" default items {last_selected_action} cancel button name "Exit" --promt user with list of options, title: Merge conflict in: unmerged_file
 			handle_merge_conflict_dialog(the_action, unmerged_file, local_repo_path, branch, unmerged_files)
-		end repeat
+		}
 	end resolve_merge_conflicts
 }
