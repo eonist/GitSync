1c6d528d063e4d017c429ee74b7d94963e267cae
Eonist <unicornhive@gmail.com>
2015-09-30 11:32:15 +0200

    Files modified: 1
    Modified 1 file:test/UITest.swift

--- test/UITest.swift
+++ test/UITest.swift
@@ -1,1 +1,16 @@
 //test creating buttons,lists,checkbuttons,textfields, etc to the view
+
+//Button
+let myButton = UIButton()
+myButton.setTitle("Hai Touch Me", forState: .Normal)
+myButton.setTitleColor(UIColor.blueColor(), forState: .Normal)
+myButton.frame = CGRectMake(15, 50, 300, 500)
+myButton.addTarget(self, action: "pressedAction:", forControlEvents: .TouchUpInside)
+self.view.addSubview( myButton)
+
+
+func pressedAction(sender: UIButton!) {
+   // do your stuff here 
+  NSLog("you clicked on button %@", sender.tag)
+}
+
