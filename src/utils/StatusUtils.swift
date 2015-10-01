8f04d50d0aceaff16cab9cba722da4991f595b73
Eonist <unicornhive@gmail.com>
2015-10-01 18:34:57 +0200

    Files modified: 1
    Modified 1 file:src/utils/StatusUtils.swift

--- src/utils/StatusUtils.swift
+++ src/utils/StatusUtils.swift
@@ -70,19 +70,18 @@ class StatusUtils{
 		for statusItem in statusList
 			//--log "len of status_item: " & (length of statusItem)
 			//--set cmd to cmd of status_item
-
 			if (statusItem["state"] = "Untracked files") { //--this is when there exists a new file
 				//log tab & "1. " & "Untracked files"
 				GitModifier's add(localRepoPath, statusItem["fileName"]) //--add the file to the next commit
 			}else if (state = "Changes not staged for commit"){ //--this is when you have not added a file that has changed to the next commit
 				//log tab & "2. " & "Changes not staged for commit"
 				GitModifier's add(localRepoPath, statusItem["fileName"]) //--add the file to the next commit
-			}else if (statusItem["state"] = "Changes to be committed" ) //--this is when you have added a file to the next commit, but not commited it
+			}else if (statusItem["state"] = "Changes to be committed" ){ //--this is when you have added a file to the next commit, but not commited it
 				//log tab & "3. " & "Changes to be committed" --do nothing here
 			}else if statusItem["state"] = "Unmerged path" ){ //--This is when you have files that have to be resolved first, but eventually added aswell
 				//log tab & "4. " & "Unmerged path"
 				GitModifier's add(localRepoPath, file_name) //--add the file to the next commit
-			end if
+			}
 		end repeat
 	}
 }
