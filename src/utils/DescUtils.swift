3de07af21b5016e087ded0070492185af0111e0d
Eonist <unicornhive@gmail.com>
2015-10-01 21:45:17 +0200

    Files modified: 1
    Modified 1 file:src/utils/DescUtils.swift

--- src/utils/DescUtils.swift
+++ src/utils/DescUtils.swift
@@ -5,7 +5,7 @@ class DescUtil{
 	/*
 	 * Returns a "Git Commit Message Description" derived from a "git status list" with "status items records"
 	 */
-	func sequence_description(status_list){
+	func sequenceDescription(statusList){
 	
 	
 	
@@ -13,7 +13,7 @@ class DescUtil{
 	
 	
 	
-		set desc_text to ""
+		set descText to ""
 		set modified_items to {}
 		set deleted_items to {}
 		set added_items to {}
@@ -23,25 +23,25 @@ class DescUtil{
 			if (cmd of status_item is "??") then set added_items to ListModifier's add_list(added_items, status_item) --add a record to a list
 			if (cmd of status_item is "UU") then set modified_items to ListModifier's add_list(modified_items, status_item) --add a record to a list
 		end repeat
-		set desc_text to desc_text & description_paragraph(added_items, "Added ") & return --add an extra line break at the end "paragraph like"
-		set desc_text to desc_text & description_paragraph(deleted_items, "Deleted ") & return
-		set desc_text to desc_text & description_paragraph(modified_items, "Modified ")
-		return desc_text
+		set descText to descText & description_paragraph(added_items, "Added ") & return --add an extra line break at the end "paragraph like"
+		set descText to descText & description_paragraph(deleted_items, "Deleted ") & return
+		set descText to descText & description_paragraph(modified_items, "Modified ")
+		return descText
 	}
 	/*
 	 * Returns a paragraph with a detailed description for Deleted, added and modified files
 	 */
 	func description_paragraph(the_list, prefix_text){
-		set desc_text to ""
+		set descText to ""
 		if (length of the_list > 0) {
 			set the_suffix to " file"
 			if (length of the_list > 1) then set the_suffix to the_suffix & "s" --multiple
-			set desc_text to desc_text & prefix_text & length of the_list & the_suffix & ":" & return
+			set descText to descText & prefix_text & length of the_list & the_suffix & ":" & return
 			repeat with the_item in the_list
-				set desc_text to desc_text & (file_name of the_item) & return
+				set descText to descText & (file_name of the_item) & return
 			end repeat
 		}
-		return desc_text
+		return descText
 	}
 }
 
