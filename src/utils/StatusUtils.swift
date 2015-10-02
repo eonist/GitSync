585e095c5a6eb7395ea5c710932d304b386db5f5
Eonist <unicornhive@gmail.com>
2015-10-02 13:34:08 +0200

    Files modified: 1
    Modified 1 file:src/utils/StatusUtils.swift

--- src/utils/StatusUtils.swift
+++ src/utils/StatusUtils.swift
@@ -6,19 +6,19 @@ class StatusUtils{
 	 * Returns a descriptive status list of the current git changes
 	 * NOTE: you may use short staus, but you must interpret the message if the state has an empty space infront of it
 	 */
-	func generateStatusList(localRepoPath){
-		set theStatus = GitParser's status(localRepoPath, "-s") //-- the -s stands for short message, and returns a short version of the status message, the short stauslist is used because it is easier to parse than the long status list
+	func generateStatusList(localRepoPath)->Array{
+		set theStatus = GitParser.status(localRepoPath, "-s") //-- the -s stands for short message, and returns a short version of the status message, the short stauslist is used because it is easier to parse than the long status list
 		//--log tab & "theStatus: " & theStatus
 		set theStatus_list = TextParsers.paragraph(theStatus) //--store each line as items in a list
 		set transformedList = []
 		if (theStatusList.count > 0) {
-			set transformedList to my transformStatusList(theStatusList)
+			set transformedList = transformStatusList(theStatusList)
 		}else{
 			//--log "nothing to commit, working directory clean"// --this is the status msg if there has happened nothing new since last, but also if you have commits that are ready for push to origin
 		}
 		//--log "len of theStatus_list: " & (length of theStatusList)
 		//--log transformedList
-		return transformed_list
+		return transformedList
 	}
 	/*
  	 * Transforms the "compact git status list" by adding more context to each item (a list with acociative lists, aka records)
@@ -30,64 +30,59 @@ class StatusUtils{
 	 * @Param: theStatusList is a list with status messages like: {"?? test.txt"," M index.html","A home.html"}
 	 * NOTE: can also be "UU" unmerged paths
  	 */
-	func transform_status_list(theStatusList){
-		var transformedList:Array = []
-		for (the_status_item in theStatusList){ 
+	func transformStatusList(theStatusList)->Array{
+		set transformedList = []
+		for (theStatusItem in theStatusList){ 
 			//--log "the_status_item: " & the_status_item
-			set theStatusParts to RegExpUtil's match(the_status_item, "^( )*([MARDU?]{1,2}) (.+)$") --returns 3 capturing groups, 
+			set theStatusParts to RegExpParser.match(theStatusItem, "^( )*([MARDU?]{1,2}) (.+)$") //--returns 3 capturing groups, 
 			//--log "length of theStatusParts: " & (length of theStatusParts)
 			//--log theStatusParts
-			if ((theStatusParts.second) == " ") { //--aka " M", remember that the second item is the first capturing group
-				set cmd = theStatusParts.third //--Changes not staged for commit:
-				var state to "Changes not staged for commit" //-- you need to add them
-			else //-- Changes to be committed--aka "M " or  "??" or "UU"
-				set cmd to theStatusParts.third //--rename cmd to type
+			let statusItem = ["state":"", "cmd":"", "fileName":""] //--store the individual parts in an accociative
+			if (theStatusParts.second == " ") { //--aka " M", remember that the second item is the first capturing group
+				statusItem["cmd"] = theStatusParts.third //--Changes not staged for commit:
+				statusItem["state"] = "Changes not staged for commit" //-- you Pneed to add them
+			}else{ //-- Changes to be committed--aka "M " or  "??" or "UU"
+				statusItem["cmd"] = theStatusParts.third //--rename cmd to type
 				//--log "cmd: " & cmd
-				if (cmd == "??") then
-					state = "Untracked files"
-				else if (cmd == "UU") then //--Unmerged path
+				if (statusItem["cmd"] == "??"){
+					statusItem["state"] = "Untracked files"
+				}else if (statusItem["cmd"] == "UU") { //--Unmerged path
 					//--log "Unmerged path"
-					state = "Unmerged path"
-				else
-					set state = "Changes to be committed" //--this is when the file is ready to be commited
-				end if
-			end if
-			let file_name = theStatusParts.fourth
+					statusItem["state"] = "Unmerged path"
+				}else{
+					statusItem["state"] = "Changes to be committed" //--this is when the file is ready to be commited
+				}
+			}
+			statusItem["fileName"] = theStatusParts.fourth
 			//--log "state: " & state & ", cmd: " & cmd & ", file_name: " & file_name --logs the file named added changed etc
-			let statusItem to ["state":state, "cmd":cmd, "fileName":fileName] //--store the individual parts in an accociative
-			
-			//continue here
-			
-			let transformedList = ListModifier's add_list(transformedList, statusItem) //--add a record to a list
-		end repeat
-		return transformed_list
-	end transform_status_list
+			transformedList += statusItem //--add a record to a list
+		}
+		return transformedList
+	}
 	/*
 	 * Iterates over the status items and "git add" the item unless it's already added (aka "staged for commit")
 	 * NOTE: if the status list is empty then there is nothing to process
 	 * NOTE: even if a file is removed, its status needs to be added to the next commit
 	 * TODO: Squash some of the states together with if or or or etc
 	 */
-	func process_status_list(localRepoPath, status_list){
-		--log "process_status_list()"
-		repeat with status_item in status_list
-			--log "len of status_item: " & (length of status_item)
-			set state to state of status_item
-			--set cmd to cmd of status_item
-			set file_name to file_name of status_item
-			if state = "Untracked files" then --this is when there exists a new file
-				log tab & "1. " & "Untracked files"
-				GitModifier's add(localRepoPath, file_name) --add the file to the next commit
-			else if state = "Changes not staged for commit" then --this is when you have not added a file that has changed to the next commit
-				log tab & "2. " & "Changes not staged for commit"
-				GitModifier's add(localRepoPath, file_name) --add the file to the next commit
-			else if state = "Changes to be committed" then --this is when you have added a file to the next commit, but not commited it
-				log tab & "3. " & "Changes to be committed" --do nothing here
-			else if state = "Unmerged path" then --This is when you have files that have to be resolved first, but eventually added aswell
-				log tab & "4. " & "Unmerged path"
-				GitModifier's add(localRepoPath, file_name) --add the file to the next commit
-			end if
-		end repeat
+	func processStatusList(localRepoPath, statusList){
+		//--log "process_status_list()"
+		for (statusItem in statusList){
+			//--log "len of status_item: " & (length of statusItem)
+			//--set cmd to cmd of status_item
+			if (statusItem["state"] = "Untracked files") { //--this is when there exists a new file
+				//log tab & "1. " & "Untracked files"
+				GitModifier.add(localRepoPath, statusItem["fileName"]) //--add the file to the next commit
+			}else if (state = "Changes not staged for commit"){ //--this is when you have not added a file that has changed to the next commit
+				//log tab & "2. " & "Changes not staged for commit"
+				GitModifier.add(localRepoPath, statusItem["fileName"]) //--add the file to the next commit
+			}else if (statusItem["state"] = "Changes to be committed" ){ //--this is when you have added a file to the next commit, but not commited it
+				//log tab & "3. " & "Changes to be committed" --do nothing here
+			}else if statusItem["state"] = "Unmerged path" ){ //--This is when you have files that have to be resolved first, but eventually added aswell
+				//log tab & "4. " & "Unmerged path"
+				GitModifier.add(localRepoPath, statusItem["fileName"]) //--add the file to the next commit
+			}
+		}
 	}
 }
 /*
