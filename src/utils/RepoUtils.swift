65bc94caa090e30e8d28607245d95a1fefbf2e91
Eonist <unicornhive@gmail.com>
2015-10-03 18:58:39 +0200

    Files modified: 1
    Modified 1 file:src/utils/RepoUtils.swift

--- src/utils/RepoUtils.swift
+++ src/utils/RepoUtils.swift
@@ -9,12 +9,12 @@ class RepoUtils{//Utility methods for parsing the repository.xml file
 		let xml:String = XMLParser.data(filePath)
 		let children:Array = xml["."]["repositories"][0]["."]["repository"]
 		let numChildren:Int = children.count //number of xml children in xml root element
-		var theRepoList:Array to []
+		var theRepoList:Array = []
 		for (var i:Int; i++; i < numChildren){
 			let child = children[i]
-			let localPath = child["@"]["local-path"] //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
-			let localPath = ShellUtils.run("echo " + StringModifer.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths. 
-			let remotePath: String = child["@"]["remote_path"]
+			let localPath:String = child["@"]["local-path"] //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
+			localPath = ShellUtils.run("echo " + StringModifer.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths. 
+			let remotePath:String = child["@"]["remote_path"]
 			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
 			//print(remotePath)
 			let keychainItemName: String = child["@"]["keychain-item-name"]
