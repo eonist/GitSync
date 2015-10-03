0788e1bc25e9c9a9a317edd5d25f321e4b9b5df4
Eonist <unicornhive@gmail.com>
2015-10-03 19:00:49 +0200

    Files modified: 1
    Modified 1 file:src/utils/RepoUtils.swift

--- src/utils/RepoUtils.swift
+++ src/utils/RepoUtils.swift
@@ -11,15 +11,15 @@ class RepoUtils{//Utility methods for parsing the repository.xml file
 		let numChildren:Int = children.count //number of xml children in xml root element
 		var theRepoList:Array = []
 		for (var i:Int; i++; i < numChildren){
-			let child = children[i]
+			let child:Dictionary = children[i]
 			let localPath:String = child["@"]["local-path"] //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
 			localPath = ShellUtils.run("echo " + StringModifer.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths. 
 			let remotePath:String = child["@"]["remote_path"]
 			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
 			//print(remotePath)
-			let keychainItemName: String = child["@"]["keychain-item-name"]
-			let interval: String = child["@"]["interval"]//default is 1min
-			let remoteAccountName: String = child["@"]["remote-account-name"]
+			let keychainItemName:String = child["@"]["keychain-item-name"]
+			let interval:String = child["@"]["interval"]//default is 1min
+			let remoteAccountName:String = child["@"]["remote-account-name"]
 			theRepoList += ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval,"remoteAccountName":remoteAccountName]
 		}
 		return theRepoList
