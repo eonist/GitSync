6b577822ea4f23e79d1f20af6dba727b50cc8979
Eonist <unicornhive@gmail.com>
2015-10-06 23:21:06 +0200

    Files modified: 1
    Modified 1 file:DataSource.swift

--- DataSource.swift
+++ DataSource.swift
@@ -5,7 +5,10 @@ class DataSource:NSObject, NSTableViewDataSource{
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
         return nameArray.count;
     }
-        
+    func tableView(tableColumn: NSTableColumn?, row: Int)->AnyObject?{
+        return "test"//nameArray[row]
+    }
+
     // (id)tableView:(NSTableView *)aTable objectValueForColumn:(NSTableColumn *)aCol row:(int)aRow;
 
     //tableView:(NSTableView *)tableView objectValueForTableColumn:(NSTableColumn *)tableColumn row:(NSInteger)row {
