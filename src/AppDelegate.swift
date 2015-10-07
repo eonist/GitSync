eedc85f52388548f5f1ce1a9e1f0171669ce8621
Eonist <unicornhive@gmail.com>
2015-10-07 09:03:54 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -79,7 +79,7 @@ class AppDelegate: NSObject, NSApplicationDelegate,NSTableViewDataSource,NSTable
         let tableContainerRect:NSRect = NSRect(x: 20, y: 20, width: 300,height:400)//view.bounds
         let tableContainer = NSScrollView(frame: tableContainerRect)
         let tableView = CustomTableView(frame: tableContainer.frame)//tableContainer.frame.width-100, height: tableContainer.frame.height
-        window?.makeFirstResponder(tableView)//focus tableView
+        window?.makeFirstResponder(tableView)//focus tableView,doesnt work yet
         
         tableView.setDataSource(tableView)//set the datasource
         tableView.autoresizingMask = NSAutoresizingMaskOptions.ViewWidthSizable //TODO: try to get height working here to
