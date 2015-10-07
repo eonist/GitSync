6ae186c814867b930cb0005661412e38d5372092
Eonist <unicornhive@gmail.com>
2015-10-07 08:16:06 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -66,7 +66,7 @@ class AppDelegate: NSObject, NSApplicationDelegate,NSTableViewDataSource,NSTable
         //[tableContainer setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
         //tableContainer.autoresizingMask = NSAutoresizingMaskOptions.
         
-        let tableView = NSTableView(frame: tableContainer.frame)
+        let tableView = CustomTableView(frame: tableContainer.frame)//NSTableView(frame: tableContainer.frame)
         //let dataSource = DataSource()
         
         
