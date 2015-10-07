53e5c0819a64e2415c4ce8ff1bbd90b95a37892b
Eonist <unicornhive@gmail.com>
2015-10-07 08:14:42 +0200

    Files modified: 1
    Modified 1 file:src/CustomTableView.swift

--- src/CustomTableView.swift
+++ src/CustomTableView.swift
@@ -1,4 +1,12 @@
 import Foundation
-class CustomTableView :NSTableViewDataSource,NSTableViewDelegate{
-    
+import Cocoa
+class CustomTableView:NSTableView,NSTableViewDataSource,NSTableViewDelegate{
+    let nameArray:Array = ["March","April","May"]
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return nameArray.count;
+    }
+    func tableView(tableView: NSTableView,objectValueForTableColumn tableColumn: NSTableColumn?,row: Int) -> AnyObject?{
+        return nameArray[row]
+    }
+
 }
