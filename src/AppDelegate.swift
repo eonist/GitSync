fd3a9eb5f91d440bca00450d6f21f2bc6c63d9a5
Eonist <unicornhive@gmail.com>
2015-10-07 09:23:31 +0200

    Files modified: 2
    Modified 2 files:src/AppDelegate.swiftsrc/window/WinUtils.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -12,24 +12,13 @@ class AppDelegate: NSObject, NSApplicationDelegate,NSTableViewDataSource,NSTable
     var window: NSWindow?
     var view:FlippedView = FlippedView(frame: NSRect(x: 0, y: 0, width: AppDelegate.width, height: AppDelegate.height))
     //MARK: - Init
-    var newWindow:NSWindow?
-    func createNewWindow(){
-        //NSWindow(contentRect: NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2), styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, defer: false)
-        let winRect = NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2)
-        newWindow = CustomWindow(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
-        newWindow!.title = "New Window"
-        newWindow!.opaque = false
-        newWindow!.hasShadow = false
-        newWindow!.center()
-        newWindow!.movableByWindowBackground = true
-        newWindow!.backgroundColor = NSColor(calibratedHue: 0, saturation: 1.0, brightness: 0, alpha: 0.7)
-        newWindow!.makeKeyAndOrderFront(nil)
-    }
+    var newWindow:NSWindow = WinUtils.win()
+    
     /**
      * Initializes your application
      */
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        createNewWindow()
+        
         
         view = FlippedView(frame: NSRect(x: 0, y: 0, width: AppDelegate.width, height: AppDelegate.height))
         window!.contentView = (view)
--- src/window/WinUtils.swift
+++ src/window/WinUtils.swift
@@ -1,12 +1,15 @@
 import Foundation
 import Cocoa
 class WinUtils {
+    /*
+     * Creates a basic window
+     */
     class func win()->NSWindow{
         let winRect = NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2)
         let newWindow = NSWindow(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
         newWindow.title = "New Window"
-        newWindow.opaque = false
-        newWindow.hasShadow = false
+        newWindow.opaque = true
+        newWindow.hasShadow = true
         newWindow.center()
         newWindow.movableByWindowBackground = true
         //newWindow!.backgroundColor = NSColor(calibratedHue: 0, saturation: 1.0, brightness: 0, alpha: 0.7)
