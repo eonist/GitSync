4a8e2c57bb4e6e1496d1e4b073dd48e6e476e8dd
Eonist <unicornhive@gmail.com>
2015-10-21 17:36:26 +0200

    Files modified: 1
    Modified 1 file:src/utils/RepoUtils.swift

--- src/utils/RepoUtils.swift
+++ src/utils/RepoUtils.swift
@@ -1,3 +1,4 @@
+import Foundation
 class RepoUtils{//Utility methods for parsing the repository.xml file
 	/**
 	 * Returns a list with repo values derived from an XML file
@@ -7,29 +8,19 @@ class RepoUtils{//Utility methods for parsing the repository.xml file
 	 */
 	func compileRepoList(filePath:String)->[Dictionary<String,String>]{
 		let xml:Dictionary<String,Any> = XMLParser.data(filePath)
-        let rootContent:Dictionary<String,Any> = xml["."] as! Dictionary<String,Any>
-        let repositoriesChildren:[Dictionary<String,Any>] = rootContent["repositories"] as! [Dictionary<String,Any>]
-        let firstRepositoriesChild:Dictionary<String,Any> = repositoriesChildren[0]
-        let firstRepositoriesChildContent:Dictionary<String,Any> = firstRepositoriesChild["."] as! Dictionary<String,Any>
-		let repositoryChildren:[Dictionary<String,Any>] = firstRepositoriesChildContent["repository"] as! [Dictionary<String,Any>]
-        
-        //let numChildren:Int = repositoryChildren.count //number of xml children in xml root element
-		var theRepoList:[Dictionary<String,String>] = []
-		
-        for repositoryChild:Dictionary<String,Any> in repositoryChildren{
-			//let child:Dictionary = repositoryChildren[i]
-            let attr:Dictionary<String,String> = repositoryChild["@"] as! Dictionary<String,String>
-			var localPath:String = attr["local-path"]! //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
-			localPath = ShellUtils.run("echo " + StringModifier.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths.
-			var remotePath:String = attr["remote_path"]!
-			remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
-			//print(remotePath)
-			let keychainItemName:String = attr["keychain-item-name"]!
-			let interval:String = attr["interval"]!//default is 1min
-			let remoteAccountName:String = attr["remote-account-name"]!
-            let repoItem:Dictionary<String,String> = ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval,"remoteAccountName":remoteAccountName]
-			theRepoList.append(repoItem)
-		}
-		return theRepoList
+        let children:Array<NSXMLElement> = XMLParser.rootChildren(xmlStr)
+        var theRepoList:[Dictionary<String,String>] = []
+        for child:NSXMLElement in children{
+            let localPath:String = XMLParser.attribute(child,"local-path")! //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
+            //localPath = ShellUtils.run("echo " + StringModifier.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths.
+            let remotePath:String = XMLParser.attribute(child,"remote-path")!
+            //remotePath = RegExpModifier.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
+            //print(remotePath)
+            let keychainItemName:String = XMLParser.attribute(child,"keychain-item-name")!
+            let interval:String = XMLParser.attribute(child,"interval")!//default is 1min
+            
+            let repoItem:Dictionary<String,String> = ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval]
+            theRepoList.append(repoItem)
+        }
 	}
 }
