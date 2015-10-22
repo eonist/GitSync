c8711dcc07dafa9f004381b4dac257e41007ef51
Eonist <unicornhive@gmail.com>
2015-10-22 16:31:55 +0200

    Files modified: 1
    Modified 1 file:src/utils/Align.swift

--- src/utils/Align.swift
+++ src/utils/Align.swift
@@ -1,1 +1,27 @@
 import Foundation
+import Cocoa
+
+class Align {
+    /**
+    *
+    */
+    class func horizontalAlignElements(elements:Array<NSView>,_ viewWidth:Int, _ yOffset:Int,_ horisontalSpacing:Int){
+        var totalWidth:Int = 0//(buttons.count * buttonWidth) + (buttonSpacing * (buttons.count-1))
+        for elmnt:NSView in elements {//find the total width
+            totalWidth += Int(elmnt.frame.width) + (elmnt != elements.last ? horisontalSpacing : 0)
+        }
+        
+        var tempX:Int = (viewWidth/2) - (totalWidth/2)
+        
+        
+        for element:NSView in elements{//align elements
+            let x:Int = tempX
+            let y:Int = yOffset
+            
+            element.setFrameOrigin(NSPoint(x: x, y: y))
+            tempX += Int(element.frame.width) + buttonSpacing
+            
+        }
+    }
+
+}
