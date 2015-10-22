276a6a01ad09b0297dc040d8b9ebda7c986f569d
Eonist <unicornhive@gmail.com>
2015-10-22 06:49:17 +0200

    Files modified: 1
    Modified 1 file:src/window/TempWin.swift

--- src/window/TempWin.swift
+++ src/window/TempWin.swift
@@ -1,1 +1,25 @@
 import Foundation
+import Cocoa
+
+class TempWin:NSWindow{
+    override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, `defer` flag: Bool) {
+        let winRect = NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2)
+        
+        
+        
+        super.init(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
+        
+        self.makeKeyAndOrderFront(nil)//moves the window to the front
+        createContent()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /**
+    *
+    */
+    func createContent(){
+        
+    }
+}
