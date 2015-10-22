c69cf7f0d337420a06caa5b3f27e1b7d02b85c69
Eonist <unicornhive@gmail.com>
2015-10-22 16:54:56 +0200

    Files modified: 1
    Modified 1 file:src/utils/Create.swift

--- src/utils/Create.swift
+++ src/utils/Create.swift
@@ -4,7 +4,7 @@ class Create {
     /**
      *
      */
-    class func button(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:String = "")->NSButton{
+    class func button(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:UInt = NSBezelStyle.SmallSquareBezelStyle)->NSButton{
         let button = NSButton(frame: NSRect(x: 0, y: 0, width: width, height: height))
         
         //button.highlight(true)
@@ -16,10 +16,32 @@ class Create {
     /**
      *
      */
-    class func textButton(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:String = "", title:String = "")->NSButton{
+    class func textButton(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:UInt = NSBezelStyle.SmallSquareBezelStyle, title:String = "")->NSButton{
         let button:NSButton = Create.button(width,height,x,y,style)
         button.title = title
         return button
     }
     
 }
+
+
+/*
+case RoundedBezelStyle
+case RegularSquareBezelStyle
+case ThickSquareBezelStyle
+case ThickerSquareBezelStyle
+case DisclosureBezelStyle
+case ShadowlessSquareBezelStyle
+case CircularBezelStyle
+case TexturedSquareBezelStyle
+case HelpButtonBezelStyle
+case SmallSquareBezelStyle
+case TexturedRoundedBezelStyle
+case RoundRectBezelStyle
+case RecessedBezelStyle
+case RoundedDisclosureBezelStyle
+// The inline bezel style contains a solid round-rect border background. It can be used to create an "unread" indicator in an outline view, or another inline button in a tableview, such as a stop progress button in a download panel. Use text for an unread indicator, and a template image for other buttons.
+@available(OSX 10.7, *)
+case InlineBezelStyle
+
+*/
