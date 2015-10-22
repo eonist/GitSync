69207357a8e11a3d849669ef5ba4f27a59fb9d0a
Eonist <unicornhive@gmail.com>
2015-10-22 20:14:58 +0200

    Files modified: 1
    Modified 1 file:src/window/WinUtils.swift

--- src/window/WinUtils.swift
+++ src/window/WinUtils.swift
@@ -19,6 +19,9 @@ class WinUtils {
         //win.makeMainWindow()
         win.maxSize = NSSize(width: 900,height: 900)//sets the  max size of a window
         win.minSize = NSSize(width: 400,height: 400)//sets the  max size of a window
+        
+        //win.orderFront(self)//ordering window:
+        //win.orderBack(self)//ordering window:
         //win.center()//places the window in the center
         //win.setIsMiniaturized(false)
         //win.update()// Subclasses of NSWindow can override this method to examine the state of the application and change their own state or appearance accordingly-enabling or disabling menus, buttons, and other controls based on the object that's selected, for example.
