c3447a83f0b0336f7539cb212273dff1bfaca9ac
Eonist <unicornhive@gmail.com>
2015-10-22 17:59:35 +0200

    Files modified: 1
    Modified 1 file:src/window/TempWin.swift

--- src/window/TempWin.swift
+++ src/window/TempWin.swift
@@ -57,8 +57,12 @@ class TempWin:NSWindow, NSApplicationDelegate,NSTableViewDelegate{
             //TODO: try to add data to current selected row
             //TODO: try to add data to row 2 col id: branch
             //TODO: add data from repo xml to the table
-            //TODO: change a value in table cell
             //TODO: this change of cell should be reflected in the xml on "enter text session complete"
+        
+        
+        
+        
+        //TODO: table changes should be reflected in the repo.xml on app exit
         
         //TODO: try to figure out how not to load the default appdelegate win
         
