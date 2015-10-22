a50ad3ee4d5ce0667c3536b79c1b7706d5dc1881
Eonist <unicornhive@gmail.com>
2015-10-22 16:53:01 +0200

    Files modified: 1
    Modified 1 file:src/utils/Create.swift

--- src/utils/Create.swift
+++ src/utils/Create.swift
@@ -1,17 +1,25 @@
 import Foundation
-
+import Cocoa
 class Create {
     /**
      *
      */
-    class func button(width:Int,_ height:Int, _ x:Int, _ y:Int, _ style:String){
+    class func button(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:String = "")->NSButton{
+        let button = NSButton(frame: NSRect(x: 0, y: 0, width: width, height: height))
         
+        //button.highlight(true)
+        let buttonCell:NSButtonCell = button.cell! as! NSButtonCell
+        buttonCell.bezelStyle = NSBezelStyle.SmallSquareBezelStyle//NSBezelStyle.RoundedBezelStyle
+        
+
     }
     /**
      *
      */
-    class func textButton(width:Int, _ height:Int, _ x:Int, _ y:Int, _ style:String, _ text:String){
-        
+    class func textButton(width:Int = 100,_ height:Int = 30, _ x:Int = 0, _ y:Int = 0, _ style:String = "", title:String = "")->NSButton{
+        let button:NSButton = Create.button(width,height,x,y,style)
+        button.title = title
+        return button
     }
     
 }
