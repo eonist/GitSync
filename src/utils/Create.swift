dbe8f6fdb7adb758d60629a0341a8f92e12d9c3f
Eonist <unicornhive@gmail.com>
2015-10-22 16:49:17 +0200

    Files modified: 1
    Modified 1 file:src/utils/Create.swift

--- src/utils/Create.swift
+++ src/utils/Create.swift
@@ -2,9 +2,16 @@ import Foundation
 
 class Create {
     /**
-    *
-    */
-    class func button(width:Int,Height:Int,x:Int,y:Int,Style:String){
+     *
+     */
+    class func button(width:Int,_ height:Int, _ x:Int, _ y:Int, _ style:String){
         
     }
+    /**
+     *
+     */
+    class func textButton(width:Int, _ height:Int, _ x:Int, _ y:Int, _ style:String, _ text:String){
+        
+    }
+    
 }
