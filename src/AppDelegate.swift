9d78026d8c05f2b1a2e909ed653da7a4c16a0147
Eonist <unicornhive@gmail.com>
2015-10-22 21:55:50 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -15,7 +15,9 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         let winRect = NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2)//TODO: us ns rect?
         win = TempWin(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
         //self.win.setContentView(self.newContentView)
-        self.win!.makeKeyAndOrderFront(nil)
+        self.win!.makeKeyAndOrderFront(self)
+        win!.makeMainWindow()
+        
         //print(aNotification)
         let app:NSApplication = aNotification.object as! NSApplication
         print(app)
@@ -23,7 +25,7 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         print(app.keyWindow)
         for w in app.windows{
             print("windowNumber: " + "\(w.windowNumber)")
-            //app.windowWithWindowNumber(<#T##windowNum: Int##Int#>)
+            //app.windowWithWindowNumber(<#T##windowNum: Int##Int#>)//this is how you can manage windows
         }
         app.windows[0].close()//close the initial window
         //app.windows[1].close()//close the initial window
