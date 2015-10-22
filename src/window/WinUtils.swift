5bca0ee334e6b5a13a353f947cd603b7a8c0de2c
Eonist <unicornhive@gmail.com>
2015-10-22 19:24:39 +0200

    Files modified: 1
    Modified 1 file:src/window/WinUtils.swift

--- src/window/WinUtils.swift
+++ src/window/WinUtils.swift
@@ -6,16 +6,18 @@ class WinUtils {
      */
     class func win()->NSWindow{
         let winRect = NSMakeRect(0, 0, NSScreen.mainScreen()!.frame.width/2, NSScreen.mainScreen()!.frame.height/2)
-        let newWindow = NSWindow(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
-        newWindow.title = "New Window"
-        newWindow.opaque = false
-        //newWindow.styleMask = NSBorderlessWindowMask
-        newWindow.hasShadow = true
-        newWindow.center()//moves the window to the center
-        newWindow.movableByWindowBackground = true
-        newWindow.backgroundColor = NSColor(calibratedHue: 0, saturation: 1.0, brightness: 0, alpha: 0.7)
-        newWindow.makeKeyAndOrderFront(nil)
-        return newWindow
+        let win = NSWindow(contentRect: winRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
+        win.title = "New Window"
+        win.opaque = false
+        //win = NSBorderlessWindowMask
+        win.hasShadow = true
+        win.center()//moves the window to the center
+        win.movableByWindowBackground = true
+        win.backgroundColor = NSColor(calibratedHue: 0, saturation: 1.0, brightness: 0, alpha: 0.7)
+        win.makeKeyAndOrderFront(nil)
+        //win(true)
+        
+        return win
     }
     
     /**
