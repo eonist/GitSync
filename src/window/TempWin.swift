a8830341f605911c7034674d5816829ead57dcd1
Eonist <unicornhive@gmail.com>
2015-10-22 10:25:59 +0200

    Files modified: 1
    Modified 1 file:src/window/TempWin.swift

--- src/window/TempWin.swift
+++ src/window/TempWin.swift
@@ -16,7 +16,7 @@ class TempWin:NSWindow, NSApplicationDelegate,NSTableViewDelegate{
         //controller.showWindow(self)
         self.center()
         self.contentView = view
-        self.title = "GitSync 0.1"
+        self.title = "GitSync"
         createContent()
     }
     required init?(coder: NSCoder) {
