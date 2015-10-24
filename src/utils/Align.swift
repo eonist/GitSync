d6e9351f559e668747c5aeb25a4efeb622243394
Eonist <unicornhive@gmail.com>
2015-10-24 19:34:20 +0200

    Files modified: 1
    Modified 1 file:src/utils/Align.swift

--- src/utils/Align.swift
+++ src/utils/Align.swift
@@ -5,13 +5,24 @@ class Align {
     /**
      * General interface horizontal align method
      */
-    class func horizontal(elements:Array<NSView>,_ viewWidth:Int, _ yOffset:Int,_ horisontalSpacing:Int){
+    class func horizontal(elements:Array<NSView>,_ viewWidth:Int = 0, xOffset:Int = 0 , _ yOffset:Int = 0,_ horisontalSpacing:Int = 0, alignType:String = "center"){
         var totalWidth:Int = 0//(buttons.count * buttonWidth) + (buttonSpacing * (buttons.count-1))
         for elmnt:NSView in elements {//find the total width
             totalWidth += Int(elmnt.frame.width) + (elmnt != elements.last ? horisontalSpacing : 0)
         }
         
-        var tempX:Int = (viewWidth/2) - (totalWidth/2)
+        var tempX:Int = 0
+        
+        switch alignType{
+            case "left":
+                tempX = 0
+            case "center":
+                tempX = (viewWidth/2) - (totalWidth/2)
+            default:
+                break;
+            
+        }
+       
         
         
         for element:NSView in elements{//align elements
