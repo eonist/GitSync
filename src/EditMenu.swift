b77047eafcc59dec30312e4d157f78b766293026
Eonist <unicornhive@gmail.com>
2015-10-24 19:38:17 +0200

    Files modified: 1
    Modified 1 file:src/EditMenu.swift

--- src/EditMenu.swift
+++ src/EditMenu.swift
@@ -2,5 +2,7 @@ import Foundation
 
 class EditMenu {
     static var height = 64
+    static var topPadding:Int = 12
+    static var spacing:Int = 12
     //this is the menu bellow the project list
 }
