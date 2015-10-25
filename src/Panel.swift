c727133c6476583bb50c09817e1cad40b210b9eb
Eonist <unicornhive@gmail.com>
2015-10-25 06:56:27 +0100

    Files modified: 1
    Modified 1 file:src/Panel.swift

--- src/Panel.swift
+++ src/Panel.swift
@@ -5,12 +5,15 @@ class Panel:NSView {
     static var leftPadding = 12
     static var rightPadding = 12
     init() {
-        let x = Table.width+Table.leftPadding+24
+        let x = Table.width+Table.leftPadding+Panel.leftPadding
         let y = Table.topPadding
         let width = Win.width-Table.width-Panel.leftPadding-Panel.rightPadding
         let height = Win.height-Table.topPadding-EditMenu.height
         let rect:NSRect = NSRect(x:x, y: y, width: width,height:height)//view.bounds
-        super.init(frame: rect)
+        super.init(frame:rect)
+    }
+    required init?(coder: NSCoder) {//try to get rid of this
+        fatalError("init(coder:) has not been implemented")
     }
     override func drawRect(dirtyRect: NSRect) {
         let pathRect = NSInsetRect(self.bounds, 1, 1);
