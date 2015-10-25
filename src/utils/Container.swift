0a2323b1593c4373e2d42b004d33370e073d4cf8
Eonist <unicornhive@gmail.com>
2015-10-25 10:12:34 +0100

    Files modified: 1
    Modified 1 file:src/utils/Container.swift

--- src/utils/Container.swift
+++ src/utils/Container.swift
@@ -1,1 +1,31 @@
 import Foundation
+import Cocoa
+
+class Container:NSView{
+    init(_ width:Int = 200, _ height:Int = 30) {
+        let frame = NSRect(x: 0, y: 0, width: width, height: height)
+        super.init(frame: frame)
+    }
+    /*
+    * Disables clipping of the view
+    */
+    override var wantsDefaultClipping: Bool {
+        get {
+            return false
+        }
+    }
+    /*
+    * Required by super class
+    */
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    /*
+    * This makes sure that the view draws from top left corner
+    */
+    override var flipped:Bool {
+        get {
+            return true
+        }
+    }
+}
