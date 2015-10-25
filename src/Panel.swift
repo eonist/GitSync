5f53b710a098d1e6c86033c7b91056a92d80c596
Eonist <unicornhive@gmail.com>
2015-10-25 06:44:42 +0100

    Files modified: 1
    Modified 1 file:src/Panel.swift

--- src/Panel.swift
+++ src/Panel.swift
@@ -1,1 +1,19 @@
 import Foundation
+import Cocoa
+
+class Panel:NSView {
+    override func drawRect(dirtyRect: NSRect) {
+        var pathRect = NSInsetRect(self.bounds, 1, 1);
+        
+        var path = NSBezierPath(roundedRect:pathRect, xRadius:10, yRadius:10);
+        
+        path.lineWidth = 4
+        
+        NSColor.greenColor().setFill();
+        NSColor.blackColor().setStroke();
+        path.fill()
+        path.stroke()
+        
+        
+    }
+}
