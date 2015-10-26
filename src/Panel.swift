fdf617f4ea7c1985e39696a0f66b1f812d0d4275
Eonist <unicornhive@gmail.com>
2015-10-26 16:32:36 +0100

    Files modified: 1
    Modified 1 file:src/Panel.swift

--- src/Panel.swift
+++ src/Panel.swift
@@ -9,8 +9,8 @@ class Panel:Section {
         let y = Win.topPadding
         let width = Win.width-Table.width-Panel.leftPadding-Panel.rightPadding
         let height = Win.height-Win.topPadding-EditMenu.height
-        let rect:NSRect = NSRect(x:x, y: y, width: width,height:height)//view.bounds
-        super.init(frame:rect)
+        //let rect:NSRect = NSRect(x:x, y: y, width: width,height:height)//view.bounds
+        super.init(NSColor.orangeColor(), NSColor.clearColor(), width, height)
         createContent()
     }
     /**
@@ -49,19 +49,6 @@ class Panel:Section {
     }
     required init?(coder: NSCoder) {//try to get rid of this
         fatalError("init(coder:) has not been implemented")
-    }
-    /*
-    * Draws the background
-    */
-    override func drawRect(dirtyRect: NSRect) {
-        let pathRect = NSInsetRect(self.bounds, 1, 1);
-        let path = NSBezierPath(roundedRect:pathRect, xRadius:0, yRadius:0);
-        path.lineWidth = 1
-        NSColor.whiteColor().setFill();
-        NSColor.grayColor().setStroke();
-        path.fill()
-        path.stroke()
-        //super.wantsDefaultClipping
     }
     /*
     * This makes sure that the view draws from top left corner
