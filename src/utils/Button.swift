c66ee8b812d1fe8a8b5020c0bc4987104027296f
Eonist <unicornhive@gmail.com>
2015-10-26 18:15:19 +0100

    Files modified: 1
    Modified 1 file:src/utils/Button.swift

--- src/utils/Button.swift
+++ src/utils/Button.swift
@@ -1,1 +1,19 @@
-import Foundation
+import Cocoa
+
+class Button: NSButton {
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        self.wantsLayer = true
+    }
+    override func updateLayer() {//called on init if wantsUpdateLayer is true
+        self.layer!.backgroundColor = CGColorCreateGenericRGB(1, 1, 0, 1)
+        self.layer?.borderColor = CGColorCreateGenericRGB(1, 0, 1, 1)
+        self.layer?.borderWidth = 5
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override var wantsUpdateLayer:Bool{//enables the call of updateLayer
+        return true
+    }
+}
