9947a01df41d98600f87066ee4c07909ce8a73d7
Eonist <unicornhive@gmail.com>
2015-10-26 18:21:20 +0100

    Files modified: 1
    Modified 1 file:src/utils/Button.swift

--- src/utils/Button.swift
+++ src/utils/Button.swift
@@ -1,15 +1,25 @@
 import Cocoa
 
 class Button: NSButton {
+    var fillColor:NSColor = NSColor.clearColor()
+    var strokeColor:NSColor = NSColor.clearColor()
     init(_ width: Int = 100, _ height: Int = 100, _ fillColor:NSColor = NSColor.clearColor(), _ strokeColor:NSColor = NSColor.clearColor()) {
+        self.fillColor = fillColor
+        self.strokeColor = strokeColor
         let frame = NSRect(x: 0, y: 0, width: width, height: height)
         super.init(frame: frame)
         self.wantsLayer = true
     }
     override func updateLayer() {//called on init if wantsUpdateLayer is true
-        self.layer!.backgroundColor = CGColorCreateGenericRGB(1, 1, 0, 1)
-        self.layer?.borderColor = CGColorCreateGenericRGB(1, 0, 1, 1)
-        self.layer?.borderWidth = 5
+        if(fillColor != NSColor.clearColor()){
+            let fillColor = NSColorParser.cgColor(self.fillColor)
+            layer?.backgroundColor = fillColor//CGColorCreateGenericRGB(1, 0, 1, 1)
+        }
+        if(strokeColor != NSColor.clearColor()){
+            let strokeColor = NSColorParser.cgColor(self.strokeColor)
+            layer?.borderColor = strokeColor//CGColorCreateGenericRGB(0, 1, 0, 1)
+            layer?.borderWidth = 5
+        }
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
