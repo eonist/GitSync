6866bf86079ec42145ce83e79150b8912de7cdf6
Eonist <unicornhive@gmail.com>
2015-10-26 09:06:12 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -1,1 +1,23 @@
 import Foundation
+import Cocoa
+
+class Section:Container {
+    override init(_ fillColor:NSColor = NSColor.grayColor(), _ strokeColor:NSColor = nil, _ width: Int = 100, _ height: Int = 100) {
+        
+        super.init(width, height)
+    }
+    /*
+    *
+    */
+    override func drawRect(dirtyRect: NSRect) {
+        super.drawRect(dirtyRect)
+        color.setFill()
+        NSRectFill(dirtyRect)
+    }
+    /*
+    * Required by super class
+    */
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
