6eab3f28576deb4d12082948d39b69e5bc8b068e
Eonist <unicornhive@gmail.com>
2015-10-26 07:36:14 +0100

    Files modified: 1
    Modified 1 file:src/utils/Container.swift

--- src/utils/Container.swift
+++ src/utils/Container.swift
@@ -4,7 +4,7 @@ import Cocoa
 class Container:FlippedView{
     init(_ width:Int = 300, _ height:Int = 30) {
         let frame = NSRect(x: 0, y: 0, width: width, height: height)
-        super.init(frame: frame) 
+        super.init(frame: frame)
     }
     /*
     * Required by super class
