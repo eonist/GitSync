a46a007ba153d26ff92606feb6d2b1e2d4911cfb
Eonist <unicornhive@gmail.com>
2015-10-27 09:19:27 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -7,34 +7,6 @@ class Section:Element {//Unlike Container, section can have a style applied
 
     }
     /*
-    override func updateLayer() {//called on init if wantsUpdateLayer is true
-        if(style.fill.color != NSColor.clearColor()){
-            layer?.backgroundColor = style.fill.cgColor
-        }
-        if(style.stroke.color != NSColor.clearColor()){
-            layer?.borderColor = style.stroke.cgColor
-            layer?.borderWidth = style.stroke.width
-        }
-    }
-    */
-    /*
-    *
-    */
-    /*
-    override func drawRect(dirtyRect: NSRect) {
-        super.drawRect(dirtyRect)
-        if(fillColor != NSColor.clearColor()){
-            let fillColor = NSColorParser.cgColor(self.fillColor)
-            layer?.backgroundColor = fillColor//CGColorCreateGenericRGB(1, 0, 1, 1)
-        }
-        if(strokeColor != NSColor.clearColor()){
-            let strokeColor = NSColorParser.cgColor(self.strokeColor)
-            layer?.borderColor = strokeColor//CGColorCreateGenericRGB(0, 1, 0, 1)
-            layer?.borderWidth = 1
-        }
-    }
-*/
-    /*
     * Required by super class
     */
     required init?(coder: NSCoder) {
