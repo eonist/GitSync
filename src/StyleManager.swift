940ae0e771e5989c33432fc8b42f50c7361793e2
Eonist <unicornhive@gmail.com>
2015-10-27 16:31:34 +0100

    Files modified: 1
    Modified 1 file:src/StyleManager.swift

--- src/StyleManager.swift
+++ src/StyleManager.swift
@@ -1,1 +1,10 @@
 import Foundation
+class StyleManager{
+    static var styles:Array<IStyle> = []
+    
+    func getStyle(name:String):IStyle{
+        var numOfStyles:int = _styles.length;
+        for(var i:int = 0;i < numOfStyles;i++) if(IStyle(_styles[i]).name == name) return  _styles[i];
+        return null;
+    }
+}
