740857e724136661a34980fae68f046189a81474
Eonist <unicornhive@gmail.com>
2015-10-27 09:25:41 +0100

    Files modified: 1
    Modified 1 file:src/utils/Section.swift

--- src/utils/Section.swift
+++ src/utils/Section.swift
@@ -1,3 +1,10 @@
 class Section:Element {//Unlike Container, section can have a style applied
+    init(_ width: Int, _ height: Int, _ style: IStyle) {
+        <#code#>
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     // this class exists because one shouldnt use the Element class as a holder of content
 }
