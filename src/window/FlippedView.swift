d88b2c54b72ec35e380515b581bbea3aad6d94d1
Eonist <unicornhive@gmail.com>
2015-10-27 15:45:25 +0100

    Files modified: 1
    Modified 1 file:src/window/FlippedView.swift

--- src/window/FlippedView.swift
+++ src/window/FlippedView.swift
@@ -2,7 +2,7 @@ import Cocoa
 import Foundation
 protocol IView{
     var layer: CALayer?{get}
-    var style:IStyle{get}
+    var style:IGraphicStyle{get}
 }
 class FlippedView:NSView{//Organizes your view from top to bottom
     override var flipped:Bool {
