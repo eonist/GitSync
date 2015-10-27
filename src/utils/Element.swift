bad237638c75fe1c402d46339a64b232ff97a6c6
Eonist <unicornhive@gmail.com>
2015-10-27 09:00:52 +0100

    Files modified: 1
    Modified 1 file:src/utils/Element.swift

--- src/utils/Element.swift
+++ src/utils/Element.swift
@@ -1,1 +1,35 @@
-import Foundation
+import Cocoa
+protocol IElement{
+    
+}
+class Element: FlippedView,IElement {
+    var style:IStyle
+    init(_ width: Int = 100, _ height: Int = 40, _ style:IStyle = Style.clear){
+        self.style = style
+        let frame = NSRect(x: 0, y: 0, width: width, height: height)
+        super.init(frame: frame)
+        self.wantsLayer = true//need for the updateLayer method to be called internally
+    }
+    func resolveSkin() {
+        
+    }
+    override func updateLayer() {//called on init if wantsUpdateLayer is true
+        if(style.fill.color != NSColor.clearColor()){
+            layer?.backgroundColor = style.fill.cgColor
+        }
+        if(style.stroke.color != NSColor.clearColor()){
+            layer?.borderColor = style.stroke.cgColor
+            layer?.borderWidth = style.stroke.width
+        }
+    }
+    /*
+    * Required by NSView
+    */
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension Element{
+    
+}
