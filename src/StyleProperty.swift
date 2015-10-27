147e6c6462fe3299082a47409ee7c202819c6301
Eonist <unicornhive@gmail.com>
2015-10-27 15:43:15 +0100

    Files modified: 1
    Modified 1 file:src/StyleProperty.swift

--- src/StyleProperty.swift
+++ src/StyleProperty.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class StyleProperty {
-    
+    var name : String;
+    var value : Any;
+    /**
+    *
+    */
+    init(_ name:String,_ value:Any){
+        self.name = name
+        self.value = value
+    }
 }
