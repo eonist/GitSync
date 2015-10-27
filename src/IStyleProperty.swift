92b8805e96a018970cbdec76fbc47e7853f6bc57
Eonist <unicornhive@gmail.com>
2015-10-27 15:41:23 +0100

    Files modified: 2
    Modified 2 files:src/IStyleProperty.swiftsrc/StyleProperty.swift

--- src/IStyleProperty.swift
+++ src/IStyleProperty.swift
@@ -1,1 +1,10 @@
 import Foundation
+
+class IStyleProperty {
+    /*
+    function get name() : String
+    function get value() : *
+    function set value(value : *) : void
+    function get depth() : uint
+    */
+}
--- src/StyleProperty.swift
+++ src/StyleProperty.swift
@@ -1,1 +1,5 @@
 import Foundation
+
+class StyleProperty {
+    
+}
