3c6d04b6f2c8e3ff60d25c0a56604f67af81196f
Eonist <unicornhive@gmail.com>
2015-10-28 08:28:42 +0100

    Files modified: 1
    Modified 1 file:src/StyleUtils.swift

--- src/StyleUtils.swift
+++ src/StyleUtils.swift
@@ -1,1 +1,13 @@
 import Foundation
+class StyleUtils {
+    /**
+     * Clones a style
+     * CSSParser.as, StyleHeritageResolver.as uses this function
+     * // :TODO: explain what newSelectors does
+     */
+    class func clone(style:IStyle, newName:String = null, newSelectors:Array = null)->IStyle{
+        var returnStyle:IStyle = new Style(newName || style.name, newSelectors || style.selectors, []);
+        for each (var styleProperty : IStyleProperty in style.styleProperties) returnStyle.addStyleProperty(new StyleProperty(styleProperty.name, styleProperty.value, styleProperty.depth));
+        return returnStyle;
+    }
+}
