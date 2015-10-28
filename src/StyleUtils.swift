2a1288b3288fffe5aaa61d56aae22111c9981e32
Eonist <unicornhive@gmail.com>
2015-10-28 08:30:55 +0100

    Files modified: 1
    Modified 1 file:src/StyleUtils.swift

--- src/StyleUtils.swift
+++ src/StyleUtils.swift
@@ -6,7 +6,7 @@ class StyleUtils {
      * // :TODO: explain what newSelectors does
      */
     class func clone(style:IStyle, newName:String = null, newSelectors:Array = null)->IStyle{
-        var returnStyle:IStyle = new Style(newName || style.name, newSelectors || style.selectors, []);
+        var returnStyle:IStyle = Style(newName || style.name, newSelectors || style.selectors, []);
         for each (var styleProperty : IStyleProperty in style.styleProperties) returnStyle.addStyleProperty(new StyleProperty(styleProperty.name, styleProperty.value, styleProperty.depth));
         return returnStyle;
     }
