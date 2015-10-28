a91909e0c5d3ee3e1a0b11565ea2ee9b7a6a56ae
Eonist <unicornhive@gmail.com>
2015-10-28 08:57:19 +0100

    Files modified: 1
    Modified 1 file:src/StyleUtils.swift

--- src/StyleUtils.swift
+++ src/StyleUtils.swift
@@ -12,4 +12,25 @@ class StyleUtils {
         }
         return returnStyle;
     }
+    /**
+    *
+    */
+    class func overrideStyleProperty(style:IStyle, styleProperty:IStyleProperty){
+        var stylePropertiesLength:Int = style.styleProperties.count;
+        for (var i:Int=0; i<stylePropertiesLength; i++) { // :TODO: use fore each
+            if(IStyleProperty(style.styleProperties[i]).name == styleProperty.name){
+                style.styleProperties[i] = styleProperty;
+                return;
+            }
+        }
+        Swift.print("\(String(style))"+" PROPERTY BY THE NAME OF "+styleProperty.name+" WAS NOT FOUND IN THE PROPERTIES ")//this should throw error
+    }
+}
+
+
+/**
+*
+*/
+public static function overrideStyleProperty(style:IStyle, styleProperty:IStyleProperty):void {// :TODO: argument should only be a styleProperty
+    
 }
