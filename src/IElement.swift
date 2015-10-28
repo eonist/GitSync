3219feb6fbb9336dc41f6c58081e70d27572a371
Eonist <unicornhive@gmail.com>
2015-10-28 08:09:24 +0100

    Files modified: 2
    Modified 2 files:src/IElement.swiftsrc/utils/Element.swift

--- src/IElement.swift
+++ src/IElement.swift
@@ -1,6 +1,6 @@
 import Foundation
 protocol IElement:IView{
-    mutating func resolveSkin()
+    func resolveSkin()
     var style:IStyle{get}
     func setStyle(style:IStyle)
     func getClassType()->String
