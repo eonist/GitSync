cf6936581032b4ff44bfce0320cff5cff56cdbc9
Eonist <unicornhive@gmail.com>
2015-11-05 17:23:26 +0100

    Files modified: 1
    Modified 1 file:src/window/Win.swift

--- src/window/Win.swift
+++ src/window/Win.swift
@@ -70,9 +70,9 @@ class Win:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         //gradientStyle
         let idleStyle2Selctor:ISelector = Selector("Element",[],"",[SkinStates.none])
         let idleStyle2:IStyle = Style("idleStyle2",[],idleStyle2Selctor)
-        idleStyle2.addStyleProperty(StyleProperty("gradientType", "linear"))
-        idleStyle2.addStyleProperty(StyleProperty("gradientColorA", Colors.lightGray))
-        idleStyle2.addStyleProperty(StyleProperty("gradientColorB", Colors.purple))
+        idleStyle2.addStyleProperty(StyleProperty("fillGradientType", "linear"))
+        idleStyle2.addStyleProperty(StyleProperty("fillGradientColorA", Colors.lightGray))
+        idleStyle2.addStyleProperty(StyleProperty("fillGradientColorB", Colors.purple))
         idleStyle2.addStyleProperty(StyleProperty("lineWidth", 12))
         idleStyle2.addStyleProperty(StyleProperty("lineColor", Colors.lightGray))
         idleStyle2.addStyleProperty(StyleProperty("lineAlpha", 1.0))
