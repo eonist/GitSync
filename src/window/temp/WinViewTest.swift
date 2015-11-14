6d56721e8411708163a1c856a357b7621a5fa0bc
Eonist <unicornhive@gmail.com>
2015-11-14 17:40:53 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinViewTest.swift

--- src/window/temp/WinViewTest.swift
+++ src/window/temp/WinViewTest.swift
@@ -34,6 +34,7 @@ class GrapixDecorator:IGrapixDecorator{
 }
 class Grapix:IGrapixDecorator{//base class for decorators
     var graphics:Graphics
+    var path:CGPath = CGPathCreateMutable()
     init(){
         graphics = Graphics()
     }
@@ -45,7 +46,12 @@ class Grapix:IGrapixDecorator{//base class for decorators
     }
 }
 class RectGrapix:GrapixDecorator{//adds rectangular path
-    
+    override init(_ decoratable: IGrapixDecorator) {
+        super.init(decoratable)
+        getGrapix().path = CGPathParser.rect(CGFloat(100), CGFloat(100))//Shapes
+        GraphicModifier.applyProperties(getGrapix().graphics, FillStyle(NSColor.purpleColor())/*, lineStyle*/)//apply style
+        GraphicModifier.stylize(getGrapix().path,getGrapix().graphics)//realize style on the graphic
+    }
 }
 class RoundRectGrapix{//adds round-rectangular path
     init(){
