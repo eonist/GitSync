c030849d5c2419bdc2466046e50ce86c00989871
Eonist <unicornhive@gmail.com>
2015-11-16 20:23:29 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/GraphicTest.swift

--- src/window/temp/GraphicTest.swift
+++ src/window/temp/GraphicTest.swift
@@ -6,14 +6,14 @@ class GraphicsTest:Graphic{
     var width:Int
     var height:Int
     var color:NSColor
-    //var path:CGPath
+    var thePath:CGPath
     init(_ x:Int = 0, _ y:Int = 0,_ width:Int = 100, _ height:Int = 100, _ color:NSColor = NSColor.blueColor()) {
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.color = color
-        self.path = CGPathParser.rect(CGFloat(width/2),CGFloat(height/2))//Shapes
+        self.thePath = CGPathParser.rect(CGFloat(width/2),CGFloat(height/2))//Shapes
         super.init()
         self.wantsLayer = false//this avoids calling drawLayer() and enables drawingRect()
         needsDisplay = true;
@@ -32,11 +32,11 @@ class GraphicsTest:Graphic{
     override func drawRect(dirtyRect: NSRect) {
         Swift.print("GraphicsTest.drawRect: " )
         
-        CGPathModifier.translate(&path,CGFloat(x),CGFloat(y))//Transformations
+        CGPathModifier.translate(&thePath,CGFloat(x),CGFloat(y))//Transformations
         //graphics.line(12)//Stylize the line
         
         graphics.fill(color)//Stylize the fill
-        graphics.draw(path)//draw everything
+        graphics.draw(thePath)//draw everything
         
         //super.drawRect(dirtyRect)
     }
