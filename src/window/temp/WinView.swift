474641e5b3d0e823028e262054a7dde22e56e400
Eonist <unicornhive@gmail.com>
2015-11-17 05:58:03 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -32,7 +32,8 @@ class WinView:FlippedView{
         Swift.print(String(Trig.pi*2))
         let position = PointParser.polarPoint(100,-Trig.pi*2)
         let p:CGPoint = CGPointMake(0, 0)
-        DegreesToRadian(180)
+        
+        
         Swift.print(String(position))
         CGPathModifier.translate(&circle.path,CGFloat(position.x),CGFloat(position.y))//Transformations
         circle.graphics.draw(circle.path)
