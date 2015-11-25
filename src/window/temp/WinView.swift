f02bae1a52f954dfd0ceebc4634f76ad2d9c78d9
Eonist <unicornhive@gmail.com>
2015-11-25 09:02:21 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -28,7 +28,7 @@ class WinView:FlippedView{
      *
      */
     func gradientTest(){
-        let css:String = "Element{fill:linear-gradient(top,#BCD5EE 1 0.0087,#BAD4EE 1 0.0371,#B4CEEB 1 0.0473,#A8C4E7 1 0.0546,#98B6E0 1 0.0605,#98B5E0 1 0.0607,#96B4DF 1 0.2707,#8EB0DD 1 0.3632,#81A9DA 1 0.4324,#6EA0D6 1 0.4855,#538ECB 1 0.5087,#8ABBE3 1 0.8283,#A8D6EF 1 1);fill-alpha:0.5;line:gray;line-alpha:0.6;line-thickness:1px;corner-radius:4px;line-offset-type:center;}"//"Blob{fill:green;fill-alpha:1.0;corner-radius:10px;}"//
+        let css:String = "Element{fill:linear-gradient(top,#BCD5EE 1 0.0087,#98B5E0 1 0.0607,#A8D6EF 1 1);fill-alpha:0.5;line:gray;line-alpha:0.6;line-thickness:1px;corner-radius:4px;line-offset-type:center;}"//"Blob{fill:green;fill-alpha:1.0;corner-radius:10px;}"//
         //css +=
         StyleManager.addStyle(css)
         let element = Element(96,24,50,50)
