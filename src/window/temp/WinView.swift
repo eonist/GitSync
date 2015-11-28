ca1ce276dec5992cd97bb970feed5ec7a46d4f28
Eonist <unicornhive@gmail.com>
2015-11-28 08:54:23 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -138,7 +138,8 @@ class WinView:FlippedView{
         //line:linear-gradient(bottom,green,orange);
         //fill:linear-gradient(top,red,blue);
         //line-alpha:1.0;
-        let css:String = "Element{fill:red;fill-alpha:0.5;line:blue;line-alpha:0.5;line-thickness:20px;corner-radius:10px;line-offset-type:center;}"//"Blob{fill:green;fill-alpha:1.0;corner-radius:10px;}"//
+        //var css:String = "Element{fill:red;fill-alpha:0.5;line:blue;line-alpha:0.5;line-thickness:20px;corner-radius:10px;line-offset-type:center;}"//"Blob{fill:green;fill-alpha:1.0;corner-radius:10px;}"//
+        let css = "Element{fill:red;}"
         let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
         StyleManager.addStyle(styleCollection.styles)
         let element = Element(200,200,50,50)
