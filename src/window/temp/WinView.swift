d534c30a5f79eb19e758c0fec7ead3e510c092e0
Eonist <unicornhive@gmail.com>
2015-11-28 13:09:08 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -35,13 +35,13 @@ class WinView:FlippedView{
     }
     func lineTest(){
         let line = LineGraphic(CGPoint(20,20),CGPoint(50,50))
-        line.setPosition(CGPoint(150,150))
+        //line.setPosition(CGPoint(150,150))
         line.initialize()
     }
     func cricleTest(){
         let circle = CircleGraphic(10)
         
-        circle.setPosition(CGPoint(50,50))
+        circle.graphics.setPosition(CGPoint(50,50))
         circle.initialize()
     }
     func gradientBoxTest(){
