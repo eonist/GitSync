c5db6bbc456c2840b814555a42e8633c8eedb8e8
Eonist <unicornhive@gmail.com>
2015-11-29 08:20:22 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -21,7 +21,7 @@ class WinView:FlippedView{
     }
     func createContent(){
         
-        //gradientTest()
+        gradientTest()
         //testTextButton()
         //testButton()
         //testTextElement()
