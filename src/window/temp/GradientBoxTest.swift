808e69373b4454af9906861335e0ebaf1811c2c7
Eonist <unicornhive@gmail.com>
2015-11-29 07:59:31 +0100

    Files modified: 2
    Modified 2 files:src/window/temp/GradientBoxTest.swiftsrc/window/temp/WinView.swift

--- src/window/temp/GradientBoxTest.swift
+++ src/window/temp/GradientBoxTest.swift
@@ -1,5 +1,46 @@
-import Foundation
+import Cocoa
 
 class GradientBoxTest:View {
-
+    override func drawRect(dirtyRect: NSRect) {
+        Swift.print("WinView.drawRect()")
+        createContent()
+    }
+    func createContent(){
+        let rect = RectGraphic(200,100)
+        rect.setPosition(CGPoint(100,100))
+        rect.initialize()
+        
+        let cgRect:CGRect = CGRect(rect.getPosition().x,rect.getPosition().y,rect.width,rect.height)
+        Swift.print(cgRect.corners.count)
+        
+        for corner:CGPoint in cgRect.corners{
+            //Swift.print(corner)
+            let circle = CircleGraphic(10)
+            circle.setPosition(corner)
+            circle.initialize()
+        }
+        
+        let centerCircle = CircleGraphic(10,NSColor.blueColor())
+        centerCircle.setPosition(cgRect.center)
+        centerCircle.initialize()
+        
+        let angle = -135*㎭
+        Swift.print("angle: " + "\(angle)")
+        let polarPoint = cgRect.center.polarPoint(150, angle)
+        let line = LineGraphic(cgRect.center,polarPoint)
+        line.initialize()
+        
+        Swift.print("cgRect.topLeft: " + "\(cgRect.topLeft)")
+        let temp = PointParser.directionalAxisDifference(cgRect.center, cgRect.topLeft, angle)
+        Swift.print(temp)
+        //create Line and Circle And Rect for easy testing, and document it wells, and it must be simple to add styles to them
+        
+        //create a visual feedback test with a line, corners as circles etc
+        //test different rotations
+        //for p1:
+        //rotate all corners from the center to the negative rotation value
+        //Measure which point has the lowest y value.
+        //measure the distance between this y value and the center y value
+        //use this distance to find the p1 and p2
+    }
 }
--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -12,13 +12,15 @@ class WinView:FlippedView{
     }
     override func drawRect(dirtyRect: NSRect) {
         Swift.print("WinView.drawRect()")
-        gradientBoxTest()
+        //gradientBoxTest()
         
         //lineTest()
         //rectTest()
         //cricleTest()
     }
     func createContent(){
+        let gradientBoxTest = GradientBoxTest()
+        addSubview(gradientBoxTest)
         //gradientTest()
         //testTextButton()
         //testButton()
@@ -42,45 +44,6 @@ class WinView:FlippedView{
         circle.initialize()
     }
     func gradientBoxTest(){
-        
-        let rect = RectGraphic(200,100)
-        rect.setPosition(CGPoint(100,100))
-        rect.initialize()
-        
-        let cgRect:CGRect = CGRect(rect.getPosition().x,rect.getPosition().y,rect.width,rect.height)
-        Swift.print(cgRect.corners.count)
-        
-        
-        
-        for corner:CGPoint in cgRect.corners{
-            //Swift.print(corner)
-            let circle = CircleGraphic(10)
-            circle.setPosition(corner)
-            circle.initialize()
-        }
-        
-        let centerCircle = CircleGraphic(10,NSColor.blueColor())
-        centerCircle.setPosition(cgRect.center)
-        centerCircle.initialize()
-        
-        let angle = -135*㎭
-        Swift.print("angle: " + "\(angle)")
-        let polarPoint = cgRect.center.polarPoint(150, angle)
-        let line = LineGraphic(cgRect.center,polarPoint)
-        line.initialize()
-        
-        Swift.print("cgRect.topLeft: " + "\(cgRect.topLeft)")
-        let temp = PointParser.directionalAxisDifference(cgRect.center, cgRect.topLeft, angle)
-        Swift.print(temp)
-        //create Line and Circle And Rect for easy testing, and document it wells, and it must be simple to add styles to them
-        
-        //create a visual feedback test with a line, corners as circles etc
-            //test different rotations
-        //for p1:
-            //rotate all corners from the center to the negative rotation value
-            //Measure which point has the lowest y value.
-                //measure the distance between this y value and the center y value
-            //use this distance to find the p1 and p2
         
     }
     /**
