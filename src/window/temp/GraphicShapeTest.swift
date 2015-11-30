d6dd1af3563128281f719de60890bf1812c24dc3
Eonist <unicornhive@gmail.com>
2015-11-30 05:29:33 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/GraphicShapeTest.swift

--- src/window/temp/GraphicShapeTest.swift
+++ src/window/temp/GraphicShapeTest.swift
@@ -1,5 +1,18 @@
 import Foundation
 
 class GraphicShapeTest {
-
+    func rectTest(){
+        let rect = RectGraphic(300,300)
+        //line.setPosition(CGPoint(150,150))
+        rect.initialize()
+    }
+    func lineTest(){
+        let line = LineGraphic(CGPoint(20,20),CGPoint(50,50))
+        //line.setPosition(CGPoint(150,150))
+        line.initialize()
+    }
+    func cricleTest(){
+        let circle = CircleGraphic(10)
+        circle.initialize()
+    }
 }
