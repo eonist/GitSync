1e05486e526d3fb32625fa158b170c9dde1958e1
Eonist <unicornhive@gmail.com>
2015-12-02 08:57:24 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/TestSelectGroup.swift

--- src/window/temp/TestSelectGroup.swift
+++ src/window/temp/TestSelectGroup.swift
@@ -1,5 +1,25 @@
 import Foundation
 
-class TestSelectGroup {
+class TestSelectGroup :View{
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        createContent()
+    }
+    func createContent(){
+        
+    }
 
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+
+    /*
+     * let radioButtonGroup = RadioButtonGroup([rb1,rb2, rb3]);
+     * NSNotificationCenter.defaultCenter().addObserver(radioButtonGroup, selector: "onSelect:", name: SelectGroupEvent.select, object: radioButtonGroup)
+     * func onSelect(sender: AnyObject) { Swift.print("Event: " + ((sender as! NSNotification).object as ISelectable).isSelected}
+     */
+    
+    
+    //create a view
+    //make it work
+    //add 2 nice buttons
+    //try select group
 }
