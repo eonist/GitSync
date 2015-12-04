2e29a8b843e3a2749e75ebe128b2220cb71ee033
Eonist <unicornhive@gmail.com>
2015-12-04 18:47:57 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -25,6 +25,7 @@ class NotificationTest:NSView{
 class WinView:FlippedView{
     var element:Element?
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    override var wantsUpdateLayer:Bool{return false;}
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
         wantsLayer = true
@@ -33,10 +34,6 @@ class WinView:FlippedView{
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    /**
-     *
-     */
-    
     override func drawRect(dirtyRect: NSRect) {
         Swift.print("WinView.drawRect()")
         super.drawRect(dirtyRect)
