7a92951940005750a3cb0ca08de5ef9284d643e8
Eonist <unicornhive@gmail.com>
2015-12-09 17:53:49 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -20,7 +20,7 @@ class WinView:FlippedView{
     func createContent(){
         
        
-        //testDropShadow()
+        testDropShadow()
         //testOverlapping()
         //styleTesting()
         //testTabBar()
@@ -29,7 +29,7 @@ class WinView:FlippedView{
         //addSubview(gradientBoxTest)
         //gradientTest()
         //testTextButton()
-        testButton()
+        //testButton()
         //testTextElement()
         //testRotation()
         //testSkin()
@@ -235,10 +235,10 @@ class WinView:FlippedView{
         //Swift.print("WinView.onButtonDown() Sender: " + String(sender))
     }
     func testButton(){
-        let css:String = "Button{fill:red;line:red;line-offset-type:inside;line-thickness:4px;drop-shadow:drop-shadow(0px 0 #000000 0.9 4 4 1 2 true);}Button:over{fill:yellow;drop-shadow:none;}Button:down{fill:green;drop-shadow:drop-shadow(0px 0 #000000 0.4 4 4 1 2 true);}"//
+        let css:String = "Button{fill:red;line:blue;line-offset-type:inside;line-thickness:15px;drop-shadow:drop-shadow(0px 0 #000000 0.9 4 4 1 2 true);}Button:over{fill:yellow;drop-shadow:none;}Button:down{fill:green;drop-shadow:drop-shadow(0px 0 #000000 0.4 4 4 1 2 true);}"//
         let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
         StyleManager.addStyle(styleCollection.styles)
-        let button = Button(200,40)
+        let button = Button(200,200,20,20)
         //button.setPosition(CGPoint(120,120))
         self.addSubview(button)
     }
