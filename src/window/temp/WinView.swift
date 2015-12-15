606eb74ff9401ef8b81901c84028dcf829b2d347
Eonist <unicornhive@gmail.com>
2015-12-15 20:36:28 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -23,7 +23,7 @@ class WinView:FlippedView{
         //Continue here:
         
         
-        //1. then make the BaseGraphic2 with a stroke and a fill CALayer's
+        //1. then make the BaseGraphic2 with a stroke and a fill CALayer's (we could use CAShapeLayer, but we need CGCOntext to get the gradientstroke)
         //2. get the outer shadow to work on the layer and the inner shadow to work on the fill only
         //3. start making other Elements
 
