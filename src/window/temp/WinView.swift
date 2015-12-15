4784a3ca77924b7e163d8e2cd10d77d9e88c31aa
Eonist <unicornhive@gmail.com>
2015-12-15 16:41:42 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView.swift

--- src/window/temp/WinView.swift
+++ src/window/temp/WinView.swift
@@ -18,6 +18,17 @@ class WinView:FlippedView{
         //gradientBoxTest()
     }
     func createContent(){
+        //Continue here:
+        //1. make this method so that you dont need to call super, do the same with mouseOut
+        //2. finish the interactiveview and then test it on button etc.
+        //3. then make the GraphicSkin with a stroke and a fill CALayer
+        //4. get the outer shadow to work on the layer and the inner shadow to work on the fill only
+        //5. start making other Elements
+        //6. mouseUpInside and mouseUpOutSide should work how the current mouseOut and mouseOver works. Respecting visible part of the view.
+        //7. mouseUpInside and mouseUpOutside is so general that we can include it in INteractiveView and thus the Button class will be alot simpler, which is good
+        
+
+        
         testInteractiveView()
         //testOverLappingNSViews()
         //testCALayer()
