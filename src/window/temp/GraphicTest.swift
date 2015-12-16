d043753fe497439113947deeb4e04e25a4d2cfb9
Eonist <unicornhive@gmail.com>
2015-12-16 16:44:54 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/GraphicTest.swift

--- src/window/temp/GraphicTest.swift
+++ src/window/temp/GraphicTest.swift
@@ -42,7 +42,9 @@ class GraphicsTest:Graphic{
         
         
         fillShape.graphics.fill(color)//Stylize the fill
-        //fillShape.graphics.draw(thePath)//draw everything
+        
+        fillShape.graphics.draw(thePath)//draw everything
+        fillShape.graphics.stopFill()
         
         fillShape.graphics.line(15)//Stylize the line
         fillShape.graphics.draw(thePath)//draw everything
