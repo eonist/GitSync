43ff1805911325a78434aed1905425621cc57486
Eonist <unicornhive@gmail.com>
2015-12-17 12:59:11 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/GraphicTest.swift

--- src/window/temp/GraphicTest.swift
+++ src/window/temp/GraphicTest.swift
@@ -16,6 +16,8 @@ class GraphicsTest:Graphic{
         self.thePath = CGPathParser.rect(CGFloat(width/*/2*/),CGFloat(height/*/2*/))//Shapes
         let offsetType:OffsetType = OffsetType(OffsetType.center)
         
+        //Continue here: keep testing, but this seems to be it!!!
+        
         offsetType.top = OffsetType.outside
         offsetType.bottom = OffsetType.outside
         offsetType.left = OffsetType.inside
