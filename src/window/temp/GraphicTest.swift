fbc2a46ef9e1c9f434f3ed1d82ba98fdab258469
Eonist <unicornhive@gmail.com>
2015-12-17 05:45:28 +0100

    Files modified: 1
    Modified 1 file: src/window/temp/GraphicTest.swift

--- src/window/temp/GraphicTest.swift
+++ src/window/temp/GraphicTest.swift
@@ -21,18 +21,6 @@ class GraphicsTest:Graphic{
         //needsDisplay = true;
         //Swift.print("graphics: " + String(graphics.context))
         
-        //continue here:
-        
-        //move this into the GraphicTest class
-        //start making the alignment classes for the stroke, maybe you should base the stroke path on the fillpath
-        //remember that the Graphic class shouldnt do any path drawing etc. this is the task of the subclass or utility methods
-        
-        let a:Shape = fillShape//TempShape()
-        a.path = CGPathParser.rect(CGFloat(50/*/2*/),CGFloat(50/*/2*/))//Shapes
-        a.frame = CGRect(12,12,50,50);
-        
-        
-        a.display()
         //a.masksToBounds = false
     }
     /*
