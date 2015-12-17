803d3a7f4a226d613e8f15a1c3be7b2c8eae9654
Eonist <unicornhive@gmail.com>
2015-12-17 12:55:15 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/GraphicTest.swift

--- src/window/temp/GraphicTest.swift
+++ src/window/temp/GraphicTest.swift
@@ -15,12 +15,12 @@ class GraphicsTest:Graphic{
         self.color = color
         self.thePath = CGPathParser.rect(CGFloat(width/*/2*/),CGFloat(height/*/2*/))//Shapes
         let offsetType:OffsetType = OffsetType(OffsetType.center)
-        /*
-        offsetType.top = OffsetType.outside
-        offsetType.bottom = OffsetType.center
+        
+        offsetType.top = OffsetType.center
+        offsetType.bottom = OffsetType.outside
         offsetType.left = OffsetType.center
         offsetType.right = OffsetType.center
-        */
+        /**/
         super.init(FillStyle(NSColor.yellowColor().alpha(0.5)),LineStyle(20,NSColor.blueColor().alpha(0.5)),offsetType)
         
         //frame = NSRect(x: x,y: y,width: width,height: height)
