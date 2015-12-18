9fdd57544e0aa344687a582369aad5c30a93db5f
Eonist <unicornhive@gmail.com>
2015-12-18 15:27:04 +0100

    Files modified: 2
    Modified 2 files:src/window/temp/TempGraphic.swiftsrc/window/temp/WinView2.swift

--- src/window/temp/TempGraphic.swift
+++ src/window/temp/TempGraphic.swift
@@ -1,5 +1,60 @@
-import Foundation
+import Cocoa
 
-class TempGraphic {
-
+class TempGraphic:Graphic{
+    var x:Int
+    var y:Int
+    var width:Int
+    var height:Int
+    var color:NSColor
+    var thePath:CGMutablePath
+    init(_ x:Int = 0, _ y:Int = 0,_ width:Int = 0, _ height:Int = 0, _ color:NSColor = NSColor.blueColor()) {
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.thePath = CGPathParser.rect(CGFloat(width/*/2*/),CGFloat(height/*/2*/))//Shapes
+        let offsetType:OffsetType = OffsetType(OffsetType.center)
+        
+        /*
+        offsetType.top = OffsetType.inside
+        offsetType.bottom = OffsetType.outside
+        offsetType.left = OffsetType.inside
+        offsetType.right = OffsetType.outside
+        */
+        /**/
+        super.init(FillStyle(NSColor.yellowColor().alpha(0.5)),LineStyle(20,NSColor.blueColor().alpha(0.5)),offsetType)
+        
+        //frame = NSRect(x: x,y: y,width: width,height: height)
+        //self.wantsLayer = true//this avoids calling drawLayer() and enables drawingRect()
+        //needsDisplay = true;
+        //Swift.print("graphics: " + String(graphics.context))
+        
+        //a.masksToBounds = false
+        
+        let rect:CGRect = CGRect(100,100,200,200)
+        
+        fillShape.path = CGRect(0,0,rect.width,rect.height).path/*Draws in the local coordinate space of the shape*/
+        fillShape.frame = rect/*,position and set the size of the frame*/
+        fillShape.display()/*draw the fileShape*/
+        //alignStroke(self)
+        let offsetRects = RectGraphicUtil.offsetRect(fillShape.frame, lineShape.lineStyle!, lineOffsetType)
+        lineShape.frame = offsetRects.frameRect
+        lineShape.path = offsetRects.lineRect.path
+        lineShape.display()/*draw the lineShape*/
+        
+        
+        
+        //continue here
+        //return a tuple with (frameRect and pathRect)
+        //Mimic the current RectUtil setup
+        //support all the different left right top bottom etc.
+        //test in the current element framework with a simple shape
+        //the move on to do the same to round rect and circle and ellipse
+        //add support for positioning
+        //test with the tabbar example
+        //move on
+        
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by super class*/
 }
--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -14,23 +14,13 @@ class WinView2:FlippedView{
     }
     override func drawRect(dirtyRect: NSRect) {
         //Swift.print("WinView.drawRect()")
-        //super.drawRect(dirtyRect)
-        //gradientBoxTest()
-        //graphicsTest()
     }
     func createContent(){
         //testBorderAligment()
         testGraphic()
     }
-    func testBorderAligment(){
-        let css = "Element{fill:orange;fill-alpha:0.5;line:blue;line-alpha:0.5;line-offset-type:center;line-thickness:20px;}"
-        let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
-        StyleManager.addStyle(styleCollection.styles)
-        let element = Element(00,00,100,100)
-        self.addSubview(element)
-    }
     func testGraphic(){
-        let a = GraphicsTest()
+        let a = TempGraphic()
         self.addSubview(a)
     }
 }
