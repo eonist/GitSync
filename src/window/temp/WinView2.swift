f81ad8fced1ac10cc8d624dc74bf38d60bf58c73
Eonist <unicornhive@gmail.com>
2015-12-19 09:36:10 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView2.swift

--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -31,6 +31,7 @@ class WinView2:FlippedView{
         //let rect = CGRect(0,0,100,100)
         let b = TempGraphic()
         a.addSubview(b)
+        a.layer!.masksToBounds = false
         b.frame.origin = (NSPoint(-50,50))
         b.layer!.masksToBounds = false
     }
