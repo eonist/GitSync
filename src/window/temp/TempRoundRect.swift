ef1cf9ae83d00f7959b813cf8cbc578654989735
Eonist <unicornhive@gmail.com>
2015-12-20 13:07:21 +0100

    Files modified: 1
    Modified 1 file: src/window/temp/TempRoundRect.swift

--- src/window/temp/TempRoundRect.swift
+++ src/window/temp/TempRoundRect.swift
@@ -1,5 +1,21 @@
-import Foundation
+import Cocoa
 
-class TempRoundRect {
+class TempRoundRect:Graphic {
+    init(){
+        let offsetType:OffsetType = OffsetType(OffsetType.outside)
+        
+        /*
+        offsetType.top = OffsetType.outside
+        offsetType.bottom = OffsetType.inside
+        offsetType.left = OffsetType.outside
+        offsetType.right = OffsetType.inside
+        */
+        let fillStyle = FillStyle(NSColor.yellowColor().alpha(0.5))
+        let lineStyle = LineStyle(20,NSColor.blueColor().alpha(0.5))
+        super.init(fillStyle,lineStyle,offsetType)
+    }
 
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }
