bed0d329863fde68cacf312b41bcc878953c62e6
Eonist <unicornhive@gmail.com>
2015-12-20 09:44:21 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView2.swift

--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -20,10 +20,10 @@ class WinView2:FlippedView{
         //testGraphic()
     }
     func testLayerBackedElement(){
-        let css = "Element{fill:orange;fill-alpha:0.5;line:blue;line-alpha:0.5;line-offset-type:outside;line-thickness:20px;}"
+        let css = "Element{fill:green;fill-alpha:0.5;line:red;line-alpha:0.5;line-offset-type:outside;line-thickness:20px;}"
         let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
         StyleManager.addStyle(styleCollection.styles)
-        let element = Element(200,200)
+        let element = Element(200,200,100,100)
         self.addSubview(element)
     }
     func testGraphic(){
