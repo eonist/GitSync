24279f51264b6b0e8bd8de14889e11f8906781e9
Eonist <unicornhive@gmail.com>
2015-12-21 15:52:45 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView2.swift

--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -49,9 +49,9 @@ class WinView2:FlippedView{
         //fill:radial-gradient(50% 20% 40% 120% 90 -1, white 1 0,white 0.33 0.4724,white 0 1);
         
         var fillStyle = FillStyle(NSColor.yellowColor().alpha(0.5))
-        let gradient:IGradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor],[],GradientType.Radial,CGPoint(0,0),CGPoint(0,0),CGSize(1,1))
+        let gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor],[],GradientType.Radial,1.5707963267949,CGPoint(0,0),CGPoint(0,0),CGSize(0,0),CGSize(1,1))
 
-        fillStyle = GradientFillStyle(Gradient(),NSColor.clearColor());
+        fillStyle = GradientFillStyle(gradient,NSColor.clearColor());
         
         let lineStyle = LineStyle(20,NSColor.blueColor().alpha(0.5))
         a = Graphic(fillStyle,lineStyle)
