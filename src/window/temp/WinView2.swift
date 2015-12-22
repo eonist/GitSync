ba4ddf9cd10c176d2078bf99a4881fe5fc74fbe2
Eonist <unicornhive@gmail.com>
2015-12-22 09:46:09 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView2.swift

--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -1,33 +1,6 @@
 import Cocoa
 class TempGraphix:FlippedView{
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-        self.wantsLayer = true/*if true then view is layer backed*/
-        layer = CALayer()/*needs to be layer-hosted so that we dont get clipping of children*/
-        layer!.masksToBounds = false//this is needed!!!
-        
-        
-        let fillStyle = FillStyle(NSColor.yellowColor().alpha(0.5))
-        //let gradient = Gradient([NSColor.blueColor().CGColor,NSColor.redColor().CGColor],[],GradientType.Radial,1.5707963267949,CGPoint(0,0),CGPoint(0,0),CGSize(0,0),CGSize(1,1))
-        //gradient.gradientType = GradientType.Axial
-        //try to make a Linear gradient and see if that gets clipped
-        
-        //fillStyle = GradientFillStyle(gradient,NSColor.clearColor());
-        
-        let lineStyle = LineStyle(20,NSColor.blueColor().alpha(0.5))
-        //a = Graphic(fillStyle,nil)
-        
-        //test how easy it is to create a rectGraphic etc
-        let baseGraphic:BaseGraphic = BaseGraphic(fillStyle,nil,OffsetType(OffsetType.outside))
-        let rectGraphic:RectGraphic = RectGraphic(200,200,baseGraphic)
-        //baseGraphic.lineShape.delegate = self
-        baseGraphic.fillShape.delegate = self
-        //baseGraphic.layer!.delegate = self
-        rectGraphic.draw()
-        addSubview(baseGraphic)
-        
-
-    }
+    
     override func drawLayer(layer: CALayer, inContext ctx: CGContext) {
         Swift.print("TempGraphix.drawLayer")
     }
@@ -71,12 +44,12 @@ class WinView2:FlippedView{
         //try to implement radial gradient as it was in the original concept
         //make the Radio bullet example and post a gif brewery animation on twitter and on github (maybe add a touch of animation?)
         //tomorrow implement the svg engine
-        radialGradientTest()
+        //radialGradientTest()
         //testTabBar()
         //testDropShadow()
         //testOverlapping()
         //testTextButton()
-        //testButton()
+        testButton()
         //testTempRoundRect()
         //testLayerBackedElement()
         //testGraphic()
