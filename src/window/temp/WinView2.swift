16b06e1fd3e88addd309184837f73d5c9b4a6e2a
Eonist <unicornhive@gmail.com>
2015-12-24 08:25:54 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView2.swift

--- src/window/temp/WinView2.swift
+++ src/window/temp/WinView2.swift
@@ -112,7 +112,7 @@ class WinView2:FlippedView{
         
         
         
-        let css:String = "Element{fill:radial-gradient(50% 50% 100% 100% 0 -1, red 1 0,green 1 1);}"//,blue 0.33 0.4724
+        let css:String = "Element{fill:radial-gradient(50% 50% 200% 100% 0 -1, red 1 0,green 1 1);}"//,blue 0.33 0.4724
         let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
         let style:IStyle = styleCollection.getStyle("Element")!
         let styleProperty:IStyleProperty = style.getStyleProperty("fill")!
