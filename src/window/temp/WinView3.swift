a54b0a71d38c9939700f22962e92ec316f6eaab8
Eonist <unicornhive@gmail.com>
2015-12-31 14:29:28 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -37,7 +37,8 @@ class WinView3:NSView {
     }
     func regExpBackRefTest(){
         
-        //continue here: figure out how to backref in swift, see old notes etc. Maybe write an example of this. ?
+        //continue here: it may be the \\s or it may be the backref. Take a look at the css regexp and also do isolated test with capturing a single number with space etc
+        
         let str:String = "86.629,26.288 64,48.916 41.373,26.287 26.288,41.372 48.916,64 26.289,86.628   41.373,101.713 64,79.085 86.627,101.712 101.713,86.627 79.086,64 101.713,41.372"
         let part1:String = "(?<=^|\\,|\\s|px|\\b)"/*group 1, preseeding match must aprear but is not included in the final result */
         let part2:String = "\\-?\\d*?"/*optional minus sign followed by a digit zero or more times*/
