73614589ba7d9625c6736d1aaeae8e6403595a75
Eonist <unicornhive@gmail.com>
2015-12-31 11:07:06 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -9,8 +9,8 @@ class WinView3:NSView {
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     func createContent(){
         //continue here: make winview3.swift and start testing the SVGLib, you probably will need to look over some old notes
-        svgTest()
-        //regExpBackRefTest()
+        //svgTest()
+        regExpBackRefTest()
     }
     /**
      *
@@ -38,14 +38,14 @@ class WinView3:NSView {
     func regExpBackRefTest(){
         
         //continue here: figure out how to backref in swift, see old notes etc. Maybe write an example of this. ?
-        
+        let str:String = "86.629,26.288 64,48.916 41.373,26.287 26.288,41.372 48.916,64 26.289,86.628   41.373,101.713 64,79.085 86.627,101.712 101.713,86.627 79.086,64 101.713,41.372"
         let part1:String = "(?<=^|\\,|\\s|px|\\b)"/*group 1*/
         let part2:String = "\\-?\\d*?"
         let part3:String = "(\\.?)"/*group 2*/
         let part4:String = "(($1)\\d+?)"/*group 3*/
         let part5:String = "(?=px|\\s|\\,|\\-|$)"/*group 4*/
         let pattern:String = part1 + part2 + part3 + part4 + part5
-        let stringArray:Array<String> = "".match(pattern);
+        let stringArray:Array<String> = str.match(pattern);
         Swift.print("stringArray: " + "\(stringArray)")
         //let array:Array<CGFloat> = stringArray.map {CGFloat(Double($0)!)}//<--temp fix
     }
