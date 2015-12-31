ab4575d6cd588ceb4505b633ef65f4ad40e7794b
Eonist <unicornhive@gmail.com>
2015-12-31 14:44:52 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -12,7 +12,7 @@ class WinView3:NSView {
         //svgTest()
         //regExpBackRefTest()
         let str:String = " 12.15 "
-        Swift.print(str.test("\\s\\d+?(\\.?)(?($1)\\d+?)\\s"))
+        Swift.print(str.test("\\s\\d+?(\\.?)(($\\1)\\d+?)\\s"))
     }
     /**
      *
