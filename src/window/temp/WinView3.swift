c2a7afa2e739732c5440971c69dd12599e3cce68
Eonist <unicornhive@gmail.com>
2015-12-31 11:10:55 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -39,9 +39,9 @@ class WinView3:NSView {
         
         //continue here: figure out how to backref in swift, see old notes etc. Maybe write an example of this. ?
         let str:String = "86.629,26.288 64,48.916 41.373,26.287 26.288,41.372 48.916,64 26.289,86.628   41.373,101.713 64,79.085 86.627,101.712 101.713,86.627 79.086,64 101.713,41.372"
-        let part1:String = "(?<=^|\\,|\\s|px|\\b)"/*group 1, preceding */
-        let part2:String = "\\-?\\d*?"
-        let part3:String = "(\\.?)"/*group 2*/
+        let part1:String = "(?<=^|\\,|\\s|px|\\b)"/*group 1, preseeding match must aprear but is not included in the final result */
+        let part2:String = "\\-?\\d*?"/*optional minus sign followed by a digit zero or more times*/
+        let part3:String = "(\\.?)"/*group 2, a dot*/
         let part4:String = "(($1)\\d+?)"/*group 3*/
         let part5:String = "(?=px|\\s|\\,|\\-|$)"/*group 4*/
         let pattern:String = part1 + part2 + part3 + part4 + part5
