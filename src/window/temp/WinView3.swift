aa00d5f86fba7bc6a5bf8f39841778efa18b7b86
Eonist <unicornhive@gmail.com>
2015-12-31 09:03:27 +0100

    Files modified: 2
    Modified 2 files:src/window/Win.swiftsrc/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -8,6 +8,15 @@ class WinView3:NSView {
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     func createContent(){
+        //continue here: make winview3.swift and start testing the SVGLib, you probably will need to look over some old notes
         svgTest()
+    }
+    /**
+     *
+     */
+    func svgTest(){
+        let path = "~/Desktop/plus.svg".tildePath
+        let content = FileParser.content(path)
+        Swift.print("content: " + "\(content)")
     }
 }
