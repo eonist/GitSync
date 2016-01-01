32d371178b4196f417b9922d7c0bc1f560fe7217
Eonist <unicornhive@gmail.com>
2016-01-01 06:28:40 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -64,13 +64,13 @@ class WinView3:NSView {
         
         //continue here: it may be the \\s or it may be the backref. Take a look at the css regexp and also do isolated test with capturing a single number with space etc
         
-        let str:String = "86.629,26.288 64,48.916 41.373,26.287 26.288,41.372 48.916,64 26.289,86.628   41.373,101.713 64,79.085 86.627,101.712 101.713,86.627 79.086,64 101.713,41.372"
+        let str:String = "86,26.288 64,48.916 41.373,26.287 -26.288,41.372 48.916,6 26.289,86.628   41.373,101.713 64,79.085 86.627,101.712 101.713,86.627 79.086,64 101.713,41.372"
         let part1:String = "(?<=^|\\,|\\s|px|\\b)"/*group 1, preseeding match must aprear but is not included in the final result */
-        let part2:String = "\\-?\\d*?"/*optional minus sign followed by a digit zero or more times*/
-        let part3:String = "(\\.?)"/*group 2, optional dot char*/
-        let part4:String = "(($1)\\d+?)"/*group 3, if there is a match in group 1 and followed by a digit more than zero times*/
+        //let part2:String = "\\-?\\d*?"/*optional minus sign followed by a digit zero or more times*/
+        //let part3:String = "(\\.?)"/*group 2, optional dot char*/
+        //let part4:String = "(($1)\\d+?)"/*group 3, if there is a match in group 1 and followed by a digit more than zero times*/
         let part5:String = "(?=px|\\s|\\,|\\-|$)"/*group 4,the subseeding pattern must apear, but is not included in the match, the pattern is: px*/
-        let pattern:String = part1 + part2 + part3 + part4 + part5
+        let pattern:String = part1 + digitAssertPattern + part5
         let stringArray:Array<String> = str.match(pattern);
         Swift.print("stringArray: " + "\(stringArray)")
         //let array:Array<CGFloat> = stringArray.map {CGFloat(Double($0)!)}//<--temp fix
