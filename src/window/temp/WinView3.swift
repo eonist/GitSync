95f32dc90f19c23fa3c48eef4079aa3b0ac6b7e2
Eonist <unicornhive@gmail.com>
2016-01-01 06:24:48 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -8,22 +8,34 @@ class WinView3:NSView {
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     func createContent(){
-        //continue here: make winview3.swift and start testing the SVGLib, you probably will need to look over some old notes
+        
+        
+        //debugRegExpDigitPattern()
         //svgTest()
-        //regExpBackRefTest()
+        regExpBackRefTest()
+        
+    }
+    func debugRegExpDigitPattern(){
         let testCases:Array<String> = ["2.3","2","44","22.11","-2.3","-2","-44","-22.11","20.",".10",".3","1.",".","-20.","-.10","-.3","-1.","-."]
         
         let g1:String = "(?=\\s|^)"/*must be preceeded by a space char or the begining*/
+        let g6:String = "(?=$)"/*followed by an end or */
+        
+        let pattern:String = g1 + digitAssertPattern + g6
+        for testCase in testCases{
+            Swift.print(testCase.test(pattern))
+        }
+    }
+    var digitAssertPattern:String{
+        //let g1:String = "(?=\\s|^)"/*must be preceeded by a space char or the begining*/
         let g2:String = "\\-*\\d+?"/*optional minus sign followed by one or more digits*/
         let g3:String = "(?=\\.\\d|\\s|,|$)"/*Must be followed by a dot with a digit or space, comma or end*/
         let g4:String = "((?<=\\d)\\.(?=\\d))*"/*zero or one dot char that must be preceeded with a digit and subseeded wutha digit*/
         let g5:String = "((?<=\\d\\.)\\d+?)*"/*zero or more digits that must be preceeded with a digit and a char*/
-        let g6:String = "(?=$)"/*followed by an end or */
+        //let g6:String = "(?=$)"/*followed by an end or */
         
-        let pattern:String = g1 + g2 + g3 + g4 + g5 + g6
-        for testCase in testCases{
-            Swift.print(testCase.test(pattern))
-        }
+        let pattern:String = /*g1 +*/ g2 + g3 + g4 + g5 /*+ g6*/
+        return pattern
     }
     /**
      *
