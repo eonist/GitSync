abedd6c7cd26d9750fd68a994b1e980d4175b003
Eonist <unicornhive@gmail.com>
2016-01-08 10:16:39 +0100

    Files modified: 2
    Modified 2 files:src/window/temp/TempGraphic.swiftsrc/window/temp/TempRoundRect.swift

--- src/window/temp/TempGraphic.swift
+++ src/window/temp/TempGraphic.swift
@@ -33,7 +33,7 @@ class TempGraphic:Graphic{
         
         let rect:CGRect = CGRect(x: 0,y: 0,width: width,height: height)//these values will be derived from somewhere else in the future
         
-        let fillOffsetRect = RectGraphicUtils.fillFrame(rect, self.lineStyle!, lineOffsetType)
+        let fillOffsetRect = RectGraphicUtils.fillFrame(rect, self.lineStyle!.thickness, lineOffsetType)
         let lineOffsetRect = RectGraphicUtils.lineOffsetRect(rect, self.lineStyle!, lineOffsetType)
         
         fillShape.frame = fillOffsetRect/*,position and set the size of the frame*/
--- src/window/temp/TempRoundRect.swift
+++ src/window/temp/TempRoundRect.swift
@@ -20,7 +20,7 @@ class TempRoundRect:Graphic {
         let fillet = Fillet(CGFloat(20))
         let rect:CGRect = CGRect(x: 0,y: 0,width: width,height: height)//these values will be derived from somewhere else in the future
         
-        let fillOffsetRect = RectGraphicUtils.fillFrame(rect, self.lineStyle!, lineOffsetType)
+        let fillOffsetRect = RectGraphicUtils.fillFrame(rect, self.lineStyle!.thickness, lineOffsetType)
         let lineOffsetRect = RectGraphicUtils.lineOffsetRect(rect, self.lineStyle!, lineOffsetType)
         
         fillShape.frame = fillOffsetRect/*,position and set the size of the frame*/
