904060a78a61bf1a5172bb754c26d13d7d8c65ee
Eonist <unicornhive@gmail.com>
2016-01-11 10:53:07 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -13,7 +13,7 @@ class WinView3:FlippedView {
         Swift.print("temp: " + "\(temp)")
         */
         
-        svgTest()
+        //svgTest()
         //let result = SVGPathParser.parameters("-75,53.571-147.029,36.822-185-89.748")//[-75.0, 53.571, -147.029, 36.822, -185.0, -89.748]
         //Swift.print("result: " + "\(result)")
 
