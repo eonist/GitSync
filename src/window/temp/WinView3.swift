3d64ec0b13bd7f6f8c04247e6e11b1b57fabf703
Eonist <unicornhive@gmail.com>
2016-01-12 06:37:50 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -40,7 +40,7 @@ class WinView3:FlippedView {
         
         let svg:SVG = SVGParser.svg(rootElement);
         //SVGParser.describeAll(svg)
-        SVGModifier.scale(svg, CGPoint(), CGPoint(2,2));
+        //SVGModifier.scale(svg, CGPoint(), CGPoint(2,2));
         addSubview(svg);
         
     }
