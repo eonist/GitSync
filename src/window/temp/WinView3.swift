220733ae6ef345bfce4d9bab3fa1b9b6a1a5d223
Eonist <unicornhive@gmail.com>
2016-01-17 15:35:22 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -67,7 +67,7 @@ class WinView3:FlippedView {
         //Swift.print("child.localName: " + "\(child.localName)")
         
         let svg:SVG = SVGParser.svg(rootElement);
-        SVGParser.describeAll(svg)
+        //SVGParser.describeAll(svg)
         SVGModifier.scale(svg, CGPoint(), CGPoint(4,4));
         addSubview(svg);
         
