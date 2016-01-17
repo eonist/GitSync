41bf9000cd5370c9d7dbe7e688165329a4478e4b
Eonist <unicornhive@gmail.com>
2016-01-17 22:05:12 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -64,9 +64,9 @@ class WinView3:FlippedView {
         //Swift.print("svg.subviews.count: " + "\(svg.subviews.count)")
         //Swift.print("svg.subviews[0]: " + "\(svg.subviews[0])")
         
-        //SVGUtils.describeAll(svg)
+        SVGUtils.describeAll(svg)
         //SVGModifier.scale(svg, CGPoint(), CGPoint(4,4));
-        SVGUtils.dsc(svg, CGPoint(), CGPoint(4,4));
+        //SVGUtils.dsc(svg, CGPoint(), CGPoint(4,4));
         
         addSubview(svg);
         
