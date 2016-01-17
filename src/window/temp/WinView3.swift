5ccc9901e7f2c66f5a0d438d34eb36a6df0698f0
Eonist <unicornhive@gmail.com>
2016-01-17 17:29:37 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -36,10 +36,10 @@ class WinView3:FlippedView {
         //path += "radial_test.svg"
         //path += "ellipse_2.svg"
         //path += "maximize.svg"
-        //path += "ellipse.svg"
+        path += "ellipse.svg"
         //path += "radial_gradient_rect.svg"
         //path += "gradient_rect_2.svg"
-        path += "circle.svg"
+        //path += "circle.svg"
         //path += "rect.svg"
         //path += "cross.svg"
         
@@ -63,7 +63,7 @@ class WinView3:FlippedView {
         //Swift.print("svg.subviews[0]: " + "\(svg.subviews[0])")
         
         //SVGModifier.describeAll(svg)
-        SVGModifier.scale(svg, CGPoint(), CGPoint(4,4));
+        //SVGModifier.scale(svg, CGPoint(), CGPoint(4,4));
         
         addSubview(svg);
         
