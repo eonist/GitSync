dc733b77e75f2ec3246bd0b4e9bb526a12fb6808
Eonist <unicornhive@gmail.com>
2016-01-19 21:28:49 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -9,8 +9,6 @@ class WinView3:FlippedView {
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     func createContent(){
         
-        
-        
         //depthTest()
         assetCSSTest()
         //svgTest()
