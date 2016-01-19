19261f862c793a5ea8b4ee64c6399a3aeaba79fc
Eonist <unicornhive@gmail.com>
2016-01-19 21:26:34 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -9,21 +9,6 @@ class WinView3:FlippedView {
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
     func createContent(){
         
-        //make the IconBar
-        //make the SearchBar
-        //make a window with custom svg buttons and custom white background (el capitan osx close max min button)
-        //make animation examples of the above, and figure out gif and mp4 anim on twitter, use dummy account to test
-        
-        //lever
-        //leverSpinner
-        //make anim of the above
-        
-        //implement the float system
-        //implement layer system for the GraphicSkin
-        //make the Radio bullet example and post a gif brewery animation on twitter and on github (maybe add a touch of animation?)
-        
-        
-        
         
         
         //depthTest()
