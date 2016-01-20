762cf12860e494188c59f6ea5acf28eba72b9354
Eonist <unicornhive@gmail.com>
2016-01-20 10:00:22 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView3.swift

--- src/window/temp/WinView3.swift
+++ src/window/temp/WinView3.swift
@@ -37,7 +37,7 @@ class WinView3:FlippedView {
     func assetCSSTest(){
         var path = "~/Desktop/icons/"
         path += "search.svg"
-        let css:String = "Element{fill:red," + path + " grey5;}"//,blue 0.33 0.4724
+        let css:String = "Element{fill:red," + path + " blue;}"//,blue 0.33 0.4724
         Swift.print("css: " + "\(css)")
         let styleCollection:IStyleCollection = CSSParser.styleCollection(css)
         let style:IStyle = styleCollection.getStyle("Element")!
