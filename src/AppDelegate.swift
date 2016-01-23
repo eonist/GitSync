7c800f539a3859f810ba0bbe90755e451561ab1b
Eonist <unicornhive@gmail.com>
2016-01-23 15:06:34 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -18,12 +18,12 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         //let winRect = NSMakeRect(100, 0, 100, 100)//TODO: us ns rect?
         //let styleMask:Int = NSBorderlessWindowMask|NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask
         //win = Win()
-        win = Window()
+        win = Window(600,400)
         Swift.print("---")
         //self.win!.contentView!.wantsLayer = true
         //self.win.setContentView(self.newContentView)
         self.win!.makeKeyAndOrderFront(self)
-        win!.makeMainWindow()
+        //win!.makeMainWindow()
         //print(aNotification)
         let app:NSApplication = aNotification.object as! NSApplication
         //print(app)
@@ -36,7 +36,7 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         }
         /**/
         Swift.print("app.windows.count: " + "\(app.windows.count)")
-        app.windows[0].close()//close the initial window
+        //app.windows[0].close()//close the initial window
         //app.windows[1].close()//close the initial window
         Swift.print("after close")
         Swift.print("app.windows.count: " + "\(app.windows.count)")
