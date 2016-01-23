0e967e40dfd3f1c54b89eb0e2623df5f7fbf9bad
Eonist <unicornhive@gmail.com>
2016-01-23 21:37:57 +0100

    Files modified: 1
    Modified 1 file:src/window/CustomWin.swift

--- src/window/CustomWin.swift
+++ src/window/CustomWin.swift
@@ -1,5 +1,22 @@
 import Foundation
 
-class CustomWin {
-
+class CustomWin:Window{
+    override func resolveSkin() {
+        self.contentView = WindowView(frame.width,frame.height)/*Sets the mainview of the window*/
+    }
+}
+ /*
+ * TODO: You should extend the window not the view
+ */
+class TestWinView:WindowView{
+    /**
+     * Add content here
+     */
+    override func resolveSkin() {
+        super.resolveSkin()
+        let section = Section(120,40)
+        addSubview(section)
+        //let button = Button(120,40)
+        //addSubview(button)
+    }
 }
