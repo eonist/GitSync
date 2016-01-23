f2cd0db0385b276acb951940621fb3e69856c22d
Eonist <unicornhive@gmail.com>
2016-01-23 15:37:04 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,10 +1,7 @@
 import Cocoa
-import Foundation
-
 @NSApplicationMain/*<-required by the application*/
 /*
  * The class for the application
- * TODO: figure out how not to open the default window
  */
 class AppDelegate: NSObject, NSApplicationDelegate{
     var win:NSWindow?// = WinUtils.win()
@@ -13,15 +10,15 @@ class AppDelegate: NSObject, NSApplicationDelegate{
      */
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         Swift.print("applicationDidFinishLaunching")
-        win = Window(600,400)//Win()
+        win = Win()//Window(600,400)/*Init the window*///Win()
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
     }
     /*
      * When the application closes
+     * NOTE: Insert code here to tear down your application
      */
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
         print("Good-bye")
     }
 }
