0eb43148677f683dca01c459e728c89015ab7b36
Eonist <unicornhive@gmail.com>
2016-01-23 09:04:59 +0100

    Files modified: 2
    Modified 2 files:src/window/Win.swiftsrc/window/temp/WinView4.swift

--- src/window/temp/WinView4.swift
+++ src/window/temp/WinView4.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class WinView4 {
+import Cocoa
 
+class WinView4:FlippedView {
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        createContent()
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    func createContent(){
+        Swift.print("hellow world")
+    }
 }
