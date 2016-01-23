83e736c9f8c9d402fe93807bb52dd1f4178b3f81
Eonist <unicornhive@gmail.com>
2016-01-23 15:31:07 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -24,24 +24,9 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         //self.win.setContentView(self.newContentView)
         self.win!.makeKeyAndOrderFront(self)/*this may be need when using non borderless windows etc*/
         //win!.makeMainWindow()
-        //print(aNotification)
+        Swift.print(aNotification)
         let app:NSApplication = aNotification.object as! NSApplication
-       
-        
-        /*for w in app.windows{
-        print("windowNumber: " + "\(w.windowNumber)")
-        app.windowWithWindowNumber(w.windowNumber)//this is how you can manage windows
-        }*/
-        /**/
-        //Swift.print("app.windows.count: " + "\(app.windows.count)")
-        app.windows[0].close()/*close the initial window*/
-        //app.windows[1].close()//close the initial window
-        //Swift.print("after close")
-        //Swift.print("app.windows.count: " + "\(app.windows.count)")
-        /*for w in app.windows{
-        print("windowNumber: " + "\(w.windowNumber)")
-        app.windowWithWindowNumber(w.windowNumber)//this is how you can manage windows
-        }*/
+        app.windows[0].close()/*close the initial non-optional default window*/
     }
     /*
      * When the application closes
