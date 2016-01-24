6e019deff21c5784ee3ef5e5c6f5bcd1ecd9bab7
Eonist <unicornhive@gmail.com>
2016-01-24 07:13:52 +0100

    Files modified: 1
    Modified 1 file:src/window/TranslucentWin.swift

--- src/window/TranslucentWin.swift
+++ src/window/TranslucentWin.swift
@@ -5,13 +5,13 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
      *
      */
     override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, `defer` flag: Bool) {
-        super.init(contentRect: Win.sizeRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask|NSFullSizeContentViewWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
+        super.init(contentRect: Win.sizeRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
         self.contentView!.wantsLayer = true;/*this can and is set in the view*/
-        self.backgroundColor = NSColor.grayColor()
-        self.opaque = true
+        self.backgroundColor = NSColor.greenColor().alpha(0.0)
+        self.opaque = false
         self.makeKeyAndOrderFront(nil)//moves the window to the front
         self.makeMainWindow()//makes it the apps main menu?
-        
+        self.appearance = NSAppearance(named: NSAppearanceNameVibrantDark)
         self.center()
         //view.wantsLayer = true;//this should be set in the iew not here
         //self.contentView = view
