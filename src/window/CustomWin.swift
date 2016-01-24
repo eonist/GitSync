c9cd07661e6549778447bdf95d9d02472eff9316
Eonist <unicornhive@gmail.com>
2016-01-24 14:37:22 +0100

    Files modified: 1
    Modified 1 file:src/window/CustomWin.swift

--- src/window/CustomWin.swift
+++ src/window/CustomWin.swift
@@ -2,34 +2,6 @@ import Foundation
 
 class CustomWin:Window{
     override func resolveSkin() {
-        self.contentView!.addSubview(CustomView(frame.width,frame.height))/*Sets the mainview of the window*/
-    }
-}
- /*
- * TODO: You should extend the window not the view
- */
-class CustomView:WindowView{
-    
-    /**
-     * Add content here
-     */
-    override func resolveSkin() {
-        super.resolveSkin()
-        
-        
-        
-        //make the section also have round corners but only the top corners
-        //add close button, min, max
-        //add event listeners to these buttons
-        
-        Swift.print("CustomView.resolveSkin()")
-        
-        //remember the contentview is sort of the container to hold items. might need to add things to this instance
-        
-        
-        let section = Section(120,40)
-        addSubview(section)
-        //let button = Button(120,40)
-        //addSubview(button)
+        self.contentView = CustomView(frame.width,frame.height)/*Sets the mainview of the window*/
     }
 }
