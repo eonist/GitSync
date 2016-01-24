2388b8fe1e8fb44b0199ba81e8527ff8d9eabe21
Eonist <unicornhive@gmail.com>
2016-01-24 11:04:56 +0100

    Files modified: 2
    Modified 2 files:src/AppDelegate.swiftsrc/window/CustomWin.swift

--- src/window/CustomWin.swift
+++ src/window/CustomWin.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class CustomWin:Window{
     override func resolveSkin() {
-        self.contentView = CustomView(frame.width,frame.height)/*Sets the mainview of the window*/
+        self.contentView!.addSubview(CustomView(frame.width,frame.height))/*Sets the mainview of the window*/
     }
 }
  /*
