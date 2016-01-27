ffe68c032d07b4d49733cbcab5d2093d8fbb54f7
Eonist <unicornhive@gmail.com>
2016-01-27 06:53:48 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView4.swift

--- src/window/temp/WinView4.swift
+++ src/window/temp/WinView4.swift
@@ -18,8 +18,9 @@ class WinView4:FlippedView {
      */
     func floatTest(){
         var css:String = ""
-        css += "Element{fill:blue;float:left;clear:left;}"
+        css += "Element{fill:green;float:left;clear:left;}"
         css += "Element#box2{fill:red;}"
+        css += "Element#box2{fill:purple;}"
         StyleManager.addStyle(css)
         let section = Section(400,400)
         addSubview(section)
@@ -27,9 +28,9 @@ class WinView4:FlippedView {
         section.addSubview(box1)
         let box2 = Element(100,100,section,"box2")
         section.addSubview(box2)
+        let box2 = Element(100,100,section,"box2")
+        section.addSubview(box2)
         
-        //Continue here: Try to align elements in a row, try to float things to the right, then clear to the right
-        //look into how you center align things, see the css from the textReplacer demo app
         
         //let tempStyle = StyleResolver.style(box2)
         //StyleParser.describe(tempStyle)
