947886145d9296cfa47d7677c47770d3affa6078
Eonist <unicornhive@gmail.com>
2016-01-27 17:01:21 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -19,30 +19,34 @@ class CustomView:WindowView{
         //remember the contentview is sort of the container to hold items. might need to add things to this instance
         
         
-        section = Section(frame.width,24)
-        addSubview(section!)
+        
         
         //let button = Button(20,20)
         //section.addSubview(button)
         
         var css:String = ""
         css += ""
-        css += "Button{width:12px,12px;height:12px,12px;margin-left:0px;margin-top:0px;float:left;}"
+        css += "Button{width:12px,12px;height:12px,12px;margin-left:8px;margin-top:4px;float:left;}"
         css += "Button:over{fill:~/Desktop/icons/title_bar/hover.svg none;}"
         css += "Button#close{fill:~/Desktop/icons/title_bar/close.svg none;}"
         css += "Button#minimize{fill:~/Desktop/icons/title_bar/min.svg none;}"
         css += "Button#maximize{fill:~/Desktop/icons/title_bar/max.svg none;}"
         
         StyleManager.addStyle(css)
+        
+        section = Section(frame.width,24)
+        self.addSubview(section!)
+        
+        
         let closeButton = Button(12,12,nil,"close")/*<--the w and h should be NaN, test if it supports this*/
         let minimizeButton = Button(12,12,nil,"minimize")
         let maximizeButton = Button(12,12,nil,"maximize")
         
-        self.addSubview(closeButton)
+        section!.addSubview(closeButton)
         //closeButton.setPosition(CGPoint(8,4))
-        self.addSubview(minimizeButton)
+        section!.addSubview(minimizeButton)
         //minimizeButton.setPosition(CGPoint(28,4))
-        self.addSubview(maximizeButton)
+        section!.addSubview(maximizeButton)
         //maximizeButton.setPosition(CGPoint(48,4))
         
         //Event listeners:
