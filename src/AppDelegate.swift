978d3c9be50d618ec4a898971a41a1bde474537b
Eonist <unicornhive@gmail.com>
2016-01-27 17:48:21 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate: NSObject, NSApplicationDelegate{
         //css += "Section{fill:#EFEFF4;corner-radius:4px 4px 0px 0px;}"
         //css += "Button{fill:green;}"
         css += ""
-        StyleManager.addStyle(css)
+        //StyleManager.addStyle(css)
 
-        win = CustomWin()//Win()//TranslucentWin()//CustomWin()//Win()//CustomWin()/*Init the window*///Win()//TranslucentWin()
+        win = win()//CustomWin()//Win()//TranslucentWin()//CustomWin()//Win()//CustomWin()/*Init the window*///Win()//TranslucentWin()
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
     }
