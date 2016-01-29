dfafed5b7a61476cc36091b63f78af0febd78b88
Eonist <unicornhive@gmail.com>
2016-01-29 19:38:32 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -1,5 +1,19 @@
-import Foundation
+import Cocoa
 
-class WinView5 {
-
+class WinView5:FlippedView {
+    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        createContent()
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    func createContent(){
+        hitTest()
+    }
+    /**
+     *
+     */
+    func hitTest(){
+        //
+    }
 }
