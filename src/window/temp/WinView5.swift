804c68b766501ad74be50d2e5973b7ae0c83bde7
Eonist <unicornhive@gmail.com>
2016-01-30 22:15:19 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -30,7 +30,7 @@ class WinView5:FlippedView {
 }
 private class ViewA:FlippedView{
     var viewB:NSView!
-    containsPoint
+    
     init(_ width: CGFloat, _ height: CGFloat) {
         super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
         self.wantsLayer = true/*if true then view is layer backed*/
