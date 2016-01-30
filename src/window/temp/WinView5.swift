f693dbaac10e23b9da61c3ed0cb8e4c5305c0a21
Eonist <unicornhive@gmail.com>
2016-01-30 11:53:23 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -52,16 +52,16 @@ private class ViewA:FlippedView{
         addSubview(viewB)
         viewB.frame.origin = CGPoint(50,50)
     }
-    /*override func hitTest(aPoint: NSPoint) -> NSView? {
-    Swift.print("ViewA aPoint: " + "\(aPoint)")
-    return nil
-    }*/
-    override func mouseDown(theEvent: NSEvent) {
-        Swift.print("ViewA.mouseDown()")
-        //Swift.print("window?.mouseLocationOutsideOfEventStream: " + "\(window?.mouseLocationOutsideOfEventStream)")
-        let theHitView = window!.contentView?.hitTest((window?.mouseLocationOutsideOfEventStream)!)
-        Swift.print("theHitView: " + "\(theHitView)")
+    /**/override func hitTest(aPoint: NSPoint) -> NSView? {
+        Swift.print("ViewA aPoint: " + "\(aPoint)")
+        return self
     }
+    /*override func mouseDown(theEvent: NSEvent) {
+    Swift.print("ViewA.mouseDown()")
+    //Swift.print("window?.mouseLocationOutsideOfEventStream: " + "\(window?.mouseLocationOutsideOfEventStream)")
+    let theHitView = window!.contentView?.hitTest((window?.mouseLocationOutsideOfEventStream)!)
+    Swift.print("theHitView: " + "\(theHitView)")
+    }*/
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
 
