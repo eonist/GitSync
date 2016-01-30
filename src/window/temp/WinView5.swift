ff00e6991c8702c148615dce2e0f32bd64476839
Eonist <unicornhive@gmail.com>
2016-01-30 23:35:57 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -53,11 +53,12 @@ private class ViewA:FlippedView{
         addSubview(viewB)
         viewB.frame.origin = CGPoint(50,50)
     }
-    /* override func hitTest(aPoint: NSPoint) -> NSView? {
+     override func hitTest(aPoint: NSPoint) -> NSView? {
+        Swift.print("hit")
     //Swift.print("ViewA aPoint: " + "\(aPoint)")
-    viewB.hitTest(aPoint)
+    //viewB.hitTest(aPoint)
     return nil
-    }*/
+    }/**/
     override func mouseDown(theEvent: NSEvent) {
         Swift.print("ViewA.mouseDown() theEvent: " + "\(theEvent)")
         //Swift.print("window?.mouseLocationOutsideOfEventStream: " + "\(window?.mouseLocationOutsideOfEventStream)")
