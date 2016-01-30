c254bf13e0328d7525e9824bce82e50d7b75125e
Eonist <unicornhive@gmail.com>
2016-01-30 23:43:55 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -49,9 +49,9 @@ private class ViewA:FlippedView{
         blueBox.draw()
         //blueBox.graphic.frame.origin = CGPoint(50,50)
         
-        /*viewB = ViewB(200,200)
+        viewB = ViewB(200,200)
         addSubview(viewB)
-        viewB.frame.origin = CGPoint(50,50)*/
+        viewB.frame.origin = CGPoint(50,50)/**/
     }
     /*override func hitTest(aPoint: NSPoint) -> NSView? {
     Swift.print("hit")
