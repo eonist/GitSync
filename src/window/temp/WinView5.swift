4a56c6b2684e669cf8c50f123915f13c6635b82c
Eonist <unicornhive@gmail.com>
2016-01-30 10:20:15 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/WinView5.swift

--- src/window/temp/WinView5.swift
+++ src/window/temp/WinView5.swift
@@ -5,6 +5,7 @@ class WinView5:FlippedView {
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
+        
         createContent()
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
