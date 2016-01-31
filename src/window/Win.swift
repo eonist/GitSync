6db78a6243aa79bbc512064b12dcf170d2a652cb
Eonist <unicornhive@gmail.com>
2016-01-31 08:20:57 +0100

    Files modified: 1
    Modified 1 file:src/window/Win.swift

--- src/window/Win.swift
+++ src/window/Win.swift
@@ -37,7 +37,7 @@ class Win:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         self.center()
         //view.wantsLayer = true;//this should be set in the iew not here
         self.contentView = view
-        makeFirstResponder(view.viewA.viewB) 
+        /*makeFirstResponder(view.viewA.viewB) */
         //self.title = ""/*Sets the title of the window*/
         self.title = ""//GitSync
         createContent()
