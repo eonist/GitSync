38e3d60cd3a20e0b1406c8e0a1ec01dc64846a99
Eonist <unicornhive@gmail.com>
2016-02-03 08:25:21 +0100

    Files modified: 1
    Modified 1 file:src/window/interactive/InteractiveView2.swift

--- src/window/interactive/InteractiveView2.swift
+++ src/window/interactive/InteractiveView2.swift
@@ -41,7 +41,7 @@ class InteractiveView2:FlippedView{
      * Only fires if the mouse is "rolls" out of the visible part of this view
      */
     func mouseOut(/*event:MouseEvent*/){
-        //continue here: setup the mouseOut and mouseMove
+        //continue here: setup the mouseOut and mouseMove, mousemove needs a bool to turn it on or it will flood its parents with calls, isMouseMovable could be used
     }
     /**
      * MouseMoved
