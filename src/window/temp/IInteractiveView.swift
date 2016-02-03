a43efacf4c2e9121241a4655d75596333a33fd3b
Eonist <unicornhive@gmail.com>
2016-02-03 21:18:47 +0100

    Files modified: 1
    Modified 1 file:src/window/temp/IInteractiveView.swift

--- src/window/temp/IInteractiveView.swift
+++ src/window/temp/IInteractiveView.swift
@@ -1,5 +1,10 @@
 import Foundation
 
-class IInteractiveView {
-
+protocol IInteractiveView {
+    func mouseMoved(event:MouseEvent)
+    func mouseOver(event:MouseEvent)
+    func mouseOut(event:MouseEvent)
+    func mouseDown(event:MouseEvent)
+    func mouseUpInside(event: MouseEvent)
+    func mouseUpOutside(event: MouseEvent)
 }
