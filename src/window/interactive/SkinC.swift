e181be51f8613b5fea99e6dce4e3bfa3db83234b
Eonist <unicornhive@gmail.com>
2016-02-05 19:36:16 +0100

    Files modified: 2
    Modified 2 files:src/window/interactive/SkinC.swiftsrc/window/interactive/ViewA.swift

--- src/window/interactive/SkinC.swift
+++ src/window/interactive/SkinC.swift
@@ -10,7 +10,7 @@ class SkinC:InteractiveView2{
      *
      */
     func createContent(){
-        let redBox:RoundRectGraphic = RoundRectGraphic(0,0,frame.width,frame.height,Fillet(frame.width*0.25),FillStyle(NSColor.redColor()),LineStyle(5,NSColor.greenColor().alpha(0)),OffsetType(OffsetType.center))
+        let redBox:RoundRectGraphic = RoundRectGraphic(0,0,200,200,Fillet(200*0.25),FillStyle(NSColor.redColor()),LineStyle(5,NSColor.greenColor().alpha(0)),OffsetType(OffsetType.center))
         addSubview(redBox.graphic)
         redBox.draw()
         //redBox.graphic.frame.origin = CGPoint(50,50)
