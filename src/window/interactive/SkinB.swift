7a7fd1c19d68bc8f8b2662c29042dbae8f12c030
Eonist <unicornhive@gmail.com>
2016-02-05 08:55:06 +0100

    Files modified: 2
    Modified 2 files:src/window/interactive/SkinB.swiftsrc/window/interactive/TrackingView.swift

--- src/window/interactive/SkinB.swift
+++ src/window/interactive/SkinB.swift
@@ -1,13 +1,13 @@
 import Cocoa
 
 class SkinB:TrackingView{
-    override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
+    //override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
     override init(_ frameRect:NSRect,_ parent:NSView) {
         super.init(frameRect, parent)
         createContent()
     }
     func createContent(){
-        let redBox:RoundRectGraphic = RoundRectGraphic(0,0,frame.width,frame.height,Fillet(frame.width*0.25),FillStyle(NSColor.redColor()),LineStyle(5,NSColor.greenColor().alpha(0)),OffsetType(OffsetType.center))
+        let redBox:RoundRectGraphic = RoundRectGraphic(0,0,200,200,Fillet(200*0.25),FillStyle(NSColor.redColor()),LineStyle(5,NSColor.greenColor().alpha(0)),OffsetType(OffsetType.center))
         addSubview(redBox.graphic)
         redBox.draw()
         //redBox.graphic.frame.origin = CGPoint(50,50)
