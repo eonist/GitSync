62c6f855a0008e0510250df47c46d11eb9b9f7c4
Eonist <unicornhive@gmail.com>
2016-02-05 07:40:43 +0100

    Files modified: 2
    Modified 2 files:src/AppDelegate.swiftsrc/window/interactive/SkinC.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -10,16 +10,16 @@ class AppDelegate: NSObject, NSApplicationDelegate{
      */
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         //.print("applicationDidFinishLaunching")
-        /**/
+        /*
         var css = ""//E8E8E8
         css += "Window{fill:#EFEFF4;corner-radius:4px;}"//<--you should target a bg element not the window it self, since now everything inherits these values
         //css += "Section{fill:#EFEFF4;corner-radius:4px 4px 0px 0px;}"
         //css += "Button{fill:green;}"
         css += ""
         StyleManager.addStyle(css)
+        */
 
-
-        win = CustomWin(300,200)//Win()//TranslucentWin()//CustomWin()//Win()//CustomWin()/*Init the window*///Win()//TranslucentWin()
+        win = Win(300,200)//Win()//TranslucentWin()//CustomWin()//Win()//CustomWin()/*Init the window*///Win()//TranslucentWin()
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
     }
--- src/window/interactive/SkinC.swift
+++ src/window/interactive/SkinC.swift
@@ -1,6 +1,10 @@
 import Foundation
 
-class SkinC {
+class SkinC:InteractiveView2{
+    init(_ width: CGFloat, _ height: CGFloat){
+        super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
+    }
     //extend interactiveView
-    //implement immediate 
+    //implement immediate
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
