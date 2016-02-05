afe269adb5b780d136d0adf58645bff4cc7464d6
Eonist <unicornhive@gmail.com>
2016-02-05 09:38:52 +0100

    Files modified: 2
    Modified 2 files:src/window/interactive/SkinB.swiftsrc/window/interactive/SkinC.swift

--- src/window/interactive/SkinC.swift
+++ src/window/interactive/SkinC.swift
@@ -2,8 +2,8 @@ import Cocoa
 
 class SkinC:InteractiveView2{
     override var wantsDefaultClipping:Bool{return false}//avoids clipping the view
-    init(_ width: CGFloat, _ height: CGFloat){
-        super.init(frame: NSRect(0,0,width,height))//<--This can be a zero rect since the children contains the actual graphics. And when you use Layer-hosted views the subchildren doesnt clip
+    override init(frame frameRect:NSRect) {
+        super.init(frame: frameRect)
         createContent()
     }
     /**
