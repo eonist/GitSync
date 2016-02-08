cad4ad4099b7ec8d6d279394b3f3bad6432ae324
Eonist <unicornhive@gmail.com>
2016-02-08 09:34:35 +0100

    Files modified: 1
    Modified 1 file:src/ui/TempTextInput.swift

--- src/ui/TempTextInput.swift
+++ src/ui/TempTextInput.swift
@@ -16,16 +16,16 @@ class TempTextInput:NSView{
      * 
      */
     func createContent(){
-        nameText = CustomTextField(frame: NSRect(x: 0, y: 0, width: 144, height: 24))
+        /* nameText = CustomTextField(frame: NSRect(x: 0, y: 0, width: 144, height: 24))
         nameText.stringValue = title
         nameText.editable = false
         nameText.bordered = false
-        nameText.selectable = true
-        self.addSubview(nameText)
+        nameText.selectable = true*/
+        //self.addSubview(nameText)
         
-        let spacing = 12
-        let x = Int(nameText.frame.origin.x) + Int(nameText.frame.width) + spacing
-        let y = Int(nameText.frame.origin.y)
+        //let spacing = 12
+        let x = 20//Int(nameText.frame.origin.x) + Int(nameText.frame.width) + spacing
+        let y = 0//Int(nameText.frame.origin.y)
         nameInputText = CustomTextField(frame: NSRect(x: x, y: y, width: 144, height: 24))
         nameInputText.stringValue = defaultInput
         nameInputText.editable = true
@@ -66,7 +66,7 @@ class TempTextInput:NSView{
 //continue here: the hit test is all wrong when using NSText. tet if its the same on NSVIew, maybe Element is special etc. also do research online
 
 class CustomTextField:NSTextField{
-    //override var flipped:Bool {return true}/*Organizes your view from top to bottom*/
+    override var flipped:Bool {return true}/*Organizes your view from top to bottom*/
 
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
