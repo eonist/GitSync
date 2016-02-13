050a53e20e57b1761e6d4a1b42fc8233a821a0a5
Eonist <unicornhive@gmail.com>
2016-02-13 15:13:14 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -622,7 +622,7 @@ class CustomView:WindowView{
         
         func onSelect(event:Event){
             if(event.type == SelectGroupEvent.change){
-                Swift.print("onSelect selected" + "\((event as! SelectGroupEvent).selected)")
+                Swift.print("CustomView.onSelect selected" + "\((event as! SelectGroupEvent).selected)")
             }
         }
         selectGroup.event = onSelect
