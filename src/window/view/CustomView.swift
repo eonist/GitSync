b2aeff2af57f3115bb39f9d4c50ce0371f782c08
Eonist <unicornhive@gmail.com>
2016-02-13 15:15:31 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -26,7 +26,7 @@ class CustomView:WindowView{
         //createLeverSpinner()
         //createLeverStepper()
         
-        createRadioBullets()
+        //createRadioBullets()
         //addEventListeners()
         
         /*
