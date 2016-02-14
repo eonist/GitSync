29386eecb521a61c48eebc51434cec90653ee4d2
Eonist <unicornhive@gmail.com>
2016-02-14 13:11:59 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -62,11 +62,7 @@ class CustomView:WindowView{
         
         /**/
         Swift.print(dp.count())
-        
-        var temp = [1,2,3,4]
-        temp = temp.splice(0, 1)
-        Swift.print("temp.count: " + "\(temp.count)")
-    }
+            }
     /**
      *
      */
