36bdbd1b288eab59d2c49e8c969cea43369975da
Eonist <unicornhive@gmail.com>
2016-02-19 08:32:39 +0100

    Files modified: 1
    Modified 1 file:src/window/experiments/ScrollContainer.swift

--- src/window/experiments/ScrollContainer.swift
+++ src/window/experiments/ScrollContainer.swift
@@ -1,5 +1,23 @@
-import Foundation
+import Cocoa
 
-class ScrollContainer {
-
+class ScrollContainer :Container{
+    override func resolveSkin() {
+        super.resolveSkin()
+        let vSlider = self.addSubView(VSlider(24,120,30,0,self)) as! VSlider
+        
+        func onEvent(event:Event){
+            if(event.type == SliderEvent.change && event.origin === vSlider){Swift.print("\(self.dynamicType)" + ".onEvent() progress: " + "\((event as! SliderEvent).progress)")}
+        }
+        
+        vSlider.event = onEvent
+        
+    }
+    
+    override func scrollWheel(theEvent: NSEvent) {
+        Swift.print("theEvent: " + "\(theEvent)")
+        super.scrollWheel(theEvent)
+    }
+    override func getClassType() -> String {
+        return String(Container)
+    }
 }
