fa15f335c916a83c486405ae58d6b869be9401ea
Eonist <unicornhive@gmail.com>
2016-02-20 16:34:00 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -77,10 +77,11 @@ class CustomView:WindowView{
                 animation.duration = 1;
                 animation.fillMode = kCAFillModeForwards
                 animation.removedOnCompletion = false
+                animation.autoreverses = true
                 animation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)//kCAMediaTimingFunctionEaseIn,kCAMediaTimingFunctionEaseOut,kCAMediaTimingFunctionEaseInEaseOut,kCAMediaTimingFunctionDefault,kCAMediaTimingFunctionLinear
                 //functionWithControlPoints:0.5:0:0.9:0.7
                 //CAMediaTimingFunction(controlPoints: 0.5, 0, 0.9, 0.7)//bezier curve animation
-                //you can start multiple animations with : CAAnimationGroup. 
+                //you can start multiple animations with : CAAnimationGroup.
                 
                 rect.graphic.layer!.addAnimation(animation, forKey: "basic")//:animation forKey:@"basic"];
                 //rect.graphic.layer!.position = CGPointMake(100, 60);
