55a41840fa60d32613787569da8f48860dfc5384
Eonist <unicornhive@gmail.com>
2016-02-23 15:02:23 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -113,7 +113,7 @@ class CustomView:WindowView{
         CGLLockContext(context.CGLContextObj)
         
         
-        rect.graphic.fillShape.graphics.context = context.
+        //rect.graphic.fillShape.graphics.context = context.
         if(rect.graphic.frame.x < 200){
             rect.graphic.frame.x += 1
         }else{
