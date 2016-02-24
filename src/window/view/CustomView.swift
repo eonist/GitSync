d24926750a19346e61ab946cc0302abf5408c859
Eonist <unicornhive@gmail.com>
2016-02-24 09:20:13 +0100

    Files modified: 1
    Modified 1 file:src/window/view/CustomView.swift

--- src/window/view/CustomView.swift
+++ src/window/view/CustomView.swift
@@ -23,4 +23,57 @@ class CustomView:WindowView{
         minimizeButton = section!.addSubView(Button(0,0,section!,"minimize")) as? Button
         maximizeButton = section!.addSubView(Button(0,0,section!,"maximize")) as? Button
     }
+    /**
+     *
+     */
+    override func setSize(width:CGFloat,_ height:CGFloat){
+        //Swift.print("CustomView.setSize() size: " + "\(size)")
+        //self.skin!.setSize(size.width, size.height)
+        super.setSize(width, height)
+        section!.setSize(width, section!.height)
+    }
+    /**
+     *
+     */
+    func onCloseButtonReleaseInside() {
+        Swift.print("onCloseButtonReleaseInside")
+        //Close window here
+        //self.window?.close()//this closes the window
+        NSApp.terminate(self)//quits the app
+    }
+    /**
+     *
+     */
+    func onMinimizeButtonReleaseInside(){
+        Swift.print("onMinimizeButtonReleaseInside")
+        //minimize the window here
+        
+        //NSApp.miniaturizeAll(self)//minimizes all windows in the app
+        self.window?.miniaturize(self)
+    }
+    /**
+     * TODO: Add support for fullscreen mode aswell: window.setFrame(NSScreen.mainScreen()!.visibleFrame, display: true, animate: true)
+     * TODO: add support for zooming back to normal size
+     */
+    func onMaximizeButtonReleaseInside(){
+        Swift.print("onMaximizeButtonReleaseInside")
+        //maximize the window here
+        self.window?.zoom(self)
+    }
+    /**
+     *
+     */
+    func onTestButtonDown(){
+        Swift.print("onTestButtonDown")
+    }
+    /**
+     *
+     */
+    override func onEvent(event: Event) {
+        //Swift.print( "CustomView.onEvent() event:" + "\(event)")
+        //Swift.print("CustomView.onEvent: " + "\(event)" + " event.origin: " + "\(event.origin)")
+        if(event.origin === closeButton && event.type == ButtonEvent.upInside){onCloseButtonReleaseInside()}
+        else if(event.origin === minimizeButton && event.type == ButtonEvent.upInside){onMinimizeButtonReleaseInside()}
+        else if(event.origin === maximizeButton && event.type == ButtonEvent.upInside){onMaximizeButtonReleaseInside()}
+    }
 }
