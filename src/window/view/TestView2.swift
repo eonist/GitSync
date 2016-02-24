14cc81ca371da0dceae6037fb378a2728b7b08c6
Eonist <unicornhive@gmail.com>
2016-02-24 09:32:51 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView2.swift

--- src/window/view/TestView2.swift
+++ src/window/view/TestView2.swift
@@ -1,5 +1,32 @@
-import Foundation
+import Cocoa
 
-class TestView2 {
+class TestView2:CustomView{
+    var rect:RectGraphic!
+    override func resolveSkin() {
+        super.resolveSkin()
+        //Swift.print("CustomView.resolveSkin()")
+        frameAnimTest()
 
+    }
+    func frameAnimTest(){
+        StyleManager.addStyle("Button{fill:#5AC8FA;float:left;clear:left;}Button:down{fill:#007AFF;}")
+        let btn = addSubView(Button(100,24,self)) as! Button//add a button
+        
+        var toggle:Bool = true
+        func onEvent(event:Event){
+            if(event.type == ButtonEvent.upInside && event.origin === btn){
+                Swift.print("button works")
+                //toggle ? CVDisplayLinkStart(displayLink) : CVDisplayLinkStop(displayLink);//To start capturing events from the display link, you'd use
+                toggle = !toggle
+            }
+        }
+        btn.event = onEvent
+
+        let fill:FillStyle = FillStyle(NSColorParser.nsColor(0x4CD964))
+        /*Rect*/
+        rect = RectGraphic(0,0,150,150,fill,nil)//Add a red box to the view
+        addSubview(rect.graphic)
+        rect.draw()
+        rect.graphic.frame.y = 60/**/
+    }
 }
