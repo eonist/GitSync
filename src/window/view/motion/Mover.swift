d75cbedeae0130e66164f319c8387b993b90a505
Eonist <unicornhive@gmail.com>
2016-02-26 20:26:59 +0100

    Files modified: 1
    Modified 1 file:src/window/view/motion/Mover.swift

--- src/window/view/motion/Mover.swift
+++ src/window/view/motion/Mover.swift
@@ -4,13 +4,13 @@ class Mover {
     var value:CGFloat = 0
     var velocity:CGFloat
     var target:NSView//TODO: change this to IPositionable in the future
-    init(target:NSView, value:CGFloat, velocity:CGFloat = 0){
-        self.target = target;
-        self.value = value;
+    init(_ target:NSView, _ value:CGFloat, _ velocity:CGFloat = 0){
+        self.target = target
+        self.value = value
         self.velocity = velocity;
     }
     func updatePosition() {
-        //trace("updatePosition")
+        //Swift.print("\(self.)" + "updatePosition")
         value += velocity;
     }
 }
