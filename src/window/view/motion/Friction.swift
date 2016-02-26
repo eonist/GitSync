d8e4c4e84d96c3f5335015e2101bd33cd8b734f1
Eonist <unicornhive@gmail.com>
2016-02-26 20:33:23 +0100

    Files modified: 1
    Modified 1 file:src/window/view/motion/Friction.swift

--- src/window/view/motion/Friction.swift
+++ src/window/view/motion/Friction.swift
@@ -1,11 +1,12 @@
 import Cocoa
 
 class Friction:Mover{
-    var frictionStrength:CGFloat;/*This value is the strength of the friction*/
-    var lastValue:CGFloat = 0;/*this value is a temporary value that is used when checking if the motion is about to stop*/
-    var slowDownFriction:CGFloat = 1;/*slowDownFriction is basically an inactive friction value, unless you change it to something else than 1*/
+    var frictionStrength:CGFloat/*This value is the strength of the friction*/
+    var lastValue:CGFloat = 0/*this value is a temporary value that is used when checking if the motion is about to stop*/
+    var slowDownFriction:CGFloat = 1/*slowDownFriction is basically an inactive friction value, unless you change it to something else than 1*/
     init(_ target:NSView, _ value:CGFloat, _ velocity:CGFloat = 0, _ frictionStrength:CGFloat = 0.98){
-        super.init(target, value, velocity)
         self.frictionStrength = frictionStrength
+        super.init(target, value, velocity)
+        
     }
 }
