3d758ea2b4dfe59139db047dd110f9233fb6fa30
Eonist <unicornhive@gmail.com>
2016-02-26 09:10:34 +0100

    Files modified: 1
    Modified 1 file:src/window/view/AnimatableView.swift

--- src/window/view/AnimatableView.swift
+++ src/window/view/AnimatableView.swift
@@ -1,6 +1,8 @@
 import Cocoa
 /**
- * NOTE: This view class serves as a basis for frame animation. 
+ * NOTE: This view class serves as a basis for frame animation.
+ * NOTE: override the onFrame method to do frame animations
+ * NOTE: Start and stop with CVDisplayLinkStart(displayLink) and CVDisplayLinkStop(displayLink)
  */
 class AnimatableView:CustomView,IAnimatable {
     var displayLink: CVDisplayLink!
