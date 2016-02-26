d9c7a21819bd9e2b5ac6fd4bb798eff7a2c914e6
Eonist <unicornhive@gmail.com>
2016-02-26 20:22:38 +0100

    Files modified: 1
    Modified 1 file:src/window/view/motion/Friction.swift

--- src/window/view/motion/Friction.swift
+++ src/window/view/motion/Friction.swift
@@ -1,5 +1,8 @@
-import Foundation
+import Cocoa
 
-class Friction {
-
+class Friction:Mover{
+    init(target:NSView,aValue:Number, aVelocity:Number = 0, aFrictionStrength:Number=0.98){
+        super(aTarget, aValue, aVelocity);
+        this.frictionStrength = aFrictionStrength;
+    }
 }
