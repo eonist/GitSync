2c68a4c7447e2c5c26d4c1b2d6e50d9e60fcf82d
Eonist <unicornhive@gmail.com>
2016-02-26 09:42:40 +0100

    Files modified: 1
    Modified 1 file:src/window/view/motion/Mover.swift

--- src/window/view/motion/Mover.swift
+++ src/window/view/motion/Mover.swift
@@ -1,5 +1,13 @@
-import Foundation
+import Cocoa
 
 class Mover {
-
+    var value:CGFloat = 0
+    var velocity:CGFloat
+    var target:NSView//change this to IPositionable in the future
+    init(target:NSView, value:CGFloat, velocity:CGFloat = 0){
+        self.target = target;
+        self.value = value;
+        self.velocity = velocity;
+        
+    }
 }
