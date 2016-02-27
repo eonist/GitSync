ea4e4f3dacbcf4befcc62050de65f8802f10ad80
Eonist <unicornhive@gmail.com>
2016-02-27 19:25:58 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView3.swift

--- src/window/view/TestView3.swift
+++ src/window/view/TestView3.swift
@@ -52,18 +52,18 @@ class TestView3:AnimatableView {
     }
     func movePictures(value:CGFloat){
         for(var i:Int = 0;i < items.count;i++){
-            var spacing:Int = DISPLAY_HEIGHT;
-            var totalHeight:Number = (_btnList.length) *spacing;//move this outside of the method
-            var currentPos:Number = (aValue);
-            if(_btnList[i].mc.y > totalHeight-spacing){
-                var leftOver:Number = (currentPos-_btnList[i].tempPos+spacing-(totalHeight-_btnList[i].pos));
+            var spacing:CGFloat = h
+            var totalHeight:CGFloat = CGFloat(items.count) * spacing;//move this outside of the method
+            var currentPos:CGFloat = (value);
+            if(items[i]["view"].frame.y > totalHeight-spacing){
+                var leftOver:CGFloat = (currentPos-_btnList[i].tempPos+spacing-(totalHeight-_btnList[i].pos));
                 //trace("over right border btn id: "+i+" leftover"+leftOver);
-                _btnList[i].tempPos = currentPos+_btnList[i].pos+spacing-leftOver;
+                _btnList[i].tempPos = currentPos + _btnList[i].pos+spacing-leftOver;
                 //trace("over btn id: "+i+" percentage: "+percentage+" tempPos "+_btnList[i].tempPos)
             };
             if(_btnList[i].mc.y < ((-1*totalHeight)+spacing)){
                 //trace("over left border btn id: "+i+" POSITIONS: "+" currentPos: "+currentPos+" tempPos: "+_btnList[i].tempPos)
-                var leftOver2:Number = (currentPos-(_btnList[i].tempPos)-spacing-(-1*totalHeight-_btnList[i].pos));
+                var leftOver2:CGFloat = (currentPos-(_btnList[i].tempPos)-spacing-(-1*totalHeight-_btnList[i].pos));
                 //trace("over left border btn id: "+i+" leftover"+leftOver2);
                 _btnList[i].tempPos = currentPos+_btnList[i].pos-spacing-leftOver2;
                 //trace("over btn id: "+i+" percentage: "+percentage+" currentPos: "+currentPos+" tempPos "+_btnList[i].tempPos)
