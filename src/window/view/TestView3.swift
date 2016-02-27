9d3da116bb2c5c4ac0c56325b5f147332b1477f9
Eonist <unicornhive@gmail.com>
2016-02-27 14:53:55 +0100

    Files modified: 2
    Modified 2 files:src/window/view/TestView3.swiftsrc/window/view/experiment/VerticalThrowArea.swift

--- src/window/view/TestView3.swift
+++ src/window/view/TestView3.swift
@@ -9,5 +9,13 @@ class TestView3:AnimatableView {
     func animTest(){
         let throwArea = addSubView(VerticalThrowArea())
         throwArea.frame.y = 40
+        
+        //create 3 color rectangles
+        
+        //add these rectangles to a container that clips
+        
+        //write the a simple move code and hook up the DisplayLink
+        
+        //write the loop movment code
     }
 }
