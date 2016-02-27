e8047f6d2c485f9187313dabdc16a0f05ddaeb4f
Eonist <unicornhive@gmail.com>
2016-02-27 07:57:40 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea.swift

--- src/window/view/experiment/VerticalThrowArea.swift
+++ src/window/view/experiment/VerticalThrowArea.swift
@@ -1,16 +1,18 @@
 import Foundation
 
 class VerticalThrowArea :FlippedView{
-    var mover:Friction
+    var mover:Friction?
     var timer:NSTimer?
     var startTime:CFAbsoluteTime?
     var elapsedTime:CFAbsoluteTime?
+    var lastPos:CGPoint?
     
     init(){
         //var b1:Rect2 = addSubView(Rect2(_width,_height,FillStyle(Colors.GREEN,_alpha))) as Button
         let frictionValue:CGFloat = 0;
         self.mover = Friction(self,frictionValue,0,0.98)
         //self.timer = NSTimer(100,Int.MAX_VALUE)
+        super.init(frame: <#T##NSRect#>)
     }
 
     
@@ -23,17 +25,14 @@ class VerticalThrowArea :FlippedView{
         
         self.elapsedTime = CFAbsoluteTimeGetCurrent() - self.startTime!
         
-        //CFAbsoluteTimeGetCurrent()
-        var duration:uint = getTimer()
-        self.lastTime = duration
-        self.lastPos = CGPoint(self.mouseX,self.mouseY)
+        self.lastPos = localPos()
     }
     func startSettingTime(){//100ms
         self.startTime = CFAbsoluteTimeGetCurrent() ;
         self.timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "onTimer:", userInfo: nil, repeats: false)
     }
     func stopSettingTime(){
-        timer.invalidate()
+        if(timer != nil){timer!.invalidate()}
     }
     
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
