99ea1ca3e19dc82db64b0112abe556af7ded5a6a
Eonist <unicornhive@gmail.com>
2016-02-27 06:53:11 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea.swift

--- src/window/view/experiment/VerticalThrowArea.swift
+++ src/window/view/experiment/VerticalThrowArea.swift
@@ -1,5 +1,28 @@
 import Foundation
 
-class ThrowArea {
-    
+class VerticalThrowArea :FlippedView{
+    private var mover:Friction
+    private var timer:Timer?
+    init(){
+        //var b1:Rect2 = addSubView(Rect2(_width,_height,FillStyle(Colors.GREEN,_alpha))) as Button
+        var frictionValue:CGFloat = 0;
+        self.mover = Friction(this,frictionValue,0,0.98)
+        //self.timer = NSTimer(100,Int.MAX_VALUE)
+    }
+    func onTimer(timer: NSTimer) {
+        let theStringToPrint = timer.userInfo as! String
+        print(theStringToPrint)
+        //print("ontimer")
+        var duration:uint = getTimer()
+        self.lastTime = duration
+        self.lastPos = CGPoint(self.mouseX,self.mouseY)
+    }
+    func startSettingTime(){//100ms
+        self.timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: "onTimer:", userInfo: nil, repeats: false)
+    }
+    func stopSettingTime(){
+        timer.invalidate()
+    }
 }
+
+//CFAbsoluteTimeGetCurrent()
