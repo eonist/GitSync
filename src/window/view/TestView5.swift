7f2a59c401426abac5375afd8e70a0c38e928633
Eonist <unicornhive@gmail.com>
2016-02-29 20:08:00 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView5.swift

--- src/window/view/TestView5.swift
+++ src/window/view/TestView5.swift
@@ -1,5 +1,18 @@
 import Foundation
 
-class TestView5 {
-
+class TestView5:AnimatableView {
+    override func resolveSkin() {
+        super.resolveSkin()
+        motionTest()
+    }
+    /**
+     * RobertPenner.com has many nice easeing equation you can recycle
+     */
+    func motionTest(){
+        //Create a button to start the animation
+        //create a circle that represents the object to animate
+        //first add inear animation
+        //then add ease out
+        //
+    }
 }
