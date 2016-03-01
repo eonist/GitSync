d8ce4133dfc593f87c5d6f4c03f14c271a3e7b44
Eonist <unicornhive@gmail.com>
2016-03-01 13:53:53 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea2.swift

--- src/window/view/experiment/VerticalThrowArea2.swift
+++ src/window/view/experiment/VerticalThrowArea2.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class VerticalThrowArea2 {
+class VerticalThrowArea2 :InteractiveView2{
+    var w:CGFloat = 200
+    var h:CGFloat = 200
+    init(){
+        super.init(frame: NSRect(0,0,w,h))
+    }
 
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }
