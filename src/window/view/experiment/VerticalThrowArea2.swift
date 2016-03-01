b9e5fe6683be341d08fa5b9d986aa942ab21bcee
Eonist <unicornhive@gmail.com>
2016-03-01 15:42:38 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea2.swift

--- src/window/view/experiment/VerticalThrowArea2.swift
+++ src/window/view/experiment/VerticalThrowArea2.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 
 class VerticalThrowArea2 :InteractiveView2{
     var w:CGFloat = 200
@@ -8,7 +8,25 @@ class VerticalThrowArea2 :InteractiveView2{
         
         //scrolling
     }
-
+    /**
+     * NOTE: you can use the event.deviceDeltaY to check which direction the gesture is moving in.
+     */
+    override func scrollWheel(theEvent: NSEvent) {
+        //Swift.print("theEvent: " + "\(theEvent)")
+        //Swift.print("scrollingDeltaY: " + "\(theEvent.scrollingDeltaY)")
+        //Swift.print("velocities: " + "\(velocities)")
+        if(theEvent.phase == NSEventPhase.Changed){//fires everytime there is direct scrollWheel gesture movment.
+            //Swift.print("changed")
+            if(!mover!.hasStopped){
+                if(CVDisplayLinkIsRunning((self.superview as! AnimatableView).displayLink)) {CVDisplayLinkStart((self.superview as! AnimatableView).displayLink);mover!.hasStopped = true;}//the if clause is just a precausion
+            }
+            mover!.value += theEvent.scrollingDeltaY
+            prevScrollingDeltaY = theEvent.scrollingDeltaY//needed to calc the velocity onScrollWheelUp
+            
+            velocities.removeLast()
+            velocities = [theEvent.scrollingDeltaY] + velocities
+        }
+    }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         
     
