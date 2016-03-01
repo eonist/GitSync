d04c8db42f64d21544e70383cf750a61dc14142e
Eonist <unicornhive@gmail.com>
2016-03-01 16:45:40 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea2.swift

--- src/window/view/experiment/VerticalThrowArea2.swift
+++ src/window/view/experiment/VerticalThrowArea2.swift
@@ -35,8 +35,43 @@ class VerticalThrowArea2 :InteractiveView2{
             
             velocities.removeLast()
             velocities = [theEvent.scrollingDeltaY] + velocities
+        }else if(theEvent.phase == NSEventPhase.MayBegin){//can be used to detect if two fingers are touching the trackpad
+            //Swift.print("MayBegin")
+            onScrollWheelDown()
+        }else if(theEvent.phase == NSEventPhase.Ended){//if you release your touch-gesture and the momentum of the gesture has stopped.
+            //Swift.print("Ended ")
+            onScrollWheelUp()
+            //dont start the CVDisplayLink, since your momentum has stopped
+        }else if(theEvent.phase == NSEventPhase.Cancelled){
+            //Swift.print("Cancelled")
+            onScrollWheelUp()
         }
         super.scrollWheel(theEvent)//call super to forward the event to the parent view
+    }
+    func onScrollWheelDown(){
+        //Swift.print("onScrollWheelDown")
+        prevScrollingDeltaY = 0
+        velocities = [0,0,0,0,0,0,0,0,0,0]//reset the velocities
+        mover!.slowDownFriction = 0.40//set this to 0.70 and the slowdown prosses is slower
+    }
+    func onScrollWheelUp(){
+        //Swift.print("onScrollWheelUp " + "\(prevScrollingDeltaY)")
+        mover!.slowDownFriction = 1/*reset the slowDownFriction, 1 equals inactive*/
+        mover!.hasStopped = false/*reset this value to false*/
+        //checkTime(this);/*calcs the speed aka the velocity and starts the anim in this speed*/
+        if(prevScrollingDeltaY != 1.0 && prevScrollingDeltaY != -1.0){/*1 and -1 indicates stationaryness*/
+            //Swift.print("is not stationary")
+            var velocity:CGFloat = 0
+            if(prevScrollingDeltaY > 0){
+                velocity = NumberParser.max(velocities)
+            }else{
+                velocity = NumberParser.min(velocities)
+            }
+            mover!.velocity = velocity/*set the mover velocity to the current mouse gesture velocity*/
+            CVDisplayLinkStart((self.superview as! AnimatableView).displayLink)//'start the frameTicker here, do this part in parent view or use event or Selector
+        }else{
+            //Swift.print("is stationary")
+        }
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
         
