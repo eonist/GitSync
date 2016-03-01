ee1ef876b21c811dca9ef88bad13f480c44fea8e
Eonist <unicornhive@gmail.com>
2016-03-01 13:27:33 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView6.swift

--- src/window/view/TestView6.swift
+++ src/window/view/TestView6.swift
@@ -10,6 +10,15 @@ class TestView6:AnimatableView {
     func listAnimTest(){
         //create a container with a mask 200x200
         
-        //create a container with 3 rects insider 
+        //create a container with 3 rects insider 200x150 per rect
+        
+        //add the scrollEvent mover scheme to move the container with the rects
+        
+        //when y+height of rectContainer is above y+height of the maskContainer then stop moving the rectContainer
+        
+        //then apply spring and more friction when the above event happens. 
+        
+        //if the above works do the same scheme for 
+        
     }
 }
