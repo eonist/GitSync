5f39afbbe45a40ff80a671cc997d5684f1f88cf2
Eonist <unicornhive@gmail.com>
2016-03-01 13:25:35 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView6.swift

--- src/window/view/TestView6.swift
+++ src/window/view/TestView6.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class TestView6 {
-
+class TestView6:AnimatableView {
+    let target:CGPoint = CGPoint(100,60)
+    var circ:EllipseGraphic!
+    override func resolveSkin() {
+        super.resolveSkin()
+        listAnimTest()
+    }
+    func listAnimTest(){
+        //create a container with a mask 200x200
+        
+        //create a container with 3 rects insider 
+    }
 }
