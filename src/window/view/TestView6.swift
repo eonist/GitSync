180852ae9f85db2621540655ec29d119ebc73bfb
Eonist <unicornhive@gmail.com>
2016-03-02 14:49:37 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView6.swift

--- src/window/view/TestView6.swift
+++ src/window/view/TestView6.swift
@@ -15,13 +15,10 @@ class TestView6:AnimatableView {
         listAnimTest()
     }
     func listAnimTest(){
-        //create a container with a mask 200x200
-        maskContainer = addSubView(ItemContainer(frame: NSRect(0,0,w,h))) as! ItemContainer
+        maskContainer = addSubView(ItemContainer(frame: NSRect(0,0,w,h))) as! ItemContainer//create a container with a mask 200x200
         maskContainer.frame.y = 20
-        
-        //create a container with 3 rects insider 200x150 per rect
-        itemContainer = maskContainer.addSubView(InteractiveView2(frame: NSRect(0,0,w,itemH*3))) as! InteractiveView2
-        itemContainer.frame.y = 0
+
+        itemContainer = maskContainer.addSubView(InteractiveView2(frame: NSRect(0,0,w,itemH*3))) as! InteractiveView2//create a container with 3 rects insider 200x150 per rect
         
         let colors:Array<NSColor> = [Colors.green(),Colors.yellow(),Colors.purple(),Colors.orange(),Colors.lightBlue()]
         for var i = 0; i < colors.count; ++i{/*Rect*/
@@ -29,27 +26,14 @@ class TestView6:AnimatableView {
             itemContainer.addSubview(r1.graphic)
             r1.draw()
         }
-        
         throwArea = addSubView(VerticalThrowArea2()) as? VerticalThrowArea2
         throwArea!.frame.y = 20
-        
-        //add the scrollEvent mover scheme to move the container with the rects
-        
-        //when y+height of rectContainer is above y+height of the maskContainer then stop moving the rectContainer
-            //create checkpoints and print when the checkpoinst are passed
-        
-        //then apply spring and more friction when the above event happens. 
-        
-        //if the above works do the same scheme for when rectContainer.y top is bellow top maskContainer.y
-        
-        //then apply the when dragging bellow and above the limits then apply log10 friction. on release apply normal conditions, spring and friction
     }
     /**
      * loop movment code
      */
     func moveViews(value:CGFloat){
         //Swift.print("moveViews() value: " + "\(value)")
-
         itemContainer.frame.y = value
     }
     override func scrollWheel(theEvent: NSEvent) {
