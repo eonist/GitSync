8f2e061c89f537b228b78f05173dede6867d334f
Eonist <unicornhive@gmail.com>
2016-03-02 14:37:31 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView6.swift

--- src/window/view/TestView6.swift
+++ src/window/view/TestView6.swift
@@ -43,6 +43,7 @@ class TestView6:AnimatableView {
         let r5 = RectGraphic(0,itemH*4,w,itemH,FillStyle(Colors.lightBlue()),nil)//Add a red box to the view
         itemContainer.addSubview(r5.graphic)
         r5.draw()
+        
         let colors:Array<NSColor> = [Colors.green(),Colors.yellow(),Colors.purple(),Colors.orange(),Colors.lightBlue()]
         for var i = 0; i < colors.count; ++i{
             print(i)
