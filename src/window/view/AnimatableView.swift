118c2493d6a7f2407bd177efcdfe719a09607c14
Eonist <unicornhive@gmail.com>
2016-03-03 17:52:55 +0100

    Files modified: 1
    Modified 1 file:src/window/view/AnimatableView.swift

--- src/window/view/AnimatableView.swift
+++ src/window/view/AnimatableView.swift
@@ -6,10 +6,9 @@ import Cocoa
  * TODO: Eventually you will have to move this class bellow InteractiveView
  */
 class AnimatableView:FlippedView,IAnimatable {
-    var displayLink: CVDisplayLink!/*This is the instance that enables frame animation*/
+    lazy var displayLink: CVDisplayLink = self.setUpDisplayLink()/*This is the instance that enables frame animation, lazying this value will probably haunt me later, playing with fire*/
     override init(frame frameRect: NSRect) {
         super.init(frame: frameRect)
-        displayLink = setUpDisplayLink()
         //Swift.print("displayLink: " + "\(displayLink)")
     }
     /**
