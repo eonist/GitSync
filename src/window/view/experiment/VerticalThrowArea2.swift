2646c8b1746f3a983ae621c4076ac4ae9ff18375
Eonist <unicornhive@gmail.com>
2016-03-03 14:05:51 +0100

    Files modified: 1
    Modified 1 file:src/window/view/experiment/VerticalThrowArea2.swift

--- src/window/view/experiment/VerticalThrowArea2.swift
+++ src/window/view/experiment/VerticalThrowArea2.swift
@@ -15,7 +15,7 @@ class VerticalThrowArea2 :InteractiveView2{
     var velocities:Array<CGFloat> = [0,0,0,0,0,0,0,0,0,0]
     init(){
         super.init(frame: NSRect(0,0,w,h))
-        self.mover = RubberBand(0,0,0.98)
+        self.mover = RubberBand(CGRect(0,0,200,200),CGRect(0,0,200,150*5))
         
         let rect = RectGraphic(0,0,w,h,FillStyle(NSColor.redColor().alpha(0.0)),nil)//Add a red box to the view
         addSubview(rect.graphic)
