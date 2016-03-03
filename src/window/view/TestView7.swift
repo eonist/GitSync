3de02a8c1abbed1547a13200555aec46e0d843a1
Eonist <unicornhive@gmail.com>
2016-03-03 17:27:53 +0100

    Files modified: 2
    Modified 2 files:src/window/CustomWin.swiftsrc/window/view/TestView7.swift

--- src/window/view/TestView7.swift
+++ src/window/view/TestView7.swift
@@ -3,7 +3,18 @@ import Foundation
  * TODO: Setup the test class
  * TODO: setup a new list class
  * TODO: add the new list class here
+ * TODO: test rubberband scrolling on list
+ * TODO: move the AnimatableView class before or after interactive view, i guess bfore because we may have animatable views that are not interactive in the future
  */
-class TestView7 {
-
+class TestView7:AnimatableView {
+    override func resolveSkin() {
+        super.resolveSkin()
+        testRBList()
+    }
+    /**
+     *
+     */
+    func testRBList(){
+        
+    }
 }
