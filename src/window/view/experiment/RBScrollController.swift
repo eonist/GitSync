0ac5b3a5ee3c551f664e8611dd64cec7bde0df77
Eonist <unicornhive@gmail.com>
2016-03-03 15:56:57 +0100

    Files modified: 2
    Modified 2 files:src/window/view/experiment/RBScrollController.swiftsrc/window/view/motion/RubberBand.swift

--- src/window/view/experiment/RBScrollController.swift
+++ src/window/view/experiment/RBScrollController.swift
@@ -3,7 +3,11 @@ import Foundation
  * NOTE: You forward the scrollWheel events here
  */
 class RBScrollController {
-    init(){
-        
+    var frame:CGRect/*represents the visible part of the content*/
+    var itemRect:CGRect/*represents the total size of the content*/
+
+    init(_ frame:CGRect, _ itemRect:CGRect){
+        self.frame = frame
+        self.itemRect = itemRect
     }
 }
