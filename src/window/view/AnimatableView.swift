9de0ecd6ab323bc95eaec5214a2f285475b643b6
Eonist <unicornhive@gmail.com>
2016-03-06 17:34:17 +0100

    Files modified: 1
    Modified 1 file:src/window/view/AnimatableView.swift

--- src/window/view/AnimatableView.swift
+++ src/window/view/AnimatableView.swift
@@ -18,7 +18,7 @@ class AnimatableView:FlippedView,IAnimatable {
     func onFrame(){
         //Swift.print("\(self.dynamicType)" + "onFrame()")
         for animator in animators{animator.onFrame()}
-        CATransaction.flush()/*if you dont flush your animation wont animate and you get this message: CoreAnimation: warning, deleted thread with uncommitted CATransaction; set CA_DEBUG_TRANSACTIONS=1 in environment to log backtraces.*/
+        //CATransaction.flush()/*if you dont flush your animation wont animate and you get this message: CoreAnimation: warning, deleted thread with uncommitted CATransaction; set CA_DEBUG_TRANSACTIONS=1 in environment to log backtraces.*/
     }
     /**
      * Note: It seems that you can't move this method into a static class method. Either internally in the same file or externally in another file
