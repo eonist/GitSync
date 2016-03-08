3408956bd153c54d555dea0090c0cc403879ca7a
Eonist <unicornhive@gmail.com>
2016-03-08 15:45:17 +0100

    Files modified: 1
    Modified 1 file:src/window/view/utils/IAnimatable.swift

--- src/window/view/utils/IAnimatable.swift
+++ src/window/view/utils/IAnimatable.swift
@@ -1,7 +1,8 @@
-import Foundation
+import Cocoa
 
 protocol IAnimatable:class {//this should probably be renamed to IAnimatableView
     func onFrame()
     var animators:Array<BaseAnimation>{get set}
     var drawCalls:Array<()->Void>{get set}
+    var displayLink: CVDisplayLink{get}
 }
