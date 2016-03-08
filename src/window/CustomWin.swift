803ec56c18e02af21b7c8028330a26e21456174a
Eonist <unicornhive@gmail.com>
2016-03-08 10:16:50 +0100

    Files modified: 2
    Modified 2 files:src/window/CustomWin.swiftsrc/window/view/AnimatableView.swift

--- src/window/CustomWin.swift
+++ src/window/CustomWin.swift
@@ -1,6 +1,9 @@
-import Foundation
+import Cocoa
 
 class CustomWin:Window{
+    var animators:Array<BaseAnimation> = []
+    var drawCalls:Array<()->Void> = []
+    lazy var displayLink: CVDisplayLink = self.setUpDisplayLink()/*This is the instance that enables frame animation, lazying this value will probably haunt me later, playing with fire*/
     override func resolveSkin() {
         self.contentView = TestView7(frame.width,frame.height)/*Sets the mainview of the window*/
     }
@@ -9,4 +12,41 @@ class CustomWin:Window{
         //Swift.print("CustomWin.windowDidResize")
         (self.contentView as! Element).setSize(self.frame.size.width,self.frame.size.height)
     }
+    
+    /**
+     * Fires on every screen refresh at 60 FPS, or device speed
+     */
+    func onFrame(){
+        //Swift.print("\(self.dynamicType)" + "onFrame()")
+        for animator in animators{animator.onFrame()}
+        while drawCalls.count > 0{
+            if(drawCalls.count > 0){drawCalls.removeFirst()()}//the extra assert was needed strangly enough, or els bugs started to appear after some time with stress testing
+        }
+        CATransaction.flush()/*if you dont flush your animation wont animate and you get this message: CoreAnimation: warning, deleted thread with uncommitted CATransaction; set CA_DEBUG_TRANSACTIONS=1 in environment to log backtraces.*/
+    }
+    /**
+     * Note: It seems that you can't move this method into a static class method. Either internally in the same file or externally in another file
+     */
+    func setUpDisplayLink() -> CVDisplayLink {
+        Swift.print("setUpDisplayLink()")
+        var displayLink: CVDisplayLink?
+        var status = kCVReturnSuccess
+        status = CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
+        //Swift.print("status: " + "\(status)")
+        /* Set up DisplayLink. This method fires 60fps*/
+        func displayLinkOutputCallback( displayLink: CVDisplayLink,_ inNow: UnsafePointer<CVTimeStamp>, _ inOutputTime: UnsafePointer<CVTimeStamp>,_ flagsIn: CVOptionFlags, _ flagsOut: UnsafeMutablePointer<CVOptionFlags>,_ displayLinkContext: UnsafeMutablePointer<Void>) -> CVReturn{
+            //Swift.print("displayLink is setup")
+            unsafeBitCast(displayLinkContext, CustomWin.self).onFrame()//drawRect(unsafeBitCast(displayLinkContext, NSOpenGLView.self).frame)
+            return kCVReturnSuccess
+        }
+        let outputStatus = CVDisplayLinkSetOutputCallback(displayLink!, displayLinkOutputCallback, UnsafeMutablePointer<Void>(unsafeAddressOf(self)))
+        outputStatus
+        //Swift.print("outputStatus: " + "\(outputStatus)")
+        let displayID = CGMainDisplayID()
+        let displayIDStatus = CVDisplayLinkSetCurrentCGDisplay(displayLink!, displayID)
+        displayIDStatus
+        //Swift.print("displayIDStatus: " + "\(displayIDStatus)")
+        return displayLink!
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
--- src/window/view/AnimatableView.swift
+++ src/window/view/AnimatableView.swift
@@ -5,47 +5,5 @@ import Cocoa
  * NOTE: Start and stop with CVDisplayLinkStart(displayLink) and CVDisplayLinkStop(displayLink) and CVDisplayLinkIsRunning(displayLink) to assert if the displayLink is running
  */
 class AnimatableView:FlippedView,IAnimatable {
-    var animators:Array<BaseAnimation> = []
-    var drawCalls:Array<()->Void> = []
-    lazy var displayLink: CVDisplayLink = self.setUpDisplayLink()/*This is the instance that enables frame animation, lazying this value will probably haunt me later, playing with fire*/
-    override init(frame frameRect: NSRect) {
-        super.init(frame: frameRect)
-        //Swift.print("displayLink: " + "\(displayLink)")
-    }
-    /**
-     * Fires on every screen refresh at 60 FPS, or device speed
-     */
-    func onFrame(){
-        //Swift.print("\(self.dynamicType)" + "onFrame()")
-        for animator in animators{animator.onFrame()}
-        while drawCalls.count > 0{
-            if(drawCalls.count > 0){drawCalls.removeFirst()()}//the extra assert was needed strangly enough, or els bugs started to appear after some time with stress testing
-        }
-        CATransaction.flush()/*if you dont flush your animation wont animate and you get this message: CoreAnimation: warning, deleted thread with uncommitted CATransaction; set CA_DEBUG_TRANSACTIONS=1 in environment to log backtraces.*/
-    }
-    /**
-     * Note: It seems that you can't move this method into a static class method. Either internally in the same file or externally in another file
-     */
-    func setUpDisplayLink() -> CVDisplayLink {
-        Swift.print("setUpDisplayLink()")
-        var displayLink: CVDisplayLink?
-        var status = kCVReturnSuccess
-        status = CVDisplayLinkCreateWithActiveCGDisplays(&displayLink)
-        //Swift.print("status: " + "\(status)")
-        /* Set up DisplayLink. This method fires 60fps*/
-        func displayLinkOutputCallback( displayLink: CVDisplayLink,_ inNow: UnsafePointer<CVTimeStamp>, _ inOutputTime: UnsafePointer<CVTimeStamp>,_ flagsIn: CVOptionFlags, _ flagsOut: UnsafeMutablePointer<CVOptionFlags>,_ displayLinkContext: UnsafeMutablePointer<Void>) -> CVReturn{
-            //Swift.print("displayLink is setup")
-            unsafeBitCast(displayLinkContext, AnimatableView.self).onFrame()//drawRect(unsafeBitCast(displayLinkContext, NSOpenGLView.self).frame)
-            return kCVReturnSuccess
-        }
-        let outputStatus = CVDisplayLinkSetOutputCallback(displayLink!, displayLinkOutputCallback, UnsafeMutablePointer<Void>(unsafeAddressOf(self)))
-        outputStatus
-        //Swift.print("outputStatus: " + "\(outputStatus)")
-        let displayID = CGMainDisplayID()
-        let displayIDStatus = CVDisplayLinkSetCurrentCGDisplay(displayLink!, displayID)
-        displayIDStatus
-        //Swift.print("displayIDStatus: " + "\(displayIDStatus)")
-        return displayLink!
-    }
-    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+ 
 }
