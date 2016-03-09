900a5186cd26e084084aca5b4b6d236356d090d8
Eonist <unicornhive@gmail.com>
2016-03-09 17:22:37 +0100

    Files modified: 1
    Modified 1 file:src/window/TranslucencyWin.swift

--- src/window/TranslucencyWin.swift
+++ src/window/TranslucencyWin.swift
@@ -23,8 +23,8 @@ class TranslucencyWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         self.center()/*centers the window, this can also be done via setOrigin and calculating screen size etc*/
         self.movableByWindowBackground = true/*This enables you do drag the window around via the background*/
         self.delegate = self/*So that we can use this class as the Window controller aswell*/
-        self.contentView = TranslucencyView(frame: NSRect(0,0,300,300))
+        let visualEffectView = TranslucencyView(frame: NSRect(0,0,300,300))
+        self.contentView?.addSubview(visualEffectView)
     }
-    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by the NSWindow*/
 }
