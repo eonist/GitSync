1259c7c16580fdf41aa6986ec08dd8d737045cfa
Eonist <unicornhive@gmail.com>
2016-03-09 17:03:52 +0100

    Files modified: 1
    Modified 1 file:src/window/translucent/TranslucencyView.swift

--- src/window/translucent/TranslucencyView.swift
+++ src/window/translucent/TranslucencyView.swift
@@ -1,5 +1,27 @@
-import Foundation
+import Cocoa
 
-class TranslucencyView {
+class TranslucencyView:NSVisualEffectView{
+    override init(frame frameRect: NSRect) {
+        super.init(frame: frameRect)
+        self.material = NSVisualEffectMaterial.UltraDark//AppearanceBased,Dark,MediumLight,PopOver,UltraDark,AppearanceBased,Titlebar,Menu
+        self.blendingMode = NSVisualEffectBlendingMode.BehindWindow
+        self.state = NSVisualEffectState.Active
+        self.maskImage = maskImage(cornerRadius: 8.0)/*this line applies the mask to the view*/
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
 
+extension TranslucencyView{
+    func maskImage(cornerRadius cornerRadius: CGFloat) -> NSImage {
+        let edgeLength = 2.0 * cornerRadius + 1.0
+        let maskImage = NSImage(size: NSSize(width: edgeLength, height: edgeLength), flipped: false) { rect in
+            let bezierPath = NSBezierPath(roundedRect: rect, xRadius: cornerRadius, yRadius: cornerRadius)
+            NSColor.blackColor().set()
+            bezierPath.fill()
+            return true
+        }
+        maskImage.capInsets = NSEdgeInsets(top: cornerRadius, left: cornerRadius, bottom: cornerRadius, right: cornerRadius)
+        maskImage.resizingMode = .Stretch
+        return maskImage
+    }
 }
