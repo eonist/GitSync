b9be99c84fcd43c9ba5d731d531f5a4e6efc63b4
Eonist <unicornhive@gmail.com>
2016-03-09 16:18:48 +0100

    Files modified: 2
    Modified 2 files:src/AppDelegate.swiftsrc/window/TranslucentWin.swift

--- src/window/TranslucentWin.swift
+++ src/window/TranslucentWin.swift
@@ -1,25 +1,21 @@
 import Cocoa
-
+/**
+ * Regular Translucent window
+ */
 class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
-    override var canBecomeMainWindow:Bool{return true}
-    override var canBecomeKeyWindow:Bool{return true}/*If you want a titleless window to be able to become a key window, you need to create a subclass of NSWindow and override -canBecomeKeyWindow*/
-    override var acceptsFirstResponder:Bool{return true}
     /**
      *
      */
     override init(contentRect: NSRect, styleMask aStyle: Int, backing bufferingType: NSBackingStoreType, `defer` flag: Bool) {
-         //NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask|NSFullSizeContentViewWindowMask
-        super.init(contentRect: Win.sizeRect, styleMask: NSBorderlessWindowMask|NSResizableWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
+        super.init(contentRect: Win.sizeRect, styleMask: NSTitledWindowMask|NSResizableWindowMask|NSMiniaturizableWindowMask|NSClosableWindowMask|NSFullSizeContentViewWindowMask, backing: NSBackingStoreType.Buffered, `defer`: false)
         self.contentView!.wantsLayer = true;/*this can and is set in the view*/
-        self.backgroundColor = NSColor.greenColor().alpha(0.0)
+        self.backgroundColor = NSColor.greenColor().alpha(0.2)
         self.opaque = false
         self.makeKeyAndOrderFront(nil)//moves the window to the front
         self.makeMainWindow()//makes it the apps main menu?
-        self.hasShadow = true
         //self.appearance = NSAppearance(named: NSAppearanceNameVibrantDark)
         self.titlebarAppearsTransparent = true
         self.center()
-        self.movableByWindowBackground = true
         
         //self.contentView = view
         //self.title = ""/*Sets the title of the window*/
@@ -28,15 +24,11 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         self.delegate = self
         
         let visualEffectView = NSVisualEffectView(frame: NSMakeRect(0, 0, Win.sizeRect.width, Win.sizeRect.height))
-        visualEffectView.material = NSVisualEffectMaterial.UltraDark//AppearanceBased,Dark,MediumLight,PopOver,UltraDark,AppearanceBased,Titlebar,Menu
+        visualEffectView.material = NSVisualEffectMaterial.AppearanceBased//Dark,MediumLight,PopOver,UltraDark,AppearanceBased,Titlebar,Menu
         visualEffectView.blendingMode = NSVisualEffectBlendingMode.BehindWindow
         visualEffectView.state = NSVisualEffectState.Active
         
         self.contentView?.addSubview(visualEffectView)
-        
-        visualEffectView.maskImage = maskImage(cornerRadius: 10.0)/*this line applies the mask to the view*/
-        
-        
         //self.contentView = visualEffectView
        
         /* self.contentView!.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-0-[visualEffectView]-0-|", options: NSLayoutFormatOptions.DirectionLeadingToTrailing, metrics: nil, views: ["visualEffectView":visualEffectView]))
@@ -48,22 +40,6 @@ class TranslucentWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         //visualEffectView.blendingMode = NSVisualEffectBlendingModeWithinWindow,
    
     }
-    
-    
-    
-    func maskImage(cornerRadius cornerRadius: CGFloat) -> NSImage {
-        let edgeLength = 2.0 * cornerRadius + 1.0
-        let maskImage = NSImage(size: NSSize(width: edgeLength, height: edgeLength), flipped: false) { rect in
-            let bezierPath = NSBezierPath(roundedRect: rect, xRadius: cornerRadius, yRadius: cornerRadius)
-            NSColor.blackColor().set()
-            bezierPath.fill()
-            return true
-        }
-        maskImage.capInsets = NSEdgeInsets(top: cornerRadius, left: cornerRadius, bottom: cornerRadius, right: cornerRadius)
-        maskImage.resizingMode = .Stretch
-        return maskImage
-    }
-    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by the NSWindow*/
 }
 /*class ViewAllowsVibrancy: NSView {
