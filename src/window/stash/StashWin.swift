a999bc9ed1022e1348329ab1fba602591f234b24
Eonist <unicornhive@gmail.com>
2016-03-09 15:16:53 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/StashWin.swift

--- src/window/stash/StashWin.swift
+++ src/window/stash/StashWin.swift
@@ -1,5 +1,12 @@
-import Foundation
+import Cocoa
 
-class StashWin {
-
+class StashWin:Window {
+    override func resolveSkin() {
+        self.contentView = TestView7(frame.width,frame.height)/*Sets the mainview of the window*/
+    }
+    override func windowDidResize(notification: NSNotification) {
+        //notification
+        //Swift.print("CustomWin.windowDidResize")
+        (self.contentView as! Element).setSize(self.frame.size.width,self.frame.size.height)
+    }
 }
