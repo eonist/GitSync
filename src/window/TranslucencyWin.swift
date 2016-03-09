01f01127bee0f5c1562b869254e1c91b317df686
Eonist <unicornhive@gmail.com>
2016-03-09 17:35:34 +0100

    Files modified: 1
    Modified 1 file:src/window/TranslucencyWin.swift

--- src/window/TranslucencyWin.swift
+++ src/window/TranslucencyWin.swift
@@ -33,6 +33,7 @@ class TranslucencyWin:NSWindow, NSApplicationDelegate, NSWindowDelegate{
         visualEffectView?.setFrameSize(self.frame.size)
         visualEffectView?.setBoundsSize(self.frame.size)
         visualEffectView?.maskImage?.size = self.frame.size
+        visualEffectView?.maskImage?.frame = self.frame
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}/*Required by the NSWindow*/
 }
