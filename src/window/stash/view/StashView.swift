f9962123d1d33da35ae9caec19c87dc5c3d42845
Eonist <unicornhive@gmail.com>
2016-03-09 19:06:29 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -3,6 +3,9 @@ import Cocoa
 class StashView:CustomView {
 
     override func resolveSkin() {
+        var css = ""//E8E8E8
+        css += "Window Element#background{fill:#EFEFF4;fill-alpha:0;corner-radius:4px;}"//<--you should target a bg element not the window it self, since now everything inherits these values
+        StyleManager.addStyle(css)
         super.resolveSkin()
         Swift.print("Hello world")
     }
