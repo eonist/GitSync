219df3bf56dc1a5f06a2f5865091d9b633cf5a52
Eonist <unicornhive@gmail.com>
2016-03-10 14:23:06 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -32,54 +32,6 @@ class StashView:CustomView {
 
 //import the stylekit logo
 
-class LeftSideBar:Element{
-    static let w:CGFloat = 75
-    override func resolveSkin() {
-        StyleManager.addStylesByURL("~/Desktop/css/stash.css")
-        
-        super.resolveSkin()
-        
-        
-        //background = addSubView(Element(width,height,self,"background")) as? IElement
-    }
-    /**
-     *
-     */
-    func createButtons(){
-        let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
-        //buttonSection.addSubView(Button(50,50,buttonSection,"avatar")) as! Button
-        let btn1 = buttonSection.addSubView(SelectButton(20,20,true,buttonSection,"inbox")) as! SelectButton
-        let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"home")) as! SelectButton
-        let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"pics")) as! SelectButton
-        let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"camera")) as! SelectButton
-        let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"game")) as! SelectButton
-        let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"view")) as! SelectButton
-        
-        let selectGroup = SelectGroup([btn1,btn2,btn3,btn4,btn5,btn6],btn1);
-        func onSelect(event:Event){
-            //do something here
-        }
-        selectGroup.event = onSelect
-    }
-    
-}
-class MainContent:Element{
-    //var background:IElement?
-    /**
-     * Draws the graphics
-     */
-    override func resolveSkin() {
-        let css = "MainContent{fill:#EFEFF4;fill-alpha:1;float:left;clear:none;corner-radius:0px 6px 0px 6px;}"
-        StyleManager.addStyle(css)
-        Swift.print("MainContent.resolveSkin()")
-        super.resolveSkin()
-        //background = addSubView(Element(width,height,self,"background")) as? IElement
-    }
 
-    override func setSize(width: CGFloat, _ height: CGFloat) {
-        super.setSize(width, height)
-        //background?.setSize(width, height)
-    }
-}
 
 
