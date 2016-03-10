ecfeb3f6a088190d5de108d3bcfabfb48266b8d3
Eonist <unicornhive@gmail.com>
2016-03-10 15:13:46 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/MainContent.swift

--- src/window/stash/view/MainContent.swift
+++ src/window/stash/view/MainContent.swift
@@ -14,8 +14,37 @@ class MainContent:Element{
         background = addSubView(Element(width,height,self,"background")) as? IElement
         let box = addSubView(Element(40,40,self,"box")) as? IElement
         box
+        createArticleItem()
     }
-    
+    /**
+    *
+    */
+    func createArticleItem(){
+        var css:String = ""
+        css += "Section#textContainer{fill:white;float:left;clear:left;}"
+        css += "Text{"
+        css +=     "float:left;"
+        css +=     "clear:left;"
+        css +=     "font:Lucida Grande;"
+        css +=     "size:12px;"
+        css +=     "align:left;"
+        css +=     "autoSize:none;"
+        css +=     "color:grey6;"
+        css +=     "type:input;"
+        css +=     "selectable:true;"
+        css +=     "wordWrap:true;"
+        css +=     "margin-top:4px;"
+        css +=     "backgroundColor:orange;"
+        css +=     "background:false;"
+        css += "}"
+        StyleManager.addStyle(css)
+        
+        let container = Section(200,50,self,"textContainer")
+        addSubview(container)
+        
+        let text:Text = container.addSubView(Text(100,24,"This is text: ",container)) as! Text
+        text
+    }
     override func setSize(width: CGFloat, _ height: CGFloat) {
         super.setSize(width, height)
         //background?.setSize(width, height)
