5647f3e3aa330146d83f65334019104126de5c8b
Eonist <unicornhive@gmail.com>
2016-03-10 14:23:06 +0100

    Files modified: 2
    Modified 2 files:src/window/stash/view/LeftSideBar.swiftsrc/window/stash/view/MainContent.swift

--- src/window/stash/view/MainContent.swift
+++ src/window/stash/view/MainContent.swift
@@ -1,5 +1,21 @@
 import Foundation
 
-class MainContent {
-
+class MainContent:Element{
+    //var background:IElement?
+    /**
+    * Draws the graphics
+    */
+    override func resolveSkin() {
+        let css = "MainContent{fill:#EFEFF4;fill-alpha:1;float:left;clear:none;corner-radius:0px 6px 0px 6px;}"
+        StyleManager.addStyle(css)
+        Swift.print("MainContent.resolveSkin()")
+        super.resolveSkin()
+        //background = addSubView(Element(width,height,self,"background")) as? IElement
+    }
+    
+    override func setSize(width: CGFloat, _ height: CGFloat) {
+        super.setSize(width, height)
+        //background?.setSize(width, height)
+    }
 }
+
