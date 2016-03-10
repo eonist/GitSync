b127ab1e6cdefbb9c4cceb0a3112f86373ce171d
Eonist <unicornhive@gmail.com>
2016-03-10 17:05:46 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/MainContent.swift

--- src/window/stash/view/MainContent.swift
+++ src/window/stash/view/MainContent.swift
@@ -67,5 +67,20 @@ class ArticleList:List{
     }
 }
 
-class 
+class ArticleItem:Element{
+    override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
+        //Swift.print("Button.init()")
+        super.init(width, height, parent, id)
+        //addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
+    }
+
+    
+        
+    
+    override func resolveSkin() {
+        super.resolveSkin()
+        
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
 
