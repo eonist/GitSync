0ade78c5f5a7a36fe5e102926e6b64852d062363
Eonist <unicornhive@gmail.com>
2016-03-10 14:19:02 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -48,14 +48,14 @@ class LeftSideBar:Element{
     func createButtons(){
         let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
         //buttonSection.addSubView(Button(50,50,buttonSection,"avatar")) as! Button
-        let btn1 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"inbox")) as! SelectButton
+        let btn1 = buttonSection.addSubView(SelectButton(20,20,true,buttonSection,"inbox")) as! SelectButton
         let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"home")) as! SelectButton
         let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"pics")) as! SelectButton
         let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"camera")) as! SelectButton
         let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"game")) as! SelectButton
         let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"view")) as! SelectButton
         
-        let selectGroup = SelectGroup([btn1,btn2,btn3,btn4,btn5,btn6/**/],btn1);
+        let selectGroup = SelectGroup([btn1,btn2,btn3,btn4,btn5,btn6],btn1);
         selectGroup
     }
     
