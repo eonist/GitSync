932b36d5272eb10dad673464af109ba3c58af074
Eonist <unicornhive@gmail.com>
2016-03-10 15:33:18 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/MainContent.swift

--- src/window/stash/view/MainContent.swift
+++ src/window/stash/view/MainContent.swift
@@ -12,8 +12,8 @@ class MainContent:Element{
         Swift.print("MainContent.resolveSkin()")
         super.resolveSkin()
         background = addSubView(Element(width,height,self,"background")) as? IElement
-        let box = addSubView(Element(40,40,self,"box")) as? IElement
-        box
+        //let box = addSubView(Element(40,40,self,"box")) as? IElement
+        //box
         createArticleItem()
     }
     /**
@@ -26,7 +26,7 @@ class MainContent:Element{
         //let textString:String = "Test something fun this is the tech behind this years revolution in computer technology. Internet continues to widen as the spread for A.I is heading twords it's end"
         let header:Text = container.addSubView(Text(120,24,"Header goes here",container,"header")) as! Text
         header
-        let date:Text = container.addSubView(Text(80,24,"24 June 2016",container,"date")) as! Text
+        let date:Text = container.addSubView(Text(100,24,"24 June 2016",container,"date")) as! Text
         date
     }
     override func setSize(width: CGFloat, _ height: CGFloat) {
