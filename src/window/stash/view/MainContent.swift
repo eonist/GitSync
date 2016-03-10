cd7f2e80ce462074491411218f37df7225ccbecf
Eonist <unicornhive@gmail.com>
2016-03-10 17:07:50 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/MainContent.swift

--- src/window/stash/view/MainContent.swift
+++ src/window/stash/view/MainContent.swift
@@ -68,7 +68,7 @@ class ArticleList:List{
 }
 
 class ArticleItem:Element{
-    override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = nil){
+    override init(_ width:CGFloat, _ height:CGFloat, header:String,date:String,title:String,_ parent:IElement? = nil, _ id:String? = nil){
         //Swift.print("Button.init()")
         super.init(width, height, parent, id)
         //addTrackingRect(self.bounds, owner: self, userData: nil, assumeInside: true)//This enables entered and exited events to fire //let focusTrackingAreaOptions:NSTrackingAreaOptions = [NSTrackingActiveInActiveApp,NSTrackingMouseEnteredAndExited,NSTrackingAssumeInside,NSTrackingInVisibleRect,NSTrackingEnabledDuringMouseDrag]//NSTrackingEnabledDuringMouseDrag to mine to make sure the rollover behaves still when dragging in and out of the area.//TODO: you may need to update trackingarea: - (void)updateTrackingAreas
