f533afcc050ca26a3c5f8c7cced91903b55080ac
Eonist <unicornhive@gmail.com>
2016-03-10 14:00:45 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -37,14 +37,16 @@ class LeftSideBar:Element{
     override func resolveSkin() {
         var css = "LeftSideBar{float:left;clear:left;}"
         css += "Section#buttonSection {padding-top:16px;padding-left:12px;}"
-        css += "Section#buttonSection Button{fill-alpha:0.2;float:left;clear:left;margin-top:12px;margin-left:16px;padding:0px;}"
+        css += "Section#buttonSection SelectButton{fill-alpha:0.2;float:left;clear:left;margin-top:12px;margin-left:16px;padding:0px;}"
+        css += "Section#buttonSection SelectButton:selected{fill-alpha:0.6;}"
+        
         //css += "Section#buttonSection Button#avatar{fill-alpha:1;fill:~/Desktop/svg_icons/avatar.svg none;margin-left:0px;margin-top:0px;}"
-        css += "Section#buttonSection Button#inbox{fill:~/Desktop/svg_icons/inbox.svg white;fill-alpha:0.6;}"
-        css += "Section#buttonSection Button#home{fill:~/Desktop/svg_icons/home.svg white;}"
-        css += "Section#buttonSection Button#pics{fill:~/Desktop/svg_icons/pics.svg white;}"
-        css += "Section#buttonSection Button#camera{fill:~/Desktop/svg_icons/camera.svg white;}"
-        css += "Section#buttonSection Button#game{fill:~/Desktop/svg_icons/game.svg white;}"
-        css += "Section#buttonSection Button#view{fill:~/Desktop/svg_icons/view.svg white;}"
+        css += "Section#buttonSection SelectButton#inbox{fill:~/Desktop/svg_icons/inbox.svg white;}"
+        css += "Section#buttonSection SelectButton#home{fill:~/Desktop/svg_icons/home.svg white;}"
+        css += "Section#buttonSection SelectButton#pics{fill:~/Desktop/svg_icons/pics.svg white;}"
+        css += "Section#buttonSection SelectButton#camera{fill:~/Desktop/svg_icons/camera.svg white;}"
+        css += "Section#buttonSection SelectButton#game{fill:~/Desktop/svg_icons/game.svg white;}"
+        css += "Section#buttonSection SelectButton#view{fill:~/Desktop/svg_icons/view.svg white;}"
         StyleManager.addStyle(css)
         Swift.print("MainContent.resolveSkin()")
         super.resolveSkin()
@@ -58,12 +60,12 @@ class LeftSideBar:Element{
     func createButtons(){
         let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
         //buttonSection.addSubView(Button(50,50,buttonSection,"avatar")) as! Button
-        let btn1 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"inbox")) as! SelectButton
-        let btn2 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"home")) as! SelectButton
-        let btn3 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"pics")) as! SelectButton
-        let btn4 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"camera")) as! SelectButton
-        let btn5 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"game")) as! SelectButton
-        let btn6 = buttonSection.addSubView(SelectButton(20,20,buttonSection,"view")) as! SelectButton
+        let btn1 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"inbox")) as! SelectButton
+        let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"home")) as! SelectButton
+        let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"pics")) as! SelectButton
+        let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"camera")) as! SelectButton
+        let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"game")) as! SelectButton
+        let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"view")) as! SelectButton
         
         let selectGroup = SelectGroup([btn1,btn2,btn3,btn4/**/],btn1);
         selectGroup
