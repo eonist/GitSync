669c1de496ab04cec8b73d30cf1d4a04551d74b8
Eonist <unicornhive@gmail.com>
2016-03-11 19:04:01 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/LeftSideBar.swift

--- src/window/stash/view/LeftSideBar.swift
+++ src/window/stash/view/LeftSideBar.swift
@@ -1,15 +1,16 @@
 import Foundation
 
 class LeftSideBar:Element{
+    static let w:CGFloat = 75
     override func resolveSkin() {
         super.resolveSkin()
     }
     func createButtons(){
         let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
         let buttonTitles = ["inbox","home","pics","camera","game","view"]
-        var buttons:Array<SelectButton> = []
+        var buttons:Array<ISelectable> = []
         for buttonTitle in buttonTitles{
-            buttons.append(buttonSection.addSubView(SelectButton(20,20,true,buttonSection,buttonTitle)) as! SelectButton)
+            buttons.append(buttonSection.addSubView(SelectButton(20,20,true,buttonSection,buttonTitle)) as! ISelectable)
         }
         let selectGroup = SelectGroup(buttons,buttons[0]);
         func onSelect(event:Event){
