8f967c73f5bd4da84763ec053ad0341fe5b913ae
Eonist <unicornhive@gmail.com>
2016-03-11 18:59:45 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/LeftSideBar.swift

--- src/window/stash/view/LeftSideBar.swift
+++ src/window/stash/view/LeftSideBar.swift
@@ -7,12 +7,13 @@ class LeftSideBar:Element{
     }
     func createButtons(){
         let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
-        let btn1 = buttonSection.addSubView(SelectButton(20,20,true,buttonSection,"inbox")) as! SelectButton
-        let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"home")) as! SelectButton
-        let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"pics")) as! SelectButton
-        let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"camera")) as! SelectButton
-        let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"game")) as! SelectButton
-        let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"view")) as! SelectButton
+        let buttons = ["inbox","home","pics","camera","game","view"]
+        let btn1 = buttonSection.addSubView(SelectButton(20,20,true,buttonSection,"")) as! SelectButton
+        let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
+        let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
+        let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
+        let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
+        let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
         
         let selectGroup = SelectGroup([btn1,btn2,btn3,btn4,btn5,btn6],btn1);
         func onSelect(event:Event){
