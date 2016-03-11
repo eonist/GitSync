0298445bbcc4e71d4e4d792c1d8ec3fe7a8265f0
Eonist <unicornhive@gmail.com>
2016-03-11 19:01:59 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/LeftSideBar.swift

--- src/window/stash/view/LeftSideBar.swift
+++ src/window/stash/view/LeftSideBar.swift
@@ -1,21 +1,17 @@
 import Foundation
 
 class LeftSideBar:Element{
-    static let w:CGFloat = 75
     override func resolveSkin() {
         super.resolveSkin()
     }
     func createButtons(){
         let buttonSection = self.addSubView(Section(75,200,self,"buttonSection")) as! Section
-        let buttons = ["inbox","home","pics","camera","game","view"]
-        let btn1 = buttonSection.addSubView(SelectButton(20,20,true,buttonSection,"")) as! SelectButton
-        let btn2 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
-        let btn3 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
-        let btn4 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
-        let btn5 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
-        let btn6 = buttonSection.addSubView(SelectButton(20,20,false,buttonSection,"")) as! SelectButton
-        
-        let selectGroup = SelectGroup([btn1,btn2,btn3,btn4,btn5,btn6],btn1);
+        let buttonTitles = ["inbox","home","pics","camera","game","view"]
+        var buttons:Array<SelectButton> = []
+        for buttonTitle in buttonTitles{
+            buttons.append(buttonSection.addSubView(SelectButton(20,20,true,buttonSection,buttonTitle)) as! SelectButton)
+        }
+        let selectGroup = SelectGroup(buttons,buttons[0]);
         func onSelect(event:Event){
             //do something here
         }
