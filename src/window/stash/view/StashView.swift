2a1b2485d81d2a67908aa81ec6abc8241f933b3b
Eonist <unicornhive@gmail.com>
2016-03-11 12:56:10 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -3,6 +3,10 @@ import Cocoa
 class StashView:CustomView {
     var leftSideBar:LeftSideBar?
     override func resolveSkin() {
+        Swift.print("hello world")
+        
+        return
+        
         var css = ""//E8E8E8
         css += "Window Element#background{fill:#EFEFF4;fill-alpha:0;}"//<--you should target a bg element not the window it self, since now everything inherits these values
         StyleManager.addStyle(css)
