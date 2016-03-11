01ac611c3f2b70fe9d123d0d0f3b52eaf9de0372
Eonist <unicornhive@gmail.com>
2016-03-11 17:07:26 +0100

    Files modified: 1
    Modified 1 file:src/window/stash/view/StashView.swift

--- src/window/stash/view/StashView.swift
+++ src/window/stash/view/StashView.swift
@@ -14,16 +14,19 @@ class StashView:CustomView {
         //Swift.print("cssString: " + "\(cssString)")
         //cssString += ""
         
+        /*
         let cssString:String = FileParser.content("~/Desktop/css/test.css".tildePath)!
         
         let result = CSSFileParser.separateImportsAndStyles(cssString)
         
         Swift.print("result.imports: " + "\(result.imports)")
         Swift.print("result.style: " + "\(result.style)")
+        */
         
         
-        /*StyleManager.addStylesByURL("~/Desktop/css/test.css")
-        Swift.print("StyleManager.styles.count: " + "\(StyleManager.styles.count)")*/
+        StyleManager.addStylesByURL("~/Desktop/css/test.css")
+        Swift.print("StyleManager.styles.count: " + "\(StyleManager.styles.count)")
+        /**/
         /**/
         
         return
