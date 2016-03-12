15a96491abd6f3ac58d5b3c70231c27034d968d5
Eonist <unicornhive@gmail.com>
2016-03-12 08:47:55 +0100

    Files modified: 1
    Modified 1 file:src/window/explorer/BasicView.swift

--- src/window/explorer/BasicView.swift
+++ src/window/explorer/BasicView.swift
@@ -7,8 +7,14 @@ class BasicView:CustomView {
     }
 }
 class Card:Element{
-    init(_ width: CGFloat, _ height: CGFloat, text:String = "", _ parent: IElement?, _ id: String?) {
+    var text:String;
+    init(_ width: CGFloat, _ height: CGFloat, _ text:String = "", _ parent: IElement?, _ id: String?) {
+        self.text = text
         super.init(width, height, parent, id)
+    }
+    override func resolveSkin() {
+        super.resolveSkin()
+        
     }
 
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
