b746c32d3db89b4af3a6f7a21c1b632de238598c
Eonist <unicornhive@gmail.com>
2016-03-12 08:43:48 +0100

    Files modified: 1
    Modified 1 file:src/window/explorer/BasicView.swift

--- src/window/explorer/BasicView.swift
+++ src/window/explorer/BasicView.swift
@@ -7,5 +7,11 @@ class BasicView:CustomView {
     }
 }
 class Card:Element{
-    
+    init(_ width: CGFloat, _ height: CGFloat, text:String = "", _ parent: IElement?, _ id: String?) {
+        super.init(width, height, parent, id)
+    }
+
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
+
+
