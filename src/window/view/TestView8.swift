d8c204c15174c01880cae479c168d40530367824
Eonist <unicornhive@gmail.com>
2016-03-12 15:38:05 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView8.swift

--- src/window/view/TestView8.swift
+++ src/window/view/TestView8.swift
@@ -6,6 +6,10 @@ class TestView8 :CustomView {
         StyleManager.addStylesByURL("~/Desktop/css/del.css")
         let blueBox = addSubView(Element(100,100,self,"blueBox"))
         blueBox.addSubView(Element(0,50,blueBox,"redBox"))
+        
+        //Continue here: I think the bug is that you cant have NaN values in the frame. 
+        //So store width and height in variables and set the frame to zero. Try it!
+        
     }
 }
 
