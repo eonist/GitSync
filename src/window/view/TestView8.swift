c61aa5a7ff7c7ccd456684322958ade696a1eace
Eonist <unicornhive@gmail.com>
2016-03-12 16:13:49 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView8.swift

--- src/window/view/TestView8.swift
+++ src/window/view/TestView8.swift
@@ -7,10 +7,8 @@ class TestView8 :CustomView {
         let blueBox = addSubView(Element(100,100,self,"blueBox"))
         blueBox.addSubView(Element(0,50,blueBox,"redBox"))
         
-        //Continue here: I think the bug is that you cant have NaN values in the frame. 
+        //Continue here: I think the bug is that you cant have NaN values in the frame.
         //So store width and height in variables and set the frame to zero. Try it!
         
     }
 }
-
-//continue here: get correct width from parent when parent has padding both left and right. When child is 100% in width
