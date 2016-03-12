32182769febbdad49e6316e496a1a3205ae9b42b
Eonist <unicornhive@gmail.com>
2016-03-12 10:47:42 +0100

    Files modified: 1
    Modified 1 file:src/window/explorer/BasicView.swift

--- src/window/explorer/BasicView.swift
+++ src/window/explorer/BasicView.swift
@@ -3,21 +3,12 @@ import Foundation
 class BasicView:CustomView {
     var container:Container!
     override func resolveSkin() {
-        //StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
+        StyleManager.addStylesByURL("~/Desktop/css/explorer/explorer.css")
         super.resolveSkin()
         var css:String = ""
-        css += "Window Container#main{"
-        css +=     "float:left;"
-        css +=     "clear:left;"
-        css +=     "padding-left:76px;"
-        css +=     "padding-top:32px;"
-        css += "}"
-        css += "Card{"
-        css +=     "fill:blue;"
-        css +=     "float:left;"
-        css += "}"
+        css += ""
+        //StyleManager.addStyle(css)
         Swift.print("hello world")
-        StyleManager.addStyle(css)
         container = addSubView(Container(1000,800,self,"main")) as! Container
         createButton()
     }
