c1f0ec26dac05670ccdf92c530c52d7e37f806c7
Eonist <unicornhive@gmail.com>
2016-03-12 12:47:56 +0100

    Files modified: 1
    Modified 1 file:src/window/view/TestView8.swift

--- src/window/view/TestView8.swift
+++ src/window/view/TestView8.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-class TestView8 {
-
+class TestView8 :CustomView {
+    override func resolveSkin() {
+        super.resolveSkin()
+        
+        let box = addSubView(Element(100,100,self,"box"))
+    }
 }
+
+//continue here: get correct width from parent when parent has padding both left and right. When child is 100% in width
