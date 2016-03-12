7c3fae20ad4b233386c0166cced7b480e825002a
Eonist <unicornhive@gmail.com>
2016-03-12 08:49:52 +0100

    Files modified: 1
    Modified 1 file:src/window/explorer/BasicView.swift

--- src/window/explorer/BasicView.swift
+++ src/window/explorer/BasicView.swift
@@ -14,7 +14,8 @@ class Card:Element{
     }
     override func resolveSkin() {
         super.resolveSkin()
-        
+        addSubView(Element(CGFloat.NaN, CGFloat.NaN, self, "ruler")) as! Element;
+        addSubView(Text(CGFloat.NaN, CGFloat.NaN, _text, self, "cardText"));
     }
 
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
