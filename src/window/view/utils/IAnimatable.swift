a1f670cc746ca009ed24e83914e6c262522bd610
Eonist <unicornhive@gmail.com>
2016-03-13 08:32:36 +0100

    Files deleted: 1, Files renamed: 1
    Deleted 1 file:src/window/view/utils/IAnimatable.swift

--- src/window/view/utils/IAnimatable.swift
+++
@@ -1,8 +0,0 @@
-import Cocoa
-
-protocol IAnimatable:class {//this should probably be renamed to IAnimatableView
-    func onFrame()
-    var animators:Array<BaseAnimation>{get set}
-    //var drawCalls:Array<()->Void>{get set}
-    var displayLink: CVDisplayLink{get}
-}
