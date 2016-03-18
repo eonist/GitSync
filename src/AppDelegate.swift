da830561b871f3b300f308b3e385e9970f56ce48
Eonist <unicornhive@gmail.com>
2016-03-18 06:47:07 +0100

    Files modified: 1, Files deleted: 1
    Deleted 1 file:src/utils/Create.swiftModified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -4,24 +4,11 @@ import Cocoa
  * The class for the application
  */
 class AppDelegate: NSObject, NSApplicationDelegate{
-    var win:NSWindow?
     /**
      * Initializes your application
      */
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        //.print("applicationDidFinishLaunching")
-        /**/
-        var css = ""//E8E8E8
-        css += "Window Element#background{fill:#EFEFF4;corner-radius:4px;}"//<--you should target a bg element not the window it self, since now everything inherits these values
-        //css += "Section{fill:#EFEFF4;corner-radius:4px 4px 0px 0px;}"
-        //css += "Button{fill:green;}"
-        css += ""
-        StyleManager.addStyle(css)
-
-
-        win = TranslucencyWin()//StashWin(300,300)//CustomWin(1000,800)/*CustomWin(300,300)*///TranslucencyWin()//TranslucentWin()////CustomWin(300,300)//Win()//Win()//TranslucentWin()//CustomWin(300,200)//Win()//CustomWin()/*Init the window*///Win()//TranslucentWin()
-        let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
-        app.windows[0].close()/*close the initial non-optional default window*/
+        swift.print("applicationDidFinishLaunching")
     }
     /*
      * When the application closes
