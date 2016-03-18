246f20c1696010292055452a59d77ee9e6564675
Eonist <unicornhive@gmail.com>
2016-03-18 07:59:27 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -8,6 +8,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         // Insert code here to initialize your application
         Swift.print("hello world")
+        GitParser.status("~/Desktop/css","")
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
