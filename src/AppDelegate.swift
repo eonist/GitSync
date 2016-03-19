d130a5547dc4d328c3ec133ce92d66f81b0d3b72
Eonist <unicornhive@gmail.com>
2016-03-19 07:19:37 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -28,7 +28,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
      */
     func doCommit(repoItem:Dictionary<String,String>){
         //log "GitSync's handle_commit_interval() a repo with doCommit " & (remote_path of repo_item) & " local path: " & (local_path of repo_item)
-        Swift.print("doCommit()" + "\(repoItem["remote-path"])")
+        Swift.print("doCommit() remote path: " + "\(repoItem["remote-path"])" + " local path: " + "\(repoItem["local-path"])")
         
         
     }
