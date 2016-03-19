731c20f24d01ebad313916db25dec9d7500df804
Eonist <unicornhive@gmail.com>
2016-03-19 17:50:32 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -33,9 +33,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         let test = "testing this stuff"
         Swift.print("test.utf16: " + "\(test.utf16)")
+        Swift.print("test.utf8: " + "\(test.utf8)")
         
         let transform = "Any-Hex/Java"
         let input = "\\u5404\\u500b\\u90fd" as NSString
+        Swift.print("input.UTF8String: " + "\(input.UTF8String)")
+
         let convertedString = input.mutableCopy() as! NSMutableString
         
         CFStringTransform(convertedString, nil, transform as NSString, true)
