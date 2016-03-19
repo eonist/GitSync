71372ee9b0a3114d6c542c55668cb3596e6014d3
Eonist <unicornhive@gmail.com>
2016-03-19 11:50:19 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -40,7 +40,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let unMergedFiles = GitParser.unMergedFiles(localPath) //Asserts if there are unmerged paths that needs resolvment
             MergeUtils.resolveMergeConflicts(localPath, branch, unMergedFiles)
         }
-        let hasCommited = doCommit(localPath) //if there were no commits false will be returned
+        let hasCommited = GitSyncUtils.doCommit(localPath) //if there were no commits false will be returned
         Swift.print("hasCommited: " + "\(hasCommited)")
     }
     /**
@@ -54,32 +54,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let localPath:String = repoItem["local-path"]!
         let remotePath:String = repoItem["remote-path"]!
         MergeUtils.manualMerge(localPath, remotePath, branch)//commits, merges with promts, (this method also test if a merge is needed or not, and skips it if needed)
-    }
-    /**
-     * This method generates a git status list,and asserts if a commit is due, and if so, compiles a commit message and then tries to commit
-     * Returns true if a commit was made, false if no commit was made or an error occured
-     * NOTE: checks git staus, then adds changes to the index, then compiles a commit message, then commits the changes, and is now ready for a push
-     * NOTE: only commits if there is something to commit
-     * TODO: add branch parameter to this call
-     * NOTE: this a purly local method, does not need to communicate with remote servers etc..
-     */
-    func doCommit(localRepoPath:String)->Bool{
-        Swift.print("doCommit()")
-        let statusList = StatusUtils.generateStatusList(localRepoPath)//get current status
-        if (statusList.count > 0) {
-            Swift.print("there is something to add or commit")
-            StatusUtils.processStatusList(localRepoPath, statusList) //process current status by adding files, now the status has changed, some files may have disapared, some files now have status as renamed that prev was set for adding and del
-            let commitMsgTitle = CommitUtils.sequenceCommitMsgTitle(statusList) //sequence commit msg title for the commit
-            Swift.print("commitMsgTitle: " + "\(commitMsgTitle)")
-            let commitMsgDesc = DescUtils.sequenceDescription(statusList)//sequence commit msg description for the commit
-            Swift.print("commitMsgDesc: " + "\(commitMsgDesc)")
-            let commitResult = GitModifier.commit(localRepoPath, commitMsgTitle, commitMsgDesc) //commit
-            Swift.print("commitResult: " + "\(commitResult)")
-            return true//return true to indicate that the commit completed
-        }else{
-            Swift.print("nothing to add or commit")
-            return false //break the flow since there is nothing to commit or process
-        }
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         // Insert code here to tear down your application
