a7de00f5637b53e5e5ccf7f88820cbd9b725ac51
Eonist <unicornhive@gmail.com>
2016-03-20 13:02:08 +0100

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -4,9 +4,24 @@ class MainView:CustomView{
     var leftContainer:Container?
     var leftSideBar:LeftSideBar?
     override func resolveSkin() {
-        StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
         super.resolveSkin()
-        leftContainer = Container(0,0,self)
-        leftSideBar = leftContainer!.addSubView(LeftSideBar(LeftSideBar.w,height,self))
+        leftContainer = addSubView(Container(0,0,self))
+        createCustomTitleBar()
+        leftSideBar = leftContainer!.addSubView(LeftSideBar(LeftSideBar.w,height,leftContainer))
+    }
+    /**
+     *
+     */
+    func createCustomTitleBar() {
+        StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
+        StyleManager.addStyle("Section#titleBar{padding-top:16px;padding-left:12px;}")
+        
+        section = leftSideBar!.addSubView(Section(75,16,leftSideBar,"titleBar"))
+        closeButton = section!.addSubView(Button(0,0,section!,"close"))/*<--TODO: the w and h should be NaN, test if it supports this*/
+        minimizeButton = section!.addSubView(Button(0,0,section!,"minimize"))
+        maximizeButton = section!.addSubView(Button(0,0,section!,"maximize"))
+    }
+    override func createTitleBar() {
+        //
     }
 }
