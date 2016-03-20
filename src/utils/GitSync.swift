70002e0b9db86614ae7690044f0885627c7b85c0
Eonist <unicornhive@gmail.com>
2016-03-20 11:23:11 +0100

    Files modified: 1
    Modified 1 file:src/utils/GitSync.swift

--- src/utils/GitSync.swift
+++ src/utils/GitSync.swift
@@ -28,4 +28,48 @@ class GitSync{
             return false //break the flow since there is nothing to commit or process
         }
     }
+    /**
+     * Handles the process of making a commit for a single repository
+     */
+    class func initCommit(repoItem:Dictionary<String,String>,_ branch:String){
+        //log "GitSync's handle_commit_interval() a repo with doCommit " & (remote_path of repo_item) & " local path: " & (local_path of repo_item)
+        
+        let localPath:String = repoItem["local-path"]!
+        //Swift.print("localPath: " + "\(localPath)")
+        let hasUnMergedpaths = GitAsserter.hasUnMergedPaths(localPath)//Asserts if there are unmerged paths that needs resolvment
+        Swift.print("hasUnMergedpaths: " + "\(hasUnMergedpaths)")
+        if(hasUnMergedpaths){
+            Swift.print("has unmerged paths to resolve")
+            let unMergedFiles = GitParser.unMergedFiles(localPath) //Asserts if there are unmerged paths that needs resolvment
+            MergeUtils.resolveMergeConflicts(localPath, branch, unMergedFiles)
+        }
+        let hasCommited = GitSync.doCommit(localPath) //if there were no commits false will be returned
+        Swift.print("hasCommited: " + "\(hasCommited)")
+    }
+    /**
+     * Handles the process of making a push for a single repository
+     * NOTE: We must always merge the remote branch into the local branch before we push our changes.
+     * NOTE: this method performs a "manual pull" on every interval
+     * TODO: contemplate implimenting a fetch call after the pull call, to update the status, whats the diff between git fetch and git remote update again?
+     */
+    class func initPush(repoItem:Dictionary<String,String>,_ branch:String){
+        Swift.print("initPush")
+        let localPath:String = repoItem["local-path"]!
+        var remotePath:String = repoItem["remote-path"]!
+        
+        if(remotePath.test("^https://.+$")){remotePath = remotePath.subString(8, remotePath.count)}//support for partial and full url,strip away the https://, since this will be added later
+        
+        MergeUtils.manualMerge(localPath, remotePath, branch)//commits, merges with promts, (this method also test if a merge is needed or not, and skips it if needed)
+        let hasLocalCommits = GitAsserter.hasLocalCommits(localPath, branch) //TODO: maybe use GitAsserter's is_local_branch_ahead instead of this line
+        if (hasLocalCommits) { //only push if there are commits to be pushed, hence the has_commited flag, we check if there are commits to be pushed, so we dont uneccacerly push if there are no local commits to be pushed, we may set the commit interval and push interval differently so commits may stack up until its ready to be pushed, read more about this in the projects own FAQ
+            let theKeychainItemName = repoItem["keychain-item-name"]!
+            //Swift.print("theKeychainItemName: " + "\(theKeychainItemName)")
+            let keychainPassword = KeyChainParser.password(theKeychainItemName)
+            Swift.print("keychainPassword: " + "\(keychainPassword)")
+            let remoteAccountName = theKeychainItemName
+            Swift.print("remoteAccountName: " + "\(remoteAccountName)")
+            let pushCallBack = GitModifier.push(localPath, remotePath, remoteAccountName, keychainPassword, branch)
+            Swift.print("pushCallBack: " + "\(pushCallBack)")
+        }
+    }
 }
