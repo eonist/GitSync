bb108f8c16a8ea31891041c92c2fc664f5b1bfe9
Eonist <unicornhive@gmail.com>
2016-03-20 12:42:11 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,25 +1,4 @@
 import Cocoa
-
-@NSApplicationMain
-class AppDelegate: NSObject, NSApplicationDelegate {
-    weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
-    var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
-    func applicationDidFinishLaunching(aNotification: NSNotification) {
-        StyleManager.addStylesByURL("~/Desktop/css/window.css")
-        win = TranslucencyWin()//Win(1000,800)
-        NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
-    }
-    func applicationWillTerminate(aNotification: NSNotification) {
-        print("Good-bye")
-    }
-}
-
-
-
-
-
-
-import Cocoa
 /**
  * This is the main class for the application
  * TODO: create the window
@@ -33,39 +12,28 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         StyleManager.addStyle("Window{fill:#EFEFF4;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
-        
-        
         win = GitSyncWin(300,300)/*Init the window*/
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
-        
-        
-        
-        
-        /*
-        let repoXML = FileParser.xml(repoFilePath.tildePath)
-        let repoList = XMLParser.toArray(repoXML)
-        Swift.print("repoList.count: " + "\(repoList.count)")
-        
-        GitSync.initCommit(repoList[0], "master")
-
-        
-        GitSync.initPush(repoList[0], "master")
-        
-        */
-        
-        
-        
     }
     func applicationWillTerminate(aNotification: NSNotification) {
-        // Insert code here to tear down your application
+        print("Good-bye")
     }
 }
 
+
+
+
+
 /*
-let appSupportPath = (NSFileManager.defaultManager().URLsForDirectory(.ApplicationSupportDirectory, inDomains: .UserDomainMask).first! as NSURL).path!
-Swift.print("appSupportPath: " + "\(appSupportPath)")
+let repoXML = FileParser.xml(repoFilePath.tildePath)
+let repoList = XMLParser.toArray(repoXML)
+Swift.print("repoList.count: " + "\(repoList.count)")
 
-let libraryPath = (NSFileManager.defaultManager().URLsForDirectory(.LibraryDirectory, inDomains: .UserDomainMask).first! as NSURL).path!
+GitSync.initCommit(repoList[0], "master")
 
-Swift.print("libraryPath: " + "\(libraryPath)")*/
+
+GitSync.initPush(repoList[0], "master")
+
+*/
+        
