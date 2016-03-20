100132212d953d5b3c1b009bc18b6d3cf9f7bca0
Eonist <unicornhive@gmail.com>
2016-03-20 14:50:44 +0100

    Files modified: 1
    Modified 1 file:src/window/view/ContentView.swift

--- src/window/view/ContentView.swift
+++ src/window/view/ContentView.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 class ContentView:Container{
-    
+    override func resolveSkin() {
+        StyleManager.addStyle("ContentView{float:left;clear:none;fill:orange;}")
+        super.resolveSkin()
+    }
 }
