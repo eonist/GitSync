74d15d760b4de959493cd7dd1322c765df99aede
Eonist <unicornhive@gmail.com>
2016-03-20 12:56:03 +0100

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -1,11 +1,12 @@
 import Foundation
 
 class MainView:CustomView{
-    var leftContainer:
+    var leftContainer:Container?
     var leftSideBar:LeftSideBar?
     override func resolveSkin() {
         StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
         super.resolveSkin()
-        leftSideBar = addSubView(LeftSideBar(LeftSideBar.w,height,self))
+        leftContainer = Container(0,0,self)
+        leftSideBar = leftContainer!.addSubView(LeftSideBar(LeftSideBar.w,height,self))
     }
 }
