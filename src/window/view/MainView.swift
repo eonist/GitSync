e492898a6fb50b7f3fce17a0b2230b950232bfcd
Eonist <unicornhive@gmail.com>
2016-03-20 12:48:05 +0100

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -4,5 +4,6 @@ class MainView:CustomView{
     override func resolveSkin() {
         StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
         super.resolveSkin()
+        leftSideBar = addSubView(LeftSideBar(LeftSideBar.w,height,self))
     }
 }
