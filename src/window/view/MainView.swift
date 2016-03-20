1fe50bc0e577e46832f29a3885044afc68bb32e8
Eonist <unicornhive@gmail.com>
2016-03-20 12:46:07 +0100

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -2,6 +2,7 @@ import Foundation
 
 class MainView:CustomView{
     override func resolveSkin() {
+        StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
         super.resolveSkin()
     }
 }
