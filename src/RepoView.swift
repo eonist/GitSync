29ca0d2f4104524494e8fac2c67819adb96eb27d
Eonist <unicornhive@gmail.com>
2016-03-20 14:46:44 +0100

    Files modified: 1
    Modified 1 file:src/RepoView.swift

--- src/RepoView.swift
+++ src/RepoView.swift
@@ -1,5 +1,26 @@
 import Foundation
 
-class RepoView {
-
+class RepoView:Element{
+    var topBar:TopBar?
+    override func resolveSkin() {
+        super.resolveSkin()
+        topBar = addSubView(TopBar(width,24,self))
+    }
+    //topBar with add and remove buttons, use Text buttons at first
+    //Load all the xml items
+    //contentView
+    //try to load a list with just the titles first
+    //Create RepoListItem
+    //Create RepoList
+    //Hock up the add and remove functionality
+    //adding a repo-item shows a InputModalView with the repo-URL,name,branch,etc
+    //removing a repo-item just removes the item from the List
+    //save to xml after each remove and add and each repo-settings-update
+}
+class TopBar:Element{
+    override func resolveSkin() {
+        StyleManager.addStyle("TopBar{fill:red;}")
+        super.resolveSkin()
+        //add buttons here
+    }
 }
