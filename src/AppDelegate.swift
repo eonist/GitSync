c7513174e4df153178fd0247badcb11bb28b7da7
Eonist <unicornhive@gmail.com>
2016-03-20 12:38:15 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -1,33 +1,46 @@
 import Cocoa
 
-//create the window
-//the window has a view
-//the view has leftMenuView and MainView
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    weak var window: NSWindow!/*<--This is here only so that the compiler wont throw an error*/
+    var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        StyleManager.addStylesByURL("~/Desktop/css/window.css")
+        win = TranslucencyWin()//Win(1000,800)
+        NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
+    }
+    func applicationWillTerminate(aNotification: NSNotification) {
+        print("Good-bye")
+    }
+}
 
+
+
+
+
+
+import Cocoa
+/**
+ * This is the main class for the application
+ * TODO: create the window
+ * TODO: the window has a view
+ * TODO: the view has leftMenuView and MainView
+ */
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!
     var repoFilePath:String = "~/Desktop/repo.xml"
-
+    var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
-        //Swift.print("hello world")
-        
-        
-        
-        
-        
-        //.print("applicationDidFinishLaunching")
-        /**/
-        var css = ""//E8E8E8
-        css += "Window{fill:#EFEFF4;corner-radius:4px;}"//<--you should target a bg element not the window it self, since now everything inherits these values
+        var css = "Window{fill:#EFEFF4;corner-radius:4px;}"//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
+        css += "}"
         //css += "Section{fill:#EFEFF4;corner-radius:4px 4px 0px 0px;}"
         //css += "Button{fill:green;}"
         css += ""
         StyleManager.addStyle(css)
         
         
-        window = GitSyncWin(300,300)/*Init the window*/
+        win = GitSyncWin(300,300)/*Init the window*/
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
         app.windows[0].close()/*close the initial non-optional default window*/
         
