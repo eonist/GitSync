98eecc6da584cbdb0d5851424412fcb577e52e7f
Eonist <unicornhive@gmail.com>
2016-03-21 06:33:17 +0100

    Files modified: 1
    Modified 1 file:src/window/view/RepoDetailView.swift

--- src/window/view/RepoDetailView.swift
+++ src/window/view/RepoDetailView.swift
@@ -3,6 +3,11 @@ import Foundation
 class RepoDetailView:Element{
     override func resolveSkin() {
         super.resolveSkin()
+        //Name: (TextInput)
+        //Local-path: (TextInput)
+        //Remote-path: (TextInput)
+        //Broadcast: (CheckBox Button)
+        //SubScribe: (CheckBox Button)
         
     }
 }
