57ae29ae60b32b5f5c4427d2be64b8d7436347c1
Eonist <unicornhive@gmail.com>
2016-03-21 06:44:57 +0100

    Files modified: 1
    Modified 1 file:src/window/view/RepoDetailView.swift

--- src/window/view/RepoDetailView.swift
+++ src/window/view/RepoDetailView.swift
@@ -1,14 +1,19 @@
 import Foundation
 
 class RepoDetailView:Element{
+    var nameTextInput:TextInput?
+    var localPathTextInput:TextInput?
+    var remotePathTextInput:TextInput?
     override func resolveSkin() {
         super.resolveSkin()
         //Name: (TextInput)
-        let textInput:TextInput = addSubView(TextInput(width, 32, "Name: ", "Test A", self))
-        textInput
+        nameTextInput = addSubView(TextInput(width, 32, "Name: ", "Test A", self))
         //Local-path: (TextInput)
+        localPathTextInput = addSubView(TextInput(width, 32, "Local-path: ", "/Desktop/", self))
         //Remote-path: (TextInput)
+        remotePathTextInput = addSubView(TextInput(width, 32, "Remote-path: ", "github.com", self))
         //Broadcast: (CheckBox Button)
+        remotePathTextInput = addSubView(TextInput(width, 32, "Remote-path: ", "github.com", self))
         //SubScribe: (CheckBox Button)
         //Auto-sync: (CheckBox Button)
         //Auto-sync-Interval: (LeverSlider)
