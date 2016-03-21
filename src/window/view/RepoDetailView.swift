63ea46f0fe4a32850eb400cd9f12cb7b4417d46d
Eonist <unicornhive@gmail.com>
2016-03-21 07:59:14 +0100

    Files modified: 1
    Modified 1 file:src/window/view/RepoDetailView.swift

--- src/window/view/RepoDetailView.swift
+++ src/window/view/RepoDetailView.swift
@@ -15,7 +15,7 @@ class RepoDetailView:Element{
         StyleManager.addStyle("RepoDetailView TextInput TextArea{drop-shadow:none;line-alpha:0;line-thickness:0px;}")
         StyleManager.addStyle("RepoDetailView CheckBoxButton{clear:left;}")
         StyleManager.addStyle("RepoDetailView CheckBoxButton CheckBox{float:right;}")
-        StyleManager.addStyle("RepoDetailView CheckBoxButton CheckBox{float:right;}")
+        StyleManager.addStyle("RepoDetailView CheckBoxButton{width:110px;}")
         
         
         let repoData = RepoData.sharedInstance
