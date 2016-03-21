418720189f0aa73e5ae8b703a3d52efb1c3b3d0e
Eonist <unicornhive@gmail.com>
2016-03-21 15:17:35 +0100

    New files added: 2, Files deleted: 2
    Added 2 files:src/window/view/repo/item/RepoDetailView.swiftsrc/window/view/repo/list/RepoView.swiftDeleted 2 files:src/RepoView.swiftsrc/window/view/RepoDetailView.swift

--- src/window/view/repo/list/RepoView.swift
+++ src/window/view/repo/list/RepoView.swift
@@ -43,26 +43,3 @@ class RepoView:Element{//rename to RepoListView
         }
     }
 }
-class TopBar:Element{
-    var addButton:TextButton?
-
-    override func resolveSkin() {
-        StyleManager.addStyle("TopBar{float:left;clear:left;corner-radius:0px 4px 0px 0px;padding-left:10px;}")
-        super.resolveSkin()
-        
-        StyleManager.addStyle("TopBar TextButton{width:50px;}")
-        addButton = addSubView(TextButton("Add",32,24,self))
-        
-        /*
-        StyleManager.addStyle("TopBar Button#add{float:left;clear:none;line:none;corner-radius:0px;line-thickness:0px;}")//fill:green;
-        StyleManager.addStyle("TopBar Button#add{fill:white,~/Desktop/gitsync/assets/svg/add.svg grey8;}")
-        StyleManager.addStyle("TopBar Button#add{width:24px,16px;height:24px,16px;margin:0px,4px;}")
-        
-        
-        addButton = addSubView(Button(24,24,self,"add"))
-        */
-
-       
-        
-    }
-}
