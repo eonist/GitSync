bd830b6c433989e4ffb2fcc297b98bf436e19d4e
Eonist <unicornhive@gmail.com>
2016-03-21 17:24:12 +0100

    Files modified: 2
    Modified 2 files:src/AppDelegate.swiftsrc/window/GitSyncWin.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -8,11 +8,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var repoFilePath:String = "~/Desktop/repo.xml"
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
+        
+        
+        Swift.print("hello world")
+        
+        
+        
+        /*StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
         StyleManager.addStylesByURL("~/Desktop/css/gitsync.css")
         win = GitSyncWin(300,400)/*Init the window*/
         let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
-        app.windows[0].close()/*close the initial non-optional default window*/
+        app.windows[0].close()/*close the initial non-optional default window*/*/
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         print("Good-bye")
