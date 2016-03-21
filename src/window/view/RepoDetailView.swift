e0662f5c9ed78fd1d0df487f3539b9c52b568fcc
Eonist <unicornhive@gmail.com>
2016-03-21 08:28:35 +0100

    Files modified: 1
    Modified 1 file:src/window/view/RepoDetailView.swift

--- src/window/view/RepoDetailView.swift
+++ src/window/view/RepoDetailView.swift
@@ -29,11 +29,11 @@ class RepoDetailView:Element{
         let repoData = RepoData.sharedInstance
         let repoItem = repoData.dp!.getItemAt(0)!
         
-        //Name: (TextInput)
+        
         nameTextInput = addSubView(TextInput(width, 32, "Name: ", repoItem["title"]!, self))
-        //Local-path: (TextInput)
+        
         localPathTextInput = addSubView(TextInput(width, 32, "Local-path: ", repoItem["local-path"]!, self))
-        //Remote-path: (TextInput)
+        
         remotePathTextInput = addSubView(TextInput(width, 32, "Remote-path: ", repoItem["remote-path"]!, self))
         //Broadcast: (CheckBox Button)
         broadCastCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Broadcast:", true, self))
