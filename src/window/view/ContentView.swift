b6b1b50e86055a14eb86cef8f52f330661728b7e
Eonist <unicornhive@gmail.com>
2016-03-21 06:41:09 +0100

    Files modified: 1
    Modified 1 file:src/window/view/ContentView.swift

--- src/window/view/ContentView.swift
+++ src/window/view/ContentView.swift
@@ -2,9 +2,12 @@ import Foundation
 
 class ContentView:Element{
     var repoView:RepoView?
+    var repoDetailView:RepoDetailView?
     override func resolveSkin() {
         StyleManager.addStyle("ContentView{float:left;clear:none;/*fill:orange;*/}")
         super.resolveSkin()
         //repoView = addSubView(RepoView(width,height,self))
+        
+        repoDetailView = addSubView(RepoDetailView(width,height,self))
     }
 }
