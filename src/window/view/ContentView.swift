e626fb481c25c6e27a998a7413bfb12a6a4381c1
Eonist <unicornhive@gmail.com>
2016-03-21 08:59:42 +0100

    Files modified: 1
    Modified 1 file:src/window/view/ContentView.swift

--- src/window/view/ContentView.swift
+++ src/window/view/ContentView.swift
@@ -2,18 +2,19 @@ import Foundation
 
 class ContentView:Element{
     var repoView:RepoView?
-    var repoDetailView:RepoDetailView?
+    var repoDetailView:RepoDetailView {return RepoDetailView(width,height,self)}
+    
     override func resolveSkin() {
         StyleManager.addStyle("ContentView{float:left;clear:none;/*fill:orange;*/}")
         super.resolveSkin()
         repoView = addSubView(RepoView(width,height,self))
         
-        //repoDetailView = addSubView(RepoDetailView(width,height,self))
+
     }
     override func onEvent(event: Event) {
         if(event.type == ListEvent.select){
             Swift.print("ContentView select")
-            self.removeFromSuperview()
+            repoView!.removeFromSuperview()
         }
     }
 }
