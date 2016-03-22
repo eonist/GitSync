de1ec74f55abf4800f30accab20170208f2d8268
Eonist <unicornhive@gmail.com>
2016-03-22 13:33:13 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -25,6 +25,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         NodeModifier.removeAll(root)
         Swift.print("root.children.count: " + "\(root.children.count)")
         
+        //continue implementing NodeMOdifiers
+        
         /*Swift.print("hello world")
         let result = AdvanceArrayParser.childAt([["red","green"],[["four","five"],[1,2,3]]],[1,0,1])//[five]
         Swift.print("result: " + "\(result)")*/
