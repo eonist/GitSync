2746ea94911f912b9f2994dac7cda35468f10977
Eonist <unicornhive@gmail.com>
2016-03-23 08:13:44 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -82,7 +82,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         nameDict["name"] = titleDict
         nameDict["color"] = "blue"
         
-        Swift.print("nameDict.keys[0]: " + "\(nameDict.keys)")
+        Swift.print("nameDict.keys[0]: " + "\(nameDict.keys.startIndex)")
 
         
         
