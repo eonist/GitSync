6cb34905910d192387f2253e16bcc2515bc8ff3f
Eonist <unicornhive@gmail.com>
2016-03-23 17:40:30 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -12,6 +12,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //Continue here: Then start doing node manipulation, removal, additions, updates, creation. aka crud
         //a benefit of not using Node and instead using XML is that you then can access things deply nested inside xml, with ease. Which i guess you can if you convert the xml to NOde aswell. But then its 3 conversions which may not be great.
         
+        //add attribute in a nested node
+        
+        //change the name of a nested node
+        
+        //remove a nested node
+        
+        //remove a branch
+        
+        //remove a deep leaf
+        
+        //add a branch to a leaf
+        
+        //sort a branch by name
+        
+        //sort a tree by name
+        
+        
+        
+        //sort by attribute
+        
+        
         /*StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
         StyleManager.addStylesByURL("~/Desktop/css/gitsync.css")
         win = GitSyncWin(300,400)/*Init the window*/
