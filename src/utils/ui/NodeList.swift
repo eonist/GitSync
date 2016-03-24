683b1e1d907bf71ba575814da59dc206d74c6183
Eonist <unicornhive@gmail.com>
2016-03-24 17:34:45 +0100

    Files modified: 1
    Modified 1 file:src/utils/ui/NodeList.swift

--- src/utils/ui/NodeList.swift
+++ src/utils/ui/NodeList.swift
@@ -1,8 +1,12 @@
 import Foundation
 
 class NodeList:List{
+    var node:Node
     init(_ width: CGFloat, _ height: CGFloat, _ itemHeight:CGFloat = CGFloat.NaN, _ node:Node? = nil, _ parent: IElement?, _ id: String? = "") {
-    
-        
+        self.node = node!
     }
+
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+        
+    
 }
