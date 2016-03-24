422dad1e4127943222d58b19a352ffcb32f877ae
Eonist <unicornhive@gmail.com>
2016-03-24 17:36:58 +0100

    Files modified: 1
    Modified 1 file:src/utils/ui/NodeList.swift

--- src/utils/ui/NodeList.swift
+++ src/utils/ui/NodeList.swift
@@ -4,9 +4,8 @@ class NodeList:List{
     var node:Node
     init(_ width: CGFloat, _ height: CGFloat, _ itemHeight:CGFloat = CGFloat.NaN, _ node:Node? = nil, _ parent: IElement?, _ id: String? = "") {
         self.node = node!
+        super.init(<#T##width: CGFloat##CGFloat#>, <#T##height: CGFloat##CGFloat#>, <#T##itemHeight: CGFloat##CGFloat#>, <#T##dataProvider: DataProvider?##DataProvider?#>, <#T##parent: IElement?##IElement?#>, <#T##id: String?##String?#>)
     }
-
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
-        
     
 }
