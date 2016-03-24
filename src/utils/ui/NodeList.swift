ba1e6a32bdde77fdd6797c635cf8f0ff96d5e1ec
Eonist <unicornhive@gmail.com>
2016-03-24 19:43:42 +0100

    Files modified: 1
    Modified 1 file:src/utils/ui/NodeList.swift

--- src/utils/ui/NodeList.swift
+++ src/utils/ui/NodeList.swift
@@ -3,12 +3,16 @@ import Foundation
 class NodeList:Element{
     var node:Node
     var itemHeight:CGFloat
+    var list:List?
     init(_ width: CGFloat, _ height: CGFloat, _ itemHeight:CGFloat = CGFloat.NaN, _ node:Node? = nil, _ parent: IElement?, _ id: String? = "") {
         self.node = node!
         self.itemHeight = itemHeight
         super.init(width, height, parent, id)
     }
-    
+    override func resolveSkin() {
+        super.resolveSkin()
+        list = List()
+    }
     //create a list here
     
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
