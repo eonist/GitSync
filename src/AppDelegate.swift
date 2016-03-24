9749e031d014f69d8d7ef05ec65cceb86b8e9806
Eonist <unicornhive@gmail.com>
2016-03-24 15:41:30 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -8,10 +8,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var repoFilePath:String = "~/Desktop/repo.xml"
     var win:NSWindow?/*<--The window must be a class variable, local variables doesnt work*/
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        
-        
-        
-        
+        /*
+        StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
+        StyleManager.addStylesByURL("~/Desktop/css/gitsync.css")
+        win = GitSyncWin(300,400)/*Init the window*/
+        let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
+        app.windows[0].close()/*close the initial non-optional default window*/
+        */
+    }
+    /**
+     *
+     */
+    func xmlTesting(){
+
         //TODO: remember to also test if you can grab the value inside nodes
         var xmlStr:String = ""
         xmlStr += "<subCategories>"
@@ -24,8 +33,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         xml
         
         Swift.print("xml: " + "\(xml)")
-        
-        
         
         //let result = xml.valueAt([0,0])
         //Swift.print("result: " + "\(result)")
@@ -63,7 +70,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         */
         
         /**/
-
+        
         //Swift.print(xml.children![0].childCount)//2
         
         /*
@@ -72,13 +79,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         */
         
         
-        
-        
-        /*StyleManager.addStyle("Window{fill:white;corner-radius:4px;}")//E8E8E8//<--you should target a bg element not the window it self, since now everything inherits these values
-        StyleManager.addStylesByURL("~/Desktop/css/gitsync.css")
-        win = GitSyncWin(300,400)/*Init the window*/
-        let app:NSApplication = aNotification.object as! NSApplication/*grab the app instance from the notification*/
-        app.windows[0].close()/*close the initial non-optional default window*/*/
+
     }
     func nodeTesting(){
         /*
