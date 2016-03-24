b9f4baa5f797a93b3b75a36290dd105fae58c4fb
Eonist <unicornhive@gmail.com>
2016-03-24 17:07:36 +0100

    Files modified: 1
    Modified 1 file:src/window/view/repo/list/TopBar.swift

--- src/window/view/repo/list/TopBar.swift
+++ src/window/view/repo/list/TopBar.swift
@@ -8,9 +8,10 @@ class TopBar:Element{
         StyleManager.addStyle("TopBar{float:left;clear:left;corner-radius:0px 4px 0px 0px;padding-left:10px;margin-bottom:12px;}")
         super.resolveSkin()
         
+        StyleManager.addStyle("TopBar TextButton#back{width:50px;float:left;clear:none;}")
         backButton = addSubView(TextButton("Back",32,24,self,"back"))
         
-        StyleManager.addStyle("TopBar TextButton{width:50px;float:right;clear:none;}")
+        StyleManager.addStyle("TopBar TextButton#add{width:50px;float:right;clear:none;}")
         addButton = addSubView(TextButton("Add",32,24,self,"add"))
         
         /*
