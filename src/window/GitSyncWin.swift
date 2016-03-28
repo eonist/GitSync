eaeb21b9dcbf24420ef1f61914598dd4ce7769c7
Eonist <unicornhive@gmail.com>
2016-03-28 17:06:02 +0200

    Files modified: 1
    Modified 1 file:src/window/GitSyncWin.swift

--- src/window/GitSyncWin.swift
+++ src/window/GitSyncWin.swift
@@ -2,8 +2,8 @@ import Foundation
 
 class GitSyncWin:Window {
     override func resolveSkin() {
-        Swift.print("frame.width: " + "\(frame.width)")
-        Swift.print("frame.height: " + "\(frame.height)")
+//        Swift.print("frame.width: " + "\(frame.width)")
+//        Swift.print("frame.height: " + "\(frame.height)")
         self.contentView = MainView(frame.width,frame.height)/*Sets the mainview of the window*/
     }
 }
