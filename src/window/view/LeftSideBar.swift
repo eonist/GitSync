bc1aeba5446fd015e6ec276ba2db708db5efb887
Eonist <unicornhive@gmail.com>
2016-03-28 19:26:45 +0200

    Files modified: 1
    Modified 1 file:src/window/view/LeftSideBar.swift

--- src/window/view/LeftSideBar.swift
+++ src/window/view/LeftSideBar.swift
@@ -13,7 +13,7 @@ class LeftSideBar:Element{
         let buttonSection = self.addSubView(Section(LeftSideBar.w,200,self,"buttonSection"))
         var buttons:Array<ISelectable> = []
         for buttonTitle in buttonTitles{
-            let selectTextButton:SelectTextButton = buttonSection.addSubView(SelectTextButton(80,20,buttonTitle,false,buttonSection,buttonTitle))
+            let selectTextButton:SelectTextButton = buttonSection.addSubView(SelectTextButton(80,20,buttonTitle.capitalizedString,false,buttonSection,buttonTitle))
             buttons.append(selectTextButton)
             //buttons.append(buttonSection.addSubView(SelectButton(20,20,false,buttonSection,buttonTitle)))
         }
