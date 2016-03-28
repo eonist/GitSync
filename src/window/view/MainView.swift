b3b58d41f6710362ff4c02c6d47b1812b0fc1966
Eonist <unicornhive@gmail.com>
2016-03-28 17:01:48 +0200

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -7,7 +7,7 @@ class MainView:CustomView{
     override func resolveSkin() {
         super.resolveSkin()
         StyleManager.addStyle("Section#leftSection{fill:grey2;corner-radius:4px 0px 4px 0px;float:left;clear:none;}")
-        leftSection = addSubView(Section(LeftSideBar.w,500,self,"leftSection"))
+        leftSection = addSubView(Section(LeftSideBar.w,600,self,"leftSection"))
         createCustomTitleBar()
         leftSideBar = leftSection!.addSubView(LeftSideBar(LeftSideBar.w,height,leftSection))
         contentView = addSubView(ContentView(width-LeftSideBar.w,height,self))
@@ -19,7 +19,7 @@ class MainView:CustomView{
         StyleManager.addStylesByURL("~/Desktop/css/titleBar.css")
         StyleManager.addStyle("Section#titleBar{padding-top:16px;padding-left:12px;}")
         
-        section = leftSection!.addSubView(Section(75,16,leftSection,"titleBar"))
+        section = leftSection!.addSubView(Section(LeftSideBar.w,16,leftSection,"titleBar"))
         closeButton = section!.addSubView(Button(0,0,section!,"close"))/*<--TODO: the w and h should be NaN, test if it supports this*/
         minimizeButton = section!.addSubView(Button(0,0,section!,"minimize"))
         maximizeButton = section!.addSubView(Button(0,0,section!,"maximize"))
