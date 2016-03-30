5f9520414168ce564bb2eba58877ea38b4ab60ce
Eonist <unicornhive@gmail.com>
2016-03-30 15:47:42 +0200

    Files modified: 1
    Modified 1 file:src/window/view/TranslucencyView.swift

--- src/window/view/TranslucencyView.swift
+++ src/window/view/TranslucencyView.swift
@@ -17,7 +17,6 @@ class TranslucencyView:NSVisualEffectView{
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
-
 extension TranslucencyView{
     func maskImage(cornerRadius cornerRadius: CGFloat) -> NSImage {
         let edgeLength = 2.0 * cornerRadius + 1.0
