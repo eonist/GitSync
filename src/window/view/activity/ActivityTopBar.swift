abc11e1d21c926383876700a164b7ee5c4cc76ea
Eonist <unicornhive@gmail.com>
2016-03-30 09:10:29 +0200

    Files modified: 1
    Modified 1 file:src/window/view/activity/ActivityTopBar.swift

--- src/window/view/activity/ActivityTopBar.swift
+++ src/window/view/activity/ActivityTopBar.swift
@@ -4,5 +4,6 @@ class ActivityTopBar:Element{
     override func resolveSkin() {
         StyleManager.addStyle("ActivityTopBar{float:left;clear:left;corner-radius:0px 4px 0px 0px;margin-bottom:12px;}")
         super.resolveSkin()
+        
     }
 }
