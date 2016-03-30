ca9267967b14c57436603bec4ad0dfcab5a3562c
Eonist <unicornhive@gmail.com>
2016-03-30 08:44:01 +0200

    Files modified: 1
    Modified 1 file:src/window/view/activity/list/ActivityListItem.swift

--- src/window/view/activity/list/ActivityListItem.swift
+++ src/window/view/activity/list/ActivityListItem.swift
@@ -25,11 +25,11 @@ class ActivityListItem:Button,ISelectable{
         contributorText.isInteractive = false
         let titleText:Text = container.addSubView(Text(180,24,title,container,"title"))
         titleText.isInteractive = false
-        /*let descText:Text = container.addSubView(Text(180,24,desc,container,"description"))
+        let descText:Text = container.addSubView(Text(180,50,desc,container,"description"))
         descText.isInteractive = false
         let dateText:Text = container.addSubView(Text(180,24,date,container,"date"))
         dateText.isInteractive = false
-        */
+        
     }
     override func mouseUpInside(event: MouseEvent) {
         isSelected = true
