a0ff045cd28c20c7f3c742f7ae21ca255bfcce3e
Eonist <unicornhive@gmail.com>
2016-03-30 09:23:30 +0200

    Files modified: 1
    Modified 1 file:src/window/view/activity/ActivityTopBar.swift

--- src/window/view/activity/ActivityTopBar.swift
+++ src/window/view/activity/ActivityTopBar.swift
@@ -2,7 +2,7 @@ import Foundation
 
 class ActivityTopBar:Element{
     override func resolveSkin() {
-        StyleManager.addStyle("ActivityTopBar{float:left;clear:left;corner-radius:0px 4px 0px 0px;margin-bottom:12px;}")
+        StyleManager.addStyle("ActivityTopBar{float:left;clear:left;corner-radius:0px 4px 0px 0px;margin-bottom:12px;padding-left:10px;}")
         super.resolveSkin()
         let container = addSubView(Container(120,120,self,"searchBoxCard"))
         let searchBox:TextArea = container.addSubView(TextArea(NaN, NaN, "Search", container))
