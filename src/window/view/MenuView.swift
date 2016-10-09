5185fa8e6040cb0d093e2d5df3bef63ad97d6093
Eonist <unicornhive@gmail.com>
2016-10-09 17:41:07 +0200

    Files modified: 1
    Modified 1 file:src/window/view/MenuView.swift

--- src/window/view/MenuView.swift
+++ src/window/view/MenuView.swift
@@ -1,5 +1,32 @@
 import Foundation
 
-class MenuView {
-
+class MenuView:Element{
+    static let commits:String = "commits"
+    static let repos:String = "repos"
+    static let stats:String = "stats"
+    static let prefs:String = "prefs"
+    static let buttonTitles = [MenuView.commits,MenuView.repos,MenuView.stats,MenuView.prefs]
+    override func resolveSkin() {
+        Swift.print("MenuView.resolveSkin()")
+        super.resolveSkin()
+        createButtons()
+    }
+    func createButtons(){
+        let buttonSection = self.addSubView(Section(200,40,self,"buttonSection"))
+        var buttons:Array<ISelectable> = []
+        for buttonTitle in MenuView.buttonTitles{
+            let selectTextButton:SelectTextButton = buttonSection.addSubView(SelectTextButton(80,20,buttonTitle.capitalizedString,false,buttonSection,buttonTitle))
+            buttons.append(selectTextButton)
+            //buttons.append(buttonSection.addSubView(SelectButton(20,20,false,buttonSection,buttonTitle)))
+        }
+        let selectGroup = SelectGroup(buttons,buttons[0])
+        buttons[0].setSelected(true)
+        func onSelect(event:Event){
+            //do something here
+            let buttonId:String = ((event as! SelectGroupEvent).selectable as! SelectTextButton).id!
+            Swift.print("MainMenu.onSelect() buttonId: " + "\(buttonId)")
+            //Navigation.sharedInstance.setView(buttonId)
+        }
+        selectGroup.event = onSelect
+    }
 }
