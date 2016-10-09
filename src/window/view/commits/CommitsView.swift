d667404eb76d11606db81e40131f1e987aadcf5d
Eonist <unicornhive@gmail.com>
2016-10-09 20:32:59 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -1,7 +1,6 @@
 import Foundation
 
 class CommitsView:Element {
-
     //var list:ActivityList?
     override func resolveSkin() {
         //StyleManager.addStyle("ActivityView{fill:white;fill-alpha:1;float:left;clear:none;corner-radius:0px 8px 0px 8px;}")/**/
@@ -18,7 +17,7 @@ class CommitsView:Element {
         StyleManager.addStyle("ActivityView List Container SelectTextButton Text{margin-top:8px;}")
         StyleManager.addStyle("ActivityView List{drop-shadow:none;}")
         */
-        let xml = FileParser.xml("~/Desktop/repo.xml")
+        let xml = FileParser.xml("~/Desktop/repo.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
         Swift.print("dp.count(): " + "\(dp.count())")
         //list = addSubView(ActivityList(width, height, NaN, dp, self,"activityList"))
