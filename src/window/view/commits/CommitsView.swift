66152975c3bc0c8482ccde78a6e97bb6f410c784
Eonist <unicornhive@gmail.com>
2016-10-09 20:46:32 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -3,20 +3,12 @@ import Foundation
 class CommitsView:Element {
     var list:CommitsList?
     override func resolveSkin() {
-        //StyleManager.addStyle("ActivityView{fill:white;fill-alpha:1;float:left;clear:none;corner-radius:0px 8px 0px 8px;}")/**/
-        //StyleManager.addStyle("ActivityView{padding-top:0px;}")//padding-left:6px;padding-right:6px;
         super.resolveSkin()
-        
         createList()
     }
     func createList(){
         //let dp:DataProvider = DataProvider()
         //dp.addItems([["title":"brown"],["title":"pink"],["title":"purple"]])
-        /*
-        StyleManager.addStyle("ActivityView List Container SelectTextButton{height:32px;}")
-        StyleManager.addStyle("ActivityView List Container SelectTextButton Text{margin-top:8px;}")
-        StyleManager.addStyle("ActivityView List{drop-shadow:none;}")
-        */
         let xml = FileParser.xml("~/Desktop/repo.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
         Swift.print("dp.count(): " + "\(dp.count())")
