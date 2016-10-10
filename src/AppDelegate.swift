cef31e85510306edb9e8305b0b877eb9a66d3685
Eonist <unicornhive@gmail.com>
2016-10-10 20:28:38 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -8,13 +8,27 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     weak var window: NSWindow!
     var repoFilePath:String = "~/Desktop/repo.xml"
     var win:NSWindow?/*<--The window must be a class variable, local variables doesn't work*/
-    
+    var fileWatcher:FileWatcher?
     func applicationDidFinishLaunching(aNotification: NSNotification) {
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
         
         Swift.print("GitSync - A really simple Git app")
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
         
+        
+        let url:String = "~/Desktop/ElCapitan/gitsync.css"
+        fileWatcher = FileWatcher([url.tildePath])
+        fileWatcher!.event = { event in
+            //Swift.print(self)
+            Swift.print(event.description)
+            if(event.fileChange && event.path == url.tildePath) {
+                Swift.print("update to the file happened")
+                StyleManager.addStylesByURL(url,true)
+                ElementModifier.refreshSkin(win as! Window)
+                ElementModifier.floatChildren(win as! Window)
+            }
+        }
+        fileWatcher!.start()
 
         win = MainWin(320,480)
         
