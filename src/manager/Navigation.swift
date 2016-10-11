b63a212dd5aac226c91ea6286e7fb11bfcdfca8f
Eonist <unicornhive@gmail.com>
2016-10-11 12:57:21 +0200

    Files modified: 1
    Modified 1 file:src/manager/Navigation.swift

--- src/manager/Navigation.swift
+++ src/manager/Navigation.swift
@@ -1,5 +1,31 @@
 import Foundation
 
+/**
+ * TODO: Migrate to its own .swift file when appropriate
+ */
 class Navigation {
-
+    /**
+     * Navigate between views
+     */
+    static func setView(viewName:String){
+        Swift.print("Navigation.setView() viewName: " + "\(viewName)")
+        let mainView:MainView = MainWin.mainView!
+        if(mainView.currentView != nil) {mainView.currentView!.removeFromSuperview()}
+        
+        let width:CGFloat = CommitsView.w
+        let height:CGFloat = CommitsView.h
+        
+        switch viewName{
+        case MenuView.commits:
+            mainView.currentView = mainView.addSubView(CommitsView(width,height,mainView))
+        case MenuView.repos:
+            mainView.currentView = mainView.addSubView(RepoView(width,height,mainView))
+        case MenuView.stats:
+            Swift.print("stats")
+        case MenuView.prefs:
+            Swift.print("prefs")
+        default:
+            break;
+        }
+    }
 }
