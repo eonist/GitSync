a66927f37b2962311fd4be59a389e9b44ecf340f
Eonist <unicornhive@gmail.com>
2016-10-11 15:01:43 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -1,8 +1,11 @@
 import Foundation
 
 class RepoView:Element {
+    var treeList:TreeList?
     override func resolveSkin() {
         Swift.print("RepoView.resolveSkin()")
         super.resolveSkin()
+        let xml:NSXMLElement = FileParser.xml("~/Desktop/repo2.xml")
+        treeList = addSubView(TreeList(width, height-24, NaN, Node(xml), self))
     }
 }
