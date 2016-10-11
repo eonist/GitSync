0b4e0dea2c3f26484731d586d1aea59a420c2545
Eonist <unicornhive@gmail.com>
2016-10-11 16:49:06 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -51,7 +51,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             //Setup Master view for RepoView
             //Setup Detail view for RepoView
             //Setup TopBar with Add/Remove/Edit Buttons
-        //7. Setup a TopBar that goes under the Titlebar with Add,Remove,Edit,move etc
+        //7. Setup a TopBar that goes under the Titlebar with Add,Remove,Edit,Cut,Paste
     }
     func applicationWillTerminate(aNotification: NSNotification) {
         print("Good-bye")
