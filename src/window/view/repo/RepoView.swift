15216ec737c3c20449151850e74b7bb6de40e4ae
Eonist <unicornhive@gmail.com>
2016-10-11 15:23:07 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -7,5 +7,13 @@ class RepoView:Element {
         super.resolveSkin()
         let xml:NSXMLElement = FileParser.xml("~/Desktop/repo2.xml".tildePath)
         treeList = addSubView(TreeList(width, height-24, NaN, Node(xml), self))
+        
+        let someNaN:Any?
+        someNaN = Int.NaN
+        someNaN = UInt.NaN
+        someNaN = CGFloat.NaN
+        someNaN = Double.NaN
+        someNaN = Float.NaN
+        
     }
 }
