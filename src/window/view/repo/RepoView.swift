742a2504162008c90640f32e72c5a2b49eeb9dc6
Eonist <unicornhive@gmail.com>
2016-10-11 15:15:24 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -4,7 +4,6 @@ class RepoView:Element {
     var treeList:TreeList?
     override func resolveSkin() {
         Swift.print("RepoView.resolveSkin()")
-        //StyleManager.addStyle("RepoView{padding-top:8px;}")//padding-left:6px;padding-right:6px;
         super.resolveSkin()
         let xml:NSXMLElement = FileParser.xml("~/Desktop/repo2.xml".tildePath)
         treeList = addSubView(TreeList(width, height-24, NaN, Node(xml), self))
