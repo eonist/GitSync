49ed3e839602d0cc67eaa43c32fbcb4b8acd3a9f
Eonist <unicornhive@gmail.com>
2016-10-11 18:39:55 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -21,11 +21,15 @@ class RepoView:Element {
 class TopBar:Element{
     var removeButton:TextButton?
     var addButton:TextButton?
+    var copyButton:TextButton?
+    var pasteButton:TextButton?
     override func resolveSkin() {
         Swift.print("TopBar.resolveSkin()")
         super.resolveSkin()
         addButton = addSubView(TextButton(NaN,24,"Add",self,"add"))
         removeButton = addSubView(TextButton(NaN,24,"Remove",self,"remove"))
+        copyButton = addSubView(TextButton(NaN,24,"Copy",self,"copy"))
+        pasteButton = addSubView(TextButton(NaN,24,"Paste",self,"paste"))
         
         /*
         StyleManager.addStyle("TopBar Button#add{float:left;clear:none;line:none;corner-radius:0px;line-thickness:0px;}")//fill:green;
