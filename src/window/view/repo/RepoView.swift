916a2d22a7a19ca697a42951dcd2858d3d6fc0a6
Eonist <unicornhive@gmail.com>
2016-10-11 18:19:49 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -13,32 +13,23 @@ class RepoView:Element {
     }
 }
 
-//Add,Remove,Edit,Cut,Paste
-
-
-class TopBar:Element{
+/**
+ * Add,Remove,Edit,Cut,Paste
+ */
+private class TopBar:Element{
     var removeButton:TextButton?
     var addButton:TextButton?
-    
     override func resolveSkin() {
-        
         super.resolveSkin()
-        
-        addButton = addSubView(TextButton("Add",NaN,24,self,"add"))
-        
-        
-        removeButton = addSubView(TextButton("Remove",NaN,24,self,"remove"))
+        addButton = addSubView(TextButton(NaN,24,"Add",self,"add"))
+        removeButton = addSubView(TextButton(NaN,24,"Remove",self,"remove"))
         
         /*
         StyleManager.addStyle("TopBar Button#add{float:left;clear:none;line:none;corner-radius:0px;line-thickness:0px;}")//fill:green;
         StyleManager.addStyle("TopBar Button#add{fill:white,~/Desktop/gitsync/assets/svg/add.svg grey8;}")
         StyleManager.addStyle("TopBar Button#add{width:24px,16px;height:24px,16px;margin:0px,4px;}")
-        
-        
         addButton = addSubView(Button(24,24,self,"add"))
         */
-        
-        
         
     }
 }
