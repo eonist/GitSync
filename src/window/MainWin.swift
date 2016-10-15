aecbe11c1ebffdd1413188109d325df6cd881895
Eonist <unicornhive@gmail.com>
2016-10-15 17:00:42 +0200

    Files modified: 1
    Modified 1 file:src/window/MainWin.swift

--- src/window/MainWin.swift
+++ src/window/MainWin.swift
@@ -2,14 +2,18 @@ import Foundation
 
 class MainWin:Window {
     static var mainView:MainView?
+    var visualEffectView:TranslucencyView?//we set the to the background 
+    
     required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
         super.init(docWidth, docHeight)
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
+        self.contentView = FlippedView(frame: NSRect(0,0,frame.width,frame.height))
+        visualEffectView = TranslucencyView(frame: NSRect(0,0,frame.width,frame.height))
     }
     override func resolveSkin() {
         super.resolveSkin()
-        self.contentView = MainView(frame.width,frame.height,"GitSync")/*Sets the mainview of the window*/
-        MainWin.mainView = self.contentView as? MainView
+        MainWin.mainView = MainView(frame.width,frame.height,"GitSync")/*Sets the mainview of the window*/
+        self.contentView?.addSubView(MainWin.mainView)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
