d988241aa0f6cd03b837780ee22b18a6125b5354
Eonist <unicornhive@gmail.com>
2016-10-15 18:21:23 +0200

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -13,12 +13,15 @@ class MainView:TitleView{
     }
     override func resolveSkin() {
         Swift.print("MainView.resolveSkin()")
-        super.resolveSkin()
-        super.textArea!.setTextValue(title)
         MainWin.mainView = self
         Navigation.setView(MenuView.repos)
         menuView = addSubView(MenuView(240,48,self))
         Align.align(menuView!, CGSize(width/**/,height/**/), Alignment.bottomCenter, Alignment.bottomCenter,CGPoint(0,0))
+        
+        super.resolveSkin()
+        super.textArea!.setTextValue(title)
+        
+        
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
