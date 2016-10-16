8706768d32c9a2db01903d4f6826e7d22c186303
Eonist <unicornhive@gmail.com>
2016-10-16 21:33:25 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/detail/RepoDetailView.swift

--- src/window/view/repo/detail/RepoDetailView.swift
+++ src/window/view/repo/detail/RepoDetailView.swift
@@ -17,14 +17,10 @@ class RepoDetailView:Element {
         localPathTextInput = addSubView(TextInput(width, 32, "Local-path: ", "", self))
         remotePathTextInput = addSubView(TextInput(width, 32, "Remote-path: ", "", self))
         branchTextInput = addSubView(TextInput(width, 32, "Branch: ", "", self))//branch-text-input: master is default, set to dev for instance
-        //to disable an item uncheck broadcast and subscribe
-        broadCastCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Broadcast:", false, self))
+        broadCastCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Broadcast:", false, self))//to disable an item uncheck broadcast and subscribe
         subscribeCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Subscribe:", false, self))
-        //if auto sync is off then a manual commit popup dialog will appear (with pre-populated text)
-        autoSyncCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Auto-sync:", false, self))
-        //autoSyncIntervall needs to be a time setter: Day,Hour,Min,Seconds
-        //0 means do not sync on an interval
-        autoSyncIntervalLeverSpinner = addSubView(LeverSpinner(width, 32, "Auto-Interval: ", 0, 1, Int.min.cgFloat, Int.max.cgFloat, 0, 100, 200, self))
+        autoSyncCheckBoxButton = addSubView(CheckBoxButton(width, 32, "Auto-sync:", false, self))//if auto sync is off then a manual commit popup dialog will appear (with pre-populated text)
+        autoSyncIntervalLeverSpinner = addSubView(LeverSpinner(width, 32, "Auto-Interval: ", 0, 1, Int.min.cgFloat, Int.max.cgFloat, 0, 100, 200, self))//autoSyncIntervall needs to be a time setter: Day,Hour,Min,Seconds,0 means do not sync on an interval
     }
     /**
      *
@@ -33,6 +29,7 @@ class RepoDetailView:Element {
         nameTextInput!.inputTextArea!.setTextValue(repoData["title"]!)
         localPathTextInput!.inputTextArea!.setTextValue(repoData["local-path"]!)
         remotePathTextInput!.inputTextArea!.setTextValue(repoData["remote-path"]!)
+        branchTextInput!.inputTextArea!.setTextValue(repoData["branch"]!)
         broadCastCheckBoxButton!.setChecked(repoData["broadcast"]!.bool)
         subscribeCheckBoxButton!.setChecked(repoData["subscribe"]!.bool)
         autoSyncCheckBoxButton!.setChecked(repoData["auto-sync"]!.bool)
