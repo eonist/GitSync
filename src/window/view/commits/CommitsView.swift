598b617cacdb6e358ec0fd9cd3746c81436d01a8
Eonist <unicornhive@gmail.com>
2016-10-17 18:47:45 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -15,7 +15,7 @@ class CommitsView:Element {
         //dp.addItems([["title":"brown"],["title":"pink"],["title":"purple"]])
         let xml = FileParser.xml("~/Desktop/repo.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
-        Swift.print("dp.count(): " + "\(dp.count())")
+        Swift.print("dp.count(): " + "\(dp.count)")
         list = addSubView(CommitsList(320, height, NaN, dp, self,"commitsList"))
         ListModifier.selectAt(list!, 2)
     }
