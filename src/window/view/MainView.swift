2622ddefa2ac1f0bb92b785ed70b820beb1e70f7
Eonist <unicornhive@gmail.com>
2016-10-17 14:10:31 +0200

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -6,7 +6,7 @@ class MainView:TitleView{
     var title:String/*the title must be set after the init of the Window instance*/
     //var menuView:MenuView?
     var currentView:Element?
-    var mainTopBar:MainTopBar?
+    
     init(_ width: CGFloat, _ height: CGFloat,_ title:String = "", _ parent: IElement? = nil, _ id: String? = "") {
         self.title = title
         super.init(width, height, parent, "doc")
@@ -16,19 +16,10 @@ class MainView:TitleView{
         super.resolveSkin()
         super.textArea!.setTextValue(title)
         MainWin.mainView = self
-        //Navigation.setView(MenuView.repos)
-        mainTopBar = addSubView(MainTopBar(width-24,36,self))
+        Navigation.setView(MenuView.commits)
+        
         //menuView = addSubView(MenuView(240,48,self))
         //Align.align(menuView!, CGSize(width/**/,height/**/), Alignment.bottomCenter, Alignment.bottomCenter,CGPoint(0,0))
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
-}
-class MainTopBar:Element{
-    var reposButton:Button?
-    var prefsButton:Button?
-    override func resolveSkin() {
-        self.skin = SkinResolver.skin(self)//super.resolveSkin()
-        reposButton = addSubView(Button(16,16,self,"repos"))
-        prefsButton = addSubView(Button(16,16,self,"prefs"))
-    }
 }
