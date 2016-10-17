fda9226cb90166b55d1ce0dde45d45ff5f7070bd
Eonist <unicornhive@gmail.com>
2016-10-17 18:16:20 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -5,6 +5,7 @@ import Foundation
 class RepoView:Element {
     var topBar:TopBar?
     var list:List?
+    static var selecteListItemIndex:Int = 0
     override func resolveSkin() {
         Swift.print("RepoView.resolveSkin()")
         
@@ -14,7 +15,7 @@ class RepoView:Element {
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)//~/Desktop/repo2.xml
         let dp:DataProvider = DataProvider(xml)
         list = addSubView(List(width, height-24, NaN, dp,self))
-        list!.selectAt(0)
+        list!.selectAt(RepoView.selecteListItemIndex)
     }
     func onAddButtonClick(){
         Swift.print("addButton.click")
@@ -31,7 +32,8 @@ class RepoView:Element {
     }
     func onListSelect(){
         Navigation.setView(String(RepoDetailView))
-        let repoItem:Dictionary<String,String> = list!.dataProvider.getItemAt(list!.selectedIndex)!
+        RepoView.selecteListItemIndex = list!.selectedIndex
+        let repoItem:Dictionary<String,String> = list!.dataProvider.getItemAt(RepoView.selecteListItemIndex)!
         (Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
     }
     override func onEvent(event:Event) {
