26cc6c3e1c2535f97619d9c5b849091874d51c96
Eonist <unicornhive@gmail.com>
2016-10-17 15:39:57 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -25,20 +25,24 @@ class RepoView:Element {
         (Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
         //list!.onEvent(ListEvent(ListEvent.select,0,list!))
     }
-    func onRemoveButtonClick(){
+    func onBackButtonClick(){
         Swift.print("onRemoveButton()")
     }
-    func onEditButtonClick(){
+    /*func onRemoveButtonClick(){
+        Swift.print("onRemoveButton()")
+    }*/
+    /*func onEditButtonClick(){
         Swift.print("onEditButton()")
         Navigation.setView(String(RepoDetailView))
         
         let repoItem:Dictionary<String,String> = list!.dataProvider.getItemAt(list!.selectedIndex)!
         (Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
-    }
+    }*/
     override func onEvent(event:Event) {
         if(event.type == ButtonEvent.upInside && event.origin === topBar!.addButton){onAddButtonClick()}
-        else if(event.type == ButtonEvent.upInside && event.origin === topBar!.removeButton){onRemoveButtonClick()}
-        else if(event.type == ButtonEvent.upInside && event.origin === topBar!.editButton){onEditButtonClick()}
+        else if(event.type == ButtonEvent.upInside && event.origin === topBar!.backButton){onBackButtonClick()}
+        //else if(event.type == ButtonEvent.upInside && event.origin === topBar!.removeButton){onRemoveButtonClick()}
+        //else if(event.type == ButtonEvent.upInside && event.origin === topBar!.editButton){onEditButtonClick()}
         //else if(event.type == ListEvent.select){super.onEvent(event)}//forward this event to the parent
         //else if(event.type == SelectEvent.select && event.immediate === list){super.onEvent(event)}//forward this event to the parent
         
@@ -49,14 +53,16 @@ class RepoView:Element {
  * Add,Remove,Edit,Cut,Paste
  */
 class TopBar:Element{
-    var editButton:Button?
-    var removeButton:Button?
+    var backButton:Button?
+    //var editButton:Button?
+    //var removeButton:Button?
     var addButton:Button?
     override func resolveSkin() {
         Swift.print("TopBar.resolveSkin()")
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
-        editButton = addSubView(Button(NaN,NaN,self,"edit"))
-        removeButton = addSubView(Button(NaN,NaN,self,"remove"))
+        //editButton = addSubView(Button(NaN,NaN,self,"edit"))
+        //removeButton = addSubView(Button(NaN,NaN,self,"remove"))
+        backButton = addSubView(Button(NaN,NaN,self,"back"))
         addButton = addSubView(Button(NaN,NaN,self,"add"))
     }
 }
