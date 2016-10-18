cedcbf96518c6ed345efbc8aefdeb6601b162a64
Eonist <unicornhive@gmail.com>
2016-10-18 13:20:32 +0200

    Files modified: 1
    Modified 1 file:src/window/view/prefs/PrefsView.swift

--- src/window/view/prefs/PrefsView.swift
+++ src/window/view/prefs/PrefsView.swift
@@ -1,9 +1,11 @@
 import Foundation
 
 class PrefsView:Element {
+    var keychainUserName:TextInput?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)
         //keychain-user-name (TextInput)
+        keychainUserName = addSubView(TextInput(width, 32, "Name: ", "", self))
         //Git-Config-UserName
         //Git-Config-EmailName
         //UI sounds [x]
