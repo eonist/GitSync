e5bb295e0eaeceb9fe2e15ac365c424982ba1fdc
Eonist <unicornhive@gmail.com>
2016-10-18 20:33:00 +0200

    Files modified: 1
    Modified 1 file:src/window/ConflictDialogWin.swift

--- src/window/ConflictDialogWin.swift
+++ src/window/ConflictDialogWin.swift
@@ -1,5 +1,36 @@
 import Foundation
 
-class ConflictDialogWin {
-
+class ConflictDialogWin:Window {
+    required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
+        super.init(docWidth, docHeight)
+        WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
+    }
+    override func resolveSkin() {
+        MainWin.mainView = MainView(frame.width,frame.height,"GitSync")/*Sets the mainview of the window*/
+        self.contentView = MainWin.mainView!
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+class ConflictDialogView:TitleView{
+    static let w:CGFloat = 220
+    static let h:CGFloat = 380
+    var title:String/*the title must be set after the init of the Window instance*/
+    var menuView:MenuView?
+    var currentView:Element?
+    
+    init(_ width: CGFloat, _ height: CGFloat,_ title:String = "", _ parent: IElement? = nil, _ id: String? = "") {
+        self.title = title
+        super.init(width, height, parent, "doc")
+    }
+    override func resolveSkin() {
+        Swift.print("MainView.resolveSkin()")
+        super.resolveSkin()
+        super.textArea!.setTextValue(title)
+        MainWin.mainView = self
+        Navigation.setView(MenuView.repos)
+        
+        menuView = addSubView(MenuView(240,48,self))
+        Align.align(menuView!, CGSize(width/**/,height/**/), Alignment.bottomCenter, Alignment.bottomCenter,CGPoint(0,0))
+    }
+    required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
