6252695819a86617ad72f32278cc666b714b78c0
Eonist <unicornhive@gmail.com>
2016-10-18 14:05:54 +0200

    Files modified: 1
    Modified 1 file:src/window/view/prefs/PrefsView.swift

--- src/window/view/prefs/PrefsView.swift
+++ src/window/view/prefs/PrefsView.swift
@@ -3,6 +3,10 @@ import Foundation
  * NOTE: this must be a view, if you wan't to be consistent between macOS and iOS
  */
 class PrefsView:Element {
+    static var keychainUserName:String = "John"
+    static var gitConfigUserName:String = "John"
+    static var gitEmailNameText:String = "user@hotmail.com"
+    static var uiSoundsCheck:Bool = true
     var keychainUserNameTextinput:TextInput?
     var gitConfigUserNameTextinput:TextInput?
     var gitEmailNameTextinput:TextInput?
@@ -10,17 +14,17 @@ class PrefsView:Element {
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)
         //keychain-user-name (TextInput)
-        keychainUserNameTextinput = addSubView(TextInput(width, 32, "keychain user: ", "", self))
+        keychainUserNameTextinput = addSubView(TextInput(width, 32, "keychain user: ", PrefsView.keychainUserName, self))
         //Git-Config-UserName
-        gitConfigUserNameTextinput = addSubView(TextInput(width, 32, "Git Config User: ", "", self))
+        gitConfigUserNameTextinput = addSubView(TextInput(width, 32, "Git Config User: ", PrefsView.gitConfigUserName, self))
         //Git-Config-EmailName
-        gitEmailNameTextinput = addSubView(TextInput(width, 32, "Git Config Email: ", "", self))
+        gitEmailNameTextinput = addSubView(TextInput(width, 32, "Git Config Email: ", PrefsView.gitEmailNameText, self))
         //UI sounds [x]
-        uiSoundsCheckBoxButton = addSubView(CheckBoxButton(width, 32, "UI sounds:", true, self))
+        uiSoundsCheckBoxButton = addSubView(CheckBoxButton(width, 32, "UI sounds:", PrefsView.uiSoundsCheck, self))
     }
     override func onEvent(event: Event) {
         if(event.assert(Event.update, keychainUserNameTextinput)){
-            
+            PrefsView.keychainUserName = (event as! TextFieldEvent).stringValue
         }
     }
 }
