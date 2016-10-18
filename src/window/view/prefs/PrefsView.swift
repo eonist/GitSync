300e9ecff80726fa209a414c409d9b4396c04ce1
Eonist <unicornhive@gmail.com>
2016-10-18 13:24:25 +0200

    Files modified: 2
    Modified 2 files:src/window/view/menu/MenuView.swiftsrc/window/view/prefs/PrefsView.swift

--- src/window/view/prefs/PrefsView.swift
+++ src/window/view/prefs/PrefsView.swift
@@ -2,12 +2,16 @@ import Foundation
 
 class PrefsView:Element {
     var keychainUserName:TextInput?
+    var gitConfigUserName:TextInput?
+    var gitEmailName:TextInput?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)
         //keychain-user-name (TextInput)
-        keychainUserName = addSubView(TextInput(width, 32, "Name: ", "", self))
+        keychainUserName = addSubView(TextInput(width, 32, "keychain user name: ", "", self))
         //Git-Config-UserName
+        gitConfigUserName = addSubView(TextInput(width, 32, "Git Config UserName: ", "", self))
         //Git-Config-EmailName
+        gitEmailName = addSubView(TextInput(width, 32, "Git Config EmailName: ", "", self))
         //UI sounds [x]
     }
 }
