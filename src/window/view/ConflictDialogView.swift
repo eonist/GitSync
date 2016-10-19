63462d735209e3e64da858db8e87b853f8c4e4e7
Eonist <unicornhive@gmail.com>
2016-10-19 18:29:06 +0200

    Files modified: 1
    Modified 1 file:src/window/view/ConflictDialogView.swift

--- src/window/view/ConflictDialogView.swift
+++ src/window/view/ConflictDialogView.swift
@@ -18,12 +18,12 @@ class ConflictDialogView:Element{
         addSubView(TextArea(NaN,NaN,"Resolve merge conflict:",self,"conflictText"))
         
         let repoText:String = "Repository: " + "Element - iOS"
-        addSubView(Text(NaN,NaN,repoText,self,"repoText"))
+        addSubView(TextArea(NaN,NaN,repoText,self,"repoText"))
         
         let fileText:String = "File: " + "AppDelegate.swift"
-        addSubView(Text(NaN,NaN,fileText,self,"fileText"))
+        addSubView(TextArea(NaN,NaN,fileText,self,"fileText"))
         let issueText:String = "Issue: " + "Local file is older than remote"
-        addSubView(Text(NaN,NaN,issueText,self,"issueText"))
+        addSubView(TextArea(NaN,NaN,issueText,self,"issueText"))
         /*
         //Create 3 TextButtons (Review local,remote,mix)
         addSubView(TextButton(NaN,NaN,"Review local version",self,"reviewBtn"))
