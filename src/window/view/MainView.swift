3dcfe1ec16a6557d779cdd4892c30bc27f84b553
Eonist <unicornhive@gmail.com>
2016-10-19 20:14:17 +0200

    Files modified: 2
    Modified 2 files:src/manager/Navigation.swiftsrc/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -16,12 +16,10 @@ class MainView:TitleView{
         super.resolveSkin()
         super.textArea!.setTextValue(title)
         MainWin.mainView = self
-        
+        Navigation.setView(String(ConflictDialogView))//MenuView.repos
         
         menuView = addSubView(MenuView(240,48,self))
         Align.align(menuView!, CGSize(width/**/,height/**/), Alignment.bottomCenter, Alignment.bottomCenter,CGPoint(0,0))
-    
-        Navigation.setView(String(ConflictDialogView))//MenuView.repos
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
