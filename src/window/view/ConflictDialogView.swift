a219d1f0cf650b847d37dec41c4dd24136efe40c
Eonist <unicornhive@gmail.com>
2016-10-19 16:37:18 +0200

    Files modified: 1
    Modified 1 file:src/window/view/ConflictDialogView.swift

--- src/window/view/ConflictDialogView.swift
+++ src/window/view/ConflictDialogView.swift
@@ -25,7 +25,15 @@ class ConflictDialogView:Element{
         addSubView(TextButton(NaN,NaN,"Review mix version",self,"reviewBtn"))
         //Create 3 RadioButtons in a collumn: (keep local,remote, mix)
         //Continue here: review the boilerplate code from Explorer on RadioButton, you need the SelectGroup aswell
-        addSubView(RadioButton())
+        let rb1 = addSubView(RadioButton(NaN,NaN,"keep local version",true,self))
+        let rb2 = addSubView(RadioButton(NaN,NaN,"keep remote version",true,self))
+        let rb3 = addSubView(RadioButton(NaN,NaN,"keep mix of both versions",true,self))
+        let selectGroup:SelectGroup = SelectGroup([radioButton1,radioButton2],radioButton2)
+        func onSelectGroupChange(event:Event){
+            Swift.print("event.selectable: " + "\(event)")
+        }
+        selectGroup.event = onSelectGroupChange
+        
         //A checkBoxButton:[x] apply to all conflicts in this repo's (reset after sync complete)
         //A checkBoxButton:[x] apply to all conflicts in all repo's (reset after sync complete)
         //ok button
