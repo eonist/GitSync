67fc62ab9e34d8a17a27ab7b673a9347a242a6cc
Eonist <unicornhive@gmail.com>
2016-10-20 12:18:52 +0200

    Files modified: 1
    Modified 1 file:src/window/ConflictDialogWin.swift

--- src/window/ConflictDialogWin.swift
+++ src/window/ConflictDialogWin.swift
@@ -9,5 +9,4 @@ class ConflictDialogWin:Window {
         self.contentView = ConflictDialogView(frame.width,frame.height)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
-
 }
