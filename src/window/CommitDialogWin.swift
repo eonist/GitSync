4929c870b0208342e45f785ee811af0398e6649a
Eonist <unicornhive@gmail.com>
2016-10-20 15:00:57 +0200

    Files modified: 1
    Modified 1 file:src/window/CommitDialogWin.swift

--- src/window/CommitDialogWin.swift
+++ src/window/CommitDialogWin.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class CommitDialogWin {
-
+class CommitDialogWin:Window {
+    required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
+        super.init(docWidth, docHeight)
+        WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
+    }
+    override func resolveSkin() {
+        Swift.print("frame.width: " + "\(frame.width)")
+        Swift.print("frame.height: " + "\(frame.height)")
+        self.contentView = CommitDialogView(frame.width,frame.height)
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
