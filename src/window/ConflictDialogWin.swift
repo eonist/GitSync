9b550027f89981b35743950dd71f908ef7f3c284
Eonist <unicornhive@gmail.com>
2016-10-20 12:06:30 +0200

    Files modified: 1
    Modified 1 file:src/window/ConflictDialogWin.swift

--- src/window/ConflictDialogWin.swift
+++ src/window/ConflictDialogWin.swift
@@ -1,13 +1,12 @@
 import Foundation
 
-class ConflictWin:Window {
+class ConflictDialogWin:Window {
     required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
         super.init(docWidth, docHeight)
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
     }
     override func resolveSkin() {
-        
-        self.contentView = ConflictView(frame.width,frame.height,"GitSync")
+        self.contentView = ConflictDialogView(frame.width,frame.height,"GitSync")
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 
