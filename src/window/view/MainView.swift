5185a2a680995ce46596e285668839069c075beb
Eonist <unicornhive@gmail.com>
2016-10-20 12:23:02 +0200

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -20,6 +20,9 @@ class MainView:TitleView{
         
         menuView = addSubView(MenuView(240,48,self))
         Align.align(menuView!, CGSize(width/**/,height/**/), Alignment.bottomCenter, Alignment.bottomCenter,CGPoint(0,0))
+        
+        let conflictDialogWin = ConflictDialogWin(400,500)
+        conflictDialogWin
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
