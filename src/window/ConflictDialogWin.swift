01a8cf3a34c4d6d671d21770a1abe24094f4b737
Eonist <unicornhive@gmail.com>
2016-10-20 12:27:31 +0200

    Files modified: 1
    Modified 1 file:src/window/ConflictDialogWin.swift

--- src/window/ConflictDialogWin.swift
+++ src/window/ConflictDialogWin.swift
@@ -6,6 +6,8 @@ class ConflictDialogWin:Window {
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
     }
     override func resolveSkin() {
+        Swift.print("frame.width: " + "\(frame.width)")
+        Swift.print("frame.height: " + "\(frame.height)")
         self.contentView = ConflictDialogView(frame.width,frame.height)
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
