8c99078c721dee57a127cc7e87e1119e4d00937b
Eonist <unicornhive@gmail.com>
2016-10-21 17:15:06 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -7,7 +7,11 @@ class StatsView:Element {
     }
     func createGraph(){
         //day,week,month,year,all (focus on day and week)
-        //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p 12p
+        //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
+        //00:00, 01:00, 02
+        //00,02,04,06,08,10,12,14,16,18,20,22,24
+        //00:00,04:00,08:00,12:00,16:00,20:00,24:00
+        //00,04,08,12,16,20,24 ()
         //M,T,W,T,F,S,S
         //Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec
         
