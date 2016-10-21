cf0a7a1c874fb573f9f2b13039be2ce66ccafc39
Eonist <unicornhive@gmail.com>
2016-10-21 17:13:07 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -10,5 +10,7 @@ class StatsView:Element {
         //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p 12p
         //M,T,W,T,F,S,S
         //Jan,Feb,Mar,Apr,May,Jun,Jul,Aug,Sep,Oct,Nov,Dec
+        
+        
     }
 }
