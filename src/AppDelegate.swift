c89710868e5fdc55ea20aed54f0e4a27a843460c
Eonist <unicornhive@gmail.com>
2016-10-22 00:01:33 +0200

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -64,6 +64,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 //RBList should read from an array but only display few at the time
                 //Make sure all loose ends are covered
         //later
+            //Contemplate sort of pinching from one time mode to another, with a sort of pop between transitions (<--killer UI idea, A prodHunt instant hit!)
             //adjust the dialog designs
             //drag and drop support for .git urls -> adds it self to the top of the repo list
             //use the color themes from that opensource twitter client in electron
