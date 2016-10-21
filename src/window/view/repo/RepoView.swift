f614557d7ad6745cb1c11137c7da63174949cf01
Eonist <unicornhive@gmail.com>
2016-10-21 13:22:02 +0200

    Files modified: 1
    Modified 1 file:src/window/view/repo/RepoView.swift

--- src/window/view/repo/RepoView.swift
+++ src/window/view/repo/RepoView.swift
@@ -1,6 +1,6 @@
 import Foundation
 /**
- * TODO: should remember previouse selected item between transitions
+ * TODO: should remember previous selected item between transitions
  */
 class RepoView:Element {
     var topBar:TopBar?
