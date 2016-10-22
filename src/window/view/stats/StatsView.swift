23cc2cb90fe2ace93a0b03a40497d9dd162b6118
Eonist <unicornhive@gmail.com>
2016-10-22 12:37:19 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -1,11 +1,13 @@
 import Foundation
 
 class StatsView:Element {
+    var graph:Graph?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)
         createGraph()
     }
     func createGraph(){
+        graph = addSubView(Graph(NaN,NaN,self))
         //day,week,month,year,all (focus on day and week)
         //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
         //00:00, 01:00, 02
@@ -28,11 +30,7 @@ class StatsView:Element {
         //YearGraph
         //AllGraph
         
-        //onResize
-            //recalc spacing
-            //height should be uniform to the width
-            //Realign components
-        
+                
         //GraphTypePicker
             //TabBar with: Day/Week/Month (center aligned)
         //Time Iterator Left and right stepper use the left and right arrows similar to the up and down arrows in stepper (right aligned) (use the stepper just horizontally aligned)
