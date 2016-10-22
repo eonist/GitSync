ba8377c6491999e20f47b265a45af5166d14a295
Eonist <unicornhive@gmail.com>
2016-10-22 12:39:14 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -7,7 +7,7 @@ class StatsView:Element {
         createGraph()
     }
     func createGraph(){
-        graph = addSubView(Graph(NaN,NaN,self))
+        graph = addSubView(Graph(width,height,self))
         //day,week,month,year,all (focus on day and week)
         //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
         //00:00, 01:00, 02
