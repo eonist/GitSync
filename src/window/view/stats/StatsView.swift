702bb16d7d853d4d634292aa240211449518a806
Eonist <unicornhive@gmail.com>
2016-10-23 17:15:00 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -23,7 +23,7 @@ class StatsView:Element {
         stepper
         
         
-        let graphContainer = Container(NaN,NaN,self,"graph")
+        let graphContainer = addSubView(Container(width,height-,self,"graph"))
         graph = graphContainer.addSubView(Graph(width,height-48,graphContainer))
         //day,week,month,year,all (focus on day and week)
         //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
