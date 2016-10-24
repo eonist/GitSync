3a966ac4a45a1b01d4c6800e16f08639d23bbbff
Eonist <unicornhive@gmail.com>
2016-10-24 14:41:32 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -43,29 +43,11 @@ class StatsView:Element {
         //Time Iterator Left and right stepper use the left and right arrows similar to the up and down arrows in stepper (right aligned) (use the stepper just horizontally aligned)
         let stepper:LeverStepper = addSubView(LeverStepper(NaN,NaN,0,1,Int.min.cgFloat,Int.max.cgFloat,0,100,200,self))
         stepper
-        
-        //day,week,month,year,all (focus on day and week)
-        //12a 1a 2a 3a 4a 5a 6a 7a 8a 9a 10a 11a 12p 1p 2p 3p 4p 5p 6p 7p 8p 9p 10p 11p
-        //00:00, 01:00, 02
-        //00,02,04,06,08,10,12,14,16,18,20,22,24 (12 part time)
-        //00:00,04:00,08:00,12:00,16:00,20:00 ()
-        //00,04,08,12,16,20 (6 part time)
-        
-        //Night 00, Morning 06, Noon 12, evening: 18 night 00
-        
-        //00,03,06,09,12,15,18,21,00
-        //12a,03a,06a,09a,12a,03p,06p,09p,12a
-        
         //for all repos:
             //get the commits from today where the user is Eonist
                 //store the time in an [[Int]] (basically a arr with an arr of times)
                 //if time is between 20:00 and 00:00, add to timeArr[0]
         
-        
-        //DayGraph:Graph
-            //override var timeUnits etc
-        //YearGraph
-        //AllGraph
     }
 }
 /**
