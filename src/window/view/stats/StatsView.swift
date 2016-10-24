15b35a742d4d81d8b3cc1edcb0d70abcff9703ef
Eonist <unicornhive@gmail.com>
2016-10-24 12:03:59 +0200

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -111,6 +111,6 @@ class MonthGraph:Graph{
     //GraphPoints should animate if you set the position differently on iteration
 class CommitGraph:Graph{
     override var hValues:[CGFloat] {return [14,8,13,17,25,9,14,20,33,19]}
-    override var hValNames:[String] {return ["10/12","10/13","10/14","10/15","10/16","10/17","10/18","10/19","10/20","10/21"]}
+    override var hValNames:[String] {return ["10/12","13","14","15","16","17","18","19","20","10/21"]}
 }
 
