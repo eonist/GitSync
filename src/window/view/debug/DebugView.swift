1bedacfcf1841e223296b76d523b28e2ae9d2090
Eonist <unicornhive@gmail.com>
2016-10-25 17:59:01 +0200

    Files modified: 1
    Modified 1 file:src/window/view/debug/DebugView.swift

--- src/window/view/debug/DebugView.swift
+++ src/window/view/debug/DebugView.swift
@@ -1,6 +1,7 @@
 import Foundation
 
 class DebugView:Element{
+    var volumeSlider:VolumeSlider?
     var startButton:TextButton?
     var stopButton:TextButton?
     override func resolveSkin() {
@@ -9,12 +10,17 @@ class DebugView:Element{
         
         //add ProgressIndicator
         //add a progressSlider (volumeControl)
-        let volumeSlider = self.addSubView(VolumeSlider(120,20,20,0,self))
-        volumeSlider.setProgressValue(0.5)
+        volumeSlider = self.addSubView(VolumeSlider(120,20,20,0,self))
+        volumeSlider!.setProgressValue(0.5)
         //add a start button (TexteButton)
         startButton = addSubView(TextButton(100,24,"start",self))
         //add a stop button (TexteButton)
         stopButton = addSubView(TextButton(100,24,"stop",self))
         
+    }
+    override func onEvent(event: Event) {
+        if(event.assert(SliderEvent.change, volumeSlider)){
+            
+        }
     }
 }
