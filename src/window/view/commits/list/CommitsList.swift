7246c37c0f41947a8a56348ba01402b2d47f991a
Eonist <unicornhive@gmail.com>
2016-10-26 20:01:53 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitsList.swift

--- src/window/view/commits/list/CommitsList.swift
+++ src/window/view/commits/list/CommitsList.swift
@@ -1,6 +1,31 @@
 import Cocoa
 
 class CommitsList:RBSliderList {
+    var progressIndicator:ProgressIndicator?
+    override func resolveSkin() {
+        let piContainer = addSubView(Container(CommitsView.w, CommitsView.h,self,"progressIndicatorContainer"))
+        progressIndicator = piContainer.addSubView(ProgressIndicator(30,30,piContainer))
+    }
+    /**
+     * Happens when you use the scrollwheel or use the slider
+     * TODO: Spring back motion shouldn't produce ProgressIndicator, only pull should
+     */
+    var isPulledBeyondRefreshSpace:Bool = false
+    func onScroll(){
+        let progressValue = self.progressValue
+        Swift.print("onScroll() progressValue: " + "\(progressValue)")
+        if(progressValue < -0.1){
+            Swift.print("go into refresh mode")
+            isPulledBeyondRefreshSpace = true
+        }else{
+            if(progressValue <  0 && progressValue > -0.1){//between 0 and -1
+                Swift.print("start progressing the ProgressIndicator")
+                let scalarVal:CGFloat = progressValue / -0.1//0 to 1
+                progressIndicator!.progress(scalarVal)
+            }
+            isPulledBeyondRefreshSpace = false//reset
+        }
+    }
     /**
      * NOTE: this method overrides the mergeAt method to facilitate special list items
      */
