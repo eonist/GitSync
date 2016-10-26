cdaa2c9061011202a3d8e08410975af25c680afa
Eonist <unicornhive@gmail.com>
2016-10-26 12:20:25 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -17,7 +17,7 @@ class CommitsView:Element {
         let dp:DataProvider = DataProvider(xml)
         Swift.print("dp.count(): " + "\(dp.count)")
         Swift.print("width: " + "\(width)")
-        list = addSubView(CommitsList(width, height, NaN, dp, self,"commitsList"))
+        list = addSubView(CommitsList(width, height, 102, dp, self,"commitsList"))
         ListModifier.selectAt(list!, 2)
     }
 }
