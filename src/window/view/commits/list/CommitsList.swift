a1377e71c343ded9c8762f70854a20ae965ef9b8
Eonist <unicornhive@gmail.com>
2016-10-26 13:49:53 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitsList.swift

--- src/window/view/commits/list/CommitsList.swift
+++ src/window/view/commits/list/CommitsList.swift
@@ -8,8 +8,8 @@ class CommitsList:RBSliderList {
         /**/var i:Int = index;
         //Swift.print("mergeAt: index: " + "\(index)");
         for object:Dictionary<String,String> in objects {// :TODO: use for i
-            let item:CommitsListItem = CommitsListItem(width, self.itemHeight ,object["repo-name"]!, object["contributor"]!,object["title"]!,object["description"]!,object["date"]!, false, self.lableContainer)
-            self.lableContainer!.addSubviewAt(item, i)/*the first index is reserved for the List skin, what?*/
+            let item:CommitsListItem = CommitsListItem(width, itemHeight ,object["repo-name"]!, object["contributor"]!,object["title"]!,object["description"]!,object["date"]!, false, self.lableContainer)
+            lableContainer!.addSubviewAt(item, i)/*the first index is reserved for the List skin, what?*/
             i++
         }
     }
