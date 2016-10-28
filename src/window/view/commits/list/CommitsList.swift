75c9d616833ee90097a0efd9fbeef2de9c7949a2
Eonist <unicornhive@gmail.com>
2016-10-28 12:35:40 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitsList.swift

--- src/window/view/commits/list/CommitsList.swift
+++ src/window/view/commits/list/CommitsList.swift
@@ -32,18 +32,17 @@ class CommitsList:RBSliderList {
             
             //isInRefreshMode
             progressIndicator!.progress(scalarVal)
-            
-            //Continue here: 
-            
-            //the progress indicator needs to be able to be able to reveal it self 1 tick at the time in the init state
-            //
-            
             progressIndicator!.frame.y = -45 + (scalarVal * 60)
         }else if(value > 60){
             progressIndicator!.frame.y = 15
             hasPulledBeyondRefreshSpace = true
         }
     }
+    
+    //Continue here:
+    
+    //the progress indicator needs to be able to be able to reveal it self 1 tick at the time in the init state
+    //
     
     //Continue here: onScrollWheleDown -> set topMargin to 0
         //implement event for scrollWheelDown
