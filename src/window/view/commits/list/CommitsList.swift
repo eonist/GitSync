dd9a4a04c11e179b4b9f893aa24e4ddbf24b558f
Eonist <unicornhive@gmail.com>
2016-10-29 17:04:50 +0200

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitsList.swift

--- src/window/view/commits/list/CommitsList.swift
+++ src/window/view/commits/list/CommitsList.swift
@@ -25,7 +25,10 @@ class CommitsList:RBSliderList {
             let scalarVal:CGFloat = value / 60//0 to 1
             if(hasPulledAndReleasedBeyondRefreshSpace){//isInRefreshMode
                 progressIndicator!.frame.y = -45 + (scalarVal * 60)
-            }else if(isTwoFingersTouching || progressIndicator!.revealProgress > 0.0){
+            }else if(isTwoFingersTouching || progressIndicator!.revealProgress > 0.02){
+                
+                //Continue here: try to set the reveal progress to 0 so that the above assert starts working
+                
                 progressIndicator!.frame.y = 15//<--this could be set else where but something kept interfering with it
                 progressIndicator!.reveal(scalarVal)//the progress indicator needs to be able to be able to reveal it self 1 tick at the time in the init state
             }
