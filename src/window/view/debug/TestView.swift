4d22a44c7c9998cdf2595237e0be510913d2e4e8
Eonist <unicornhive@gmail.com>
2016-10-30 13:03:10 +0100

    Files modified: 1
    Modified 1 file:src/window/view/debug/TestView.swift

--- src/window/view/debug/TestView.swift
+++ src/window/view/debug/TestView.swift
@@ -1,5 +1,19 @@
 import Foundation
 
-class TestView {
-
+class TestView:Element {
+    var volumeSlider:VolumeSlider?
+    override func resolveSkin() {
+        Swift.print("TestView.resolveSkin()")
+        skin = SkinResolver.skin(self)
+        
+        volumeSlider = addSubView(VolumeSlider(120,20,20,0,self))
+        volumeSlider!.setProgressValue(0.0)
+        
+    }
+    override func onEvent(event: Event) {
+        if(event.assert(SliderEvent.change, volumeSlider)){
+            let volumSliderProgress = (event as! SliderEvent).progress
+            Swift.print("volumSliderProgress: " + "\(volumSliderProgress)")
+        }
+    }
 }
