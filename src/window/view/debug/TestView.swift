21111cc024a6231a72698cadb7bb07b6cec703d1
Eonist <unicornhive@gmail.com>
2016-10-31 19:03:45 +0100

    Files modified: 1
    Modified 1 file:src/window/view/debug/TestView.swift

--- src/window/view/debug/TestView.swift
+++ src/window/view/debug/TestView.swift
@@ -10,13 +10,13 @@ class TestView:Element {
         volumeSlider = addSubView(VolumeSlider(120,20,20,0,self))
         volumeSlider!.setProgressValue(0.0)
         
-        list = addSubView(FastList(150,250,self))
+        //list = addSubView(FastList(150,250,self))
     }
     override func onEvent(event: Event) {
         if(event.assert(SliderEvent.change, volumeSlider)){
             let volumSliderProgress = (event as! SliderEvent).progress
             //Swift.print("volumSliderProgress: " + "\(volumSliderProgress)")
-            list?.setProgress(volumSliderProgress)
+            //list?.setProgress(volumSliderProgress)
         }
     }
 }
