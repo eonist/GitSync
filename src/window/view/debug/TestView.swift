09e4eabb811c2f53f7f1f39055af824d9ab10710
Eonist <unicornhive@gmail.com>
2016-11-02 15:50:44 +0100

    Files modified: 1
    Modified 1 file:src/window/view/debug/TestView.swift

--- src/window/view/debug/TestView.swift
+++ src/window/view/debug/TestView.swift
@@ -19,7 +19,8 @@ class TestView:Element {
         
         let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
         let dp:DataProvider = DataProvider(xml)
-        let sliderList:SliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
+        //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
+        let sliderList:ISliderList = self.addSubView(SliderFastList(140, 73, 24, dp, self))
         ListModifier.select(sliderList, "white")
     }
     override func onEvent(event: Event) {
