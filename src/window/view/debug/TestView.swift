748c3c9c79f6666b768c37151daf0e8b8cc48ad8
Eonist <unicornhive@gmail.com>
2016-11-03 19:58:41 +0100

    Files modified: 1
    Modified 1 file:src/window/view/debug/TestView.swift

--- src/window/view/debug/TestView.swift
+++ src/window/view/debug/TestView.swift
@@ -14,12 +14,15 @@ class TestView:Element {
         volumeSlider = addSubView(VolumeSlider(220,20,20,0,self))
         volumeSlider!.setProgressValue(0.0)
         
-        /*
+        //createList()
+        //createSliderList()
+    }
+    func createFastList(){
         let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
         let dp:DataProvider = DataProvider(xml)
         list = addSubView(FastList(140,73,24,dp,self))
-        */
-        
+    }
+    func createSliderFastList(){
         let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
         let dp:DataProvider = DataProvider(xml)
         //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
@@ -28,7 +31,16 @@ class TestView:Element {
         /**/
         //ListModifier.select(sliderList, "white")
     }
-    
+    /**
+     *
+     */
+    func createRBFastList(){
+        let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
+        let dp:DataProvider = DataProvider(xml)
+        //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
+        let sliderList:ISliderList = self.addSubView(SliderFastList(140, 73, 24, dp, self))
+        sliderList
+    }
     override func onEvent(event: Event) {
         if(event.assert(SliderEvent.change, volumeSlider)){
             let volumSliderProgress = (event as! SliderEvent).progress
