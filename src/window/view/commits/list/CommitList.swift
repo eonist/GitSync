7e47b20f9e7524f16d95223b22296dea2883de6a
Eonist <unicornhive@gmail.com>
2016-11-05 20:29:40 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitList.swift

--- src/window/view/commits/list/CommitList.swift
+++ src/window/view/commits/list/CommitList.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 class CommitList:RBSliderFastList{
-    override func spawn(idx: Int) -> Element {
+    override func spawn(idx:Int) -> Element {
         let dpItem = dataProvider.items[idx]
         let item:CommitsListItem = CommitsListItem(width, itemHeight ,dpItem["repo-name"]!, dpItem["contributor"]!,dpItem["title"]!,dpItem["description"]!,dpItem["date"]!, false, self.lableContainer)
         return item
@@ -9,10 +9,8 @@ class CommitList:RBSliderFastList{
     override func spoof(listItem:FastListItem) {
         let item:CommitsListItem = listItem.item as! CommitsListItem
         let idx:Int = listItem.idx/*the index of the data in dataProvider*/
-        let dpItem = dataProvider.items[idx]
-        let title:String = dpItem["title"]!
         let selected:Bool = idx == selectedIdx//dpItem["selected"]!.bool
+        item.setData(dataProvider.items[idx])
         if(item.selected != selected){item.setSelected(selected)}//only set this if the selected state is different from the current selected state in the ISelectable
-        item.setData(<#T##repoName: String##String#>, <#T##contributor: String##String#>, <#T##title: String##String#>, <#T##desc: String##String#>, <#T##date: String##String#>)
     }
 }
