60f69e8b288ddc8ffcd2930b0eb71aea71909a98
Eonist <unicornhive@gmail.com>
2016-11-05 19:10:57 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/list/CommitList.swift

--- src/window/view/commits/list/CommitList.swift
+++ src/window/view/commits/list/CommitList.swift
@@ -2,11 +2,10 @@ import Foundation
 
 class CommitList:RBSliderFastList{
     override func spawn(idx: Int) -> Element {
-        return super.spawn(idx)
+        let item:CommitsListItem = CommitsListItem(width, itemHeight ,object["repo-name"]!, object["contributor"]!,object["title"]!,object["description"]!,object["date"]!, false, self.lableContainer)
+        lableContainer!.addSubviewAt(item, i)/*the first index is reserved for the List skin, what?*/
+        return item
     }
     override func spoof(listItem: FastListItem) {
-        //let item:CommitsListItem = CommitsListItem(width, itemHeight ,object["repo-name"]!, object["contributor"]!,object["title"]!,object["description"]!,object["date"]!, false, self.lableContainer)
-        //lableContainer!.addSubviewAt(item, i)/*the first index is reserved for the List skin, what?*/
-        
     }
 }
