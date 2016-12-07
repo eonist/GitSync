8581ff9da46b0f3af860f45c489d07c4206b8b1f
Eonist <unicornhive@gmail.com>
2016-12-07 19:33:00 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/detail/CommitDetailView.swift

--- src/window/view/commits/detail/CommitDetailView.swift
+++ src/window/view/commits/detail/CommitDetailView.swift
@@ -6,6 +6,7 @@ class CommitDetailView:Element {
     var titleText:Text?
     var descText:Text?
     override func resolveSkin() {
+        Swift.print("CommitDetailView.resolveSkin()")
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
         titleText = addSubView(Text(280,24,"commit title",self,"title"))
         titleText!.isInteractive = false
