18ef114da027fea5930d9d1bce497ed853d39b2e
Eonist <unicornhive@gmail.com>
2016-12-07 16:10:17 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/detail/CommitDetailView.swift

--- src/window/view/commits/detail/CommitDetailView.swift
+++ src/window/view/commits/detail/CommitDetailView.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class CommitDetailView {
-
+class CommitDetailView:Element {
+    var titleText:Text?
+    var descText:Text?
+    override func resolveSkin() {
+        self.skin = SkinResolver.skin(self)//super.resolveSkin()
+        titleText = addSubView(Text(280,24,title,container,"title"))
+        titleText!.isInteractive = false
+        descText = container.addSubView(Text(180,50,desc,container,"description"))
+        descText!.isInteractive = false
+    }
 }
