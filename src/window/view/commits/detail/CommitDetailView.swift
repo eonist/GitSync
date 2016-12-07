9ea6983de19dc78684e9e18d35e3b67e6ae6c0a6
Eonist <unicornhive@gmail.com>
2016-12-07 16:14:06 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/detail/CommitDetailView.swift

--- src/window/view/commits/detail/CommitDetailView.swift
+++ src/window/view/commits/detail/CommitDetailView.swift
@@ -5,9 +5,16 @@ class CommitDetailView:Element {
     var descText:Text?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
-        titleText = addSubView(Text(280,24,title,container,"title"))
+        titleText = addSubView(Text(280,24,"commit title",self,"title"))
         titleText!.isInteractive = false
-        descText = container.addSubView(Text(180,50,desc,container,"description"))
+        descText = addSubView(Text(180,50,"commit desc",self,"description"))
         descText!.isInteractive = false
+    }
+    /**
+     * Populates the UI elements with data from the dp item
+     */
+    func setCommitData(commitData:Dictionary<String,String>){
+        titleText!.setText(commitData["title"]!)
+        descText!.setText(commitData["desc"]!)
     }
 }
