4be9e887c3ec268d3bf96e8fd0ae834eae2f953f
Eonist <unicornhive@gmail.com>
2016-12-08 12:48:35 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/StatsView.swift

--- src/window/view/stats/StatsView.swift
+++ src/window/view/stats/StatsView.swift
@@ -169,10 +169,6 @@ class CommitGraph:Graph{
         }
         return (values,dayNames)
     }
-    
-    
-    
-    
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
 
