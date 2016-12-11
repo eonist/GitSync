023fc4010d9416999e626adb3526ca6048b37511
Eonist <unicornhive@gmail.com>
2016-12-11 21:46:59 +0100

    Files modified: 1
    Modified 1 file:src/window/view/MainView.swift

--- src/window/view/MainView.swift
+++ src/window/view/MainView.swift
@@ -40,7 +40,8 @@ class MainView:TitleView{
         let localPath = repoList[1]["local-path"]
         Swift.print("localPath: " + "\(localPath)")
         
-        let cmd:String = "-3 --pretty=format:\"Date:%cr%nSubject:%s%nBody:%b\""
+        let cmd:String = "-3 --pretty=format:''Date:%cr%nSubject:%s%nBody:%b''"
+        Swift.print("cmd: " + "\(cmd)")
         let logResult:String = GitParser.log(localPath!, cmd)
         Swift.print("logResult: " + "\(logResult)")
         
