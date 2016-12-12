32404a99b24b174308e99b9d84b94a8f02d88e98
Eonist <unicornhive@gmail.com>
2016-12-12 12:42:31 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -60,7 +60,23 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
         //make the 4h, 2d, 5m,  3sec logic
         
-        //
+        //basically: 
+        
+        //cur time - time
+        
+        //cur year - year = 0
+        
+        //cur month - month = 0
+        
+        //cur day - day = 0
+        
+        //cur hour - hour = 0
+        
+        //cur min - min = 0
+        
+        //cur sec - sec = 4
+        
+        //result: 4s ago
     }
     func initApp(){
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)//<--toggle this bool for live refresh
