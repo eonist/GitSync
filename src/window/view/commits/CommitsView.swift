7c0a46f13d15613fdfac982ca3681f502a09c0f6
Eonist <unicornhive@gmail.com>
2016-12-13 20:16:55 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -81,7 +81,7 @@ private class Utils{
             
             let compactBody:String = GitLogParser.compactBody(commitData.body)/*compact the commit msg body*/
             Swift.print("compactBody: " + "\(compactBody)")
-            commitItems.append(["repo-name":repoTitle,"contributor":commitData.author,"title":commitData.subject,"description":compactBody,"date":descendingDate,"relativedate":relativeDate,"hash":commitData.hash])////we store the full hash in the CommitData and in the dp item, so that when you click on an item you can generate all commit details in the CommitDetailView
+            commitItems.append(["repo-name":repoTitle,"contributor":commitData.author,"title":commitData.subject,"description":compactBody,"date":relativeDate,"descendingDate":descendingDate,"hash":commitData.hash])////we store the full hash in the CommitData and in the dp item, so that when you click on an item you can generate all commit details in the CommitDetailView
         }
         
         //Continue here: sort DP
