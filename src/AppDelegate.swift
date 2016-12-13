9e83ff965546661385a9314864b7d22fab50ee6f
Eonist <unicornhive@gmail.com>
2016-12-13 23:39:40 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -38,7 +38,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         func trim(str:String){
            
             
-            str.matches("(?:[',\n]{1,3})(.*?)(?:\n)").forEach{
+            str.matches("(?:[',\n]{0,3})(.*?)(?:\n)").forEach{
                 if($0.numberOfRanges > 1){
                     let body = $0.value(str, 1)/*capturing group 1*/
                     Swift.print(">")
