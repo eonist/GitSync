5cac655b5e31eb2e10d8d0d5f05b98d517515bca
Eonist <unicornhive@gmail.com>
2016-12-14 14:42:34 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/CommitsView.swift

--- src/window/view/commits/CommitsView.swift
+++ src/window/view/commits/CommitsView.swift
@@ -97,46 +97,6 @@ private class Utils{
         
         return dp
         
-        //Continue here: 
-            //parsing log now works.
-                //lets try and parse the last 20 items from every repo and combine them in a dataprovider (done)
-                    //store in a array-dictionary and create the dp after you have sorted the the arr-dict (done)
-                        //then try to populate the GUI (done)
-                            //remember to store the relative date and compacted body. You should not do any data processing when you display in the GUI (done)
-                                //You can always get the full body from the hash when going to CommentDetailView
-        
-        
-        //TODO: Use RegExp to convert the commit data item to an Triplet item in an array (done)
-            //or find the first two linebreaks and split at these integers, then you will have 3 seperate string parts <---easier
-        
-        
-        //Figure out how to seperate each commit item. With a special char combo that indicates start and end?
-            //20 last commits
-            //get commit count: (git rev-list HEAD --count) - 1
-            //let length:Int = commitCount > 20 ? 20 : commitCount//20 = maxCount
-            //for _ in 0..<length{
-                //replace 31 with i bellow:
-                //let logItem:String = git show head~31  --pretty=oneline --no-patch//--no-patch suppresses the diff output of git show
-                //convert the logItem to Tupple
-        
-            //}
-            //return array of Tupples
-        
-        
-        //on refresh
-            //load all commit log items into one dataprovider
-                //remember to attach repo name to the commitData tuple
-        
-        
-        //Actually do optimization later:
-            //Store each Tupple array in its own dataProvider item
-                //so that we don't have to reload every log item for every refresh
-                //also store these DataProvider items when the app closes
-                    //Then re-use them on App start
-        
-            //When a refresh occurs
-                //only grab the log items that are newer than the newst logItem in the dataProvider
-                //then prepend these to the dataProvider
     }
 }
 /*
@@ -155,3 +115,45 @@ class CommitsTopBar:Element{
     }
 }
 */
+
+
+//Continue here: 
+    //parsing log now works.
+        //lets try and parse the last 20 items from every repo and combine them in a dataprovider (done)
+            //store in a array-dictionary and create the dp after you have sorted the the arr-dict (done)
+                //then try to populate the GUI (done)
+                    //remember to store the relative date and compacted body. You should not do any data processing when you display in the GUI (done)
+                        //You can always get the full body from the hash when going to CommentDetailView
+
+
+//TODO: Use RegExp to convert the commit data item to an Triplet item in an array (done)
+    //or find the first two linebreaks and split at these integers, then you will have 3 seperate string parts <---easier
+
+
+//Figure out how to seperate each commit item. With a special char combo that indicates start and end?
+    //20 last commits
+    //get commit count: (git rev-list HEAD --count) - 1
+    //let length:Int = commitCount > 20 ? 20 : commitCount//20 = maxCount
+    //for _ in 0..<length{
+        //replace 31 with i bellow:
+        //let logItem:String = git show head~31  --pretty=oneline --no-patch//--no-patch suppresses the diff output of git show
+        //convert the logItem to Tupple
+
+    //}
+    //return array of Tupples
+
+
+//on refresh
+    //load all commit log items into one dataprovider
+        //remember to attach repo name to the commitData tuple
+
+
+//Actually do optimization later:
+    //Store each Tupple array in its own dataProvider item
+        //so that we don't have to reload every log item for every refresh
+        //also store these DataProvider items when the app closes
+            //Then re-use them on App start
+
+    //When a refresh occurs
+        //only grab the log items that are newer than the newst logItem in the dataProvider
+        //then prepend these to the dataProvider
