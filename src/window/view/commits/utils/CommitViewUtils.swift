9699d85e81ff53e66da7ca9f7d7442b846ddd5f8
Eonist <unicornhive@gmail.com>
2016-12-16 16:47:04 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/utils/CommitViewUtils.swift

--- src/window/view/commits/utils/CommitViewUtils.swift
+++ src/window/view/commits/utils/CommitViewUtils.swift
@@ -1,5 +1,20 @@
 import Foundation
 
 class CommitViewUtils {
-
+    /**
+     *
+     */
+    class func processCommitData(repoTitle:String,_ commitData:CommitData)-> Dictionary<String, String>{
+        let date:NSDate = GitLogParser.date(commitData.date)
+        //Swift.print("date.shortDate: " + "\(date.shortDate)")
+        let relativeTime = DateParser.relativeTime(NSDate(),date)[0]
+        let relativeDate:String = relativeTime.value.string + relativeTime.type/*create date like 3s,4m,5h,6w,2y*/
+        //Swift.print("relativeDate: " + "\(relativeDate)")
+        let descendingDate:String = DateParser.descendingDate(date)
+        let compactBody:String = GitLogParser.compactBody(commitData.body)/*compact the commit msg body*/
+        //Swift.print("compactBody: " + "\(compactBody)")
+        let subject:String = StringParser.trim(commitData.subject, "'", "'")
+        let processedCommitData:Dictionary<String, String> = ["repo-name":repoTitle,"contributor":commitData.author,"title":subject,"description":compactBody,"date":relativeDate,"sortableDate":descendingDate,"hash":commitData.hash]
+        return processedCommitData
+    }
 }
