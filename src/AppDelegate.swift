020fd71c2565df00b06ef32efd551798f51e5955
Eonist <unicornhive@gmail.com>
2016-12-18 13:54:03 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -75,39 +75,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         sortedArr.insertAt(item, insertAt)
         Swift.print("sortedArr: " + "\(sortedArr)")
     }
-    /**
-     * This binarySearch finds a suitable index to insert an item in a sorted list (a regular binarySearch would return nil if no match is found, this implmentation returns the closestIndex)
-     * NOTE: Binary search, also known as half-interval search or logarithmic search, is a search algorithm that finds the position of a target value within a sorted array.
-     * NOTE: Binary search compares the target value to the middle element of the array; if they are unequal, the half in which the target cannot lie is eliminated and the search continues on the remaining half until it is successful.
-     * NOTE: Binary search runs in at worst logarithmic time, making O(log n) comparisons, where n is the number of elements in the array and log is the logarithm. Binary search takes only constant (O(1)) space, meaning that the space taken by the algorithm is the same for any number of elements in the array.[5] Although specialized data structures designed for fast searching—such as hash tables—can be searched more efficiently, binary search applies to a wider range of search problems.
-     * NOTE: This implementation of binary search is recursive (it calls it self) (Binary search is recursive in nature because you apply the same logic over and over again to smaller and smaller subarrays.)
-     * IMPORTANT: Although the idea is simple, implementing binary search correctly requires attention to some subtleties about its exit conditions and midpoint calculation.
-     * IMPORTANT: Note that the numbers array is sorted. The binary search algorithm does not work otherwise!
-     * DISCUSSION: Is it a problem that the array must be sorted first? It depends. Keep in mind that sorting takes time -- the combination of binary search plus sorting may be slower than doing a simple linear search. Binary search shines in situations where you sort just once and then do many searches.
-     * TRIVIA:  YOu can also implement binary serach as iterative implementation by using a while loop
-     * TODO: use range instead of start and end int?!?
-     */
-    func closestIndex<T:Comparable>(arr:[T],_ i:T,_ start:Int,_ end:Int) -> Int{//arr[Stridable] or something indexable
-        Swift.print("start: " + "\(start)")
-        Swift.print("end: " + "\(end)")
-        if(start == end){
-            Swift.print("i doesn't exist, this is the closest at: \(start) ")
-            return start
-        }
-        let mid:Int = start + ((end - start) / 2)/*start + middle of the distance between start and end*/
-        Swift.print("mid: " + "\(mid)")
-        //Swift.print("arr[mid]: " + "\(arr[mid])")
-        if(i < arr[mid]){/*index is in part1*/
-            Swift.print("a")
-            return closestIndex(arr,i,start,mid)
-        }else if(i > arr[mid]){/*index is in part2*/
-            Swift.print("b")
-            return closestIndex(arr,i,mid+1,end)
-        }else{/*index is at middleIndex*/
-            Swift.print("at middle: \(mid)")
-            return mid
-        }
-    }
+    
     //this part isn't needed, the else part takes care of it
     /*
     if(end-start == 1){/*the range is narrowed down to 2 indecies: at start and at end*/
@@ -427,16 +395,54 @@ func == (a: SortableCommit, b: SortableCommit) -> Bool {
     return a.date == b.date && a.hash == b.hash && a.repoId == b.repoId
 }
 class CommitDB{
-    var sortedArr:[SortableCommit] = []
+    var sortedArr:[SortableCommit]
     init(_ sortedArr:[SortableCommit]){
-        
+        self.sortedArr = sortedArr
         let itemA = SortableCommit(0,"fak42a",201602)
-        let closestIdx:Int = closestIndex(sortedArr, itemA, 0, sortedArr.endIndex)
+        
+    }
+    /**
+     *
+     */
+    func add(item:SortableCommit){
+        let closestIdx:Int = closestIndex(sortedArr, item, 0, sortedArr.endIndex)
         Swift.print("closestIndex: " + "\(closestIdx)")
         
         let insertAt:Int = itemA > sortedArr.last && sortedArr.count != 0 ? closestIdx + 1 : closestIdx  //this line enables you to insert the new item correctly in the sorted array
         Swift.print("insertAt: " + "\(insertAt)")
         sortedArr.insertAt(itemA, insertAt)
     }
-    
+    /**
+     * This binarySearch finds a suitable index to insert an item in a sorted list (a regular binarySearch would return nil if no match is found, this implmentation returns the closestIndex)
+     * NOTE: Binary search, also known as half-interval search or logarithmic search, is a search algorithm that finds the position of a target value within a sorted array.
+     * NOTE: Binary search compares the target value to the middle element of the array; if they are unequal, the half in which the target cannot lie is eliminated and the search continues on the remaining half until it is successful.
+     * NOTE: Binary search runs in at worst logarithmic time, making O(log n) comparisons, where n is the number of elements in the array and log is the logarithm. Binary search takes only constant (O(1)) space, meaning that the space taken by the algorithm is the same for any number of elements in the array.[5] Although specialized data structures designed for fast searching—such as hash tables—can be searched more efficiently, binary search applies to a wider range of search problems.
+     * NOTE: This implementation of binary search is recursive (it calls it self) (Binary search is recursive in nature because you apply the same logic over and over again to smaller and smaller subarrays.)
+     * IMPORTANT: Although the idea is simple, implementing binary search correctly requires attention to some subtleties about its exit conditions and midpoint calculation.
+     * IMPORTANT: Note that the numbers array is sorted. The binary search algorithm does not work otherwise!
+     * DISCUSSION: Is it a problem that the array must be sorted first? It depends. Keep in mind that sorting takes time -- the combination of binary search plus sorting may be slower than doing a simple linear search. Binary search shines in situations where you sort just once and then do many searches.
+     * TRIVIA:  YOu can also implement binary serach as iterative implementation by using a while loop
+     * TODO: use range instead of start and end int?!?
+     */
+    func closestIndex<T:Comparable>(arr:[T],_ i:T,_ start:Int,_ end:Int) -> Int{//arr[Stridable] or something indexable
+        Swift.print("start: " + "\(start)")
+        Swift.print("end: " + "\(end)")
+        if(start == end){
+            Swift.print("i doesn't exist, this is the closest at: \(start) ")
+            return start
+        }
+        let mid:Int = start + ((end - start) / 2)/*start + middle of the distance between start and end*/
+        Swift.print("mid: " + "\(mid)")
+        //Swift.print("arr[mid]: " + "\(arr[mid])")
+        if(i < arr[mid]){/*index is in part1*/
+            Swift.print("a")
+            return closestIndex(arr,i,start,mid)
+        }else if(i > arr[mid]){/*index is in part2*/
+            Swift.print("b")
+            return closestIndex(arr,i,mid+1,end)
+        }else{/*index is at middleIndex*/
+            Swift.print("at middle: \(mid)")
+            return mid
+        }
+    }
 }
