a36a6d9e907a88c66cc4476ce960865c6cc9ca53
Eonist <unicornhive@gmail.com>
2016-12-18 19:12:46 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -496,3 +496,24 @@ extension SortableCommit:UnWrappable{
         return SortableCommit(repoId,hash,date) as? T
     }
 }
+/**
+ * DISCUSSION: Using struct is justified because the data is never modified. Just stored and reproduced
+ */
+struct Commit{
+    let repoName:String
+    let contributor:String
+    let title:String
+    let description:String
+    let date:String
+    let sortableDate:Int
+    let hash:String
+    init(_ repoName:String,_ contributor:String,_ title:String,_ description:String,_ date:String,_ sortableDate:Int,_ hash:String){
+        self.repoName = repoName
+        self.contributor = contributor
+        self.title = title
+        self.description = description
+        self.date = date
+        self.sortableDate = sortableDate
+        self.hash = hash
+    }
+}
