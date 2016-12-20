7937010cb8c2ee5c41e7e47c3655670647ca43df
Eonist <unicornhive@gmail.com>
2016-12-20 20:49:16 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -128,6 +128,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
         //Continue here:
             //do the git date range research and tests
+                //commit count after date ✅
             //make the refresh CommitDB algo🤖 as described
             //how do we refresh after commits and pushes to remote? 👉 after 👈 because -> simplicity 👌
         
