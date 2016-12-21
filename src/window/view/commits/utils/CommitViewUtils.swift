bc0a4dca2eb199f23e3259e9a5f471a5f6bb9abc
Eonist <unicornhive@gmail.com>
2016-12-21 15:03:01 +0100

    Files modified: 1
    Modified 1 file:src/window/view/commits/utils/CommitViewUtils.swift

--- src/window/view/commits/utils/CommitViewUtils.swift
+++ src/window/view/commits/utils/CommitViewUtils.swift
@@ -7,7 +7,7 @@ class CommitViewUtils {
     static func processCommit(repoTitle:String,_ commitData:CommitData)->(date:NSDate,relativeDate:String,descendingDate:String,compactBody:String,subject:String,hash:String){
         let date:NSDate = GitLogParser.date(commitData.date)
         //Swift.print("date.shortDate: " + "\(date.shortDate)")
-        let relativeTime = DateParser.relativeTime(NSDate(),date)[0]
+        let relativeTime:(value:Int,type:String) = DateParser.relativeTime(NSDate(),date)[0]
         let relativeDate:String = relativeTime.value.string + relativeTime.type/*create date like 3s,4m,5h,6w,2y*/
         //Swift.print("relativeDate: " + "\(relativeDate)")
         let descendingDate:String = DateParser.descendingDate(date)
