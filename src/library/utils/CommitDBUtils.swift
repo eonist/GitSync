473801d9e0bafff334caa6d7518b146ca56c11fc
Eonist <unicornhive@gmail.com>
2016-12-21 13:14:01 +0100

    Files modified: 1
    Modified 1 file:src/library/utils/CommitDBUtils.swift

--- src/library/utils/CommitDBUtils.swift
+++ src/library/utils/CommitDBUtils.swift
@@ -7,8 +7,9 @@ class CommitDBUtils {
     static func refresh(commitDB:CommitDB){
         //You loop the repos
             //find the range of commits to add to CommitDB for this repo
-        if (commitDB.sortedArr.count >= 100){
-            
+        if(commitDB.sortedArr.count >= 100){
+            let lastDate = sortedArr.count > 0 ? sortedByDates.last.date : Int.min<--min represents max negative num
+            //range = now..lastDate in the repo (date based) Needs --> 🔬 (how does querying for date ranges in git work)
         }else {//< 100
             
         }
