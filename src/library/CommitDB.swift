ac34e6ee3583e45662c4ebb45ba67405c9c9b068
Eonist <unicornhive@gmail.com>
2016-12-21 17:21:51 +0100

    Files modified: 1
    Modified 1 file:src/library/CommitDB.swift

--- src/library/CommitDB.swift
+++ src/library/CommitDB.swift
@@ -23,6 +23,7 @@ extension CommitDB{
         //let insertAt:Int = item > sortedArr.last && sortedArr.count != 0 ? closestIdx + 1 : closestIdx  //this line enables you to insert the new item correctly in the sorted array
         //Swift.print("insertAt: " + "\(insertAt)")
         sortedArr.insertAt(item, closestIdx)
+        if(sortedArr.count > 100){sortedArr.popLast()}
     }
     /**
      * This binarySearch finds a suitable index to insert an item in a sorted list (a regular binarySearch would return nil if no match is found, this implmentation returns the closestIndex)
