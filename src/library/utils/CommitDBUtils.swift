b60d3ea7f089a0f5ece809aa20e19267ee54928f
Eonist <unicornhive@gmail.com>
2016-12-21 17:05:50 +0100

    Files modified: 1
    Modified 1 file:src/library/utils/CommitDBUtils.swift

--- src/library/utils/CommitDBUtils.swift
+++ src/library/utils/CommitDBUtils.swift
@@ -7,15 +7,14 @@ class CommitDBUtils {
     static var commitDB = CommitDB()
     static var operations:[CommitLogOperation] = []
     /**
-     *
+     * TODO: time test
      */
     static func refresh(){
         //1. You loop the repos
         let repoXML = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)//~/Desktop/repo2.xml
         var repoList = XMLParser.toArray(repoXML)//or use dataProvider
-        repoList = [repoList[1]]//test with one repo the element ios repo
-        //repoList = [repoList[1]]
         Swift.print("repoList.count: " + "\(repoList.count)")
+        //repoList = [repoList[1]]//test with one repo the element ios repo
         for (index,element) in repoList.enumerate(){/*Loops through repos*/
             let localPath:String = element["local-path"]!//local-path to repo
             let repoTitle = element["title"]!//name of repo
@@ -31,7 +30,7 @@ class CommitDBUtils {
                 //commitCount = commitCount > repoCommitCount ? repoCommitCount : commitCount/* so that we don't query for commit items that doesnt exist */
             }
             //3. Retrieve the commit log items for this repo with the range specified
-            Swift.print("max: " + "\(commitCount)")
+            //Swift.print("max: " + "\(commitCount)")
             let args:[String] = CommitViewUtils.commitItems(localPath,commitCount)/*creates an array of arguments that will return commit item logs*/
             for (_,element) in args.enumerate(){
                 let operation = CommitViewUtils.configOperation([element],localPath,repoTitle,index)/*setup the NSTask correctly*/
@@ -62,9 +61,9 @@ class CommitDBUtils {
             let commit:Commit = CommitViewUtils.processCommitData($0.repoTitle,commitData,$0.repoIndex)/*Format the data*/
             commitDB.add(commit)/*add the commit log items to the CommitDB*/
         }
-        
-        Swift.print("Printing sortedArr after refresh: ")
+  
         Swift.print("commitDB.sortedArr.count: " + "\(commitDB.sortedArr.count)")
+        //Swift.print("Printing sortedArr after refresh: ")
         //commitDB.sortedArr.forEach{Swift.print($0.sortableDate)}
     }
 }
