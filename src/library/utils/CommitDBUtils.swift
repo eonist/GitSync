9c0159ce5845ec0e1af5299d31f138de88a29c2d
Eonist <unicornhive@gmail.com>
2016-12-21 13:34:58 +0100

    Files modified: 1
    Modified 1 file:src/library/utils/CommitDBUtils.swift

--- src/library/utils/CommitDBUtils.swift
+++ src/library/utils/CommitDBUtils.swift
@@ -28,5 +28,14 @@ class CommitDBUtils {
 }
 
 private class Utils{
-    
+    /**
+     * Formats chronological date to git time-> "2016-11-12 00:00:00"
+     * NOTE: YYYYMMDDHHmmss -> YYYY-MM-DD HH:mm:ss
+     * Alt name: chronologicalTime2GitTime
+     * EXAMPLE: gitTime("20161111205959")//Output2016-11-11 20:59:59
+     */
+    static func gitTime(chronoTime:String)->String{
+        let gitTime = chronoTime.insertCharsAt([("-",4),("-",6),(" ",8),(":",10),(":",12)])
+        return gitTime
+    }
 }
