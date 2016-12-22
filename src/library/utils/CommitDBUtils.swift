e47febb2490da6dcd98616efed33a9f185f91d60
Eonist <unicornhive@gmail.com>
2016-12-22 12:51:43 +0100

    Files modified: 1
    Modified 1 file:src/library/utils/CommitDBUtils.swift

--- src/library/utils/CommitDBUtils.swift
+++ src/library/utils/CommitDBUtils.swift
@@ -26,7 +26,7 @@ class CommitDBUtils {
             //Arrange the repos to make the algo faster
                 //ask each repo for what date is attached to commit nr 100  (we try to calc how fresh a repo is)
                     //if the repo doesnt have a commit nr  100, then use the farthest commit and get the date. then do some division to get a ratio nr to measure against
-        
+                    
                     
         //1. You loop the repos
         let repoXML = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)//~/Desktop/repo2.xml
