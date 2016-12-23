7a1e0885685f0db3b9bae34bd9388f8d02f28a10
Eonist <unicornhive@gmail.com>
2016-12-23 23:30:50 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -22,7 +22,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
             //runing NSTask on a background thread is now working, its a bit of a hassle to setup
                 //try to clean the setup process
                 //try retriving the commits from repo on a background thread
-                //
+                //try to make the speedy commit count method (combines rev-list and show->error etc)
             //try to speed test the retrival of commits from repo 🏀
                 //first with the freshness algo set manualy
                 //then do a speed test where the repo list is not optimally sorted
