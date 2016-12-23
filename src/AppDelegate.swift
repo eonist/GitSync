13880ba402d15a99d5a89cd9c8effc24402a3bba
Eonist <unicornhive@gmail.com>
2016-12-23 21:45:40 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -66,9 +66,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         Swift.print("run.before")
         let repoXML = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)//~/Desktop/repo2.xml
         repoList = XMLParser.toArray(repoXML)//or use dataProvider
-        repoList.forEach{
-            let localPath:String = $0["local-path"]!
-            run(localPath)
+        for (index,element) in repoList{
+            let localPath:String = element["local-path"]!
+            run(localPath,index)
         }
         Swift.print("run.after")
     }
@@ -81,45 +81,45 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         let taskQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)//swift 3-> let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
         dispatch_async(taskQueue, { () -> Void in
             //2. Creates a new Process object and assigns it to the TasksViewController‘s buildTask property. The launchPath property is the path to the executable you want to run. Assigns the BuildScript.command‘s path to the Process‘s launchPath, then assigns the arguments that were passed to runScript:to Process‘s arguments property. Process will pass the arguments to the executable, as though you had typed them into terminal.
-            tasks.append(NSTask())
+            self.tasks.append(NSTask())
             //let localPath = "~/_projects/_code/_active/swift/GitSyncOSX"
-            task.currentDirectoryPath = localPath
-            task.launchPath = "/bin/sh"
+            self.tasks[index].currentDirectoryPath = localPath
+            self.tasks[index].launchPath = "/bin/sh"
             let cmd:String = "git rev-list HEAD --count"
-            task.arguments = ["-c",cmd]//["echo", "hello world","  echo","again","&& echo again","\n echo again"]//["ls"]//"-c", "/usr/bin/killall Dock",
+            self.tasks[index].arguments = ["-c",cmd]//["echo", "hello world","  echo","again","&& echo again","\n echo again"]//["ls"]//"-c", "/usr/bin/killall Dock",
             
             //3.Process has a terminationHandler property that contains a block which is executed when the task is finished. This updates the UI to reflect that finished status as you did before.
-            task.terminationHandler = {
+            self.tasks[index].terminationHandler = {
                 task in
                 dispatch_async(dispatch_get_main_queue()) {
                     //Swift.print("it worked, back on main thread")
                     self.isRunning = false
                 }
             }
-            self.captureStandardOutputAndRouteToTextView(task)
+            self.captureStandardOutputAndRouteToTextView(index)
             
             //4.In order to run the task and execute the script, calls launch on the Process object. There are also methods to terminate, interrupt, suspend or resume an Process.
-            task.launch()
+            self.tasks[index].launch()
             
             //5.Calls waitUntilExit, which tells the Process object to block any further activity on the current thread until the task is complete. Remember, this code is running on a background thread. Your UI, which is running on the main thread, will still respond to user input.
-            task.waitUntilExit()
+            self.tasks[index].waitUntilExit()
         })
     }
     /**
      *
      */
-    func captureStandardOutputAndRouteToTextView(task:NSTask) {
+    func captureStandardOutputAndRouteToTextView(index:Int) {
         //1.//Creates an Pipe and attaches it to buildTask‘s standard output. Pipe is a class representing the same kind of pipe that you created in Terminal. Anything that is written to buildTask‘s stdout will be provided to this Pipe object.
-        let pipe = NSPipe()//we create a new pipe for each task
-        task.standardOutput = pipe
+        self.pipes.append(NSPipe())//we create a new pipe for each task
+        self.tasks[index].standardOutput = self.pipes[index]
         
         //2.the fileHandleForReading is used to read the data in the pipe, You call waitForDataInBackgroundAndNotify on it to use a separate background thread to check for available data.
-        pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()//
+        self.pipes[index].fileHandleForReading.waitForDataInBackgroundAndNotify()//
         
         //3.Whenever data is available, waitForDataInBackgroundAndNotify notifies you by calling the block of code you register with NSNotificationCenter to handle NSFileHandleDataAvailableNotification.
-        NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: pipe.fileHandleForReading, queue: nil){  notification -> Void in
+        NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: self.pipes[index].fileHandleForReading, queue: nil){  notification -> Void in
             //4. Inside your notification handler, gets the data as an NSData object and converts it to a string.
-            let output = pipe.fileHandleForReading.availableData
+            let output = self.pipes[index].fileHandleForReading.availableData
             let outputString:String = NSString(data:output, encoding:NSUTF8StringEncoding) as? String ?? ""/*decode the date to a string*/
             
             dispatch_async(dispatch_get_main_queue()) {//was->DispatchQueue.main.async(execute: {
@@ -129,7 +129,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         }
         
         //6.Finally, repeats the call to wait for data in the background. This creates a loop that will continually wait for available data, process that data, wait for available data, and so on.
-        pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()
+        self.pipes[index].fileHandleForReading.waitForDataInBackgroundAndNotify()
     }
     /**
      *
