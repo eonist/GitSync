36095af9975669e2e789a11a985564d2a1814cf7
Eonist <unicornhive@gmail.com>
2016-12-23 22:26:29 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -123,9 +123,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         self.pipes[index].fileHandleForReading.waitForDataInBackgroundAndNotify()//
         
         //3.Whenever data is available, waitForDataInBackgroundAndNotify notifies you by calling the block of code you register with NSNotificationCenter to handle NSFileHandleDataAvailableNotification.
-        NSNotificationCenter.defaultCenter().addObserverForName(NSFileHandleDataAvailableNotification, object: self.pipes[index].fileHandleForReading, queue: nil){  notification -> Void in
+        NSNotificationCenter.defaultCenter().addObserverForName(NSTaskDidTerminateNotification/*NSFileHandleDataAvailableNotification*/, object: self.pipes[index]/*.fileHandleForReading*/, queue: nil){  notification -> Void in
             //4. Inside your notification handler, gets the data as an NSData object and converts it to a string.
-            let output = self.pipes[index].fileHandleForReading.availableData
+            let output = self.pipes[index].fileHandleForReading.readDataToEndOfFile()/*fileHandleForReading.availableData*/
             let outputString:String = NSString(data:output, encoding:NSUTF8StringEncoding) as? String ?? ""/*decode the date to a string*/
             
             dispatch_async(dispatch_get_main_queue()) {//was->DispatchQueue.main.async(execute: {
