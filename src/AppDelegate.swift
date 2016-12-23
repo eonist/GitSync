b1a153d18030a06592555951a31297ea5950f30f
Eonist <unicornhive@gmail.com>
2016-12-23 21:41:42 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -49,9 +49,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         Swift.print("tick" + "\(abs(tickerDate!.timeIntervalSinceNow))")
         //timer!.timer!.fireDate
     }
-    //var pipes:[NSPipe] = []
+    var pipes:[NSPipe] = []
     //var pipe:NSPipe!
-    //var tasks:[NSTask] = []
+    var tasks:[NSTask] = []
     //var task:NSTask!
     var startTime:NSDate?
     dynamic var isRunning = false
@@ -81,7 +81,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         let taskQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)//swift 3-> let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
         dispatch_async(taskQueue, { () -> Void in
             //2. Creates a new Process object and assigns it to the TasksViewController‘s buildTask property. The launchPath property is the path to the executable you want to run. Assigns the BuildScript.command‘s path to the Process‘s launchPath, then assigns the arguments that were passed to runScript:to Process‘s arguments property. Process will pass the arguments to the executable, as though you had typed them into terminal.
-            let task = NSTask()
+            tasks.append(NSTask())
             //let localPath = "~/_projects/_code/_active/swift/GitSyncOSX"
             task.currentDirectoryPath = localPath
             task.launchPath = "/bin/sh"
