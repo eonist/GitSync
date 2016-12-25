6ce84557b4b508eb2efd070bf2bcb5551c6a0e2b
Eonist <unicornhive@gmail.com>
2016-12-25 20:58:48 +0100

    Files modified: 1
    Modified 1 file:src/test/ASyncTaskTest.swift

--- src/test/ASyncTaskTest.swift
+++ src/test/ASyncTaskTest.swift
@@ -15,8 +15,33 @@ class ASyncTaskTest {
     init(){
         
     }
+    /**
+     * NOTE: task.waitUntilExit() //is only needed if we stream data
+     */
     func run(localPath:String,_ title:String,_ task:NSTask, _ pipe:NSPipe){
-        
+        let taskQueue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0)//swift 3-> let taskQueue = DispatchQueue.global(qos: DispatchQoS.QoSClass.background)
+        dispatch_async(taskQueue, { () -> Void in
+            //2. Creates a new Process object and assigns it to the TasksViewController‘s buildTask property. The launchPath property is the path to the executable you want to run. Assigns the BuildScript.command‘s path to the Process‘s launchPath, then assigns the arguments that were passed to runScript:to Process‘s arguments property. Process will pass the arguments to the executable, as though you had typed them into terminal.
+            //self.tasks.append(NSTask())
+            //Swift.print(title + " launched")
+            //let localPath = "~/_projects/_code/_active/swift/GitSyncOSX"
+            task.currentDirectoryPath = localPath
+            task.launchPath = "/bin/sh"
+            let cmd:String = "git rev-list HEAD --count"
+            task.arguments = ["-c",cmd]//["echo", "hello world","  echo","again","&& echo again","\n echo again"]//["ls"]//"-c", "/usr/bin/killall Dock",
+            
+            //3.Process has a terminationHandler property that contains a block which is executed when the task is finished. This updates the UI to reflect that finished status as you did before.
+            
+            //this wont work, the NSNOtification will sometimes never complete
+            task.terminationHandler = { task in/*Avoid using NSNotification if you use this callback, as it will block NSNotification from fireing sometimes*/
+                let data:NSData = pipe.fileHandleForReading.readDataToEndOfFile()/*retrive the date from the nstask output*/
+                let output:String = NSString(data:data, encoding:NSUTF8StringEncoding) as! String/*decode the date to a string*/
+                Swift.print("completed " + "output.count: " + "\(output.trim("\n"))")
+            }
+            task.standardOutput = pipe//1.//Creates an Pipe and attaches it to buildTask‘s standard output. Pipe is a class representing the same kind of pipe that you created in Terminal. Anything that is written to buildTask‘s stdout will be provided to this Pipe object.
+            pipe.fileHandleForReading.waitForDataInBackgroundAndNotify()//2.the fileHandleForReading is used to read the data in the pipe, You call waitForDataInBackgroundAndNotify on it to use a separate background thread to check for available data.
+            task.launch()/*In order to run the task and execute the script, calls launch on the Process object. There are also methods to terminate, interrupt, suspend or resume an Process.*/
+        })
     }
 }
 
