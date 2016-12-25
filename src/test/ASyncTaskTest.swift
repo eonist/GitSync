154f84326186e757faca0ed27dc6d9d9a74f4dfb
Eonist <unicornhive@gmail.com>
2016-12-25 15:24:56 +0100

    Files modified: 1
    Modified 1 file:src/test/ASyncTaskTest.swift

--- src/test/ASyncTaskTest.swift
+++ src/test/ASyncTaskTest.swift
@@ -9,7 +9,7 @@ class ASyncTaskTest {
      * Testing running an NSTask on a background thread
      * 1. Create, NSTask,NSPipe,LocalPath, Command and run the code
      * 2. Attaches the task to a concurrent background-thread (spins up many cores at once)
-     * 3. Completion callback on the main thread
+     * 3. Completion callback on the main thread -> (NSTask,Output)
      */
     init(){
         
