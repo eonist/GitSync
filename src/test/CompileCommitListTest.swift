25577c339d8777cf92dda178e8a277a341128c80
Eonist <unicornhive@gmail.com>
2016-12-26 13:53:10 +0100

    Files modified: 1
    Modified 1 file:src/test/CompileCommitListTest.swift

--- src/test/CompileCommitListTest.swift
+++ src/test/CompileCommitListTest.swift
@@ -6,10 +6,12 @@ class CompileCommitListTest {
      */
     func refresh(){
         //sort repos by freshness: (makes the process of populating CommitsDB much faster)
+        //we run the sorting algo on a bg thread as serial work (one by one)
         
         
         //get the 100 last commits from every repo:
-            //for each repo
+            //we populate the CommitsDB on a bg thread
+            //for each repo in sortedRepos (aka sorted by freshness)
                 //get commit count
                 //retrive only commits that are newer than the most distante time in the CommitsDB 
         
