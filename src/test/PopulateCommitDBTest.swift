9c35327c70e103de85f6eb38b3ad8b74e8ab111f
Eonist <unicornhive@gmail.com>
2016-12-26 14:54:06 +0100

    Files modified: 1
    Modified 1 file:src/test/PopulateCommitDBTest.swift

--- src/test/PopulateCommitDBTest.swift
+++ src/test/PopulateCommitDBTest.swift
@@ -54,6 +54,45 @@ class PopulateCommitDB {
     /**
      *
      */
+    func refreshRepo(index:Int,_ element:[String:String]){
+        repoIndex += 1//increment the repoIndex
+        let localPath:String = element["local-path"]!//local-path to repo
+        let repoTitle = element["title"]!//name of repo
+        //2. Find the range of commits to add to CommitDB for this repo
+        var commitCount:Int
+        //Swift.print("commitDB.sortedArr.count: " + "\(commitDB.sortedArr.count)")
+        if(commitDB.sortedArr.count >= 100){
+            let firstDate = commitDB.sortedArr.first!.sortableDate
+            //Swift.print("firstDate: " + "\(firstDate)")
+            let gitTime = GitDateUtils.gitTime(firstDate.string)
+            let rangeCount:Int = GitUtils.commitCount(localPath, after: gitTime).int//now..lastDate
+            commitCount = rangeCount > 100 ? 100 : rangeCount//force the value to be no more than max allowed
+        }else {//< 100
+            commitCount = 100 - commitDB.sortedArr.count
+        }
+        Swift.print("\(repoTitle): rangeCount: " + "\(commitCount)")
+        //3. Retrieve the commit log items for this repo with the range specified
+        //Swift.print("max: " + "\(commitCount)")
+        let args:[String] = CommitViewUtils.commitItems(localPath,commitCount)/*creates an array of arguments that will return commit item logs*/
+        if(args.count > 0){
+            operations = []//reset the operations array
+            for (_,element) in args.enumerate(){
+                let operation = CommitViewUtils.configOperation([element],localPath,repoTitle,index)/*setup the NSTask correctly*/
+                operations.append(operation)
+            }
+            
+            let finalTask = operations[operations.count-1].task/*We listen to the last task for completion*/
+            NSNotificationCenter.defaultCenter().addObserverForName(NSTaskDidTerminateNotification, object: finalTask, queue: nil, usingBlock:observer)/*{ notification in})*/
+            operations.forEach{/*launch all tasks*/
+                $0.task.launch()
+            }
+        }else{//no operations to launch and observe
+            iterate()//but we still need to iterate
+        }
+    }
+    /**
+     *
+     */
     func onFreshnessSortComplete(){
         Swift.print("onFreshnessSortComplete")
         sortableRepoList.forEach{Swift.print($0.repo["title"])}
