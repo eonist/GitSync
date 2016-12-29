8487befd35749cf24066aba7fa0988ba40f1f887
Eonist <unicornhive@gmail.com>
2016-12-29 18:42:15 +0100

    Files modified: 1
    Modified 1 file:src/test/ListTransitionTestWin.swift

--- src/test/ListTransitionTestWin.swift
+++ src/test/ListTransitionTestWin.swift
@@ -1,5 +1,33 @@
 import Foundation
 
-class ListTransitionTestWin {
+class ListTransitionTestWin:Window {
+    required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
+        super.init(docWidth, docHeight)
+        WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
+    }
+    override func resolveSkin() {
+        Swift.print("frame.width: " + "\(frame.width)")
+        Swift.print("frame.height: " + "\(frame.height)")
+        self.contentView = ListTransitionTestView(frame.width,frame.height)
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+import Foundation
 
+class ListTransitionTestView:TitleView{
+       override init(_ width:CGFloat, _ height:CGFloat, _ parent:IElement? = nil, _ id:String? = "") {
+        //self.title = "Resolve merge conflict:"//Title: Resolve sync conflict:
+        super.init(width, height, parent, "listTransitionTestView")
+    }
+    override func resolveSkin() {
+        Swift.print("ListTransitionTestView.resolveSkin()")
+        super.resolveSkin()
+        Swift.print(ElementParser.stackString(self))
+        createGUI()
+    }
+    func createGUI(){
+        //List goes here
+        
+    }
+    required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
