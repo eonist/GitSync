8d30d3d2637fd2c81874b920b3cbf49f869bdb3c
Eonist <unicornhive@gmail.com>
2017-01-21 00:34:51 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -1,6 +1,6 @@
-import Foundation
+import Cocoa
 
-class BarGraph {
+class BarGraph:NSView {
     //Continue here:
         //Extract the gesture out of CommitGraph
         //override createGraph
@@ -11,6 +11,13 @@ class BarGraph {
      *
      */
     func helloWorld(){
+        let style = Style("Button")
+        style.addStyleProperty(StyleProperty("fill",NSColor.blue))
+        let btn = addSubView(Button(96,24))
         
+        func onClick(event:Event){
+            Swift.print("Hello world")
+        }
+        btn.event = onClick
     }
 }
