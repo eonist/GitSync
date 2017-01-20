01bc802d462e836a2d094a5ee9a66d637f728ad6
Eonist <unicornhive@gmail.com>
2017-01-21 00:25:38 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -1,5 +1,8 @@
 import Foundation
 
 class BarGraph {
-    
+    //Continue here:
+        //Extract the gesture out of CommitGraph
+        //create dummy methods with sudo code that calcs the bars and draws them etc
+        //test it
 }
