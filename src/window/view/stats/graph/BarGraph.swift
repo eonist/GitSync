b7cca9ae12147bd2299dbb921b8e34483dabf51a
Eonist <unicornhive@gmail.com>
2017-01-21 00:38:43 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -7,17 +7,5 @@ class BarGraph:NSView {
         //create dummy methods with sudo code that calcs the bars and draws them etc
         //test it
         //create the touch point visualisations
-    /**
-     *
-     */
-    func helloWorld(){
-        let style = Style("Button")
-        style.addStyleProperty(StyleProperty("fill",NSColor.blue))
-        let btn = addSubView(Button(96,24))
-        
-        func onClick(event:Event){
-            Swift.print("Hello world")
-        }
-        btn.event = onClick
-    }
+    
 }
