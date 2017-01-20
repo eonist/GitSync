53eda8de1fc9035cfefdfe735988df23bcf26057
Eonist <unicornhive@gmail.com>
2017-01-21 00:29:19 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -3,6 +3,8 @@ import Foundation
 class BarGraph {
     //Continue here:
         //Extract the gesture out of CommitGraph
+        //override createGraph
         //create dummy methods with sudo code that calcs the bars and draws them etc
         //test it
+        //create the touch point visualisations
 }
