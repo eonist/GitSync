e3ef3170274eeda2964c068d39de58bdd3f1415b
Eonist <unicornhive@gmail.com>
2017-01-21 17:04:10 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -15,11 +15,9 @@ class BarGraph:Graph {
     func createBarGraph(_ size:CGSize,_ graphPts:[CGPoint]){
         //graphArea?.addSubview()
         graphPts.forEach{
-            let bar:Bar = graphArea!.addSubView(Bar(NaN,NaN,graphArea,"graphPoint"))
+            let bar:Bar = graphArea!.addSubView(Bar(NaN,size.height,graphArea))
             bars.append(bar)
             bar.setPosition($0)
-            //style the button similar to VolumSlider knob (with a blue center, a shadow and white border, test different designs)
-            //set the size as 12px and offset to -6 (so that its centered)
         }
     }
 }
