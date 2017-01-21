b253dc08db04c4b55b97f6b99934aca0c81eb9a5
Eonist <unicornhive@gmail.com>
2017-01-21 14:29:38 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/CommitGraph.swift

--- src/window/view/stats/graph/CommitGraph.swift
+++ src/window/view/stats/graph/CommitGraph.swift
@@ -62,23 +62,23 @@ class CommitGraph:Graph{
         updateDateText()
     }
     /**
-	 * Interpolates
+	 * Interpolates between 0 and 1 while the duration of the animation
 	 * NOTE: ReCalc the hValue indicators (each week has a different max hValue etc)
 	 */
     func interpolateValue(_ val:CGFloat){
         Swift.print("interpolateValue() val: " + "\(val)")
         var positions:[CGPoint] = []
         for i in 0..<graphPts.count{
-            let pos:CGPoint = initGraphPts[i].interpolate(graphPts[i], val)
+            let pos:CGPoint = initGraphPts[i].interpolate(graphPts[i], val)/*interpolates from one point to another*/
             positions.append(pos)
-            graphPoints[i].setPosition(pos)
+            graphPoints[i].setPosition(pos)//moves the points
         }
         /*GraphLine*/
         let path:IPath = PolyLineGraphicUtils.path(positions)/*convert points to a Path*/
         //TODO: Ideally we should create the CGPath from the points use CGPathParser.polyline
 		let cgPath = CGPathUtils.compile(CGMutablePath(), path)//convert path to cgPath
-        graphLine!.line!.cgPath = cgPath.clone()
-        graphLine!.line!.draw()
+        graphLine!.line!.cgPath = cgPath.clone()//applies the new path
+        graphLine!.line!.draw()//draws the path
     }
     var graphPts:[CGPoint] = []
     var initGraphPts:[CGPoint] = []/*animates from these points*/
