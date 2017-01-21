06af9a15a11ce0e25b818bbe891b1ef14e84fd9b
Eonist <unicornhive@gmail.com>
2017-01-21 18:13:26 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -26,7 +26,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
          /**/
     }
     func initTestWin(){
-        StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
+        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
         win = ListTransitionTestWin(600,400)/*Debugging Different List components*/
         
         /*let url:String = "~/Desktop/ElCapitan/explorer.css"
