92463fdd5af8eb807e6e8adb549b242eb96cf921
Eonist <unicornhive@gmail.com>
2017-01-22 20:13:13 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/CommitGraph.swift

--- src/window/view/stats/graph/CommitGraph.swift
+++ src/window/view/stats/graph/CommitGraph.swift
@@ -160,13 +160,13 @@ class GestureUtils{
      */
     static func twoFingersTouches(_ view:NSView, _ event:NSEvent)->[String:NSTouch]?{
         var twoFingersTouches:[String:NSTouch]? = nil//NSMutualDictionary was used before and didn't require casting id to string, revert if sideeffects manifest
-        if(event.type == NSEventType.gesture){//was NSEventTypeGesture, could maybe be: EventTypeBeginGesture
-            let touches:NSSet = event.touches(matching: NSTouchPhase.any, in: view) as NSSet //touchesMatchingPhase:NSTouchPhaseAny inView:self
+        if(event.type == NSEventType.gesture){// could maybe be: EventTypeBeginGesture
+            let touches:Set<NSTouch> = event.touches(matching:NSTouchPhase.any, in: view)  //touchesMatchingPhase:NSTouchPhaseAny inView:self
             if(touches.count == 2){
                 twoFingersTouches = [String:NSTouch]()
                 for touch in touches {//
                     //Swift.print("id: "+"\((touch as! NSTouch).identity)")
-                    twoFingersTouches!["\((touch as! NSTouch).identity)"] = touch as? NSTouch/*assigns each touch to the identity of the same touch*///was [ setObject: forKey:];
+                    twoFingersTouches!["\((touch).identity)"] = touch/*assigns each touch to the identity of the same touch*///was [ setObject: forKey:];
                 }
             }
         }
