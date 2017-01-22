131fc99e3ea08610819b4ef56b71652c687c1bb1
Eonist <unicornhive@gmail.com>
2017-01-22 21:59:52 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -109,7 +109,7 @@ class BarGraph:Graph {
             let id:String = "\(touch.identity)"
             let pos:CGPoint = event.localPos(self) - CGPoint(20,20)//touch.normalizedPosition
             //Swift.print("pos: " + "\(pos)")
-            let touchPos:CGPoint = touch.normalizedPosition
+            let touchPos:CGPoint = (touch.normalizedPosition * CGPoint(1,-1)) + CGPoint(0,1)//flip the touch coordinates
             
             Swift.print("touchPos: " + "\(touchPos)")
             
