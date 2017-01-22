b95cda13cf19692bc3cbf9d99da87c528e59b559
Eonist <unicornhive@gmail.com>
2017-01-22 13:36:14 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/BarGraph.swift

--- src/window/view/stats/graph/BarGraph.swift
+++ src/window/view/stats/graph/BarGraph.swift
@@ -58,7 +58,7 @@ class BarGraph:Graph {
         
         //initGraphPts = self.graphPts.map{$0}//grabs the location of where the pts are now
         //self.graphPts = GraphUtils.points(newSize!, newPostition!, spacing!, vValues, maxValue)
-        graphPts = GraphUtils.points(newSize!, newPostition!, spacing!, vValues, maxValue)
+        graphPts = GraphUtils.points(newSize!, newPosition!, spacing!, vValues, maxValue)
         initGraphPts = graphPoints.map{$0.frame.origin}//grabs the location of where the pts are now
         
         Swift.print("initGraphPts: " + "\(initGraphPts)")
