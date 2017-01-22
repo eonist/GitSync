fad883f2d83583dbf044e86c815fd0b149970116
Eonist <unicornhive@gmail.com>
2017-01-22 18:34:28 +0100

    Files modified: 1
    Modified 1 file:src/window/view/stats/graph/CommitGraph.swift

--- src/window/view/stats/graph/CommitGraph.swift
+++ src/window/view/stats/graph/CommitGraph.swift
@@ -156,8 +156,8 @@ class GestureUtils{
     /**
      * To avoid duplicate code we could extract the content of this method into an GestureUtils method. Return nil if there isnt 2 touches and set the array only if != nil
      */
-    static func twoFingersTouches(_ view:NSView, _ event:NSEvent)->NSMutableDictionary?{
-        var twoFingersTouches:NSMutableDictionary? = nil
+    static func twoFingersTouches(_ view:NSView, _ event:NSEvent)->[String:NSTouch]?{
+        var twoFingersTouches:[String:NSTouch]? = nil
         if(event.type == NSEventType.gesture){//was NSEventTypeGesture, could maybe be: EventTypeBeginGesture
             let touches:NSSet = event.touches(matching: NSTouchPhase.any, in: view) as NSSet //touchesMatchingPhase:NSTouchPhaseAny inView:self
             if(touches.count == 2){
