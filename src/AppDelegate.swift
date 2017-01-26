7a5189c01a8c577dbe6e2756873f5113917a0f34
Eonist <unicornhive@gmail.com>
2017-01-26 14:08:45 +0100

    Files modified: 1
    Modified 1 file:src/AppDelegate.swift

--- src/AppDelegate.swift
+++ src/AppDelegate.swift
@@ -13,7 +13,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var timer:SimpleTimer?
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-         Swift.print("GitSync - Simple git automation for macOS")
+         Swift.print("GitSync - The futrure is automated")//Simple git automation for macOS
          NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
          
          //_ = Test()
