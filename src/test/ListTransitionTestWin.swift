94fae7f0c8a0f0c6739bcfe5e15d5c2b33fb36fb
Eonist <unicornhive@gmail.com>
2017-01-27 15:33:49 +0100

    Files modified: 1
    Modified 1 file:src/test/ListTransitionTestWin.swift

--- src/test/ListTransitionTestWin.swift
+++ src/test/ListTransitionTestWin.swift
@@ -57,7 +57,7 @@ class ListTransitionTestView:TitleView{
         func onButtonDown(event:Event){
             if(event.type == ButtonEvent.upInside){
                 Swift.print("click")
-                animator = Animator(Animation.sharedInstance,0.5,0,300,progress,Easing.back.easeOutBack)
+                animator = Animator(Animation.sharedInstance,0.5,0,300,progress,Back.easeOutBack)
                 animator!.start()
             }
         }
