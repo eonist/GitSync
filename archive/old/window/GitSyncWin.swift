3e14ace726d65465091bccd311d7204a23be533b
Eonist <unicornhive@gmail.com>
2017-02-08 13:52:15 +0100

    Files deleted: 52
    Deleted 52 files:archive/font_awesome_icons_to_be_considered/add-square-button.svgarchive/font_awesome_icons_to_be_considered/bar-graph-on-a-rectangle.svgarchive/font_awesome_icons_to_be_considered/book.svgarchive/font_awesome_icons_to_be_considered/cog-wheel-silhouette.svgarchive/font_awesome_icons_to_be_considered/font-selection-editor.svgarchive/font_awesome_icons_to_be_considered/inbox.svgarchive/font_awesome_icons_to_be_considered/listing-option.svgarchive/font_awesome_icons_to_be_considered/minus-sign-inside-a-black-rounded-square-shape.svgarchive/font_awesome_icons_to_be_considered/minus-sign-on-a-square-outline.svgarchive/font_awesome_icons_to_be_considered/open-folder.svgarchive/font_awesome_icons_to_be_considered/pencil.svgarchive/font_awesome_icons_to_be_considered/plus-symbol-in-a-rounded-black-square.svgarchive/font_awesome_icons_to_be_considered/settings.svgarchive/font_awesome_icons_to_be_considered/text-file-1.svgarchive/material_icons_to_be_considered/bars.svgarchive/material_icons_to_be_considered/chat.svgarchive/material_icons_to_be_considered/cog.svgarchive/material_icons_to_be_considered/home.svgarchive/material_icons_to_be_considered/ic_chat_black_24px.svgarchive/material_icons_to_be_considered/ic_delete_black_24px.svgarchive/material_icons_to_be_considered/ic_home_black_24px.svgarchive/material_icons_to_be_considered/ic_inbox_black_24px.svgarchive/material_icons_to_be_considered/ic_insert_chart_black_24px.svgarchive/material_icons_to_be_considered/ic_settings_black_24px.svgarchive/material_icons_to_be_considered/ic_subject_black_24px.svgarchive/material_icons_to_be_considered/list.svgarchive/material_icons_to_be_considered/spacerocket.svgarchive/material_icons_to_be_considered/wrench.svgarchive/old/AppDelegate.swiftarchive/old/utils/ui/DEPRECATEDNodeList.swiftarchive/old/window/GitSyncWin.swiftarchive/old/window/TranslucencyWin.swiftarchive/old/window/view/ContentView.swiftarchive/old/window/view/LeftSideBar.swiftarchive/old/window/view/MainView.swiftarchive/old/window/view/TranslucencyView.swiftarchive/old/window/view/activity/ActivityTopBar.swiftarchive/old/window/view/activity/ActivityView.swiftarchive/old/window/view/activity/list/ActivityList.swiftarchive/old/window/view/activity/list/ActivityListItem.swiftarchive/old/window/view/repo/item/RepoDetailView.swiftarchive/old/window/view/repo/item/RepoItemTopBar.swiftarchive/old/window/view/repo/list/RepoView.swiftarchive/old/window/view/repo/list/TopBar.swiftarchive/try_these_sounds/add.wavarchive/try_these_sounds/delete.wavarchive/try_these_sounds/disable.wavarchive/try_these_sounds/done.wavarchive/try_these_sounds/enable.wavarchive/try_these_sounds/error.wavarchive/try_these_sounds/play.wavarchive/try_these_sounds/startup.wav

--- archive/old/window/GitSyncWin.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-class GitSyncWin:Window {
-    override func resolveSkin() {
-//      Swift.print("frame.width: " + "\(frame.width)")
-//      Swift.print("frame.height: " + "\(frame.height)")
-        self.contentView = MainView(frame.width,frame.height)/*Sets the mainview of the window*/
-    }
-}
