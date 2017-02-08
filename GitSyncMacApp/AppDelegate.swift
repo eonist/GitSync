05f2031a3d089035970d1270ca3eb67b7a95f450
Eonist <unicornhive@gmail.com>
2017-02-08 13:57:10 +0100

    New files added: 38, Files modified: 1, Files deleted: 40
    Deleted 40 files:GitSyncMac/applet.icnssrc/AppDelegate.swiftsrc/assets/Sounds.swiftsrc/library/Commit.swiftsrc/library/CommitDB.swiftsrc/library/utils/CommitDBCache.swiftsrc/library/utils/CommitDBRefreshTest.swiftsrc/manager/Navigation.swiftsrc/test/ASyncTaskTest.swiftsrc/test/ListTransitionTestWin.swiftsrc/test/PopulateCommitDBTest.swiftsrc/test/Test.swiftsrc/test/Test2.swiftsrc/test/ThreadTesting.swiftsrc/utils/CommitUtils.swiftsrc/utils/DescUtils.swiftsrc/utils/GitSync.swiftsrc/utils/GitSyncUtils.swiftsrc/utils/MergeUtils.swiftsrc/utils/RepoUtils.swiftsrc/utils/StatusUtils.swiftsrc/window/CommitDialogWin.swiftsrc/window/ConflictDialogWin.swiftsrc/window/MainWin.swiftsrc/window/view/CommitDialogView.swiftsrc/window/view/ConflictDialogView.swiftsrc/window/view/MainView.swiftsrc/window/view/commits/CommitsView.swiftsrc/window/view/commits/detail/CommitDetailView.swiftsrc/window/view/commits/list/CommitsList.swiftsrc/window/view/commits/list/CommitsListItem.swiftsrc/window/view/commits/utils/CommitViewUtils.swiftsrc/window/view/debug/DebugView.swiftsrc/window/view/debug/TestView.swiftsrc/window/view/menu/MenuView.swiftsrc/window/view/prefs/PrefsView.swiftsrc/window/view/repo/RepoView.swiftsrc/window/view/repo/detail/RepoDetailView.swiftsrc/window/view/stats/StatsView.swiftsrc/window/view/stats/graph/CommitGraph.swiftModified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -1,36 +1,72 @@
+import Cocoa
 @testable import Utils
 @testable import Element
-
-import Cocoa
-
+/**
+ * This is the main class for the application
+ * TODO: An idea is to hide parts of the interface when the mouse is not over the app (anim in and out) (maybe)
+ */
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-    @IBOutlet weak var window: NSWindow!
-
-
+    weak var window: NSWindow!
+    var repoFilePath:String = "~/Desktop/repo.xml"
+    var win:NSWindow?/*<--The window must be a class variable, local variables doesn't work*/
+    var fileWatcher:FileWatcher?
+    var test:PopulateCommitDB?
+    var timer:SimpleTimer?
+    
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
-        Swift.print(StringParser.sansSuffix("Blue"))
+         Swift.print("GitSync - The future is automated")//Simple git automation for macOS, The autonomouse git client
+         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
+         
+         //_ = Test()
+         //initApp()
+         initTestWin()
+         //_ = PopulateCommitDB()
         
-        //continue here: 
-            //copy the GitSync code into the project
-            //start making debug rects for testing your new fastlit theory
+        print("test")
+    }
+    func initTestWin(){
+        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)
+        win = ListTransitionTestWin(600,400)/*Debugging Different List components*/
         
-        
-        StyleManager.addStyle("Button{fill:blue;}")
-        let btn = Button(100,20)
-        window.contentView!.addSubview(btn)
-        func onClick(event:Event){
-            if(event.type == ButtonEvent.upInside){Swift.print("hello world")}
+        let url:String = "~/Desktop/ElCapitan/"
+        fileWatcher = FileWatcher([url.tildePath])
+        fileWatcher!.event = { event in
+            //Swift.print(self)
+            if(event.fileChange && FilePathParser.fileExtension(event.path) == "css") {//assert for .css file changes, so that .ds etc doesnt trigger events etc
+                Swift.print(event.description)
+                Swift.print("update to the file happened: " + "\(event.path)")
+                StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
+                let view:NSView = self.win!.contentView!//MainWin.mainView!
+                ElementModifier.refreshSkin(view as! IElement)
+                ElementModifier.floatChildren(view)
+            }
         }
-        btn.event = onClick
+        fileWatcher!.start()
     }
-
+    func initApp(){
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)//<--toggle this bool for live refresh
+        
+        win = MainWin(MainView.w,MainView.h)
+        //win = ConflictDialogWin(380,400)
+        //win = CommitDialogWin(400,356)
+        
+    }
     func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+        //store the app prefs
+        if(PrefsView.keychainUserName != nil){//make sure the data has been read and written to first
+            let xml:XML = "<prefs></prefs>".xml
+            xml.appendChild("<keychainUserName>\(PrefsView.keychainUserName!)</keychainUserName>".xml)
+            xml.appendChild("<gitConfigUserName>\(PrefsView.gitConfigUserName!)</gitConfigUserName>".xml)
+            xml.appendChild("<gitEmailName>\(PrefsView.gitEmailNameText!)</gitEmailName>".xml)
+            xml.appendChild("<uiSounds>\(String(PrefsView.uiSounds!))</uiSounds>".xml)
+            _ = FileModifier.write("~/Desktop/gitsyncprefs.xml".tildePath, xml.xmlString)
+        }
+        //store the repo xml
+        if(RepoView.dp != nil){//make sure the data has been read and written to first
+            _ = FileModifier.write("~/Desktop/assets/xml/list.xml".tildePath, RepoView.dp!.xml.xmlString)
+        }
+        print("Good-bye")
     }
-
-
 }
-
