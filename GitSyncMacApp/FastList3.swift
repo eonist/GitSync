16d583c25cc6b5df9f90d662f6b87cba66c78185
Eonist <unicornhive@gmail.com>
2017-02-10 13:26:43 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList3.swift

--- GitSyncMacApp/FastList3.swift
+++ GitSyncMacApp/FastList3.swift
@@ -176,6 +176,7 @@ class FastList3:Element,IList{
         Swift.print("range.start: " + "\(range.start)")
         if(range.start >= firstPoolIdx && range.start <= lastPoolIdx){//within
             let mergableRange = range.start..<lastPoolIdx
+            Swift.print("mergableRange: " + "\(mergableRange)")
             for i in mergableRange{/*For loop because the act of adding an item doesnt require shuffling from top to bottoom or bottom to top*/
                 Swift.print("reuse: i: \(i)")
                 let item:FastListItem = pool[i]
