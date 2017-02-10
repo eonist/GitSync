3a868c064a121e1470b4d36b22d6934799d56bc2
Eonist <unicornhive@gmail.com>
2017-02-10 19:02:38 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList3.swift

--- GitSyncMacApp/FastList3.swift
+++ GitSyncMacApp/FastList3.swift
@@ -184,6 +184,8 @@ class FastList3:Element,IList{
                 
                 //continue here. 
                     //keep testing and figure out bugs etc
+                    //more debugging tools == more understanding
+                    //use indecies in the text in the items 0. blue, etc
             }
         }
     }
