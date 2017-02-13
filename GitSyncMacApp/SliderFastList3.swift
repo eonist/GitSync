3d32ea55f18dee494f240aa2b333e7d70ae95c7d
Eonist <unicornhive@gmail.com>
2017-02-13 11:19:40 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/SliderFastList3.swift

--- GitSyncMacApp/SliderFastList3.swift
+++ GitSyncMacApp/SliderFastList3.swift
@@ -31,15 +31,14 @@ class SliderFastList3:FastList3,ISliderList {
     }
     override func onDataProviderEvent(_ event: DataProviderEvent) {
         super.onDataProviderEvent(event)
-        if(event.type == DataProviderEvent.add){
-            
-            /*Updates the slider interval and the sliderThumbSize*/
-            sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight
-            let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height/*<--this should probably be .getHeight()*/);
-            slider!.setThumbHeightValue(thumbHeight)
-            let progress:CGFloat = SliderParser.progress(lableContainer!.y, height, itemsHeight)//TODO: use getHeight() instead of height
-            slider!.setProgressValue(progress)
-        }
+        //if(event.type == DataProviderEvent.add){}
+        
+        /*Updates the slider interval and the sliderThumbSize*/
+        sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height/*<--this should probably be .getHeight()*/);
+        slider!.setThumbHeightValue(thumbHeight)
+        let progress:CGFloat = SliderParser.progress(lableContainer!.y, height, itemsHeight)//TODO: use getHeight() instead of height
+        slider!.setProgressValue(progress)
         
         //TODO: Add hide slider asssert here see SliderList for implementation
     }
