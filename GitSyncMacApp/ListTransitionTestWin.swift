a4a90273582d29cf972e052ea5eb6ec02bdf5f16
Eonist <unicornhive@gmail.com>
2017-02-13 16:46:10 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/ListTransitionTestWin.swift

--- GitSyncMacApp/ListTransitionTestWin.swift
+++ GitSyncMacApp/ListTransitionTestWin.swift
@@ -149,7 +149,7 @@ class ListTransitionTestView:TitleView{
                 }else if(event.origin === removeBtnTop){
                     _ = list!.dataProvider.removeItemAt(0)
                 }else if(event.origin === removeBtnBottom){
-                    _ = list!.dataProvider.removeItemAt(list!.dataProvider.count)
+                    _ = list!.dataProvider.removeItemAt(list!.dataProvider.count-1)
                 }
             }
         }
