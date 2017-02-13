bbac050488c260bd9d2f54758d4252765ef4764f
Eonist <unicornhive@gmail.com>
2017-02-13 15:45:25 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList3.swift

--- GitSyncMacApp/FastList3.swift
+++ GitSyncMacApp/FastList3.swift
@@ -130,7 +130,7 @@ class FastList3:Element,IList{
         Swift.print("👉 updatePool  dp.count:  \(dp.count) pool.count:  \(pool.count)")
         let itemsToFillHeight:Int = floor(height / itemHeight).int + 1
         Swift.print("itemsToFillHeight: " + "\(itemsToFillHeight)")
-        if(dp.count > pool.count && dp.count < itemsToFillHeight){
+        if(dp.count > pool.count){
             
             let min:Int = Swift.min(dp.count,itemsToFillHeight)
             let numOfItemsNeeded =  min - pool.count
