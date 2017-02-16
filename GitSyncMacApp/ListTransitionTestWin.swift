78483fb4ba4b3b622c22e1c45ada4c712e462c1d
Eonist <unicornhive@gmail.com>
2017-02-16 14:01:15 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/ListTransitionTestWin.swift

--- GitSyncMacApp/ListTransitionTestWin.swift
+++ GitSyncMacApp/ListTransitionTestWin.swift
@@ -32,7 +32,7 @@ class ListTransitionTestView:TitleView{
         //animTest()
         //uiSwitchTest()
         //barGraphTest()
-        rbSliderFastList3()
+        //rbSliderFastList3()
         //sliderFastList3()
         //fastList3()
         //dpTests()
@@ -43,6 +43,23 @@ class ListTransitionTestView:TitleView{
         //fastList()
         //sliderList()
         //list()
+        
+        /**
+         *
+         */
+        func difference<T>(_ a:Range<T>, _ b:Range<T>) -> (Range<T>?,Range<T>?){
+            let intersection = RangeParser.intersection(a,b)
+            if(intersection != nil){
+                let range1 = Swift.min(a.start,b.start)..<intersection!.start
+                let range2 = intersection!.end..<Swift.max(a.end,b.end)
+                return (range1,range2)
+            }
+            return (nil,nil)
+        }
+        
+        let diff = difference(0..<4, 2..<7)
+        Swift.print("diff.0: " + "\(diff.0)")
+        
     }
     
     var animator:Animator?
