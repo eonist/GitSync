6fb93bb51baabd5265f9dbecd8571db7e90ac11e
Eonist <unicornhive@gmail.com>
2017-02-16 12:25:50 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList4.swift

--- GitSyncMacApp/FastList4.swift
+++ GitSyncMacApp/FastList4.swift
@@ -39,7 +39,7 @@ class FastList4:Element,IList {
         let intersectingItems:[FastListItem] = []
         let firstOldIdx:Int = pool.first!.idx
         for i in intersection!.start...intersection!.end{
-            
+            ArrayModifier.removeRange(&<#T##array: Array<T>##Array<T>#>, <#T##rangeStart: Int##Int#>, <#T##rangeEnd: Int##Int#>)
         }
         
         
