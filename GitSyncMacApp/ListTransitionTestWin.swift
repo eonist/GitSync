ecfa10ef2ccc36de24859289f7fdadad8e80ace6
Eonist <unicornhive@gmail.com>
2017-02-16 16:35:45 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/ListTransitionTestWin.swift

--- GitSyncMacApp/ListTransitionTestWin.swift
+++ GitSyncMacApp/ListTransitionTestWin.swift
@@ -94,7 +94,7 @@ class ListTransitionTestView:TitleView{
         let graph = graphContainer.addSubView(BarGraph(200,200/*,4*/,graphContainer))
         _ = graph
     }
-    var list:SliderList?
+    var list:FastList4?
     /*Add*/
     var btnTop:TextButton?
     var btnBottom:TextButton?
@@ -115,13 +115,14 @@ class ListTransitionTestView:TitleView{
     //Continue here:
         //Debug the reUse method with a textField that displays the pool indecies on each scroll/dp.event -> [2,3,4,5,6] etc
     
-    func rbSliderFastList3(){//DataProvider()//
-        let dp:DataProvider = DataProvider("~/Desktop/ElCapitan/assets/xml/scrollist.xml".tildePath)//longlist.xml,list.xml//DataProvider()//
-        /*dp.addItem(["title":"pink"])
-         dp.addItem(["title":"orange"])
-         dp.addItem(["title":"purple"])*/
+    func rbSliderFastList3(){////
+        let dp:DataProvider = DataProvider()//DataProvider("~/Desktop/ElCapitan/assets/xml/scrollist.xml".tildePath)//longlist.xml,list.xml//DataProvider()//
+        dp.addItem(["title":"pink"])
+        dp.addItem(["title":"orange"])
+        dp.addItem(["title":"purple"])
+        /**/
         
-        list = addSubView(SliderList(140, 145, 24, dp, self))/*RBSliderFastList3*/
+        list = addSubView(FastList4(140, 145, 24, dp, self))/*RBSliderFastList3*/
         
         
         //continue here:
