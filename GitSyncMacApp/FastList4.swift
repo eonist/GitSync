2ab7ca15e4f305a27a68a01cba4f1da7e8a4d2da
Eonist <unicornhive@gmail.com>
2017-02-17 11:32:35 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList4.swift

--- GitSyncMacApp/FastList4.swift
+++ GitSyncMacApp/FastList4.swift
@@ -13,7 +13,7 @@ class FastList4:Element,IList {
         self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
         super.init(width, height, parent, id)
         self.dataProvider.event = self.onEvent/*Add event handler for the dataProvider*/
-        
+        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
     }
     override func resolveSkin() {
         super.resolveSkin()
