50485c18e6d7cbf84a7be73f892c1ed6c7a221d0
Eonist <unicornhive@gmail.com>
2017-02-17 19:33:51 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/FastList4.swift

--- GitSyncMacApp/FastList4.swift
+++ GitSyncMacApp/FastList4.swift
@@ -83,8 +83,7 @@ class FastList4:Element,IList {
     func onListItemUpInside(_ buttonEvent:ButtonEvent) {
         let viewIndex:Int = lableContainer!.indexOf(buttonEvent.origin as! NSView)
         ListModifier.selectAt(self,viewIndex)//unSelect all other visibleItems
-        ArrayParser.first(<#T##variables: [T]##[T]#>, <#T##match: V##V#>, <#T##method: (T, V) -> Bool##(T, V) -> Bool#>)
-        pool.forEach{if($0.item === buttonEvent.origin){selectedIdx = $0.idx}}/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
+        selectedIdx = ArrayParser.first(pool, buttonEvent.origin, {$0.item === $1}) ?? selectedIdx/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
         super.onEvent(ListEvent(ListEvent.select,selectedIdx ?? -1,self))/*if selectedIdx is nil then use -1 in the event*///TODO: probably use FastListEvent here in the future
     }
     override func onEvent(_ event:Event) {
