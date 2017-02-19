0d6894d092346766fa2f73801cc8cc44d1d919d9
Eonist <unicornhive@gmail.com>
2017-02-20 00:03:44 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -22,9 +22,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //_ = Test()
         //initApp()
         //initTestWin()
-        _ = PopulateCommitDB()
+        //_ = PopulateCommitDB()
         
-        print("test")
+        //print("test")
+        
+        let temp:Temp = Temp()
+        let xml:XML = Reflection.toXML(temp)
+        Swift.print("xml.xmlString: " + "\(xml.xmlString)")
     }
     func initTestWin(){
         //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
@@ -70,4 +74,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
         print("Good-bye")
     }
+}
+class Temp{
+    let temp:[[String:String]] = [["a":"b"],["1":"2"]]
 }
