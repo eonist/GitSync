99b312e9aab1c67cff100521b55ba7f77ae8279e
Eonist <unicornhive@gmail.com>
2017-02-19 21:13:35 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/CommitDB.swift

--- Sources/GitSyncMac/library/CommitDB.swift
+++ Sources/GitSyncMac/library/CommitDB.swift
@@ -62,7 +62,7 @@ extension CommitDB{
 //this makes CommitDB unwrappable (XML->CommitDB)
 extension CommitDB:UnWrappable{
     static func unWrap<T>(_ xml:XML) -> T? {
-        let items:Dictionary<String, String> = unWrap(xml, "items")
+        let sortedArr:[Commit?] = unWrap(xml, "sortedArr")
         //let prevCommits:[Int:String] = unWrap(xml,"prevCommits")
         return CommitDB(sortedArr.flatMap{$0}/*,prevCommits*/) as? T/*flatMap is used to remove any nil values*/
     }
