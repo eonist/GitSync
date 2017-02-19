ac75ad0657e431f24673d3655d2538348f602009
Eonist <unicornhive@gmail.com>
2017-02-19 20:44:53 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/utils/CommitDPCache.swift

--- Sources/GitSyncMac/library/utils/CommitDPCache.swift
+++ Sources/GitSyncMac/library/utils/CommitDPCache.swift
@@ -7,10 +7,10 @@ class CommitDPCache {
      * Read commits from disk (xml)
      */
     static func read()->CommitDP{
-        let url:String = CommitDBCache.url.tildePath
+        let url:String = CommitDPCache.url.tildePath
         let xml = FileParser.xml(url)
         //Swift.print("xml.XMLString: " + "\(xml.XMLString)")
-        let commitDP:CommitDP? = CommitDB.unWrap(xml)
+        let commitDP:CommitDP? = CommitDP.unWrap(xml)
         //Swift.print("Printing sortedArr after unwrap: ")
         //commitDB.sortedArr.forEach{Swift.print($0.sortableDate)}
         return commitDP!
@@ -18,8 +18,8 @@ class CommitDPCache {
     /**
      * Write commits to disk (xml)
      */
-    static func write(_ commitDB:CommitDP){
-        let xml = Reflection.toXML(commitDB)/*Reflection*/
+    static func write(_ commitDP:CommitDP){
+        let xml = Reflection.toXML(commitDP)/*Reflection*/
         //Swift.print(xml.XMLString)
         let contentToWriteToDisk = xml.xmlString
         _ = FileModifier.write(CommitDBCache.url.tildePath, contentToWriteToDisk)
