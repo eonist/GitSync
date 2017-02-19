3cfb7039c8387404b7ac8d63730dc2e59cabdf5e
Eonist <unicornhive@gmail.com>
2017-02-19 21:45:04 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/PopulateCommitDBTest.swift

--- Sources/GitSyncMac/test/PopulateCommitDBTest.swift
+++ Sources/GitSyncMac/test/PopulateCommitDBTest.swift
@@ -96,10 +96,11 @@ class PopulateCommitDB {
         Swift.print("commitDB.sortedArr.count: " + "\(commitDP.items.count)")
         Swift.print("Printing sortedArr after refresh: ")
         commitDP.items.forEach{
-            Swift.print("hash: \($0["hash"]) date: \(GitDateUtils.gitTime($0["sortableDate"]!)) repo: \($0["repo-name"]!) ")
+            Swift.print("hash: \($0["hash"]!) date: \(GitDateUtils.gitTime($0["sortableDate"]!)) repo: \($0["repo-name"]!) ")
         }
         Swift.print("💚 onRefreshReposComplete() Time: " + "\(abs(startTime.timeIntervalSinceNow))")/*How long did the gathering of git commit logs take?*/
         CommitDPCache.write(commitDP)//write data to disk, we could also do this on app exit
+        Swift.print("Written to disk")
     }
 }
 
