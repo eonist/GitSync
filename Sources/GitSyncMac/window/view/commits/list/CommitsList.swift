fdfa9d71033c3a0650b6527a4dce6810cfe42312
Eonist <unicornhive@gmail.com>
2017-02-20 17:01:58 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -95,7 +95,7 @@ extension CommitsList{
      */
     func loopAnimationCompleted(){
         Swift.print("❤️️ CommitList.loopAnimationCompleted()")
-        pool.forEach{reUse($0)}
+        pool.forEach{reUse($0)}//force a refresh of all items
         progressIndicator!.progress(0)
         progressIndicator!.stop()
         isInDeactivateRefreshModeState = true
