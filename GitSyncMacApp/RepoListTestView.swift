3399db95eabeff99d50fe28f36d1549f70a34a8f
Eonist <30n1st@gmail.com>
2017-02-23 18:33:27 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/RepoListTestView.swift

--- GitSyncMacApp/RepoListTestView.swift
+++ GitSyncMacApp/RepoListTestView.swift
@@ -65,7 +65,8 @@ class RepoListTestView:TitleView{
         Swift.print("newRepo")
         let idx = TreeListParser.selectedIndex(treeList!)
         //let selectedXML:XML = XMLParser.childAt(treeList!.node.xml, idx)!
-        TreeListUtils.itemData(XMLParser)
+        let child:XML = Utils.XMLParser.childAt(<#T##xml: XML?##XML?#>, <#T##index: Array<Int>##Array<Int>#>)
+        //TreeListUtils.itemData()
         treeList!.node.addAt(idx, "<item title=\"New repo\"/>".xml)
         Swift.print("Promt repo name popup")
     }
