5f82f2e294ec9813b7f7fa46dc6dc8dfdbb5e386
Eonist <30n1st@gmail.com>
2017-02-25 00:06:29 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -48,9 +48,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     let a:[AnyObject] = item as! [AnyObject]
                     result += recFlatMap(a)
                 }else{
-                    if(item["xml"] != nil && item["xml"] is AnyArray){
-                        let temp:[AnyObject] = item["xml"]
-                        result += recFlatMap(temp)
+                    if(item is AnyDictionary && item["xml"] != nil && item["xml"] is AnyArray){
+                        let dict:[String:Any] = item as! [String : Any]
+                        let temp:Any = dict["xml"] as Any
+                        result += recFlatMap(temp as! [AnyObject])
                     }else{
                         result.append(arr[i] as! T)
                     }
