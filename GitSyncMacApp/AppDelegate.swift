457081e0e5ae070db4d2867734b02f8975709d02
Eonist <30n1st@gmail.com>
2017-02-24 22:19:02 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         Swift.print("GitSync - The future is automated")//Simple git automation for macOS, The autonomouse git client
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
         //_ = Test()
-        initApp()
+        //initApp()
         //initTestWin()
         //AutoSync.sync()
         //refreshReposTest()
@@ -25,6 +25,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
          let b:Range<Int> = 10..<15
          let diff = RangeParser.difference(a, b)
          Swift.print("diff: " + "\(diff)")*/
+        
+        
     }
     /**
      *
