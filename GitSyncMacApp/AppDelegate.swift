f8cb3ae5bfac30f3227e483ea52b2643fe31af02
Eonist <30n1st@gmail.com>
2017-02-25 21:49:58 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let localPath:String = repoList[1].localPath
         Swift.print("localPath: " + "\(localPath)")
         
-        rateOfCommits(repoList)
+        let repoCommits:[[Int]] = rateOfCommits(repoList)
     }
     static func rateOfCommits(_ repoList:[RepoItem] )->[[Int]]{
         var repoCommits:[[Int]] = []
