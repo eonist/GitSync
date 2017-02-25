c67e89c5fb4bb5a3bcfff672a027b0b8ce15bc70
Eonist <30n1st@gmail.com>
2017-02-25 14:23:46 +0100

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/library/utils/AutoSync.swiftSources/GitSyncMac/utils/RepoUtils.swift

--- Sources/GitSyncMac/library/utils/AutoSync.swift
+++ Sources/GitSyncMac/library/utils/AutoSync.swift
@@ -31,38 +31,3 @@ class AutoSync {
         }
     }
 }
-extension AutoSync{
-    /**
-     * Returns an array of RepoItems derived from a nested xml Structure
-     */
-    var repoList:[RepoItem]{
-        let repoXML:XML
-        if(RepoView.node != nil){
-            repoXML = RepoView.node!.xml//re-use if it already exists
-        }else{
-            repoXML = FileParser.xml(RepoView.repoList.tildePath)//or load a fresh copy
-        }
-        let arr:[Any] = XMLParser.arr(repoXML)//convert xml to multidimensional array
-        let flatArr:[[String:String]] = arr.recursiveFlatmap()
-        /*
-         Swift.print("flatArr.count: " + "\(flatArr.count)")
-         flatArr.forEach{
-         Swift.print("$0: " + "\($0)")
-         }
-         */
-        let repoList:[RepoItem] = flatArr.filter{
-            ($0["hasChildren"] == nil) && ($0["isOpen"] == nil)//skips folders
-            }.map{//create array of tuples
-                AutoSync.repoItem($0)
-        }
-        //Swift.print("repoList.count: " + "\(repoList.count)")
-        //Swift.print("repoList[0]: " + "\(repoList[0])")
-        return repoList
-    }
-    /**
-     *
-     */
-    static func repoItem(_ dict:[String:String]) -> RepoItem{
-        return (localPath:dict["local-path"]!,interval:dict["interval"]!.int,branch:dict["branch"]!,keyChainItemName:dict["keychain-item-name"]!,broadcast:dict["broadcast"]!.bool,title:dict["title"]!,subscribe:dict["subscribe"]!.bool,autoSync:dict["auto-sync"]!.bool,remotePath:dict["remote-path"]!)
-    }
-}
--- Sources/GitSyncMac/utils/RepoUtils.swift
+++ Sources/GitSyncMac/utils/RepoUtils.swift
@@ -10,20 +10,38 @@ class RepoUtils{
  	 * TODO: if the interval values is not set, then use default values
 	 * TODO: test if the full/partly file path still works?
 	 */
-	func compileRepoList(_ filePath:String)->[Dictionary<String,String>]{
-        let children:Array<XMLElement> = XMLParser.rootChildrenByFilePath(filePath)
-        var theRepoList:[Dictionary<String,String>] = []
-        for child:XMLElement in children{
-            var localPath:String = XMLParser.attribute(child,"local-path")! //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
-            localPath = ShellUtils.run("echo " + StringModifier.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths.
-            var remotePath:String = XMLParser.attribute(child,"remote-path")!
-            remotePath = RegExp.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
-            //print(remotePath)
-            let keychainItemName:String = XMLParser.attribute(child,"keychain-item-name")!
-            let interval:String = XMLParser.attribute(child,"interval")!//default is 1min
-            let repoItem:Dictionary<String,String> = ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval]
-            theRepoList.append(repoItem)
+	
+    /**
+     * Returns an array of RepoItems derived from a nested xml Structure
+     */
+    static var repoList:[RepoItem]{
+        let repoXML:XML
+        if(RepoView.node != nil){
+            repoXML = RepoView.node!.xml//re-use if it already exists
+        }else{
+            repoXML = FileParser.xml(RepoView.repoList.tildePath)//or load a fresh copy
         }
-        return theRepoList
-	}
+        let arr:[Any] = XMLParser.arr(repoXML)//convert xml to multidimensional array
+        let flatArr:[[String:String]] = arr.recursiveFlatmap()
+        /*
+         Swift.print("flatArr.count: " + "\(flatArr.count)")
+         flatArr.forEach{
+         Swift.print("$0: " + "\($0)")
+         }
+         */
+        let repoList:[RepoItem] = flatArr.filter{
+            ($0["hasChildren"] == nil) && ($0["isOpen"] == nil)//skips folders
+            }.map{//create array of tuples
+                RepoUtils.repoItem($0)
+        }
+        //Swift.print("repoList.count: " + "\(repoList.count)")
+        //Swift.print("repoList[0]: " + "\(repoList[0])")
+        return repoList
+    }
+    /**
+     *
+     */
+    static func repoItem(_ dict:[String:String]) -> RepoItem{
+        return (localPath:dict["local-path"]!,interval:dict["interval"]!.int,branch:dict["branch"]!,keyChainItemName:dict["keychain-item-name"]!,broadcast:dict["broadcast"]!.bool,title:dict["title"]!,subscribe:dict["subscribe"]!.bool,autoSync:dict["auto-sync"]!.bool,remotePath:dict["remote-path"]!)
+    }
 }
