6e2a40fc1b41d0481ab32ffdca9bb450993fcb89
Eonist <30n1st@gmail.com>
2017-02-25 14:04:24 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepoView.swift

--- Sources/GitSyncMac/window/view/repo/RepoView.swift
+++ Sources/GitSyncMac/window/view/repo/RepoView.swift
@@ -17,7 +17,6 @@ class RepoView:Element {
     override func resolveSkin() {
         Swift.print("RepoView.resolveSkin()")
         self.skin = SkinResolver.skin(self)//super.resolveSkin()//
-        //topBar = addSubView(TopBar(width-12,44,self))
         
         if(RepoView.node == nil){/*loads 1 time*/
             let xml = FileParser.xml(RepoView.repoList.tildePath)//
@@ -29,38 +28,16 @@ class RepoView:Element {
         //if(RepoView.selectedListItemIndex != -1){list!.selectAt(RepoView.selectedListItemIndex)}
     }
     
-    /*
-    func onAddButtonClick(){
-        Swift.print("addButton.click")
-        Sounds.play?.play()
-        list!.dataProvider.addItemAt(["title":"New repo","local-path":"~/Desktop/","remote-path":"https://github.com/userName/repoName.git","interval":"0","keychain-item-name":"","branch":"master","broadcast":"false","subscribe":"true","auto-sync":"false"], 0)
-        ListModifier.selectAt(list!, 0)
-        let repoItem:Dictionary<String,String> = list!.dataProvider.getItemAt(list!.selectedIndex)!
-        Navigation.setView("\(RepoDetailView.self)")
-        (Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
-        //list!.onEvent(ListEvent(ListEvent.select,0,list!))
-    }
- 
-    func onBackButtonClick(){
-        Swift.print("onBackButtonClick()")
-        Navigation.setView(MenuView.commits)
-    }
-     */
     func onListSelect(){
-        Sounds.play?.play()
+        //Sounds.play?.play()
         Navigation.setView("\(RepoDetailView.self)")
         let selectedIndex:Array = TreeListParser.selectedIndex(treeList!)
         RepoView.selectedListItemIndex = selectedIndex
-        
+        //TODO: Use the RepoItem on the bellow line see AutoSync class for implementation
         let repoItem:Dictionary<String,String> = NodeParser.dataAt(treeList!.node, selectedIndex)
         (Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
     }
     override func onEvent(_ event:Event) {
-        //if(event.type == ButtonEvent.upInside && event.origin === topBar!.addButton){onAddButtonClick()}
-        //else if(event.type == ButtonEvent.upInside && event.origin === topBar!.backButton){onBackButtonClick()}
-        //else if(event.type == ListEvent.select){onListSelect()}
-        //else if(event.type == SelectEvent.select && event.immediate === list){super.onEvent(event)}//forward this event to the parent
-        
         if(event.type == SelectEvent.select && event.immediate === treeList){
             //Swift.print("event.origin: " + "\(event.origin)")
             let selectedIndex:Array = TreeListParser.selectedIndex(treeList!)
@@ -77,9 +54,8 @@ class RepoView:Element {
         }
     }
 }
-
 /**
- * Right click menu
+ * Right click context menu
  */
 class ContextMenu:NSMenu{
     var rightClickItemIdx:[Int]?
@@ -152,20 +128,5 @@ extension ContextMenu{
         Swift.print("delete")
         let idx = rightClickItemIdx!
         _ = treeList.node.removeAt(idx)
-    }
-}
-/**
- * Add,Remove,Edit,Cut,Paste
- */
-class TopBar:Element{
-    //var backButton:Button?
-    //var editButton:Button?
-    //var removeButton:Button?
-    var addButton:Button?
-    override func resolveSkin() {
-        Swift.print("TopBar.resolveSkin()")
-        self.skin = SkinResolver.skin(self)//super.resolveSkin()//
-        //backButton = addSubView(Button(NaN,NaN,self,"back"))
-        addButton = addSubView(Button(NaN,NaN,self,"add"))
     }
 }
