92d7c4c2cbed4ecdc566ffe43acad95903055046
Eonist <30n1st@gmail.com>
2017-02-25 14:30:04 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/utils/RepoUtils.swift

--- Sources/GitSyncMac/utils/RepoUtils.swift
+++ Sources/GitSyncMac/utils/RepoUtils.swift
@@ -4,12 +4,7 @@ import Foundation
  * Utility methods for parsing the repository.xml file
  */
 class RepoUtils{
-	/**
-	 * Returns a list with repo values derived from an XML file
- 	 * PARAM: file_path
- 	 * TODO: if the interval values is not set, then use default values
-	 * TODO: test if the full/partly file path still works?
-	 */
+	
 	
     /**
      * Returns an array of RepoItems derived from a nested xml Structure
@@ -39,18 +34,19 @@ class RepoUtils{
         return repoList
     }
     /**
-     *
+     * Returns a RepoItem
+     * TODO: if the interval values is not set, then use default values
+     * TODO: test if the full/partly file path still works?
      */
     static func repoItem(_ dict:[String:String]) -> RepoItem{
-        var localPath:String = XMLParser.attribute(child,"local-path")! //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
+        var localPath:String = dict["local-path"]! //this is the path to the local repository (we need to be in this path to execute git commands on this repo)
         localPath = ShellUtils.run("echo " + StringModifier.wrapWith(localPath,"'") + " | sed 's/ /\\\\ /g'")//--Shell doesnt handle file paths with space chars very well. So all space chars are replaced with a backslash and space, so that shell can read the paths.
-        var remotePath:String = dict["remote-path""]
+        var remotePath:String = dict["remote-path"]!
         remotePath = RegExp.replace(remotePath,"^https://.+$","")//support for partial and full url, strip away the https://, since this will be added later
         //print(remotePath)
-        let keychainItemName:String = dict["keychain-item-name"]
-        let interval:String = dict["interval"]//default is 1min
-        let repoItem:Dictionary<String,String> = ["localPath":localPath,"remotePath":remotePath,"keychainItemName":keychainItemName,"interval":interval]
-        
-        return (localPath:dict["local-path"]!,interval:dict["interval"]!.int,branch:dict["branch"]!,keyChainItemName:dict["keychain-item-name"]!,broadcast:dict["broadcast"]!.bool,title:dict["title"]!,subscribe:dict["subscribe"]!.bool,autoSync:dict["auto-sync"]!.bool,remotePath:dict["remote-path"]!)
+        let keychainItemName:String = dict["keychain-item-name"]!
+        let interval:String = dict["interval"]!//default is 1min
+        let repoItem:RepoItem = (localPath:localPath,interval:interval.int,branch:dict["branch"]!,keyChainItemName:keychainItemName,broadcast:dict["broadcast"]!.bool,title:dict["title"]!,subscribe:dict["subscribe"]!.bool,autoSync:dict["auto-sync"]!.bool,remotePath:remotePath)
+        return repoItem
     }
 }
