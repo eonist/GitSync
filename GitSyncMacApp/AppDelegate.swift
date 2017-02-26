7bbb6748adc2a6af56ea8e3d15f24829b3e81da9
Eonist <30n1st@gmail.com>
2017-02-27 00:36:07 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -69,7 +69,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                     }
                     //result[i] = result[i] + $0[i]
                 }
-                
             }
         }
     }
