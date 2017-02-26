ea9622df8b16a4044b8f2df400dd3d72b5bcb2d6
Eonist <30n1st@gmail.com>
2017-02-26 23:32:15 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -95,8 +95,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             let sinceGitDate:String = GitDateUtils.gitTime(sinceDate)
             let untilDate:Date = Date().offsetByDays(dayOffset+1)
             let untilGitDate:String = GitDateUtils.gitTime(untilDate)
-            let 
-            let commitCount:String = GitUtils.commitCount(repoItem.localPath, since: sinceGitDate, until:untilGitDate)
+            let commitData:(localPath:String,since:Date,until:Date) = (repoItem.localPath,sinceGitDate,untilGitDate)
+            let commitCount:String = GitUtils.commitCount(repoItem.localPath, since: , until:)
             //Swift.print("commitCount: " + "\(commitCount)")
             commits.append(commitCount.int)
         }
