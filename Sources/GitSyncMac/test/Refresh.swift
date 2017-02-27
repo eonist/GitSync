a0ec5c54ac1abcb31647b77f3eece6b79b2ce8d0
Eonist <30n1st@gmail.com>
2017-02-27 21:06:25 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/Refresh.swift

--- Sources/GitSyncMac/test/Refresh.swift
+++ Sources/GitSyncMac/test/Refresh.swift
@@ -64,10 +64,10 @@ class RefreshUtils{
     static func refreshRepo(_ dp:CommitDP,_ repo:RepoItem){
         let commitCount:Int = self.commitCount(dp,repo)
         Swift.print("💙\(repo.title): rangeCount: " + "\(commitCount)")
-        let results:[String] = Utils.commitItems(repo.localPath, commitCount)/*creates an array raw commit item logs, from repo*/
+        let results:[String] = Utils.commitItems(repo.localPath, commitCount)//👈many Git calls/*creates an array raw commit item logs, from repo*/
         results.forEach{
             if($0.count > 0){//resulting string must have characters
-                let commitData:CommitData = GitLogParser.commitData($0) /*Compartmentalizes the result into a Tuple*/
+                let commitData:CommitData = GitLogParser.commitData($0)/*Compartmentalizes the result into a Tuple*/
                 //let commit:Commit = CommitViewUtils.processCommitData(repoTitle,commitData,0)/*Format the data*/
                 let commitDict:[String:String] = CommitViewUtils.processCommitData(repo.title, commitData, 0)//<---TODO:add repo idx here
                 dp.add(commitDict)/*add the commit log items to the CommitDB*/
