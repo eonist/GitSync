f116874ac6846c2d318646388349b7e5dc0ec29d
Eonist <30n1st@gmail.com>
2017-02-27 13:55:54 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -12,7 +12,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     var win:NSWindow?/*<--The window must be a class variable, local variables doesn't work*/
     var fileWatcher:FileWatcher?
     //var timer:SimpleTimer?
-    var startTime:Date? = nil
+    
     
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         Swift.print("GitSync - The future is automated")//Simple git automation for macOS, The autonomouse git client
@@ -33,13 +33,77 @@ class AppDelegate: NSObject, NSApplicationDelegate {
      * CommitCount per day for all projects in the last 7 days where the user is "eonist"
      */
     func rateOfCommitsTest(){
-        startTime = Date()
-        initRateOfCommitsProcess(0/*<--dayOffset*/)
+        
     }
+    
+    /**
+     *
+     */
+    func refreshReposTest(){
+        func onComplete(){
+            Swift.print("🏆🏆🏆 CommitDB finished!!! ")
+        }
+        CommitDPRefresher.commitDP = CommitDPCache.read()
+        CommitDPRefresher.onComplete = onComplete
+        CommitDPRefresher.refresh()
+    }
+    func initTestWin(){
+        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)
+        win = ListTransitionTestWin(600,400)/*Debugging Different List components*/
+        
+        let url:String = "~/Desktop/ElCapitan/"
+        fileWatcher = FileWatcher([url.tildePath])
+        fileWatcher!.event = { event in
+            //Swift.print(self)
+            if(event.fileChange && FilePathParser.fileExtension(event.path) == "css") {//assert for .css file changes, so that .ds etc doesnt trigger events etc
+                Swift.print(event.description)
+                Swift.print("update to the file happened: " + "\(event.path)")
+                StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
+                let view:NSView = self.win!.contentView!//MainWin.mainView!
+                ElementModifier.refreshSkin(view as! IElement)
+                ElementModifier.floatChildren(view)
+            }
+        }
+        fileWatcher!.start()
+    }
+    func initApp(){
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)//<--toggle this bool for live refresh
+        win = MainWin(MainView.w,MainView.h)
+        //win = ConflictDialogWin(380,400)
+        //win = CommitDialogWin(400,356)
+        
+    }
+    func applicationWillTerminate(_ aNotification: Notification) {
+        //store the app prefs
+        if(PrefsView.keychainUserName != nil){//make sure the data has been read and written to first
+            let xml:XML = "<prefs></prefs>".xml
+            xml.appendChild("<keychainUserName>\(PrefsView.keychainUserName!)</keychainUserName>".xml)
+            xml.appendChild("<gitConfigUserName>\(PrefsView.gitConfigUserName!)</gitConfigUserName>".xml)
+            xml.appendChild("<gitEmailName>\(PrefsView.gitEmailNameText!)</gitEmailName>".xml)
+            xml.appendChild("<uiSounds>\(String(PrefsView.uiSounds!))</uiSounds>".xml)
+            _ = FileModifier.write("~/Desktop/gitsyncprefs.xml".tildePath, xml.xmlString)
+        }
+        //store the repo xml
+        
+        if(RepoView.node != nil){//make sure the data has been read and written to first
+            _ = FileModifier.write(RepoView.repoList.tildePath, RepoView.node!.xml.xmlString)
+            //Swift.print("RepoList was saved")
+        }
+        print("Good-bye")
+    }
+}
+
+class RateOfCommits{
     var repoCommits:[[CommitCountWork]]?
     var totCount:Int?
     var result:[Int] = [0,0,0,0,0,0,0]
     var idx:Int = 0
+    var startTime:Date? = nil
+    init(){
+        startTime = Date()
+        initRateOfCommitsProcess(0/*<--dayOffset*/)
+    }
     
     func onComplete(){
         idx += 1
@@ -115,66 +179,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //Swift.print("commits: " + "\(commits)")
         return commitCountWorks
     }
-    /**
-     *
-     */
-    func refreshReposTest(){
-        func onComplete(){
-            Swift.print("🏆🏆🏆 CommitDB finished!!! ")
-        }
-        CommitDPRefresher.commitDP = CommitDPCache.read()
-        CommitDPRefresher.onComplete = onComplete
-        CommitDPRefresher.refresh()
-    }
-    func initTestWin(){
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
-        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)
-        win = ListTransitionTestWin(600,400)/*Debugging Different List components*/
-        
-        let url:String = "~/Desktop/ElCapitan/"
-        fileWatcher = FileWatcher([url.tildePath])
-        fileWatcher!.event = { event in
-            //Swift.print(self)
-            if(event.fileChange && FilePathParser.fileExtension(event.path) == "css") {//assert for .css file changes, so that .ds etc doesnt trigger events etc
-                Swift.print(event.description)
-                Swift.print("update to the file happened: " + "\(event.path)")
-                StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
-                let view:NSView = self.win!.contentView!//MainWin.mainView!
-                ElementModifier.refreshSkin(view as! IElement)
-                ElementModifier.floatChildren(view)
-            }
-        }
-        fileWatcher!.start()
-    }
-    func initApp(){
-        StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)//<--toggle this bool for live refresh
-        win = MainWin(MainView.w,MainView.h)
-        //win = ConflictDialogWin(380,400)
-        //win = CommitDialogWin(400,356)
-        
-    }
-    func applicationWillTerminate(_ aNotification: Notification) {
-        //store the app prefs
-        if(PrefsView.keychainUserName != nil){//make sure the data has been read and written to first
-            let xml:XML = "<prefs></prefs>".xml
-            xml.appendChild("<keychainUserName>\(PrefsView.keychainUserName!)</keychainUserName>".xml)
-            xml.appendChild("<gitConfigUserName>\(PrefsView.gitConfigUserName!)</gitConfigUserName>".xml)
-            xml.appendChild("<gitEmailName>\(PrefsView.gitEmailNameText!)</gitEmailName>".xml)
-            xml.appendChild("<uiSounds>\(String(PrefsView.uiSounds!))</uiSounds>".xml)
-            _ = FileModifier.write("~/Desktop/gitsyncprefs.xml".tildePath, xml.xmlString)
-        }
-        //store the repo xml
-        
-        if(RepoView.node != nil){//make sure the data has been read and written to first
-            _ = FileModifier.write(RepoView.repoList.tildePath, RepoView.node!.xml.xmlString)
-            //Swift.print("RepoList was saved")
-        }
-        print("Good-bye")
-    }
-}
-
-class RateOfCommits{
-    
 }
 
 
