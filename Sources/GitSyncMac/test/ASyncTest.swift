d6328a9594c463c749d733b55b0663b5aacbb826
Eonist <30n1st@gmail.com>
2017-02-28 19:21:03 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -9,40 +9,19 @@ class ASyncTest {
      *
      */
     init(){
-        var outerIdx:Int = 0
-
-        func allOuterTasksCompleted(){
-            Swift.print("🏁 allOuterTasksCompleted: 🏁")
-        }
-        func onOuterComplete(_ index:Int){
-            Swift.print("all inner async tasks completed on outer async id: \(index)")
-            outerIdx += 1
-            Swift.print("outerIdx: " + "\(outerIdx)")
-            if(outerIdx == 3){
-                allOuterTasksCompleted()
-            }
-        }
-        for i in 0..<3{
+        let group = DispatchGroup()
+        group.enter()
+        for _ in 0..<3{
             bg.async {/*do 3 things at the same time*/
-                Swift.print("---outer async started i: \(i)---")
-                var innerIdx:Int = 0
-                func onInnerComplete(_ index:Int){
-                    Swift.print("inner async task completed e: \(index)")
-                    innerIdx += 1/*increment counter*/
-                    if(innerIdx == 2){
-                        main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
-                            onOuterComplete(i)
-                        }
-                    }
-                }
-                for e in 0..<2{
-                    Swift.print("===inner async started e: \(e)===")
+                for _ in 0..<2{
                     bg.async{/*do 2 things at the same time*/
                         sleep(2/*IntParser.random(1, 6).uint32*/)/*simulates task that takes between 1 and 6 secs*/
-                        onInnerComplete(e)
                     }
                 }
             }
         }
+        group.leave()
+        group.wait()
+        
     }
 }
