bbe80153025bd93e2053578441b5243c6078d5c8
Eonist <30n1st@gmail.com>
2017-02-28 16:08:23 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -30,7 +30,9 @@ class ASyncTest {
                     Swift.print("inner async task completed e: \(index)")
                     innerIdx += 1/*increment counter*/
                     if(innerIdx == 2){
-                        onOuterComplete(i)
+                        main.async{// we must jump back on main thread
+                            onOuterComplete(i)
+                        }
                     }
                 }
                 for e in 0..<2{
