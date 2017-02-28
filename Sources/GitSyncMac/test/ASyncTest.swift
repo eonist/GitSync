48d0c6727411406375a7d003289849efe7c1ee31
Eonist <30n1st@gmail.com>
2017-02-28 19:25:41 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -11,11 +11,11 @@ class ASyncTest {
     init(){
         let group = DispatchGroup()
         group.enter()
-        for _ in 0..<3{
+        for i in 0..<3{
             bg.async {/*do 3 things at the same time*/
-                for _ in 0..<2{
+                for e in 0..<2{
                     bg.async{/*do 2 things at the same time*/
-                        sleep(2/*IntParser.random(1, 6).uint32*/)/*simulates task that takes between 1 and 6 secs*/
+                        sleep(IntParser.random(1, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
                         Swift.print("i: \(i) e:\(e)")
                     }
                 }
