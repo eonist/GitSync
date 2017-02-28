6ab8334d570a13840faf715a8c2956ad68226940
Eonist <30n1st@gmail.com>
2017-02-28 14:26:29 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -10,15 +10,6 @@ class ASyncTest {
      */
     init(){
         var arr:[(i:Int,s:String,result:String)] = [(0,"a",""),(1,"b",""),(2,"c",""),(3,"d","")]
-        var i:Int = 0
-        func onComplete(/*_ idx:Int,_ result:String*/){
-            i += 1
-            Swift.print("onComplete: " + "\(i)")
-            if(i == arr.count){
-                Swift.print("all concurrent tasks completed")
-                Swift.print("arr: " + "\(arr)")//[(0, "a", "0a"), (1, "b", "1b"), (2, "c", "2c"), (3, "d", "3d")]
-            }
-        }
         for i in arr.indices {
             bgQueue.async {
                 let res:String = arr[i].i.string + arr[i].s
@@ -28,5 +19,16 @@ class ASyncTest {
                 }
             }
         }
+        /*
+        var i:Int = 0
+        func onComplete(/*_ idx:Int,_ result:String*/){
+            i += 1
+            Swift.print("onComplete: " + "\(i)")
+            if(i == arr.count){
+                Swift.print("all concurrent tasks completed")
+                Swift.print("arr: " + "\(arr)")//[(0, "a", "0a"), (1, "b", "1b"), (2, "c", "2c"), (3, "d", "3d")]
+            }
+        }
+        */
     }
 }
