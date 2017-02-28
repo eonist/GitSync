d658eb5e2708a39e30b92d6a722bf83da65900da
Eonist <30n1st@gmail.com>
2017-02-28 18:14:39 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -2,7 +2,7 @@ import Foundation
 @testable import Utils
 
 //do 3 things async, 
-//then in each 3 things do 4 things async but do something on mainthread when these 4 things are all finished
+//then in each 3 things do 2 things async but do something on mainthread when these 4 things are all finished
 
 class ASyncTest {
     /**
@@ -44,16 +44,5 @@ class ASyncTest {
                 }
             }
         }
-        /*
-        var i:Int = 0
-        func onComplete(/*_ idx:Int,_ result:String*/){
-            i += 1
-            Swift.print("onComplete: " + "\(i)")
-            if(i == arr.count){
-                Swift.print("all concurrent tasks completed")
-                Swift.print("arr: " + "\(arr)")//[(0, "a", "0a"), (1, "b", "1b"), (2, "c", "2c"), (3, "d", "3d")]
-            }
-        }
-        */
     }
 }
