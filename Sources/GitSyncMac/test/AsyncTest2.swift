57502c3f489e9bf1be1aef76ea1e817f7eb073ff
Eonist <30n1st@gmail.com>
2017-03-01 12:34:14 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/AsyncTest2.swift

--- Sources/GitSyncMac/test/AsyncTest2.swift
+++ Sources/GitSyncMac/test/AsyncTest2.swift
@@ -4,7 +4,7 @@ import Foundation
 class AsyncTest2 {
     init(){
         var outerArr = ["a","b","c"]
-        var innerAtt = [0,1]
+        var innerArr = [0,1]
         var outerIdx:Int = 0
         var innerIdx:Int = 0
         
@@ -15,7 +15,7 @@ class AsyncTest2 {
             Swift.print("🍏 onOuterComplete i: \(i_idx) e: \(e_idx) 🍏")
             outerIdx += 1
             Swift.print("outerIdx: " + "\(outerIdx)")
-            if(outerIdx == 3){
+            if(outerIdx == outerArr.count){
                 allOuterTasksCompleted()
             }
         }
@@ -23,16 +23,16 @@ class AsyncTest2 {
             Swift.print("🍌 onInnerComplete i: \(i_idx) e: \(e_idx) 🍌")
             main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
                 innerIdx += 1/*increment counter*/
-                if(innerIdx == 2){
+                if(innerIdx == innerArr.count){
                     innerIdx = 0//reset
                     onOuterComplete(i_idx,e_idx)
                 }
             }
         }
-        for i in 0..<3{
+        for i in outerArr.indices{
             bg.async {/*do 3 things at the same time*/
                 Swift.print("---outer async started i: \(i)---")
-                for e in 0..<2{
+                for e in innerArr.indices{
                     Swift.print("===inner async started e: \(e)===")
                     bg.async{/*do 2 things at the same time*/
                         sleep(IntParser.random(1, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
