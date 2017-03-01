27dcfddf2291233d243baadcbde8d4415be19d38
Eonist <30n1st@gmail.com>
2017-03-01 19:34:52 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/Refresh.swift

--- Sources/GitSyncMac/test/Refresh.swift
+++ Sources/GitSyncMac/test/Refresh.swift
@@ -58,7 +58,7 @@ class Refresh{//TODO:rename to refresh
         CommitDPCache.write(commitDP!)//write data to disk, we could also do this on app exit
         Swift.print("Written to disk")
         //isRefreshing = false
-        Swift.print("💚 onRefreshReposComplete() Time: " + "\(abs(startTime!.timeIntervalSinceNow))")/*How long did the gathering of git commit logs take?*/
+        Swift.print("alarm  onRefreshReposComplete() Time: " + "\(abs(startTime!.timeIntervalSinceNow))")/*How long did the gathering of git commit logs take?*/
         onComplete()/*🚪➡️️  calls a dynamic onComplete method, other classes can override this variable to get callback*/
     }
 }
