aca293e8848b900cd9da93d807165deef9ef20ba
Eonist <30n1st@gmail.com>
2017-03-01 14:14:44 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/Refresh.swift

--- Sources/GitSyncMac/test/Refresh.swift
+++ Sources/GitSyncMac/test/Refresh.swift
@@ -46,7 +46,7 @@ class Refresh{//TODO:rename to refresh
         repoCount = repos.count
         for i in repos.indices{
             bgQueue.async{/*run the task on a background thread*/
-                RefreshUtils.refreshRepo(self.commitDP!,repos[i])
+                RefreshUtils.refreshRepo(self.commitDP!,repos[i])//👈 0~1000's of git calls
                 mainQueue.async{/*jump back on the main thread*/
                     self.onRefreshRepoComplete()
                 }
