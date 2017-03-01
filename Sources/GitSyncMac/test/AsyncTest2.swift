5c0a74e4274596afa2c370293de22429895bf5c0
Eonist <30n1st@gmail.com>
2017-03-01 12:24:47 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/AsyncTest2.swift

--- Sources/GitSyncMac/test/AsyncTest2.swift
+++ Sources/GitSyncMac/test/AsyncTest2.swift
@@ -4,6 +4,7 @@ import Foundation
 class AsyncTest2 {
     init(){
         var outerIdx:Int = 0
+        var innerIdx:Int = 0
         
         func allOuterTasksCompleted(){
             Swift.print("🏁 allOuterTasksCompleted: 🏁")
@@ -19,12 +20,13 @@ class AsyncTest2 {
         for i in 0..<3{
             bg.async {/*do 3 things at the same time*/
                 Swift.print("---outer async started i: \(i)---")
-                var innerIdx:Int = 0
+                
                 func onInnerComplete(_ index:Int){
                     Swift.print("🍌 onInnerComplete e: \(index) 🍌")
+                    main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
                     innerIdx += 1/*increment counter*/
-                    if(innerIdx == 2){
-                        main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
+                        if(innerIdx == 2){
+                            innerIdx = 0//reset
                             onOuterComplete(i)
                         }
                     }
