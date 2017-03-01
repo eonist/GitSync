eb3159c18ffc94cf29b7d9804e908f718967eaca
Eonist <30n1st@gmail.com>
2017-03-01 01:32:44 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/ASyncTest.swift

--- Sources/GitSyncMac/test/ASyncTest.swift
+++ Sources/GitSyncMac/test/ASyncTest.swift
@@ -9,37 +9,34 @@ class ASyncTest {
      * Next implement the bellow in your example: 
      */
     init(){
-        //let outerGroup = DispatchGroup()
-        let innerGroup = DispatchGroup()
+        let outerGroup = DispatchGroup()
         Swift.print("1")
         for i in 0..<3{
             //bg.async{/*do 2 things at the same time*/
-                //outerGroup.enter()
+                outerGroup.enter()
                 Swift.print("iterate i: \(i)")
-                
+                let innerGroup = DispatchGroup()
                 for e in 0..<2{
                     bg.async{/*do 2 things at the same time*/
                         innerGroup.enter()
                         Swift.print("iterate i: \(i) e: \(e)")
-                        sleep(IntParser.random(2, 3).uint32)/*simulates task that takes between 1 and 6 secs*/
+                        sleep(IntParser.random(3, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
                         innerGroup.leave()
                     }
                 }
-               
+                innerGroup.wait()
+                innerGroup.notify(queue: bg, execute: {
+                    Swift.print("🍌 inner task completed: 🍌")
+                    outerGroup.leave()
+                })
                 
             //}
         }
-        innerGroup.wait()
-        innerGroup.notify(queue: bg, execute: {
-            Swift.print("🍌 inner task completed: 🍌")
-            //outerGroup.leave()
-        })
-        /*
         outerGroup.wait()
         outerGroup.notify(queue: bg, execute: {
             Swift.print("🏁 allTasksCompleted: 🏁")
         })
-        */
+        
     }
 }
 /*
