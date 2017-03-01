28131719c73d9a356dceb59567c2014563da1d15
Eonist <30n1st@gmail.com>
2017-03-01 12:28:32 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/AsyncTest2.swift

--- Sources/GitSyncMac/test/AsyncTest2.swift
+++ Sources/GitSyncMac/test/AsyncTest2.swift
@@ -9,8 +9,8 @@ class AsyncTest2 {
         func allOuterTasksCompleted(){
             Swift.print("🏁 allOuterTasksCompleted: 🏁")
         }
-        func onOuterComplete(_ index:Int){
-            Swift.print(" 🍏 onOuterComplete id: \(index) 🍏")
+        func onOuterComplete(_ i_idx:Int, _ e_idx:Int){
+            Swift.print("🍏 onOuterComplete i: \(i_idx) e: \(e_idx) 🍏")
             outerIdx += 1
             Swift.print("outerIdx: " + "\(outerIdx)")
             if(outerIdx == 3){
@@ -21,13 +21,13 @@ class AsyncTest2 {
             bg.async {/*do 3 things at the same time*/
                 Swift.print("---outer async started i: \(i)---")
                 
-                func onInnerComplete(_ index:Int){
-                    Swift.print("🍌 onInnerComplete e: \(index) 🍌")
+                func onInnerComplete(_ i_idx:Int, _ e_idx:Int){
+                    Swift.print("🍌 onInnerComplete i: \(i_idx) e: \(e_idx) 🍌")
                     main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
-                    innerIdx += 1/*increment counter*/
+                        innerIdx += 1/*increment counter*/
                         if(innerIdx == 2){
                             innerIdx = 0//reset
-                            onOuterComplete(i)
+                            onOuterComplete(i_idx,e_idx)
                         }
                     }
                 }
@@ -35,7 +35,7 @@ class AsyncTest2 {
                     Swift.print("===inner async started e: \(e)===")
                     bg.async{/*do 2 things at the same time*/
                         sleep(IntParser.random(1, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
-                        onInnerComplete(e)
+                        onInnerComplete(i,e)
                     }
                 }
             }
