cd00ea1887dcc1de255c68e20643889f3a0208e1
Eonist <30n1st@gmail.com>
2017-03-01 13:16:46 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/test/AsyncTest2.swift

--- Sources/GitSyncMac/test/AsyncTest2.swift
+++ Sources/GitSyncMac/test/AsyncTest2.swift
@@ -6,8 +6,16 @@ class AsyncTest2 {
         var outerArr = [1,2,3]
         var innerArr = ["a","b"]
         /*Indecies*/
-        var outerIdx:Int = 0
-        
+        var index = [[0],[0]]
+        /*Completion handlers resides on the main thread*/
+        func onInnerComplete(_ i_idx:Int, _ e_idx:Int){
+            Swift.print("🍌 onInnerComplete i: \(i_idx) e: \(e_idx) 🍌")
+            index += 1/*increment counter*/
+            if(innerIdx == innerArr.count){
+                innerIdx = 0//reset
+                onOuterComplete(i_idx,e_idx)
+            }
+        }
         func onOuterComplete(_ i_idx:Int, _ e_idx:Int){
             Swift.print("🍏 onOuterComplete i: \(i_idx) e: \(e_idx) 🍏")
             outerIdx += 1
@@ -23,26 +31,13 @@ class AsyncTest2 {
             bg.async {/*do 3 things at the same time*/
                 Swift.print("🚄 ---outer async started i: \(i)---")
                 for e in innerArr.indices{
-                    main.async{
-                        var innerIdx:Int = 0
-                        /*Completion handlers resides on the main thread*/
-                        func onInnerComplete(_ i_idx:Int, _ e_idx:Int){
-                            Swift.print("🍌 onInnerComplete i: \(i_idx) e: \(e_idx) 🍌")
-                            innerIdx += 1/*increment counter*/
-                            if(innerIdx == innerArr.count){
-                                innerIdx = 0//reset
-                                onOuterComplete(i_idx,e_idx)
-                            }
-                        }
-                        bg.async{/*do 2 things at the same time*/
-                            Swift.print("===🚗 inner async started e: \(e)===")
-                            sleep(IntParser.random(1, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
-                            main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
-                                onInnerComplete(i,e)
-                            }
+                    bg.async{/*do 2 things at the same time*/
+                        Swift.print("===🚗 inner async started e: \(e)===")
+                        sleep(IntParser.random(1, 6).uint32)/*simulates task that takes between 1 and 6 secs*/
+                        main.async{/*we must jump back on main thread, because we want to manipulate a variable that resids on the main thread*/
+                            onInnerComplete(i,e)
                         }
                     }
-                    
                 }
             }
         }
