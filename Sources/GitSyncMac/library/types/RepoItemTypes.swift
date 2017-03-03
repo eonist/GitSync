e6d3d47cd868ebe1dc4810613c1fbe623b6be8c9
Eonist <30n1st@gmail.com>
2017-03-03 17:59:05 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/types/RepoItemTypes.swift

--- Sources/GitSyncMac/library/types/RepoItemTypes.swift
+++ Sources/GitSyncMac/library/types/RepoItemTypes.swift
@@ -31,19 +31,5 @@ class RepoItemTypes{
         fileChange:false,
         pullToAutoSync:false
     )
-    static var emptyRepoItem:RepoItem = (
-        localPath:"",
-        interval:0,
-        branch:"",
-        keyChainItemName:"",
-        upload:false,
-        title:"",
-        download:false,
-        active:false,
-        remotePath:"",
-        autoSyncInterval:false,
-        autoCommitMessage:false,
-        fileChange:false,
-        pullToAutoSync:false
-    )
+    
 }
