59c072db6eb83a5f4a5f40bfd201d75d18c7da0f
Eonist <30n1st@gmail.com>
2017-03-03 18:13:53 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/types/RepoItem.swift

--- Sources/GitSyncMac/library/types/RepoItem.swift
+++ Sources/GitSyncMac/library/types/RepoItem.swift
@@ -1,5 +1,21 @@
 import Foundation
 
-class RepoItem {
+struct RepoItem {
 
 }
+
+/*static var emptyRepoItem:RepoItem = (
+ localPath:"",
+ interval:0,
+ branch:"",
+ keyChainItemName:"",
+ upload:false,
+ title:"",
+ download:false,
+ active:false,
+ remotePath:"",
+ autoSyncInterval:false,
+ autoCommitMessage:false,
+ fileChange:false,
+ pullToAutoSync:false
+ )*/
