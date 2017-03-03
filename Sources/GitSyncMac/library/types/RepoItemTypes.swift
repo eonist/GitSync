952d88a41e7418cff2e5bed0ac1a51cf429bd117
Eonist <30n1st@gmail.com>
2017-03-03 17:45:54 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/types/RepoItemTypes.swift

--- Sources/GitSyncMac/library/types/RepoItemTypes.swift
+++ Sources/GitSyncMac/library/types/RepoItemTypes.swift
@@ -1,7 +1,21 @@
 import Foundation
 
-typealias RepoItem = (localPath:String,interval:Int,branch:String,keyChainItemName:String,upload:Bool,title:String,download:Bool,active:Bool,remotePath:String,autoSyncInterval:Bool,autoCommitMessage:Bool,fileChange:Bool,pullToAutoSync:Bool)
-class RepoUtils{
+typealias RepoItem = (
+    localPath:String,
+    interval:Int,
+    branch:String,
+    keyChainItemName:String,
+    upload:Bool,
+    title:String,
+    download:Bool,
+    active:Bool,
+    remotePath:String,
+    autoSyncInterval:Bool,
+    autoCommitMessage:Bool,
+    fileChange:Bool,
+    pullToAutoSync:Bool
+)
+class RepoItemTypes{
     static var emptyRepoItem:RepoItem = (
         localPath:"",
         interval:0,
