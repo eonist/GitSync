fa3d61ba2ef1b3ca4783f7196be417a3c18fe928
Eonist <30n1st@gmail.com>
2017-03-04 21:54:14 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -18,7 +18,14 @@ class RepoDetailView:Element {
     /*LeverSpinner*/
     var autoSyncIntervalLeverSpinner:LeverSpinner?
     var container:Container?
-    
+    /*RubberBand*/
+    var mover:RubberBand?
+    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
+    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
+    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
+    /*Slider*/
+    var slider:VSlider?
+    var sliderInterval:CGFloat?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
         //Swift.print("RepoDetailView.width: " + "\(width)")
@@ -35,6 +42,17 @@ class RepoDetailView:Element {
         messageCheckBoxButton = container!.addSubView(CheckBoxButton(width, 32, "Auto message:", false, container))
         activeCheckBoxButton = container!.addSubView(CheckBoxButton(width, 32, "Active:", false, container))//if auto sync is off then a manual commit popup dialog will appear (with pre-populated text)
         intervalCheckBoxButton = container!.addSubView(CheckBoxButton(width, 32, "Interval:", false, container))
+        
+        /*RubberBand*/
+        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        let itemsRect = CGRect(0,0,width,ListParser.itemsHeight(self))/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        mover = RubberBand(Animation.sharedInstance,setProgress,frame,itemsRect)
+        mover!.event = onEvent/*Add an eventHandler for the mover object, avoids logging missing eventHandler, this has no functionality in this class, but may have in classes that extends this class*/
+        /*slider*/
+        sliderInterval = floor(ListParser.itemsHeight(self) - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/ListParser.itemsHeight(self), slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
     }
     /**
      * Populates the UI elements with data from the dp item
