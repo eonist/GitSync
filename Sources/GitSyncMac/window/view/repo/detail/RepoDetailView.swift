9e986d5bbd8e116e7c932df364f57f0261f33825
Eonist <30n1st@gmail.com>
2017-03-05 13:23:14 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -18,16 +18,7 @@ class RepoDetailView:Element/*,IRBScrollable,ISlidable*/ {
     /*LeverSpinner*/
     var autoSyncIntervalLeverSpinner:LeverSpinner?
     var lableContainer:Container?
-    /*RubberBand*/
-    var mover:RubberBand?
-    var prevScrollingDeltaY:CGFloat = 0/*this is needed in order to figure out which direction the scrollWheel is going in*/
-    var velocities:[CGFloat] = Array(repeating: 0, count: 10)/*represents the velocity resolution of the gesture movment*/
-    var progressValue:CGFloat?//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
-    /*Slider*/
-    var slider:VSlider?
-    var sliderInterval:CGFloat?
-    var itemsHeight:CGFloat = 600
-    var itemHeight:CGFloat = 24
+    
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
         //Swift.print("RepoDetailView.width: " + "\(width)")
@@ -44,27 +35,8 @@ class RepoDetailView:Element/*,IRBScrollable,ISlidable*/ {
         messageCheckBoxButton = lableContainer!.addSubView(CheckBoxButton(width, 32, "Auto message:", false, lableContainer))
         activeCheckBoxButton = lableContainer!.addSubView(CheckBoxButton(width, 32, "Active:", false, lableContainer))//if auto sync is off then a manual commit popup dialog will appear (with pre-populated text)
         intervalCheckBoxButton = lableContainer!.addSubView(CheckBoxButton(width, 32, "Interval:", false, lableContainer))
-        
-        /*RubberBand*/
-        let frame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-        let itemsRect = CGRect(0,0,width,itemsHeight)/*represents the total size of the content *///TODO: could be ranmed to contentRect
-        mover = RubberBand(Animation.sharedInstance,setProgress,frame,itemsRect)
-        mover!.event = onEvent/*Add an eventHandler for the mover object, avoids logging missing eventHandler, this has no functionality in this class, but may have in classes that extends this class*/
-        /*slider*/
-        let itemHeight:CGFloat = 24
-        sliderInterval = floor(itemsHeight - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
-        slider = addSubView(VSlider(itemHeight,height,0,0,self))
-        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
-        slider!.setThumbHeightValue(thumbHeight)
     }
-    /**
-     * NOTE: this method overides the Native NSView scrollWheel method
-     */
-    /*override func scrollWheel(with event: NSEvent) {
-     scroll(event)//forward the event to the scrollExtension
-     if(event.phase == NSEventPhase.changed){setProgress(mover!.result)}/*direct manipulation*/
-     super.scrollWheel(with: event)/*keep forwarding the scrollWheel event for NSViews higher up the hierarcy to listen to*/
-     }*/
+    
     /**
      * Modifies the dataProvider item on UI change
      */
