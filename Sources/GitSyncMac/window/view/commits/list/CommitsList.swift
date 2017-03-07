3a10c0aea138a7e2bbed76b67f3ae9a722999bb8
Eonist <30n1st@gmail.com>
2017-03-07 12:17:31 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -125,7 +125,7 @@ extension CommitsList{
      */
     func onProgress(){
         let value = mover!.result
-        Swift.print("CommitsList.onProgress() mover!.result: \(mover!.result) progressValue: \(progressValue!)  hasPulledAndReleasedBeyondRefreshSpace: \(hasPulledAndReleasedBeyondRefreshSpace)")
+        Swift.print("CommitsList.onProgress() mover!.result: \(mover!.result) progressValue: \(progressValue!)  hasPulledAndReleasedBeyondRefreshSpace: \(hasPulledAndReleasedBeyondRefreshSpace) isTwoFingersTouching \(isTwoFingersTouching)")
         Swift.print("value: " + "\(value)")
         if(value >  0 && value < 60){//between 0 and 60
             //Swift.print("start progressing the ProgressIndicator")
