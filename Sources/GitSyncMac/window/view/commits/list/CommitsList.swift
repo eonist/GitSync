34f18b9471ee4424366a29063bf7128116698e7d
Eonist <30n1st@gmail.com>
2017-03-07 12:57:29 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -78,7 +78,45 @@ class CommitsList:RBSliderFastList{
         onProgress()
     }
 }
+protocol ICommitList:IRBSlidable{
+    var isTwoFingersTouching:Bool {get set}
+    var progressIndicator:ProgressIndicator? {get set}
+    var hasPulledAndReleasedBeyondRefreshSpace:Bool{get set}
+    var hasReleasedBeyondTop:Bool {get set}
+    var autoSyncAndRefreshStartTime:NSDate? {get set}
+    func startAutoSync()
+    /*fast list related*/
+    func reUseAll()
+    func reUse(_ listItem:FastListItem)
+}
+extension ICommitList{
 
+    func scrollWheelEnter() {
+        Swift.print("CommitsList.scrollWheelEnter")
+        reUseAll()/*Refresh*/
+        isTwoFingersTouching = true
+        defaultScrollWheelEnter()
+    }
+    func scrollWheelExit(){
+        Swift.print("CommitList.scrollWheelExit()")
+        isTwoFingersTouching = false
+        let value = mover!.result
+        if(value > 60){
+            //Swift.print("start animation the ProgressIndicator")
+            mover!.frame.y = 60
+            progressIndicator!.start()//1. start spinning the progressIndicator
+            hasPulledAndReleasedBeyondRefreshSpace = true
+            autoSyncAndRefreshStartTime = NSDate()//init debug timer
+            startAutoSync()/*🚪⬅️️ <- starts the process of downloading commits here*/
+        }else if (value > 0){
+            hasReleasedBeyondTop = true
+            //scrollController!.mover.topMargin = 0
+        }else{
+            hasReleasedBeyondTop = false
+        }
+    }
+
+}
 extension CommitsList{
     /**
      * Basically not in refreshState
