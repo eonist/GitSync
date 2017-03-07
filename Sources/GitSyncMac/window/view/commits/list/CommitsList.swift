fd48d06680eaa342844a034f88291f5fa2550278
Eonist <30n1st@gmail.com>
2017-03-07 13:09:45 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -52,6 +52,7 @@ class CommitsList:RBSliderFastList,ICommitList{
             hasPulledAndReleasedBeyondRefreshSpace = false//reset
             isInDeactivateRefreshModeState = false//reset
         }
+        fatalError("debug")
     }
     override func onEvent(_ event:Event) {
         //Swift.print("CommitsList.onEvent() event.type: " + "\(event.type)")
