c51d7f7f461d61bcb51f45b1b06601a46659ba67
Eonist <30n1st@gmail.com>
2017-03-07 16:40:32 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -21,7 +21,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         //initApp()
         
-        let a:[Int] = ["2",CGFloat(3),Int(4)].reduce{0,$0.int}
+        let a:[Int] = ["2",CGFloat(3),Int(4)].reduce{0,
+            if($0 is Int){
+                return $0 as! Int)
+            }else if($0 is CGFloat){
+                return $0 as! CGFloat
+            }else{
+                return $0 as! String
+            }
+        }
         Swift.print("a: " + "\(a)")
         //Continue here:
             //Keep the current RepoDetail design, just make it scrollable.
