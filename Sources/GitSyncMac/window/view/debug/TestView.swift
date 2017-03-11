d19ae0f97b0ac8a2095e8375456b7305c21de7a3
Eonist <30n1st@gmail.com>
2017-03-11 23:26:22 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/debug/TestView.swift

--- Sources/GitSyncMac/window/view/debug/TestView.swift
+++ Sources/GitSyncMac/window/view/debug/TestView.swift
@@ -37,7 +37,7 @@ class TestView:Element {
         let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
         let dp:DataProvider = DataProvider(xml)
         //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
-        let list = self.addSubView(RBSliderList(140, 73, 24, dp, self))
+        let list = self.addSubView(DEPRECATED_RBSliderList(140, 73, 24, dp, self))
         _ = list
     }
     /**
@@ -47,7 +47,7 @@ class TestView:Element {
         let xml = FileParser.xml("~/Desktop/assets/xml/longlist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
         let dp:DataProvider = DataProvider(xml)
         //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
-        let list = self.addSubView(RBSliderFastList(140, 145, 24, dp, self))
+        let list = self.addSubView(DEPRECATED_RBSliderFastList(140, 145, 24, dp, self))
         _ = list
     }
     /**
