0d8e0a9f916a78ae6ca4c28a2c05d3d354bf0364
Eonist <30n1st@gmail.com>
2017-03-11 12:22:39 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/ListTransitionTestView.swift

--- GitSyncMacApp/ListTransitionTestView.swift
+++ GitSyncMacApp/ListTransitionTestView.swift
@@ -268,7 +268,7 @@ class ListTransitionTestView:TitleView{
             //use DisplacableView, because SliderList will use List. SliderList needs to get trais from Slideable etc
         
         let dp = DataProvider(FileParser.xml("~/Desktop/ElCapitan/assets/xml/scrollist.xml".tildePath))/*Loads xml from a xml file on the desktop*/
-        let list = self.addSubView(List(140, 144, NaN, dp,self))
+        let list = self.addSubView(List2(140, 144, NaN, dp,self))
         _ = list
     }
     required init(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
