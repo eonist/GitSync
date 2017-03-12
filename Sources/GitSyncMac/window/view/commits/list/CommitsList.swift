58a656ceb23a733596be9dad2906010fc10c30db
Eonist <30n1st@gmail.com>
2017-03-12 11:34:33 +0100

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/window/view/commits/list/CommitsList.swiftSources/GitSyncMac/window/view/commits/list/ICommitList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -68,35 +68,6 @@ class CommitsList: ElasticSlideScrollFastList,ICommitList{
     }
 }
 
-extension ICommitList{
-    
-    //these are just add hock methods, you can just adhock them with the scrollWheel method, only 2 lines of code 
-    
-    func scrollWheelEnter() {
-        Swift.print("CommitsList.scrollWheelEnter")
-        reUseAll()/*Refresh*/
-        isTwoFingersTouching = true
-        defaultScrollWheelEnter()
-    }
-    func scrollWheelExit(){
-        Swift.print("CommitList.scrollWheelExit()")
-        isTwoFingersTouching = false
-        let value = mover!.result
-        if(value > 60){
-            //Swift.print("start animation the ProgressIndicator")
-            mover!.frame.y = 60
-            progressIndicator!.start()//1. start spinning the progressIndicator
-            hasPulledAndReleasedBeyondRefreshSpace = true
-            autoSyncAndRefreshStartTime = NSDate()//init debug timer
-            startAutoSync()/*🚪⬅️️ <- starts the process of downloading commits here*/
-        }else if (value > 0){
-            hasReleasedBeyondTop = true
-            //scrollController!.mover.topMargin = 0
-        }else{
-            hasReleasedBeyondTop = false
-        }
-    }
-}
 extension CommitsList{
     /**
      * Starts the auto sync process
--- Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
+++ Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
@@ -9,3 +9,32 @@ protocol ICommitList:IFastList2 {
     var autoSyncAndRefreshStartTime:NSDate? {get set}
     func startAutoSync()
 }
+extension ICommitList{
+    
+    //these are just add hock methods, you can just adhock them with the scrollWheel method, only 2 lines of code
+    
+    func scrollWheelEnter() {
+        Swift.print("CommitsList.scrollWheelEnter")
+        reUseAll()/*Refresh*/
+        isTwoFingersTouching = true
+        defaultScrollWheelEnter()
+    }
+    func scrollWheelExit(){
+        Swift.print("CommitList.scrollWheelExit()")
+        isTwoFingersTouching = false
+        let value = mover!.result
+        if(value > 60){
+            //Swift.print("start animation the ProgressIndicator")
+            mover!.frame.y = 60
+            progressIndicator!.start()//1. start spinning the progressIndicator
+            hasPulledAndReleasedBeyondRefreshSpace = true
+            autoSyncAndRefreshStartTime = NSDate()//init debug timer
+            startAutoSync()/*🚪⬅️️ <- starts the process of downloading commits here*/
+        }else if (value > 0){
+            hasReleasedBeyondTop = true
+            //scrollController!.mover.topMargin = 0
+        }else{
+            hasReleasedBeyondTop = false
+        }
+    }
+}
