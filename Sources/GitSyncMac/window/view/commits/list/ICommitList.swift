77fbd9337857717c7dc3bcc765c2cc27b8b0b284
Eonist <30n1st@gmail.com>
2017-03-12 11:32:29 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/ICommitList.swift

--- Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
+++ Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
@@ -1,13 +1,11 @@
 import Foundation
 
-protocol ICommitList: DEPRECATEDIRBSlidable {
+protocol ICommitList:IFastList2 {
+    /*Related to ICommitList*/
     var isTwoFingersTouching:Bool {get set}
     var progressIndicator:ProgressIndicator? {get set}
     var hasPulledAndReleasedBeyondRefreshSpace:Bool{get set}
     var hasReleasedBeyondTop:Bool {get set}
     var autoSyncAndRefreshStartTime:NSDate? {get set}
     func startAutoSync()
-    /*fast list related*/
-    func reUseAll()
-    func reUse(_ listItem:FastListItem)
 }
