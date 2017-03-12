fce12e13682a3b7e8be32446f01758ddc16c3613
Eonist <30n1st@gmail.com>
2017-03-12 16:10:51 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/ICommitList.swift

--- Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
+++ Sources/GitSyncMac/window/view/commits/list/ICommitList.swift
@@ -99,7 +99,7 @@ extension ICommitList{
      * TODO: Spring back motion shouldn't produce ProgressIndicator, only pull should
      */
     func iterateProgressBar(_ value:CGFloat){//TODO: rename to iterateProgressBar
-        Swift.print("🌵 ICommitsList.onProgress()")
+        Swift.print("🌵 ICommitsList.iterateProgressBar(\(value))")
         //let value = mover!.result
         Swift.print("CommitsList.onProgress() mover!.result: \(mover!.result) progressValue: \(progressValue!)  hasPulledAndReleasedBeyondRefreshSpace: \(hasPulledAndReleasedBeyondRefreshSpace) isTwoFingersTouching \(isTwoFingersTouching)")
         Swift.print("value: " + "\(value)")
