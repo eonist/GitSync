94af16116507f09860079182d1463e5e5f9c6222
Eonist <30n1st@gmail.com>
2017-03-12 11:30:22 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -67,17 +67,7 @@ class CommitsList: ElasticSlideScrollFastList,ICommitList{
         onProgress()
     }
 }
-protocol ICommitList: DEPRECATEDIRBSlidable {
-    var isTwoFingersTouching:Bool {get set}
-    var progressIndicator:ProgressIndicator? {get set}
-    var hasPulledAndReleasedBeyondRefreshSpace:Bool{get set}
-    var hasReleasedBeyondTop:Bool {get set}
-    var autoSyncAndRefreshStartTime:NSDate? {get set}
-    func startAutoSync()
-    /*fast list related*/
-    func reUseAll()
-    func reUse(_ listItem:FastListItem)
-}
+
 extension ICommitList{
     
     //these are just add hock methods, you can just adhock them with the scrollWheel method, only 2 lines of code 
