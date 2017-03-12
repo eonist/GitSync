8929af7ee45d4b5559615db5b6ca7eddfa87e49b
Eonist <30n1st@gmail.com>
2017-03-12 11:28:05 +0100

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 @testable import Element
 
-class CommitsList: DEPRECATED_RBSliderFastList,ICommitList{
+class CommitsList: ElasticSlideScrollFastList,ICommitList{
     /*The following variables exists to facilitate the pull to refresh functionality*/
     var progressIndicator:ProgressIndicator?
     var hasPulledAndReleasedBeyondRefreshSpace:Bool = false
