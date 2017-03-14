ba9c7e173163ad8abe1f571beaacc5ca5e391e48
Eonist <30n1st@gmail.com>
2017-03-14 11:12:32 +0100

    'Files modified: 1'
    '
    
    Modified 1 file:
    Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
    '

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -25,7 +25,6 @@ class RepoDetailView: ElasticSlideScrollView {
     override func resolveSkin() {
         super.resolveSkin()/*self.skin = SkinResolver.skin(self)*/
         //Swift.print("RepoDetailView.width: " + "\(width)")
-        
         nameTextInput = lableContainer!.addSubView(TextInput(width, 32, "Name: ", "", lableContainer))
         localPathTextInput = lableContainer!.addSubView(TextInput(width, 32, "Local-path: ", "", lableContainer))
         remotePathTextInput = lableContainer!.addSubView(TextInput(width, 32, "Remote-path: ", "", lableContainer))
