9d330514df48c3b8cd72e0fb31cad7a26d714117
Eonist <30n1st@gmail.com>
2017-03-15 11:12:36 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph2.swift

--- GitSyncMacApp/lib/graph2/Graph2.swift
+++ GitSyncMacApp/lib/graph2/Graph2.swift
@@ -1,5 +1,37 @@
 import Foundation
-
-class Graph2 {
-
+@testable import Element
+@testable import Utils
+/**
+ * New Graph component (dynamic/zoomable/slidable/snappable) (year/month/day/hour)
+ * Inspiration from DrawLab
+ */
+class Graph2:ElasticScrollView{
+    override func resolveSkin() {
+        super.resolveSkin()
+    }
+    //bottomBar
+    //rightBar
+    //graphLine
+    //graphPoint
+    
 }
+
+//Pinch to zoom
+//slidable in x-axis
+//bounce back x-axis
+//bounce back on zoom min and max
+//Snappable x-axis (possibly snappable z-axis)
+//1. try with fake date data first (14 days at 7 days resolution)
+//2. you need to adjust the graph in the visible time span. 
+    //from day 5 to 12 the range is 0...122, but for 6-13 its 0...96
+    //you must adjust rightBar according to the interpolated high between 2 time units. 
+//3. When you zoom you reach a threshold between time units, day to hour for instance. 
+    //when the threshold is reached you animate the transition in the graphLine by:
+    //See old notes about day units, and how many to use. 5, 9, 6, 7, 12
+    //the day GraphDot becomes the EOD in hour mode, so graphline tesselate around this point
+    //The graph Dot grows in size to reveal it self 0.2 sec anim, and shrinks if removed etc.
+    //The graph dots animate to their destination regardless of zooming, zoom only zooms the canvas, graph can still move.
+    //
+
+
+    
