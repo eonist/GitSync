801adb1a4c5ad839f97e9996e40729921cee6468
Eonist <30n1st@gmail.com>
2017-03-15 15:41:20 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/ElasticView.swift

--- GitSyncMacApp/lib/graph2/ElasticView.swift
+++ GitSyncMacApp/lib/graph2/ElasticView.swift
@@ -21,7 +21,7 @@ class ElasticView:Element{
         layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
         
         maskFrame = CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
-        contentFrame = CGRect(0,0,width,600)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        contentFrame = CGRect(0,0,width,height)/*represents the total size of the content *///TODO: could be ranmed to contentRect
         mover = RubberBand(Animation.sharedInstance,setProgress/*👈important*/,maskFrame,contentFrame)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
     
