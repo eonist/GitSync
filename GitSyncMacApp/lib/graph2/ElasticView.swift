4f5d5fa015496ea7a0add55fcb9da3cb44d8fa0f
Eonist <30n1st@gmail.com>
2017-03-15 15:27:03 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/ElasticView.swift

--- GitSyncMacApp/lib/graph2/ElasticView.swift
+++ GitSyncMacApp/lib/graph2/ElasticView.swift
@@ -29,12 +29,24 @@ class ElasticView:Element{
     /**
      *
      */
-    func setProgress(_ value:CGFloat){
-        Swift.print("value: " + "\(value)")
+    func setProgress(_ value:CGFloat){//DIRECT TRANSMISSION 💥
+        Swift.print("Elastic2.setProgress() value: " + "\(value)")
+        contentContainer!.frame.y = value/*<--this is where we actully move the labelContainer*/
+        //the bellow var may not be need to be set
+        iterimScroll.progressValue = value / -(contentFrame.height - height)/*get the the scalar values from value.*/
     }
     
     override func scrollWheel(with event: NSEvent) {
         Swift.print("scrollWheel")
+        switch event.phase{
+            case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
+            case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
+            case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesnt fire if you begin the scrollWheel gesture very quickly*/
+            case NSEventPhase.ended:onScrollWheelExit();//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
+            case NSEventPhase.cancelled:onScrollWheelExit();//Swift.print("cancelled")/*this trigers if the scrollWhell gestures goes off the trackpad etc*/
+            case NSEventPhase(rawValue:0):onInDirectScrollWheelChange(event);/*Swift.print("none");*/break;//swift 3 update, was -> NSEventPhase.none
+            default:break;
+        }
         super.scrollWheel(with: event)
     }
     
