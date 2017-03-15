fa1c9f4619dc3d05e0d8db2702e40b5891193111
Eonist <30n1st@gmail.com>
2017-03-15 19:00:29 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/ElasticView.swift

--- GitSyncMacApp/lib/graph2/ElasticView.swift
+++ GitSyncMacApp/lib/graph2/ElasticView.swift
@@ -85,7 +85,8 @@ class ElasticView:Element{
         Swift.print("zoom: \(zoom)")
         Swift.print("self.localPos(): " + "\(self.localPos())")
         Swift.print("tempPagePos: " + "\(tempPagePos)")
-        let newPos:CGPoint = PointModifier.scale(tempPagePos!, self.localPos(), CGPoint(zoom,zoom))/*<--the 1 is needed because the zoom value is additative*/
+        let relativeZoom:CGFloat = (zoom-prevMagnificationValue)
+        let newPos:CGPoint = PointModifier.scale(tempPagePos!, self.localPos(), CGPoint(relativeZoom,relativeZoom))/*<--the 1 is needed because the zoom value is additative*/
         Swift.print("newPos: " + "\(newPos)")
         zoomContainer!.point = newPos
         
