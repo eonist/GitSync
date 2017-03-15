31c0fc6b9bd99b243a0a8a1ae271503ee7216cce
Eonist <30n1st@gmail.com>
2017-03-15 16:43:34 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph2.swift

--- GitSyncMacApp/lib/graph2/Graph2.swift
+++ GitSyncMacApp/lib/graph2/Graph2.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 @testable import Element
 @testable import Utils
 /**
@@ -34,7 +34,41 @@ class Graph2:ElasticView{
         
         /*Debug*/
         gestureHUD = GestureHUD(self)
+        self.acceptsTouchEvents = true/*Enables gestures*/
+        self.wantsRestingTouches = true/*Makes sure all touches are registered. Doesn't register when used in playground*/
     }
+    /**
+     * Detects when touches are made
+     * NOTE: event.localPos(self) equals the pos of the mouseCursor
+     */
+    override func touchesBegan(with event:NSEvent) {
+        //Swift.print("touchesBeganWithEvent: " + "\(event)")
+        /*Debug*/
+        gestureHUD!.touchesBegan(event)
+    }
+    /**
+     * Detects if a two finger left or right swipe has occured
+     */
+    override func touchesMoved(with event:NSEvent) {
+        //Swift.print("touchesMovedWithEvent: " + "\(event)")
+        /*Debug*/
+        gestureHUD!.touchesMoved(event)
+    }
+    /**
+     * NOTE: playground doesnt fire a touch up when there is only one touch detected. to work aroudn this limitation you have to detect any touch and then when there are only 2, delete all debugCircs
+     */
+    override func touchesEnded(with event:NSEvent) {//for debugging
+        Swift.print("touchesEndedWithEvent: " + "\(event)")
+        //Swift.print("event.phase.type: " + "\(event.phase.type)" + " event.phase: " + "\(event.phase)")
+        /*Debug*/
+        gestureHUD!.touchesEnded(event)
+    }
+    override func touchesCancelled(with event:NSEvent) {//for debugging
+        Swift.print("touchesCancelledWithEvent: " + "\(event)")
+        super.touchesCancelled
+        
+    }
+    
     //bottomBar
     //rightBar
     //graphLine
