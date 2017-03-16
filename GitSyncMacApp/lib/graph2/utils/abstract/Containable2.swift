5a4e360f9fd6e68bb0571af43e865660e0f4a3ad
Eonist <30n1st@gmail.com>
2017-03-16 16:11:32 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/Containable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/Containable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/Containable2.swift
@@ -2,11 +2,9 @@ import Foundation
 @testable import Element
 @testable import Utils
 
-protocol Containable2:class {//TODO: RENAME TO displaceable
-    var maskFrame:CGFloat{get}//used to represent the maskHeight aka the visible part.
-    var itemSize:CGSize{get}//item of one item, used to calculate interval
-    var itemsHeight:CGFloat{get}//total height of the items
-    
+protocol Containable2:class {
+    var maskSize:CGSize{get}
+    var contentSize:CGSize{get}
+    var itemSize:CGSize{get}
     var contentContainer:Element? {get}
-    //func setProgress(_ progress:CGFloat)
 }
