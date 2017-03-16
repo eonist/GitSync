a50ecdf9ba2838fe41ce83b26819fc8f3abc2669
Eonist <30n1st@gmail.com>
2017-03-16 16:05:28 +0100

    New files added: 1, Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swift
@@ -1,10 +1,8 @@
 import Foundation
+@testable import Element
+@testable import Utils
 
 protocol Elastic2 {
     var mover:RubberBand?{get set}
-    var prevScrollingDeltaY:CGFloat{get set}
-    var velocities:[CGFloat]{get set}
-    //⚠️️ you may be able to remove progressvalue in the future. as it works differently now!=!=?
-    var progressValue:CGFloat?{get set}//<--same as progress but unclamped (because RBSliderList may go beyond 0 to 1 values etc)
     var iterimScroll:InterimScroll{get set}
 }
---
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScroll2.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+class ElasticScroll2 {
+
+}
