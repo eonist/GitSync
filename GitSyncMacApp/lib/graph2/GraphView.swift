0bf3c04cbf7d5a3becc4e57401dabee9c11f2299
Eonist <30n1st@gmail.com>
2017-03-16 12:47:16 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/GraphView.swift

--- GitSyncMacApp/lib/graph2/GraphView.swift
+++ GitSyncMacApp/lib/graph2/GraphView.swift
@@ -89,6 +89,7 @@ extension GraphView{
         css +=    "line-alpha:1;"
         css +=    "line-thickness:0.5px;"
         css += "}"
+        StyleManager.addStyle(css)
         typealias P = CGPoint
         let points:[P] = [P(0,0),P(50,300),P(100,50),P(150,350),P(200,250)]
         let path:IPath = PolyLineGraphicUtils.path(points)
