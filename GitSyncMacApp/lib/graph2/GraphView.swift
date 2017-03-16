73ce1f9383d1a86d3ad68cdc79dad3b8386905f7
Eonist <30n1st@gmail.com>
2017-03-16 11:24:52 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/GraphView.swift

--- GitSyncMacApp/lib/graph2/GraphView.swift
+++ GitSyncMacApp/lib/graph2/GraphView.swift
@@ -1,13 +1,33 @@
-import Foundation
+import Cocoa
 @testable import Element
 @testable import Utils
 class GraphView:Element{
-    //TimeBar
-    //ValueBar
-    //GraphLine2
-    //HorizontalView
+    var contentContainer:Element?
+    
     override func resolveSkin() {
         //StyleManager.addStyle("GraphView{fill:green;}")
         super.resolveSkin()
+        contentContainer = addSubView(Container(width,height,self,"content"))
+    }
+    override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
+        //scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
+        super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
+        switch event.phase{
+        case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
+        case NSEventPhase(rawValue:0):onScrollWheelChange(event);/*Swift.print("none");*/break;//swift 3 update, was -> NSEventPhase.none
+        default:break;
+        }
+    }
+    /**
+     * NOTE: Basically when you perform a scroll-gesture on the touch-pad
+     */
+    func onScrollWheelChange(_ event:NSEvent) {/*Direct scroll, not momentum*/
+        Swift.print("📜 Scrollable.onScrollWheelChange: \(event.type)")
+        let progressVal:CGFloat = SliderListUtils.progress(event.deltaY, interval, progress)
+        setProgress(progressVal)
     }
 }
+//TimeBar
+//ValueBar
+//GraphLine2
+//HorizontalView
