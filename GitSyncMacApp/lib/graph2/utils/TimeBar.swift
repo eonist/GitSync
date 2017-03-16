5df453241c6268cc25ff69a2009d7006b3a9d992
Eonist <30n1st@gmail.com>
2017-03-16 13:32:58 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/TimeBar.swift

--- GitSyncMacApp/lib/graph2/utils/TimeBar.swift
+++ GitSyncMacApp/lib/graph2/utils/TimeBar.swift
@@ -5,6 +5,16 @@ import Foundation
 class TimeBar:Element {
     override func resolveSkin() {
         super.resolveSkin()
-        
+        let spaceX:CGFloat = 100
+        for i in 0..<20{
+            let x:CGFloat = (i*spaceX)
+            let str:String = x.string
+            //Swift.print("str: " + "\(str)")
+            let textArea:TextArea = TextArea(NaN,NaN,str,self)
+            _ = addSubView(textArea)
+            //Swift.print("CGPoint(x,0): " + "\(CGPoint(x,0))")
+            textArea.setPosition(CGPoint(x,0))
+            
+        }
     }
 }
