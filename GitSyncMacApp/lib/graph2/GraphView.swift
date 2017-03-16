b5160485dc33e93066510af1aa59c7f3fc8b5192
Eonist <30n1st@gmail.com>
2017-03-16 11:34:51 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/GraphView.swift

--- GitSyncMacApp/lib/graph2/GraphView.swift
+++ GitSyncMacApp/lib/graph2/GraphView.swift
@@ -9,13 +9,13 @@ class GraphView:Element{
     var progress:CGFloat{return SliderParser.progress(contentContainer!.y, height, itemsHeight)}
     
     override func resolveSkin() {
-        //StyleManager.addStyle("GraphView{fill:green;}")
+        StyleManager.addStyle("GraphView{fill:green;fill-alpha:0;}")
         super.resolveSkin()
         contentContainer = addSubView(Container(width,height,self,"content"))
-        
         addEllipse()
     }
     override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
+        Swift.print("scrollWheel: ")
         //scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
         super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
         switch event.phase{
@@ -45,7 +45,32 @@ class GraphView:Element{
 }
 
 extension GraphView{
-    
+    /**
+     *
+     */
+    func addEllipse(){
+        let gradient = LinearGradient(Gradients.blue(),[],π/2)
+        let lineGradient = LinearGradient(Gradients.deepPurple(0.5),[],π/2)
+        /*Styles*/
+        let fill:GradientFillStyle = GradientFillStyle(gradient);
+        let lineStyle = LineStyle(20,NSColorParser.nsColor(Colors.green()).alpha(0.5),CGLineCap.round)
+        let line = GradientLineStyle(lineGradient,lineStyle)
+        
+        let objSize:CGSize = CGSize(200,200)
+        Swift.print("objSize: " + "\(objSize)")
+        let viewSize:CGSize = CGSize(width,height)
+        Swift.print("viewSize: " + "\(viewSize)")
+        let p = Align.alignmentPoint(objSize, viewSize, Alignment.centerCenter, Alignment.centerCenter,CGPoint())
+        Swift.print("p: " + "\(p)")
+        
+        /*let rect = RectGraphic(0,0,width,height,fill,line)
+         zoomContainer!.addSubview(rect.graphic)
+         rect.draw()*/
+        
+        let ellipse = EllipseGraphic(p.x,p.y,200,200,fill.mix(Gradients.green()),line.mix(Gradients.lightGreen(0.5)))
+        contentContainer!.addSubview(ellipse.graphic)
+        ellipse.draw()
+    }
 }
 //TimeBar
 //ValueBar
