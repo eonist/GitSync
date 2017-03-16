880f00c288d5bc8aa7e8e93a23a21aac50326064
Eonist <30n1st@gmail.com>
2017-03-16 16:45:42 +0100

    Files modified: 2
    Modified 2 files:GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swiftGitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/Elastic2.swift
@@ -9,6 +9,6 @@ protocol Elastic2:Containable2{
 extension Elastic2{
     func setProgress(_ value:CGFloat){
         contentContainer!.frame.x = value/*<--this is where we actully move the labelContainer*/
-        progressValue = value / -(contentSize.width - maskSize.width)/*get the the scalar values from value.*/
+        //progressValue = value / -(contentSize.width - maskSize.width)/*get the the scalar values from value.*/
     }
 }
--- GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
@@ -1,8 +1,55 @@
-import Foundation
+import Cocoa
+@testable import Element
+@testable import Utils
 
-protocol ElasticScrollable2 {
+protocol ElasticScrollable2:Elastic2, Scrollable2 {
     
 }
 extension ElasticScrollable2{
-    
+    func setX(_ value:CGFloat){
+        contentContainer!.frame.x = value
+    }
+    func onScrollWheelChange(_ event:NSEvent){
+        Swift.print("👻📜 (ElasticScrollable).onScrollWheelChange : \(event.type)")
+        iterimScroll.prevScrollingDelta = event.scrollingDeltaX
+        _ = iterimScroll.velocities.pushPop(event.scrollingDeltaX)
+        mover!.value += event.scrollingDeltaX
+        mover!.updatePosition()
+        setX(mover!.result)//new ⚠️️
+    }
+    /**
+     * NOTE: Basically when you enter your scrollWheel gesture
+     */
+    func onScrollWheelEnter(){
+        Swift.print("👻📜 (ElasticScrollable).onScrollWheelEnter")
+        //Swift.print("IRBScrollable.onScrollWheelDown")
+        mover!.stop()
+        mover!.hasStopped = true
+        iterimScroll.prevScrollingDelta = 0
+        mover!.isDirectlyManipulating = true
+        iterimScroll.velocities = Array(repeating: 0, count: 10)
+        //⚠️️scrollWheelEnter()
+    }
+    /**
+     * NOTE: Basically when you release your scrollWheel gesture
+     */
+    func onScrollWheelExit(){
+        Swift.print("👻📜 (ElasticScrollable).onScrollWheelExit")
+        //Swift.print("IRBScrollable.onScrollWheelUp")
+        mover!.hasStopped = false
+        mover!.isDirectlyManipulating = false
+        mover!.value = mover!.result
+        Swift.print("prevScrollingDeltaY: " + "\(iterimScrollY.prevScrollingDelta)")
+        
+        /*X*/
+        if(iterimScroll.prevScrollingDelta != 1.0 && iterimScroll.prevScrollingDelta != -1.0){/*Not 1 and not -1 indicates that the wheel is not stationary*/
+            var velocity:CGFloat = 0
+            if(iterimScroll.prevScrollingDelta > 0){velocity = NumberParser.max(iterimScroll.velocities)}/*Find the most positive velocity value*/
+            else{velocity = NumberParser.min(iterimScroll.velocities)}/*Find the most negative velocity value*/
+            mover!.velocity = velocity/*set the mover velocity to the current mouse gesture velocity, the reason this can't be additive is because you need to be more immediate when you change direction, this could be done by assering last direction but its not a priority atm*///td try the += on the velocity with more rects to see its effect
+            mover!.start()/*start the frameTicker here, do this part in parent view or use event or Selector*/
+        }else{/*stationary*/
+            mover!.start()/*This needs to start if your in the overshoot areas, if its not in the overshoot area it will just stop after a frame tick*/
+        }
+    }
 }
