bd422076827156e2390829aa8cd08ebf3986d318
Eonist <30n1st@gmail.com>
2017-03-17 13:15:12 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/ContaierView2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/ContaierView2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ContaierView2.swift
@@ -1,5 +1,12 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
 class ContaierView2:Containable2 {
-
+    var maskSize:CGSize = CGSize()
+    var contentSize:CGSize = CGSize()
+    var contentContainer:Element?
+    var itemSize:CGSize {fatalError("must be overriden")}//override this for custom value
+    var interval:CGFloat{fatalError("must be overriden")}
+    var progress:CGFloat{fatalError("must be overriden")}
 }
