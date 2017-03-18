31aa04871e18c5eede32261a035e3be0bc9bbf7c
Eonist <30n1st@gmail.com>
2017-03-18 12:48:26 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/CurveGraph.swift

--- GitSyncMacApp/lib/graph2/CurveGraph.swift
+++ GitSyncMacApp/lib/graph2/CurveGraph.swift
@@ -39,12 +39,12 @@ extension CurveGraph{
         var commands:[Int] = [PathCommand.moveTo]
         let y0:CGFloat = (0..<h).random.cgFloat
         var pathData:[CGFloat] = [0,y0]
-        
-        (0...5).forEach{ i in
+        var prevEnd:P = 
+        (1...5).forEach{ i in
             let x:CGFloat = w * i
             let y:CGFloat = (0..<h).random.cgFloat
             let a:P = P(x,y)
-            let cp1:P = P(a.x-rad,a.y)
+            let cp1:P = P(a.x+rad,a.y)
             let cp2:P = P(a.x+rad,a.y)
             pathData += [a.x,a.y,cp1.x,cp1.y,cp2.x,cp2.y]
             let cmd:Int = PathCommand.cubicCurveTo
