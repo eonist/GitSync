697be60e1d5417135634a1c785e0a446fa24d714
Eonist <30n1st@gmail.com>
2017-03-20 13:27:52 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph4.swift

--- GitSyncMacApp/lib/graph2/Graph4.swift
+++ GitSyncMacApp/lib/graph2/Graph4.swift
@@ -1,34 +1,6 @@
 import Foundation
 
-//Final concept: 
 
-//1. The TimeIndicator: (Top center)
-    //shows the time range. 
-    //Iterates on scroll
-
-//2. The TimeBar (bottom)
-    //ElasticSlidable
-    //Elegantly Snaps to whole values (avoids masked out text, and enables elegant graph.y anims)
-    //7 values
-
-//3. The ValueBar (left)
-    //Changes onGraphAnimComplete
-    //5 values
-
-//4. The graph (center)
-    //Animates onScrollMomentumComplete /onSnapToPositionComplete
-    //7 GraphPoints for (hours,days,weeks,months,years)
-    //Zoom in and out of timeUnitStates via pinch zoom gesture.
-    //Zoom in of the timeRange that is hovered above
-
-//5. Data handeling:
-    //calculate Git values for Graph onSnapToPositionComplete, onViewLoad, timer 👌
-    //When a new TimeUnit occures. from 16 to 17 for instance, or Mon to Tue. Then Animate to the new TimeUnit if graph was pinned to the right 👌
-    //
-
-//6. Future improvments: 
-    //Cache the Git values for an even more responsive feel. also saves cpu/network
-    //
 
 class Graph4 {
 
