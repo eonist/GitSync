d3f30df5c7077e260f7d295091918b9ffb962aab
Eonist <30n1st@gmail.com>
2017-03-20 13:04:38 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph4.swift

--- GitSyncMacApp/lib/graph2/Graph4.swift
+++ GitSyncMacApp/lib/graph2/Graph4.swift
@@ -2,9 +2,18 @@ import Foundation
 
 //Final concept: 
 
-//1. The TimeIndicator:
-    
+//1. The TimeIndicator: (Top center)
+    //shows the time range. 
+    //Iterates on scroll
 
+//2. The TimeBar (bottom)
+    //ElasticSlidable
+    //Elegantly Snaps to whole values
+    //7 values
+
+//3. The ValueBar (left)
+    //Changes onGraph anim complete
+    //5 values
 class Graph4 {
 
 }
