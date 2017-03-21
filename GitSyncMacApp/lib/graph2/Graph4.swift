379f49008ba832d8d6da289edeee513cf8b5ada5
Eonist <30n1st@gmail.com>
2017-03-21 11:30:48 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph4.swift

--- GitSyncMacApp/lib/graph2/Graph4.swift
+++ GitSyncMacApp/lib/graph2/Graph4.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 //slide with momentum (Try to use the momentum delta for velocity!?!?)
-//magnetic points. Or friction slows down but stop 
+//magnetic points. Or friction slows down but stops applying friction unitl the value is (value % space) == 0 at which point it stops
 
 
 class Graph4 {
