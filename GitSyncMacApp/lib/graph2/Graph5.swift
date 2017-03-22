4d3e3b0f66d4e8e82c39acbd9562251b548e5ef1
Eonist <30n1st@gmail.com>
2017-03-22 11:03:58 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -1,5 +1,16 @@
 import Foundation
 
-class Graph5 {
+//the next experiment is: (tomorrow )
 
+    //try to add the long TimeBar to a ScrollView that uses snap friction, then throw it around and make it work
+        //make an indicator mark that the TimeBar must align to.
+        //add line sperators in the TimeBar aswell.
+        //basically you need to not use the native momentum but your own. 
+            //this presents a good opertunity to also try to implement transferal of velocity from native momentum
+    //when valuebar settles redraw the graph
+
+    //then try elasticView with snapfriction
+
+class Graph5 {
+    
 }
