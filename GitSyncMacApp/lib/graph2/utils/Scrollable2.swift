878d6adbea65a779588ab130939102218bfacba2
Eonist <30n1st@gmail.com>
2017-03-22 14:26:12 +0100

    Files modified: 2
    Modified 2 files:GitSyncMacApp/lib/graph2/utils/Scrollable2.swiftGitSyncMacApp/lib/graph2/utils/abstract/ContainerView2.swift

--- GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
@@ -1,9 +1,26 @@
 import Cocoa
 @testable import Element
 @testable import Utils
+
 protocol Scrollable2:Containable2 {
     func onScrollWheelChange(_ event:NSEvent)
     func onScrollWheelEnter()
     func onScrollWheelExit()
 }
 
+extension Scrollable2{
+    /**
+     *
+     */
+    func scroll(_ event:NSEvent){
+        switch event.phase{
+            case NSEventPhase.changed:onScrollWheelChange(event)
+            case NSEventPhase.mayBegin:onScrollWheelEnter()
+            case NSEventPhase.began:onScrollWheelEnter()
+            case NSEventPhase.ended:onScrollWheelExit();
+            case NSEventPhase.cancelled:onScrollWheelExit();
+            case NSEventPhase(rawValue:0):onScrollWheelChange(event);
+            default:break;
+        }
+    }
+}
--- GitSyncMacApp/lib/graph2/utils/abstract/ContainerView2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ContainerView2.swift
@@ -17,15 +17,7 @@ class ContainerView2:Element,Containable2 {
 extension ContainerView2:Scrollable2{
     override open func scrollWheel(with event: NSEvent) {
         Swift.print("scrollWheel")
-        switch event.phase{
-        case NSEventPhase.changed:onScrollWheelChange(event)
-        case NSEventPhase.mayBegin:onScrollWheelEnter()
-        case NSEventPhase.began:onScrollWheelEnter()
-        case NSEventPhase.ended:onScrollWheelExit();
-        case NSEventPhase.cancelled:onScrollWheelExit();
-        case NSEventPhase(rawValue:0):onScrollWheelChange(event);
-        default:break;
-        }
+        
         super.scrollWheel(with: event)
     }
 }
