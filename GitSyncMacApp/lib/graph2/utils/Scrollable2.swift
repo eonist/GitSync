cb93d21f35445c2b8696adb39af42cda6f658148
Eonist <30n1st@gmail.com>
2017-03-22 14:40:49 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/Scrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Element
 @testable import Utils
 
-protocol Scrollable2 {
+protocol Scrollable2:Containable2 {
     /**/func onScrollWheelChange(_ event:NSEvent)
      func onScrollWheelEnter()
      func onScrollWheelExit()
