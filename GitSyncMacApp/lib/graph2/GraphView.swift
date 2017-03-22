146a211bf2ad274fe4d48e48027d4acb0b562b86
Eonist <30n1st@gmail.com>
2017-03-22 13:16:50 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/GraphView.swift

--- GitSyncMacApp/lib/graph2/GraphView.swift
+++ GitSyncMacApp/lib/graph2/GraphView.swift
@@ -40,24 +40,7 @@ class GraphView:ContainerView2{
         createTimeBar()
         createValueBar()
     }
-    /*override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
-        Swift.print("scrollWheel: ")
-        //scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
-        super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
-        switch event.phase{
-            case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
-            case NSEventPhase(rawValue:0):onScrollWheelChange(event);/*Swift.print("none");*/break;//swift 3 update, was -> NSEventPhase.none
-            default:break;
-        }
-    }*/
-    /**
-     * NOTE: Basically when you perform a scroll-gesture on the touch-pad
-     */
-    /*func onScrollWheelChange(_ event:NSEvent) {/*Direct scroll, not momentum*/
-     Swift.print("📜 Scrollable.onScrollWheelChange: \(event.type)")
-     let progressVal:CGFloat = SliderListUtils.progress(event.deltaX, interval, progress)
-     setProgress(progressVal)
-     }*/
+    
     func setProgress(_ value:CGFloat){
         //Swift.print("🖼️ moving lableContainer up and down progress: \(value)")
         //Swift.print("IScrollable.setProgress() progress: \(progress)")
@@ -165,3 +148,23 @@ extension GraphView{
 //ValueBar
 //GraphLine2
 //HorizontalView
+
+
+/*override func scrollWheel(with event: NSEvent) {//TODO: move to displaceview
+ Swift.print("scrollWheel: ")
+ //scroll(event)/*forward the event to the extension which adjust Slider and calls setProgress in this method*/
+ super.scrollWheel(with: event)/*forward the event other delegates higher up in the stack*/
+ switch event.phase{
+ case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
+ case NSEventPhase(rawValue:0):onScrollWheelChange(event);/*Swift.print("none");*/break;//swift 3 update, was -> NSEventPhase.none
+ default:break;
+ }
+ }*/
+/**
+ * NOTE: Basically when you perform a scroll-gesture on the touch-pad
+ */
+/*func onScrollWheelChange(_ event:NSEvent) {/*Direct scroll, not momentum*/
+ Swift.print("📜 Scrollable.onScrollWheelChange: \(event.type)")
+ let progressVal:CGFloat = SliderListUtils.progress(event.deltaX, interval, progress)
+ setProgress(progressVal)
+ }*/
