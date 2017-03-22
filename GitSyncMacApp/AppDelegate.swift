4e526390df239ce065e9b66a015043ead13598a1
Eonist <30n1st@gmail.com>
2017-03-22 16:46:15 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -19,8 +19,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         //_ = Test2()
         
         //rateOfCommitsTest()
-        initApp()
-        //initTestWin()//🔨
+        //initApp()
+        initTestWin()//🔨
         //_ = ASyncTest()
         //_ = AsyncTest2()
         //AutoSync.sync()
