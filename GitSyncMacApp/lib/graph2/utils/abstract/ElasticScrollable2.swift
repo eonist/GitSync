5058292c9d153b3e6a26cab88f14703abfd2731b
Eonist <30n1st@gmail.com>
2017-03-23 17:13:25 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
@@ -54,11 +54,11 @@ extension ElasticScrollable2{
             var velocity:CGFloat
             if(iterimScroll.prevScrollingDelta > 1.0){
                 Swift.print("> momentum")
-                velocity = CGFloatParser.average(iterimScroll.velocities)//NumberParser.max()
+                velocity = CGFloatParser.average(iterimScroll.velocities.filter{$0 != 0})//NumberParser.max()
             }/*Find the most positive velocity value*/
             else if(iterimScroll.prevScrollingDelta < -1.0){
                 Swift.print("< momentum")
-                velocity = CGFloatParser.average(iterimScroll.velocities)//NumberParser.min(iterimScroll.velocities)
+                velocity = CGFloatParser.average(iterimScroll.velocities.filter{$0 != 0})//NumberParser.min(iterimScroll.velocities)
             }/*Find the most negative velocity value*/
             else {
                 velocity = 0
