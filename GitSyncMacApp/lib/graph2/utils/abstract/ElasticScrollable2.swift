943e3692d5aabe942d34b7cc9da9d1fd07b1698d
Eonist <30n1st@gmail.com>
2017-03-23 12:06:47 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
@@ -10,7 +10,7 @@ extension ElasticScrollable2{
         contentContainer!.frame.x = value
      }*/
      func onScrollWheelChange(_ event:NSEvent){
-        Swift.print("👻📜 (ElasticScrollable2).onScrollWheelChange : \(event.type)")
+        Swift.print("👻📜 (ElasticScrollable2).onScrollWheelChange event.scrollingDeltaX: \(event.scrollingDeltaX)")
         iterimScroll.prevScrollingDelta = event.scrollingDeltaX
         _ = iterimScroll.velocities.pushPop(event.scrollingDeltaX)
         mover!.value += event.scrollingDeltaX
@@ -45,16 +45,16 @@ extension ElasticScrollable2{
         //if(iterimScroll.prevScrollingDelta != 1.0 && iterimScroll.prevScrollingDelta != -1.0){/*Not 1 and not -1 indicates that the wheel is not stationary, or in other words: -1 or 1 means that the scrollwheel is stationary*/
             var velocity:CGFloat
             if(iterimScroll.prevScrollingDelta > 1.0){
-                Swift.print("forward momentum")
+                Swift.print("> momentum")
                 velocity = NumberParser.max(iterimScroll.velocities)
             }/*Find the most positive velocity value*/
             else if(iterimScroll.prevScrollingDelta < -1.0){
-                Swift.print("backward momentum")
+                Swift.print("< momentum")
                 velocity = NumberParser.min(iterimScroll.velocities)
             }/*Find the most negative velocity value*/
             else {
                 velocity = 0
-                Swift.print("scrollWhell is stationary aka has no momentum")
+                Swift.print("0 momentum")
             }//The prevScrollingDelta is: either '-1' or '+1' which means that the scrollwheel is stationary
             Swift.print("exit: velocity: \(velocity)")
             mover!.velocity = velocity/*set the mover velocity to the current mouse gesture velocity, the reason this can't be additive is because you need to be more immediate when you change direction, this could be done by assering last direction but its not a priority atm*///td try the += on the velocity with more rects to see its effect
