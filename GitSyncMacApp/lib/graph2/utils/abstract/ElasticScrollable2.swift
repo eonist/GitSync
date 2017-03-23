f517f2c49253029285e142eb59812881c358a1ac
Eonist <30n1st@gmail.com>
2017-03-23 11:33:54 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
@@ -42,13 +42,13 @@ extension ElasticScrollable2{
         mover!.value = mover!.result
         
         /*X*/
-        if(iterimScroll.prevScrollingDelta != 1.0 && iterimScroll.prevScrollingDelta != -1.0){/*Not 1 and not -1 indicates that the wheel is not stationary, or in other words: -1 or 1 means that the scrollwheel is stationary*/
+        //if(iterimScroll.prevScrollingDelta != 1.0 && iterimScroll.prevScrollingDelta != -1.0){/*Not 1 and not -1 indicates that the wheel is not stationary, or in other words: -1 or 1 means that the scrollwheel is stationary*/
             var velocity:CGFloat
-            if(iterimScroll.prevScrollingDelta > 0){
+            if(iterimScroll.prevScrollingDelta > 1.0){
                 Swift.print("forward momentum")
                 velocity = NumberParser.max(iterimScroll.velocities)
             }/*Find the most positive velocity value*/
-            else if(iterimScroll.prevScrollingDelta < 0){
+            else if(iterimScroll.prevScrollingDelta < -1.0){
                 Swift.print("backward momentum")
                 velocity = NumberParser.min(iterimScroll.velocities)
             }/*Find the most negative velocity value*/
@@ -59,8 +59,8 @@ extension ElasticScrollable2{
             Swift.print("exit: velocity: \(velocity)")
             mover!.velocity = velocity/*set the mover velocity to the current mouse gesture velocity, the reason this can't be additive is because you need to be more immediate when you change direction, this could be done by assering last direction but its not a priority atm*///td try the += on the velocity with more rects to see its effect
             mover!.start()/*start the frameTicker here, do this part in parent view or use event or Selector*/
-        }else{/*stationary*/
-            mover!.start()/*This needs to start if your in the overshoot areas, if its not in the overshoot area it will just stop after a frame tick*/
-        }
+        //}else{/*stationary*/
+            //mover!.start()/*This needs to start if your in the overshoot areas, if its not in the overshoot area it will just stop after a frame tick*/
+        //}
     }
 }
