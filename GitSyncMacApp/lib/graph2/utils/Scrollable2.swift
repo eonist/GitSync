f70e7100efcc038bd22d5bb4c53137705fe04405
Eonist <30n1st@gmail.com>
2017-03-23 13:22:23 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/Scrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/Scrollable2.swift
@@ -13,12 +13,12 @@ extension Scrollable2{
     func scroll(_ event:NSEvent){
         Swift.print("Scrollable2.scroll() \(event.phase.type) scrollDeltaX: \(event.scrollingDeltaX) deltaX: \(event.deltaX)")
         switch event.phase{
-            case NSEventPhase.changed:onScrollWheelChange(event)
+            case NSEventPhase.changed:onScrollWheelChange(event)/*Direct scroll*/
             case NSEventPhase.mayBegin:onScrollWheelEnter()
             case NSEventPhase.began:onScrollWheelEnter()
             case NSEventPhase.ended:onScrollWheelExit()
             case NSEventPhase.cancelled:onScrollWheelExit()
-            case NSEventPhase(rawValue:0):onScrollWheelChange(event)
+            case NSEventPhase(rawValue:0):onScrollWheelChange(event)/*this is the same as momentum aka inDirect scroll*/
             default:break;
         }
     }
