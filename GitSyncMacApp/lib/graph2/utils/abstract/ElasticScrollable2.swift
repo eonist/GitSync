75ea39783b233d045e25f4960269f41aab5b5ea9
Eonist <30n1st@gmail.com>
2017-03-23 13:48:28 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift

--- GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
+++ GitSyncMacApp/lib/graph2/utils/abstract/ElasticScrollable2.swift
@@ -42,7 +42,7 @@ extension ElasticScrollable2{
         mover!.value = mover!.result
         
         //you need to record the last directional scroll, as the last changeEvent can actually be 0
-        if(iterimScroll.prevScrollingDelta == 0 || iterimScroll.prevScrollingDelta == 1.0 || iterimScroll.prevScrollingDelta == -1.0){
+        if(iterimScroll.prevScrollingDelta == 0 || iterimScroll.prevScrollingDelta == 1.0 || iterimScroll.prevScrollingDelta == -1.0){
             
         }
         /*X*/
