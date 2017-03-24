55fd5071e076c68b9a381312fdf91eb3e3f7c7b1
Eonist <30n1st@gmail.com>
2017-03-24 11:14:15 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -4,7 +4,7 @@ import Foundation
     //The pinch gesture needs to zoom iterativly (y:2,m:1,d:0) (also Week and Hour later, maybe premium?)
     //the TimeBar needs to morph between: M,T,W,T,F,S,S and Jan,Feb,Mar, 12,13,14,15,16,17
     //when you pinch in, you record the mouse.x, and zoom into that time-range (when you zoom out this doesn't matter)
-    //
+        //
 class Graph5 {
     
 }
