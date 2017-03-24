bb5fa914b85fa5af344aba9b0742a8348887d272
Eonist <30n1st@gmail.com>
2017-03-24 17:19:19 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/TimeBar2.swift

--- GitSyncMacApp/lib/graph2/utils/TimeBar2.swift
+++ GitSyncMacApp/lib/graph2/utils/TimeBar2.swift
@@ -6,8 +6,15 @@ class TimeBar2:TimeBar{
     init(_ width: CGFloat, _ height: CGFloat, _ items:[String], _ parent: IElement?, _ id: String?) {
         super.init(width, height, items.count, parent, id)
     }
-    override func createItems() {
-        //
+    override func createItem(_ idx:Int){
+        let spaceX:CGFloat = 100
+        let x:CGFloat = (idx * spaceX)
+        let str:String = x.string
+        //Swift.print("str: " + "\(str)")
+        let textArea:TextArea = TextArea(NaN,NaN,str,self)
+        _ = addSubView(textArea)
+        //Swift.print("CGPoint(x,0): " + "\(CGPoint(x,0))")
+        textArea.setPosition(CGPoint(x,0))
     }
     override func getClassType() -> String {
         return "\(TimeBar.self)"
