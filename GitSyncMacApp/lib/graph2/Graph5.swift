4f947aca8d9eba4d2750fcc8e46f59fb6a10b49a
Eonist <30n1st@gmail.com>
2017-03-24 16:22:24 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -1,10 +1,14 @@
 import Foundation
 
-class Graph5 {
+
+class Graph5:ContainerView2{
     let dayNames:[String] = ["M","T","W","T","F","S","S"]
     let monthNames:[String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     let yearNames:[String] = ["11","12","13","14","15","16","17"]
-    //TimeBAr with 7 items
+    //TimeBar with 7 items
     //gesture recognizer
     //change timeBar textFields on gesture event
+    override func resolveSkin() {
+        <#code#>
+    }
 }
