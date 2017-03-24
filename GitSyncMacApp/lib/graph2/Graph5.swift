8698aa02232364a488461b536c43f3d8ae89e2d2
Eonist <30n1st@gmail.com>
2017-03-24 16:38:27 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -6,14 +6,42 @@ class Graph5:ContainerView2{
     let dayNames:[String] = ["M","T","W","T","F","S","S"]
     let monthNames:[String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
     let yearNames:[String] = ["11","12","13","14","15","16","17"]
+    
+    override var itemSize:CGSize {return CGSize(48,48)}//override this for custom value
+    override var interval:CGFloat{return floor(contentSize.w - maskSize.w)/itemSize.width}
+    override var progress:CGFloat{return SliderParser.progress(contentContainer!.x, maskSize.w, contentSize.w)}
+    var timeBar:TimeBar?
+    var valueBar:ValueBar?
+    /*Anim*/
+    
     //TimeBar with 7 items
     //gesture recognizer
     //change timeBar textFields on gesture event
     override func resolveSkin() {
-        StyleManager.addStyle("Graph5{fill:green;fill-alpha:0.0;}")//Needed so that scrollWheel works
+        StyleManager.addStyle("Graph5{float:left;clear:left;fill:green;fill-alpha:0.0;}")//Needed so that scrollWheel works
         super.resolveSkin()
         
-        
-        
+        /*config*/
+        maskSize = CGSize(width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
+        contentSize = CGSize(1600,height)/*represents the total size of the content *///TODO: could be ranmed to contentRect
+        /*add UI*/
+        createTimeBar()
+    }
+}
+extension Graph5{
+    /**
+     * Creates the TimeBar
+     */
+    func createTimeBar(){
+        //TODO: make line marks
+        timeBar = addSubView(TimeBar(contentSize.width,32,20,self))
+        let objSize = CGSize(timeBar!.w,32)
+        Swift.print("objSize: " + "\(objSize)")
+        let canvasSize = CGSize(w,h)
+        Swift.print("canvasSize: " + "\(canvasSize)")
+        let p = Align.alignmentPoint(objSize, canvasSize, Alignment.bottomLeft, Alignment.bottomLeft, CGPoint())
+        Swift.print("p: " + "\(p)")
+        //align timeBar to bottom with Align
+        timeBar!.point = p
     }
 }
