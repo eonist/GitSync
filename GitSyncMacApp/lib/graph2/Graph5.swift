2519122d080bfcd3c4da0208672e5aaf796669c0
Eonist <30n1st@gmail.com>
2017-03-24 18:34:21 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -4,9 +4,9 @@ import Cocoa
 
 class Graph5:ContainerView2{
     var dayNames:[String] {return ["M","T","W","T","F","S","S"]}
-    let monthNames:[String] = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
-    let yearNames:[String] = ["11","12","13","14","15","16","17"]
-    var timeLevels:[[String]]? {return [dayNames]}
+    var monthNames:[String] { return ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]}
+    var yearNames:[String] { return ["11","12","13","14","15","16","17"] }
+    var timeLevels:[[String]] {return [dayNames,monthNames,yearNames]}
     
     override var itemSize:CGSize {return CGSize(48,48)}//override this for custom value
     override var interval:CGFloat{return floor(contentSize.w - maskSize.w)/itemSize.width}
