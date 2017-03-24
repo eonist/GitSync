fe3d0dea66454eae902cb3d600d8bc182fadf2be
Eonist <30n1st@gmail.com>
2017-03-24 11:02:10 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -1,8 +1,8 @@
 import Foundation
 
 //the next experiment is:
-
-    //The pinch gesture needs to zoom iterativly 
+    //The pinch gesture needs to zoom iterativly (y:2,m:1,d:0)
+    //
 class Graph5 {
     
 }
