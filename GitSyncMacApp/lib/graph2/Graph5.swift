b7eaa14a0f894cb51d0979c7d40fedf1814ef04f
Eonist <30n1st@gmail.com>
2017-03-24 16:16:12 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph5.swift

--- GitSyncMacApp/lib/graph2/Graph5.swift
+++ GitSyncMacApp/lib/graph2/Graph5.swift
@@ -1,5 +1,9 @@
 import Foundation
 
 class Graph5 {
-    
+    let dayNames:[String] = ["M","T","W","T","F","S","S"]
+    let monthNames:[String] = 
+    //TimeBAr with 7 items
+    //gesture recognizer
+    //change timeBar textFields on gesture event
 }
