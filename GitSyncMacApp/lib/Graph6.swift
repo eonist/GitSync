7dc7822a829d5258d8fab36b07f2c6fd7a35d4b5
Eonist <30n1st@gmail.com>
2017-03-25 16:05:26 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/Graph6.swift

--- GitSyncMacApp/lib/Graph6.swift
+++ GitSyncMacApp/lib/Graph6.swift
@@ -34,7 +34,7 @@ extension Graph6{
         var css:String = "List#vertical Container#lable SelectTextButton{"
         css = "float:left;"
         css = "clear:none;"
-        css = "fill-alpha:0;"
+        css = "fill-alpha:1;"
         css = "}"
         StyleManager.addStyle(css)
         
