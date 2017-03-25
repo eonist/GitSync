2340fed5718e223b693a4e714071d2c7513bfcf2
Eonist <30n1st@gmail.com>
2017-03-25 13:03:17 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/graph6/VList.swift

--- GitSyncMacApp/lib/graph2/graph6/VList.swift
+++ GitSyncMacApp/lib/graph2/graph6/VList.swift
@@ -4,10 +4,11 @@ import Foundation
 
 class VList:ContainerView2 {
     var dp:DataProvider
+    override var itemSize:CGSize /*{return CGSize(48,48)}*///override this for custom value
     
-    init(_ width: CGFloat, _ height: CGFloat, _ itemHeight:CGFloat = NaN, _ dataProvider:DataProvider? = nil, _ parent: IElement?, _ id: String? = "") {
-        self._itemHeight = itemHeight
-        self.dataProvider = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+    init(_ width: CGFloat, _ height: CGFloat, _ itemSize:CGSize = CGSize(NaN,NaN), _ dataProvider:DataProvider? = nil, _ parent: IElement?, _ id: String? = "") {
+        self.itemSize = itemSize
+        self.dp = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
         super.init(width,height,parent,id)
         self.dataProvider.event = onEvent/*Add event handler for the dataProvider*/
         //layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
