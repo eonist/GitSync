1078b32084ca1d7ce9d4d8fa06cb3f50efefa707
Eonist <30n1st@gmail.com>
2017-03-25 13:07:34 +0100

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/graph6/VList.swift

--- GitSyncMacApp/lib/graph2/graph6/VList.swift
+++ GitSyncMacApp/lib/graph2/graph6/VList.swift
@@ -13,4 +13,27 @@ class VList:ContainerView2 {
         self.dataProvider.event = onEvent/*Add event handler for the dataProvider*/
         //layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
     }
+    /**
+     * Creates the components in the List Component
+     */
+    override func resolveSkin() {
+        super.resolveSkin()
+        mergeAt(dataProvider.items, 0)
+    }
+    /**
+     * Creates and adds items to the _lableContainer
+     * TODO: possibly move into ListModifier, TreeList has its mergeAt in an Utils class see how it does it
+     */
+    func mergeAt(_ objects:[[String:String]], _ index:Int){//TODO: possible rename to something better, placeAt? insertAt?
+        var i:Int = index
+        for obj:[String:String] in objects {//TODO: use for i
+            mergeAt(obj,i)
+            i += 1
+        }
+    }
+    func mergeAt(_ objects:[String:String], _ index:Int) -> Element{
+        let item:SelectTextButton = SelectTextButton(getWidth(), itemHeight ,object["title"]!, false, lableContainer)
+        lableContainer!.addSubviewAt(item, i)/*the first index is reserved for the List skin, what?*/
+        item
+    }
 }
