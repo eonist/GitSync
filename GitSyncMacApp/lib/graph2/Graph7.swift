48e6feec70dde148866086e9d59ce6d333ddd772
Eonist <30n1st@gmail.com>
2017-03-27 00:08:13 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph7.swift

--- GitSyncMacApp/lib/graph2/Graph7.swift
+++ GitSyncMacApp/lib/graph2/Graph7.swift
@@ -2,6 +2,38 @@ import Foundation
 @testable import Utils
 @testable import Element
 
-class Graph7:ContainerView2{
-    
+class Graph7:ContainerView2,Scrollable2{
+    override func resolveSkin() {
+        super.resolveSkin()
+        createEllipse()
+        
+    }
+}
+extension Graph7{
+    func createEllipse(){
+        /*Styles*/
+        let gradient = LinearGradient(Gradients.blue(),[],π/2)
+        let lineGradient = LinearGradient(Gradients.deepPurple(0.5),[],π/2)
+        let fill:GradientFillStyle = GradientFillStyle(gradient);
+        let lineStyle = LineStyle(20,NSColorParser.nsColor(Colors.green()).alpha(0.5),CGLineCap.round)
+        let line = GradientLineStyle(lineGradient,lineStyle)
+        /*size*/
+        let objSize:CGSize = CGSize(200,200)
+        Swift.print("objSize: " + "\(objSize)")
+        let viewSize:CGSize = CGSize(width,height)
+        Swift.print("viewSize: " + "\(viewSize)")
+        let p = Align.alignmentPoint(objSize, viewSize, Alignment.centerCenter, Alignment.centerCenter,CGPoint())
+        Swift.print("p: " + "\(p)")
+        /*Graphics*/
+        let ellipse = EllipseGraphic(p.x,p.y,200,200,fill.mix(Gradients.green()),line.mix(Gradients.lightGreen(0.5)))
+        contentContainer!.addSubview(ellipse.graphic)
+        ellipse.draw()
+        
+        
+        /*let rect = RectGraphic(0,0,width,height,fill,line)
+         zoomContainer!.addSubview(rect.graphic)
+         rect.draw()*/
+        
+        
+    }
 }
