1db70df0b675b3883bbe99355ce16e638480acb4
Eonist <30n1st@gmail.com>
2017-03-26 20:20:18 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/ElasticView.swift

--- GitSyncMacApp/lib/graph2/ElasticView.swift
+++ GitSyncMacApp/lib/graph2/ElasticView.swift
@@ -233,7 +233,10 @@ extension ElasticView{
         let caseA = iterimScrollX.prevScrollingDelta != 1.0 && iterimScrollX.prevScrollingDelta != -1.0
         let caseB = iterimScrollY.prevScrollingDelta != 1.0 && iterimScrollY.prevScrollingDelta != -1.0/*Not 1 and not -1 indicates that the wheel is not stationary*/
         
-        
+        let velocity:CGPoint {
+            let x:CGFloat = iterimScrollX.velocities.filter{$0 != 0}.average
+            let y:CGFloat = iterimScrollY.velocities.filter{$0 != 0}.average
+        }
         moverX!.velocity = iterimScrollX.velocities.filter{$0 != 0}.average/*set the mover velocity to the current mouse gesture velocity, the reason this can't be additive is because you need to be more immediate when you change direction, this could be done by assering last direction but its not a priority atm*///td try the += on the velocity with more rects to see its effect
         moverGroup!.start()/*start the frameTicker here, do this part in parent view or use event or Selector*//*This needs to start if your in the overshoot areas, if its not in the overshoot area it will just stop after a frame tick*/
     }
