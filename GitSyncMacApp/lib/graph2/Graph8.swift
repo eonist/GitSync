23d4bbe917ab750b29d364d923b206e148fa3160
Eonist <30n1st@gmail.com>
2017-03-29 17:30:25 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph8.swift

--- GitSyncMacApp/lib/graph2/Graph8.swift
+++ GitSyncMacApp/lib/graph2/Graph8.swift
@@ -1,9 +1,9 @@
 import Foundation
 
 //test snappy friction
-//test zoom to time-levels ✅
-//test fastList with dates 👈
-//Test animate graphPoint
+//test zoom to time-levels
+//test fastList with dates ✅
+//Test animate graphPoint ✅
 
 //between zoom levels you just do random graph
 //you derive the time range based on the first and last visible items in fastList
