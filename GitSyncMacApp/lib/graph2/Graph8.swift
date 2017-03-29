a43d4dcff4e98325aebc7fcc0e88150be4d5b1d9
Eonist <30n1st@gmail.com>
2017-03-29 20:03:14 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph8.swift

--- GitSyncMacApp/lib/graph2/Graph8.swift
+++ GitSyncMacApp/lib/graph2/Graph8.swift
@@ -31,4 +31,5 @@ class Graph8 {
                     //look at the FastList and see if it can support it! 👀 ✅
                         //All you need todo is SubClass DP and make it adhere to a dateRange of 7 years. or longer if the commit hist is longer, but 7 years for now
                         //you can even have 3 dp's that you switch between zoom levels
+    
 }
