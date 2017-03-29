a0e9013c2f2debea5bf456579f93086486ef535e
Eonist <30n1st@gmail.com>
2017-03-29 17:07:16 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph8.swift

--- GitSyncMacApp/lib/graph2/Graph8.swift
+++ GitSyncMacApp/lib/graph2/Graph8.swift
@@ -7,7 +7,9 @@ import Foundation
 
 //between zoom levels you just do random graph
 //you derive the time range based on the first and last visible items in fastList
-//
+//you make a rand value list for each time zoon on app init which is consistent across time level zooming
+
+//hock up the timeRangeDescTextComponent
 
 class Graph8 {
 
