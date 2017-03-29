22b0952c180a8127d0faf190d532572eb3f1e5c2
Eonist <30n1st@gmail.com>
2017-03-29 17:04:50 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph8.swift

--- GitSyncMacApp/lib/graph2/Graph8.swift
+++ GitSyncMacApp/lib/graph2/Graph8.swift
@@ -5,6 +5,8 @@ import Foundation
 //test fastList with dates 👈
 //Test animate graphPoint
 
+//between zoom levels you just do random graph
+//you derive the time range based on the first and last visible items in fastList
 //
 
 class Graph8 {
