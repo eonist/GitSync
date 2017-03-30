cb93886b6826c44fbc662305909188688be1d1d2
Eonist <30n1st@gmail.com>
2017-03-30 15:22:02 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/Graph9.swift

--- GitSyncMacApp/lib/graph2/Graph9.swift
+++ GitSyncMacApp/lib/graph2/Graph9.swift
@@ -1,5 +1,15 @@
 import Foundation
-
+//Graph9
+    //add fastList .hor 👈
+    //try it with year,month,day
+    //try to add pinch gestures to the fold
+    //try to calc the pos of mouse in relation to the timeBar
+    //try to zoom in and out with correct indecies
+    //try generate fake graphdata on anim stop
+    //draw the fake graph data as a graphline with points
+    //try to update the valuebar
+    //try to update the timeIndicator
+    //add git to the fold
 class Graph9 {
-
+    
 }
