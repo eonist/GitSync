3ef9466e0dbe9f11eda484d86f67b26b9306e631
Eonist <30n1st@gmail.com>
2017-04-02 17:09:21 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/TimeBar3.swift

--- GitSyncMacApp/lib/graph2/utils/TimeBar3.swift
+++ GitSyncMacApp/lib/graph2/utils/TimeBar3.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 @testable import Element
 @testable import Utils
 
@@ -9,5 +9,19 @@ class TimeBar3:ElasticScrollFastList{
         let itemsRect:RubberBand.Frame = (min:0,len:contentSize[dir])/*represents the total size of the content *///TODO: could be ranmed to contentRect
         mover = SnappyRubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+    }
+    override func scrollWheel(with event: NSEvent) {
+        super.scrollWheel(with: event)
+        /*⚠️️ temp fix for SnappyRubberBand support, move this into a new protocol extension*/
+        if(event.phase == .changed || event.phase == NSEventPhase(rawValue:0)){
+            let prevDir = (mover! as! SnappyRubberBand).prevDir
+            var tempDir:CGFloat = 0
+            let curDir = event.scrollingDeltaX
+            if(curDir == 0){tempDir = prevDir}//use old value
+            else if(curDir.isNegative){tempDir = -1}
+            else if(curDir.isPositive){tempDir = 1}
+            Swift.print("tempDir: " + "\(tempDir)")
+            (mover! as! SnappyRubberBand).prevDir = tempDir
+        }
     }
 }
