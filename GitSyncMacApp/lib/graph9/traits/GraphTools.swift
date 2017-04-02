e9c18a96d7ace35e8b5e1fb9b2ba6e620af87ad3
Eonist <30n1st@gmail.com>
2017-04-02 21:35:05 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/GraphTools.swift

--- GitSyncMacApp/lib/graph9/traits/GraphTools.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphTools.swift
@@ -6,8 +6,9 @@ extension Graph9 {
     var randomGraphPoints:[CGPoint]{
         typealias P = CGPoint
         var points:[P] = []
+        let padding:CGFloat = 50
         for i in 0..<7{
-            let x:CGFloat = 100*i
+            let x:CGFloat = padding+(100*i)
             let y:CGFloat = (0..<(height.int-32)).random.cgFloat
             let p = P(x,y)
             points.append(p)
