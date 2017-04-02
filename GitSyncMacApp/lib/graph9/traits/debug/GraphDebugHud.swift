08cfe09b2b12c9a83076401908e73afe07237bac
Eonist <30n1st@gmail.com>
2017-04-02 22:06:08 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/debug/GraphDebugHud.swift

--- GitSyncMacApp/lib/graph9/traits/debug/GraphDebugHud.swift
+++ GitSyncMacApp/lib/graph9/traits/debug/GraphDebugHud.swift
@@ -1,5 +1,14 @@
-import Foundation
+import Cocoa
+@testable import Utils
 
-class GraphDebugHud {
-
+extension Graph9{
+    override func touchesBegan(with event:NSEvent) {
+        gestureHUD.touchesBegan(event)
+    }
+    override func touchesMoved(with event:NSEvent) {
+        gestureHUD.touchesMoved(event)
+    }
+    override func touchesEnded(with event:NSEvent) {
+        gestureHUD.touchesEnded(event)
+    }
 }
