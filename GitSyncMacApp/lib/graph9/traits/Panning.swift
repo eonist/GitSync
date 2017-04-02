e01fbe0ed284b428d8f4ab2cdfe25e892153a162
Eonist <30n1st@gmail.com>
2017-04-02 15:33:41 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/lib/graph9/traits/Panning.swiftGitSyncMacApp/lib/graph9/traits/Zooming.swift

--- GitSyncMacApp/lib/graph9/traits/Panning.swift
+++ GitSyncMacApp/lib/graph9/traits/Panning.swift
@@ -1,5 +1,59 @@
-import Foundation
+import Cocoa
+@testable import Element
+@testable import Utils
 
-class Panning {
 
+/*Panning controller*/
+extension Graph9{
+    override func scrollWheel(with event:NSEvent) {
+        //Swift.print("Graph9.scrollWheel()")
+        super.scrollWheel(with:event)
+        updateDateText()
+    }
+    /**
+     * Updates the DateText UI Element
+     */
+    func updateDateText(){
+        if(visibleRange == nil || visibleRange != timeBar!.visibleItemRange){/*If the range has changed, then update text*/
+            visibleRange = timeBar!.visibleItemRange
+            let yearRange = (timeBar!.dp as! TimeDP).yearRange
+            Swift.print("yearRange: " + "\(yearRange)")
+            var dateStr:String = ""
+            switch curTimeType{
+            case .year:
+                /*Year*/
+                let startYearStr:String = YearDP.year(visibleRange!.start,yearRange).string
+                let endYearStr:String = YearDP.year(visibleRange!.end,yearRange).string
+                dateStr = "\(startYearStr) - \(endYearStr)"
+            case .month:
+                /*Month*/
+                let startMonth:Date = MonthDP.month(visibleRange!.start, yearRange)
+                let endMonth:Date = MonthDP.month(visibleRange!.end, yearRange)
+                /*Year*/
+                let startYearIdx:Int = MonthDP.year(visibleRange!.start, yearRange)//sort of the offset
+                let startYearStr:String = YearDP.year(startYearIdx,yearRange).string
+                let endYearIdx:Int = MonthDP.year(visibleRange!.end, yearRange)
+                let endYearStr:String = YearDP.year(endYearIdx,yearRange).string
+                dateStr = "\(startYearStr).\(startMonth.shortMonthName) - \(endYearStr).\(endMonth.shortMonthName)"
+            case .day:
+                /*day*/
+                let startDayDate:Date = DayDP.day(visibleRange!.start, yearRange)
+                let startDayDateStr:String = startDayDate.day.string
+                let endDayDate:Date = DayDP.day(visibleRange!.end, yearRange)
+                let endDayDateStr:String = endDayDate.day.string
+                /*Month*/
+                let startMonthIdx:Int = DayDP.month(visibleRange!.start,yearRange)
+                let startMonth:Date = MonthDP.month(startMonthIdx, yearRange)
+                let endMonthIdx:Int = DayDP.month(visibleRange!.end,yearRange)
+                let endMonth:Date = MonthDP.month(endMonthIdx, yearRange)
+                /*year*/
+                let startYearIdx:Int = MonthDP.year(startMonthIdx, yearRange)//sort of the offset
+                let startYearStr:String = YearDP.year(startYearIdx,yearRange).string
+                let endYearIdx:Int = MonthDP.year(startMonthIdx, yearRange)
+                let endYearStr:String = YearDP.year(endYearIdx,yearRange).string
+                dateStr = "\(startYearStr).\(startMonth.shortMonthName).\(startDayDateStr) - \(endYearStr).\(endMonth.shortMonthName).\(endDayDateStr)"
+            }
+            dateText!.setTextValue(dateStr)
+        }
+    }
 }
--- GitSyncMacApp/lib/graph9/traits/Zooming.swift
+++ GitSyncMacApp/lib/graph9/traits/Zooming.swift
@@ -1,5 +1,61 @@
 import Foundation
 
-class Zooming {
-
+/*Zoom Controller*/
+extension Graph9{
+    /**
+     * Detects if a zoom gesture has occured +-100 deltaZ
+     */
+    override func magnify(with event: NSEvent) {
+        //Swift.print("Graph9.magnify()")
+        super.magnify(with: event)
+        let prevZoom:Int = curZoom
+        if(event.phase == .changed){
+            zoom += event.deltaZ
+        }else if(event.phase == .began){
+            zoom = 0//reset
+        }else if(event.phase == .ended){
+            var dir:Int
+            if(zoom < -100){dir = 1}
+            else if(zoom > 100){dir = -1}
+            else{dir = 0}
+            let newZoom = curZoom + dir
+            if(newZoom >= 0 && newZoom < maxZoom){
+                curZoom = newZoom
+            }
+            if(curZoom != prevZoom){
+                onZoomLevelChange()//only toggle if zoom is not prevZoom
+            }
+        }
+    }
+    func onZoomLevelChange() {
+        //Swift.print("Graph9.onZoomLevelChange()")
+        let prevTimeType:TimeType = curTimeType
+        curTimeType = TimeType.types[curZoom]
+        /**/
+        timeBar!.removeFromSuperview()
+        timeBar = nil
+        let dp:TimeDP = TimeDPUtils.timeDP(curTimeType,range)
+        timeBar = addSubView(ElasticScrollFastList(w,24,24,dp,self,nil,.hor,100))
+        alignTimeBar()
+        /*let dp:TimeDP = TimeDPUtils.timeDP(curTimeType,range)
+         ViewModifier.removeAll(timeBar!.lableContainer!)
+         timeBar!.pool = []
+         timeBar!.inActive = []
+         timeBar!.dataProvider = dp*/
+        Swift.print("🍐 timeBar!.contentSize[timeBar!.dir]: " + "\(timeBar!.contentSize[timeBar!.dir])")
+        let mouseLocIdx:Int = StatUtils.mouseLocIdx(mouseX, w, 100)
+        Swift.print("mouseLocIdx: " + "\(mouseLocIdx)")
+        var progress:CGFloat = StatUtils.progress(timeBar!, (prevTimeType,curTimeType), mouseLocIdx)/*0-1*/
+        progress = progress.clip(0, 1)
+        Swift.print("progress: " + "\(progress)")
+        let progressVal:CGFloat = SliderParser.y(progress, timeBar!.maskSize[timeBar!.dir], timeBar!.contentSize[timeBar!.dir])
+        (timeBar! as! ElasticScrollFastList).mover!.value = progressVal//temp fix
+        (timeBar! as! ElasticScrollFastList).setProgress(progressVal)
+        
+        //let visRange:Range<Int> = timeBar!.visibleItemRange.start..<(timeBar!.visibleItemRange.end > timeBar!.dp.count ? timeBar!.visibleItemRange.end - 1 : timeBar!.visibleItemRange.end)
+        //timeBar!.renderItems(visRange)
+        /**/
+        visibleRange = nil/*rest so we force update dateText*/
+        updateDateText()
+    }
 }
