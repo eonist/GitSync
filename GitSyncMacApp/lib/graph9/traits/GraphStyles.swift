980d11533880fe7e65e832f09f1d0a13053d7fbf
Eonist <30n1st@gmail.com>
2017-04-02 20:10:20 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/GraphStyles.swift

--- GitSyncMacApp/lib/graph9/traits/GraphStyles.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphStyles.swift
@@ -1,5 +1,37 @@
 import Foundation
-
-class GraphStyles {
-
+@testable import Utils
+@testable import Element
+/**
+ * 🎨 Styles
+ */
+extension Graph9{
+    func addGraphLineStyle(){
+        var css:String = "GraphLine{"
+        css +=    "float:none;"
+        css +=    "clear:none;"
+        css +=    "line:#2AA3EF;"
+        css +=    "line-alpha:1;"
+        css +=    "line-thickness:0.5px;"
+        css += "}"
+        StyleManager.addStyle(css)
+    }
+    func addGraphPointStyle(){
+        /*GraphPoint*/
+        var css:String = ""
+        css += "Element#graphPoint{"
+        css +=     "float:none;"
+        css +=     "clear:none;"
+        css +=     "fill:#128BF2,#192633;"
+        css +=     "width:12px,11px;"
+        css +=     "height:12px,11px;"
+        css +=     "margin-left:-6px,-5.5px;"
+        css +=     "margin-right:6px,5.5px;"
+        css +=     "margin-top:-6px,-5.5px;"
+        css +=     "margin-bottom:6px,5.5px;"
+        css +=     "drop-shadow:drop-shadow(1px 90 #000000 0.3 0.5 0.5 0 0 false);"
+        css +=     "corner-radius:6px,5.5px;"
+        css += "}"
+        StyleManager.addStyle(css)
+    }
 }
+
