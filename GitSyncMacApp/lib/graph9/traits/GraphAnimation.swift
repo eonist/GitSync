e61674955c7b072d03506097d824ff549f643f33
Eonist <30n1st@gmail.com>
2017-04-02 21:18:35 +0200

    Files modified: 3
    Modified 3 files:GitSyncMacApp/lib/graph2/Graph9.swiftGitSyncMacApp/lib/graph2/animation/SnappyRubberBand.swiftGitSyncMacApp/lib/graph9/traits/GraphAnimation.swift

--- GitSyncMacApp/lib/graph9/traits/GraphAnimation.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphAnimation.swift
@@ -3,5 +3,36 @@ import Cocoa
 @testable import Utils
 
 extension Graph9 {
-
+    /**
+     * Re-calc and set the graphPoint positions (for instance if the hValues has changed etc)
+     */
+    func updateGraph(){
+        prevGraphPts = graphPts.map{$0}//grabs the location of where the pts are now
+        graphPts = randomGraphPoints
+        
+        /*GraphPoints*/
+        if(animator != nil){animator!.stop()}/*stop any previous running animation*/
+        animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeIn)
+        animator!.start()
+    }
+    /**
+     * Interpolates between 0 and 1 while the duration of the animation
+     * NOTE: ReCalc the hValue indicators (each week has a different max hValue etc)
+     */
+    func interpolateValue(_ val:CGFloat){
+        //Swift.print("interpolateValue() val: " + "\(val)")
+        var positions:[CGPoint] = []
+        /*GraphPoints*/
+        for i in 0..<graphPts!.count{
+            let pos:CGPoint = prevGraphPts![i].interpolate(graphPts![i], val)/*interpolates from one point to another*/
+            positions.append(pos)
+            graphPoints![i].setPosition(pos)//moves the points
+        }
+        /*GraphLine*/
+        let path:IPath = PolyLineGraphicUtils.path(positions)/*convert points to a Path*/
+        //TODO: Ideally we should create the CGPath from the points use CGPathParser.polyline
+        let cgPath = CGPathUtils.compile(CGMutablePath(), path)//convert path to cgPath
+        graphLine!.line!.cgPath = cgPath.clone()//applies the new path
+        graphLine!.line!.draw()//draws the path
+    }
 }
