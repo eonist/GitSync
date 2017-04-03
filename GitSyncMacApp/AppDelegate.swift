336713fbad9718af44be063ba6e05cdd2cc96d41
Eonist <30n1st@gmail.com>
2017-04-03 19:50:59 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -65,7 +65,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func initTestWin(){
         //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",false)
-        win = TestWin(600,400)/*Debugging Different List components*/
+        win = TestWin(700,400)/*Debugging Different List components*/
         
         let url:String = "~/Desktop/ElCapitan/"
         fileWatcher = FileWatcher([url.tildePath])
