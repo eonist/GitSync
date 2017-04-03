7f5b50e1425fe02f24452dcdc47314419d2a968f
Eonist <30n1st@gmail.com>
2017-04-03 12:31:52 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift

--- Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
+++ Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
@@ -5,8 +5,17 @@ class CommitCountWorkUtils {
     /**
      *
      */
-    static func commitCountWork(_ from:Date, _ until:Date)->CommitCountWork{
-        
+    static func commitCountWork(_ from:Date, _ until:Date, _ timeType:TimeType)->CommitCountWork{
+        switch timeType{
+        case .year:
+            print("one")
+        case .month:
+            print("range from 3 to 8")
+        case .month:
+            print("range from 3 to 8")
+        default:
+            break;
+        }
         return ("","","",0)
     }
 
