dcdaa62f82bb210acfb7eedd0bd6317a684f62b4
Eonist <30n1st@gmail.com>
2017-04-03 13:09:46 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift

--- Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
+++ Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
@@ -8,7 +8,7 @@ class CommitCountWorkUtils {
      *
      */
     static func commitCountWork(_ repoItem:RepoItem,_ from:Date, _ until:Date, _ timeType:TimeType)->CommitCountWork{
-        var commitCountWorks:[CommitCountWork] = []
+        
         switch timeType{
             case .year:
                 let numOfYears = from.numOfYears(until)
@@ -23,5 +23,17 @@ class CommitCountWorkUtils {
     }
 }
 private class Utils{
-    
+    typealias OffsetDateMethod = (_ date:Date,_ offset:Int)->Date/*Method signature*/
+    static func commitCountWork(_ repoItem:RepoItem, _ from:Date, _ numOfTimeUnits:Int, _ offsetBy:OffsetDateMethod)->[CommitCountWork]{
+        var commitCountWorks:[CommitCountWork] = []
+        for i in (0...numOfTimeUnits){//7 days
+            let sinceDate:Date = from.offsetByDays(i)
+            let sinceGitDate:String = GitDateUtils.gitTime(sinceDate)
+            let untilDate:Date = from.offsetByDays(i+1)
+            let untilGitDate:String = GitDateUtils.gitTime(untilDate)
+            let comitCountWork:CommitCountWork = (repoItem.localPath,sinceGitDate,untilGitDate,0)
+            commitCountWorks.append(comitCountWork)
+        }
+        return commitCountWorks
+    }
 }
