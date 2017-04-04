22cc893978b6657e17344c70628089aa116c6f65
Eonist <30n1st@gmail.com>
2017-04-04 17:42:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/ValueBar.swift

--- GitSyncMacApp/lib/graph2/utils/ValueBar.swift
+++ GitSyncMacApp/lib/graph2/utils/ValueBar.swift
@@ -19,47 +19,22 @@ class ValueBar:Element{
             textArea.setPosition(CGPoint(0,y))
         }
     }
-}
-extension ValueBar{
     /**
-     *
+     * Creates the Text items that represents data in the y-axis
      */
-    func addStyles(){
-        var css:String = ""
-        css +=	"ValueBar{"
-        css +=		"float:none;"
-        css +=		"clear:none;"
-        css +=		"fill:yellow;"
-        css +=		"fill-alpha:0.0;"
-        css +=		"width:32px;"
-        css +=	"}"
-        css +=	"ValueBar TextArea{"
-        css +=		"float:none;"
-        css +=		"clear:none;"
-        css +=		"fill:green;"
-        css +=		"fill-alpha:0;"
-        css +=		"line:grey;"
-        css +=		"line-alpha:0;"
-        css +=		"line-thickness:0px;"
-        css +=		"padding-right:0px;"
-        css +=		"padding-top:0px;"
-        css +=		"margin-top:-10px;"
-        css +=		"margin-bottom:10px;"
-        css +=		"margin-left:8px;"
-        css +=		"height:20px;"
-        css +=		"width:100%;"
-        css +=	"}"
-        css +=	"ValueBar TextArea Text{"
-        css +=		"border:false;"
-        css +=		"color:#C5C9CC;"
-        css +=		"backgroundColor:orange;"
-        css +=		"background:false;"
-        css +=		"width:100%;"
-        css +=		"size:10px;"
-        css +=		"margin-top:0px;"
-        css +=		"height:20px;"
-        css +=		"margin-left:0px;"
-        css +=	"}"
-        StyleManager.addStyle(css)
+    func createLeftBar(){
+        leftBar = addSubView(Section(NaN,newSize!.height,self,"leftBar"))//create left bar
+        leftBar!.setPosition(CGPoint(0,newPosition!.y))
+        
+        let strings:[String] = GraphUtils.verticalIndicators(vCount, maxValue!)
+        
+        var y:CGFloat = itemYSpace!
+        strings.forEach{
+            let textArea:TextArea = TextArea(NaN,NaN,$0,leftBar!)
+            leftBarItems.append(textArea)
+            _ = leftBar!.addSubView(textArea)
+            textArea.setPosition(CGPoint(0,y))
+            y += itemYSpace!
+        }
     }
 }
