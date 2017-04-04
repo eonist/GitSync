3b2cebad3d1c646d710380d3f1f771de7d8b6955
Eonist <30n1st@gmail.com>
2017-04-04 13:42:32 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/lib/graph9/traits/GraphPanning.swiftGitSyncMacApp/lib/graph9/traits/GraphUpdate.swift

--- GitSyncMacApp/lib/graph9/traits/GraphUpdate.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphUpdate.swift
@@ -1,5 +1,74 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
 extension Graph9{
-    
+    /**
+     *
+     */
+    func update(){
+        
+    }
+    /**
+     * Updates the DateText UI Element
+     */
+    func updateDateText(){
+        Swift.print("🍐🍌🍊 updateDateText")
+        if(visibleRange == nil || visibleRange != timeBar!.visibleItemRange){/*If the range has changed, then update text*/
+            let visibleRange = timeBar!.visibleItemRange
+            let yearRange = (timeBar!.dp as! TimeDP).yearRange
+            Swift.print("yearRange: " + "\(yearRange)")
+            var dateStr:String = ""
+            switch curTimeType{
+            case .year:
+                /*Year*/
+                Swift.print("visibleRange!: " + "\(visibleRange!)")
+                let startYearStr:String = YearDP.year(visibleRange!.start,yearRange).string
+                Swift.print("startYearStr: " + "\(startYearStr)")
+                let endYearStr:String = YearDP.year(visibleRange!.end - 2,yearRange).string
+                Swift.print("endYearStr: " + "\(endYearStr)")
+                dateStr = "\(startYearStr) - \(endYearStr)"
+            case .month:
+                /*Month*/
+                let startMonth:Date = MonthDP.month(visibleRange!.start, yearRange)
+                let endMonth:Date = MonthDP.month(visibleRange!.end, yearRange)
+                /*Year*/
+                let startYearIdx:Int = MonthDP.year(visibleRange!.start, yearRange)//sort of the offset
+                let startYearStr:String = YearDP.year(startYearIdx,yearRange).string
+                let endYearIdx:Int = MonthDP.year(visibleRange!.end, yearRange)
+                let endYearStr:String = YearDP.year(endYearIdx,yearRange).string
+                dateStr = "\(startYearStr).\(startMonth.shortMonthName) - \(endYearStr).\(endMonth.shortMonthName)"
+            case .day:
+                /*day*/
+                let startDayDate:Date = DayDP.day(visibleRange!.start, yearRange)
+                let startDayDateStr:String = startDayDate.day.string
+                let endDayDate:Date = DayDP.day(visibleRange!.end, yearRange)
+                let endDayDateStr:String = endDayDate.day.string
+                /*Month*/
+                let startMonthIdx:Int = DayDP.month(visibleRange!.start,yearRange)
+                let startMonth:Date = MonthDP.month(startMonthIdx, yearRange)
+                let endMonthIdx:Int = DayDP.month(visibleRange!.end,yearRange)
+                let endMonth:Date = MonthDP.month(endMonthIdx, yearRange)
+                /*year*/
+                let startYearIdx:Int = MonthDP.year(startMonthIdx, yearRange)//sort of the offset
+                let startYearStr:String = YearDP.year(startYearIdx,yearRange).string
+                let endYearIdx:Int = MonthDP.year(startMonthIdx, yearRange)
+                let endYearStr:String = YearDP.year(endYearIdx,yearRange).string
+                dateStr = "\(startYearStr).\(startMonth.shortMonthName).\(startDayDateStr) - \(endYearStr).\(endMonth.shortMonthName).\(endDayDateStr)"
+            }
+            dateText!.setTextValue(dateStr)
+        }
+    }
+    /**
+     * Re-calc and set the graphPoint positions (for instance if the hValues has changed etc)
+     */
+    func updateGraph(){
+        prevGraphPts = graphPts.map{$0}//grabs the location of where the pts are now
+        graphPts = randomGraphPoints
+        
+        /*GraphPoints*/
+        if(animator != nil){animator!.stop()}/*stop any previous running animation*/
+        animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeIn)
+        animator!.start()
+    }
 }
