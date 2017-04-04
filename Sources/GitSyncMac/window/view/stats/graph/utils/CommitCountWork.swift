e9083dfef5c43d856c1d87bf788a44beb88e8547
Eonist <30n1st@gmail.com>
2017-04-04 12:20:35 +0200

    Files modified: 1, Files deleted: 1
    Deleted 1 file: Sources/GitSyncMac/window/view/stats/graph/utils/TimeTypeExtensions.swiftModified 1 file:Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift

--- Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
+++ Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
@@ -43,3 +43,25 @@ private class Utils{
         return commitCountWorks
     }
 }
+private extension TimeType{
+    func offsetBy(_ date:Date,_ offset:Int)->Date {
+        switch self {
+        case .year:
+            return DateModifier.offsetByYears(date,offset)
+        case .month:
+            return DateModifier.offsetByMonths(date,offset)
+        case .day:
+            return DateModifier.offsetByDays(date,offset)
+        }
+    }
+    func numOfTimeUnits(_ from:Date,_ until:Date)->Int {
+        switch self {
+        case .year:
+            return from.numOfYears(until)
+        case .month:
+            return from.numOfMonths(until)
+        case .day:
+            return from.numOfDays(until)
+        }
+    }
+}
--- Sources/GitSyncMac/window/view/stats/graph/utils/TimeTypeExtensions.swift
+++
@@ -1,4 +0,0 @@
-import Foundation
-@testable import Utils
-
-
