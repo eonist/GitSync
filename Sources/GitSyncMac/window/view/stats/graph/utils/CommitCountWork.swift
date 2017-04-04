daf3a106a544fcb68388e6bfb8e36c07288c17ad
Eonist <30n1st@gmail.com>
2017-04-04 12:23:05 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift

--- Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
+++ Sources/GitSyncMac/window/view/stats/graph/utils/CommitCountWork.swift
@@ -46,22 +46,22 @@ private class Utils{
 private extension TimeType{
     func offsetBy(_ date:Date,_ offset:Int)->Date {
         switch self {
-        case .year:
-            return DateModifier.offsetByYears(date,offset)
-        case .month:
-            return DateModifier.offsetByMonths(date,offset)
-        case .day:
-            return DateModifier.offsetByDays(date,offset)
+            case .year:
+                return DateModifier.offsetByYears(date,offset)
+            case .month:
+                return DateModifier.offsetByMonths(date,offset)
+            case .day:
+                return DateModifier.offsetByDays(date,offset)
         }
     }
     func numOfTimeUnits(_ from:Date,_ until:Date)->Int {
         switch self {
-        case .year:
-            return from.numOfYears(until)
-        case .month:
-            return from.numOfMonths(until)
-        case .day:
-            return from.numOfDays(until)
-        }
+            case .year:
+                return from.numOfYears(until)
+            case .month:
+                return from.numOfMonths(until)
+            case .day:
+                return from.numOfDays(until)
+            }
     }
 }
