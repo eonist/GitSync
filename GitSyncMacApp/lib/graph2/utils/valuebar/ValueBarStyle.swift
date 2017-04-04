5d4b87b610ebe594a24e019393f24889cddda5b1
Eonist <30n1st@gmail.com>
2017-04-04 17:42:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/valuebar/ValueBarStyle.swift

--- GitSyncMacApp/lib/graph2/utils/valuebar/ValueBarStyle.swift
+++ GitSyncMacApp/lib/graph2/utils/valuebar/ValueBarStyle.swift
@@ -1,5 +1,46 @@
 import Foundation
+@testable import Utils
 
-class ValueBarStyle {
-
+extension ValueBar{
+    /**
+     *
+     */
+    func addStyles(){
+        var css:String = ""
+        css +=	"ValueBar{"
+        css +=		"float:none;"
+        css +=		"clear:none;"
+        css +=		"fill:yellow;"
+        css +=		"fill-alpha:0.0;"
+        css +=		"width:32px;"
+        css +=	"}"
+        css +=	"ValueBar TextArea{"
+        css +=		"float:none;"
+        css +=		"clear:none;"
+        css +=		"fill:green;"
+        css +=		"fill-alpha:0;"
+        css +=		"line:grey;"
+        css +=		"line-alpha:0;"
+        css +=		"line-thickness:0px;"
+        css +=		"padding-right:0px;"
+        css +=		"padding-top:0px;"
+        css +=		"margin-top:-10px;"
+        css +=		"margin-bottom:10px;"
+        css +=		"margin-left:8px;"
+        css +=		"height:20px;"
+        css +=		"width:100%;"
+        css +=	"}"
+        css +=	"ValueBar TextArea Text{"
+        css +=		"border:false;"
+        css +=		"color:#C5C9CC;"
+        css +=		"backgroundColor:orange;"
+        css +=		"background:false;"
+        css +=		"width:100%;"
+        css +=		"size:10px;"
+        css +=		"margin-top:0px;"
+        css +=		"height:20px;"
+        css +=		"margin-left:0px;"
+        css +=	"}"
+        StyleManager.addStyle(css)
+    }
 }
