f3c2207c0989d2cf63afa8ab16db0b93cbbf859c
Eonist <30n1st@gmail.com>
2017-04-04 12:13:00 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/stats/graph/utils/TimeTypeExtensions.swift

--- Sources/GitSyncMac/window/view/stats/graph/utils/TimeTypeExtensions.swift
+++ Sources/GitSyncMac/window/view/stats/graph/utils/TimeTypeExtensions.swift
@@ -1,5 +1,16 @@
 import Foundation
+@testable import Utils
 
-extension TimeType {
-
+extension TimeType{
+    func offsetBy(_ date:Date,_ offset:Int)->Date {
+        switch self {
+            case .year:
+                return DateModifier.offsetByYears(date,offset)
+            case .month:
+                return DateModifier.offsetByMonths(date,offset)
+            case .day:
+                return DateModifier.offsetByDays(date,offset)
+        }
+    }
+    var numOfTimeUnits:Int
 }
