2410c9d99441647838098d6be42f38318f69c457
Eonist <30n1st@gmail.com>
2017-04-04 15:21:32 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/GraphZooming.swift

--- GitSyncMacApp/lib/graph9/traits/GraphZooming.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphZooming.swift
@@ -31,6 +31,7 @@ extension Graph9{
     }
     func onZoomLevelChange() {
         Swift.print("onZoomLevelChange: " + "\(self.prevTimeType)")
+        prevRange = nil
         let prevTimeType = self.prevTimeType//temp store this
         if(hasZoomChanged){//only toggle if zoom is not prevZoom
             //Swift.print("Graph9.onZoomLevelChange()")
