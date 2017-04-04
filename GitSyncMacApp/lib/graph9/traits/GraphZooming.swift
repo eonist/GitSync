4440e496a421d7bef74b77a32fd3d0245bc0a3aa
Eonist <30n1st@gmail.com>
2017-04-04 15:54:23 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/GraphZooming.swift

--- GitSyncMacApp/lib/graph9/traits/GraphZooming.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphZooming.swift
@@ -57,6 +57,7 @@ extension Graph9{
             let progressVal:CGFloat = SliderParser.y(progress, timeBar!.maskSize[timeBar!.dir], timeBar!.contentSize[timeBar!.dir])
             Swift.print("progressVal: " + "\(progressVal)")
             (timeBar! as! TimeBar3).mover!.value = progressVal//temp fix
+            (timeBar! as! TimeBar3).mover!.result = progressVal
             (timeBar! as! TimeBar3).setProgress(progressVal)
             
             //let visRange:Range<Int> = timeBar!.visibleItemRange.start..<(timeBar!.visibleItemRange.end > timeBar!.dp.count ? timeBar!.visibleItemRange.end - 1 : timeBar!.visibleItemRange.end)
