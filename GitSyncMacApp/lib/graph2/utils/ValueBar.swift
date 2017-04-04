30e7fff89a22326aab7a25646a0cf234ab79eb3f
Eonist <30n1st@gmail.com>
2017-04-04 17:52:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/ValueBar.swift

--- GitSyncMacApp/lib/graph2/utils/ValueBar.swift
+++ GitSyncMacApp/lib/graph2/utils/ValueBar.swift
@@ -7,6 +7,26 @@ class ValueBar:Element{
         super.resolveSkin()
         //400
         //8
+        
+    }
+    /**
+     * Creates the Text items that represents data in the y-axis
+     */
+    func createLeftBar(){
+        /*leftBar = addSubView(Section(NaN,newSize!.height,self,"leftBar"))//create left bar
+         leftBar!.setPosition(CGPoint(0,newPosition!.y))*/
+        let strings:[String] = GraphUtils.verticalIndicators(6/*vCount*/, 60/*maxValue!*/)
+        let itemYSpace:CGFloat = height/8
+        var y:CGFloat = itemYSpace
+        strings.forEach{ str in
+            let textArea:TextArea = TextArea(NaN,NaN,str,self/*leftBar!*/)
+            /*leftBarItems*///self.append(textArea)
+            _ = /*leftBar!*/self.addSubView(textArea)
+            textArea.setPosition(CGPoint(0,y))
+            y += itemYSpace
+        }
+    }
+    func createOldLeftBar(){
         let ySpace:CGFloat = height/8
         let textValues:[String] = (0...8).reversed().map { ($0 * 50).int.string }//"400","350","300"..."0"
         for i in (0...8){
@@ -17,24 +37,6 @@ class ValueBar:Element{
             let textArea:TextArea = TextArea(NaN,NaN,textValue,self)
             _ = self.addSubView(textArea)
             textArea.setPosition(CGPoint(0,y))
-        }
-    }
-    /**
-     * Creates the Text items that represents data in the y-axis
-     */
-    func createLeftBar(){
-        /*    leftBar = addSubView(Section(NaN,newSize!.height,self,"leftBar"))//create left bar
-         leftBar!.setPosition(CGPoint(0,newPosition!.y))*/
-        
-        let strings:[String] = GraphUtils.verticalIndicators(6/*vCount*/, 60/*maxValue!*/)
-        
-        var y:CGFloat = height/8/*itemYSpace!*/
-        strings.forEach{
-            let textArea:TextArea = TextArea(NaN,NaN,$0,self/*leftBar!*/)
-            /*leftBarItems*///self.append(textArea)
-            _ = /*leftBar!*/self.addSubView(textArea)
-            textArea.setPosition(CGPoint(0,y))
-            y += itemYSpace!
         }
     }
 }
