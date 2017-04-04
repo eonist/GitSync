875c58024bd95b3d97a78f03ae4ea771631a0131
Eonist <30n1st@gmail.com>
2017-04-04 14:08:32 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph9/traits/GraphState.swift

--- GitSyncMacApp/lib/graph9/traits/GraphState.swift
+++ GitSyncMacApp/lib/graph9/traits/GraphState.swift
@@ -1,5 +1,14 @@
 import Foundation
 
-class GraphState {
-
+extension Graph9 {
+    var hasZoomChanged:Bool{
+        let hasZoomChanged:Bool = prevZoom != nil && prevZoom != curZoom
+        prevZoom = curZoom
+        return hasZoomChanged
+    }
+    var hasPanningChanged:Bool {
+        let hasPanningChanged:Bool = prevRange != nil && prevRange != curRange
+        prevRange = curRange
+        return hasPanningChanged
+    }
 }
