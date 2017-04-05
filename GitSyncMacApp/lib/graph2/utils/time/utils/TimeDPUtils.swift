964b27542e0159236b7566c61bf4d3ccde861f5b
Eonist <30n1st@gmail.com>
2017-04-05 14:49:58 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/time/utils/TimeDPUtils.swift

--- GitSyncMacApp/lib/graph2/utils/time/utils/TimeDPUtils.swift
+++ GitSyncMacApp/lib/graph2/utils/time/utils/TimeDPUtils.swift
@@ -3,16 +3,13 @@ import GitSyncMac
 
 class TimeDPUtils {
     static func timeDP(_ timeType:TimeType, _ range:Range<Int>)->TimeDP{
-        let dp:TimeDP = {
-            switch timeType{
-                case .day:
-                    return DayDP(range)
-                case .month:
-                    return MonthDP(range)
-                case .year:
-                    return YearDP(range)
-            }
-        }()
-        return dp
+        switch timeType{
+            case .day:
+                return DayDP(range)
+            case .month:
+                return MonthDP(range)
+            case .year:
+                return YearDP(range)
+        }
     }
 }
