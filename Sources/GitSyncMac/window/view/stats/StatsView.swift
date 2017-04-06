da32695323acf20ae6fc9479eee4bfc7ca934bcd
Eonist <30n1st@gmail.com>
2017-04-06 20:21:23 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/stats/StatsView.swift

--- Sources/GitSyncMac/window/view/stats/StatsView.swift
+++ Sources/GitSyncMac/window/view/stats/StatsView.swift
@@ -1,12 +1,12 @@
 import Cocoa
 @testable import Utils
 @testable import Element
-@testable import GitSyncMac
+
 /**
  * TODO: When entering the Graph component, you should animate the graph component from the old data to the new data, if there is new data. Very satesfiyng seeing your day commit graph go up after a long day of work
  */
 class StatsView:Element {
-    var graph:CommitGraph?
+    var graph:Graph9?
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)
         //Swift.print("StatsView.width: " + "\(width)")
@@ -16,6 +16,6 @@ class StatsView:Element {
     func createGraph(){
         let graphContainer = addSubView(Container(width,height,self,"graph"))
         //graph = graphContainer.addSubView(CommitGraph(width,height-48/*,4*/,graphContainer))
-        graph = self.addSubView(Graph9(width,height-48,self))
+        graph = graphContainer.addSubView(Graph9(width,height-48,graphContainer))
     }
 }
