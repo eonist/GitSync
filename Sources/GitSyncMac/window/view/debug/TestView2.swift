381c6179177d4877a1b0990b492cbe608496ffd5
Eonist <30n1st@gmail.com>
2017-04-06 19:07:22 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/debug/TestView2.swift

--- Sources/GitSyncMac/window/view/debug/TestView2.swift
+++ Sources/GitSyncMac/window/view/debug/TestView2.swift
@@ -16,53 +16,9 @@ class TestView2:Element {
         //volumeSlider = addSubView(VolumeSlider(220,20,20,0,self))
         //volumeSlider!.setProgressValue(0.0)
         
-        //createList()
-        //createSliderList()
-        //createRBSliderList()
-        createRBSliderFastList()
+        
     }
-    func createFastList(){
-        let dp:DataProvider = DataProvider("~/Desktop/assets/xml/scrollist.xml".tildePath)
-        list = addSubView(DEPRECATED_FastList(140,73,24,dp,self))
-    }
-    func createSliderFastList(){
-        let dp = DataProvider("~/Desktop/assets/xml/scrollist.xml".tildePath)
-        //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
-        let sliderList: DEPRECATED_ISliderList = self.addSubView(SliderFastList(140, 73, 24, dp, self))
-        _ = sliderList
-        /**/
-        //ListModifier.select(sliderList, "white")
-    }
-    /**
-     *
-     */
-    func createRBSliderList(){
-        let xml = FileParser.xml("~/Desktop/assets/xml/scrollist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
-        let dp:DataProvider = DataProvider(xml)
-        //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
-        let list = self.addSubView(DEPRECATED_RBSliderList(140, 73, 24, dp, self))
-        _ = list
-    }
-    /**
-     *
-     */
-    func createRBSliderFastList(){
-        let xml = FileParser.xml("~/Desktop/assets/xml/longlist.xml".tildePath)//TODO:  create a method tht takes url and makes dp
-        let dp:DataProvider = DataProvider(xml)
-        //let sliderList:ISliderList = self.addSubView(SliderList(140, 73, 24, dp, self))
-        let list = self.addSubView(DEPRECATED_RBSliderFastList(140, 145, 24, dp, self))
-        _ = list
-    }
-    /**
-     *
-     */
-    override func onEvent(_ event:Event) {
-        if(event.assert(SliderEvent.change, volumeSlider)){
-            let volumSliderProgress = (event as! SliderEvent).progress
-            //Swift.print("volumSliderProgress: " + "\(volumSliderProgress)")
-            list?.setProgress(volumSliderProgress)
-        }
-    }
+    
 }
 /**
  * TestView when making the progressIndicator
