f56fc939ad253a18bc0d25b089a74c108b924228
Eonist <30n1st@gmail.com>
2017-04-06 14:56:23 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/GraphComponent.swift

--- GitSyncMacApp/lib/graph2/utils/GraphComponent.swift
+++ GitSyncMacApp/lib/graph2/utils/GraphComponent.swift
@@ -3,12 +3,12 @@ import Foundation
 @testable import Element
 
 class GraphComponent:Element {//alternate name? GraphArea?
-    var graphPoints:[Element]?//rename to graphDots for clearity?
+    var dots:[Element] = []//rename to graphDots for clearity?
     var graphLine:GraphLine?
     var contentContainer:Element?//remove?, or rename to dotContainer?
     /*Anim*/
-    var graphPts:[CGPoint]?
-    var prevGraphPts:[CGPoint]?/*interim var*/
+    var points:[CGPoint]?
+    var prevPoints:[CGPoint]?/*interim var*/
     var animator:Animator?
     override func resolveSkin() {
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync/stats/graphcomponent.css")
@@ -25,9 +25,9 @@ extension GraphComponent{
      * Re-calc and set the graphPoint positions (for instance if the hValues has changed etc)
      */
     func updateGraph(_ vValues:[CGFloat]){
-        prevGraphPts = graphPts.map{$0}//grabs the location of where the pts are now
+        prevPoints = points.map{$0}//grabs the location of where the pts are now
         let maxValue:CGFloat = vValues.max()!//Finds the largest number in among vValues
-        graphPts = GraphUtils.points(CGSize(w,h), CGPoint(0,0), CGSize(100,100), vValues, maxValue,Graph9.config.margin.width,Graph9.config.margin.height)
+        points = GraphUtils.points(CGSize(w,h), CGPoint(0,0), CGSize(100,100), vValues, maxValue,Graph9.config.margin.width,Graph9.config.margin.height)
         /*GraphPoints*/
         if(animator != nil){animator!.stop()}/*stop any previous running animation*/
         animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeIn)
@@ -42,8 +42,8 @@ extension GraphComponent{
     func createGraphLine(/*_ vValues:[CGFloat], _ maxValue:CGFloat*/){
         let vValues:[CGFloat] = Array(repeating: 0, count: 7)
         let maxValue:CGFloat = 0
-        graphPts = GraphUtils.points(CGSize(w,h), CGPoint(0,0), CGSize(100,100), vValues, maxValue, 50, 50)
-        let path:IPath = PolyLineGraphicUtils.path(graphPts!)
+        points = GraphUtils.points(CGSize(w,h), CGPoint(0,0), CGSize(100,100), vValues, maxValue, 0, 0)
+        let path:IPath = PolyLineGraphicUtils.path(points!)
         graphLine = contentContainer!.addSubView(GraphLine(width,height,path,contentContainer!))
     }
     /**
@@ -51,10 +51,9 @@ extension GraphComponent{
      * NOTE: We could create something called GraphPoint, but it would be another thing to manager so instead we just use an Element with id: graphPoint
      */
     func createGraphPoints(){
-        graphPoints = []//what is this?
-        graphPts!.forEach{
+        points!.forEach{
             let graphPoint:Element = contentContainer!.addSubView(Element(NaN,NaN,contentContainer!,"graphPoint"))
-            graphPoints!.append(graphPoint)
+            dots.append(graphPoint)
             graphPoint.setPosition($0)
         }
     }
@@ -89,10 +88,10 @@ extension GraphComponent{
         //Swift.print("interpolateValue() val: " + "\(val)")
         var positions:[CGPoint] = []
         /*GraphPoints*/
-        for i in 0..<graphPts!.count{
-            let pos:CGPoint = prevGraphPts![i].interpolate(graphPts![i], val)/*interpolates from one point to another*/
+        for i in 0..<points!.count{
+            let pos:CGPoint = prevPoints![i].interpolate(points![i], val)/*interpolates from one point to another*/
             positions.append(pos)
-            graphPoints![i].setPosition(pos)//moves the points
+            dots[i].setPosition(pos)//moves the points
         }
         /*GraphLine*/
         let path:IPath = PolyLineGraphicUtils.path(positions)/*convert points to a Path*/
