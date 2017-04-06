2316ee8fd8108c127266ab334d37ce23f99add8b
Eonist <30n1st@gmail.com>
2017-04-06 21:02:11 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/graph/extensions/debug/GraphDebugHud.swift

--- Sources/GitSyncMac/library/graph/extensions/debug/GraphDebugHud.swift
+++ Sources/GitSyncMac/library/graph/extensions/debug/GraphDebugHud.swift
@@ -2,13 +2,13 @@ import Cocoa
 @testable import Utils
 
 extension Graph9{
-    override func touchesBegan(with event:NSEvent) {
-        gestureHUD.touchesBegan(event)
-    }
-    override func touchesMoved(with event:NSEvent) {
-        gestureHUD.touchesMoved(event)
-    }
-    override func touchesEnded(with event:NSEvent) {
-        gestureHUD.touchesEnded(event)
-    }
+    /*override func touchesBegan(with event:NSEvent) {
+     gestureHUD.touchesBegan(event)
+     }
+     override func touchesMoved(with event:NSEvent) {
+     gestureHUD.touchesMoved(event)
+     }
+     override func touchesEnded(with event:NSEvent) {
+     gestureHUD.touchesEnded(event)
+     }*/
 }
