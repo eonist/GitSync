819e638af68add82abbb31aab6e2f72327999280
Eonist <30n1st@gmail.com>
2017-04-06 15:28:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/date/DateIndicator.swift

--- GitSyncMacApp/lib/graph2/date/DateIndicator.swift
+++ GitSyncMacApp/lib/graph2/date/DateIndicator.swift
@@ -1,5 +1,10 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class DateIndicator {
-
+class DateIndicator:Element{
+    override func resolveSkin() {
+        super.resolveSkin()
+        dateText = addSubView(TextArea(NaN,NaN,"00/00/00 - 00/00/00",self,"date"))/*A TextField that displays the time range of the graph*/
+    }
 }
