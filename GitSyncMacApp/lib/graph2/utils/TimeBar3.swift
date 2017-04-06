65fcd7e4b4ad786ed1f5a26fffc4f65ee17d4a94
Eonist <30n1st@gmail.com>
2017-04-06 13:40:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/lib/graph2/utils/TimeBar3.swift

--- GitSyncMacApp/lib/graph2/utils/TimeBar3.swift
+++ GitSyncMacApp/lib/graph2/utils/TimeBar3.swift
@@ -11,7 +11,7 @@ class TimeBar3:ElasticScrollFastList{
         /*RubberBand*/
         let frame:RubberBand.Frame = (min:0,len:maskSize[dir])//CGRect(0,0,width,height)/*represents the visible part of the content *///TODO: could be ranmed to maskRect
         let itemsRect:RubberBand.Frame = (min:0,len:contentSize[dir])/*represents the total size of the content *///TODO: could be ranmed to contentRect
-        mover = SnappyRubberBand(Animation.sharedInstance,setProgress/*👈important*/,frame,itemsRect)
+        mover = SnappyRubberBand(Animation.sharedInstance,setProgress/*👈*/,frame,itemsRect)
         mover!.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
     }
     override func scrollWheel(with event: NSEvent) {
