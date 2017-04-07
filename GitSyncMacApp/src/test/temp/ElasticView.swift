6a056708e4d7675c8b9d75ed101f75fc80494774
Eonist <30n1st@gmail.com>
2017-04-07 18:06:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/ElasticView.swift

--- GitSyncMacApp/src/test/temp/ElasticView.swift
+++ GitSyncMacApp/src/test/temp/ElasticView.swift
@@ -41,11 +41,11 @@ extension ElasticView{
      */
     func onScrollWheelChange(_ event:NSEvent){
         //Swift.print("👻📜 (ElasticScrollable).onScrollWheelChange : \(event.type)")
-        iterimScroll(.hor).prevScrollingDelta = event.scrollingDelta[.hor]/*is needed when figuring out which dir the wheel is spinning and if its spinning at all*/
-        iterimScroll(.ver).prevScrollingDelta = event.scrollingDelta[.ver]
+        iterimScrollGroup!.iterimScroll(.hor).prevScrollingDelta = event.scrollingDelta[.hor]/*is needed when figuring out which dir the wheel is spinning and if its spinning at all*/
+        iterimScrollGroup!.iterimScroll(.ver).prevScrollingDelta = event.scrollingDelta[.ver]
         
-        _ = iterimScroll(.hor).velocities.shiftAppend(event.scrollingDelta[.hor])/*insert new velocity at the begining and remove the last velocity to make room for the new*/
-        _ = iterimScroll(.ver).velocities.shiftAppend(event.scrollingDelta[.ver])
+        _ = iterimScrollGroup.iterimScroll(.hor).velocities.shiftAppend(event.scrollingDelta[.hor])/*insert new velocity at the begining and remove the last velocity to make room for the new*/
+        _ = iterimScrollGroup.iterimScroll(.ver).velocities.shiftAppend(event.scrollingDelta[.ver])
         moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
         moverGroup!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
         let p = CGPoint(moverGroup!.mover(.hor).result,moverGroup!.mover(.ver).result)
