a80a764bbebb6f59883963a4173a6c191050ccc0
Eonist <30n1st@gmail.com>
2017-04-08 14:00:33 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
@@ -6,5 +6,15 @@ protocol Progressable3:Containable3{
     var progress:CGFloat {get}
     var interval:CGFloat {get}
     //setProgress
+    func setProgress(_ progress:CGFloat)
+    var dir:Dir {get}
 }
 
+extension Progressable3{
+    func setProgress(_ value:CGFloat,_ dir:Dir){
+        contentContainer!.point[dir] = value
+    }
+    func setProgress(_ point:CGPoint){
+        contentContainer!.point = point
+    }
+}
