485c3af6b304c61ba250762915fee73c5659c1e0
Eonist <30n1st@gmail.com>
2017-04-08 13:12:09 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/utils/IterimScrollGroup.swift

--- GitSyncMacApp/src/test/temp/utils/IterimScrollGroup.swift
+++ GitSyncMacApp/src/test/temp/utils/IterimScrollGroup.swift
@@ -18,4 +18,7 @@ extension IterimScrollGroup{
         get{return zip(iterimScrollX.velocities,iterimScrollY.velocities).map{CGPoint($0.0,$0.1)}}
         set{iterimScrollX.velocities = newValue.map{$0.x};iterimScrollY.velocities = newValue.map{$0.y}}
     }
+    func shiftAppend(delta:[CGFloat]){
+        
+    }
 }
