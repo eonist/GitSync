cfb3dc0194d2813044ed5e86f1eb6755c55bec01
Eonist <30n1st@gmail.com>
2017-04-08 22:43:47 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -1,5 +1,13 @@
-import Foundation
+import Cocoa
+@testable import Element
+@testable import Utils
 
 protocol ElasticSlidableScrollable3:ElasticScrollable, Slidable {
     
+}
+extension ElasticSlidableScrollable3{
+    func scrollWheelExitedAndIsStationary() {
+        Swift.print("👻🏂📜 ElasticSlidableScrollable2.scrollWheelExitedAndIsStationary()")
+        hideSlider()
+    }
 }
