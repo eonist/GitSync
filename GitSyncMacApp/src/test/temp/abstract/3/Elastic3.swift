903c879722dfefc714a78e25e4a5f77d23b7d46f
Eonist <30n1st@gmail.com>
2017-04-08 13:52:48 +0200

    New files added: 4, Files modified: 2, Files deleted: 4
    Deleted 4 files:GitSyncMacApp/src/test/temp/abstract/Containable2.swiftGitSyncMacApp/src/test/temp/abstract/Elastic2.swiftGitSyncMacApp/src/test/temp/abstract/ElasticScrollable2.swiftGitSyncMacApp/src/test/temp/abstract/Scrollable2.swiftModified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swiftGitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
@@ -6,20 +6,3 @@ protocol Elastic3 {
     var moverGroup:MoverGroup? {get}
     var iterimScrollGroup:IterimScrollGroup? {get}
 }
-protocol Scrollable3:Progressable3 {
-    func onScrollWheelChange(_ event:NSEvent)
-    func onScrollWheelEnter()
-    func onScrollWheelExit()
-}
-protocol Progressable3:Containable3{
-    var progress:CGFloat {get}
-    var interval:CGFloat {get}
-    //setProgress
-}
-
-protocol Containable3{
-    var maskSize:CGSize{get}
-    var contentSize:CGSize{get}
-    var itemSize:CGSize{get}
-    var contentContainer:Element? {get}
-}
