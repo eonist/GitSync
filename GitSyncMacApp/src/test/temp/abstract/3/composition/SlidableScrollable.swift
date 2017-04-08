6c46515ec44f02281285189b77865fa1e4563823
Eonist <30n1st@gmail.com>
2017-04-08 20:41:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
@@ -5,6 +5,12 @@ import Cocoa
 protocol SlidableScrollable3:Slidable3, Scrollable3 {}
 extension SlidableScrollable3 {
     /**
+     *
+     */
+    func progress(){
+        
+    }
+    /**
      * TODO: you could also override scroll and hock after the forward scroll call and then retrive the progress from the var. less code, but the value must be written in Displaceview, it could mess up Elastic, because it needs different progress. etc, do later
      */
     func onScrollWheelChange(_ event:NSEvent) {
