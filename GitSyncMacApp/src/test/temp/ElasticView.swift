63e3b9af2ebf5d72cbf58ca46b66e947e1b7f82f
Eonist <30n1st@gmail.com>
2017-04-08 15:40:58 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/ElasticView.swiftGitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift

--- GitSyncMacApp/src/test/temp/ElasticView.swift
+++ GitSyncMacApp/src/test/temp/ElasticView.swift
@@ -16,15 +16,7 @@ class ElasticView:ContainerView2{//
     }
     override func scrollWheel(with event: NSEvent) {
         //Swift.print("scrollWheel event.scrollingDeltaX: \(event.scrollingDeltaX) event.scrollingDeltaY: \(event.scrollingDeltaY)")
-        switch event.phase{
-            case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
-            case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
-            case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesn't fire if you begin the scrollWheel gesture very quickly*/
-            case NSEventPhase.ended:onScrollWheelExit()//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
-            case NSEventPhase.cancelled:onScrollWheelExit()//Swift.print("cancelled")/*this trigers if the scrollWhell gestures goes off the trackpad etc*/
-            //case NSEventPhase(rawValue:0):onInDirectScrollWheelChange(event);/*Swift.print("none");*/break;//swift 3 update, was -> NSEventPhase.none
-            default:break;
-        }
+        
         super.scrollWheel(with:event)
     }
 }
--- GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
@@ -7,21 +7,12 @@ protocol Scrollable3:Progressable3 {
     func onScrollWheelEnter()
     func onScrollWheelExit()
 }
-extension Scrollable2{
+extension Scrollable3{
     /**
      * NOTE: if the prev Change event only had -1 or 1 or 0. Then you released with no momentum and so no anim should be initiated
      */
     func scroll(_ event:NSEvent){
         //Swift.print("Scrollable2.scroll() \(event.phase.type) scrollDeltaX: \(event.scrollingDeltaX) deltaX: \(event.deltaX)")
-        switch event.phase{
-            case NSEventPhase.changed:onScrollWheelChange(event)/*Direct scroll*/
-            case NSEventPhase.mayBegin:onScrollWheelEnter()
-            case NSEventPhase.began:onScrollWheelEnter()
-            case NSEventPhase.ended:onScrollWheelExit()//always exits with event with no delta
-            case NSEventPhase.cancelled:onScrollWheelExit()//always exits with event with no delta
-            case NSEventPhase(rawValue:0):onScrollWheelChange(event)/*this is the same as momentum aka inDirect scroll, Toggeling this on and off can break things*/
-            default:break;
-        }
         switch event.phase{
             case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
             case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
@@ -33,7 +24,7 @@ extension Scrollable2{
         }
         //super.scrollWheel(with:event)
     }
-    func onScrollWheelChange(_ event:NSEvent) {}/*Direct scroll, not momentum*/
-    func onScrollWheelEnter() {}
-    func onScrollWheelExit() {}
+    func onScrollWheelChange(_ event:NSEvent){}/*Direct scroll, not momentum*/
+    func onScrollWheelEnter(){}
+    func onScrollWheelExit(){}
 }
