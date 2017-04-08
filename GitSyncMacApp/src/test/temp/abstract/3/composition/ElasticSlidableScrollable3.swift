297e2c40aa1232c8d5c3c7871c8de26c9d6a73c2
Eonist <30n1st@gmail.com>
2017-04-08 22:52:43 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -4,6 +4,15 @@ import Cocoa
 
 protocol ElasticSlidableScrollable3:ElasticScrollable3,Slidable3{}
 extension ElasticSlidableScrollable3{
+    /**
+     * PARAM: value represents real contentContainer x/y value, not 0-1 val
+     */
+    func setProgress(_ value:CGFloat) {
+        Swift.print("👻🏂📜 ElasticSlidableScrollable.setProgress(\(value)) 💥")
+        (self as Elastic).setProgress(value)
+        let sliderProgress = ElasticUtils.progress(value,contentSize[dir],maskSize[dir])
+        slider!.setProgressValue(sliderProgress)//<- scalar value 0-1
+    }
     func onScrollWheelEnter() {
         showSlider()
     }
