1c1e60e2ef534a0a758fe8dc316137c9e0ff57e2
Eonist <30n1st@gmail.com>
2017-04-08 14:07:28 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
@@ -7,3 +7,23 @@ protocol Scrollable3:Progressable3 {
     func onScrollWheelEnter()
     func onScrollWheelExit()
 }
+extension Scrollable2{
+    /**
+     * NOTE: if the prev Change event only had -1 or 1 or 0. Then you released with no momentum and so no anim should be initiated
+     */
+    func scroll(_ event:NSEvent){
+        //Swift.print("Scrollable2.scroll() \(event.phase.type) scrollDeltaX: \(event.scrollingDeltaX) deltaX: \(event.deltaX)")
+        switch event.phase{
+            case NSEventPhase.changed:onScrollWheelChange(event)/*Direct scroll*/
+            case NSEventPhase.mayBegin:onScrollWheelEnter()
+            case NSEventPhase.began:onScrollWheelEnter()
+            case NSEventPhase.ended:onScrollWheelExit()//always exits with event with no delta
+            case NSEventPhase.cancelled:onScrollWheelExit()//always exits with event with no delta
+            case NSEventPhase(rawValue:0):onScrollWheelChange(event)/*this is the same as momentum aka inDirect scroll, Toggeling this on and off can break things*/
+            default:break;
+        }
+    }
+    func onScrollWheelChange(_ event:NSEvent) {}/*Direct scroll, not momentum*/
+    func onScrollWheelEnter() {}
+    func onScrollWheelExit() {}
+}
