d05715758afbac3baa04dcee695840258fa2d332
Eonist <30n1st@gmail.com>
2017-04-08 18:30:05 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Slideable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Slideable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Slideable3.swift
@@ -1,5 +1,39 @@
-import Foundation
-
-class Slideable3 {
-
+import Cocoa
+@testable import Utils
+@testable import Element
+/**
+ * Slidable is for Elements that has a slider attached
+ */
+protocol Slideable3:Progressable3 {
+    func updateSlider()
+    var slider:VSlider?{get}
+    var sliderInterval:CGFloat?{get set}//i think this is the same as intervall, remove
+}
+/**
+ * ⚠️️ IMPORTANT: Slidable does not override scroll because a SlideView can't detect scroll. SlideScrollView however can access scroll and call hide and show slider. And then use protocol ambiguity to call scroll on the Scrollable after
+ */
+extension Slidable {
+    /**
+     * Updates the slider interval and the sliderThumbSize (after DP events: add/remove etc)
+     */
+    func updateSlider(){
+        /*
+        sliderInterval = floor(self.itemsHeight - height)/itemHeight
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height/*<--this should probably be .getHeight()*/);
+        slider!.setThumbHeightValue(thumbHeight)
+        let progress:CGFloat = SliderParser.progress(lableContainer!.y, height, itemsHeight)//TODO: use getHeight() instead of height
+        slider!.setProgressValue(progress)
+        */
+    }
+    func hideSlider(){
+        Swift.print("🏂 hide slider")
+        //self.slider!.thumb!.setSkinState("inActive")
+        //if(slider?.thumb?.getSkinState() == SkinStates.none){slider?.thumb?.fadeOut()}/*only fade out if the state is none, aka not over*/
+        //slider?.thumb?.fadeOut()
+    }
+    func showSlider(){
+        Swift.print("🏂 show slider")
+        //self.slider!.thumb!.setSkinState(SkinStates.none)
+        //slider!.thumb!.fadeIn()
+    }
 }
