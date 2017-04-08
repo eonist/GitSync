6f3b39956f835f702ad23ee45dba1f165e6a2518
Eonist <30n1st@gmail.com>
2017-04-08 14:31:11 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
@@ -2,7 +2,15 @@ import Cocoa
 @testable import Utils
 @testable import Element
 
-protocol Elastic3 {
+protocol Elastic3:Progressable3{
     var moverGroup:MoverGroup? {get}
     var iterimScrollGroup:IterimScrollGroup? {get}
+}
+extension Elastic3{
+    func setProgress(_ value:CGFloat,_ dir:Dir){
+        contentContainer!.point[dir] = value
+    }
+    func setProgress(_ point:CGPoint){
+        contentContainer!.point = point
+    }
 }
