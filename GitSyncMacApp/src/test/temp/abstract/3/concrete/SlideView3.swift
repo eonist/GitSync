fc06a3192498d4bab53e8f8bc41933b42b599a1c
Eonist <30n1st@gmail.com>
2017-04-08 21:00:15 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
@@ -2,13 +2,14 @@ import Cocoa
 @testable import Utils
 
 class SlideView3:ContainerView3, Slidable3 {
+    var slider
     var horSlider:Slider?
     var verSlider:Slider?
     override func resolveSkin() {
         super.resolveSkin()
         /*slider*/
-        intervalX = floor(contentSize.height - maskSize.height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
-        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        //intervalX = floor(contentSize.height - maskSize.height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(Slider(/*itemHeight,height,0,0,self*/))
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
         slider!.setThumbHeightValue(thumbHeight)
         slider!.thumb!.fadeOut()//inits fade out anim on init
