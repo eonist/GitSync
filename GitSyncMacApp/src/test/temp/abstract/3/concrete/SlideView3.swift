27938af678a0d6b8e08b092a577da0c910357ff6
Eonist <30n1st@gmail.com>
2017-04-08 20:30:24 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
@@ -1,5 +1,23 @@
-import Foundation
+import Cocoa
+@testable import Utils
 
-class SlideView3 {
-
+class SlideView3:ContainerView3, Slidable3 {
+    var horSlider:Slider?
+    var verSlider:Slider?
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*slider*/
+        sliderInterval = floor(contentSize - height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        slider = addSubView(VSlider(itemHeight,height,0,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        slider!.setThumbHeightValue(thumbHeight)
+        slider!.thumb!.fadeOut()//inits fade out anim on init
+    }
+    override func onEvent(_ event:Event) {
+        //Swift.print("event: " + "\(event)")
+        if(event === (SliderEvent.change,slider!)){
+            setProgress((event as! SliderEvent).progress)
+        }/*events from the slider*/
+        super.onEvent(event)
+    }
 }
