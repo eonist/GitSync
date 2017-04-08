5974249d414cb548f135898d183787291839e0e4
Eonist <30n1st@gmail.com>
2017-04-08 15:38:48 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/ElasticView.swiftGitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
@@ -22,6 +22,16 @@ extension Scrollable2{
             case NSEventPhase(rawValue:0):onScrollWheelChange(event)/*this is the same as momentum aka inDirect scroll, Toggeling this on and off can break things*/
             default:break;
         }
+        switch event.phase{
+            case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
+            case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
+            case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesn't fire if you begin the scrollWheel gesture very quickly*/
+            case NSEventPhase.ended:onScrollWheelExit()//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
+            case NSEventPhase.cancelled:onScrollWheelExit()//Swift.print("cancelled")/*this trigers if the scrollWhell gestures goes off the trackpad etc*/
+            case NSEventPhase(rawValue:0):/*onInDirectScrollWheelChange*/onScrollWheelChange(event);/*this is the same as momentum aka inDirect scroll, Toggeling this on and off can break things*/
+            default:break;
+        }
+        //super.scrollWheel(with:event)
     }
     func onScrollWheelChange(_ event:NSEvent) {}/*Direct scroll, not momentum*/
     func onScrollWheelEnter() {}
