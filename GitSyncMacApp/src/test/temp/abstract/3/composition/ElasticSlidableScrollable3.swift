998796c0f723aff29de6b627ea357b1243c77dd5
Eonist <30n1st@gmail.com>
2017-04-08 23:05:02 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -5,11 +5,10 @@ import Cocoa
 protocol ElasticSlidableScrollable3:ElasticScrollable3,Slidable3{}
 extension ElasticSlidableScrollable3{
     func onScrollWheelChange(_ event: NSEvent) {
-        (self as ElasticScrollable3).onScrollWheelChange(event)
+        (self as ElasticScrollable3).onScrollWheelChange(event)//forward the call
         if(event.phase == NSEventPhase.changed){
             if(moverGroup!.isDirectlyManipulating){
-                //also manipulates slider, but only on directTransmission, as mover calls setProgress from shallow in indirectTransmission
-                setProgress(mover!.result)//👈NEW, this migth need to be inSide scrollWheel call, as it needs to be shallow to reach inside setProgress in ElasticFastList.setProgress, but maybe not, To be continued
+                setProgress(moverGroup!.result)
             }
         }
     }
