91f85d2eead923f40bbb9971a5a1ceb32e1efca1
Eonist <30n1st@gmail.com>
2017-04-09 23:41:08 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ElasticScrollList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ElasticScrollList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ElasticScrollList3.swift
@@ -1,5 +1,6 @@
 import Foundation
 
-class ElasticScrollList {
-
+class ElasticScrollList3:List3,ElasticScrollable3 {
+    lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
+    lazy var iterimScrollGroup:IterimScrollGroup? = IterimScrollGroup()
 }
