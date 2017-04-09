0d0257b1d265381a02ea9811d07d00e2901a9258
Eonist <30n1st@gmail.com>
2017-04-09 23:17:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift
@@ -10,13 +10,7 @@ class SlideScrollList3:List3,SlidableScrollable3{
         horSlider = self.addSubView(Slider(width,itemSize.height,.hor,itemSize,0,self))
         verSlider = self.addSubView(Slider(itemSize.width,height,.ver,itemSize,0,self))
         
-        //Continue here:
-            //thumbHeight is wrong, see old code replace height/itemsize.height
-        
         let thumbHeight:CGFloat = SliderParser.thumbSize(height/contentSize.height, verSlider!.height)
-        Swift.print("thumbHeight: " + "\(thumbHeight)")
-        Swift.print("contentSize.height: " + "\(contentSize.height)")
-        Swift.print("maskSize.height: " + "\(maskSize.height)")
         verSlider!.setThumbSide(thumbHeight)
         
         let thumbWidth:CGFloat = SliderParser.thumbSize(width/itemSize.width, horSlider!.width)
