38395f4b9e8bb11ed6f5653b5a29a2800356f5c2
Eonist <30n1st@gmail.com>
2017-04-09 15:45:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/SlideView3.swift
@@ -14,14 +14,14 @@ class SlideView3:ContainerView3, Slidable3 {
     override func resolveSkin() {
         super.resolveSkin()
         /*slider*/
-        //intervalX = floor(contentSize.height - maskSize.height)/itemHeight// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
+        let intervalX = floor(contentSize.height - maskSize.height)/24// :TODO: use ScrollBarUtils.interval instead?// :TODO: explain what this is in a comment
         
         verSlider = addSubView(Slider(width,24))
         horSlider = addSubView(Slider(24,height))
         
-        /*let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemsHeight, slider!.height)
          slider!.setThumbHeightValue(thumbHeight)
-         slider!.thumb!.fadeOut()//inits fade out anim on init*/
+         slider!.thumb!.fadeOut()//inits fade out anim on init
     }
     override func onEvent(_ event:Event) {
         //Swift.print("event: " + "\(event)")
