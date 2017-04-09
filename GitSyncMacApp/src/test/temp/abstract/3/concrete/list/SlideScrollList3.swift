aedeec384a6d5a90fc49752637c43b7243cac645
Eonist <30n1st@gmail.com>
2017-04-09 21:37:14 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideScrollList3.swift
@@ -1,6 +1,17 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
 class SlideScrollList3:List3,SlidableScrollable3{
     var horSlider:Slider?
     var verSlider:Slider?
+    override func resolveSkin() {
+        super.resolveSkin()
+        
+        
+        verSlider = self.addSubView(Slider(itemSize.width,height,.ver,itemSize,0,self))
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/itemSize.height, verSlider!.height)
+        verSlider!.setThumbSide(thumbHeight)
+        verSlider!.thumb!.fadeOut()
+    }
 }
