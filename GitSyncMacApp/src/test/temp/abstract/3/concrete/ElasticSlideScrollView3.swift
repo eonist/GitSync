91e5a53a4fb1bcc9abd8b527b62d0d2215d0e720
Eonist <30n1st@gmail.com>
2017-04-10 00:38:03 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift
@@ -4,9 +4,16 @@ import Cocoa
 class ElasticSlideScrollView3:SlideView3,ElasticSlidableScrollable3 {
     lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
     lazy var iterimScrollGroup:IterimScrollGroup? = IterimScrollGroup()
-
+    
+    
+    override func resolveSkin() {
+        super.resolveSkin()
+        moverGroup!.xMover.event = onEvent/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+        moverGroup!.yMover.event = onEvent
+    }
     override func onEvent(_ event:Event) {
-        if(event === (AnimEvent.stopped, moverGroup!)){
+        Swift.print("ElasticSlideScrollView3.onEvent \(event.type)")
+        if(event === (AnimEvent.stopped, moverGroup!.yMover)){//only supports yMover atm
             Swift.print("anim stopped")
             hideSlider()/*hides the slider when bounce back anim stopps*/
         }
