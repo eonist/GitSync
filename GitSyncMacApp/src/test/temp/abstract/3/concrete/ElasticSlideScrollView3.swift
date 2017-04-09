875e0d485c37dae5919df3c16d989ae12027e461
Eonist <30n1st@gmail.com>
2017-04-09 12:10:59 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/ElasticSlideScrollView3.swift
@@ -1,5 +1,13 @@
-import Foundation
+import Cocoa
+@testable import Utils
 
-class ElasticSlideScrollView3 {
-
+class ElasticSlideScrollView3:SlideView3,ElasticSlidableScrollable3 {
+    var moverGroup:MoverGroup?
+    var iterimScrollGroup:IterimScrollGroup?
+    
+    override func resolveSkin() {
+        super.resolveSkin()
+        iterimScrollGroup = IterimScrollGroup()
+        moverGroup = MoverGroup(setProgress,maskSize,contentSize)
+    }
 }
