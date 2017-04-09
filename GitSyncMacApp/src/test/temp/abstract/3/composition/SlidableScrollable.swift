d53f9e60a0fc4a691dfc5bcacd83738dc0d50931
Eonist <30n1st@gmail.com>
2017-04-09 16:18:30 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
@@ -15,9 +15,9 @@ extension SlidableScrollable3 {
      */
     func onScrollWheelChange(_ event:NSEvent) {
         Swift.print("🏂📜 SlidableScrollable3.onScrollWheelChange: \(event.type)")
-        Swift.print("event.delta[.hor]: " + "\(event.delta[.hor])")
-        Swift.print("interval(.hor): " + "\(interval(.hor))")
-        Swift.print("slider(.hor).progress: " + "\(slider(.hor).progress)")
+        /*Swift.print("event.delta[.hor]: " + "\(event.delta[.hor])")
+         Swift.print("interval(.hor): " + "\(interval(.hor))")
+         Swift.print("slider(.hor).progress: " + "\(slider(.hor).progress)")*/
         let horProg:CGFloat = SliderListUtils.progress(event.delta[.hor], 5/*interval(.hor)*/, slider(.hor).progress)//TODO: ⚠️️ merge these 2 lines into one and make a method in SliderListUtils that returns point
         let verProg:CGFloat = SliderListUtils.progress(event.delta[.ver], 5/*interval(.ver)*/, slider(.ver).progress)
         let progress:CGPoint = CGPoint(horProg,verProg)
