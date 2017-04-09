a127fb1f1efc24f081fed376af8272c1395fc330
Eonist <30n1st@gmail.com>
2017-04-10 00:14:52 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/SlideList3.swift
@@ -1,5 +1,28 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
-class SlideList3 {
-
+class SlideList3:List3,Slidable3 {
+    var horSlider:Slider?
+    var verSlider:Slider?
+    override func resolveSkin() {
+        super.resolveSkin()
+        horSlider = self.addSubView(Slider(width,itemSize.height,.hor,itemSize,0,self))
+        verSlider = self.addSubView(Slider(itemSize.width,height,.ver,itemSize,0,self))
+        
+        let thumbHeight:CGFloat = SliderParser.thumbSize(height/contentSize.height, verSlider!.height)
+        verSlider!.setThumbSide(thumbHeight)
+        verSlider!.thumb!.fadeOut()//inits fade out anim on init
+        
+        let thumbWidth:CGFloat = SliderParser.thumbSize(width/itemSize.width, horSlider!.width)
+        horSlider!.setThumbSide(thumbWidth)
+        horSlider!.thumb!.fadeOut()//inits fade out anim on init
+    }
+    override func onEvent(_ event:Event) {
+        if(event == SliderEvent.change){
+            let dir:Dir = event.origin === horSlider ? .hor : .ver
+            setProgress((event as! SliderEvent).progress,dir)
+        }
+        super.onEvent(event)
+    }
 }
