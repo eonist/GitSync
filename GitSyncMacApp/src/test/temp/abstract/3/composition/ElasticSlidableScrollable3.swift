1dd1a734d8f775e2eef9c1ee6f6b2d21ce07983a
Eonist <30n1st@gmail.com>
2017-04-09 17:22:37 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -4,20 +4,14 @@ import Cocoa
 
 protocol ElasticSlidableScrollable3:Slidable3,ElasticScrollable3{}
 extension ElasticSlidableScrollable3{
-    
-    override func onScrollWheelChange(_ event: NSEvent) {
-        if(event.phase == NSEventPhase.changed){
-            if(moverGroup!.isDirectlyManipulating){
-                (self as Elastic3).setProgress(moverGroup!.result)
-            }
-        }
-    }
     /**
      * PARAM: value represents real contentContainer x/y value, not 0-1 val
      */
     func setProgress(_ value:CGFloat, _ dir:Dir) {
+        Swift.print("ElasticSlidableScrollable3.setProgress()")
         (self as Elastic3).setProgress(value,dir)
         let sliderProgress = ElasticUtils.progress(value,contentSize[dir],maskSize[dir])
+        Swift.print("sliderProgress: " + "\(sliderProgress)")
         (self as Slidable3).setProgress(sliderProgress,dir)
     }
     func onScrollWheelEnter() {
@@ -26,4 +20,14 @@ extension ElasticSlidableScrollable3{
     func scrollWheelExitedAndIsStationary() {
         hideSlider()
     }
+}
+
+extension SlideView3{
+    /*override func scrollWheel(with event: NSEvent) {
+     if(event.phase == NSEventPhase.changed){
+     if((self as! Elastic3).moverGroup!.isDirectlyManipulating){
+     (self as! Elastic3).setProgress((self as! Elastic3).moverGroup!.result)
+     }
+     }
+     }*/
 }
