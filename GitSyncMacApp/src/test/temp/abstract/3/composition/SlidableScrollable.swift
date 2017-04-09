e62cd20a84575029b0859eaa4c040e3cdc17eba7
Eonist <30n1st@gmail.com>
2017-04-09 16:16:10 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
@@ -18,8 +18,8 @@ extension SlidableScrollable3 {
         Swift.print("event.delta[.hor]: " + "\(event.delta[.hor])")
         Swift.print("interval(.hor): " + "\(interval(.hor))")
         Swift.print("slider(.hor).progress: " + "\(slider(.hor).progress)")
-        let horProg:CGFloat = SliderListUtils.progress(event.delta[.hor], interval(.hor), slider(.hor).progress)//TODO: ⚠️️ merge these 2 lines into one and make a method in SliderListUtils that returns point
-        let verProg:CGFloat = SliderListUtils.progress(event.delta[.ver], interval(.ver), slider(.ver).progress)
+        let horProg:CGFloat = SliderListUtils.progress(event.delta[.hor], 5/*interval(.hor)*/, slider(.hor).progress)//TODO: ⚠️️ merge these 2 lines into one and make a method in SliderListUtils that returns point
+        let verProg:CGFloat = SliderListUtils.progress(event.delta[.ver], 5/*interval(.ver)*/, slider(.ver).progress)
         let progress:CGPoint = CGPoint(horProg,verProg)
         Swift.print("progress: " + "\(progress)")
         (self as Slidable3).setProgress(progress)
