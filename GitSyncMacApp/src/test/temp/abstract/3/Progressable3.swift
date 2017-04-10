36697b15810ed8b6aee58a9d96e7d3949ca08679
Eonist <30n1st@gmail.com>
2017-04-10 13:24:51 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swiftGitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
@@ -6,6 +6,7 @@ protocol Progressable3:Containable3{
     func progress(_ dir:Dir) -> CGFloat/*0-1 atBegining <-> atEnd*/
     func interval(_ dir:Dir) -> CGFloat/*describes the speed when scrolling (distance per scroll tick)*/
     func setProgress(_ progress:CGFloat,_ dir:Dir)
+    func setProgress(_ point:CGPoint)
 }
 extension Progressable3{
     var itemSize:CGSize {return CGSize(24,24)}//temp, use a static interval like 4 or 8, then use itemsize only for listable etc
--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -4,6 +4,9 @@ import Cocoa
 
 protocol ElasticSlidableScrollable3:Slidable3,ElasticScrollable3{}
 extension ElasticSlidableScrollable3{
+    func setProgress(_ point: CGPoint) {
+        Swift.print("ElasticSlidableScrollable3.setProgress(p)")
+    }
     /**
      * PARAM: value represents real contentContainer x/y value, not 0-1 val
      */
