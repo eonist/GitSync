e60412d950926cd95ab1ee86af050f11d6172d35
Eonist <30n1st@gmail.com>
2017-04-10 13:26:59 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swiftGitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -6,6 +6,7 @@ protocol ElasticSlidableScrollable3:Slidable3,ElasticScrollable3{}
 extension ElasticSlidableScrollable3{
     func setProgress(_ point: CGPoint) {
         Swift.print("ElasticSlidableScrollable3.setProgress(p)")
+        fatalError("debug")
     }
     /**
      * PARAM: value represents real contentContainer x/y value, not 0-1 val
