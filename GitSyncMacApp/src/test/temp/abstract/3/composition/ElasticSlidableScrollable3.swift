f31812f56d17537953c14f6fe503c35238d432da
Eonist <30n1st@gmail.com>
2017-04-10 14:59:16 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -12,7 +12,7 @@ extension ElasticSlidableScrollable3{
      * PARAM: value represents real contentContainer x/y value, not 0-1 val
      */
     func setProgress(_ value:CGFloat, _ dir:Dir) {
-        Swift.print("ElasticSlidableScrollable3.setProgress()")
+        Swift.print("ElasticSlidableScrollable3.setProgress() dir: \(dir)")
         //(self as Elastic3).setProgress(value,dir)
         let sliderProgress = ElasticUtils.progress(value,contentSize[dir],maskSize[dir])
         //Swift.print("sliderProgress: " + "\(sliderProgress)")
