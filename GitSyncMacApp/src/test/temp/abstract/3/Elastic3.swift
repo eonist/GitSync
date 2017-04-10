24a5baf9256d2d1723825a0c6173ffb5c7c78f80
Eonist <30n1st@gmail.com>
2017-04-10 15:31:27 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swiftGitSyncMacApp/src/test/temp/abstract/3/Slidable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Elastic3.swift
@@ -11,11 +11,11 @@ extension Elastic3{
      * PARAM: value: contentContainer x/y value
      */
     func setProgress(_ value:CGFloat,_ dir:Dir){
-        Swift.print("Elastic3.setProgress(value)")
+        //Swift.print("Elastic3.setProgress(value)")
         contentContainer!.point[dir] = value
     }
     func setProgress(_ point:CGPoint){
-        Swift.print("Elastic3.setProgress(p)")
+        //Swift.print("Elastic3.setProgress(p)")
         contentContainer!.point = point
     }
 }
