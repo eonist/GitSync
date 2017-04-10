8f3453da2dea21961ab9103f4c5a7879fc32a14d
Eonist <30n1st@gmail.com>
2017-04-10 23:05:35 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/SlidableScrollable.swift
@@ -3,7 +3,7 @@ import Cocoa
 @testable import Utils
 
 protocol SlidableScrollable3:Slidable3,Scrollable3 {
-    
+    //func onScrollWheelMomentumEnded(_ dir:Dir)
 }
 extension SlidableScrollable3 {
     /**
@@ -23,22 +23,31 @@ extension SlidableScrollable3 {
     }
     func onInDirectScrollWheelChange(_ event: NSEvent) {//enables momentum
         onScrollWheelChange(event)
+        /*if(event.scrollingDeltaX == 0){
+         onScrollWheelMomentumEnded(.hor)
+         }
+         if(event.scrollingDeltaY == 0){
+         onScrollWheelMomentumEnded(.ver)
+         }*/
     }
     func onScrollWheelEnter() {
         showSlider()
     }
     func onScrollWheelExit() {
         Swift.print("onScrollWheelExit()")
-        hideSlider()
+        //hideSlider()
         //continue here: 
             //try to listen to delta from NSEvent.current, or else pass the event to the method
     }
     /**
      * Called only be called when scrollwheel becomes stationary. find the code that does this.
      */
-    func onScrollWheelMomentumEnded(_ dir:Dir)  {
+    //func onScrollWheelMomentumEnded(_ dir:Dir)  {
         //Swift.print("onScrollWheelMomentumEnded")
         //hideSlider()
+    //}
+    func onScrollWheelMomentumEnded()  {
+        hideSlider()
     }
 }
 /*let caseA = iterimScrollGroup!.iterimScrollX.prevScrollingDelta != 1.0 && iterimScrollGroup!.iterimScrollX.prevScrollingDelta != -1.0
