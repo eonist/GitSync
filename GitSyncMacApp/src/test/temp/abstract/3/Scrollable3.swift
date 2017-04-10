d81cec8d2c3b414b9bf8d2b1ef964c02b7261c81
Eonist <30n1st@gmail.com>
2017-04-10 20:13:41 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Scrollable3.swift
@@ -18,7 +18,10 @@ extension Scrollable3{
             case NSEventPhase.changed:onScrollWheelChange(event)/*Fires everytime there is direct scrollWheel gesture movment and momentum, the momentum fades.*/
             case NSEventPhase.mayBegin:onScrollWheelEnter()/*Can be used to detect if two fingers are touching the trackpad*/
             case NSEventPhase.began:onScrollWheelEnter()/*The mayBegin phase doesn't fire if you begin the scrollWheel gesture very quickly*/
-            case NSEventPhase.ended:onScrollWheelExit()//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
+            case NSEventPhase.ended:
+                Swift.print("event.scrollingDelta: " + "\(event.scrollingDelta)")
+                Swift.print("event.delta: " + "\(event.delta)")
+                onScrollWheelExit()//Swift.print("ended")/*if you release your touch-gesture and the momentum of the gesture has stopped.*/
             case NSEventPhase.cancelled:onScrollWheelExit()//Swift.print("cancelled")/*this trigers if the scrollWhell gestures goes off the trackpad etc*/
             case NSEventPhase(rawValue:0):onInDirectScrollWheelChange(event);/**//*onScrollWheelChange(event)*/_ = "";/*this is the same as momentum aka inDirect scroll, Toggeling this on and off can break things*/
             default:break;
