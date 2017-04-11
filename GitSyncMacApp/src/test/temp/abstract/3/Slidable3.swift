0b8a0bc090428320987824cf1297b05f3313d6a0
Eonist <30n1st@gmail.com>
2017-04-11 11:39:31 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Slidable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Slidable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Slidable3.swift
@@ -55,14 +55,18 @@ extension Slidable3 {
         //Swift.print("🏂 hide slider dir: \(dir)")
         //self.slider!.thumb!.setSkinState("inActive")
         //if(slider(dir).thumb!.getSkinState() == SkinStates.none){slider(dir).thumb!.fadeOut()}/*only fade out if the state is none, aka not over*/
-        slider?.thumb?.fadeOut()
+        Swift.print("slider(dir).thumb!.getSkinState(): " + "\(slider(dir).thumb!.getSkinState())")
+        if(slider(dir).thumb!.getSkinState() == SkinStates.none){
+            slider(dir).thumb!.fadeOut()
+        }
+        
         /*slider(dir).thumb!.alpha = 0
          slider(dir).thumb!.skin?.decoratables[0].draw()*/
     }
     func showSlider(_ dir:Dir){
         //Swift.print("🏂 show slider dir: \(dir)")
         //slider(dir).thumb!.setSkinState(SkinStates.none)
-        slider!.thumb!.fadeIn()
+        slider(dir).thumb!.fadeIn()
         /*slider(dir).thumb!.alpha = 1
          slider(dir).thumb!.skin?.decoratables[0].draw()*/
     }
