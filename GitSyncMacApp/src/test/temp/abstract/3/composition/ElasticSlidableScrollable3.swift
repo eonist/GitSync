3ad8aaa613501bbead93d540cb733f6f12945579
Eonist <30n1st@gmail.com>
2017-04-11 18:50:58 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticSlidableScrollable3.swift
@@ -24,25 +24,22 @@ extension ElasticSlidableScrollable3{
         //Swift.print("👻🏂📜 ElasticSlidableScrollable3.scroll()")
         (self as Scrollable3).scroll(event)
         if(event.phase == NSEventPhase.changed){
-            if(moverGroup!.isDirectlyManipulating){
-                //also manipulates slider, but only on directTransmission, as mover calls setProgress from shallow in indirectTransmission
-                //setProgress(moverGroup!.result.y,.ver)//👈NEW, this migth need to be inSide scrollWheel call, as it needs to be shallow to reach inside setProgress in ElasticFastList.setProgress, but maybe not, To be continued
-                //let sliderProgress:CGFloat = ElasticUtils.progress(moverGroup!.result.y,contentSize[.ver],maskSize[.ver])
-                let sliderProgress:CGPoint = ElasticUtils.progress(moverGroup!.result,contentSize,maskSize)
-                //Swift.print("sliderProgress: " + "\(sliderProgress)")
-                (self as Slidable3).setProgress(sliderProgress)
-                //slider(.ver).setProgressValue(sliderProgress)//temp fix
-            }
+            //also manipulates slider, but only on directTransmission, as mover calls setProgress from shallow in indirectTransmission
+            //setProgress(moverGroup!.result.y,.ver)//👈NEW, this migth need to be inSide scrollWheel call, as it needs to be shallow to reach inside setProgress in ElasticFastList.setProgress, but maybe not, To be continued
+            //let sliderProgress:CGFloat = ElasticUtils.progress(moverGroup!.result.y,contentSize[.ver],maskSize[.ver])
+            let sliderProgress:CGPoint = ElasticUtils.progress(moverGroup!.result,contentSize,maskSize)
+            //Swift.print("sliderProgress: " + "\(sliderProgress)")
+            (self as Slidable3).setProgress(sliderProgress)
+            //slider(.ver).setProgressValue(sliderProgress)//temp fix
+            
         }else if(event.phase == NSEventPhase.mayBegin || event.phase == NSEventPhase.began){
-            showSlider(.ver)
-            showSlider(.hor)
+            showSlider()
         }
     }
     func onInDirectScrollWheelChange(_ event: NSEvent) {}//override to cancel out the event
     func scrollWheelExitedAndIsStationary() {
         Swift.print("⚠️️⚠️️⚠️️ ElasticSlidableScrollable3.scrollWheelExitedAndIsStationary()")
-        hideSlider(.ver)
-        hideSlider(.hor)
+        hideSlider()
     }
 }
 
