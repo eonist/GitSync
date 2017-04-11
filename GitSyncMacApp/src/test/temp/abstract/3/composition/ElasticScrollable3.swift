efc7e8b175ced4314f633565d629eacaac13e640
Eonist <30n1st@gmail.com>
2017-04-11 15:01:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticScrollable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/composition/ElasticScrollable3.swift
@@ -13,7 +13,6 @@ extension ElasticScrollable3{
      */
     func onScrollWheelChange(_ event:NSEvent){/*Direct scroll*/
         //Swift.print("👻📜 (ElasticScrollable3).onScrollWheelChange : \(event.type)")
-        
         moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
         moverGroup!.updatePosition()/*the mover still governs the resulting value, in order to get the displacement friction working*/
         let p = moverGroup!.result
