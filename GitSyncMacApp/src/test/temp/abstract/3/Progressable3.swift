016719404f858b91975d0c9a8db9fdf2b8914251
Eonist <30n1st@gmail.com>
2017-04-12 18:17:13 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swiftGitSyncMacApp/src/test/temp/abstract/3/composition/ElasticScrollable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swift
@@ -6,7 +6,7 @@ protocol Progressable3:Containable3{
     func progress(_ dir:Dir) -> CGFloat/*0-1 atBegining <-> atEnd*/
     func interval(_ dir:Dir) -> CGFloat/*describes the speed when scrolling (distance per scroll tick)*/
     func setProgress(_ progress:CGFloat,_ dir:Dir)
-    //func setProgress(_ point:CGPoint)
+    func setProgress(_ point:CGPoint)
     var itemSize:CGSize {get}
 }
 extension Progressable3{
