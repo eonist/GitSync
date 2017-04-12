fc4f12a41b589897d4739cb5a3e7fd1ea077bc5c
Eonist <30n1st@gmail.com>
2017-04-12 16:34:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swift
@@ -1,5 +1,15 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
 class ElasticScrollFastList3:FastList3,ElasticScrollable3 {
     lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
+    var rbContainer:Container?/*needed for the overshot animation*/
+    override func resolveSkin() {
+        super.resolveSkin()
+        /*rbContainer*/
+        rbContainer = addSubView(Container(w,h,self,"rb"))
+        rbContainer!.addSubview(contentContainer!)//add lable Container inside rbContainer
+        contentContainer!.parent = rbContainer!
+    }
 }
