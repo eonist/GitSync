082e01c3d7b73aa27336cf2b660b1a4b6e57389a
Eonist <30n1st@gmail.com>
2017-04-12 18:47:58 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Progressable3.swiftGitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
@@ -9,10 +9,11 @@ extension ElasticScrollableFastListable3{
     func onScrollWheelChange(_ event:NSEvent){/*Direct scroll*/
         Swift.print("ElasticScrollableFastListable3.onScrollWheelChange : \(event.type)")
         moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
-         moverGroup!.updatePosition(true)/*the mover still governs the resulting value, in order to get the displacement friction working*/
-         let p:CGPoint = moverGroup!.result
-         (self as ElasticScrollableFastListable3).setProgress(p)
-        
+        moverGroup!.updatePosition(true)/*the mover still governs the resulting value, in order to get the displacement friction working*/
+        let p:CGPoint = moverGroup!.result
+        //(self as ElasticScrollableFastListable3).setProgress(p)
+        (self).setProgress(p.x,.hor)
+        (self).setProgress(p.y,.ver)
     }
     func setProgress(_ point:CGPoint) {
         Swift.print("ElasticScrollableFastListable3.setProgress(p)")
