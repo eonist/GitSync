8a87c0f83221ffa974687c3e6fdce68ae7dd72c4
Eonist <30n1st@gmail.com>
2017-04-12 23:44:23 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticSlidableScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticSlidableScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticSlidableScrollableFastListable3.swift
@@ -1,8 +1,10 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
-protocol ElasticSlidableScrollableFastListable3:Slidable3,ElasticScrollableFastListable3 {
-
-}
+protocol ElasticSlidableScrollableFastListable3:Slidable3,ElasticScrollableFastListable3 {}
 extension ElasticSlidableScrollableFastListable3{
-    
+    func scroll(_ event: NSEvent) {
+        (self as Scrollable3).scroll(event)//forward the event
+    }
 }
