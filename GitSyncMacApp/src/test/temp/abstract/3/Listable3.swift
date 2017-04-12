1ad9a29acb1e61064f10430ade7e76e579c76db3
Eonist <30n1st@gmail.com>
2017-04-12 10:57:42 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/Listable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Listable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Listable3.swift
@@ -3,6 +3,7 @@ import Foundation
 @testable import Element
 
 protocol Listable3 {
+    var dp:DataProvider {get}
     var dir:Dir {get}//the "primary" direction the list scroll in. the secondary direction is the opposite   
     var itemSize:CGSize{get}
 }
