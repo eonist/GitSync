2a7ec75ef361a56a71b5a7b0a232a676b93d85d7
Eonist <30n1st@gmail.com>
2017-04-12 12:10:07 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Containable3.swiftGitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/Containable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/Containable3.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 @testable import Element
 
-protocol Containable3{
+protocol Containable3:class{/*We extend class because it will use NSView anyway*/
     var maskSize:CGSize {get}
     var contentSize:CGSize {get}
     var contentContainer:Element? {get}
--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
@@ -78,9 +78,12 @@ extension FastList3{
      * This is called when a item in the lableContainer has send the ButtonEvent.upInside event
      */
     func onListItemUpInside(_ buttonEvent:ButtonEvent) {
-        let viewIndex:Int = lableContainer!.indexOf(buttonEvent.origin as! NSView)
-        ListModifier.selectAt(self,viewIndex)//unSelect all other visibleItems
-        pool.forEach{if($0.item === buttonEvent.origin){selectedIdx = $0.idx}}/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
-        super.onEvent(ListEvent(ListEvent.select,selectedIdx ?? -1,self))/*if selectedIdx is nil then use -1 in the event*///TODO: probably use FastListEvent here in the future
+        fatalError("not implemented yet")
+        /*
+         let viewIndex:Int = lableContainer!.indexOf(buttonEvent.origin as! NSView)
+         ListModifier.selectAt(self,viewIndex)//unSelect all other visibleItems
+         pool.forEach{if($0.item === buttonEvent.origin){selectedIdx = $0.idx}}/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
+         super.onEvent(ListEvent(ListEvent.select,selectedIdx ?? -1,self))/*if selectedIdx is nil then use -1 in the event*///TODO: probably use FastListEvent here in the future
+        */
     }
 }
