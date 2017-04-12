a008882596c49f94674585a11bcd87f27c7799b5
Eonist <30n1st@gmail.com>
2017-04-12 13:45:07 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ScrollFastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ScrollFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/ScrollFastList3.swift
@@ -1,6 +1,12 @@
 import Foundation
 
-class ScrollFastList3:FastList3,Scrollable3 {
+class ScrollFastList3:FastList3, ScrollableFastListable3{
     //continue here: make ScrollFastListable3, which overrides onChange. 
         //setProgress
+}
+protocol ScrollableFastListable3:FastListable3,Scrollable3{}
+extension ScrollableFastListable3{
+    func onScrollWheelChange(_ event: NSEvent) {
+        
+    }
 }
