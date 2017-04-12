aeda18fd37eb68a8ab7abf295337eb7499df4d60
Eonist <30n1st@gmail.com>
2017-04-12 16:45:59 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swiftGitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticScrollFastList3.swift
@@ -2,7 +2,7 @@ import Foundation
 @testable import Utils
 @testable import Element
 
-class ElasticScrollFastList3:FastList3,ElasticScrollable3 {
+class ElasticScrollFastList3:FastList3,ElasticScrollableFastListable3 {
     lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
     var rbContainer:Container?/*needed for the overshot animation*/
     override func resolveSkin() {
--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
@@ -1,5 +1,47 @@
-import Foundation
+import Cocoa
+@testable import Element
+@testable import Utils
 
-class ElasticScrollableFastListable3 {
-
+protocol ElasticScrollableFastListable3:FastListable3,ElasticScrollable3 {
+    var rbContainer:Container?{get set}
+}
+extension ElasticScrollableFastListable3{
+    /**
+     * PARAM value: is the final y value for the lableContainer
+     * ⚠️️ Do not use scalar value here (0-1) well you know...
+     */
+    func setProgress(_ value:CGFloat){
+        //Swift.print("👻🏂📜🐎 ElasticScrollableFast.setProgress(\(value))")
+        //Swift.print("value: " + "\(value)")
+        let contentSide:CGFloat = contentSize[dir]//TODO: Use a precalculated itemsHeight instead of recalculating it on every setProgress call, what if dp.count changes though?
+        if(contentSide < maskSize[dir]){//when there is few items in view, different overshoot rules apply, this should be written more elegant
+            progressValue = value / maskSize[dir]
+            //Swift.print("progressValue: " + "\(progressValue)")
+            let val = progressValue! * maskSize[dir]
+            //Swift.print("y: " + "\(y)")
+            rbContainer!.point[dir] = val
+        }else{
+            progressValue = value / -(contentSide - maskSize[dir])/*calc scalar from value, if itemsHeight is to small then use height instead*/
+            let progress = progressValue!.clip(0, 1)
+            
+            //⚠️️🔨the bellow needs refactoring
+            //(self as Scrollable).setProgress(progress)/*moves the lableContainer up and down*/
+            lableContainer!.point[dir] = value
+            (self as IFastList).setProgress(progress)
+            //
+            let sliderProgress = ElasticUtils.progress(value,contentSide,maskSize[dir])//doing some double calculations here
+            /*finds the values that is outside 0 and 1*/
+            if(sliderProgress < 0){//⚠️️ You could also just do if value is bellow 0 -> y = value, and if y  < itemsheight - height -> y = the sapce above itemsheight - leftover
+                let v1 = maskSize[dir] * -sliderProgress
+                rbContainer!.point[dir] = v1/*the half height is to limit the rubber effect, should probably be done else where*/
+            }else if(sliderProgress > 1){
+                let v2 = maskSize[dir] * -(sliderProgress-1)
+                rbContainer!.point[dir] = v2
+            }else{
+                rbContainer!.point[dir] = 0/*default position*/
+            }
+        }
+        //Swift.print("rbContainer!.point[dir]: " + "\(rbContainer!.point[dir])")
+        //Swift.print("contentContainer.point[dir]: " + "\(contentContainer!.point[dir])")
+    }
 }
