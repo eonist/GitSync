1b195e33057a2f5ea2cf31be796123f516ff4423
Eonist <30n1st@gmail.com>
2017-04-12 21:52:36 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift
@@ -1,5 +1,14 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
 class ElasticSlideScrollFastList3:SlideFastList3,ElasticSlidableScrollable3 {
-
+    lazy var moverGroup:MoverGroup? = {let group = MoverGroup(self.setProgress,self.maskSize,self.contentSize);group.event = self.onEvent;return group;}()/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+    override func onEvent(_ event:Event) {
+        if(event.type == AnimEvent.stopped){
+            let dir:Dir = event.origin === moverGroup!.yMover ? .ver : .hor
+            hideSlider(dir)/*hides the slider when bounce back anim stopps*/
+        }
+        super.onEvent(event)
+    }
 }
