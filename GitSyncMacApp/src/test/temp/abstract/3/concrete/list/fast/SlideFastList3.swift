19d79c8ce6e448026ecdafdd3c5202645003fba9
Eonist <30n1st@gmail.com>
2017-04-12 16:11:03 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/SlideFastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/SlideFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/SlideFastList3.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 @testable import Element
 
-class SlideFastList:FastList3,Slidable3 {
+class SlideFastList3:FastList3,Slidable3 {
     lazy var horSlider:Slider? = self.hSlider
     lazy var verSlider:Slider? = self.vSlider
 }
