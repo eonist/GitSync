d1692b5bdc0b6c1f10e3e948838121f18a6564bc
Eonist <30n1st@gmail.com>
2017-04-12 11:57:52 +0200

    Files modified: 1, Files renamed: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
@@ -20,12 +20,23 @@ class FastList3:ContainerView3,FastListable3{
     }
     override func resolveSkin() {
         super.resolveSkin()
+        let visibleRange:Range<Int> = visibleItemRange/*visible ItemRange Within View, calcs visibleItems based on lableContainer.y and height*/
+        let range:Range<Int> = visibleRange.start..<min(dp.count,visibleRange.end)/*clip the range*/
+        renderItems(range)
     }
     /**
      * Apply new data / align items
      * NOTE: override this to use custom ItemList items
      */
     func reUse(_ listItem:FastListItem){
+        let item:SelectTextButton = listItem.item as! SelectTextButton
+        let idx:Int = listItem.idx/*the index of the data in dataProvider*/
+        let dpItem = dp.item(idx)!
+        let title:String = dpItem["title"]!
+        let selected:Bool = idx == selectedIdx//dpItem["selected"]!.bool
+        if(item.selected != selected){ item.setSelected(selected)}//only set this if the selected state is different from the current selected state in the ISelectable
+        item.setTextValue(/*idx.string + " " + */title)
+        item.point[dir] = listItem.idx * itemSize[dir]/*position the item*/
     }
     /**
      * CreatesItem
@@ -38,10 +49,38 @@ class FastList3:ContainerView3,FastListable3{
         return item
     }
     override func onEvent(_ event:Event) {
+        if(event.type == ButtonEvent.upInside && event.origin.superview === lableContainer){onListItemUpInside(event as! ButtonEvent)}// :TODO: should listen for SelectEvent here
+        else if(event is DataProviderEvent){onDataProviderEvent(event as! DataProviderEvent)}
         super.onEvent(event)// we stop propegation by not forwarding events to super. The ListEvents go directly to super so they wont be stopped.
     }
     override func getClassType() -> String {
         return dir == .ver ? "\(List.self)" : "VList"
     }
     required init(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+extension FastList3{
+    /**
+     * DP has changed
+     * override this method if dp change can affect the super class
+     */
+    func onDataProviderEvent(_ event:DataProviderEvent){
+        Swift.print("FastList.onDataProviderEvent: " + "\(event)")
+        Swift.print("event.startIndex: " + "\(event.startIndex)")
+        alignLableContainer(event)
+        let range:Range<Int> = visibleItemRange.start..<Swift.min(visibleItemRange.end,dp.count)
+        if(currentVisibleItemRange != range){/*Optimization: only set if it's not the same as prev range*/
+            renderItems(range)/*the visible range has changed, render it*/
+        }else{
+            reUseFromIdx(event.startIndex)/*the visible range hasn't changed, but the data has changed, apply new data*/
+        }
+    }
+    /**
+     * This is called when a item in the lableContainer has send the ButtonEvent.upInside event
+     */
+    func onListItemUpInside(_ buttonEvent:ButtonEvent) {
+        let viewIndex:Int = lableContainer!.indexOf(buttonEvent.origin as! NSView)
+        ListModifier.selectAt(self,viewIndex)//unSelect all other visibleItems
+        pool.forEach{if($0.item === buttonEvent.origin){selectedIdx = $0.idx}}/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
+        super.onEvent(ListEvent(ListEvent.select,selectedIdx ?? -1,self))/*if selectedIdx is nil then use -1 in the event*///TODO: probably use FastListEvent here in the future
+    }
 }
