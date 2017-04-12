bc2ae5c7ef8b6d6e492f5f3ac1fe1a88c2caca4a
Eonist <30n1st@gmail.com>
2017-04-12 23:19:31 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/ElasticSlideScrollFastList3.swift
@@ -4,6 +4,7 @@ import Foundation
 
 class ElasticSlideScrollFastList3:SlideFastList3,ElasticSlidableScrollableFastListable3 {
     lazy var moverGroup:MoverGroup? = {let group = MoverGroup(self.setProgress,self.maskSize,self.contentSize);group.event = self.onEvent;return group;}()/*Add an eventHandler for the mover object, , this has no functionality in this class, but may have in classes that extends this class, like hide progress-indicator when all animation has stopped*/
+    lazy var rbContainer:Container? = self.rubberBandContainer/*needed for the overshot animation*/
     override func onEvent(_ event:Event) {
         if(event.type == AnimEvent.stopped){
             let dir:Dir = event.origin === moverGroup!.yMover ? .ver : .hor
