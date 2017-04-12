9b8e7c292fc0196919824a3b4893f2763bfabb6c
Eonist <30n1st@gmail.com>
2017-04-12 19:29:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ElasticScrollableFastListable3.swift
@@ -11,6 +11,10 @@ extension ElasticScrollableFastListable3{
         moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
         moverGroup!.updatePosition(true)/*the mover still governs the resulting value, in order to get the displacement friction working*/
         let p:CGPoint = moverGroup!.result
+        
+        //continue here:
+            //measure the p. Is does it have more strength in ElasticlScrollList?
+        
         //(self as ElasticScrollableFastListable3).setProgress(p)
         setProgressVal(p.x,.hor)
         setProgressVal(p.y,.ver)
