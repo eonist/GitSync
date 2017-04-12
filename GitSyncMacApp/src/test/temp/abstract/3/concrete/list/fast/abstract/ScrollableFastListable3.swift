5f855d14a3bbc7857b480319c751bfae4cb8db34
Eonist <30n1st@gmail.com>
2017-04-12 14:16:52 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift
@@ -2,13 +2,13 @@ import Cocoa
 @testable import Element
 @testable import Utils
 
-protocol ScrollableFastListable3:Scrollable3{}
+protocol ScrollableFastListable3:FastListable3,Scrollable3{}
 
-extension ScrollableFastListable3:FastListable3{
+extension ScrollableFastListable3{
     func onScrollWheelChange(_ event:NSEvent) {
         Swift.print("ScrollableFastListable3.onScrollWheelChange()")
         let progressVal:CGPoint = SliderListUtils.progress(event.delta, interval, progress)
         (self as! FastListable3).setProgress(progressVal)
-        (self as Scrollable3).setProgress(progressVal)
+        (self as! Scrollable3).setProgress(progressVal)
     }
 }
