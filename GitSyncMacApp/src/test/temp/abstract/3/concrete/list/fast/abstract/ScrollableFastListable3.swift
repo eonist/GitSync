d703eb3cd57584ad262d7dc224ae387d965c455d
Eonist <30n1st@gmail.com>
2017-04-12 14:44:45 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/FastListable3.swiftGitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/fast/abstract/ScrollableFastListable3.swift
@@ -6,10 +6,10 @@ protocol ScrollableFastListable3:FastListable3,Scrollable3{}
 
 extension ScrollableFastListable3{
     func onScrollWheelChange(_ event:NSEvent) {
-        Swift.print("ScrollableFastListable3.onScrollWheelChange()")
+        //Swift.print("ScrollableFastListable3.onScrollWheelChange()")
         let progressVal:CGPoint = SliderListUtils.progress(event.delta, interval, progress)
         let dir:Dir = self.dir == .ver ? .ver : .hor
-        let primaryProgress:CGFloat = SliderListUtils.progress(event.delta[dir], interval[dir], progress[dir])
+        let primaryProgress:CGFloat = SliderListUtils.progress(event.delta[dir], interval(dir), progress(dir))
         (self as FastListable3).setProgress(primaryProgress)
         (self as Scrollable3).setProgress(progressVal)
     }
