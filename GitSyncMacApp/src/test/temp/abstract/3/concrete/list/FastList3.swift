cfa37cfe5350ceb9564de26fa467818b9e276783
Eonist <30n1st@gmail.com>
2017-04-12 11:11:29 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
@@ -5,10 +5,10 @@ import Foundation
 class FastList3:ContainerView3{
     var selectedIdx:Int?/*This cooresponds to the "absolute" index in dp*/
     var dataProvider:DataProvider/*data storage*/
+    var itemSize:CGSize
     var pool:[FastListItem] = []/*Stores the FastListItems*/
     var inActive:[FastListItem] = []/*Stores pool item that are not in-use*/
-    var itemSize:CGSize
-    override var contentSize:CGSize { get{return dir == .hor ? CGSize(dp.count * itemSize.width ,height) : CGSize(width ,dp.count * itemSize.height) } set{_ = newValue}}
+    override var contentSize:CGSize { get{return dir == .hor ? CGSize(dp.count * itemSize.width ,height) : CGSize(width ,dp.count * itemSize.height) } set{_ = newValue;fatalError("not supported");}}
     init(_ width: CGFloat, _ height: CGFloat, _ itemSize:CGSize = CGSize(NaN,NaN), _ dataProvider:DataProvider? = nil, _ dir:Dir = .ver, _ parent: IElement? = nil, _ id: String? = "") {
         self.itemSize = itemSize
         self.dp = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
