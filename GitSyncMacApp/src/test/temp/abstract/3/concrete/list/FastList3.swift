d6b9bfe2cfaba5b6268c7d2e6452c8625e3a64f8
Eonist <30n1st@gmail.com>
2017-04-12 10:55:25 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
@@ -3,5 +3,8 @@ import Foundation
 @testable import Element
 
 class FastList3:ContainerView3{
-    
+    var selectedIdx:Int?/*This cooresponds to the "absolute" index in dp*/
+    var dataProvider:DataProvider/*data storage*/
+    var pool:[FastListItem] = []/*Stores the FastListItems*/
+    var inActive:[FastListItem] = []/*Stores pool item that are not in-use*/
 }
