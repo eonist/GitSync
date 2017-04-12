e560ee2bf0d2cb74d8c0d69d75b454e1ea2684a5
Eonist <30n1st@gmail.com>
2017-04-12 16:22:46 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/ElasticScrollView3.swift

--- GitSyncMacApp/src/test/temp/ElasticScrollView3.swift
+++ GitSyncMacApp/src/test/temp/ElasticScrollView3.swift
@@ -1,9 +1,7 @@
 import Cocoa
 @testable import Element
 @testable import Utils
-/**
- * Testing x & y elastic scrolling
- */
+
 class ElasticScrollView3:ContainerView3,ElasticScrollable3{
     lazy var moverGroup:MoverGroup? = MoverGroup(self.setProgress,self.maskSize,self.contentSize)
 }
