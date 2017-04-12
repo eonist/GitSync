701600c9231b32ee50ade85937da081643aa4d67
Eonist <30n1st@gmail.com>
2017-04-12 11:09:00 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/FastList3.swift
@@ -7,4 +7,14 @@ class FastList3:ContainerView3{
     var dataProvider:DataProvider/*data storage*/
     var pool:[FastListItem] = []/*Stores the FastListItems*/
     var inActive:[FastListItem] = []/*Stores pool item that are not in-use*/
+    var itemSize:CGSize
+    override var contentSize:CGSize { get{return dir == .hor ? CGSize(dp.count * itemSize.width ,height) : CGSize(width ,dp.count * itemSize.height) } set{_ = newValue}}
+    init(_ width: CGFloat, _ height: CGFloat, _ itemSize:CGSize = CGSize(NaN,NaN), _ dataProvider:DataProvider? = nil, _ dir:Dir = .ver, _ parent: IElement? = nil, _ id: String? = "") {
+        self.itemSize = itemSize
+        self.dp = dataProvider ?? DataProvider()/*<--if it's nil then a DB is created*/
+        self.dir = dir
+        super.init(width,height,parent,id)
+        self.dp.event = onEvent/*Add event handler for the dataProvider*/
+        layer!.masksToBounds = true/*masks the children to the frame, I don't think this works, seem to work now*/
+    }
 }
