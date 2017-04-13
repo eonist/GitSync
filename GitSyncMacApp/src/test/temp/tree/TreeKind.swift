fb2c34e08497c22d51f18156c1543058988a7c8c
Eonist <30n1st@gmail.com>
2017-04-14 00:37:12 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/tree/Tree.swiftGitSyncMacApp/src/test/temp/tree/TreeKind.swift

--- GitSyncMacApp/src/test/temp/tree/TreeKind.swift
+++ GitSyncMacApp/src/test/temp/tree/TreeKind.swift
@@ -1,5 +1,32 @@
 import Foundation
 
-class TreeKind {
-
+protocol TreeKind {
+    //associatedtype Element
+    var children:[TreeKind] {get}
+    var props:[String:String]? {get}
+    var name:String? {get}
+    var content:String? {get}//or use Any or T
+}
+extension TreeKind{
+    /**
+     * The num of items in the entire tree
+     * NOTE: This should be cached, only re-calc on alteration
+     * IMPORTANT: ⚠️️ This is a exhaustive and naive implementation
+     */
+    var count:Int{
+        return TreeUtils.recursiveFlattened(self).count
+    }
+}
+class TreeUtils{
+    static func recursiveFlattened(_ tree:TreeKind) -> [TreeKind] {
+        var results:[TreeKind] = []
+        for child in tree.children {
+            if(child.children.count > 0) {/*Array*/
+                results += TreeUtils.recursiveFlattened(child)
+            }else{/*Item*/
+                results.append(child)
+            }
+        }
+        return results
+    }
 }
