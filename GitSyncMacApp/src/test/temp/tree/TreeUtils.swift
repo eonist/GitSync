8b16488c2a5b0aff75b4db03967007a2a68cedd0
Eonist <30n1st@gmail.com>
2017-04-14 16:26:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/TreeUtils.swift

--- GitSyncMacApp/src/test/temp/tree/TreeUtils.swift
+++ GitSyncMacApp/src/test/temp/tree/TreeUtils.swift
@@ -45,7 +45,7 @@ class TreeUtils{
         //Swift.print("handleArray: " + "name \(name)" + " $0.value: \(value)" )
         let xml = XML()
         xml.name = tree.name
-        if(tree.content){xml.value = }
+        if(tree.content != nil){xml.value = tree.content}
         //xml["type"] = "Array"
         //Swift.print("handleArray.properties.count: " + "\(properties.count)")
         tree.children.forEach{ child in
