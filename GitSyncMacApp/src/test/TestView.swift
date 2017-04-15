e9e8634daa313df7f541eb3b7c3e6141202c29e6
Eonist <30n1st@gmail.com>
2017-04-15 21:09:54 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/TestView.swift

--- GitSyncMacApp/src/test/TestView.swift
+++ GitSyncMacApp/src/test/TestView.swift
@@ -85,27 +85,7 @@ class TestView:TitleView{
          let intervalB = SliderParser.interval(200, 100, 20)
          Swift.print("intervalB: " + "\(intervalB)")*/
     }
-    /**
-     * https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
-     */
-    class HashArray{
-        var dict:[String:Int] = [:]
-        var arr:[Any] = []
-        init(){}
-        func add(_ key:String, _ content:Any){
-            if(!dict.hasKey(key)){//make sure key doesn't exist
-                let idx:Int = arr.endIndex//idx after last, can also be 0 /*arr.isEmpty ? 0 : arr.count - 1*/
-                arr[idx] = content//store content in arr
-                dict[key] = idx//store idx in key
-            }else{fatalError("key already exist")}
-        }
-        func remove(_ key:String){
-            if let idx:Int = dict[key]{//make sure the key exists
-                dict.removeValue(forKey: key)//remove key and val from dict
-                _ = arr.removeAt(idx)//remove item from arr
-            }else{fatalError("key does not exist")}
-        }
-    }
+    
     /**
      *
      */
