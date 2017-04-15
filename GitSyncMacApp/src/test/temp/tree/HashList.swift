63e095ae0e66ef70cd025488d8155d50aebbe413
Eonist <30n1st@gmail.com>
2017-04-15 23:27:08 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/HashList.swift

--- GitSyncMacApp/src/test/temp/tree/HashList.swift
+++ GitSyncMacApp/src/test/temp/tree/HashList.swift
@@ -5,7 +5,7 @@ import Foundation
  * https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
  * NOTE: we could use T:Hashable, but no need atm
  */
-struct HashList {
+class HashList {
     var dict:[String:Int] 
     var arr:[String]
     init(_ dict:[String:Int] = [:],_ arr:[String] = []){
@@ -14,25 +14,25 @@ struct HashList {
     }
 }
 extension HashList{
-    subscript(key:String) -> Any? {
+    subscript(key:String) -> String? {
         get {
             return get(key)
         }
     }
-    fileprivate mutating func add(_ key:String){
-        if(!dict.hasKey(key)){
+    func add(_ key:String){
+        if(!self.dict.hasKey(key)){
             arr.append(key)//store content in arr
             let idx:Int = arr.count-1
             dict[key] = idx//store idx in key
         }else{fatalError("key already exist")}
     }
-    mutating func remove(_ key:String){
+    func remove(_ key:String){
         if let idx:Int = dict[key]{//make sure the key exists
             dict.removeValue(forKey: key)//remove key and val from dict
-            _ = arr.removeAt(idx)//remove item from arr
+            _ = self.arr.removeAt(idx)//remove item from arr
         }else{fatalError("key does not exist")}
     }
-    fileprivate func get(_ key:String)->Any?{
+    func get(_ key:String)->String?{
         if let idx:Int = dict[key]{
             Swift.print("idx: " + "\(idx)")
             return arr[idx]
