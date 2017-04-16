f73bedd295e17046e8240fd3f02f021ffa619b8e
Eonist <30n1st@gmail.com>
2017-04-16 14:34:18 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/TestView.swift

--- GitSyncMacApp/src/test/TestView.swift
+++ GitSyncMacApp/src/test/TestView.swift
@@ -122,8 +122,8 @@ class TestView:TitleView{
         //Continue here:
             //Tree needs subscript for [int] ✅
             //convert string to int array "001" -> [0,0,1]  ✅
-            //test getting content for 2d-idx in hashList
-            //then setup fastlist test with tree data
+            //test getting content for 2d-idx in hashList ✅
+            //then setup fastlist test with tree data 👈
             //then add tree.addAt([idx]) for when you open a tree item etc, and removeAt(),removeAll(at)
         
         
