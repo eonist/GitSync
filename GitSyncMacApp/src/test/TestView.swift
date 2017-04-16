5187152d27def0764dbf7bfc69c7dd96c0df8b4c
Eonist <30n1st@gmail.com>
2017-04-16 20:04:43 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/TestView.swift

--- GitSyncMacApp/src/test/TestView.swift
+++ GitSyncMacApp/src/test/TestView.swift
@@ -45,7 +45,7 @@ import Cocoa
     //then add tree.addAt([idx]) for when you open a tree item etc, and removeAt(),removeAll(at)
         //you also need a filter method when 3d->2d , only render items within open = true
             //click arrow to open -> tree[idx].setProps["isOpen"] = true, insert trees from self.idx w/ filter open
-            //click arrow to close -> tree[idx].setProps["isOpen"] = false, remove
+            //click arrow to close -> tree[idx].setProps["isOpen"] = false, remove every item after curIdx, that has curIdx, then stop if idx is not curIdx
         //possibly update DataProvider to use DataProvidable?
 
 
