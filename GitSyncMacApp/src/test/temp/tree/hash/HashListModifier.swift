0ed8e0d10c882a7c531bde12085072e69db03731
Eonist <30n1st@gmail.com>
2017-04-17 17:11:05 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
@@ -4,8 +4,8 @@ class HashListModifier {
     /**
      *
      */
-    func removeDescendants(_ list:HashList,_ at:Int){
-        var i:Int = at
+    func removeDescendants(_ list:HashList,_ idx2D:Int,idx3d:){
+        var i:Int = idx2D
         var isDescendant:Bool = true
         while(i<list.arr.count && isDescendant){
             print(i)
