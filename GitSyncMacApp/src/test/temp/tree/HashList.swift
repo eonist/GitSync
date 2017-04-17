105f414daeefac1701e287a41b6c2f1245df6d97
Eonist <30n1st@gmail.com>
2017-04-17 20:17:57 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/HashList.swift

--- GitSyncMacApp/src/test/temp/tree/HashList.swift
+++ GitSyncMacApp/src/test/temp/tree/HashList.swift
@@ -2,6 +2,7 @@ import Foundation
 @testable import Utils
 /**
  * HashList: "data structure" that maps 2d <-> 3d array in a 2-way relationship
+ * NOTE: So the basic concept is to flatten a 3d array into a 2d array. But avoid doing it each time something changes in either 2d array or 3d array. Basically if something happens in either its a mirrored change. Now my current solution is a third DataStructure I call HashList. Which holds the index of both 2d and 3d array. And facilitates their index translation. Why? Because I need to iterate over a 2d array for speed. But I need to be able to edit the 3d structure as thats where the data is stored. My on going blog post about this: 
  * NOTE: key gives you idx in array
  * NOTE: idx in array gives you key in dict
  * NOTE: https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
