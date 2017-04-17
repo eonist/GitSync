f841ec4bf7d40c53f265ea112248030b1fad6393
Eonist <30n1st@gmail.com>
2017-04-17 18:37:17 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
@@ -3,6 +3,7 @@ import Foundation
 
 //Continue here: 
     //You can't remove while you walk forward. maybe, make a descendantCount for tree instead would be easier
+    //or you could try to find idx after idx || end, and then use the idx to calc how many items needs to be deleted
 
 class HashListModifier {
     /**
