15c05a3508d9f0708d990c5ced2d7b22b20a3c9c
Eonist <30n1st@gmail.com>
2017-04-17 11:22:01 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashListExtensions.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashListExtensions.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashListExtensions.swift
@@ -1,5 +1,24 @@
 import Foundation
+@testable import Utils
 
-class HashListExtensions {
-
+extension HashList{
+    subscript(at:Int) -> String? {//returns key for array idx
+        get {return arr[at]}
+    }
+    subscript(key:String) -> Int? {//returns the internal array idx for key
+        get {return dict[key]}
+    }
+    mutating func add(_ key:String){
+        if(!dict.hasKey(key)){
+            arr.append(key)//store content in arr
+            let idx:Int = arr.count-1
+            dict[key] = idx//store idx in key
+        }else{fatalError("key already exist")}
+    }
+    mutating func remove(_ key:String){
+        if let idx:Int = dict[key]{//make sure the key exists
+            dict.removeValue(forKey: key)//remove key and val from dict
+            _ = arr.removeAt(idx)//remove item from arr
+        }else{fatalError("key does not exist")}
+    }
 }
