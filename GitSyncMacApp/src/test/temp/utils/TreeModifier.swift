d5995563ecbade5a5868af6fede54117552110e5
Eonist <30n1st@gmail.com>
2017-04-17 15:14:48 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/tree/TreeExtensions.swiftGitSyncMacApp/src/test/temp/utils/TreeModifier.swift

--- GitSyncMacApp/src/test/temp/utils/TreeModifier.swift
+++ GitSyncMacApp/src/test/temp/utils/TreeModifier.swift
@@ -1,5 +1,13 @@
 import Foundation
 
 class TreeModifier {
-
+    /**
+     * EXAMPLE XMLModifier.setAttributeAt(xml, [0,1], "title", "someTitle")
+     * NOTE: I think this method works with depth indecies
+     */
+    static func setAttributeAt(_ tree:Tree,_ at:[Int], _ key:String,_ value:String)  {
+        if let child = TreeParser.child(tree, at){
+            child[key] = value
+        }
+    }
 }
