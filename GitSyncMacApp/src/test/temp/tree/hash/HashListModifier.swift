cdbf06709e115b201ba660ba11e97017a35398fa
Eonist <30n1st@gmail.com>
2017-04-17 16:52:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashListModifier.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class HashListModifier {
-
+    /**
+     *
+     */
+    func removeChildren(_ tree:HashList,_ at:Int){
+        
+    }
+    /**
+     *
+     */
+    func addChildren(_ tree:HashList,_ at:Int){
+        
+    }
 }
