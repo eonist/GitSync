bdec6cc2b28af26c4e561fb12bbfd5c3deb0877b
Eonist <30n1st@gmail.com>
2017-04-17 22:20:06 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -24,13 +24,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
          win = MainWin(MainView.w,MainView.h)
          //win = ConflictDialogWin(380,400)
          //win = CommitDialogWin(400,356)
-         fileWatcher = StyleWatcher.watch("~/Desktop/ElCapitan/","~/Desktop/ElCapitan/gitsync.css", self.win!.contentView!)
+         /*fileWatcher = */StyleWatcher.watch("~/Desktop/ElCapitan/","~/Desktop/ElCapitan/gitsync.css", self.win!.contentView!)
     }
     func initTestWin(){
         //StyleManager.addStylesByURL("~/Desktop/ElCapitan/explorer.css",false)
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/gitsync.css",true)
         win = TestWin(500,400)/*Debugging Different List components*/
-        fileWatcher = StyleWatcher.watch("~/Desktop/ElCapitan/","~/Desktop/ElCapitan/gitsync.css", self.win!.contentView!)
+        /*fileWatcher = */StyleWatcher.watch("~/Desktop/ElCapitan/","~/Desktop/ElCapitan/gitsync.css", self.win!.contentView!)
     }
     func applicationWillTerminate(_ aNotification:Notification) {
         //store the app prefs
