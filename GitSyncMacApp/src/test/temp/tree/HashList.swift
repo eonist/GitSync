654f5375f754ad5e1944d2beb1667230bfa1d7b8
Eonist <30n1st@gmail.com>
2017-04-17 16:49:46 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/HashList.swift

--- GitSyncMacApp/src/test/temp/tree/HashList.swift
+++ GitSyncMacApp/src/test/temp/tree/HashList.swift
@@ -6,7 +6,7 @@ import Foundation
  * NOTE: idx in array gives you key in dict
  * NOTE: https://github.com/raywenderlich/swift-algorithm-club/tree/master/Hash%20Table
  * NOTE: we could use T:Hashable, but no need atm
- * PARAM: arr: stores "3d-idx" [0,00,01,1,2]
+ * PARAM: arr: stores "3d-idx" ["0","00","01","1","2"]
  * PARAM: dict: stores "2d-idx" ["0":0,"00":1,"01":2,"1":3,"2":4]
  * NOTE: "Key-Collisions" can't happen, since 3d-idencies are always unique
  */
