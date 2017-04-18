df9938c8871c976d7e55b003352233c5a2f18a8e
Eonist <30n1st@gmail.com>
2017-04-18 15:28:23 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -1,5 +1,9 @@
 import Foundation
+@testable import Element
+@testable import Utils
 
 class TreeList3:FastList3{
-
+    func onListItemUpInside(_ buttonEvent:ButtonEvent) {
+        Swift.print("works")
+    }
 }
