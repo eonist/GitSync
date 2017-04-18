6727129eba1aa896ef0d6111fd42a44843a384e0
Eonist <30n1st@gmail.com>
2017-04-18 15:45:18 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -5,6 +5,8 @@ import Foundation
 class TreeList3:FastList3{
     override func onListItemUpInside(_ buttonEvent:ButtonEvent) {
         super.onListItemUpInside(buttonEvent)
-        
+        if let selectable:ISelectable = buttonEvent.origin as? ISelectable{
+            selectable.getSelected()
+        }
     }
 }
