1e0d9e024463c549e327c0107b550fb3d03e28ef
Eonist <30n1st@gmail.com>
2017-04-18 13:26:21 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift

--- GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift
+++ GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift
@@ -4,15 +4,17 @@ class TreeDPModifier {
     /**
      * NOTE: after this method is called, send event to FastList UI Component, the times were added
      */
-    static func open(_ dp:TreeDP, _ at:[Int]){
-        dp.tree.setProp(at,("isOpen","true"))//updates tree
+    static func open(_ dp:TreeDP, _ at:Int){
+        let idx3d:[Int] = dp.hashList[at]!
+        dp.tree.setProp(idx3d,("isOpen","true"))//updates tree
         HashListModifier.addDescendants(&dp.hashList, 2, dp.tree)//adds items to HashList (via HashListModifier.addDescendants)
     }
     /**
      * NOTE: after this method is called, send event to FastList UI component, that items were removed
      */
-    static func close(_ dp:TreeDP, _ at:[Int]){
-        dp.tree.setProp(at,("isOpen","false"))//update tree
+    static func close(_ dp:TreeDP, _ at:Int){
+        let idx3d:[Int] = dp.hashList[at]!
+        dp.tree.setProp(idx3d,("isOpen","false"))//update tree
         HashListModifier.removeDescendants(&dp.hashList, 2, dp.tree)//remove items from HashList (via HashListModifier.removeDescendants)
     }
 }
