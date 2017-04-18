f68778d2c606d4a44ec302152547bdb2ad77ddb6
Eonist <30n1st@gmail.com>
2017-04-18 13:10:52 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift

--- GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift
+++ GitSyncMacApp/src/test/temp/tree/TreeDPModifier.swift
@@ -1,5 +1,20 @@
 import Foundation
 
-class TreeDPUtils {
-
+class TreeDPModifier {
+    /**
+     *
+     */
+    static func open(_ dp:TreeDP, _ at:Int){
+        dp.tree.setProp([2],("isOpen","true"))//updates tree
+        HashListModifier.addDescendants(&dp.hashList, 2, dp.tree)//adds items to HashList (via HashListModifier.addDescendants)
+        //send event to FastList UI Component, the times were added
+    }
+    /**
+     *
+     */
+    static func close(_ dp:TreeDP, _ at:Int){
+        dp.tree.setProp([2],("isOpen","false"))//update tree
+        HashListModifier.removeDescendants(&dp.hashList, 2, dp.tree)//remove items from HashList (via HashListModifier.removeDescendants)
+        //send event to FastList UI component, that items were removed
+    }
 }
