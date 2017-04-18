21957a6e71c02664f262b657f3cfebf3075c716b
Eonist <30n1st@gmail.com>
2017-04-18 15:50:41 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -5,7 +5,7 @@ import Foundation
 class TreeList3:FastList3{
     override func onListItemUpInside(_ buttonEvent:ButtonEvent) {
         super.onListItemUpInside(buttonEvent)
-        if let selectable:ISelectable = buttonEvent.origin as? ISelectable, selectable.selected{
+        if let selectable:ISelectable = buttonEvent.origin as! ISelectable, selectable.selected{
             Swift.print("already selected 🚫")
         }else{/*not selected*/
             Swift.print("selected 🎉")
