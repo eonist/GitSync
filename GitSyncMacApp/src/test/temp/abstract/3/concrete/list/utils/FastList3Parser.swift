c3297eab4ce7ab0e811d2267479d15ed1571785a
Eonist <30n1st@gmail.com>
2017-04-18 16:45:12 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class FastList3Parser {
-
+    /**
+     *
+     */
+    func selected(_ fastList:FastListable3){
+        return pool.forEach{if($0.item === buttonEvent.origin){selectedIdx = $0.idx}}/*We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies*/
+    }
 }
