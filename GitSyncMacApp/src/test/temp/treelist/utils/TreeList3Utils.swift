10fd8c140d3c1cf4b4d9e928dd6a3c8e64eb0403
Eonist <30n1st@gmail.com>
2017-04-18 13:08:35 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift
@@ -1,5 +1,16 @@
 import Foundation
 
 class TreeList3Utils {
-
+    /**
+     *
+     */
+    static func open(_ treeList:TreeList3, _ at:[Int]){
+        
+    }
+    /**
+     *
+     */
+    static func close(_ treeList:TreeList3, _ at:[Int]){
+        
+    }
 }
