f8806ac2340922d662ded7e78efd41316f7b535f
Eonist <30n1st@gmail.com>
2017-04-18 16:19:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift
@@ -1,5 +1,10 @@
 import Foundation
 
 class TreeDPParser {
-
+    func getProp(_ treeDP:TreeDP,_ at:Int, _ key:String)-> String{
+        
+    }
+    func getProps(_ treeDP:TreeDP,_ at:Int, _ key:String)->[String:String]{
+        
+    }
 }
