2240b0cab25fff874d0333970f3f96c5fe3c2554
Eonist <30n1st@gmail.com>
2017-04-18 16:34:33 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/TreeDPParser.swift
@@ -2,9 +2,12 @@ import Foundation
 
 class TreeDPParser {
     static func getProp(_ dp:TreeDP,_ at:Int, _ key:String)-> String?{
-        return getProps(dp, at)
+        if let props = getProps(dp, at){
+            return props[key]
+        }
+        return nil
     }
-    static func getProps(_ dp:TreeDP,_ at:Int, _ key:String)->[String:String]?{
+    static func getProps(_ dp:TreeDP,_ at:Int)->[String:String]?{
         if let idx:[Int] = dp.hashList[at]{
             return dp.tree.getProps(idx)
         }
