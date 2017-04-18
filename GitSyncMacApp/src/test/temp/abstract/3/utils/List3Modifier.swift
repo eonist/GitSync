3e55335c31e2fd7670818a225e857a0a3d3269dc
Eonist <30n1st@gmail.com>
2017-04-18 15:20:16 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/Listable3.swiftGitSyncMacApp/src/test/temp/abstract/3/utils/List3Modifier.swift

--- GitSyncMacApp/src/test/temp/abstract/3/utils/List3Modifier.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/utils/List3Modifier.swift
@@ -1,12 +1,13 @@
 import Foundation
 @testable import Utils
+@testable import Element
 
 
 class List3Modifier {
     /**
      * Selects the first item that has PARAM title as its title
      */
-    static func select(_ list: Listable3, _ title:String) {
+    static func select(_ list:Listable3, _ title:String) {
         let index:Int = list.dp.getItemIndex(list.dp.getItem(title)!)
         selectAt(list,index)
     }
@@ -14,8 +15,8 @@ class List3Modifier {
      * Selects an item in the itemContainer
      */
     static func selectAt(_ list:Listable3, _ index:Int) {
-        let selectable:ISelectable = list.lableContainer!.subviews[index] as! ISelectable
+        let selectable:ISelectable = list.contentContainer!.subviews[index] as! ISelectable
         if(!selectable.getSelected()) {selectable.setSelected(true)}
-        SelectModifier.unSelectAllExcept(selectable, list.lableContainer!)
+        SelectModifier.unSelectAllExcept(selectable, list.contentContainer!)
     }
 }
