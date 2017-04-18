27ff9819aa58a6bc4278c9478822ce1bea6f674d
Eonist <30n1st@gmail.com>
2017-04-18 19:19:41 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -2,18 +2,17 @@ import Cocoa
 @testable import Element
 @testable import Utils
 
-class TreeList3:FastList3{
+class TreeList3:ScrollFastList3{
     var treeDP:TreeDP {return dp as! TreeDP}
     override func onListItemUpInside(_ buttonEvent:ButtonEvent) {
         super.onListItemUpInside(buttonEvent)
         if let idx:Int = selectedIdx{
             let isOpen:Bool = TreeDPParser.getProp(treeDP, idx, "isOpen") == "true"
-            Swift.print("isOpen: " + "\(isOpen)")
             if(isOpen){
-                Swift.print("🚫")
+                Swift.print("close 🚫")
                 TreeDPModifier.close(treeDP, idx)
             }else{
-                Swift.print("✅")
+                Swift.print("open ✅")
                 TreeDPModifier.open(treeDP, idx)
             }
         }
