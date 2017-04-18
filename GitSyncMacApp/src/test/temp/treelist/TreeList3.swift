5fb22a6d8c05e85b9f1f500e35d0b9c3b5635fc9
Eonist <30n1st@gmail.com>
2017-04-18 17:39:52 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swiftGitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -6,9 +6,11 @@ class TreeList3:FastList3{
     var treeDP:TreeDP {return dp as! TreeDP}
     override func onListItemUpInside(_ buttonEvent:ButtonEvent) {
         super.onListItemUpInside(buttonEvent)
-        let viewIndex:Int = contentContainer!.indexOf(buttonEvent.origin as! NSView)
-        //selectedIdx = FastList3Parser.selected(self, buttonEvent.origin as! NSView) ?? selectedIdx
-        let isOpen:Bool = TreeDPParser.getProp(treeDP, viewIndex, "isOpen") == "true"
-        Swift.print("isOpen: " + "\(isOpen)")
+        if let idx:Int = selectedIdx{
+            let isOpen:Bool = TreeDPParser.getProp(treeDP, selectedIdx, "isOpen") == "true"
+            Swift.print("isOpen: " + "\(isOpen)")
+        }
+        
+        
     }
 }
