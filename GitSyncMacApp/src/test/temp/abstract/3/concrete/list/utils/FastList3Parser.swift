c757337d03100813c2182f74ebcf532f0054d51a
Eonist <30n1st@gmail.com>
2017-04-18 16:59:26 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swiftGitSyncMacApp/src/test/temp/tree/HashList.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
@@ -5,6 +5,7 @@ class FastList3Parser {
      * We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies
      */
     static func selected(_ fastList:FastListable3, _ item:NSView) -> Int?{
+        //use filter on the bellow
         for obj in fastList.pool{
             if(obj.item === item){
                 return obj.idx
--- GitSyncMacApp/src/test/temp/tree/HashList.swift
+++ GitSyncMacApp/src/test/temp/tree/HashList.swift
@@ -1,7 +1,8 @@
 import Foundation
 @testable import Utils
 /**
- * HashList: "data structure" that maps 2d <-> 3d array in a 2-way relationship
+ * HashList: "data structure" that maps 2d <-> 3d array in a 2-way relationship 
+ * NOTE: "The translator" between 3d and 2d data structures
  * NOTE: So the basic concept is to flatten a 3d array into a 2d array. But avoid doing it each time something changes in either 2d array or 3d array. Basically if something happens in either its a mirrored change. Now my current solution is a third DataStructure I call HashList. Which holds the index of both 2d and 3d array. And facilitates their index translation. Why? Because I need to iterate over a 2d array for speed. But I need to be able to edit the 3d structure as thats where the data is stored. My on going blog post about this: 
  * NOTE: key gives you idx in array
  * NOTE: idx in array gives you key in dict
