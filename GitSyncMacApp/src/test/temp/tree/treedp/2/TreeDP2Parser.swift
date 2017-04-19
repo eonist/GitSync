7d38876e788a34e18e3334228405665c2466be70
Eonist <30n1st@gmail.com>
2017-04-19 15:48:35 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Modifier.swiftGitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift
@@ -20,7 +20,7 @@ class TreeDP2Parser {
         return nil
     }
     static func values(_ dp:TreeDP2,_ idx:[Int], _ key:String)->[String]{
-        let pathIndecies:[[Int]] = TreeUtils.pathIndecies(dp.tree,[],TreeUtils.isOpen)/*flattens 3d to 2d*/
+        let pathIndecies:[[Int]] = TreeUtils.pathIndecies(dp.tree,idx,TreeUtils.isOpen)/*flattens 3d to 2d*/
         var values:[String] = []
         pathIndecies.forEach{
             let treeIdx:[Int] = $0
