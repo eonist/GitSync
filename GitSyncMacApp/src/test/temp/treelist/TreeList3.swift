ae0185abcf120f43ec22ca78682f3da6904cfa34
Eonist <30n1st@gmail.com>
2017-04-20 18:13:06 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -26,8 +26,9 @@ class TreeList3:ScrollFastList3{
         let idx2d:Int = listItem.idx
         let idx3d:[Int] = treeDP.hashList[idx2d]
         listItem.item.id = idx3d.count.string
-        listItem.item.skin!.setStyle(listItem.item.skin!.style!)
-        //ElementModifier.refreshStyle(listItem.item)
+        //listItem.item.skin!.setStyle(listItem.item.skin!.style!)
+        listItem.item.state = SkinStates.none
+        ElementModifier.refreshStyle(listItem.item)
         super.reUse(listItem)
     }
 }
