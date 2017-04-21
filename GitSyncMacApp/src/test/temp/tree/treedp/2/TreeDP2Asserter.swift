25367aa8fbcc5c5ae6f2a84efe13d3f522c3a63b
Eonist <30n1st@gmail.com>
2017-04-21 12:49:11 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Asserter.swiftGitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Asserter.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Asserter.swift
@@ -4,7 +4,12 @@ class TreeDP2Asserter {
     /**
      *
      */
-    func hasChildren(_ idx2d:Int){
-        
+    static func hasChildren(_ treeDP:TreeDP2,_ idx2d:Int) -> Bool{
+        if let idx3d:[Int] = treeDP[idx2d]{
+            return hasChildren
+        }
+    }
+    static func hasChildren(_ treeDP:TreeDP2, _ idx3d:[Int]) -> Bool{
+        return false
     }
 }
