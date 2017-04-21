3b0380ab3eedbc8a51779113a3c341afdd6b4e27
Eonist <30n1st@gmail.com>
2017-04-21 15:29:34 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift
@@ -2,8 +2,9 @@ import Foundation
 @testable import Utils
 @testable import Element
 
-class TreeList3Item:SelectCheckBoxButton/*,ITreeListItem*/ {
+class TreeList3Item:SelectCheckBoxButton,LableKind/*,ITreeListItem*/ {
     override func getClassType() -> String {
         return "\(TreeListItem.self)"
     }
+    
 }
