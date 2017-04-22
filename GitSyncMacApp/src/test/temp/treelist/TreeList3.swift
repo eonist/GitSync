fed2eed1e94e68cce8ea0ee20a47cc1660297141
Eonist <30n1st@gmail.com>
2017-04-22 16:56:34 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -39,6 +39,7 @@ extension TreeList3{
         Swift.print("onItemCheck")
         Swift.print("event.origin: " + "\(event.origin)")
         let idx2d:Int = contentContainer!.indexOf((event.origin as! NSView).superview!)
+        let idx2d:Int = FastList3Parser.idx(self, buttonEvent.origin as! NSView)
         let isOpen:Bool = TreeDP2Parser.getProp(treeDP, idx2d, "isOpen") == "true"
         if(isOpen){
             Swift.print("close 🚫 idx2d: \(idx2d)")
