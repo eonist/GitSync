6ab94a3fea9de70220a288438136698444e3dadb
Eonist <30n1st@gmail.com>
2017-04-22 15:34:33 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3Item.swift
@@ -6,8 +6,8 @@ class TreeList3Item:SelectCheckBoxButton/*,ITreeListItem*/ {
     override func getClassType() -> String {
         return "\(TreeList3Item.self)"
     }
-    override func mouseOver(_ event:MouseEvent) {
-        super.mouseOver(event)
-        Swift.print("TreeList3Item.getSkinState(): " + "\(getSkinState())")
-    }
+    /*override func mouseOver(_ event:MouseEvent) {
+     super.mouseOver(event)
+     Swift.print("TreeList3Item.getSkinState(): " + "\(getSkinState())")
+     }*/
 }
