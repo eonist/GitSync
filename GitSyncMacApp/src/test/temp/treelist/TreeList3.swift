f30a414f072bb3890529f407cb7e225e150f405a
Eonist <30n1st@gmail.com>
2017-04-22 19:59:15 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -20,7 +20,7 @@ class TreeList3:ElasticScrollFastList3{//ElasticSlideScrollFastList3
         //build the style
         //change the style
         //set the style
-        let style = StyleResolver.style(element!)
+        //let style = StyleResolver.style(element!)
         //a cheaper way would be to grab the style from
         
         if let checkable = listItem.item as? ICheckable, let isOpenStr = TreeDP2Parser.getProp(treeDP, idx3d, "isOpen"){
