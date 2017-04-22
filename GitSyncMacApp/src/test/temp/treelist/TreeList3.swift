012e9decb19b29a3c7143e1713bd8fbaa8e4fae3
Eonist <30n1st@gmail.com>
2017-04-22 16:40:25 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -16,7 +16,7 @@ class TreeList3:ScrollFastList3{
         //Does item have children
         let hasChildren:Bool = TreeDP2Asserter.hasChildren(treeDP, idx3d)
     
-        disableAnim{(listItem.item as! TreeList3Item).checkBox!.isHidden = !hasChildren}
+        //disableAnim{(listItem.item as! TreeList3Item).checkBox!.isHidden = !hasChildren}
 
         super.reUse(listItem)/*sets text and position and select state*/
     }
