de32bc8e7548324413db8bf4c943a6e2d5da9efc
Eonist <30n1st@gmail.com>
2017-04-22 15:47:18 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -15,9 +15,8 @@ class TreeList3:ScrollFastList3{
         }
         //Does item have children
         let hasChildren:Bool = TreeDP2Asserter.hasChildren(treeDP, idx3d)
-        if(hasChildren){
-            
-        }
+        ElementModifier.hide(listItem.item, hasChildren)
+        
         super.reUse(listItem)/*sets text and position and select state*/
     }
     override func createItem(_ index:Int) -> Element {
