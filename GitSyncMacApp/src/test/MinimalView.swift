a1cd39efc770512592a32f6792645763478315a4
Eonist <30n1st@gmail.com>
2017-04-26 20:41:24 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/MinimalView.swift

--- GitSyncMacApp/src/test/MinimalView.swift
+++ GitSyncMacApp/src/test/MinimalView.swift
@@ -1,5 +1,22 @@
 import Foundation
+@testable import Element
+@testable import Utils
 
-class MinimalView {
-
+class MinimalView:WindowView{
+    override func resolveSkin(){
+        let css:String = "Window{fill-alpha:1;fill:white;}"//corner-radius:4px;
+        StyleManager.addStyle(css)
+        super.resolveSkin()
+        rotationUITest()
+    }
+    func rotationUITest(){
+        let css = "Button#test{fill:blue;}"
+        StyleManager.addStyle(css)
+        let btn = addSubView(Button(20,20,self,"test"))
+        _ = btn
+    }
+    func treeList(){
+        let dp:TreeDP2 = TreeDP2("~/Desktop/assets/xml/treelist.xml".tildePath)
+        _ = self.addSubView(TreeList3(140, 145, CGSize(24,24), dp, self))
+    }
 }
