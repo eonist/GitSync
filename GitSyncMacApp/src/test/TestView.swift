ec6298e305ebaa65682f8e900acafd12f0cd88a7
Eonist <30n1st@gmail.com>
2017-04-26 13:41:20 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/TestView.swift

--- GitSyncMacApp/src/test/TestView.swift
+++ GitSyncMacApp/src/test/TestView.swift
@@ -64,9 +64,17 @@ class TestView:TitleView{
     //Continue here: 🏀
         //you want to add transform css syntax
             //do research into css rotation
+            //transform: rotate(20deg);
+    
+            //
         //if Asset URL hasn't changed between state change, then don't reload asset.
             //
     
+    /*
+ 
+ 
+ 
+    */
     func treeDPUITest(){
         
         //let xml:XML = FileParser.xml()
