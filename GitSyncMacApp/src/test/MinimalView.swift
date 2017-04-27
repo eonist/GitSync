b3963a4095e5dfef63b678c83613575fba1e6d14
Eonist <30n1st@gmail.com>
2017-04-27 23:00:09 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/MinimalView.swift

--- GitSyncMacApp/src/test/MinimalView.swift
+++ GitSyncMacApp/src/test/MinimalView.swift
@@ -7,7 +7,8 @@ class MinimalView:WindowView{
         //let css:String = "Window{fill-alpha:1;fill:white;corner-radius:4px;}"//
         //StyleManager.addStyle(css)
         super.resolveSkin()
-        rotationUITest()
+        //rotationUITest()
+        checkBoxTest()
     }
     //Continue here: 
         //try using checkboxbutton on the bellow:
@@ -22,6 +23,21 @@ class MinimalView:WindowView{
         StyleManager.addStyle(css)
         let btn = addSubView(Button(100,100,self))
         _ = btn
+    }
+    /**
+     *
+     */
+    func checkBoxTest(){
+        var css = "CheckBox{"
+        css += "fill:blue,~/Desktop/ElCapitan/svg/arrow_right.svg grey6;"
+        css += "transform:rotate(0deg),rotate(0deg);"
+        css += "}"
+        css += "CheckBox:checked{"
+        css += "transform:rotate(0deg),rotate(90deg);"
+        css += "}"
+        StyleManager.addStyle(css)
+        let checkBox1 = self.addSubView(CheckBox(100,100,false,self))
+        _ = checkBox1
     }
     func treeList(){
         let dp:TreeDP2 = TreeDP2("~/Desktop/assets/xml/treelist.xml".tildePath)
