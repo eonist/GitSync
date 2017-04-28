04415def8fec89489a8d45eb4d10e4eed559ecc6
Eonist <30n1st@gmail.com>
2017-04-28 16:52:23 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -9,16 +9,17 @@ class TreeList3:ElasticScrollFastList3{//ElasticSlideScrollFastList3
         let idx3d:[Int] = treeDP.hashList[listItem.idx]
         listItem.item.id = idx3d.count.string/*the indentation level (from 1 and up)*/
         
+        
+        
+        if let checkable = listItem.item as? ICheckable, let isOpenStr = TreeDP2Parser.getProp(treeDP, idx3d, "isOpen"){/*Is checkable and open*/
+            let isChecked = isOpenStr == "true"
+            if(checkable.getChecked() != isChecked){//only alter state if that state is the opposite of current state
+                disableAnim{checkable.setChecked(isChecked)}/*Sets correct open/close icon*/
+            }
+        }
         disableAnim{//sets correct indentation
             listItem.item.setSkinState(listItem.item.getSkinState())
         }
-        
-        /*if let checkable = listItem.item as? ICheckable, let isOpenStr = TreeDP2Parser.getProp(treeDP, idx3d, "isOpen"){/*Is checkable and open*/
-         let isChecked = isOpenStr == "true"
-         if(checkable.getChecked() != isChecked){//only alter state if that state is the opposite of current state
-         disableAnim{checkable.setChecked(isChecked)}/*Sets correct open/close icon*/
-         }
-         }*/
         //let hasChildren:Bool = TreeDP2Asserter.hasChildren(treeDP, idx3d)//Does item have children?
         ////hides checkBox if item doesnt have children
         //(listItem.item as! TreeList3Item).checkBox!.isHidden = !hasChildren
