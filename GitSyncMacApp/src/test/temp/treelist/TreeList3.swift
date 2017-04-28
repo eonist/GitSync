ff825c976c4e04dea448e0210930bed30c2e94c1
Eonist <30n1st@gmail.com>
2017-04-28 18:07:05 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/TreeList3.swift

--- GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
+++ GitSyncMacApp/src/test/temp/treelist/TreeList3.swift
@@ -19,22 +19,8 @@ class TreeList3:ElasticScrollFastList3{//ElasticSlideScrollFastList3
             //listItem.item.setSkinState(listItem.item.getSkinState())
             //get to checkbox and text
             //get checkbox width 
-            if let treeListItem = listItem.item as? TreeList3Item,let checkBox = treeListItem.checkBox {
-                var style:IStyle = StyleModifier.clone(checkBox.skin!.style!)
-                var marginLeft0:IStyleProperty = style.getStyleProperty("margin-left",0)!
-                marginLeft0.value = 24
-                StyleModifier.overrideStyleProperty(&style, marginLeft0)
-                var marginLeft1:IStyleProperty = style.getStyleProperty("margin-left",1)!
-                marginLeft1.value = 30
-                StyleModifier.overrideStyleProperty(&style, marginLeft1)
-                //style.describe()
-                
-                checkBox.skin?.setStyle(style)
-                //wait a minute.
-                    //if you set indentation with a custom style
-                        //and then mouseover
-                            //the style will be correct anyway, since style adhers to id and check state. 🎉
-                //
+            if let treeListItem = listItem.item as? TreeList3Item,
+                let checkBoxWidth:CGFloat = treeListItem.checkBox {
                 
             }
         }
