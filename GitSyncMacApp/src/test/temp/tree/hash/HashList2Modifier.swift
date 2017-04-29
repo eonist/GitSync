b751679202998093a29f9a013fd3cd191e13829f
Eonist <30n1st@gmail.com>
2017-04-29 20:23:13 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift
@@ -5,33 +5,33 @@ class HashList2Modifier {
     /**
      * Inserts descendants into the 2d list
      */
-    static func addDescendants(_ list:inout [[Int]],_ idx:Int,_ tree:Tree) -> Int{
-        let idx3d:[Int] = list[idx]
+    static func addDescendants(_ list:inout [[Int]],_ idx2d:Int,_ tree:Tree) -> Int{
+        let idx3d:[Int] = list[idx2d]
         //Swift.print("idx3d: " + "\(idx3d)")
         //you want all open descendants at 3dIdx
         var indecies:[[Int]] = TreeUtils.pathIndecies(tree,idx3d,TreeUtils.isOpen)/*flattens 3d to 2d*/
         indecies = indecies.map{idx3d + $0}//prepend the parent pathIdx to get complete pathIndecies
         //Swift.print("idx: " + "\(idx)")
         //Swift.print("indecies: " + "\(indecies)")
-        let indexAfter:Int = idx+1/*we need to inser after idx*/
+        let indexAfter:Int = idx2d+1/*we need to inser after idx*/
         list.insert(contentsOf: indecies, at: indexAfter)
         return indecies.count
     }
     /**
      * Remove descendants into the 2d list
      */
-    static func removeDescendants(_ list:inout [[Int]],_ at:Int, _ tree:Tree) -> Int{
+    static func removeDescendants(_ list:inout [[Int]],_ idx2d:Int, _ tree:Tree) -> Int{
         //Swift.print("🍐 removeDescendants")
         //Swift.print("at: " + "\(at)")
         //Swift.print("list: " + "\(list)")
-        let idx3d:[Int] = list[at]
+        let idx3d:[Int] = list[idx2d]
         //Swift.print("idx3d: " + "\(idx3d)")
         let child:Tree = tree[idx3d]!
         //Swift.print("child.children.count: " + "\(child.children.count)")
         let count:Int = child.count(TreeUtils.isOpen)
         //Swift.print("removeDescendants.count: " + "\(count)")
-        let end:Int = at + count
-        _ = ArrayModifier.removeRange(&list, at + 1,end + 1)
+        let end:Int = idx2d + count
+        _ = ArrayModifier.removeRange(&list, idx2d + 1,end + 1)
         return count
     }
 }
