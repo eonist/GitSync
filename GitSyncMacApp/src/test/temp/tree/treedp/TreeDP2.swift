91fd3c19f2e3830a8fd09f73f8b0aada6a38e0a9
Eonist <30n1st@gmail.com>
2017-04-29 14:15:48 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/treedp/TreeDP2.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/TreeDP2.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/TreeDP2.swift
@@ -40,9 +40,15 @@ class TreeDP2:DataProvider {
     }
 }
 extension TreeDP2{
+    /**
+     * Returns idx3d for idx2d
+     */
     subscript(_ idx2d:Int) -> [Int] {
         return hashList[idx2d]
     }
+    /**
+     * Returns idx2d for idx3d
+     */
     subscript(_ idx3:[Int]) -> Int? {
         return hashList.index(where: {$0 == idx3})
     }
