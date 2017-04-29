670fb7aa888226553caf906b8ecf3d64f13fb97b
Eonist <30n1st@gmail.com>
2017-04-29 15:42:48 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Modifier.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Modifier.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Modifier.swift
@@ -7,23 +7,26 @@ class TreeDP2Modifier {
      */
     static func open(_ dp:TreeDP2, _ idx2d:Int){
         let idx3d:[Int] = dp.hashList[idx2d]
-        open(dp,idx3d)
+        dp.tree.setProp(idx3d,("isOpen","true"))//updates tree
+        let count:Int = HashList2Modifier.addDescendants(&dp.hashList, idx2d, dp.tree)//adds items to HashList (via HashListModifier.addDescendants)
+        dp.onEvent(DataProviderEvent(DataProviderEvent.add, idx2d, idx2d+count, dp))
     }
     /**
      * NOTE: after this method is called, send event to FastList UI component, that items were removed
      */
     static func close(_ dp:TreeDP2, _ idx2d:Int){
         let idx3d:[Int] = dp.hashList[idx2d]
-        close(dp,idx3d)
-    }
-    static func open(_ dp:TreeDP2, _ idx3d:[Int]){
-        dp.tree.setProp(idx3d,("isOpen","true"))//updates tree
-        let count:Int = HashList2Modifier.addDescendants(&dp.hashList, idx2d, dp.tree)//adds items to HashList (via HashListModifier.addDescendants)
-        dp.onEvent(DataProviderEvent(DataProviderEvent.add, idx2d, idx2d+count, dp))
-    }
-    static func close(_ dp:TreeDP2, _ idx2d:Int){
         let count:Int = HashList2Modifier.removeDescendants(&dp.hashList, idx2d, dp.tree)//remove items from HashList (via HashListModifier.removeDescendants)
         dp.tree.setProp(idx3d,("isOpen","false"))//update tree
         dp.onEvent(DataProviderEvent(DataProviderEvent.remove, idx2d, idx2d+count, dp))
+    }
+    /**
+     *
+     */
+    static func open(_ dp:TreeDP2, _ idx3d:[Int]){
+        
+    }
+    static func close(_ dp:TreeDP2, _ idx3d:[Int]){
+        
     }
 }
