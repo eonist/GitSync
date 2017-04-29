25846c78afff329e6868277c0e2cfaa1f7e8f5f7
Eonist <30n1st@gmail.com>
2017-04-29 15:15:00 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swiftGitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
@@ -10,4 +10,16 @@ class TreeList3Modifier {
             FastList3Modifier.select(treeList, idx2d, isSelected)
         }
     }
+    /**
+     *
+     */
+    static func open(_ treeList:TreeList3, _ at:[Int]){
+        
+    }
+    /**
+     *
+     */
+    static func close(_ treeList:TreeList3, _ at:[Int]){
+        
+    }
 }
--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Utils.swift
@@ -1,16 +1,5 @@
 import Foundation
 
 class TreeList3Utils {
-    /**
-     *
-     */
-    static func open(_ treeList:TreeList3, _ at:[Int]){
-        
-    }
-    /**
-     *
-     */
-    static func close(_ treeList:TreeList3, _ at:[Int]){
-        
-    }
+    
 }
