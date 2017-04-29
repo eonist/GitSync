e0ea27cabae4b5f00b0fe45a2f5ed795dd1bd57d
Eonist <30n1st@gmail.com>
2017-04-29 14:08:44 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
@@ -1,5 +1,11 @@
 import Foundation
 
 class TreeList3Modifier {
-
+    /**
+     * Sets a selectable in PARAM: treeList at PARAM: index (array index)
+     * NOTE: this does not unselect previously selected items.
+     */
+    static func selectAt(_ treeList:ITreeList, _ index:[Int],_ isSelected:Bool = true) {
+        
+    }
 }
