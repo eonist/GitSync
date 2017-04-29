e04dcb1f824aa6ad531267f64caac0abe086625e
Eonist <30n1st@gmail.com>
2017-04-29 15:01:32 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift

--- GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/concrete/list/utils/FastList3Parser.swift
@@ -5,14 +5,6 @@ class FastList3Parser {
      * We extract the index by searching for the origin among the visibleItems, the view doesn't store the index it self, but the visibleItems store absolute indecies
      */
     static func idx(_ fastList:FastListable3, _ item:NSView) -> Int?{
-        //use filter on the bellow
         return fastList.pool.first(where:{$0.item === item})?.idx
-        
-        /*for obj in fastList.pool{
-         if(obj.item === item){
-         return obj.idx
-         }
-         }
-         return nil*/
     }
 }
