0fa54f2b6e585791ad9118c6aa9bad193d9d5c2a
Eonist <30n1st@gmail.com>
2017-04-30 21:16:32 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/utils/TreeConverter.swift

--- GitSyncMacApp/src/test/temp/tree/utils/TreeConverter.swift
+++ GitSyncMacApp/src/test/temp/tree/utils/TreeConverter.swift
@@ -1,5 +1,52 @@
 import Foundation
 
 class TreeConverter {
-
+    /**
+     * Convert xml to Tree-struture
+     */
+    static func tree(_ xml:XML) -> Tree{
+        var tree:Tree = Tree()
+        tree.name = xml.name
+        //let count = xml.children!.count//or use rootElement.childCount TODO: test this
+        xml.children?.forEach{
+            let child:XML = $0 as! XML
+            var item:Tree = Tree()
+            item.name = child.name
+            let attribs:[String:String] = child.attribs
+            //Swift.print("attribs.isEmpty: " + "\(attribs.isEmpty)")
+            if(!attribs.isEmpty){
+                item.props = attribs
+            }
+            if let content = child.stringValue, content.count > 0 {
+                item.content = content
+            }else if(child.hasComplexContent) {
+                _ = item.children += TreeUtils.tree(child).children
+            }
+            tree.add(item)
+        }
+        return tree
+    }
+    /**
+     * Converts Tree to XML
+     */
+    static func xml(_ tree:Tree) -> XML{
+        /**
+         * Array types
+         */
+        //Swift.print("handleArray: " + "name \(name)" + " $0.value: \(value)" )
+        func toXML(_ tree:Tree)->XML{
+            let xml = XML()
+            xml.name = tree.name
+            if(tree.content != nil){xml.stringValue = tree.content}
+            if(tree.props != nil){xml.setAttributesWith(tree.props!)}
+            return xml
+        }
+        
+        let xml:XML = toXML(tree)
+        tree.children.forEach{ child in/*This can be a single .map method*/
+            let childXML:XML = TreeUtils.xml(child)
+            xml += childXML
+        }
+        return xml
+    }
 }
