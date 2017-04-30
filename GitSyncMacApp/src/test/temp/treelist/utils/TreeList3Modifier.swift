2d7baa7e9bd157d58d61ac587d4a828508cfff91
Eonist <30n1st@gmail.com>
2017-04-30 23:11:58 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Modifier.swift
@@ -35,13 +35,12 @@ class TreeList3Modifier {
             if isOpen == "true" {//already open
                 close(treeList,idx3d)
             }
-         
-            //traverse all items and set to open
+            //1.traverse all items and set to open
             recursiveApply(&treeList.treeDP.tree[idx3d]!,setValue,("isOpen","true"))
-            //add all descedants to 2d list
+            //2.add all descedants to 2d list
             let idx2d:Int = treeList.treeDP[idx3d]!
             let count:Int = HashList2Modifier.addDescendants(&treeList.treeDP.hashList, idx2d, idx3d, treeList.treeDP.tree)/*adds items to HashList (via HashListModifier.addDescendants)*/
-            //use the count to update DP and UI
+            //3.use the count to update DP and UI
             treeList.dp.onEvent(DataProviderEvent(DataProviderEvent.remove, idx2d, idx2d+count, treeList.dp))
         }
         let apply:TreeModifier.ApplyMethod = {tree in
@@ -54,7 +53,7 @@ class TreeList3Modifier {
      * NOTE: This method collapses all nodes from the PARAM: index
      */
     static func collapseAt(_ treeList:TreeListable3,_ idx3d:[Int]) {
-        
+        //do the same as explode at but set close not open
     }
     
     typealias KeyValue = (key:String,val:String)
