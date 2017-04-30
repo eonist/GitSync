c9a15261f0f7d10c77106065d7cce62a49492bbb
Eonist <30n1st@gmail.com>
2017-04-30 21:05:22 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift

--- GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift
+++ GitSyncMacApp/src/test/temp/tree/hash/HashList2Modifier.swift
@@ -5,8 +5,7 @@ class HashList2Modifier {
     /**
      * Inserts descendants into the 2d list
      */
-    static func addDescendants(_ list:inout [[Int]],_ idx2d:Int,_ tree:Tree) -> Int{
-        let idx3d:[Int] = list[idx2d]
+    static func addDescendants(_ list:inout [[Int]],_ idx2d:Int, idx3d:[Int], _ tree:Tree) -> Int{
         //Swift.print("idx3d: " + "\(idx3d)")
         //you want all open descendants at 3dIdx
         var indecies:[[Int]] = TreeUtils.pathIndecies(tree,idx3d,TreeUtils.isOpen)/*flattens 3d to 2d*/
@@ -20,11 +19,10 @@ class HashList2Modifier {
     /**
      * Remove descendants into the 2d list
      */
-    static func removeDescendants(_ list:inout [[Int]],_ idx2d:Int, _ tree:Tree) -> Int{
+    static func removeDescendants(_ list:inout [[Int]],_ idx2d:Int,_ idx3d:[Int], _ tree:Tree) -> Int{
         //Swift.print("🍐 removeDescendants")
         //Swift.print("at: " + "\(at)")
         //Swift.print("list: " + "\(list)")
-        let idx3d:[Int] = list[idx2d]
         //Swift.print("idx3d: " + "\(idx3d)")
         let child:Tree = tree[idx3d]!
         //Swift.print("child.children.count: " + "\(child.children.count)")
