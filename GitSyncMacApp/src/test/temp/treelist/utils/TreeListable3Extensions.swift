2bf5a136533ddf9038e3687bb8bdc7242c7f0db5
Eonist <30n1st@gmail.com>
2017-05-01 23:55:54 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeListable3Extensions.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeListable3Extensions.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeListable3Extensions.swift
@@ -3,4 +3,10 @@ import Foundation
 extension TreeListable3 {
     //add convenience methods here
     var selected:Tree? {return TreeList3Parser.selected(self)}
+    func open(_ idx3d:[Int]){/*Convenience*/
+        TreeList3Modifier.open(self, idx3d)
+    }
+    func close(_ idx3d:[Int]){/*Convenience*/
+        TreeList3Modifier.close(self, idx3d)
+    }
 }
