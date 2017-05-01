b1bbeaf7face99a87e1a847208a538cb7be50f0e
Eonist <30n1st@gmail.com>
2017-05-01 22:59:53 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/MinimalView.swift

--- GitSyncMacApp/src/test/MinimalView.swift
+++ GitSyncMacApp/src/test/MinimalView.swift
@@ -23,7 +23,8 @@ class MinimalView:WindowView{
             //pass the tests bellow:
                 //do selectedTitle next. in an easy way
         
-        treeList.treeDP.tree.p
+        TreeList3Parser.selected(treeList)
+        treeList.treeDP.tree[]
         
         /*Swift.print("selected: " + "\(TreeListParser.selected(treeList))")
          Swift.print("selectedIndex: " + "\(TreeListParser.selectedIndex(treeList))")//Output:  [2,2,0]
