ec062e5db02b818e71a20f95a631e81e80ba2bbf
Eonist <30n1st@gmail.com>
2017-05-01 15:26:03 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift

--- GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift
+++ GitSyncMacApp/src/test/temp/tree/treedp/2/TreeDP2Parser.swift
@@ -6,13 +6,13 @@ class TreeDP2Parser {
      * Returns prop-value for idx2d and key
      * EXAMPLE: TreeDP2Parser.getProp(treeDP, idx, "isOpen")
      */
-    static func getProp(_ dp:TreeDP2,_ idx2d:Int, _ key:String)-> String?{
+    static func getProp(_ dp:TreeDP2,_ idx2d:Int, _ key:String)->String?{
         return getProps(dp, idx2d)?[key]
     }
     /**
      * Returns prop-value for idx3d and key
      */
-    static func getProp(_ dp:TreeDP2,_ idx3:[Int], _ key:String)-> String?{
+    static func getProp(_ dp:TreeDP2,_ idx3:[Int], _ key:String)->String?{
         return getProps(dp, idx3)?[key]
     }
     /**
