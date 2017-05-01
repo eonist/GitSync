9df1f21657e6b72560642c78a2da61964b1e8221
Eonist <30n1st@gmail.com>
2017-05-01 15:28:24 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Parser.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Parser.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3Parser.swift
@@ -13,5 +13,12 @@ class TreeList3Parser {
     static func idx3d(_ treeList:TreeListable3,_ idx2d:Int)->[Int]?{
         return treeList.treeDP[idx2d]
     }
-    
+    /**
+     *
+     */
+    static func selected(_ treeList:TreeListable3){
+        if let selectedIdx2d = treeList.selectedIdx{
+            
+        }
+    }
 }
