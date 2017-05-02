f40d3080881aeacd027aafe0be764856b4424133
Eonist <30n1st@gmail.com>
2017-05-02 22:53:34 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/abstract/3/FastListable3.swift

--- GitSyncMacApp/src/test/temp/abstract/3/FastListable3.swift
+++ GitSyncMacApp/src/test/temp/abstract/3/FastListable3.swift
@@ -4,7 +4,7 @@ import Foundation
 
 protocol FastListable3:Progressable3,Listable3{
     var selectedIdx:Int? {get set}/*TODO: ⚠️️ rename to just selected?*/
-    var pool:[FastListItem] {get set}
+    var pool:[FastListItem] {get set}/*Stores UI item and Absolute Index*/
     func reUse(_ listItem:FastListItem)
     func createItem(_ index:Int) -> Element
     var inActive:[FastListItem] {get set}
