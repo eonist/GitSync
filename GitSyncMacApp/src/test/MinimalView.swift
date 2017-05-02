d9be75f086de7b08610f1a5db94684d27a4acce6
Eonist <30n1st@gmail.com>
2017-05-02 13:14:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/MinimalView.swift

--- GitSyncMacApp/src/test/MinimalView.swift
+++ GitSyncMacApp/src/test/MinimalView.swift
@@ -32,7 +32,7 @@ class MinimalView:WindowView{
         treeList.collapse([])//closes the treeList
         treeList.explode([])//opens the treeList
         
-        treeList.treeDP.tree.add(Tree("Indigo"))
+        treeList.insert([1],Tree("Indigo"))
         
         /*Swift.print("selected: " + "\(TreeListParser.selected(treeList))")
          Swift.print("selectedIndex: " + "\(TreeListParser.selectedIndex(treeList))")//Output:  [2,2,0]
