d6b88bd766c9bcc46bb3fbe96d24080cc82c2bbf
Eonist <30n1st@gmail.com>
2017-05-02 20:25:57 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/TestView.swiftGitSyncMacApp/src/test/temp/tree/Tree.swift

--- GitSyncMacApp/src/test/temp/tree/Tree.swift
+++ GitSyncMacApp/src/test/temp/tree/Tree.swift
@@ -9,7 +9,7 @@ struct Tree{
     var props:[String:String]?
     var name:String?
     var content:String?/*Could be Generic as well*/
-    init(_ name:String? = nil,  _ content:String? = nil,_ children:[Tree] = [],  _ props:[String:String]? = nil ) {
+    init(_ name:String? = nil,_ children:[Tree] = [],_ content:String? = nil,  _ props:[String:String]? = nil ) {
         self.children = children
         self.props = props
         self.name = name
