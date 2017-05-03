983d0a4181d24218c4386fe0fcdaf59a773cc080
Eonist <30n1st@gmail.com>
2017-05-03 12:51:29 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/treelist/utils/TreeList3AdvanceModifier.swift

--- GitSyncMacApp/src/test/temp/treelist/utils/TreeList3AdvanceModifier.swift
+++ GitSyncMacApp/src/test/temp/treelist/utils/TreeList3AdvanceModifier.swift
@@ -34,16 +34,16 @@ class TreeList3AdvanceModifier {
         //check legacy code, but isn't treeList always open by default
         if let child:Tree = treeList.treeDP.tree[idx3d] {
             Swift.print("child.children.count: " + "\(child.children.count)")
-            /*1.traverse all items and set to open*/
+            /*1.Traverse all items and set to open*/
             Swift.print("1.traverse all items and set to open")
             TreeList3Modifier.recursiveApply(&treeList.treeDP.tree[idx3d]!,TreeList3Modifier.setValue,("isOpen","false"))
-            /*2.remove all descendants to 2d list*/
+            /*2.Temove all descendants to 2d list*/
             Swift.print("2.remove all descedants to 2d list")
             let idx2d:Int = treeList.treeDP[idx3d]!
             Swift.print("idx2d: " + "\(idx2d)")
             let count:Int = HashList2Modifier.removeDescendants(&treeList.treeDP.hashList, idx2d, idx3d, treeList.treeDP.tree)/*removes items from HashList (via HashListModifier.removeDescendants)*/
             Swift.print("count: " + "\(count)")
-            /*3.use the count to update DP and UI*/
+            /*3.Use the count to update DP and UI*/
             Swift.print("3.use the count to update DP and UI")
             treeList.dp.onEvent(DataProviderEvent(DataProviderEvent.remove, idx2d, idx2d+count, treeList.dp))
         }
