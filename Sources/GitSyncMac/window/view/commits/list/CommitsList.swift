ec60e55e7f05b0554bc8f612140870e0a1d8e0ab
Eonist <30n1st@gmail.com>
2017-05-05 19:44:14 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/list/CommitsList.swift

--- Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
+++ Sources/GitSyncMac/window/view/commits/list/CommitsList.swift
@@ -25,8 +25,8 @@ class CommitsList:ElasticSlideScrollFastList3,ICommitList{
      */
     override func createItem(_ index:Int) -> Element {
         let dpItem = dp.items[index]
-        let item:CommitsListItem = CommitsListItem(width, itemHeight ,dpItem["repo-name"]!, dpItem["contributor"]!,dpItem["title"]!,dpItem["description"]!,dpItem["date"]!, false, lableContainer)
-        lableContainer!.addSubview(item)
+        let item:CommitsListItem = CommitsListItem(width, itemSize.height ,dpItem["repo-name"]!, dpItem["contributor"]!,dpItem["title"]!,dpItem["description"]!,dpItem["date"]!, false, lableContainer)
+        contentContainer!.addSubview(item)
         return item
     }
     /**
@@ -36,9 +36,9 @@ class CommitsList:ElasticSlideScrollFastList3,ICommitList{
         let item:CommitsListItem = listItem.item as! CommitsListItem
         let idx:Int = listItem.idx/*the index of the data in dataProvider*/
         let selected:Bool = idx == selectedIdx//dpItem["selected"]!.bool
-        item.setData(dataProvider.items[idx])
+        item.setData(dp.items[idx])
         if(item.selected != selected){item.setSelected(selected)}//only set this if the selected state is different from the current selected state in the ISelectable
-        item.y = idx * itemHeight/*position the item*/
+        item.y = idx * itemSize.height/*position the item*/
     }
     override func onEvent(_ event:Event) {
         //Swift.print("CommitsList.onEvent() event.type: " + "\(event.type)")
