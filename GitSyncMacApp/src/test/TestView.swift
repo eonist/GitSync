53a50922be31a75156988105172d9f01046f3c07
Eonist <30n1st@gmail.com>
2017-05-07 15:01:05 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/TestView.swift

--- GitSyncMacApp/src/test/TestView.swift
+++ GitSyncMacApp/src/test/TestView.swift
@@ -391,15 +391,15 @@ extension ElasticScrollView3Test{
         
         
         
-        let rectContainer:NSView = contentContainer!.addSubView(FlippedView())
+        let rectContainer:NSView = contentContainer!.addSubView(Container(200,200))
         rectContainer.frame.origin = p
         let hCount:Int = 10
         let vCount:Int = 10
         
         (0..<hCount).indices.forEach{ i in
             (0..<vCount).indices.forEach{ e in
-                let x:CGFloat = /*p.x + */(10 * i)
-                let y:CGFloat = /*p.y + */(10 * e)
+                let x:CGFloat = /*p.x + */(20 * i)
+                let y:CGFloat = /*p.y + */(20 * e)
                 let rect = RectGraphic(x,y,10,10,fill,line)
                 rectContainer.addSubview(rect.graphic)
                 rect.draw()
