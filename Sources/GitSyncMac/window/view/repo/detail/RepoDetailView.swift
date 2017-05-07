fa3104db1b22d88c3cee07381865a9e5dda62918
Eonist <30n1st@gmail.com>
2017-05-07 13:37:59 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -3,7 +3,8 @@ import Cocoa
 @testable import Element
 
 class RepoDetailView:ElasticSlideScrollView3 {
-    override var height:CGFloat {get{return super.height-48}set{super.height = newValue}}//lazy fix, you can use negative height padding to acchive the same thing
+    //override var height:CGFloat {get{return super.height-48}set{super.height = newValue}}//lazy fix, you can use negative height padding to acchive the same thing
+    override var maskSize: CGSize {return CGSize(super.width,super.height-48)}
     //override var itemsHeight:CGFloat {}
     override var contentSize: CGSize {return CGSize(NaN,(12 * 24)+64) }
     override var itemSize: CGSize {return CGSize(NaN,24)}
