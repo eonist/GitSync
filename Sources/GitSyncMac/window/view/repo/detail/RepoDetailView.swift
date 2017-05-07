37238f711283b32ff9dfa9e9dd3e2e1e0fa036de
Eonist <30n1st@gmail.com>
2017-05-07 13:16:05 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -2,7 +2,7 @@ import Cocoa
 @testable import Utils
 @testable import Element
 
-class RepoDetailView:ElasticSlideScrollView {
+class RepoDetailView:ElasticSlideScrollView3 {
     override var height:CGFloat {get{return super.height-48}set{super.height = newValue}}//lazy fix, you can use negative height padding to acchive the same thing
     override var itemsHeight:CGFloat {return (12 * 24)+64}
     override var itemHeight:CGFloat {return 24}
