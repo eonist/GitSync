b23bb59842354d5cf3a95cd7655fd86492c8b61f
Eonist <30n1st@gmail.com>
2017-05-08 17:33:00 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -14,12 +14,12 @@ class AppDelegate:NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification:Notification) {
         Swift.print("GitSync - Automates git")//Simple git automation for macOS, The autonomouse git client,The future is automated
-        //NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
+        NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
         
-        //initApp()
+        initApp()
         //initTestWin()//🚧👷
         //initMinimalWin()
-        hitTesting()
+        //hitTesting()
         //Continue here: 🏀
             //add general-direction-scrolling in ElasticScrollView 👈
             //figure out how to get hitTest working again
@@ -38,11 +38,11 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         container.addSubview(btn)
         /*container.layer?.position.x = 100
          container.layer?.position.y = 100*/
-        container.layer?.position = CGPoint(40,20)
+        container.frame.origin = CGPoint(40,20)
         //container.frame.origin = CGPoint(100,100)
         Swift.print("container.layer?.position: " + "\(container.layer?.position)")
         Swift.print("container.frame.origin: " + "\(container.frame.origin)")
-        btn.layer?.position = CGPoint(40,20)
+        btn.frame.origin = CGPoint(40,20)
         
         Swift.print("btn.layer?.position: " + "\(btn.layer?.position)")
         Swift.print("btn.frame.origin: " + "\(btn.frame.origin)")
