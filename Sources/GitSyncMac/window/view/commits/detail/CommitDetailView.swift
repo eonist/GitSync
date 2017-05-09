34686942d71dadfb0088d9cc04c7466cd14b0df1
Eonist <30n1st@gmail.com>
2017-05-09 23:14:17 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift

--- Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift
+++ Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift
@@ -5,16 +5,23 @@ import Foundation
  * Enables you to view the commit title and commit description of a single commit
  */
 class CommitDetailView:Element {
-    var repoNameText:Text?
-    var titleText:Text?
+    lazy var repoNameText:Text = {
+        let repoNameText = self.addSubView(Text(120,20,"repo name",self,"repoName"))
+        repoNameText.isInteractive = false
+        return repoNameText
+
+    }()
+    lazy var titleText:Text = {
+        let titleText = self.addSubView(Text(280,24,"commit title",self,"title"))
+        titleText!.isInteractive = false
+        return titleText
+    }()
     var descText:Text?
     override func resolveSkin() {
         Swift.print("CommitDetailView.resolveSkin()")
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
-        repoNameText = addSubView(Text(120,20,"repo name",self,"repoName"))
-        repoNameText!.isInteractive = false
-        titleText = addSubView(Text(280,24,"commit title",self,"title"))
-        titleText!.isInteractive = false
+        _ = repoNameText
+        
         descText = addSubView(Text(480,220,"commit desc",self,"description"))
         descText!.isInteractive = false
     }
@@ -22,7 +29,7 @@ class CommitDetailView:Element {
      * Populates the UI elements with data
      */
     func setCommitData(_ commitData:[String:String]){
-        repoNameText!.setText(commitData["repo-name"]!)
+        repoNameText.setText(commitData["repo-name"]!)
         titleText!.setText(commitData["title"]!)
         descText!.setText(commitData["description"]!)
     }
