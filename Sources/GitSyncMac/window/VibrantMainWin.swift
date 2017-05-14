e5fd047d1dc0840591c1cd6916cfd40a97d1c45f
Eonist <30n1st@gmail.com>
2017-05-14 14:58:39 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/VibrantMainWin.swift

--- Sources/GitSyncMac/window/VibrantMainWin.swift
+++ Sources/GitSyncMac/window/VibrantMainWin.swift
@@ -1,5 +1,9 @@
-import Foundation
+import Cocoa
+@testable import Element
+@testable import Utils
 
-class VibrantMainWin {
-
+class VibrantMainWin:TranslucentWin {
+    convenience init(_ w:CGFloat,_ h:CGFloat){
+        self.init(contentRect:NSRect(0,0,w,h), styleMask: [.borderless,.resizable], backing:NSBackingStoreType.buffered, defer: false)
+    }
 }
