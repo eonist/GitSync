040071f9518588408f4b7c3427a91949a88117c4
Eonist <30n1st@gmail.com>
2017-05-14 15:08:20 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/VibrantMainWin.swift

--- Sources/GitSyncMac/window/VibrantMainWin.swift
+++ Sources/GitSyncMac/window/VibrantMainWin.swift
@@ -5,5 +5,11 @@ import Cocoa
 class VibrantMainWin:TranslucentWin {
     convenience init(_ w:CGFloat,_ h:CGFloat){
         self.init(contentRect:NSRect(0,0,w,h), styleMask: [.borderless,.resizable], backing:NSBackingStoreType.buffered, defer: false)
+        WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
+        resolveSkin()
+    }
+    func resolveSkin() {
+        MainWin.mainView = MainView(frame.size.width,frame.size.height,"")/*Sets the mainview of the window*/
+        self.contentView?.addSubview(MainWin.mainView!)
     }
 }
