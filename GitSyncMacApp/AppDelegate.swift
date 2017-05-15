b79d68f269c5163cb5375558a9812f885faada5f
Eonist <30n1st@gmail.com>
2017-05-15 11:42:25 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -24,7 +24,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         let a:String = "<item title=\"New folder\" isOpen=\"false\" hasChildren=\"true\"></item>"
         //treeList.node.addAt(newIdx(idx), a.xml)//"<item title=\"New folder\"/>"
         let tree = TreeConverter.tree(a.xml)
-        tree.describe(tree, <#T##key: String##String#>)
+        Swift.print("tree.name: " + "\(tree.name)")
+        Swift.print("tree.content: " + "\(tree.content)")
+        tree.
         
         //work on const to enum? and more structs 🏀
             //clean up some of the git classes
