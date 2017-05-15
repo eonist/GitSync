d2904a841a4e5c690e83574d01e944db99656cfe
Eonist <30n1st@gmail.com>
2017-05-15 16:20:06 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepoContextMenu.swift

--- Sources/GitSyncMac/window/view/repo/RepoContextMenu.swift
+++ Sources/GitSyncMac/window/view/repo/RepoContextMenu.swift
@@ -117,6 +117,7 @@ extension RepoContextMenu{
             // return 3d idx and update 2d list. Check collapse first to see how it works. also how remove and add works etc
         if let idx = rightClickItemIdx {
             let idxAbove:Int = TreeModifier.moveUp(&treeList.treeDP.tree, idx)//_ = TreeListModifier.moveUp(treeList, idx)
+            
         }
         
     }
