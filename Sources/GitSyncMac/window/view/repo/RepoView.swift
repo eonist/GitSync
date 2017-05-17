2848ceab990aad26e640a4a8f6990a64779ea2f1
Eonist <30n1st@gmail.com>
2017-05-17 21:27:49 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepoView.swift

--- Sources/GitSyncMac/window/view/repo/RepoView.swift
+++ Sources/GitSyncMac/window/view/repo/RepoView.swift
@@ -16,25 +16,28 @@ class RepoView:Element {
         };return treeDP/*already exist, return old dp*/
     }
     lazy var treeList:TreeList3 = {
-        return  addSubView(TreeList3(width, height-60, CGSize(24,24), RepoView.treeDP, self))
+        //if(RepoView.selectedListItemIndex.count > 0){TreeListModifier.selectAt(treeList!, RepoView.selectedListItemIndex)}
+        return  self.addSubView(TreeList3(self.width, self.height-60, CGSize(24,24), RepoView.treeDP, self))
     }()// {return RepoView.list}
-    var contextMenu:RepoContextMenu?
+    lazy var contextMenu:RepoContextMenu = {
+        return RepoContextMenu(self.treeList)
+    }()
     
     override func resolveSkin() {
         self.skin = SkinResolver.skin(self)//super.resolveSkin()
+        _ = treeList
+        _ = contextMenu
         
-        contextMenu = RepoContextMenu(treeList!)
-        //if(RepoView.selectedListItemIndex.count > 0){TreeListModifier.selectAt(treeList!, RepoView.selectedListItemIndex)}
     }
     override func onEvent(_ event:Event) {
         if(event.type == ListEvent.select && event.immediate === treeList){//if(event.type == SelectEvent.select && event.immediate === treeList){}
-            Swift.print("RepoView.onTreeListEvent() selectedIndex: " + "\(treeList?.selectedIdx3d)")
+            Swift.print("RepoView.onTreeListEvent() selectedIndex: " + "\(treeList.selectedIdx3d)")
             //print("_scrollTreeList.database.xml.toXMLString(): " + _scrollTreeList.database.xml.toXMLString());
             onTreeListSelect()
         }else if(event.type == ButtonEvent.rightMouseDown){
-            contextMenu!.rightClickItemIdx = TreeList3Parser.index(treeList!, event.origin as! NSView)
-            Swift.print("RightMouseDown() rightClickItemIdx: " + "\(contextMenu!.rightClickItemIdx)")
-            NSMenu.popUpContextMenu(contextMenu!, with: (event as! ButtonEvent).event!, for: self)
+            contextMenu.rightClickItemIdx = TreeList3Parser.index(treeList, event.origin as! NSView)
+            Swift.print("RightMouseDown() rightClickItemIdx: " + "\(contextMenu.rightClickItemIdx)")
+            NSMenu.popUpContextMenu(contextMenu, with: (event as! ButtonEvent).event!, for: self)
         }
     }
 }
@@ -42,11 +45,10 @@ extension RepoView{
     func onTreeListSelect(){
         //Swift.print("RepoView.onTreeListSelect()")
         //Sounds.play?.play()
-       
-        let selectedIndex:[Int] = treeList!.selectedIdx3d!
+        let selectedIndex:[Int] = treeList.selectedIdx3d!
         RepoView.selectedListItemIndex = selectedIndex
         //TODO: Use the RepoItem on the bellow line see AutoSync class for implementation
-        if let repoItemDict:[String:String] = treeList!.treeDP.tree[selectedIndex]?.props{//NodeParser.dataAt(treeList!.node, selectedIndex)
+        if let repoItemDict:[String:String] = treeList.treeDP.tree[selectedIndex]?.props{//NodeParser.dataAt(treeList!.node, selectedIndex)
             var repoItem:RepoItem
             if(repoItemDict["hasChildren"] != nil || repoItemDict["isOpen"] != nil){/*Support for folders*/
                 repoItem = RepoItem()
@@ -55,8 +57,7 @@ extension RepoView{
             }else{
                 repoItem = RepoUtils.repoItem(repoItemDict)
             }
-            //(Navigation.currentView as! RepoDetailView).setRepoData(repoItem)//updates the UI elements with the selected repo data
-            Navigation.setView(.repoDetail(repoItem))
+            Navigation.setView(.repoDetail(repoItem))//updates the UI elements with the selected repo data
         }
     }
 }
