d37bd7c5e3d933a21101b7f6e5624b685c4301e5
Eonist <30n1st@gmail.com>
2017-05-18 23:24:35 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -14,16 +14,22 @@ class DebugText:Element {
     }
 }
 class DebugTextArea:Element {
-    lazy var text:Element = {return self.addSubView(DebugText(NaN,NaN,self,"text1"))}()
+    lazy var text:Element = {return self.addSubView(DebugText(NaN,NaN,self))}()
     override func resolveSkin() {
         super.resolveSkin()
         _ = text
     }
 }
 class DebugTextInput:Element {
-    lazy var text:Element = {return self.addSubView(DebugText(NaN,NaN,self,"text1"))}()
-    lazy var textArea:Element = {return self.addSubView(DebugTextArea(NaN,NaN,self,"text2"))}()
+    lazy var text:Element = {return self.addSubView(DebugText(NaN,NaN,self))}()
+    lazy var textArea:Element = {return self.addSubView(DebugTextArea(NaN,NaN,self))}()
     override func resolveSkin() {
+        var css:String = ""
+        css += "DebugTextInput DebugTextInput{fill:orange;width:100%;height:48px;float:left;clear:left;padding-right:100px;fill:yellow;}"
+        css += "DebugTextInput DebugTextInput #text1{fill:purple;width:100px;height:48px;float:left;clear:none;}"
+        css += "DebugTextInput DebugTextInput #text2{fill:green;width:100%;height:48px;float:left;clear:none;}"
+        
+        StyleManager.addStyle(css)
         super.resolveSkin()
         _ = text
         _ = textArea
@@ -33,17 +39,7 @@ class TestItem:Element{
     lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
     override func resolveSkin() {
-         var css:String = ""
-         css += "RepositoryDetailView TestItem{fill:orange;width:100%;height:48px;float:left;clear:left;padding-right:100px;}"
-         css += "RepositoryDetailView TestItem{fill:yellow;}"//padding-top:12px;padding-left:12px;padding-right:-24px;
-         css += "RepositoryDetailView TestItem #text1{fill:purple;width:100px;height:48px;float:left;clear:none;}"
-         css += "RepositoryDetailView TestItem #text2{fill:green;width:100%;height:48px;float:left;clear:none;}"
-         /*css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;fill:Blue;fill-alpha:1;}"
-         css += "RepositoryDetailView TextInput Text{width:100px;fill:yellow;fill-alpha:1;}"
-         css += "RepositoryDetailView TextInput TextArea{width:100%;fill:blue;fill-alpha:1;}"
-         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"
-         */
-        StyleManager.addStyle(css)
+        
         super.resolveSkin()
          _ = text1
          _ = text2
