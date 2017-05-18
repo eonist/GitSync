ec060de1a6cef8df235f5803de22cc9e4c91a189
Eonist <30n1st@gmail.com>
2017-05-18 15:54:45 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -3,5 +3,15 @@ import Foundation
 @testable import Element
 
 class RepositoryDetailView:Element {
-    
+    lazy var text1:Element = {return self.addSubView(Element(NaN,self.height,self,"text1"))}()
+    lazy var text2:Element = {return self.addSubView(Element(NaN,self.height,self,"text2"))}()
+    override func resolveSkin() {
+        var css:String = "RepositoryDetailView{float:left;clear:left;}"
+        css += "RepositoryDetailView #left{fill:orange;width:100%;float:left;clear:none;}"
+        css += "RepositoryDetailView #right{fill:green;width:100%;float:left;clear:none;}"
+        StyleManager.addStyle(css)
+        super.resolveSkin()
+        _ = leftColumn
+        _ = rightColumn
+    }
 }
