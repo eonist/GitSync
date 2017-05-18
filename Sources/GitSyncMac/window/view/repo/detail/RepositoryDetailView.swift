ac92d390ce777da55710081914b81fd632ffdc0a
Eonist <30n1st@gmail.com>
2017-05-18 20:08:42 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -6,7 +6,6 @@ class TestItem:Element{
     lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
     override func resolveSkin() {
-        
          var css:String = ""
          css += "RepositoryDetailView TestItem{fill:orange;width:100%;height:48px;float:left;clear:left;}"
         /*
@@ -15,7 +14,8 @@ class TestItem:Element{
          css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;fill:Blue;fill-alpha:1;}"
          css += "RepositoryDetailView TextInput Text{width:100px;fill:yellow;fill-alpha:1;}"
          css += "RepositoryDetailView TextInput TextArea{width:100%;fill:blue;fill-alpha:1;}"
-         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"*/
+         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"
+         */
         
         StyleManager.addStyle(css)
         super.resolveSkin()
@@ -23,7 +23,6 @@ class TestItem:Element{
          _ = text2*/
     }
 }
-
 class RepositoryDetailView:Element {
     lazy var contentContainer:Container = {return self.addSubView(Container(self.width,self.height,self,"lable"))}()
     lazy var item:TestItem = {return self.addSubView(TestItem(NaN,NaN,self))}()
@@ -31,17 +30,15 @@ class RepositoryDetailView:Element {
     //lazy var nameTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Name: ", "", self.contentContainer))}()
     //lazy var localPathTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Local-path: ", "", self.contentContainer))}()
     override func resolveSkin() {
-        
         var css:String = ""
         css += "RepositoryDetailView Container#lable{float:left;clear:left;}"
-        css += "RepositoryDetailView{float:left;clear:left;}"
+        css += "RepositoryDetailView{float:left;clear:left;100%;}"
        
-        /**/
         //StyleManager.addStyle(css)
         super.resolveSkin()
         _ = contentContainer
          _ = item
-         /**/
+       
         //_ = nameTextInput
         //_ = localPathTextInput
     }
