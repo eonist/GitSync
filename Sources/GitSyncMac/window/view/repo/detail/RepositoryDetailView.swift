bcaa1375a7fd86cc30179ee562f38acb1b846f25
Eonist <30n1st@gmail.com>
2017-05-18 20:06:19 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -6,20 +6,21 @@ class TestItem:Element{
     lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
     override func resolveSkin() {
-        /*
+        
          var css:String = ""
-         css += "RepositoryDetailView{float:left;clear:left;}"
+         css += "RepositoryDetailView TestItem{fill:orange;width:100%;height:48px;float:left;clear:left;}"
+        /*
          css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
          css += "RepositoryDetailView #text2{fill:green;width:100%;height:48px;float:left;clear:left;}"
          css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;fill:Blue;fill-alpha:1;}"
          css += "RepositoryDetailView TextInput Text{width:100px;fill:yellow;fill-alpha:1;}"
          css += "RepositoryDetailView TextInput TextArea{width:100%;fill:blue;fill-alpha:1;}"
-         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"
-         */
-        //StyleManager.addStyle(css)
+         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"*/
+        
+        StyleManager.addStyle(css)
         super.resolveSkin()
-        _ = text1
-        _ = text2
+        /*_ = text1
+         _ = text2*/
     }
 }
 
@@ -33,7 +34,7 @@ class RepositoryDetailView:Element {
         
         var css:String = ""
         css += "RepositoryDetailView Container#lable{float:left;clear:left;}"
-        css += "RepositoryDetailView TestItem{fill:orange;width:100%;height:48px;float:left;clear:left;}"
+        css += "RepositoryDetailView{float:left;clear:left;}"
        
         /**/
         //StyleManager.addStyle(css)
