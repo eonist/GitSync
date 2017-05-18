12e0fe3819409d4bbd729910c72b6de848013ac0
Eonist <30n1st@gmail.com>
2017-05-18 20:01:52 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -5,8 +5,18 @@ import Foundation
 class TestItem:Element{
     lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
-
     override func resolveSkin() {
+        /*
+         var css:String = ""
+         css += "RepositoryDetailView{float:left;clear:left;}"
+         css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
+         css += "RepositoryDetailView #text2{fill:green;width:100%;height:48px;float:left;clear:left;}"
+         css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;fill:Blue;fill-alpha:1;}"
+         css += "RepositoryDetailView TextInput Text{width:100px;fill:yellow;fill-alpha:1;}"
+         css += "RepositoryDetailView TextInput TextArea{width:100%;fill:blue;fill-alpha:1;}"
+         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"
+         */
+        //StyleManager.addStyle(css)
         super.resolveSkin()
         _ = text1
         _ = text2
@@ -15,8 +25,8 @@ class TestItem:Element{
 
 class RepositoryDetailView:Element {
     lazy var contentContainer:Container = {return self.addSubView(Container(self.width,self.height,self,"lable"))}()
-    lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
-    lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
+    lazy var text1:TestItem = {return self.addSubView(TestItem(NaN,NaN,self))}()
+    
     //lazy var nameTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Name: ", "", self.contentContainer))}()
     //lazy var localPathTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Local-path: ", "", self.contentContainer))}()
     override func resolveSkin() {
