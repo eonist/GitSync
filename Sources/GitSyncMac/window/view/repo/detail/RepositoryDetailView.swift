b1a8df604d7a1cd61af38f89cffbba60c1e91044
Eonist <30n1st@gmail.com>
2017-05-18 19:34:51 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -3,12 +3,13 @@ import Foundation
 @testable import Element
 
 class RepositoryDetailView:Element {
-    lazy var container:Container = {return self.addSubView(Container(self.width,self.height,self,"lable"))}()
+    lazy var contentContainer:Container = {return self.addSubView(Container(self.width,self.height,self,"lable"))}()
     //lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     //lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
     lazy var nameTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Name: ", "", self.contentContainer))}()
     lazy var localPathTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Local-path: ", "", self.contentContainer))}()
     override func resolveSkin() {
+        /*
         var css:String = ""
         css += "RepositoryDetailView{float:left;clear:left;}"
         css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
@@ -17,9 +18,10 @@ class RepositoryDetailView:Element {
         css += "RepositoryDetailView TextInput Text{width:100px;fill:yellow;fill-alpha:1;}"
         css += "RepositoryDetailView TextInput TextArea{width:100%;fill:blue;fill-alpha:1;}"
         css += "RepositoryDetailView TextInput TextArea Text{width:100%;fill:purple;fill-alpha:1;}"
+        */
         //StyleManager.addStyle(css)
         super.resolveSkin()
-        _ = container
+        _ = contentContainer
         /*_ = text1
          _ = text2*/
         _ = nameTextInput
