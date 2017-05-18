44bf4207a979f3492b7e3cf4a87a2566255e5689
Eonist <30n1st@gmail.com>
2017-05-18 18:55:53 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -11,6 +11,7 @@ class RepositoryDetailView:Element {
         css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
         css += "RepositoryDetailView #text2{fill:green;width:100%;height:48px;float:left;clear:left;}"
         css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;}"
+        css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;}"
         StyleManager.addStyle(css)
         super.resolveSkin()
         _ = text1
