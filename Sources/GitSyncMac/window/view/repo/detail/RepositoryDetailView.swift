a3e9ed250d6e204ea10f1ba9c7926592cdf44f1b
Eonist <30n1st@gmail.com>
2017-05-18 18:53:09 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -5,15 +5,16 @@ import Foundation
 class RepositoryDetailView:Element {
     lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
     lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
-    lazy var nameTextInput:TextInput = {return self.contentContainer.addSubView(TextInput(self.width, 32, "Name: ", "", self.contentContainer))}()
+    lazy var nameTextInput:TextInput = {return self.addSubView(TextInput(self.width, 32, "Name: ", "", self))}()
     override func resolveSkin() {
         var css:String = "RepositoryDetailView{float:left;clear:left;}"
         css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
         css += "RepositoryDetailView #text2{fill:green;width:100%;height:48px;float:left;clear:left;}"
+        css += "RepositoryDetailView TextInput{width:100%;height:48px;float:left;clear:left;}"
         StyleManager.addStyle(css)
         super.resolveSkin()
         _ = text1
         _ = text2
-        _ = 
+        _ = nameTextInput
     }
 }
