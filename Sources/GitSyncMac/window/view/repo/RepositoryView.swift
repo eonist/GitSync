fa486762d0b11dcd977398285c807b8a91f735e8
Eonist <30n1st@gmail.com>
2017-05-18 18:33:42 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepositoryView.swift

--- Sources/GitSyncMac/window/view/repo/RepositoryView.swift
+++ Sources/GitSyncMac/window/view/repo/RepositoryView.swift
@@ -4,16 +4,16 @@ import Foundation
 
 class RepositoryView:Element{
     lazy var leftColumn:Element = {return self.addSubView(Element(NaN,self.height,self,"left"))}()
-    lazy var rightColumn:Section = {
-        return RepositoryDetailView//self.addSubView(Section(NaN,self.height,self,"right"))
+    lazy var detailView:RepositoryDetailView = {
+        return self.addSubView(RepositoryDetailView(NaN,self.height,self))//self.addSubView(Section(NaN,self.height,self,"right"))
     }()
     override func resolveSkin() {
         var css:String = "RepositoryView{float:left;clear:left;}"
         css += "RepositoryView #left{fill:blue;width:200px;float:left;clear:none;}"
-        css += "RepositoryView #right{fill:red;width:100%;float:left;clear:none;}"
+        css += "RepositoryView RepositoryDetailView{fill:red;width:100%;float:left;clear:none;}"
         StyleManager.addStyle(css)
         super.resolveSkin()
         _ = leftColumn
-        _ = rightColumn
+        _ = detailView
     }
 }
