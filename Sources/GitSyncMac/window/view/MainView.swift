102e45e33c234c69c04727ec92ed3ebac9f03ee2
Eonist <30n1st@gmail.com>
2017-05-18 12:17:10 +0200

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/window/view/MainView.swiftSources/GitSyncMac/window/view/repo/RepositoryView.swift

--- Sources/GitSyncMac/window/view/MainView.swift
+++ Sources/GitSyncMac/window/view/MainView.swift
@@ -20,7 +20,7 @@ class MainView:TitleView{
         //Sounds.startup?.play()
         MainWin.mainView = self
         menuView = addSubView(MenuView(frame.size.width,48,self))
-        Navigation.setView(Views.main(.repos))/*Adds the correct view to MainView*/
+        Navigation.setView(Views.main(.repository))/*Adds the correct view to MainView*/
     }
     required init(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
--- Sources/GitSyncMac/window/view/repo/RepositoryView.swift
+++ Sources/GitSyncMac/window/view/repo/RepositoryView.swift
@@ -7,8 +7,8 @@ class RepositoryView:Element{
     lazy var rightColumn:Element = {return self.addSubView(Element(NaN,NaN,self,"right"))}()
     override func resolveSkin() {
         var css:String = "RepositoryView{float:left;clear:left;}"
-        css += "RepositoryView #left{fill:blue;width:100;height:100;float:left;clear:none;}"
-        css += "RepositoryView #left{fill:red;width:100;height:100;float:left;clear:none;}"
+        css += "RepositoryView #left{fill:blue;width:100;height:100;float:left;clear:left;}"
+        css += "RepositoryView #left{fill:red;width:100;height:100;float:left;clear:left;}"
         StyleManager.addStyle(css)
         super.resolveSkin()
         _ = leftColumn
