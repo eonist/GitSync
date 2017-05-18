343e2d8beb9a501c38b0d9cbed41018438e08217
Eonist <30n1st@gmail.com>
2017-05-18 18:43:08 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepositoryDetailView.swift
@@ -3,12 +3,12 @@ import Foundation
 @testable import Element
 
 class RepositoryDetailView:Element {
-    lazy var text1:Element = {return self.addSubView(Element(NaN,self.height,self,"text1"))}()
-    lazy var text2:Element = {return self.addSubView(Element(NaN,self.height,self,"text2"))}()
+    lazy var text1:Element = {return self.addSubView(Element(NaN,NaN,self,"text1"))}()
+    lazy var text2:Element = {return self.addSubView(Element(NaN,NaN,self,"text2"))}()
     override func resolveSkin() {
         var css:String = "RepositoryDetailView{float:left;clear:left;}"
-        css += "RepositoryDetailView #text1{fill:orange;width:100%;float:left;clear:clear;}"
-        css += "RepositoryDetailView #text2{fill:green;width:100%;float:left;clear:clear;}"
+        css += "RepositoryDetailView #text1{fill:orange;width:100%;height:48px;float:left;clear:left;}"
+        css += "RepositoryDetailView #text2{fill:green;width:100%;height:48px;float:left;clear:left;}"
         StyleManager.addStyle(css)
         super.resolveSkin()
         _ = text1
