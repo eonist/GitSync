58a0697b8e31ccfa05a2c0455eb1352debd0c330
Eonist <30n1st@gmail.com>
2017-05-19 10:36:52 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/MainView.swift

--- Sources/GitSyncMac/window/view/MainView.swift
+++ Sources/GitSyncMac/window/view/MainView.swift
@@ -23,7 +23,7 @@ class MainView:TitleView{
         //Sounds.startup?.play()
         MainWin.mainView = self
         MainView.menuView = addSubView(MenuView(frame.size.width,48,self))
-        
+        _ = currentView
     }
     required init(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
