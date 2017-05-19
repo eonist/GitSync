a1886f7a1aff2448aa5c2d3b03ae2ec4cb9fe482
Eonist <30n1st@gmail.com>
2017-05-19 13:19:18 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepositoryView.swift

--- Sources/GitSyncMac/window/view/repo/RepositoryView.swift
+++ Sources/GitSyncMac/window/view/repo/RepositoryView.swift
@@ -2,6 +2,9 @@ import Foundation
 @testable import Element
 @testable import Utils
 
+//continue here: add an inner element in left column
+    //override setSize inside detailView
+
 class RepositoryView:Element{
     lazy var contentContainer:Container = {return self.addSubView(Container(self.width,self.height,self,"content"))}()
     lazy var leftColumn:Element = {return self.contentContainer.addSubView(Element(NaN,self.height,self.contentContainer,"left"))}()
