72b9e1df584bb9460ebfecf4349a465713302d1d
Eonist <30n1st@gmail.com>
2017-05-22 01:29:26 +0200

    Files modified: 1
    Modified 1 file:Sources/Pyramidifier.swift

--- Sources/Pyramidifier.swift
+++ Sources/Pyramidifier.swift
@@ -7,32 +7,23 @@ class Pyramidifier {
      * Aligns branch to the next branch (left to right)
      */
     static func align(_ tree:CustomTree){
-        var acumulated:[CustomTree] = []//Store accumalitve branches
+        var accumulated:[CustomTree] = []//Store accumalitve branches
         
         for i in tree.children.indices {
-            
             if (i < (tree.children.count - 1)) {//if not last branch
                 let child:CustomTree = tree.children[i]
                 let next:CustomTree = tree.children[i+1]
                 let maxX = Pyramidifier.maxX(child)//find maxX of branch
                 let minX = Pyramidifier.minX(next)//find minX of nextBranch
                 let items = CustomTree.flattened(child)
-                acumulated += items//accumulate all items
+                accumulated += items//accumulate all items
                 if maxX > minX {//if diff is negative
                     let diff = minX - maxX//find diff of min and max.
-                    acumulated.forEach{$0.pt.x += diff}//offset every item in branches with diff
+                    accumulated.forEach{$0.pt.x += diff}//offset every item in branches with diff
                 }
             }
         }
-        //for i in tree.children
-            //if not last branch
-                //child
-                //find maxX of branch
-                //find minX of nextBranch
-                //find diff of min and max.
-                //if diff is negative
-                    //offset every item in branches with diff 
-        
+       
     }
     /**
      * Returns max x position in a branch
