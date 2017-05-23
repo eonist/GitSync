e0a4ab98ec981ed09d8fdd43db8b3a38d25488dd
Eonist <30n1st@gmail.com>
2017-05-23 16:21:21 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/menu/CustomAboutMenu.swift

--- Sources/GitSyncMac/menu/CustomAboutMenu.swift
+++ Sources/GitSyncMac/menu/CustomAboutMenu.swift
@@ -1,5 +1,22 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
 class CustomAboutMenu {
-
+    static let about:String = "About"
+    static let preferences:String = "Preferences…"
+    static let appName:String = "GitSync"
+    private var preferencesMenuItem:NSMenuItem?
+    init(){
+        let appMenuItem = NSApp.mainMenu!.item(at: 0)//ref to App.menu
+        if(appMenuItem?.submenu?.item(withTitle:CustomAboutMenu.preferences) != nil){
+            //Swift.print("prefs menu created")
+            preferencesMenuItem = appMenuItem!.submenu!.item(withTitle:CustomAboutMenu.preferences)
+            let index:Int = appMenuItem!.submenu!.index(of:preferencesMenuItem!)
+            appMenuItem!.submenu!.removeItem(preferencesMenuItem!)
+            preferencesMenuItem = PreferencesMenuItem()
+            appMenuItem!.submenu!.insertItem(preferencesMenuItem!, at: index)
+        }
+    }
 }
+
