24cfdf6064c6684aeb965b2b797ac33514988058
Eonist <30n1st@gmail.com>
2017-05-23 12:36:42 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -184,6 +184,7 @@ class ViewMenu:CustomMenuItem {
     override func onSelect(event sender: AnyObject){
         Swift.print("ViewMenu.onSelect() " + "\(sender)")
     }
+    required init(coder decoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
 class ShowGridMenuItem:CustomMenuItem{
     static var isSideMenuHidden:Bool = false
