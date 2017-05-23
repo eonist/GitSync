52badfcaecdd5c6b9e150f7caa2e2eab912bf075
Eonist <30n1st@gmail.com>
2017-05-23 19:16:40 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/menu/view/items/ToggleMenuBarMenuItem.swift

--- Sources/GitSyncMac/menu/view/items/ToggleMenuBarMenuItem.swift
+++ Sources/GitSyncMac/menu/view/items/ToggleMenuBarMenuItem.swift
@@ -3,5 +3,25 @@ import Cocoa
 @testable import Element
 
 class ToggleMenuBarMenuItem:CustomMenuItem {
-    static var isSideMenuHidden:Bool = false
+    static var isMenuBarHidden:Bool = false
+    init() {super.init("Show menu bar", "m")}
+    
+    override func onSelect(event:AnyObject) {
+        Swift.print("ToggleMenuBarMenuItem.onSelect()")
+        
+        let toggle:Bool = !ToggleMenuBarMenuItem.isMenuBarHidden
+        ToggleMenuBarMenuItem.isMenuBarHidden = toggle//toggle
+        if let view = MainWin.mainView?.currentView as? RepositoryView {
+            view.toggleSideBar(toggle)
+        }
+    }
+    /**
+     * Return true if you want to enable the menu item, false will disable it
+     */
+    override func validateMenuItem(_ menuItem:NSMenuItem) -> Bool {
+        self.title = ShowSideBarMenuItem.isSideMenuHidden ? "Show side bar" : "Hide side bar"
+        Swift.print("validateMenuItem")
+        return true
+    }
+    required init(coder decoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
