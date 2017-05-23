8a0603c263c79a0069896db6b73072cd845c5180
Eonist <30n1st@gmail.com>
2017-05-23 15:26:15 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/menu/view/View.swift

--- Sources/GitSyncMac/menu/view/View.swift
+++ Sources/GitSyncMac/menu/view/View.swift
@@ -1,5 +1,36 @@
 import Foundation
 
-class View {
-
+class ViewMenu:CustomMenuItem {
+    init(){
+        super.init("View", "")
+        submenu = NSMenu(title: "View")
+        //submenu!.addMenuItem(ShowRulerMenuItem())
+        _ = submenu!.addMenuItem(ShowSideBarMenuItem())
+        //submenu!.addMenuItem(CustomMenuItem("Fullscreen",""))
+    }
+    override func onSelect(event sender: AnyObject){
+        Swift.print("ViewMenu.onSelect() " + "\(sender)")
+    }
+    required init(coder decoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+}
+class ShowSideBarMenuItem:CustomMenuItem{
+    static var isSideMenuHidden:Bool = false
+    init() {super.init("Show side bar", "l")}
+    
+    override func onSelect(event:AnyObject) {
+        Swift.print("ShowGridMenuItem.onSelect()")
+        //add hiding logic here
+        
+    }
+    /**
+     * Return true if you want to enable the menu item, false will disable it
+     */
+    override func validateMenuItem(_ menuItem: NSMenuItem) -> Bool {
+        self.title = ShowSideBarMenuItem.isSideMenuHidden ? "Show side bar" : "Hide side bar"
+        ShowSideBarMenuItem.isSideMenuHidden = !ShowSideBarMenuItem.isSideMenuHidden//toggle
+        Swift.print("validateMenuItem")
+        return true
+    }
+    required init(coder decoder: NSCoder) {fatalError("init(coder:) has not been implemented")}
+    
 }
