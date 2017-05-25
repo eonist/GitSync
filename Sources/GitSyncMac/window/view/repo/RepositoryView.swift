24ae27dcc4532473b73b62c9a88eadc42f7c5d6d
Eonist <30n1st@gmail.com>
2017-05-25 14:44:20 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/RepositoryView.swift

--- Sources/GitSyncMac/window/view/repo/RepositoryView.swift
+++ Sources/GitSyncMac/window/view/repo/RepositoryView.swift
@@ -56,6 +56,6 @@ class LeftSideBar:Element{
     }
     override func setSize(_ width: CGFloat, _ height: CGFloat) {
         super.setSize(width,height)
-        list.setSize(list.getWidth(), height)
+        list.setSize(list.getWidth(), self.getHeight())
     }
 }
