44240736641f9670ac7cf713493d79be8e8b8975
Eonist <30n1st@gmail.com>
2017-05-28 21:00:30 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
+++ GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
@@ -1,5 +1,17 @@
 import Foundation
 
 class StyleTestWin {
-
+    //customMenuBar
+    //Enum nav system
+    //use nice css colors
+    //use win with gd shadow
+    
+    //4 views:
+        //1. commitsView (repoBrowserView,commitListView,commitDetailView)
+        //2. repoView (repoBrowserView,repoDetailView)
+        //3. statsView (repoBRowserView,statsView)
+        //4. prefsView
+}
+class StyleTestView{
+    
 }
