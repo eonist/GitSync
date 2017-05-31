acebf24ce84feb2349dd2a4da894372fbb81a917
Eonist <30n1st@gmail.com>
2017-05-31 13:42:54 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
+++ GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
@@ -15,20 +15,10 @@ class StyleTestWin:Window {
 class StyleTestView:WindowView{
     override func resolveSkin(){
         Swift.print("StyleTestView")
-        
-        StyleManager.addStylesByURL("~/Desktop")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")
         super.resolveSkin()
         self.window?.title = "StyleTest"
         
     }
-    //customMenuBar
-    //Enum nav system
-    //use nice css colors (4 colors from FlexTest)
-    //use win with gd shadow
-    
-    //4 views:
-        //1. commitsView (repoBrowserView,commitListView,commitDetailView)
-        //2. repoView (repoBrowserView,repoDetailView)
-        //3. statsView (repoBRowserView,statsView)
-        //4. prefsView
+   
 }
