ba2e02d39eb910801cf6a7c57fd2f0871a7e2c9e
Eonist <30n1st@gmail.com>
2017-06-01 15:28:59 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/manager/Navigation.swift

--- Sources/GitSyncMac/manager/Navigation.swift
+++ Sources/GitSyncMac/manager/Navigation.swift
@@ -42,34 +42,34 @@ class Navigation {
         let w:CGFloat = size.w/*Convenience*/
         let h:CGFloat = size.h
         switch view{
-        case .main(let viewType):/*Main*/
-            switch viewType {
-                case .commits:
-                    return CommitsView(w,h,mainView)
-                case .repos:
-                    return RepoView(w,h,mainView)
-                case .stats:
-                    return StatsView(w,h,mainView)
-                case .prefs:
-                    return PrefsView(w,h,mainView)
-                case .repository:
-                    return RepositoryView(w,h,mainView)
-            }
-        case .commitDetail(let commitData):/*CommitDetail*/
-            let view:CommitDetailView = CommitDetailView(w,h,mainView)
-            view.setCommitData(commitData)
-            return view
-        case .repoDetail(let idx3d):/*RepoDetail*/
-            let view:RepoDetailView = RepoDetailView(w,h,mainView)
-            view.setRepoData(idx3d)
-            return view
-        case .dialog(let dialog):/*Dialogs*/
-            switch dialog{
-                case .commit:
-                    fatalError("not implemented yet")
-                case .conflict:
-                    return ConflictDialogView(w,h,mainView)
-            }
+            case .main(let viewType):/*Main*/
+                switch viewType {
+                    case .commits:
+                        return CommitsView(w,h,mainView)
+                    case .repos:
+                        return RepoView(w,h,mainView)
+                    case .stats:
+                        return StatsView(w,h,mainView)
+                    case .prefs:
+                        return PrefsView(w,h,mainView)
+                    case .repository:
+                        return RepositoryView(w,h,mainView)
+                }
+            case .commitDetail(let commitData):/*CommitDetail*/
+                let view:CommitDetailView = CommitDetailView(w,h,mainView)
+                view.setCommitData(commitData)
+                return view
+            case .repoDetail(let idx3d):/*RepoDetail*/
+                let view:RepoDetailView = RepoDetailView(w,h,mainView)
+                view.setRepoData(idx3d)
+                return view
+            case .dialog(let dialog):/*Dialogs*/
+                switch dialog{
+                    case .commit:
+                        fatalError("not implemented yet")
+                    case .conflict:
+                        return ConflictDialogView(w,h,mainView)
+                }
         }
     }
 }
