8edc9d573cc0138874366ffd78569f809744cea8
Eonist <30n1st@gmail.com>
2017-06-05 18:00:32 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
+++ GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
@@ -14,14 +14,14 @@ class StyleTestWin:Window {
     }
     required init?(coder: NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
-class StyleTestView:WindowView{
+class StyleTestView:CustomView{
     var main:Section?
     override func resolveSkin(){
         Swift.print("StyleTestView")
-        //StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")//StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")//StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")
         
         var css:String = ""
-        css += "#main{width:100%;height:100%;fill:silver;padding:0px;min-width:300px;max-width:500px;min-height:350px;max-height:700;}"
+        css += "#main{width:100%;height:100%;fill:silver;fill-alpha:0;padding:0px;min-width:300px;max-width:500px;min-height:350px;max-height:700;}"
         css += "#content{fill:yellow;width:calc(100% -80px);height:100%;float:left;}"
         //"#btn{fill:blue;width:100%;height:50;float:left;clear:left;}"//calc(100% -20px)
         
