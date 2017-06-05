551372fa3ac32ceced5192f710461a42ca011463
Eonist <30n1st@gmail.com>
2017-06-05 18:05:08 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/AppDelegate.swiftGitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -16,9 +16,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         Swift.print("GitSync - Automates git")//Simple git automation for macOS, The autonomouse git client,The future is automated
    
         //initApp()
-        //styleTest()
+        styleTest()
         //initTestWin()//🚧👷
-        initMinimalWin()
+        //initMinimalWin()
         //paddingTest()
         //calcTest()
         //StyleManager.addStyle("Button{width:calc(100% - 20px);}")
