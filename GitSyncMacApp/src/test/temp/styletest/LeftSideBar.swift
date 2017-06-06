914ee15d8136fe5fd1ca7b3333e81d71bc17d030
Eonist <30n1st@gmail.com>
2017-06-06 16:55:48 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/src/test/temp/styletest/LeftSideBar.swiftSources/GitSyncMac/manager/Nav.swift

--- GitSyncMacApp/src/test/temp/styletest/LeftSideBar.swift
+++ GitSyncMacApp/src/test/temp/styletest/LeftSideBar.swift
@@ -2,15 +2,16 @@ import Foundation
 @testable import Utils
 @testable import Element
 
-class LeftSideBar:Element{ 
+class LeftSideBar:Element{
+    var menuContainer:MenuContainer?
     override func resolveSkin() {
         var css:String = ""
         css += "#leftBar{fill:orange;fill-alpha:0;width:80px;height:100%;float:left;padding-top:26px;}"
         
         StyleManager.addStyle(css)
         super.resolveSkin()
-        let menuContainer = MenuContainer(NaN,NaN,self,"buttonSection")
-        self.addSubview(menuContainer)
+        self.menuContainer = self.addSubView(MenuContainer(NaN,NaN,self,"buttonSection"))
+        
     }
     
 }
