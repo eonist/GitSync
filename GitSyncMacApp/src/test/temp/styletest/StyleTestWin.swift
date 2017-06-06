3bfd5a3361729d483b4ee177567bfa5b5d985b02
Eonist <30n1st@gmail.com>
2017-06-06 17:17:47 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
+++ GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
@@ -34,11 +34,10 @@ class StyleTestView:CustomView{
         StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")//StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")
         
         var css:String = ""
-        css += "Section#titleBar{padding-top:8px;padding-left:14px;}"
         
         css += "#main{width:100%;height:100%;fill:silver;fill-alpha:0;padding:0px;min-width:300px;max-width:500px;min-height:350px;max-height:700;}"
         css += "#content{fill:yellow;width:calc(100% -80px);height:100%;float:left;}"
-        css += "Section#commit{fill:green;width:100%;height:100%;}"
+        css += "Section#commit{fill:white;width:100%;height:100%;}"
         css += "Section#repo{fill:maroon;width:100%;height:100%;}"
         css += "Section#prefs{fill:fuchsia;width:100%;height:100%;}"
         css += "Section#repoDetail{fill:teal;width:100%;height:100%}"
