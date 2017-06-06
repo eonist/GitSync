efa27e00fb8926c70c8a109d85d87de0980ee4da
Eonist <30n1st@gmail.com>
2017-06-06 12:19:37 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift

--- GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
+++ GitSyncMacApp/src/test/temp/styletest/StyleTestWin.swift
@@ -25,6 +25,9 @@ class StyleTestView:CustomView{
         
         css += "#main{width:100%;height:100%;fill:silver;fill-alpha:0;padding:0px;min-width:300px;max-width:500px;min-height:350px;max-height:700;}"
         css += "#content{fill:yellow;width:calc(100% -80px);height:100%;float:left;}"
+        css += "#commit{fill:green;width:100%,height:100%}"
+        css += "#repo{fill:maroon;width:100%,height:100%}"
+        css += "#repoDetail{fill:teal;width:100%,height:100%}"
         //"#btn{fill:blue;width:100%;height:50;float:left;clear:left;}"//calc(100% -20px)
         
         StyleManager.addStyle(css)
@@ -45,8 +48,10 @@ class StyleTestView:CustomView{
         let commitsView = content?.addSubView(Section(NaN,NaN,content,"commit"))
         _ = commitsView
         
-        let repoView = content?.addSubView(Section(NaN,NaN,content,"repo"))
-        _ = repoView
+        /*let repoView = content?.addSubView(Section(NaN,NaN,content,"repo"))
+         _ = repoView*/
+        
+        
         //let btn = section!.addSubView(Element(NaN,NaN,section,"btn"))
         //_ = btn
         
@@ -66,45 +71,5 @@ class StyleTestView:CustomView{
         super.setSize(width, height)
         Swift.print("StyleTestView.setSize w:\(width) h:\(height)")
         ElementModifier.refreshSize(main!)
-    }
-}
-class LeftSideBar:Element{
-    override func resolveSkin() {
-        var css:String = ""
-        css += "#leftBar{fill:orange;fill-alpha:0;width:80px;height:100%;float:left;padding-top:26px;}"
-        css += "#buttonSection {"
-        css +=     "width:100%;"
-        css +=     "height:100%;"
-        css +=     "padding-top:16px;"
-        css +=     "padding-left:28px;"
-        css += "}"
-        css += "#buttonSection SelectButton{"
-        css +=     "fill:green;"
-        css +=     "fill-alpha:0.3;"
-        css +=     "width:24;"
-        css +=     "height:24;"
-        css +=     "float:left;"
-        css +=     "clear:left;"
-        css +=     "margin-bottom:12px;"
-        css += "}"
-        css += "#buttonSection SelectButton:selected{"
-        css +=      "fill-alpha:0.6;"
-        css += "}"
-        StyleManager.addStyle(css)
-        super.resolveSkin()
-        createButtons()
-    }
-    func createButtons(){
-        let buttonSection = self.addSubView(Section(NaN,NaN,self,"buttonSection"))
-        let buttonTitles = ["commits","repos","settings"]
-        var buttons:[ISelectable] = []
-        for buttonTitle in buttonTitles{
-            buttons.append(buttonSection.addSubView(SelectButton(20,20,true,buttonSection,buttonTitle)))
-        }
-        let selectGroup = SelectGroup(buttons,buttons[0]);
-        func onSelect(event:Event){
-            //do something here
-        }
-        selectGroup.event = onSelect
     }
 }
