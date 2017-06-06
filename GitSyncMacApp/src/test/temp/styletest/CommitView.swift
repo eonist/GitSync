9e58a55a3c3a8653f45b224781b8afb0515f25af
Eonist <30n1st@gmail.com>
2017-06-06 17:54:01 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/CommitView.swift

--- GitSyncMacApp/src/test/temp/styletest/CommitView.swift
+++ GitSyncMacApp/src/test/temp/styletest/CommitView.swift
@@ -1,5 +1,41 @@
 import Foundation
+@testable import Element
+@testable import Utils
+@testable import GitSyncMac
 
-class CommitView {
-
+class CommitView:Element{
+    static var selectedIdx:Int = 1
+    /*static let w:CGFloat = MainView.w
+     static let h:CGFloat = MainView.h-48*/
+    //var topBar:CommitsTopBar?
+    lazy var list:CommitsList = {
+        let dp = CommitDPCache.read()/*Creates the dp based on cached data from previous app runs*/
+        let list = self.addSubView(CommitsList.init(CommitsView.w, CommitsView.h, CGSize(24,102), dp, self,"commitsList"))/*24 should be allowed to be nan no?*/
+        //⚠️️list!.selectAt(dpIdx: CommitsView.selectedIdx)
+        return list
+    }()
+    override func resolveSkin() {
+        self.skin = SkinResolver.skin(self)//super.resolveSkin()
+        //topBar = addSubView(CommitsTopBar(width-12,36,self))
+        //add a container
+        _ = list/*creates the GUI List*/
+    }
+    /**
+     * Eventhandler when a CommitsListItem is clicked
+     */
+    func onListSelect(_ event:ListEvent){
+        Swift.print("CommitsView.onListSelect()")
+        
+        //RepoView.selectedListItemIndex = list!.selectedIndex
+        CommitsView.selectedIdx = list.selectedIdx!
+        
+        Swift.print("event.index: " + "\(event.index)")
+        let commitData:[String:String] = list.dp.getItemAt(event.index)!
+        //(Navigation.currentView as! CommitDetailView).setCommitData(commitData)//updates the UI elements with the selected commit item
+        Navigation.setView(.commitDetail(commitData))
+    }
+    override func onEvent(_ event:Event) {
+        if(event.type == ListEvent.select){onListSelect(event as! ListEvent)}
+        //else {super.onEvent(event)}//forward other events
+    }
 }
