9d4498e02a4b476763252a74355aa83777bdc0cd
Eonist <30n1st@gmail.com>
2017-06-07 13:44:26 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift

--- Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift
+++ Sources/GitSyncMac/window/view/commits/detail/CommitDetailView.swift
@@ -6,17 +6,17 @@ import Foundation
  */
 class CommitDetailView:Element {
     lazy var repoNameText:Text = {
-        let repoNameText = self.addSubView(Text(120,20,"repo name",self,"repoName"))
+        let repoNameText = self.addSubView(Text(NaN,NaN,"repo name",self,"repoName"))
         repoNameText.isInteractive = false
         return repoNameText
     }()
     lazy var titleText:Text = {
-        let titleText = self.addSubView(Text(280,24,"commit title",self,"title"))
+        let titleText = self.addSubView(Text(NaN,24,"commit title",self,"title"))
         titleText.isInteractive = false
         return titleText
     }()
     lazy var descText:Text = {
-        let descText = self.addSubView(Text(480,220,"commit desc",self,"description"))
+        let descText = self.addSubView(Text(NaN,NaN,"commit desc",self,"description"))
         descText.isInteractive = false
         return descText
     }()
