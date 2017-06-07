6302c7ba95f10dacb6f79dc285f8db9febb0b9db
Eonist <30n1st@gmail.com>
2017-06-07 18:24:28 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/RepoView2.swift

--- GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
+++ GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
@@ -1,5 +1,16 @@
-import Foundation
-
-class RepoView2 {
-
+import Cocoa
+@testable import Utils
+@testable import Element
+@testable import GitSyncMac
+/**
+ * 
+ */
+class RepoView2:Element {
+    override func resolveSkin() {
+        super.resolveSkin()//self.skin = SkinResolver.skin(self)//
+        
+    }
+    override func getClassType() -> String {
+        return "\(RepoView.self)"
+    }
 }
