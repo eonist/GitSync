cd68610892f79c452d9cc59d614788d180ffa242
Eonist <30n1st@gmail.com>
2017-06-07 19:22:48 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/RepoView2.swift

--- GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
+++ GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
@@ -10,7 +10,7 @@ class RepoView2:Element {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
-        let list:List3 = self.addSubView(List3(getWidth(), getHeight(), CGSize(NaN,NaN), dp,.ver, self))
+        let list:List3 = self.addSubView(ElasticSlideScrollFastList3(getWidth(), getHeight(), CGSize(NaN,NaN), dp,.ver, self))
         list.selectAt(1)
     }
     override func getClassType() -> String {
