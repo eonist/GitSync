5f7ef9045b47a0cdbb512ce8f9923aadfca10438
Eonist <30n1st@gmail.com>
2017-06-07 14:56:26 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/CommitView.swift

--- GitSyncMacApp/src/test/temp/styletest/CommitView.swift
+++ GitSyncMacApp/src/test/temp/styletest/CommitView.swift
@@ -39,6 +39,9 @@ class CommitView:Element{
     }
     override func setSize(_ width: CGFloat, _ height: CGFloat) {
         Swift.print("CommitView.width: " + "\(width)")
+        Swift.print("list.frame.width: " + "\(list.frame.width)")
+        /*Swift.print("list.contentContainer.frame.width: " + "\(list.contentContainer.frame.width)")
+         Swift.print("list.rbContainer.frame.width: " + "\(list.rbContainer?.frame.width)")*/
         super.setSize(width, height)
     }
 }
