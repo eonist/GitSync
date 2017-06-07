64ca496887c0e5d100dafbf6ebbadeb91b3b64cf
Eonist <30n1st@gmail.com>
2017-06-07 18:43:35 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/RepoView2.swift

--- GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
+++ GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
@@ -8,6 +8,10 @@ import Cocoa
 class RepoView2:Element {
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
+        let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)
+        let dp:DataProvider = DataProvider(xml)
+        let list:List3 = self.addSubView(List3(getWidth(), getHeight(), CGSize(NaN,NaN), dp,.ver, self))
+        list.selectAt(1)
     }
     override func getClassType() -> String {
         return "\(RepoView.self)"
