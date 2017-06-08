d24cd05a9a666c36614164513a68fdc7e09d3b7e
Eonist <30n1st@gmail.com>
2017-06-08 18:01:16 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/RepoView2.swift

--- GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
+++ GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
@@ -17,6 +17,9 @@ class RepoView2:Element {
     }()
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
+        _ = backBtn
+        _ = forwardBtn
+        
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
         let list:ElasticSlideScrollFastList3 = self.addSubView(ElasticSlideScrollFastList3.init(getWidth(), getHeight(), CGSize(24,32), dp, self, "", .ver))
