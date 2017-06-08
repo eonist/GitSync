58b64e5748b4eeba1dcad1b4231d6fb51947ef00
Eonist <30n1st@gmail.com>
2017-06-08 17:51:51 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/RepoView2.swift

--- GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
+++ GitSyncMacApp/src/test/temp/styletest/RepoView2.swift
@@ -6,21 +6,23 @@ import Cocoa
  * 
  */
 class RepoView2:Element {
+    lazy var buttonSection:Section = {
+        
+    }
+    _ = buttonSection
     
+    let backBtn = buttonSection.addSubView(TextButton(NaN,NaN,"back",buttonSection,"back"))
+    _ = backBtn
+    
+    let forwardBtn = buttonSection.addSubView(TextButton(NaN,NaN,"forward",buttonSection,"forward"))
+    _ = forwardBtn
     
     override func resolveSkin() {
         super.resolveSkin()//self.skin = SkinResolver.skin(self)//
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
         
-        let buttonSection = addSubView(Section(NaN,NaN,self,"buttonSection"))
-        _ = buttonSection
         
-        let backBtn = buttonSection.addSubView(TextButton(NaN,NaN,"back",buttonSection,"back"))
-        _ = backBtn
-        
-        let forwardBtn = buttonSection.addSubView(TextButton(NaN,NaN,"forward",buttonSection,"forward"))
-        _ = forwardBtn
         
         let list:ElasticSlideScrollFastList3 = self.addSubView(ElasticSlideScrollFastList3.init(getWidth(), getHeight(), CGSize(24,32), dp, self, "", .ver))
         list.selectAt(1)
