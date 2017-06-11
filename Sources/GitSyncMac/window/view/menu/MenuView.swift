19f711566caffa61fabf734b4a0d8ef039c46b09
Eonist <30n1st@gmail.com>
2017-06-11 17:30:41 +0200

    Files modified: 1, Files deleted: 1
    Deleted 1 file:GitSyncMacApp/src/test/temp/styletest/MainView.swiftModified 1 file:Sources/GitSyncMac/window/view/menu/MenuView.swift

--- Sources/GitSyncMac/window/view/menu/MenuView.swift
+++ Sources/GitSyncMac/window/view/menu/MenuView.swift
@@ -1,5 +1,17 @@
 import Foundation
 
-class MenuView {
+import Foundation
+@testable import Utils
+@testable import Element
 
+class LeftSideBar:Element{
+    var menuContainer:MenuContainer?
+    override func resolveSkin() {
+        
+        super.resolveSkin()
+        self.menuContainer = self.addSubView(MenuContainer(NaN,NaN,self,"buttonSection"))
+        
+        
+        _ = self.addSubView(Element(NaN, NaN, self, "ruler"))
+    }
 }
