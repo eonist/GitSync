6c654520041ac2232fd33d63cf56695941f7b474
Eonist <30n1st@gmail.com>
2017-06-11 18:35:59 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -1,5 +1,16 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class RepoDetailView {
-
+class RepoDetailView:Element {
+    lazy var repoNameText:Text = {
+        let repoNameText = self.addSubView(Text(NaN,NaN,"repo name",self,"repoName"))
+        repoNameText.isInteractive = false
+        return repoNameText
+    }()
+    override func resolveSkin() {
+        Swift.print("RepoDetailView.resolveSkin()")
+        super.resolveSkin()// self.skin = SkinResolver.skin(self)//
+        _ = repoNameText
+    }
 }
