60779cd2e4b29d3fd5d72ec8eba20142076a94f2
Eonist <30n1st@gmail.com>
2017-06-11 16:34:34 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
     }
     func styleTest()  {
         NSApp.windows[0].close()
-        win = StyleTestWin(500,350)
+        win = StyleTestWin(550,300)
     }
     func initApp(){
         NSApp.windows[0].close()/*<--Close the initial non-optional default window*/
