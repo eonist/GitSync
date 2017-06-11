fe3632ff30e62944bf9d01c54b934b91df25b442
Eonist <30n1st@gmail.com>
2017-06-11 15:36:44 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/test/temp/styletest/CommitView.swift

--- GitSyncMacApp/src/test/temp/styletest/CommitView.swift
+++ GitSyncMacApp/src/test/temp/styletest/CommitView.swift
@@ -9,7 +9,7 @@ class CommitView:Element{
      static let h:CGFloat = MainView.h-48*/
     lazy var list:CommitsList = {
         let dp = CommitDPCache.read()/*Creates the dp based on cached data from previous app runs*/
-        let list = self.addSubView(CommitsList.init(/*use NaN here*/self.getWidth(), self.getHeight(), CGSize(24,102), dp, self,"commitsList"))/*24 should be allowed to be nan no?*/
+        let list = self.addSubView(CommitsList.init(/*use NaN here*/200/*self.getWidth()*/, self.getHeight(), CGSize(24,102), dp, self,"commitsList"))/*24 should be allowed to be nan no?*/
         //⚠️️list!.selectAt(dpIdx: CommitsView.selectedIdx)
         return list
     }()
