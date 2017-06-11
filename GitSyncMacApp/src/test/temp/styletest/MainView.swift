a365653b7f71e0926b21b4f8e3aff0615f337d7f
Eonist <30n1st@gmail.com>
2017-06-11 17:30:41 +0200

    New files added: 2, Files deleted: 2
    Added 1 file:Sources/GitSyncMac/window/view/MainView.swiftDeleted 2 files: GitSyncMacApp/src/test/temp/styletest/LeftSideBar.swiftGitSyncMacApp/src/test/temp/styletest/MenuContainer.swift

---
+++ GitSyncMacApp/src/test/temp/styletest/MainView.swift
@@ -0,0 +1,33 @@
+import Cocoa
+@testable import Utils
+@testable import Element
+/**
+ * TODO: Maybe make mainView into a lazy static prop similar to RepoView
+ */
+class StyleTestView:CustomView{
+    var main:Section?
+    static var content:Section?
+    static var currentView:Element?
+    static var leftbar:LeftSideBar?
+    
+    override func resolveSkin(){
+        Swift.print("StyleTestView")
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")//StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest.css")
+        
+        super.resolveSkin()
+        main = self.addSubView(Section(NaN,NaN,self,"main"))
+        
+        StyleTestView.leftbar = main!.addSubView(LeftSideBar(NaN,NaN,main,"leftBar"))
+        StyleTestView.content = main!.addSubView(Section(NaN,NaN,main,"content"))
+        Nav.setView(Views2.main(.commit))
+        
+    }
+    /**
+     * NOTE: gets calls from Window.didResize
+     */
+    override func setSize(_ width:CGFloat,_ height:CGFloat){
+        super.setSize(width, height)
+        //Swift.print("StyleTestView.setSize w:\(width) h:\(height)")
+        ElementModifier.refreshSize(main!)
+    }
+}
