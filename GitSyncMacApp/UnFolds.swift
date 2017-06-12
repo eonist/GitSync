619a298a507116093186375725165d1529346988
Eonist <30n1st@gmail.com>
2017-06-12 21:28:45 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/UnFolds.swift

--- GitSyncMacApp/UnFolds.swift
+++ GitSyncMacApp/UnFolds.swift
@@ -1,5 +1,30 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class UnFolds {
-
+extension TextInput{
+    /**
+     * New
+     */
+    static func unFold(_ dict:[String:Any]) -> TextInput{
+        //element tuple _ width:CGFloat, _ height:CGFloat, parent, id
+        //config tuple
+        return TextInput(NaN,NaN,"","",nil)
+    }
+    typealias TextInputConfig = (text:String, inputText:String)
+    convenience init(element:ElementConfig, config:TextInputConfig) {
+        self.init(element.width, element.height, config.text, config.inputText, element.parent, element.id)
+    }
+}
+extension Element{
+    typealias ElementConfig = (width:CGFloat, height:CGFloat, parent:IElement?, id:String?)
+    /**
+     * New
+     */
+    static func elementConfig(_ dict:[String:Any], _ parent:IElement? = nil) -> ElementConfig{
+        let width:CGFloat = UnFoldUtils.cgFloat(dict, "width")
+        let height:CGFloat = UnFoldUtils.cgFloat(dict, "height")
+        let id:String? = UnFoldUtils.string(dict, "id")
+        return (width:width,height:height,parent:parent,id:id)
+    }
 }
