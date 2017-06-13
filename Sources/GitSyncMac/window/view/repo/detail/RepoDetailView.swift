c958f53c6e94e30c71d9f4fc6c7354626f47b9f0
Eonist <30n1st@gmail.com>
2017-06-13 12:06:04 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift

--- Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
+++ Sources/GitSyncMac/window/view/repo/detail/RepoDetailView.swift
@@ -3,9 +3,48 @@ import Foundation
 @testable import Element
 
 class RepoDetailView:Element {
+    var nameTextInput:TextInput?
     override func resolveSkin() {
         Swift.print("RepoDetailView.resolveSkin()")
         super.resolveSkin()//self.skin = SkinResolver.skin(self)
-        _ = addSubView(TextInput(width, NaN, "Name: ", "Test", self,""))
+        nameTextInput = addSubView(TextInput(width, NaN, "Name: ", "Test", self,""))
+    }
+}
+extension RepoDetailView{
+    /**
+     *
+     */
+    func setRepoData(_ idx3d:[Int]){
+        RepoView.selectedListItemIndex = idx3d
+        //TODO: Use the RepoItem on the bellow line see AutoSync class for implementation
+        if let tree:Tree = RepoView.treeDP.tree[idx3d], let repoItemDict = tree.props{//NodeParser.dataAt(treeList!.node, selectedIndex)
+            var repoItem:RepoItem
+            if !tree.children.isEmpty {/*Support for folders*/
+                repoItem = RepoItem()
+                if let title:String = repoItemDict[RepoItemType.title] {repoItem.title = title}
+                if let active:String = repoItemDict[RepoItemType.active] {repoItem.active = active.bool}
+            }else{
+                repoItem = RepoUtils.repoItem(repoItemDict)
+            }
+            setRepoData(repoItem)
+        }
+    }
+    /**
+     * Populates the UI elements with data from the dp item
+     */
+    private func setRepoData(_ repoItem:RepoItem){
+        nameTextInput?.inputTextArea.setTextValue(repoItem.title)
+        /*localPathTextInput.inputTextArea.setTextValue(repoItem.localPath)
+         remotePathTextInput.inputTextArea.setTextValue(repoItem.remotePath)
+         branchTextInput.inputTextArea.setTextValue(repoItem.branch)
+         /*CheckButtons*/
+         uploadCheckBoxButton.setChecked(repoItem.upload)
+         downloadCheckBoxButton.setChecked(repoItem.download)
+         messageCheckBoxButton.setChecked(repoItem.autoCommitMessage)
+         intervalCheckBoxButton.setChecked(repoItem.autoSyncInterval)
+         autoSyncIntervalLeverSpinner.setValue(repoItem.interval.cgFloat)
+         activeCheckBoxButton.setChecked(repoItem.active)
+         pullCheckBoxButton.setChecked(repoItem.pullToAutoSync)
+         fileChangeCheckBoxButton.setChecked(repoItem.fileChange)*/
     }
 }
