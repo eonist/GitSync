9f1982697717eb6786403c75b15de109993bce7f
Eonist <30n1st@gmail.com>
2017-06-13 19:32:28 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/prefs/PrefsView.swift

--- Sources/GitSyncMac/window/view/prefs/PrefsView.swift
+++ Sources/GitSyncMac/window/view/prefs/PrefsView.swift
@@ -41,53 +41,5 @@ extension PrefsView{
     var darkMode:CheckBoxButton? {return self.element("darkMode")}
 }
 extension PrefsView{
-    static var xml:XML{
-        let xml:XML = "<prefs></prefs>".xml
-        /*xml.appendChild("<keychainUserName>\(PrefsView.keychainUserName!)</keychainUserName>".xml)
-         xml.appendChild("<gitConfigUserName>\(PrefsView.gitConfigUserName!)</gitConfigUserName>".xml)
-         xml.appendChild("<gitEmailName>\(PrefsView.gitEmailNameText!)</gitEmailName>".xml)
-         xml.appendChild("<uiSounds>\(String(PrefsView.uiSounds!))</uiSounds>".xml)*/
-        return xml
-    }
-    /*
-    var keychainUserNameTextInput:TextInput?
-    var gitConfigUserNameTextInput:TextInput?
-    var gitEmailNameTextInput:TextInput?
-    var defaultLocalPathTextInput:TextInput?
-    var uiSoundsCheckBoxButton:CheckBoxButton?
-    var darkModeCheckBoxButton:CheckBoxButton?
-    var autoSyncIntervalLeverSpinner:LeverSpinner?
-    */
+    
 }
-/*
-if(event.type == Event.update && event.immediate === keychainUserNameTextInput){
-    PrefsView.keychainUserName = (event as! TextFieldEvent).stringValue
-}else if(event.type == Event.update && event.immediate === gitConfigUserNameTextInput){
-    PrefsView.gitConfigUserName = (event as! TextFieldEvent).stringValue
-}else if(event.type == Event.update && event.immediate === gitEmailNameTextInput){
-    PrefsView.gitEmailNameText = (event as! TextFieldEvent).stringValue
-}else if(event.type == CheckEvent.check && event.immediate === uiSoundsCheckBoxButton){
-    PrefsView.uiSounds = (event as! CheckEvent).isChecked
-}
-*/
-/*
-//keychain-user-name (TextInput)
-keychainUserNameTextInput = addSubView(TextInput(width, NaN, "keychain user: ", PrefsView.keychainUserName!, self))
-//Git-Config-UserName
-gitConfigUserNameTextInput = addSubView(TextInput(width, NaN, "Git Config User: ", PrefsView.gitConfigUserName!, self))
-//Git-Config-EmailName
-gitEmailNameTextInput = addSubView(TextInput(width, NaN, "Git Config Email: ", PrefsView.gitEmailNameText!, self))
-//defaultLocalPath
-defaultLocalPathTextInput = addSubView(TextInput(width, NaN, "Local path: ", PrefsView.defaultLocalPath, self))
-//UI sounds [x]
-uiSoundsCheckBoxButton = addSubView(CheckBoxButton(width, NaN, "UI sounds: ", PrefsView.uiSounds!, self))
-//Dark mode:
-darkModeCheckBoxButton = addSubView(CheckBoxButton(width, NaN, "Dark mode: ", PrefsView.darkMode, self))
-//Auto-Sync interval:
-autoSyncIntervalLeverSpinner = addSubView(LeverSpinner(width, 32, "Sync-Interval: ", 30, 1, Int.min.cgFloat, Int.max.cgFloat, 0, 100, 200, self))//autoSyncIntervall needs to be a time setter: Day,Hour,Min,Seconds,0 means do not sync on an interval, the min setting is 30 sec, anything bellow this will be clamped to 30 sec
- 
- 
- /*PrefsView.gitEmailNameText = xml.firstNode("gitEmailName")!.stringValue
- PrefsView.uiSounds = xml.firstNode("uiSounds")!.stringValue!.bool*/
- /*PrefsView.keychainUserName = xml.firstNode("keychainUserName")!.stringValue*/
-*/
