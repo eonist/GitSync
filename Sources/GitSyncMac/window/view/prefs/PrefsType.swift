0ee206110b4db6d7991351360be3452abbe7950b
Eonist <30n1st@gmail.com>
2017-06-18 15:12:04 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/prefs/PrefsType.swift

--- Sources/GitSyncMac/window/view/prefs/PrefsType.swift
+++ Sources/GitSyncMac/window/view/prefs/PrefsType.swift
@@ -1,5 +1,15 @@
 import Foundation
 
-class PrefsType {
+typealias Prefs = (login:String,pass:String,local:String,darkMode:Bool)
 
+struct PrefsType {
+    static var prefs = "prefs"
+    static var login = "login"
+    static var pass = "pass"
+    static var local = "local"
+    static var darkMode = "darkMode"
+    static var width = "width"
+    static var height = "height"
+    static var x = "x"
+    static var y = "y"
 }
