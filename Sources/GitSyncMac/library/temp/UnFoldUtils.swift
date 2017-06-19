ad30040564d7832011e652d39ba2dbf3426713e0
Eonist <30n1st@gmail.com>
2017-06-19 10:59:58 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/temp/UnFoldUtils.swift

--- Sources/GitSyncMac/library/temp/UnFoldUtils.swift
+++ Sources/GitSyncMac/library/temp/UnFoldUtils.swift
@@ -25,7 +25,6 @@ class UnFoldUtils{
                 return TextButton.unFold(dict,parent)
             case type == "\(Text.self)":
                 return Text.unFold(dict,parent)
-                
             default:
                 fatalError("Type is not unFoldable: \(type)")
                 //return nil/*we return nil here instead of fatalError, as this method could be wrapped in a custom method to add other types etc*/
