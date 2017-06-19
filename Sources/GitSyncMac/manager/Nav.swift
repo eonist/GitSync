599906c39436d1a072a493d69f407da5f93b7258
Eonist <30n1st@gmail.com>
2017-06-19 23:36:33 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/manager/Nav.swift

--- Sources/GitSyncMac/manager/Nav.swift
+++ Sources/GitSyncMac/manager/Nav.swift
@@ -26,9 +26,8 @@ class Nav {
         guard let mainView:Element = StyleTestView.content else{fatalError("mainView is nil")}
         StyleTestView.leftbar?.menuContainer?.selectButton(viewType)/*Selects the correct menu icon*/
         if let curView = StyleTestView.currentView {curView.removeFromSuperview()}
-        let view = getView(viewType,mainView/*,size*/)
+        let view = getView(viewType,mainView/*size*/)
         StyleTestView.currentView = mainView.addSubView(view)
-        
     }
     private static func getView(_ view:Views2,_ mainView:Element)->Element{
         switch view{
