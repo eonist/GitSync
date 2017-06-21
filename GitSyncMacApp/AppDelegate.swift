3cc86ba04ffef3af79929072c8e5f7131294205c
Eonist <30n1st@gmail.com>
2017-06-21 23:52:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -21,7 +21,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         //continue here: 🏀
             //create a hidden state test for an element that you toggle on of with a button ✅
             //then create a container with 2 elements that can be hidden as well etc ✅
-            //also hide title buttons when you toggle, hide these in css or
+            //also hide title buttons when you toggle, hide these in css or 👈
             //the confirm buttons seems to be wrongly positioned in full mode, maybe a css bug?
         
         //stateTest()
