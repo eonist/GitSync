cf4beb4526dbe0d953afe75897320d0d06e992c3
Eonist <30n1st@gmail.com>
2017-06-27 18:07:33 +0200

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/window/view/MainWin.swiftSources/GitSyncMac/window/view/StyleTestView.swift

--- Sources/GitSyncMac/window/view/MainWin.swift
+++ Sources/GitSyncMac/window/view/MainWin.swift
@@ -3,7 +3,7 @@ import Cocoa
 @testable import Utils
 
 class StyleTestWin:Window {
-    static var view:Element?
+    //static var view:StyleTestView?
     required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
         super.init(docWidth, docHeight)
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
@@ -11,8 +11,8 @@ class StyleTestWin:Window {
         self.maxSize = CGSize(600,800)
     }
     override func resolveSkin() {
-        StyleTestWin.view = StyleTestView(frame.size.width,frame.size.height)/*⬅️️🚪*/
-        self.contentView = StyleTestWin.view!
+        self.contentView = StyleTestView(frame.size.width,frame.size.height)/*⬅️️🚪*/
+        // = StyleTestWin.view!
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
 }
