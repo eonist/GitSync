8a86c99475e7700eba193f2176d3b960e5094e11
Eonist <30n1st@gmail.com>
2017-07-08 14:51:53 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/GraphX.swift

--- GitSyncMacApp/src/graphx/GraphX.swift
+++ GitSyncMacApp/src/graphx/GraphX.swift
@@ -27,8 +27,15 @@ class GraphArea:Element{
     //createGraphLine
     //createGraphPoints
 }
+/**
+ * Rationale: We dont use list at first. Since Element is more optimized for UI with 1000s items etc atm. We can just mask it
+ * Rationale: grapharea should probably just progress this UI element
+ */
 class TimeBar:Element{
-    //
+    //timeLabels
+    override func resolveSkin() {
+        super.resolveSkin()
+    }
 }
 class ValueBar:Element{
     //var valueLables:[TextArea] = []
