8437f977d122a770da9fbbfdffa54ac156685b28
Eonist <30n1st@gmail.com>
2017-07-08 16:26:21 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/TimeBarX.swift

--- GitSyncMacApp/src/graphx/TimeBarX.swift
+++ GitSyncMacApp/src/graphx/TimeBarX.swift
@@ -1,5 +1,13 @@
 import Foundation
 
-class TimeBarX {
-
+/**
+ * Rationale: We dont use list at first. Since Element is more optimized for UI with 1000s items etc atm. We can just mask it
+ * Rationale: grapharea should probably just progress this UI element
+ */
+class TimeBar:Element{
+    //timeLabels
+    override func resolveSkin() {
+        super.resolveSkin()
+        //createTimeLables()
+    }
 }
