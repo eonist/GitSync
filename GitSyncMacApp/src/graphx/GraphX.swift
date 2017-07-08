b0fcd1cebe37d412be95b54181a0a4144d7673ba
Eonist <30n1st@gmail.com>
2017-07-08 14:20:49 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/GraphX.swift

--- GitSyncMacApp/src/graphx/GraphX.swift
+++ GitSyncMacApp/src/graphx/GraphX.swift
@@ -1,7 +1,18 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class GraphX {
-    
+class GraphX:Element{
+    override func resolveSkin() {
+        super.resolveSkin()
+        createUI()
+    }
+    /**
+     *
+     */
+    func createUI(){
+        
+    }
 }
 class TimeBar{
     
