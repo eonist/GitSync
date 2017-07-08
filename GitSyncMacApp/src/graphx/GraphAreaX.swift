96647fcd7d70163e76df931ad0b4e2df82af3405
Eonist <30n1st@gmail.com>
2017-07-08 21:57:06 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/GraphAreaX.swift

--- GitSyncMacApp/src/graphx/GraphAreaX.swift
+++ GitSyncMacApp/src/graphx/GraphAreaX.swift
@@ -43,5 +43,17 @@ class GraphAreaX:Element{
             graphPoint.setPosition($0)
         }
     }
+    /**
+     * Re-calc and set the graphPoint positions (for instance if the hValues has changed etc)
+     */
+    func updateGraph(_ vValues:[CGFloat]){
+        prevPoints = points.map{$0}//grabs the location of where the pts are now
+        let maxValue:CGFloat = vValues.max()!//Finds the largest number in among vValues
+        points = GraphUtils.points(CGSize(w,h), CGPoint(0,0), CGSize(100,100), vValues, maxValue,0,0)
+        /*GraphPoints*/
+        /*if(animator != nil){animator!.stop()}/*stop any previous running animation*/
+         animator = Animator(Animation.sharedInstance,0.5,0,1,interpolateValue,Quad.easeIn)
+         animator!.start()
+         */    }
 }
 
