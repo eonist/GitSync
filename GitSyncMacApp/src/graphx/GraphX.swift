b859f46b8f0a6d5dd33ac03c1a3ef9814da9fb06
Eonist <30n1st@gmail.com>
2017-07-08 16:35:59 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/GraphX.swift

--- GitSyncMacApp/src/graphx/GraphX.swift
+++ GitSyncMacApp/src/graphx/GraphX.swift
@@ -6,7 +6,7 @@ import Foundation
  * TODO: ⚠️️ Focus now should be to render the graphics with some debug colors
  * TODO: ⚠️️ Then draw the graphics to screen
  * TODO: ⚠️️ Then try to add gestures that manipulates the graph content
- * TODO: ⚠️️ Then try 
+ * TODO: ⚠️️ Then try to anim the graph at trigger integers
  */
 class GraphX:Element{
     override func resolveSkin() {
