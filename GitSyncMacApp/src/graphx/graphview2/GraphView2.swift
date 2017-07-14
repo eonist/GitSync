0f740814c4af411936663338469e317234034184
Eonist <30n1st@gmail.com>
2017-07-14 22:06:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/graphx/graphview2/GraphView2.swift

--- GitSyncMacApp/src/graphx/graphview2/GraphView2.swift
+++ GitSyncMacApp/src/graphx/graphview2/GraphView2.swift
@@ -57,7 +57,7 @@ extension GraphView2{
         let x:CGFloat = ScrollableUtils.scrollTo(progress, maskSize.w, contentSize.w)
         //Swift.print("x: " + "\(x)")
         disableAnim {
-            contentContainer?.x = x
+            contentContainer?.layer?.x = x
         }
         let absX = abs(x)/*Force the x value to be possitive*/
         if absX >= prevX + 100 {/*only redraw at every 100px threshold*/
