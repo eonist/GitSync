f064bf392b6922edda2d0584ff88f45d6b9875af
Eonist <30n1st@gmail.com>
2017-07-15 15:49:43 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/GraphScrollView.swift

--- GitSyncMacApp/src/GraphScrollView.swift
+++ GitSyncMacApp/src/GraphScrollView.swift
@@ -163,7 +163,6 @@ extension GraphScrollable {
             for (i,obj) in GraphAreaX.graphDots.enumerated() {
                 obj.layer?.position = positions[i]//positions the graphDots
             }
-            
         }
     }
     /**
@@ -191,7 +190,7 @@ extension GraphScrollable {
      */
     func onScrollWheelChange(_ event:NSEvent){/*Direct scroll*/
         //Swift.print("👻📜 (ElasticScrollable3).onScrollWheelChange : \(event.type) ")
-        moverGroup!.value += event.scrollingDelta/*directly manipulate the value 1 to 1 control*/
+        moverGroup!.value += event.scrollingDelta/*Directly manipulate the value 1 to 1 control*/
         moverGroup!.updatePosition(true)/*the mover still governs the resulting value, in order to get the displacement friction working*/
         let p:CGPoint = moverGroup!.result
         setProgress(p)
