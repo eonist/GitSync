4a395fa62524a5a14304dd7778cdcb7fcbba9fd9
Eonist <30n1st@gmail.com>
2017-07-15 17:28:09 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/GraphScrollView.swift

--- GitSyncMacApp/src/GraphScrollView.swift
+++ GitSyncMacApp/src/GraphScrollView.swift
@@ -102,22 +102,16 @@ extension GraphScrollable {
         
         prevMinY = minY//set the prev anim
     }
-    
-    //Continue here: 🏀
-        //make calcScaledPoints, and rename new and prev -> from and to
-    
     /**
-     *
+     * New
      */
-    func calcScaledPoints(_ x:CGFloat,_ minY:CGFloat){
-        if let prevMinY = self.prevMinY, prevMinY != minY {/*skips anim if the graph doesn't need to scale*/
-            //let dist:CGFloat = 400.cgFloat.distance(to: minY)
-            let diff:CGFloat = height + (-1 * minY)/*Since graphs start from the bottom we need to flip the y coordinates*/
-            let ratio:CGFloat = height / diff/*Now that we have the flipped y coordinate we can get the ratio to scale all other points with */
-            prevPoints = /*newPoints ??*/ points//basically use newPoints if they exist or default points if not
-            newPoints = points!.map{CGPointModifier.scale($0/*<--point to scale*/, CGPoint($0.x,height)/*<--pivot*/, CGPoint(1,ratio)/*<--Scalar ratio*/)}
-            initAnim()/*initiates the animation*/
-        }
+    func calcScaledPoints(_ x:CGFloat,_ minY:CGFloat) -> [CGPoint]{
+        //let dist:CGFloat = 400.cgFloat.distance(to: minY)
+        let diff:CGFloat = height + (-1 * minY)/*Since graphs start from the bottom we need to flip the y coordinates*/
+        let ratio:CGFloat = height / diff/*Now that we have the flipped y coordinate we can get the ratio to scale all other points with */
+        prevPoints = /*newPoints ??*/ points//basically use newPoints if they exist or default points if not
+        let scaledPoints = points!.map{CGPointModifier.scale($0/*<--point to scale*/, CGPoint($0.x,height)/*<--pivot*/, CGPoint(1,ratio)/*<--Scalar ratio*/)}
+        return scaledPoints
     }
     /**
      * New
