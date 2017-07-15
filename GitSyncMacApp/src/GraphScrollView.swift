dbff98ac4dbe57dee9b5b118fab043fb159d46e4
Eonist <30n1st@gmail.com>
2017-07-15 15:42:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/src/GraphScrollView.swift

--- GitSyncMacApp/src/GraphScrollView.swift
+++ GitSyncMacApp/src/GraphScrollView.swift
@@ -118,16 +118,14 @@ extension GraphScrollable {
     func initAnim(){
         if(animator == nil){
             Swift.print("Start anim")
-            animator = Animator(Animation.sharedInstance,1.0,0,1,interpolateValue,Elastic.easeOut)
+            animator = Animator(Animation.sharedInstance,1.0,0,1,interpolateValue,Elastic.easeInOut)
             animator?.start()
             animator?.event = self.onAnimEvent
         }else{
             Swift.print("add animation que")
-            animationCue = Animator(Animation.sharedInstance,1.0,0,1,interpolateValue,Elastic.easeOut)
+            animationCue = Animator(Animation.sharedInstance,1.0,0,1,interpolateValue,Elastic.easeInOut)
         }
     }
-    
-    //try it 🏀
     
     func onAnimEvent(_ event:Event)  {
         if event.type == AnimEvent.completed {
