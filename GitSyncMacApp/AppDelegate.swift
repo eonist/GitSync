1316d807b1111d9fee8d10b99ff508d3c4597285
Eonist <30n1st@gmail.com>
2017-07-18 16:53:10 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -53,8 +53,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         //Continue here: 🏀
             //try to use subclassing or decoration instead of generics. You need to be able to set the init vals outside the init call as well
             
-        let animator = Springer<CGPoint>(progress, (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0)),Springer.initPointConfig)/*setup Mover animator*/
+        let animator = Springer<CGPoint>(progress, (CGPoint(0,0),CGPoint(0,0),CGPoint(0,0),CGPoint(0,0)),(CGPoint(0.02,0.02),CGPoint(0.90,0.90)))/*setup Mover animator*/
         //setup click on window event handler
+        //Springer.initPointConfig
         //Springer.defaultInitPointValues,
         func onViewEvent(_ event:Event) {
             //Swift.print("onViewEvent: " + "\(event)")
