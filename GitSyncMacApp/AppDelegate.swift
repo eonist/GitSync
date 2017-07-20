21e2dc354a2cee5325f84280ae603aee3ff0a05d
Eonist <30n1st@gmail.com>
2017-07-20 18:15:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -35,6 +35,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         window.contentView = InteractiveView2()
         window.title = ""
         //scrollList
+        StyleManager.addStylesByURL("~/Desktop/ElCapitan/styletest/" + themeStr,true)
         let xml = FileParser.xml("~/Desktop/assets/xml/list.xml".tildePath)
         let dp:DataProvider = DataProvider(xml)
         let list:List3 = window.contentView!.addSubView(List3(140, 73, CGSize(NaN,NaN), dp,.ver, nil))
