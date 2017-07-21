f9646bedbe7fa1a0db47b9e7c4aeff4e1a9703a5
Eonist <30n1st@gmail.com>
2017-07-21 23:24:20 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -27,7 +27,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         horizontalListTest()
         
         
-        Animator2().onFrame { val in
+        Animator2().onFrameTick { val in
             Swift.print("val: " + "\(val)")
         }
         
