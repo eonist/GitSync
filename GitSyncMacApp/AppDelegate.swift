85e73e89ca12b21c8e0fdea7da0266792a48cf22
Eonist <30n1st@gmail.com>
2017-07-23 19:01:00 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -70,7 +70,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         }
         let anim3 = LoopAnimator2(initValues: (duration:0.5,from:0,to:1,repeatCount:3)){ value in
             disableAnim {
-                NSColor.blue.inter
+//                NSColor.blue.inter
                 ellipse.graphic.layer?.position = CGPoint(0,100*value)
             }
         }.onComplete {//this is the final complete call in the chain
