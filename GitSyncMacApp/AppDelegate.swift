64ed9a59597e5368cde38279db653f97f0dc9fec
Eonist <30n1st@gmail.com>
2017-07-24 00:07:15 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -36,7 +36,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
                 //simultaniouse anims etc, color and position at the same time etc ✅
                     //add modal box reveal, alpha, size, color, position
                     //try with out disableAnim
-                //do circle to roundRect z-zoom effect with bounce back and forth
+                //do circle to roundRect z-zoom effect, maybedropShadow increase, with bounce back and forth
                 //try to do a rotation test back and forth with elastic
             //add interuptabable animators to the fold
             //also add bgSleep(1.5){anim.start} aka a non blocking sleep method ✅
