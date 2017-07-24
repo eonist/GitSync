664fc62af364d18a12f59b7ecee636863032f388
Eonist <30n1st@gmail.com>
2017-07-24 19:28:42 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -51,12 +51,15 @@ class AppDelegate:NSObject, NSApplicationDelegate {
      *
      */
     func easer4Test(){
-        let initial = AnimationState(value: CGPoint(0.0,0.0), velocity: CGPoint(0.2,0.2), target: CGPoint(0,0), stopVelocity: CGPoint(0,0))
-        //        let target = AnimationState(value: CGPoint(0.0,0.0), velocity: CGPoint(0.2,0.2), target: CGPoint(100,100), stopVelocity: CGPoint(0,0))
-        //        let animator = Easer3(initial: initial, target: target, damping: CGPoint(0.2,0.2)) { (value:CGPoint) in
-        //            Swift.print("value: " + "\(value)")
-        //        }
-        //        animator.start()
+        //let initial = AnimationState(value: CGPoint(0.0,0.0), velocity: CGPoint(0.2,0.2), target: CGPoint(0,0), stopVelocity: CGPoint(0,0))
+        //let target = AnimationState(value: CGPoint(0.0,0.0), velocity: CGPoint(0.2,0.2), target: CGPoint(100,100), stopVelocity: CGPoint(0,0))
+        
+//        let animator = PointEaser(Easer4.initValues,Easer4.initConfig){ (value:CGPoint) in
+//            Swift.print("value: " + "\(value)")
+//        }
+//        animator.start()
+//        
+        
     }
     /**
      *
