06441faa905e9cf795bedbc607739041002d9bae
Eonist <30n1st@gmail.com>
2017-07-26 00:26:59 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/ForceTouchEvent.swift

--- GitSyncMacApp/temp/ForceTouchEvent.swift
+++ GitSyncMacApp/temp/ForceTouchEvent.swift
@@ -2,17 +2,17 @@ import Cocoa
 @testable import Utils
 @testable import Element
 /**
- * PARAM: stageChange: it would be useful when chaning color of a button for instance
+ * PARAM: stageChange: it will be useful when changing color of a button for instance
  */
 class ForceTouchEvent:Event{
-    static var clickDown:String = "forceTouchButtonClickDown"//from noStage to clickStage
-    static var deepClickDown:String = "forceTouchButtonDeepClickDown"//from clickStage to deepClickStage
-    static var clickUp:String = "forceTouchButtonClickUp"//from clickStage to noStage
-    static var deepClickUp:String = "forceTouchButtonDeepClickUp"//from deepStage to clickStage
-    /**/
-    static var pressureChange:String = "forceTouchButtonPressureChange"//fires all the time when pressure is applied
-    static var stageChange:String = "forceTouchButtonStageChange"//fires only when tranisitoning from 1 stage to the other
-    
+    static var clickDown:String = "forceTouchButtonClickDown"/*from noStage to clickStage*/
+    static var deepClickDown:String = "forceTouchButtonDeepClickDown"/*from clickStage to deepClickStage*/
+    static var clickUp:String = "forceTouchButtonClickUp"/*from clickStage to noStage*/
+    static var deepClickUp:String = "forceTouchButtonDeepClickUp"/*from deepStage to clickStage*/
+    /*Change*/
+    static var pressureChange:String = "forceTouchButtonPressureChange"/*fires all the time when pressure is applied*/
+    static var stageChange:String = "forceTouchButtonStageChange"/*fires only when tranisitoning from 1 stage to the other*/
+    /*Original NSEvent*/
     weak var event:NSEvent?
     init(_ type:String = "", _ origin:AnyObject,_ event:NSEvent){
         self.event = event
@@ -27,9 +27,7 @@ extension ForceTouchEvent {
         return event!.stage
     }
     /**
-     * NOTE: calculates the entire range of the stage pressures so from stage 0 to 1 the pressure goes from 0 to 0.5
-     * and from stage 1 to 2 the lienar pressure goes from 0.5 to 1
-     * this makes it easier to scale things in a linear fashion from 0 to 1 in the entire stage range
+     * NOTE: calculates the entire range of the stage pressures so from stage 0 to 1 the pressure goes from 0 to 0.5 and from stage 1 to 2 the lienar pressure goes from 0.5 to 1 this makes it easier to scale things in a linear fashion from 0 to 1 in the entire stage range
      */
     var linearPressure:CGFloat{
         if stage == 0 {
