0291ec14f910b906756c4d2bd89c5ccef022a851
Eonist <30n1st@gmail.com>
2017-07-25 20:23:46 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/ForceTouchButton.swift

--- GitSyncMacApp/temp/ForceTouchButton.swift
+++ GitSyncMacApp/temp/ForceTouchButton.swift
@@ -12,18 +12,19 @@ class ForceTouchButton:Button {
     override func pressureChange(with event: NSEvent) {
         let curStage:Int = event.stage
         if event.pressureBehavior == NSPressureBehavior.primaryDeepClick,prevStage != curStage {
-            if curStage == 0 && prevStage == 1 {
-                super.onEvent(ForceTouchEvent(ForceTouchEvent.clickUp,self,event))
-            }else if curStage == 1 && prevStage == 0{
-                super.onEvent(ForceTouchEvent(ForceTouchEvent.clickDown,self,event))
-            }else if curStage == 1 && prevStage == 2 {
-                super.onEvent(ForceTouchEvent(ForceTouchEvent.deepClickUp,self,event))
-            }else if curStage == 2 && prevStage == 1{
-                super.onEvent(ForceTouchEvent(ForceTouchEvent.deepClickDown,self,event))
+            switch (curStage,prevStage){
+                case (0,1):
+                    super.onEvent(ForceTouchEvent(ForceTouchEvent.clickUp,self,event))
+                case (1,0):
+                    super.onEvent(ForceTouchEvent(ForceTouchEvent.clickDown,self,event))
+                case (1,2):
+                    super.onEvent(ForceTouchEvent(ForceTouchEvent.deepClickUp,self,event))
+                case (2,1):
+                    super.onEvent(ForceTouchEvent(ForceTouchEvent.deepClickDown,self,event))
+                default: break;//isn't possible
             }
             super.onEvent(ForceTouchEvent(ForceTouchEvent.stageChange,self,event))
             prevStage = curStage//always set prevStage to curStage on stage change
-            
         }
         super.onEvent(ForceTouchEvent(ForceTouchEvent.pressureChange,self,event))
     }
