516962949b8cad254af47990770d72871d0ae24b
Eonist <30n1st@gmail.com>
2017-07-25 14:52:58 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -42,6 +42,28 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
 
             //button
+        
+        StyleManager.addStyle("#btn{fill:white;}")
+        let btn = window.contentView?.addSubView(Button(window.size.w,window.size.h,nil,"btn"))
+        
+        /*Ellipse*/
+        let ellipse = EllipseGraphic(-50,-50,100,100,FillStyle(.blue),nil)
+        window.contentView?.addSubview(ellipse.graphic)
+        ellipse.draw()
+        
+        func progress(value:CGPoint){/*This method gets called 60FPS, add the values to be manipulated here*/
+            disableAnim {/*Important so that you don't get the apple "auto" anim as well*/
+                ellipse.graphic.layer?.position = value/*We manipulate the layer because it is GPU accelerated as oppose to setting the view.position which is slow*/
+            }
+        }
+        let animator = PointEaser(progress, PointEaser.initValues,PointEaser.initConfig)/*Setup interuptable animator*/
+        func onViewEvent(_ event:Event) {/*This is the click on window event handler*/
+            if event.type == ButtonEvent.upInside {
+                animator.targetValue = btn!.localPos()/*Set the position of where you want the anim to go*/
+                if animator.stopped {animator.start()}/*We only need to start the animation if it has already stopped*/
+            }
+        }
+        btn?.event = onViewEvent
             //event handler for deep press
         
         //2. hardpress button to activate pop
