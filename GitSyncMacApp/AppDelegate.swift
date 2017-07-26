f885ede200f56426a05bfdbeec4810e71b815824
Eonist <30n1st@gmail.com>
2017-07-26 15:51:59 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -112,11 +112,13 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         btn.event = onViewEvent
             //event handler for deep press
         
-        //2. hardpress button to activate pop
+        //2. hardpress button to activate pop ✅
             //When hardpress state is lost then Ease to circular button again
-                
+        
         
         //3. ease circular button into bigger square modal, centered
+            //add support for CGSize and CGRect in Animator
+            //setup modalIdleRect /*basically when modal is in idle state */
         
         //4. spring modal in the .y axis to your mouse.position, offset by center
         
