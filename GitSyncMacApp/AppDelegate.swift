f19536ef4a2d38da3119fa8b56cd66fa91c6c6a3
Eonist <30n1st@gmail.com>
2017-07-26 21:26:57 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -76,8 +76,8 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         }()
         
         var style:Style = btn.skin!.style! as! Style
-        let animState:AnimState5<CGRect> = AnimState5<CGRect>.initRectValues
-        var modalEaser:Easer5<CGRect> = .init(animState, CGRect(0.2,0.2,0.2,0.2)) { value in
+        
+        var modalEaser:Easer5<CGRect> = .init(AnimState5<CGRect>.initRectValues, AnimState5<CGRect>.initRectConfig) { value in
             //anim rect here buttonRect to modalRect
             Swift.print("value: " + "\(value)")
         }
