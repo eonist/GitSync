8ef8a8b3e51a1e1a8dee8864a43a980ff5ea2225
Eonist <30n1st@gmail.com>
2017-07-29 19:04:04 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/ProtoTypeView.swift

--- GitSyncMacApp/temp/prototype/ProtoTypeView.swift
+++ GitSyncMacApp/temp/prototype/ProtoTypeView.swift
@@ -72,78 +72,12 @@ class ProtoTypeView:WindowView{
         StyleManager.addStyle("Window{fill:white;}")//padding-top:24px;
         super.resolveSkin()
         
-        let forceTouchHandler = { (_ event:ForceTouchEvent) in
-            //Swift.print("event.type: " + "\(event.type)")
-            if event.type == ForceTouchEvent.clickDown{
-                Swift.print("clickDown")
-                self.modalAnimator.setTargetValue(Modal.click).start()
-            }else if event.type == ForceTouchEvent.deepClickDown{
-                Swift.print("deepClickDown")
-                self.modalAnimator.setTargetValue(Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
-                self.onMouseDownMouseY  = self.window!.contentView!.localPos().y
-                NSEvent.addMonitor(&self.leftMouseDraggedMonitor,.leftMouseDragged,self.leftMouseDraggedClosure)
-            }else if event.type == ForceTouchEvent.clickUp {
-                Swift.print("clickUp")
-                if !self.modalStayMode {//modal stay
-                    self.modalAnimator.setTargetValue(Modal.initial).start()
-                }
-            }else if event.type == ForceTouchEvent.deepClickUp {
-                Swift.print("deepClickUp")
-                if self.modalStayMode {/*modal stay*/
-                    Swift.print("modal stay")
-                    self.modalBtn.removeHandler()
-                    self.modalAnimator.direct = false
-                    var rect = Modal.expanded
-                    rect.origin.y -= 30
-                    self.modalAnimator.setTargetValue(rect).start()
-                }else{/*modal leave*/
-                    Swift.print("modal leave")
-                    self.modalAnimator.direct = false
-                    self.modalAnimator.setTargetValue(Modal.initial).start()
-
-                    /*promptBtn*/
-                    self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start() //anim bellow screen
-                }
-                NSEvent.removeMonitor(&self.leftMouseDraggedMonitor)
-            }
-            if event.type == ForceTouchEvent.stageChange {/*when forcetouch changes state*/
-                let stage:Int = event.stage
-                Swift.print("stage: " + "\(stage)")
-                if stage == 0 {
-                    if !self.modalStayMode {
-                        StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.blue)
-                        Swift.print("override to blue")
-                    }
-                }else if stage == 1{
-                    if !self.modalStayMode && event.prevStage == 0{ //only change to red if prev stage was 0
-                        StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.red)
-                        Swift.print("override to red")
-                    }
-                    
-                }else /*if stage == 2*/{
-                    if !self.modalStayMode {
-                        StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.green)
-                        Swift.print("override to green")
-                    }
-                    
-                }
-            }
-            
-            disableAnim {
-                self.modalBtn.skin?.setStyle(self.style)
-            }
-        }
         
-        modalBtn.addHandler(forceTouchHandler)
         
-        /*Handler for promptBtn*/
-        promptBtn.addHandler(type:ButtonEvent.upInside) { (event:ButtonEvent) in
-            Swift.print("promptBtn.upInside")
-            self.modalAnimator.setTargetValue(Modal.initial).start()/*outro modal*/
-            self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start()/*outro promptBtn*/
-            self.modalBtn.addHandler(forceTouchHandler)//re-Added forcetoucheventhandler, ideally add this handler on outro complete
-            self.modalStayMode = false//release modalStayMode
-        }
+        
+        
+        
+        
         
         //1. Add some design elements (the goal today is to refactor, then add the design to the anim and make a gif with iphone template, and launch Animator, ElementiOS and update ElementMacOS, and write article aout prototyping with swift and playground)
             //create other concepts in illustrator
@@ -166,13 +100,76 @@ class ProtoTypeView:WindowView{
      *
      */
     func addEventHandlers(){
+        modalBtn.addHandler(forceTouchHandler)
         
-    }
-    /**
-     *
-     */
-    func promptButtonClickHandler(){
-        
+        promptBtn.addHandler(type:ButtonEvent.upInside) { (event:ButtonEvent) in /*Handler for promptBtn*/
+            Swift.print("promptBtn.upInside")
+            self.modalAnimator.setTargetValue(Modal.initial).start()/*outro modal*/
+            self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start()/*outro promptBtn*/
+            self.modalBtn.addHandler(self.forceTouchHandler)//re-Added forcetoucheventhandler, ideally add this handler on outro complete
+            self.modalStayMode = false//release modalStayMode
+        }
     }
     
+    lazy var forceTouchHandler = { (_ event:ForceTouchEvent) in
+        //Swift.print("event.type: " + "\(event.type)")
+        if event.type == ForceTouchEvent.clickDown{
+            Swift.print("clickDown")
+            self.modalAnimator.setTargetValue(Modal.click).start()
+        }else if event.type == ForceTouchEvent.deepClickDown{
+            Swift.print("deepClickDown")
+            self.modalAnimator.setTargetValue(Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
+            self.onMouseDownMouseY  = self.window!.contentView!.localPos().y
+            NSEvent.addMonitor(&self.leftMouseDraggedMonitor,.leftMouseDragged,self.leftMouseDraggedClosure)
+        }else if event.type == ForceTouchEvent.clickUp {
+            Swift.print("clickUp")
+            if !self.modalStayMode {//modal stay
+                self.modalAnimator.setTargetValue(Modal.initial).start()
+            }
+        }else if event.type == ForceTouchEvent.deepClickUp {
+            Swift.print("deepClickUp")
+            if self.modalStayMode {/*modal stay*/
+                Swift.print("modal stay")
+                self.modalBtn.removeHandler()
+                self.modalAnimator.direct = false
+                var rect = Modal.expanded
+                rect.origin.y -= 30
+                self.modalAnimator.setTargetValue(rect).start()
+            }else{/*modal leave*/
+                Swift.print("modal leave")
+                self.modalAnimator.direct = false
+                self.modalAnimator.setTargetValue(Modal.initial).start()
+                
+                /*promptBtn*/
+                self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start() //anim bellow screen
+            }
+            NSEvent.removeMonitor(&self.leftMouseDraggedMonitor)
+        }
+        if event.type == ForceTouchEvent.stageChange {/*when forcetouch changes state*/
+            let stage:Int = event.stage
+            Swift.print("stage: " + "\(stage)")
+            if stage == 0 {
+                if !self.modalStayMode {
+                    StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.blue)
+                    Swift.print("override to blue")
+                }
+            }else if stage == 1{
+                if !self.modalStayMode && event.prevStage == 0{ //only change to red if prev stage was 0
+                    StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.red)
+                    Swift.print("override to red")
+                }
+                
+            }else /*if stage == 2*/{
+                if !self.modalStayMode {
+                    StyleModifier.overrideStylePropVal(&self.style, ("fill",0), NSColor.green)
+                    Swift.print("override to green")
+                }
+                
+            }
+        }
+        
+        disableAnim {
+            self.modalBtn.skin?.setStyle(self.style)
+        }
+    }
 }
