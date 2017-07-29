9f1b23e1ef6014076a7ad0997edeaa85dc1ef00a
Eonist <30n1st@gmail.com>
2017-07-29 17:56:27 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/ProtoTypeView.swift

--- GitSyncMacApp/temp/prototype/ProtoTypeView.swift
+++ GitSyncMacApp/temp/prototype/ProtoTypeView.swift
@@ -44,6 +44,28 @@ class ProtoTypeView:WindowView{
     var modalStayMode:Bool = false//this is set to true if modal is released above a sertion threshold (modal.y < 30) threshold
     var leftMouseDraggedMonitor:Any?
     var onMouseDownMouseY:CGFloat = CGFloat.nan
+    //EventHandlers:
+        {_ in
+            let relativePos:CGFloat =  onMouseDownMouseY - self.window!.contentView!.localPos().y
+            //Swift.print("relativePos: " + "\(relativePos)")
+            var newRect = Modal.expanded
+            newRect.y -= relativePos
+            self.modalAnimator.direct = true
+            self.modalAnimator.setTargetValue(newRect).start()
+            if self.modalAnimator.value.y < 30  {//modal in stayMode
+                self.modalStayMode = true
+                Swift.print("reveal buttons: \(self.modalAnimator.value.y)")
+                var p = self.modalAnimator.value.bottomLeft
+                p.y += 15//add some margin
+                p.y = p.y.max(PromptButton.expanded.y)
+                //
+                self.promptBtnAnimator.setTargetValue(p).start()//you could do modalBtn.layer.origin + getHeight etc.
+            }else if self.modalAnimator.value.y > 30 {//modal in leaveMode
+                self.modalStayMode = false
+                Swift.print("anim buttons out")
+                self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start() //anim bellow screen
+            }
+    }
     
     override func resolveSkin(){
         Swift.print("ProtoTypeView.resolveSkin()")
