227029e6e7370deafa1032e2f062f74bc526a033
Eonist <30n1st@gmail.com>
2017-07-30 01:17:57 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/View.swift

--- GitSyncMacApp/temp/prototype/View.swift
+++ GitSyncMacApp/temp/prototype/View.swift
@@ -8,7 +8,7 @@ class ProtoTypeView:WindowView{
     lazy var promptBtn:Button = self.createPromptButton()
     /*Animation*/
     lazy var promptBtnAnimator:Easer5<CGPoint> = Easer5<CGPoint>(AnimState.PromptButton.initial, DefaultEasing.point,self.promptButtonAnim)
-    lazy var modalAnimator:ElasticEaser5 = ElasticEaser5(AnimState.Modal.initial, DefaultEasing.rect,Constraint.content,Constraint.mask,self.modalFrameAnim)
+    lazy var modalAnimator:ElasticEaser5 = ElasticEaser5(AnimState.Modal.initial, RoundedRect.DefaultEasing.easing,Constraint.content,Constraint.mask,self.modalFrameAnim)
     /*Values*/
     var modalStayMode:Bool = false/*this is set to true if modal is released above a sertion threshold (modal.y < 30) threshold*/
     var leftMouseDraggedMonitor:Any?/*Handler for dragging modal*/
