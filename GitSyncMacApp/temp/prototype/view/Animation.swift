0cc1c90369d0498646957f2d4c6486870ddbf205
Eonist <30n1st@gmail.com>
2017-07-30 01:20:49 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/Animation.swift

--- GitSyncMacApp/temp/prototype/view/Animation.swift
+++ GitSyncMacApp/temp/prototype/view/Animation.swift
@@ -14,18 +14,18 @@ extension ProtoTypeView {
     /**
      * Frame animation for modal (buttonRect to modalRect)
      */
-    func modalFrameAnim(rect:CGRect){
+    func modalFrameAnim(roundedRect:RoundedRect){
         //anim rect here
         //Swift.print("rect: " + "\(rect)")
         disableAnim {
-            StyleModifier.overrideStylePropVal(&self.style, ("width",0), rect.size.w)
-            StyleModifier.overrideStylePropVal(&self.style, ("height",0), rect.size.h)
+            StyleModifier.overrideStylePropVal(&self.style, ("width",0), roundedRect.size.w)
+            StyleModifier.overrideStylePropVal(&self.style, ("height",0), roundedRect.size.h)
 //            let fillet:CGFloat = (rect.size.w/2).max(0)
             //continue here, make RoundRect primitive and anim that
             
-            StyleModifier.overrideStylePropVal(&self.style, ("corner-radius",0), rect.size.w/2)
+            StyleModifier.overrideStylePropVal(&self.style, ("corner-radius",0), roundedRect.fillet.topLeft)
             self.modalBtn.skin?.setStyle(self.style)
-            self.modalBtn.layer?.position = rect.origin
+            self.modalBtn.layer?.position = roundedRect.origin
         }
     }
 
