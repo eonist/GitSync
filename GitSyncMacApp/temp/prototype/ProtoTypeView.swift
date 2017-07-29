2cb22fa54c481cc097c87333a913b6bd8b68aa15
Eonist <30n1st@gmail.com>
2017-07-29 13:34:20 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/ProtoTypeView.swift

--- GitSyncMacApp/temp/prototype/ProtoTypeView.swift
+++ GitSyncMacApp/temp/prototype/ProtoTypeView.swift
@@ -1,5 +1,18 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
-class ProtoTypeView {
-
+class StyleTestView:CustomView{
+    var main:Section?
+    static var content:Section?
+    static var currentView:Element?
+    
+    
+    override func resolveSkin(){
+        Swift.print("StyleTestView")
+        
+        super.resolveSkin()
+        main = self.addSubView(Section(NaN,NaN,self,"main"))
+        
+    }
 }
