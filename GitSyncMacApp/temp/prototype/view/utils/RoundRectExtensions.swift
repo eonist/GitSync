b135871bcbdeac3fd723a9135a0496652b78ef1d
Eonist <30n1st@gmail.com>
2017-07-30 00:46:04 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/temp/prototype/Constants.swiftGitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift

--- GitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift
+++ GitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift
@@ -1,9 +1,9 @@
 import Foundation
 @testable import Utils
 
-extension RoundRect:Advancable5 {
-    static var defaults:AnimState5<RoundRect>  =  AnimState5<RoundRect>(RoundRect(), RoundRect(), RoundRect(), RoundRect(), RoundRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
-    func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
-        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
-    }
-}
+//extension RoundRect:Advancable5 {
+//    static var defaults:AnimState5<RoundRect>  =  AnimState5<RoundRect>(RoundRect(), RoundRect(), RoundRect(), RoundRect(), RoundRect(10e-5,10e-5,10e-5,10e-5,Fillet()))
+//    func isNear( value:  CGRect,  epsilon: CGRect) -> Bool {
+//        return self.size.isNear(value.size,epsilon.size.w) && self.origin.isNear(value.origin,epsilon.origin.x)
+//    }
+//}
