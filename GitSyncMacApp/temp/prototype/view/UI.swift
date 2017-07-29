4b552674b0ab77609cb6d2a298a63b8100631db9
Eonist <30n1st@gmail.com>
2017-07-29 20:04:53 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/UI.swift

--- GitSyncMacApp/temp/prototype/view/UI.swift
+++ GitSyncMacApp/temp/prototype/view/UI.swift
@@ -1,5 +1,31 @@
-import Foundation
-
-class UI {
-
+import Cocoa
+@testable import Utils
+@testable import Element
+/**
+ * Constants
+ */
+extension ProtoTypeView {
+    /**
+     * Returns Prompt Button
+     */
+    func createPromptButton() -> Button{
+        var css:String = ""
+        css += "Button#prompt{width:\(PromptButton.initial.size.w)px;height:\(PromptButton.initial.size.h);fill:purple;corner-radius:20px;clear:none;float:none;}"
+        css += "Button#prompt:down{fill:grey;}"
+        
+        StyleManager.addStyle(css)
+        
+        let btn = self.addSubView(Button(Modal.initial.w,Modal.initial.h,nil,"prompt"))
+        btn.layer?.position = PromptButton.initial.origin//out of view
+        return btn
+    }
+    /**
+     * Returns Modal UI
+     */
+    func createModal() -> ForceTouchButton{
+        StyleManager.addStyle("Button#modalBtn{width:\(Modal.initial.size.w)px;height:\(Modal.initial.size.h)px;fill:blue;corner-radius:20px;clear:none;float:none;}")
+        let btn = self.addSubView(ForceTouchButton(Modal.initial.size.w,Modal.initial.size.h,nil,"modalBtn"))
+        btn.point = Modal.initial.origin//center button
+        return btn
+    }
 }
