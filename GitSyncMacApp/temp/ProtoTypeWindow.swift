815520e9d7013e626dda52a99500173d2fb3a0fd
Eonist <30n1st@gmail.com>
2017-07-29 13:55:29 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/ProtoTypeWindow.swift

--- GitSyncMacApp/temp/ProtoTypeWindow.swift
+++ GitSyncMacApp/temp/ProtoTypeWindow.swift
@@ -6,10 +6,11 @@ class ProtoTypeWindow:Window {
     static var view:ProtoTypeView?
     required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
         super.init(docWidth, docHeight)
+        self.title = ""
+        self.styleMask = [.borderless, .resizable, .fullSizeContentView]/*represents the window attributes*/
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
     }
     override func resolveSkin() {
-        self.title = ""
         self.contentView = ProtoTypeView(frame.size.width,frame.size.height)/*⬅️️🚪*/
     }
     required init?(coder:NSCoder) {fatalError("init(coder:) has not been implemented")}
