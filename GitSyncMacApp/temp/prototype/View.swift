b809ed7f0181892b6eb9c854a0042b910d122fa7
Eonist <30n1st@gmail.com>
2017-07-29 23:29:20 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/View.swift

--- GitSyncMacApp/temp/prototype/View.swift
+++ GitSyncMacApp/temp/prototype/View.swift
@@ -40,6 +40,7 @@ class ProtoTypeView:WindowView{
 
 //Continue here 🏀
     //make the modalButton round at init 👈
+    //get rid of disableAnim
     //animate the fillet
     //move the constraineter into a util class
     //make extension for Element to manipulate size and color more easily
