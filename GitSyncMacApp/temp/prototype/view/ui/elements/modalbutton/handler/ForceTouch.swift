7d16aa4dbb0e11747708c591a957faae03139496
Eonist <30n1st@gmail.com>
2017-07-30 21:30:22 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/handler/ForceTouch.swiftGitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/handler/LeftMouseDrag.swift

--- GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/handler/ForceTouch.swift
+++ GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/handler/ForceTouch.swift
@@ -1,5 +1,73 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
-class ForceTouch {
-
+extension ModalButton{
+    /**
+     * ForceTouch handler for modal
+     */
+    func forceTouchHandler(_ event:ForceTouchEvent)  {
+        //let indexOfModal:Int = self.indexOfModal(event.origin)
+        //Swift.print("event.type: " + "\(event.type)")
+        if event.type == ForceTouchEvent.clickDown{//TODO: use switch
+            clickDown()
+        }else if event.type == ForceTouchEvent.clickUp {
+            clickUp()
+        }else if event.type == ForceTouchEvent.deepClickDown{
+            deepClickDown()
+        }else if event.type == ForceTouchEvent.deepClickUp {
+            deepClickUp()
+        }
+        if event.type == ForceTouchEvent.stageChange {
+            stageChange(event)
+        }
+    }
+    private func clickDown(){
+        Swift.print("clickDown")
+        modalAnimator.setTargetValue(ProtoTypeView.Modal.click).start()
+    }
+    private func clickUp(){
+        Swift.print("clickUp")
+        if !ProtoTypeView.shared.modalStayMode {//modal stay
+            modalAnimator.setTargetValue(ProtoTypeView.Modal.initial).start()
+        }
+    }
+    private func deepClickDown(){
+        Swift.print("deepClickDown")
+        modalAnimator.setTargetValue(ProtoTypeView.Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
+        ProtoTypeView.shared.onMouseDownMouseY  = self.window!.contentView!.localPos().y
+        NSEvent.addMonitor(&ProtoTypeView.shared.leftMouseDraggedMonitor,.leftMouseDragged,ProtoTypeView.shared.leftMouseDraggedClosure)
+    }
+    private func deepClickUp(){
+        Swift.print("deepClickUp")
+        if ProtoTypeView.shared.modalStayMode {/*modal stay*/
+            Swift.print("modal stay")
+            ProtoTypeView.shared.modalBtn.removeHandler()
+            modalAnimator.direct = false
+            var rect = ProtoTypeView.Modal.expanded
+            rect.origin.y -= 30
+            modalAnimator.setTargetValue(rect).start()
+        }else{/*modal leave*/
+            Swift.print("modal leave")
+            modalAnimator.direct = false
+            modalAnimator.setTargetValue(ProtoTypeView.Modal.initial).start()
+            /*promptBtn*/
+            ProtoTypeView.shared.promptBtnAnimator.setTargetValue(ProtoTypeView.PromptButton.initial.origin).start() //anim bellow screen
+        }
+        NSEvent.removeMonitor(&ProtoTypeView.shared.leftMouseDraggedMonitor)
+    }
+    /**
+     * when forcetouch changes state
+     */
+    private func stageChange(_ event:ForceTouchEvent){
+        let stage:Int = event.stage
+        //Swift.print("stage: " + "\(stage)")
+        if stage == 0 && !ProtoTypeView.shared.modalStayMode{
+            ProtoTypeView.shared.modalBtn.setAppearance(ProtoTypeView.Colors.Modal.initial)
+        }else if stage == 1 && !ProtoTypeView.shared.modalStayMode && event.prevStage == 0{//only change to red if prev stage was 0
+            ProtoTypeView.shared.modalBtn.setAppearance(ProtoTypeView.Colors.Modal.click)
+        }else if stage == 2 && !ProtoTypeView.shared.modalStayMode{
+            ProtoTypeView.shared.modalBtn.setAppearance(ProtoTypeView.Colors.Modal.expanded)
+        }
+    }
 }
