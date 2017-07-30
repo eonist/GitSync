9a9f68f0fc41303db1ba4b151f88bb5b1705b451
Eonist <30n1st@gmail.com>
2017-07-30 21:54:28 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/Sizes.swift

--- GitSyncMacApp/temp/prototype/view/ui/Sizes.swift
+++ GitSyncMacApp/temp/prototype/view/ui/Sizes.swift
@@ -6,28 +6,7 @@ extension ProtoTypeView {
         static let size:CGSize = CGSize(200,355)//IPhone 7: (750 x 1334) (375 x 667) ≈ (200x355)
         static let point:CGPoint = CGPoint(0,0)
     }
-    enum Modal {
-        static let svgSize:CGSize = CGSize(50,50)//the graphic
-        static let initial:RoundedRect/*CGRect*/ = {//init modal btn size
-            let size:CGSize = CGSize(100,100)
-            let p:CGPoint = Grid.position(1,size)//Align.alignmentPoint(size, WinRect.size, Alignment.centerCenter, Alignment.centerCenter)
-            let fillet:CGFloat = 50
-            return RoundedRect(p,size,fillet)
-            //return CGRect(p,size)
-        }()
-        static let click:RoundedRect = {//when modalBtn is pressed down
-            let size:CGSize = Modal.initial.size * 0.75
-            let p:CGPoint = Grid.position(1,size)//Align.alignmentPoint(size, WinRect.size, Alignment.centerCenter, Alignment.centerCenter)
-            let fillet:CGFloat = Modal.initial.fillet * 0.75
-            return RoundedRect(p,size,fillet)
-        }()
-        static let expanded:RoundedRect = {//when modal is in expanded mode
-            let size = CGSize(WinRect.size.w,WinRect.size.w) - CGSize(40,0)
-            let p:CGPoint = Align.alignmentPoint(size, WinRect.size, Alignment.centerCenter, Alignment.centerCenter)
-            let fillet:CGFloat = 20
-            return RoundedRect(p,size,fillet)
-        }()
-    }
+    
     enum PromptButton {
         static let initial:CGRect = {
             let size:CGSize = CGSize(Modal.expanded.size.w,45)
