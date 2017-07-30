f59c35c9858b7af2c41e9219c4dd478f4e2fcd14
Eonist <30n1st@gmail.com>
2017-07-30 17:17:24 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/Colors.swift

--- GitSyncMacApp/temp/prototype/view/ui/Colors.swift
+++ GitSyncMacApp/temp/prototype/view/ui/Colors.swift
@@ -1,5 +1,23 @@
-import Foundation
+import Cocoa
+@testable import Utils
 
-class Colors {
-
+extension ProtoTypeView{
+    enum Colors{
+        enum Modal {
+            static let initial:NSColor = "#66CDAD".nsColor
+            static let click:NSColor = "#555555".nsColor
+            static let expanded:NSColor = "#66CDAD".nsColor
+        }
+        enum PromptButton {
+            enum Background{
+                static let idle:NSColor = "#DDDDDD".nsColor
+                static let down:NSColor = "#66CDAD".nsColor
+            }
+            enum Text{
+                static let idle:NSColor = "#555555".nsColor
+                static let down:NSColor = "#FFFFFF".nsColor
+                
+            }
+        }
+    }
 }
