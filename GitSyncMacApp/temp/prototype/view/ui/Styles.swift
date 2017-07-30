868c5481e479900aa18dda3f293a44b1a384c5aa
Eonist <30n1st@gmail.com>
2017-07-31 01:40:01 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/Styles.swift

--- GitSyncMacApp/temp/prototype/view/ui/Styles.swift
+++ GitSyncMacApp/temp/prototype/view/ui/Styles.swift
@@ -59,7 +59,7 @@ extension ProtoTypeView{
                 css += "multiline:false;"
                 css += "}"
                 /*TextButton down Text*/
-                css += "TextButton#prompt:down:overText:down:over{"
+                css += "TextButton#prompt:down:over Text:down:over{"
                 css += "color:yellow;"//#\(Colors.PromptButton.Text.down.hexString)
                 css += "}"
                 return css
