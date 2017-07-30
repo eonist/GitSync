0fd24e1c8e30f2f88e96bba2bafd656ab30d11da
Eonist <30n1st@gmail.com>
2017-07-30 16:14:19 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift

--- GitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift
+++ GitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift
@@ -7,13 +7,17 @@ extension ElementKind {
      *
      */
     func setAppearance(_ roundedRect:RoundedRect){
-//        var style:Style = style
         var style:Style = self.skin!.style as! Style
         StyleModifier.overrideStylePropVal(&style, ("width",0), roundedRect.w)
         StyleModifier.overrideStylePropVal(&style, ("height",0), roundedRect.h)
+        StyleModifier.overrideStylePropVal(&style, ("margin-top",1), (roundedRect.w/2)-50)
+        StyleModifier.overrideStylePropVal(&style, ("margin-left",1), (roundedRect.h/2)-50)
         StyleModifier.overrideStylePropVal(&style, ("corner-radius",0), roundedRect.fillet)
         self.skin?.setStyle(style)
         self.layer?.position = roundedRect.origin
+    }
+    func setApperance(_ margin:Margin){
+    
     }
     func setAppearance(_ fill:NSColor){
         var style:Style = self.skin!.style as! Style
