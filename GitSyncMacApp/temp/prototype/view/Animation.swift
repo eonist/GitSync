893346dd840c1772497540dd90fe4fc29fba82f0
Eonist <30n1st@gmail.com>
2017-07-30 12:33:55 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/Animation.swift

--- GitSyncMacApp/temp/prototype/view/Animation.swift
+++ GitSyncMacApp/temp/prototype/view/Animation.swift
@@ -20,11 +20,6 @@ extension ProtoTypeView {
         Swift.print("roundedRect.w: " + "\(roundedRect.w)")
         disableAnim {
             self.modalBtn.setAppearance(&self.style, roundedRect)
-//            StyleModifier.overrideStylePropVal(&self.style, ("width",0), roundedRect.w)
-//            StyleModifier.overrideStylePropVal(&self.style, ("height",0), roundedRect.h)
-//            StyleModifier.overrideStylePropVal(&self.style, ("corner-radius",0), roundedRect.fillet)
-//            self.modalBtn.skin?.setStyle(self.style)
-//            self.modalBtn.layer?.position = roundedRect.origin
         }
     }
 }
