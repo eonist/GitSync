624aa71b848258e47b369694c61ba0939b270e57
Eonist <30n1st@gmail.com>
2017-07-31 01:24:53 +0200

    Files modified: 2
    Modified 2 files:GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/handler/ForceTouch.swiftGitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift

--- GitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift
+++ GitSyncMacApp/temp/prototype/view/utils/ElementExtensions.swift
@@ -22,6 +22,7 @@ extension ElementKind {
     }
     func setAppearance(_ color:NSColor,_ depth:Int = 0){
         var style:Style = self.skin!.style as! Style
+        StyleModifier.overrideStylePropVal(&style, ("fill-alpha",depth), color.alphaComponent)
         StyleModifier.overrideStylePropVal(&style, ("fill",depth), color)
         self.skin?.setStyle(style)
     }
