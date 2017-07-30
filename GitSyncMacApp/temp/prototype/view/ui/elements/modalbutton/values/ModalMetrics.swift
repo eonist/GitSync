6ea1bc1dcf4770eaa7683446c48ea9ff5fb439f9
Eonist <30n1st@gmail.com>
2017-07-30 23:49:32 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/values/ModalMetrics.swift

--- GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/values/ModalMetrics.swift
+++ GitSyncMacApp/temp/prototype/view/ui/elements/modalbutton/values/ModalMetrics.swift
@@ -3,18 +3,20 @@ import Foundation
 @testable import Element
 
 enum Modal {
-    static let svgSize:CGSize = CGSize(50,50)//the graphic
+    static let svgSize:CGSize = {
+        return CGSize(initial().size.w/2,initial().size.h/2)//the graphic
+    }()
     static func initial(_ i:Int = 0) -> RoundedRect /*CGRect*/  {//init modal btn size
-        let size:CGSize = CGSize(100,100)
+        let size:CGSize = CGSize(80,80)
         let p:CGPoint = ProtoTypeView.Grid.position(i,size)//Align.alignmentPoint(size, WinRect.size, Alignment.centerCenter, Alignment.centerCenter)
-        let fillet:CGFloat = 50
+        let fillet:CGFloat = size.w/2
         return RoundedRect(p,size,fillet)
         //return CGRect(p,size)
     }
     static func click(_ i:Int) -> RoundedRect {//when modalBtn is pressed down
-        let size:CGSize = Modal.initial().size * 0.75
+        let size:CGSize = Modal.initial().size * 1.25
         let p:CGPoint = ProtoTypeView.Grid.position(i,size)//Align.alignmentPoint(size, WinRect.size, Alignment.centerCenter, Alignment.centerCenter)
-        let fillet:CGFloat = Modal.initial().fillet * 0.75
+        let fillet:CGFloat = Modal.initial().fillet * 1.25
         return RoundedRect(p,size,fillet)
     }
     static let expanded:RoundedRect = {//when modal is in expanded mode
