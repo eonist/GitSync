6e8174431e260959909a0202dbbb017162510776
Eonist <30n1st@gmail.com>
2017-07-30 22:41:55 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/handlers/promptbutton/Click.swift

--- GitSyncMacApp/temp/prototype/view/handlers/promptbutton/Click.swift
+++ GitSyncMacApp/temp/prototype/view/handlers/promptbutton/Click.swift
@@ -8,10 +8,11 @@ extension ProtoTypeView {
      */
     func promptButtonClickHandler(_ event:ButtonEvent) { /*Handler for promptBtn*/
         Swift.print("promptBtn.upInside")
-        modalBtn.modalAnimator.setTargetValue(Modal.initial(curModal!.index)).start()/*outro modal*/
-        modalBtn.setAppearance(Colors.Modal.initial)//reset the color again
+        guard let curModal:ModalButton = curModal else{return}
+        curModal.modalAnimator.setTargetValue(Modal.initial(curModal.index)).start()/*outro modal*/
+        curModal.setAppearance(Colors.Modal.initial)//reset the color again
         self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start()/*outro promptBtn*/
-        self.modalBtn.addHandler(self.modalBtn.forceTouchHandler)//re-Added forcetoucheventhandler, ideally add this handler on outro complete
+        curModal.addHandler(curModal.forceTouchHandler)//re-Added forcetoucheventhandler, ideally add this handler on outro complete
         self.modalStayMode = false//release modalStayMode
     }
 }
