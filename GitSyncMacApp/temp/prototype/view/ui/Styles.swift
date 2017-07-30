45ef744b229971e75615b9e0d5ea0b9b8db77e4c
Eonist <30n1st@gmail.com>
2017-07-30 16:39:56 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/Styles.swift

--- GitSyncMacApp/temp/prototype/view/ui/Styles.swift
+++ GitSyncMacApp/temp/prototype/view/ui/Styles.swift
@@ -1,5 +1,23 @@
 import Foundation
+@testable import Utils
 
-class Styles {
-
+extension ProtoTypeView{
+    enum Styles{
+        enum Modal{
+            static let initial:String = {
+                var css:String = ""
+                css += "Button#modalBtn{"
+                css += "fill:#66CDAD,~/Desktop/ElCapitan/svg/question.svg white;"//fill:blue;
+                css += "width:\(ProtoTypeView.Modal.initial.w)px,50px;"
+                css += "height:\(ProtoTypeView.Modal.initial.h)px,50px;"
+                css += "corner-radius:\(ProtoTypeView.Modal.initial.fillet)px;"
+                css += "margin-top:0px,25px;"
+                css += "margin-left:0px,25px;"
+                css += "clear:none;"
+                css += "float:none;"
+                css += "}"
+                return css
+            }()
+        }
+    }
 }
