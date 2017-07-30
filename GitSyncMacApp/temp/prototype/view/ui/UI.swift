8b2992b2be7ee91bec4da5b136f5578b44dc44a9
Eonist <30n1st@gmail.com>
2017-07-30 17:36:13 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/UI.swift

--- GitSyncMacApp/temp/prototype/view/ui/UI.swift
+++ GitSyncMacApp/temp/prototype/view/ui/UI.swift
@@ -9,7 +9,7 @@ extension ProtoTypeView {
      * Returns Modal UI
      */
     func createModal() -> ForceTouchButton{
-        StyleManager.addStyle(Styles.Modal.initial)
+        StyleManager.addStyle(Styles.Modal.initial)/*Add css styling*/
         let btn = self.addSubView(ForceTouchButton(Modal.initial.size.w,Modal.initial.size.h,nil,"modalBtn"))
         btn.point = Modal.initial.origin//center button
         return btn
@@ -18,7 +18,7 @@ extension ProtoTypeView {
      * Returns Prompt Button
      */
     func createPromptButton() -> Button{
-        StyleManager.addStyle(Styles.PromptButton.initial)
+        StyleManager.addStyle(Styles.PromptButton.initial)/*Add css styling*/
         let btn = self.addSubView(Button(Modal.initial.w,Modal.initial.h,nil,"prompt"))
         btn.layer?.position = PromptButton.initial.origin//out of view
         return btn
