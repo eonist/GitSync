427939adede62facb0798a50d30b1876abacb5a3
Eonist <30n1st@gmail.com>
2017-07-30 12:31:39 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift

--- GitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift
+++ GitSyncMacApp/temp/prototype/view/utils/RoundRectExtensions.swift
@@ -2,8 +2,8 @@ import Foundation
 @testable import Utils
 
 extension RoundedRect:Advancable5 {
-    static var defaults:AnimState5<RoundedRect>  =  AnimState5<RoundedRect>(RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(10e-5,10e-5,10e-5,10e-5,10e-5))
-    func isNear( value:  RoundedRect,  epsilon: RoundedRect) -> Bool {
+    public static var defaults:AnimState5<RoundedRect>  =  AnimState5<RoundedRect>(RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(), RoundedRect(10e-5,10e-5,10e-5,10e-5,10e-5))
+    public func isNear( value:  RoundedRect,  epsilon: RoundedRect) -> Bool {
         let isSizeNear = self.size.isNear(value.size,epsilon.size.w)
         let isOriginNear = self.origin.isNear(value.origin,epsilon.origin.x)
         let isFilletNear = self.fillet.isNear(value.fillet, epsilon.fillet)
