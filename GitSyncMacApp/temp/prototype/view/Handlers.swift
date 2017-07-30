e98028147eecf93818683026e47714e643068a68
Eonist <30n1st@gmail.com>
2017-07-30 14:10:13 +0200

    New files added: 1, Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/Handlers.swift

--- GitSyncMacApp/temp/prototype/view/Handlers.swift
+++ GitSyncMacApp/temp/prototype/view/Handlers.swift
@@ -14,54 +14,7 @@ extension ProtoTypeView {
         self.modalBtn.addHandler(self.forceTouchHandler)//re-Added forcetoucheventhandler, ideally add this handler on outro complete
         self.modalStayMode = false//release modalStayMode
     }
-    /**
-     * ForceTouch handler for modal
-     */
-    func forceTouchHandler(_ event:ForceTouchEvent)  {
-        //Swift.print("event.type: " + "\(event.type)")
-        if event.type == ForceTouchEvent.clickDown{
-            Swift.print("clickDown")
-            self.modalAnimator.setTargetValue(Modal.click).start()
-        }else if event.type == ForceTouchEvent.deepClickDown{
-            Swift.print("deepClickDown")
-            self.modalAnimator.setTargetValue(Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
-            self.onMouseDownMouseY  = self.window!.contentView!.localPos().y
-            NSEvent.addMonitor(&self.leftMouseDraggedMonitor,.leftMouseDragged,self.leftMouseDraggedClosure)
-        }else if event.type == ForceTouchEvent.clickUp {
-            Swift.print("clickUp")
-            if !self.modalStayMode {//modal stay
-                self.modalAnimator.setTargetValue(Modal.initial).start()
-            }
-        }else if event.type == ForceTouchEvent.deepClickUp {
-            Swift.print("deepClickUp")
-            if self.modalStayMode {/*modal stay*/
-                Swift.print("modal stay")
-                self.modalBtn.removeHandler()
-                self.modalAnimator.direct = false
-                var rect = Modal.expanded
-                rect.origin.y -= 30
-                self.modalAnimator.setTargetValue(rect).start()
-            }else{/*modal leave*/
-                Swift.print("modal leave")
-                self.modalAnimator.direct = false
-                self.modalAnimator.setTargetValue(Modal.initial).start()
-                /*promptBtn*/
-                self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start() //anim bellow screen
-            }
-            NSEvent.removeMonitor(&self.leftMouseDraggedMonitor)
-        }
-        if event.type == ForceTouchEvent.stageChange {/*when forcetouch changes state*/
-            let stage:Int = event.stage
-            //Swift.print("stage: " + "\(stage)")
-            if stage == 0 && !self.modalStayMode{
-                modalBtn.setAppearance(NSColor.blue)
-            }else if stage == 1 && !self.modalStayMode && event.prevStage == 0{//only change to red if prev stage was 0
-                modalBtn.setAppearance(NSColor.red)
-            }else if stage == 2 && !self.modalStayMode{
-                modalBtn.setAppearance(NSColor.green)
-            }
-        }
-    }
+    
     /**
      * Drag handler for modal
      */
