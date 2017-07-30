0c57e56f71dc997efa6ee5a13b3d7e63a75b55b6
Eonist <30n1st@gmail.com>
2017-07-30 20:55:24 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/temp/prototype/view/ui/elements/ModalButton.swift

--- GitSyncMacApp/temp/prototype/view/ui/elements/ModalButton.swift
+++ GitSyncMacApp/temp/prototype/view/ui/elements/ModalButton.swift
@@ -1,4 +1,4 @@
-import Foundation
+import Cocoa
 @testable import Utils
 @testable import Element
 
@@ -27,50 +27,50 @@ extension ModalButton{
     }
     private func clickDown(){
         Swift.print("clickDown")
-        ProtoTypeView.modalAnimator.setTargetValue(Modal.click).start()
+        ProtoTypeView.shared.modalAnimator.setTargetValue(ProtoTypeView.Modal.click).start()
     }
     private func clickUp(){
         Swift.print("clickUp")
-        if !self.modalStayMode {//modal stay
-            self.modalAnimator.setTargetValue(Modal.initial).start()
+        if !ProtoTypeView.shared.modalStayMode {//modal stay
+            ProtoTypeView.shared.modalAnimator.setTargetValue(ProtoTypeView.Modal.initial).start()
         }
     }
     private func deepClickDown(){
         Swift.print("deepClickDown")
-        self.modalAnimator.setTargetValue(Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
-        self.onMouseDownMouseY  = self.window!.contentView!.localPos().y
-        NSEvent.addMonitor(&self.leftMouseDraggedMonitor,.leftMouseDragged,self.leftMouseDraggedClosure)
+        ProtoTypeView.shared.modalAnimator.setTargetValue(ProtoTypeView.Modal.expanded).start()//Swift.print("window.contentView.localPos(): " + "\(window.contentView!.localPos())")
+        ProtoTypeView.shared.onMouseDownMouseY  = self.window!.contentView!.localPos().y
+        NSEvent.addMonitor(&ProtoTypeView.shared.leftMouseDraggedMonitor,.leftMouseDragged,ProtoTypeView.shared.leftMouseDraggedClosure)
     }
     private func deepClickUp(){
         Swift.print("deepClickUp")
-        if self.modalStayMode {/*modal stay*/
+        if ProtoTypeView.shared.modalStayMode {/*modal stay*/
             Swift.print("modal stay")
-            self.modalBtn.removeHandler()
-            self.modalAnimator.direct = false
-            var rect = Modal.expanded
+            ProtoTypeView.shared.modalBtn.removeHandler()
+            ProtoTypeView.shared.modalAnimator.direct = false
+            var rect = ProtoTypeView.Modal.expanded
             rect.origin.y -= 30
-            self.modalAnimator.setTargetValue(rect).start()
+            ProtoTypeView.shared.modalAnimator.setTargetValue(rect).start()
         }else{/*modal leave*/
             Swift.print("modal leave")
-            self.modalAnimator.direct = false
-            self.modalAnimator.setTargetValue(Modal.initial).start()
+            ProtoTypeView.shared.modalAnimator.direct = false
+            ProtoTypeView.shared.modalAnimator.setTargetValue(ProtoTypeView.Modal.initial).start()
             /*promptBtn*/
-            self.promptBtnAnimator.setTargetValue(PromptButton.initial.origin).start() //anim bellow screen
+            self.promptBtnAnimator.setTargetValue(ProtoTypeView.PromptButton.initial.origin).start() //anim bellow screen
         }
         NSEvent.removeMonitor(&self.leftMouseDraggedMonitor)
     }
-    /**
-     * when forcetouch changes state
-     */
-    private func stageChange(_ event:ForceTouchEvent){
-        let stage:Int = event.stage
-        //Swift.print("stage: " + "\(stage)")
-        if stage == 0 && !self.modalStayMode{
-            modalBtn.setAppearance(Colors.Modal.initial)
-        }else if stage == 1 && !self.modalStayMode && event.prevStage == 0{//only change to red if prev stage was 0
-            modalBtn.setAppearance(Colors.Modal.click)
-        }else if stage == 2 && !self.modalStayMode{
-            modalBtn.setAppearance(Colors.Modal.expanded)
-        }
-    }
+//    /**
+//     * when forcetouch changes state
+//     */
+//    private func stageChange(_ event:ForceTouchEvent){
+//        let stage:Int = event.stage
+//        //Swift.print("stage: " + "\(stage)")
+//        if stage == 0 && !self.modalStayMode{
+//            modalBtn.setAppearance(Colors.Modal.initial)
+//        }else if stage == 1 && !self.modalStayMode && event.prevStage == 0{//only change to red if prev stage was 0
+//            modalBtn.setAppearance(Colors.Modal.click)
+//        }else if stage == 2 && !self.modalStayMode{
+//            modalBtn.setAppearance(Colors.Modal.expanded)
+//        }
+//    }
 }
