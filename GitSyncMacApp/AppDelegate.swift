963298a351699aaf3424e9cb495552e3316850ea
Eonist <30n1st@gmail.com>
2017-08-01 12:40:29 +0200

    New files added: 1, Files modified: 1, Files deleted: 47
    Added 1 file:GitSyncMacApp/temp.bundle/styles/Deleted 47 files:GitSyncMacApp/temp.bundle/advance/advance.cssGitSyncMacApp/temp.bundle/advance/table/table.cssGitSyncMacApp/temp.bundle/advance/treelist/slidertreelist.cssGitSyncMacApp/temp.bundle/advance/treelist/treelist.cssGitSyncMacApp/temp.bundle/advance/treelist/treelist3.cssGitSyncMacApp/temp.bundle/basic/basic.cssGitSyncMacApp/temp.bundle/basic/button/button.cssGitSyncMacApp/temp.bundle/basic/button/checkbox.cssGitSyncMacApp/temp.bundle/basic/button/checkboxbutton.cssGitSyncMacApp/temp.bundle/basic/button/radiobullet.cssGitSyncMacApp/temp.bundle/basic/button/radiobutton.cssGitSyncMacApp/temp.bundle/basic/button/textbutton.cssGitSyncMacApp/temp.bundle/basic/combobox/combobox.cssGitSyncMacApp/temp.bundle/basic/list/list.cssGitSyncMacApp/temp.bundle/basic/list/sliderlist.cssGitSyncMacApp/temp.bundle/basic/list/vlist.cssGitSyncMacApp/temp.bundle/basic/slider/hslider.cssGitSyncMacApp/temp.bundle/basic/slider/vslider.cssGitSyncMacApp/temp.bundle/basic/spinner/leverspinner.cssGitSyncMacApp/temp.bundle/basic/stepper/leverstepper.cssGitSyncMacApp/temp.bundle/basic/text/slidertextarea.cssGitSyncMacApp/temp.bundle/basic/text/text.cssGitSyncMacApp/temp.bundle/basic/text/textarea.cssGitSyncMacApp/temp.bundle/basic/text/textinput.cssGitSyncMacApp/temp.bundle/basic/window/titlebar.cssGitSyncMacApp/temp.bundle/basic/window/window.cssGitSyncMacApp/temp.bundle/extra/button/colortag.cssGitSyncMacApp/temp.bundle/extra/button/iconbutton.cssGitSyncMacApp/temp.bundle/extra/button/iconbuttons.cssGitSyncMacApp/temp.bundle/extra/button/tabbar.cssGitSyncMacApp/temp.bundle/extra/extra.cssGitSyncMacApp/temp.bundle/extra/misc/progressindicator.cssGitSyncMacApp/temp.bundle/extra/slider/volumeslider.cssGitSyncMacApp/temp.bundle/extra/text/searchbox.cssGitSyncMacApp/temp.bundle/extra/text/singlelinetextarea.cssGitSyncMacApp/temp.bundle/generic.cssGitSyncMacApp/temp.bundle/styletest/commit/commitslist.cssGitSyncMacApp/temp.bundle/styletest/commit/commitview.cssGitSyncMacApp/temp.bundle/styletest/commit/detail/Commitdetailview.cssGitSyncMacApp/temp.bundle/styletest/dark.cssGitSyncMacApp/temp.bundle/styletest/dialog/commitdialogview.cssGitSyncMacApp/temp.bundle/styletest/general.cssGitSyncMacApp/temp.bundle/styletest/light.cssGitSyncMacApp/temp.bundle/styletest/menu/menucontainer.cssGitSyncMacApp/temp.bundle/styletest/prefs/prefsview.cssGitSyncMacApp/temp.bundle/styletest/repo/detail/repodetailview.cssGitSyncMacApp/temp.bundle/styletest/repo/repoview.cssModified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
     
     func applicationDidFinishLaunching(_ aNotification:Notification) {
         Swift.print("GitSync - Automates git")//Simple git automation for macOS, The autonomouse git client,The future is automated
-        //initApp()
+        initApp()
 
         
         //Continue here: 🏀
@@ -39,7 +39,7 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
         let root:String  = FilePathParser.resourcePath() + "/temp.bundle"//~/Desktop/ElCapitan
         let themeStr:String = PrefsView.prefs.darkMode ? "dark.css" : "light.css"
-        StyleManager.addStylesByURL(root + "/styletest/" + themeStr,true)
+        StyleManager.addStylesByURL(root + "styles/styletest/" + themeStr,true)
         
         //StyleWatcher.watch("~/Desktop/ElCapitan/","~/Desktop/ElCapitan/gitsync.css", self.win!.contentView!)
         let rect:CGRect = PrefsView.prefs.rect
