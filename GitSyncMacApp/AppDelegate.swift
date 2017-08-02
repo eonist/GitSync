eba058e68337a1bedc49a46a4d9cd4bc82627405
Eonist <30n1st@gmail.com>
2017-08-02 23:46:16 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -25,32 +25,9 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
         
         
-        /**
-         * PARAM: baseURL: must be absolute: "Users/John/Desktop/temp"
-         * EXAMPLE: expand("Users/John/Desktop/temp")//Users/John/Desktop/temp
-         * EXAMPLE: expand("~/Desktop/temp")//Users/John/Desktop/temp
-         * EXAMPLE: expand("/temp/colors/star.svg",Users/John/Desktop)//Users/John/Desktop/temp/colors/star.svg
-         * EXAMPLE: expand("star.svg",Users/John/Desktop)//Users/John/Desktop/star.svg
-         * IMPORTANT: ⚠️️ Tilde paths can't have backlash syntax like ../../ etc
-         */
-        func expand(_ filePath:String, baseURL:String = "") -> String{
-            if FilePathAsserter.isTildePath(filePath) {
-                return filePath.tildePath
-            }else if FilePathAsserter.isBacklash(filePath) {//isRelative
-                return FilePathModifier.normalize(baseURL + filePath)//returns absolute path
-            }else if FileAsserter.exists(filePath){//absolute path that exists
-                return filePath
-            }else if FilePathAsserter.isAbsolute(filePath){//absolute but doesn't exists
-                return baseURL + filePath
-            }else{//must be just I.E: "star.svg"
-                return baseURL + "/" + filePath
-            }
-        }
+       
         
-        Swift.print(expand("/Users/John/Desktop/temp"))
-        Swift.print(expand("~/Desktop/test.txt"))
-        Swift.print(expand("/temp/colors/star.svg",baseURL:"/Users/John/Desktop"))
-        Swift.print(expand("star.svg",baseURL:"/Users/John/Desktop"))
+        
     }
     /**
      *
