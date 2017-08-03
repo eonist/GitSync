c7302cdd5c75bbef1d44bd9dbb150bca8337039a
Eonist <30n1st@gmail.com>
2017-08-03 02:05:25 +0200

    Files modified: 1
    Modified 1 file:GitSyncMacApp/AppDelegate.swift

--- GitSyncMacApp/AppDelegate.swift
+++ GitSyncMacApp/AppDelegate.swift
@@ -25,11 +25,11 @@ class AppDelegate:NSObject, NSApplicationDelegate {
         
         
         
-        let str = "bad wolf, bad dog, Bad sheep"
-        let newStr = RegExp.replace(str, pattern: "\\b([bB]ad)\\b"){
-            return $0.isLowerCased ? $0 : $0.lowercased()
-        }
-        Swift.print("newStr: " + "\(newStr)")//bad wolf, bad dog, bad sheep
+let str = "bad wolf, bad dog, Bad sheep"
+let newStr = RegExp.replace(str, pattern: "\\b([bB]ad)\\b"){
+    return $0.isLowerCased ? $0 : $0.lowercased()
+}
+Swift.print("newStr: " + "\(newStr)")//bad wolf, bad dog, bad sheep
     }
     /**
      *
