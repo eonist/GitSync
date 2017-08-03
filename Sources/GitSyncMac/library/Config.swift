94bd643e0bb3dd83e1b06c5fec8069be079aa20d
Eonist <30n1st@gmail.com>
2017-08-03 22:15:38 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/Config.swift

--- Sources/GitSyncMac/library/Config.swift
+++ Sources/GitSyncMac/library/Config.swift
@@ -6,20 +6,20 @@ import Foundation
 enum Config {
     enum Bundle{
         /*The root of the asset bundle*/
-        static let assets:String = FilePathParser.resourcePath + "/assets.bundle/user/"
+        static let assets:String = FilePathParser.resourcePath + "/assets.bundle/"
         /*Stores the repo details*/
         static let repo:String = {
-            return assets + (Config.release == .dev ? "dev/repos.xml" : "pub/repos.xml")
+            return assets + (Config.release == .dev ? "user/dev/repos.xml" : "user/pub/repos.xml")
         }()
         /*UI structure of the app*/
         static let app:String = assets + "gitsync.json"
         /*The app prefs*/
         static let prefs:String = {
-            return assets + (Config.release == .dev ? "dev/prefs.xml" : "pub/prefs.xml")
+            return assets + (Config.release == .dev ? "user/dev/prefs.xml" : "user/pub/prefs.xml")
         }()
         /*Cache.swift uses this url*/
         static let commitCacheURL:String = {
-            return assets + (Config.release == .dev ? "dev/sortedcommits.xml" : "pub/sortedcommits.xml")
+            return assets + (Config.release == .dev ? "user/dev/sortedcommits.xml" : "user/pub/sortedcommits.xml")
         }()
     }
     enum ReleaseType {case dev,pub}
