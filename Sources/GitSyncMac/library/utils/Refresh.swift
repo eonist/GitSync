e93104933beddce16dd4c592bfd8769726834f62
Eonist <30n1st@gmail.com>
2017-08-04 16:19:27 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/utils/Refresh.swift

--- Sources/GitSyncMac/library/utils/Refresh.swift
+++ Sources/GitSyncMac/library/utils/Refresh.swift
@@ -120,7 +120,6 @@ class RefreshUtils{
             }
             main.async {group.leave()}
         }
-        
     }
     /**
      * Basically creates an array of commit data from the latest commit until limit (limit:3 returns the 3 last commits)
@@ -131,12 +130,12 @@ class RefreshUtils{
         Swift.print("RefreshUtils.commitItems()")
         var results:[String] = Array(repeating: "", count:limit)//basically creates an array with many empty strings
         //        let group = DispatchGroup()
-        let group = ThreadGroup()
-        //Swift.print("🏁 Utils.commitItems() all results completed results.count: \(results.count)")
-        group.onComplete = {
+        let group = ThreadGroup{
+            //Swift.print("🏁 Utils.commitItems() all results completed results.count: \(results.count)")
             Swift.print("🏁 group completed. results: " + "\(results)")
             onComplete(results.reversed()) //reversed is a temp fix/*Jump back on the main thread bc: onComplete resides there*/
         }
+        
         let formating:String = "--pretty=format:Hash:%h%nAuthor:%an%nDate:%ci%nSubject:%s%nBody:%b".encode()!//"-3 --oneline"//
         for i in 0..<limit{
             let cmd:String = "head~" + "\(i) " + formating + " --no-patch"
