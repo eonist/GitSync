32cefc2ef6897ff644f76d145f802cfe9b0a82b8
Eonist <30n1st@gmail.com>
2017-08-04 12:36:53 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/commits/utils/CommitDataItem.swift

--- Sources/GitSyncMac/window/view/commits/utils/CommitDataItem.swift
+++ Sources/GitSyncMac/window/view/commits/utils/CommitDataItem.swift
@@ -1,5 +1,12 @@
 import Foundation
 
-class CommitData {
-
+struct CommitDataItem {
+    let repoName:String
+    let contributor:String
+    let title:String
+    let description:String
+    let date:String
+    let sortableDate:String
+    let hash:String
+    let gitDate:String
 }
