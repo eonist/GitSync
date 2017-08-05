45a724412fdd08fa289199dd1290b76ec2ab2f0a
Eonist <30n1st@gmail.com>
2017-08-05 19:36:49 +0200

    Files modified: 3
    Modified 3 files:Sources/GitSyncMac/utils/GitSync.swiftSources/GitSyncMac/utils/commit/CommitMessage.swiftSources/GitSyncMac/utils/commit/CommitMessageUtils.swift

--- Sources/GitSyncMac/utils/GitSync.swift
+++ Sources/GitSyncMac/utils/GitSync.swift
@@ -59,22 +59,6 @@ class GitSync{
      */
     static func commit(_ localRepoPath:String, commitMessage:CommitMessage? = nil)->Bool{
         //Swift.print("commit()")
-        let statusList:[[String:String]] = StatusUtils.generateStatusList(localRepoPath)//get current status
-        //Swift.print("statusList.count: " + "\(statusList.count)")
-        if (statusList.count > 0) {
-            //Swift.print("doCommit().there is something to add or commit")
-            StatusUtils.processStatusList(localRepoPath, statusList) //process current status by adding files, now the status has changed, some files may have disapared, some files now have status as renamed that prev was set for adding and del
-            let title = CommitMessageUtils.sequenceCommitMsgTitle(statusList) //sequence commit msg title for the commit
-            //Swift.print("commitMsgTitle: " + "\(title)")
-            let desc = CommitDescUtils.sequenceDescription(statusList)//sequence commit msg description for the commit
-            //Swift.print("commitMsgDesc: >" + "\(desc)" + "<")
-            let commitResult = GitModifier.commit(localRepoPath, (title,desc))//🌵 commit
-            _ = commitResult
-            //Swift.print("commitResult: " + "\(commitResult)")
-            return true/*return true to indicate that the commit completed*/
-        }else{
-            //Swift.print("nothing to add or commit")
-            return false/*break the flow since there is nothing to commit or process*/
-        }
+        CommitMessage.commitMessage(localRepoPath)
     }
 }
