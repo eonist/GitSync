19ae091924920a13c9418a2b0d434e4b08eecbe7
Eonist <30n1st@gmail.com>
2017-08-05 15:18:46 +0200

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/menu/view/items/ToggleSideBarMenuItem.swiftSources/GitSyncMac/window/view/menu/MenuView.swift

--- Sources/GitSyncMac/menu/view/items/ToggleSideBarMenuItem.swift
+++ Sources/GitSyncMac/menu/view/items/ToggleSideBarMenuItem.swift
@@ -3,21 +3,17 @@ import Cocoa
 @testable import Element
 
 class ToggleSideBarMenuItem:CustomMenuItem{
-    static var isSideMenuHidden:Bool = false
     init() {super.init("Show side bar", "m")}
-    
     override func onSelect(event:AnyObject) {
         Swift.print("ShowSideBarMenuItem.onSelect()")
-        let hidden:Bool = !ToggleSideBarMenuItem.isSideMenuHidden
-        ToggleSideBarMenuItem.isSideMenuHidden = hidden/*toggle*/
-        guard let styleTestView:StyleTestView = NSApp.mainWindow?.contentView as? StyleTestView else {fatalError("not avilable")}
-        styleTestView.toggleSideBar(hide: hidden)/*hiding logic*/
+        let toggle = !StyleTestView.shared.leftBar.isLeftBarHidden
+        StyleTestView.shared.toggleSideBar(hide: toggle)/*hiding logic*/
     }
     /**
      * Return true if you want to enable the menu item, false will disable it
      */
     override func validateMenuItem(_ menuItem:NSMenuItem) -> Bool {
-        self.title = ToggleSideBarMenuItem.isSideMenuHidden ? "Show side bar" : "Hide side bar"
+        self.title = StyleTestView.shared.leftBar.isLeftBarHidden ? "Show side bar" : "Hide side bar"
         Swift.print("validateMenuItem")
         return true
     }
