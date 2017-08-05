b2285da02454bf1697b1b0e5ad59cdeb0508ca7e
Eonist <30n1st@gmail.com>
2017-08-05 13:42:41 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/manager/NavExtensions.swift

--- Sources/GitSyncMac/manager/NavExtensions.swift
+++ Sources/GitSyncMac/manager/NavExtensions.swift
@@ -1,5 +1,19 @@
 import Foundation
 
-class NavExtensions {
-
+extension Nav{
+    enum ViewType{
+        enum Main:String{
+            case commit = "commit"
+            case repo = "repo"
+            case prefs = "prefs"
+        }
+        case main(Main)
+        case commitDetail([String:String])
+        case repoDetail([Int])
+        case dialog(Dialog)
+        enum Dialog{
+            case conflict
+            case commit
+        }
+    }
 }
