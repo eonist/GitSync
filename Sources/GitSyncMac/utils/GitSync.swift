ebcba16cae1284b0b62204cf315938bdaa1d3357
Eonist <30n1st@gmail.com>
2017-08-06 18:25:09 +0200

    'Files modified: 1'
    'Added 1 file:
    Sources/GitSyncMac/utils/GitSync.swift
    
    '

--- Sources/GitSyncMac/utils/GitSync.swift
+++ Sources/GitSyncMac/utils/GitSync.swift
@@ -15,7 +15,7 @@ class GitSync{
             }
             let hasCommited = commit(repoItem.local,commitMessage)/*🌵 if there were no commits false will be returned*/
             Swift.print("hasCommited: " + "\(hasCommited)")
-//            hasCommited ? initPush(repoItem,onComplete: onPushComplete) : onPushComplete()
+//          hasCommited ? initPush(repoItem,onComplete: onPushComplete) : onPushComplete()
             //TODO:⚠️️⚠️️⚠️️⚠️️⚠️️⚠️️⚠️️⚠️️ the next step should be psuh or check if you need to pull down changes and subsequently merge something
             initPush(repoItem,onComplete: onPushComplete)
         //}
