faea85180dba294c4403df222d987d1ce384bc52
Eonist <30n1st@gmail.com>
2017-08-06 21:55:07 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/StyleTestWin.swift

--- Sources/GitSyncMac/window/view/StyleTestWin.swift
+++ Sources/GitSyncMac/window/view/StyleTestWin.swift
@@ -5,6 +5,7 @@ import Cocoa
 class StyleTestWin:Window {
     
     required init(_ docWidth:CGFloat,_ docHeight:CGFloat){
+        Swift.print("StyleTestWin.init")
         super.init(docWidth, docHeight)
         WinModifier.align(self, Alignment.centerCenter, Alignment.centerCenter,CGPoint(6,0))/*aligns the window to the screen*/
         self.minSize = CGSize(250,250)
