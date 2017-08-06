413f43e108e91750835d6c2ae2f3e4a4658308e4
Eonist <30n1st@gmail.com>
2017-08-07 00:14:01 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/temp/UnFoldable.swift

--- Sources/GitSyncMac/library/temp/UnFoldable.swift
+++ Sources/GitSyncMac/library/temp/UnFoldable.swift
@@ -1,5 +1,7 @@
 import Foundation
-
+/**
+ * Consider adding default data methods in an extension that prints error if called to, or not as you probably wont be able to as easily override it etc
+ */
 protocol UnFoldable {
     //unfold
     //data 
