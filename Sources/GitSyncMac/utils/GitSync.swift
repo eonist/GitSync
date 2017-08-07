8f7ba2fd751ae2d80ea062f2e0c4064d3519f2bf
Eonist <30n1st@gmail.com>
2017-08-07 23:55:11 +0200

    'Files modified: 1'
    'Added 1 file:
    Sources/GitSyncMac/utils/GitSync.swift
    
    '

--- Sources/GitSyncMac/utils/GitSync.swift
+++ Sources/GitSyncMac/utils/GitSync.swift
@@ -12,7 +12,9 @@ class GitSync{
         //bg.async {/*All these git processes needs to happen one after the other*/
             if let unMergedFiles = GitParser.unMergedFiles(repoItem.local).optional {/*🌵Asserts if there are unmerged paths that needs resolvment, aka remote changes that isnt in local*/
                 Swift.print("unMergedFiles: " + "\(unMergedFiles)")
-                MergeUtils.resolveMergeConflicts(repoItem.local, repoItem.branch, unMergedFiles)
+                
+                let gitRepo:GitRepo = .init(repoItem.local, repoItem.remote, repoItem.branch)
+                MergeUtils.resolveMergeConflicts(gitRepo, unMergedFiles)
             }
             let hasCommited = commit(repoItem.local,commitMessage)/*🌵 if there were no commits false will be returned*/
             Swift.print("hasCommited: " + "\(hasCommited)")
