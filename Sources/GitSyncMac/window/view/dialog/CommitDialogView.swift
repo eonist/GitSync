a1895f961a242bf8c7f298da344ad34d3decdab5
Eonist <30n1st@gmail.com>
2017-08-07 20:39:37 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift

--- Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift
+++ Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift
@@ -27,26 +27,15 @@ extension CommitDialogView{
         static let repo = "repo"
         static let title = "title"
         static let desc = "desc"
-        
-    }
-    /**
-     * TODO: ⚠️️ this should be possible to abstract into an universal util metod for all Unfoldables
-     */
-    override var data:[String:Any] {
-        get{fatalError("error")
-        }set{fatalError("error")}
     }
     /**
      * New
      */
     func setData(_ repoItem:RepoItem, _ commitMessage:CommitMessage){
         self.repoItem = repoItem
-//        ID.repo:[:repoTitle],//TODO:⚠️️    make inputText a const
-//        ID.title:[TextInput.Key.inputText:commitTitle],
-//        ID.desc:[TextInput.Key.inputText:commitDescription]
-        
         self.apply([Key.repo,TextInput.Key.inputText],repoItem.title)
-        //self.data = ID.getData(, commitMessage.title, commitMessage.description)
+        self.apply([Key.title,TextInput.Key.inputText],commitMessage.title)
+        self.apply([Key.desc,TextInput.Key.inputText],commitMessage.description)
     }
     
     /**
@@ -54,18 +43,14 @@ extension CommitDialogView{
      */
     func onOKButtonClick(){
         //AutoSync.shared.iterateMessageCount()
-//        let title:String = UnFoldUtils.retrieve(self, [Key.repo]).
-        //let desc:String = UnFoldUtils.retrive(Key.repo,TextInput.Key.inputText])
         guard let desc:String = UnFoldUtils.retrieve(self, [Key.repo,TextInput.Key.inputText]) else{fatalError("error")}
         guard let title:String = UnFoldUtils.retrieve(self, [Key.title,TextInput.Key.inputText]) else{fatalError("error")}
-
         let commitMessage = CommitMessage(title,desc)
         Swift.print("commitMessage.title: " + "\(commitMessage.title)")
         Swift.print("commitMessage.description: " + "\(commitMessage.description)")
         bg.async {
             GitSync.initCommit(self.repoItem!, commitMessage:commitMessage, AutoSync.shared.incrementCountForRepoWithMSG)
         }
-        
 //      Nav.setView(.main(.commit))
         if let curPrompt = StyleTestView.shared.currentPrompt {curPrompt.removeFromSuperview()}//remove promptView from window
     }
