7493272d2b7d8ddbd0c02f4c9110ee9fc1d0f124
Eonist <30n1st@gmail.com>
2017-08-07 19:41:41 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift

--- Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift
+++ Sources/GitSyncMac/window/view/dialog/CommitDialogView.swift
@@ -63,8 +63,11 @@ extension CommitDialogView{
      */
     func onOKButtonClick(){
         //AutoSync.shared.iterateMessageCount()
-        let title:String = UnFoldUtils.retrieve(self, [Key.repo]).
-        let desc:String = UnFoldUtils.retrieve(self, [Key.repo,TextInput.Key.inputText])
+//        let title:String = UnFoldUtils.retrieve(self, [Key.repo]).
+        //let desc:String =
+        if let data = UnFoldUtils.retrieveData(self, [Key.repo,]) let  {
+            //[TextInput.Key.inputText]
+        }
 //        guard let title:String = data[Key.title] as? String,let desc:String = data[Key.title] as? String else{
 //            fatalError("something went wrong")
 //        }
