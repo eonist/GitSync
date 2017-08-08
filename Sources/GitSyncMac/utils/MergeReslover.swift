d82abf09475470404f062e10bafb15e597c9547c
Eonist <30n1st@gmail.com>
2017-08-08 15:36:41 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/utils/MergeReslover.swift

--- Sources/GitSyncMac/utils/MergeReslover.swift
+++ Sources/GitSyncMac/utils/MergeReslover.swift
@@ -103,6 +103,8 @@ class MergeReslover {
             }
         }
         
+        nextConflict()
+        
         
         /*open local version*/
         //_ = GitModifier.checkOut(localRepoPath, "--ours", unmergedFile)
