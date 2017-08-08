85d9e50f8e64de62e3edaa792103515de3a387b4
Eonist <30n1st@gmail.com>
2017-08-08 23:49:22 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/utils/MergeUtils.swift

--- Sources/GitSyncMac/utils/MergeUtils.swift
+++ Sources/GitSyncMac/utils/MergeUtils.swift
@@ -14,13 +14,14 @@ struct MergeConflict{
 }
 
 class MergeUtils{
+    typealias ManualMergeComplete = () -> Void
     /**
      * Manual merge
      * NOTE: tries to merge a remote branch into a local branch
      * NOTE: prompts the users if a merge conflicts occure
      * TODO: we should use two branch params here since its entirly possible to merge from a different remote branch
      */
-    static func manualMerge(_ repoItem:RepoItem){
+    static func manualMerge(_ repoItem:RepoItem, _ onManualMergeComplete:@escaping ManualMergeComplete){
         Swift.print("🍊 MergeUtils.manualMerge()")
         var hasUnMergedPaths:Bool  {
             return GitAsserter.hasUnMergedPaths(repoItem.localPath)//🌵 Asserts if there are unmerged paths that needs resolvment
@@ -33,6 +34,7 @@ class MergeUtils{
             let unMergedFiles:[String] = GitParser.unMergedFiles(repoItem.localPath)//🌵 Compile a list of conflicting files somehow
             MergeReslover.shared.resolveConflicts(repoItem, unMergedFiles){
                 //_ = GitSync.commit(repoItem.localPath)//🌵 It's best practice to always commit any uncommited files before you attempt to pull, add,commit if any files has an altered status
+                onManualMergeComplete()
             }//🌵 Asserts if there are unmerged paths that needs resolvment
         }else {
             Swift.print("MergeUtils.manualMerge() no resolvment needed")
