2f88311e529f4a5f2e938d4d9baa64c53ffac803
Eonist <30n1st@gmail.com>
2017-08-08 19:56:35 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/library/temp/UnFoldUtils.swift

--- Sources/GitSyncMac/library/temp/UnFoldUtils.swift
+++ Sources/GitSyncMac/library/temp/UnFoldUtils.swift
@@ -120,6 +120,7 @@ class UnFoldUtils{
     static func retrieveValue<T>(_ view:UnFoldable, _ path:[String]) -> T?{
         let unfoldable:UnFoldable? = retrieveUnFoldable(parent:view, path)
         Swift.print("unfoldable: " + "\(String(describing: unfoldable))")
+        Swift.print("unfoldable?.value: " + "\(unfoldable?.value)")
         let value:T? = unfoldable?.value as? T
         Swift.print("value: " + "\(String(describing: value))")
         return value
