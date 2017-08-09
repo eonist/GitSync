b5a3144a9107f185dced2408a631da3d25dcf134
Eonist <30n1st@gmail.com>
2017-08-09 22:31:22 +0200

    Files modified: 2
    Modified 2 files:Sources/GitSyncMac/library/utils/AutoSync.swiftSources/GitSyncMac/window/view/dialog/AutoInitView.swift

--- Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
+++ Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
@@ -26,3 +26,51 @@ class AutoInitView:Element,UnFoldable{
     }
 
 }
+extension AutoInitView{
+    func setData(_ mergeConflict:MergeConflict){
+        Swift.print("MergeConflictView.setData")
+        self.apply([Key.issue], mergeConflict.issue)
+        self.apply([Key.file], mergeConflict.file)
+        self.apply([Key.repo], mergeConflict.repo)
+    }
+    enum Key{
+        static let issue = "issueText"
+        static let file = "fileText"
+        static let repo = "repoText"
+        static let keepLocal = "keepLocalVersion"
+        static let keepRemote = "keepRemoteVersion"
+        static let keepMixed = "keepMixedVersion"
+        static let applyAllConflicts = "applyAllConflicts"
+        static let applyAllRepos = "applyAllRepos"
+    }
+    func onSelectGroupChange(event:Event){
+        Swift.print("onSelectGroupChange event.selectable: " + "\(event)")
+    }
+    func onCheckGroupChange(event:Event){/*this is the event handler*/
+        Swift.print("onSelectGroupChange event.selectable: " + "\(event)")
+    }
+    /**
+     * EventHandler for the okButton click event
+     */
+    func onOKButtonClick(){
+        Swift.print("onOKButtonClick")
+    }
+}
+
+
+//There is no folder in the file path: ~/dev/demo3, do you want to create it and download from remote? OK, Cancel
+//There is no content in the file path: ~/dev/demo3, do you want to download from remote? OK, Cancel
+//There is preExisiting files in path: ~/dev/demo3, do you want to download from remote and initiate a merge dialog
+
+struct AutoInitConflict{
+    let issue:String,file:String,repo:String
+}
+extension AutoInitConflict{
+    static let dummyData:MergeConflict = {
+        let issue:String = "Conflict: Local file is older than the remote file"
+        let file:String = "File: Element.swift"
+        let repo:String = "Repository: Element - iOS"
+        return MergeConflict(issue: issue, file: file, repo: repo)
+    }()
+}
+
