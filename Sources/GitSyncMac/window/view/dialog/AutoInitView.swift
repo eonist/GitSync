7fd69981c535d7a5d66919bf7af24667a816254b
Eonist <30n1st@gmail.com>
2017-08-09 22:26:15 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/AutoInitView.swift

--- Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
+++ Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
@@ -1,5 +1,28 @@
-import Foundation
+import Cocoa
+@testable import Utils
+@testable import Element
 
-class AutoInitView {
+class AutoInitView:Element,UnFoldable{
+    override func resolveSkin() {
+        Swift.print("MergeConflictView.resolveSkin()")
+        super.resolveSkin()
+        UnFoldUtils.unFold(Config.Bundle.app,"mergeConflictView",self)
+        Swift.print("unfold completed")
+        
+        self.apply([Key.issue,Text.Key.text], "Conflict: Local file is older than the remote file")
+        self.apply([Key.file,Text.Key.text], "File: AppDelegate.swift")
+        self.apply([Key.repo,Text.Key.text], "Repository: Element - iOS")
+        
+        _ = radioButtonGroup
+    }
+    override func onEvent(_ event:Event) {
+        if event.assert(.upInside, id: "ok"){
+            onOKButtonClick()
+        }else if event.assert(.upInside, id: "cancel"){
+            fatalError("not yet supported")
+        }/*else if event.assert(SelectEvent.select){
+         
+         }*/
+    }
 
 }
