278be3649a5a13ab6e5ee4f6eb2ea8bea22085b7
Eonist <30n1st@gmail.com>
2017-08-10 12:39:29 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/AutoInitView.swift

--- Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
+++ Sources/GitSyncMac/window/view/dialog/AutoInitView.swift
@@ -9,10 +9,8 @@ class AutoInitView:Element,UnFoldable{
         Swift.print("AutoInitView.resolveSkin()")
         super.resolveSkin()
         UnFoldUtils.unFold(Config.Bundle.app,"autoInitView",self)
-        Swift.print("unfold completed")
+        Swift.print("AutoInitView.unfold completed")
         
-//        self.apply([Key.issue,Text.Key.text], "Conflict: Local file is older than the remote file")
-//        self.apply([Key.file,Text.Key.text], "File: AppDelegate.swift")
         
     }
     override func onEvent(_ event:Event) {
@@ -44,66 +42,4 @@ extension AutoInitView{
     }
 }
 
-struct AutoInitConflict{
-//
-   
-//    let conflict:(issue:String,proposal:String)
-    let repoItem:RepoItem
-    
-    
-   
-    init(_ repoItem:RepoItem){
-        self.repoItem = repoItem
-        
-        
-//        self.pathExists = pathExists
-//        self.isGitRepo = isGitRepo
-//        self.hasPathContent = hasPathContent
-//        self.conflict = self.generateConflict()
-    }
-}
-extension AutoInitConflict{
-    var pathExists:Bool {
-        return FileAsserter.exists(repoItem.localPath)
-    }
-    var isGitRepo:Bool {
-        return GitAsserter.isGitRepo(repoItem.localPath)
-    }
-    
-    var hasPathContent:Bool {
-        return FileAsserter.hasContent(repoItem.localPath)
-    }//TODO: ⚠️️ make priv get pub set
-    static let dummyData:AutoInitConflict = {
-//        let issue:String = "There is no folder in the file path: ~/dev/demo3"
-//        let proposal:String = "Do you want to create it and download from remote?"
-        return AutoInitConflict(RepoItem(local: "~/dev/demo",branch: "master",title: "demo")/*pathExists:false,isGitRepo:false,hasPathContent:false*/)
-    }()
-    /**
-     * New
-     */
-    var conflict:(issue:String,proposal:String){
-        var issue:String = ""
-        var proposal:String = ""
-        let pathExists = self.pathExists
-        if pathExists == false {
-            issue = "There is no folder in the file path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to create it and download from remote?"
-            return (issue,proposal)
-        }
-        let hasPathContent = self.hasPathContent
-        if pathExists && hasPathContent == false{
-            issue = "There is no content in the file path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to download from remote?"
-            return (issue,proposal)
-        }
-        if pathExists && hasPathContent{
-            issue = "There is preExisiting files in path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to download from remote and initiate a merge wizard?"
-            return (issue,proposal)
-        }else{
-            fatalError("this case cant happen")
-        }
-        
-    }
-}
 
