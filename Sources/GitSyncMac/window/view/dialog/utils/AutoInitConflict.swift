14bf8862a183413f4b5971ccb3794cc1d140e927
Eonist <30n1st@gmail.com>
2017-08-10 14:17:13 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/utils/AutoInitConflict.swift

--- Sources/GitSyncMac/window/view/dialog/utils/AutoInitConflict.swift
+++ Sources/GitSyncMac/window/view/dialog/utils/AutoInitConflict.swift
@@ -16,65 +16,71 @@ struct AutoInitConflict{
     }
 }
 extension AutoInitConflict{
-    typealias Strategy = PathExists
-    enum PathExists{
-        enum HasPathContent{
-            enum IsGitRepo{
-                case yes
-                case no
+    enum Strategy{
+        enum PathExists{
+            enum HasPathContent{
+                enum IsGitRepo{
+                    case yes
+                    case no
+                }
+                case yes(isGitRepo:IsGitRepo)
+                case no(isGitRepo:IsGitRepo)
             }
-            case yes(isGitRepo:IsGitRepo)
-            case no(isGitRepo:IsGitRepo)
+            case yes(hasContent:HasPathContent)
+            case no(hasContent:HasPathContent)
+//            case no
         }
-        case yes(hasContent:HasPathContent)
-        case no(hasContent:HasPathContent)
-        
-    }
-    static func strategy(pathExists:Bool,isGitRepo:Bool,hasPathContent:Bool) -> Strategy{
-        if pathExists == false {
-            return .pathExists(pathExists:.no(hasContent:.no(isGitRepo:.no)))
-        }else if pathExists && hasPathContent == false{
-            return .b
-        }else if pathExists && hasPathContent{
+        case pathExists(pathExists:PathExists)
+//        case a
+//        case b/*Path exists, but has no files*/
+//        case c
+//        case d/*Path does not exist*/
+        /**
+         *
+         */
+        static func strategy(pathExists:Bool,isGitRepo:Bool,hasPathContent:Bool) -> Strategy{
+            if pathExists == false {
+                return .pathExists(pathExists:.no(hasContent:.no(isGitRepo:.no)))
+            }else if pathExists && hasPathContent == false{
+                return .b
+            }else if pathExists && hasPathContent{
+                
+            }else if isGitRepo {
+                
+            }
             
-        }else if isGitRepo {
+        }
+        /**
+         *
+         */
+        static func process(){
+            //do git stuff
+        }
+        /**
+         *
+         */
+        func text(_ repoItem:RepoItem) -> (issue:String,proposal:String){
+            var issue:String = ""
+            var proposal:String = ""
+            switch self {
+            case .a:
+                issue = "There is no folder in the file path: " + "\(repoItem.localPath)"
+                proposal = "Do you want to create it and download from remote?"
+            case .b:
+                issue = "There is no content in the file path: " + "\(repoItem.localPath)"
+                proposal = "Do you want to download from remote?"
+            case .c:
+                issue = "There is preExisiting files in path: " + "\(repoItem.localPath)"
+                proposal = "Do you want to download from remote and initiate a merge wizard?"
+            case .d:
+                issue = ""
+                proposal = ""
+            }
             
+            return (issue,proposal)
         }
-        
-    }
-    /**
-     *
-     */
-    static func process(){
-        //do git stuff
-    }
-    /**
-     *
-     */
-    func text(_ repoItem:RepoItem) -> (issue:String,proposal:String){
-        var issue:String = ""
-        var proposal:String = ""
-        switch self {
-        case .a:
-            issue = "There is no folder in the file path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to create it and download from remote?"
-        case .b:
-            issue = "There is no content in the file path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to download from remote?"
-        case .c:
-            issue = "There is preExisiting files in path: " + "\(repoItem.localPath)"
-            proposal = "Do you want to download from remote and initiate a merge wizard?"
-        case .d:
-            issue = ""
-            proposal = ""
-        }
-        
-        return (issue,proposal)
     }
     
-}
-extension AutoInitConflict.Strategy{
-   
 }
 private class Utils{
     static func pathExists(_ repoItem:RepoItem)->Bool {
