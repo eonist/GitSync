e56f4324d9a330951025049450b55f9bdae8a824
Eonist <30n1st@gmail.com>
2017-08-10 22:45:24 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift
@@ -4,13 +4,13 @@ import Foundation
 class AutoInitConflict{
     let repoItem:RepoItem
     lazy var pathExists:Bool = {
-        return FileAsserter.exists(self.repoItem.localPath)
+        return FileAsserter.exists(self.repoItem.localPath.tildePath)
     }()
     lazy var isGitRepo:Bool = {
-        return self.pathExists && GitAsserter.isGitRepo(self.repoItem.localPath)
+        return self.pathExists && GitAsserter.isGitRepo(self.repoItem.localPath.tildePath)
     }()
     lazy var hasPathContent:Bool = {
-        return self.pathExists && !self.isGitRepo && FileAsserter.hasContent(self.repoItem.localPath)
+        return self.pathExists && !self.isGitRepo && FileAsserter.hasContent(self.repoItem.localPath.tildePath)
     }()
     init(_ repoItem:RepoItem){
         self.repoItem = repoItem
