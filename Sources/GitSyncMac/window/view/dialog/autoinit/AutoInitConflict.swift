34f2fe14c45bc572f1307d3223017718f8b3cd3d
Eonist <30n1st@gmail.com>
2017-08-10 22:38:09 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/AutoInitConflict.swift
@@ -1,31 +1,20 @@
 import Foundation
 @testable import Utils
 
-struct AutoInitConflict{
+class AutoInitConflict{
     let repoItem:RepoItem
-    let pathExists:Bool
-    let isGitRepo:Bool
-    let hasPathContent:Bool
+    lazy var pathExists:Bool = {
+        return Utils.pathExists(self.repoItem)
+    }()
+    lazy var isGitRepo:Bool = {
+        return self.pathExists && Utils.isGitRepo(self.repoItem)
+    }()
+    lazy var hasPathContent:Bool = {
+        return self.pathExists && !self.isGitRepo && Utils.hasPathContent(self.repoItem)
+    }()
 //    let strategy:Strategy
     
     init(_ repoItem:RepoItem){
         self.repoItem = repoItem
-        self.pathExists = Utils.pathExists(repoItem)
-        let isGitRepo = pathExists && Utils.isGitRepo(repoItem)
-        self.isGitRepo = isGitRepo
-        self.hasPathContent = self.pathExists && !isGitRepo && Utils.hasPathContent(repoItem)
-//        self.strategy = Strategy.strategy()
-    }
-}
-
-private class Utils{
-    static func pathExists(_ repoItem:RepoItem)->Bool {
-        return FileAsserter.exists(repoItem.localPath)
-    }
-    static func isGitRepo(_ repoItem:RepoItem)->Bool {
-        return GitAsserter.isGitRepo(repoItem.localPath)
-    }
-    static func hasPathContent(_ repoItem:RepoItem)->Bool {
-        return FileAsserter.hasContent(repoItem.localPath)
     }
 }
