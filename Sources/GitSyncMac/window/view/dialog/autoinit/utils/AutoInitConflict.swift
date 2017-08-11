963847defd7b35f71a17e9948b6ee94683fc4fd4
Eonist <30n1st@gmail.com>
2017-08-11 12:20:47 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflict.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflict.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflict.swift
@@ -8,12 +8,20 @@ class AutoInitConflict{
     lazy var pathExists:Bool = {
         return FileAsserter.exists(self.repoItem.localPath.tildePath)
     }()
-    lazy var isGitRepo:Bool = {
-        return self.pathExists && GitAsserter.isGitRepo(self.repoItem.localPath.tildePath)
-    }()
     lazy var hasPathContent:Bool = {
         return self.pathExists && FileAsserter.hasContent(self.repoItem.localPath.tildePath)
     }()
+    lazy var isGitRepo:Bool = {
+        return self.pathExists && GitAsserter.isGitRepo(self.repoItem.localPath.tildePath)
+    }()
+    lazy var areRemotesEqual:Bool = {
+        let curRemotePath:String = GitParser.originUrl(self.repoItem.localPath)
+//        Swift.print("curRemotePath: " + ">\(curRemotePath)<")
+//        Swift.print("repoItem.remotePath: " + ">\(repoItem.remotePath)<")
+        return curRemotePath == self.repoItem.remotePath
+    }()
+    
+    
     init(_ repoItem:RepoItem){
         self.repoItem = repoItem
     }
