7d7b7a9388a095ce7f825d868a93c0ff24b08a18
Eonist <30n1st@gmail.com>
2017-08-11 20:08:30 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift
@@ -62,7 +62,7 @@ class AutoInitConflictUtils {
 //            _ = GitModifier.attachRemoteRepo(repoItem.localPath,repoItem.remotePath)//--add new remote origin
         case (true,false,_,_):
             Swift.print("c")
-            GitUtils.manualClone(repoItem.localPath.tildePath, repoItem.remotePath, repoItem.branch)
+            _ = GitModifier.clone(repoItem.remotePath, repoItem.localPath.tildePath)//this works because the folder is empty
             //            let status = GitModifier.clone(repoItem.remotePath,repoItem.localPath.tildePath)
         //            Swift.print("status: " + "\(status)")
         case (false,_,_,_):
