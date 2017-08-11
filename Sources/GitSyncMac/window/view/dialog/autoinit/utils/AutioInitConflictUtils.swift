0ff02d3983da0271a3bda71cd1308a08dbdb5b41
Eonist <30n1st@gmail.com>
2017-08-11 18:07:35 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutioInitConflictUtils.swift
@@ -17,7 +17,7 @@ class AutoInitConflictUtils {
         switch state {
         case (true,true,true,false):
             issue = "There is already a git project in the folder: \(repoItem.local) with a different remote URL"
-            proposal = "Do you want delete the current files, download the git repo from remote?"
+            proposal = "Do you want reset the git repo, keep the files, download the git repo from remote and start a merge wizard?"
         case (true,true,false,_):
             issue = "The folder \(repoItem.localPath) is not a git repo but there are pre-exisiting files"
             proposal = "Do you want to keep the files, download the git repo from remote and start a merge wizard?"
@@ -42,7 +42,9 @@ class AutoInitConflictUtils {
         switch state {
         case (true,true,true,false):
             Swift.print("a")
-            
+            FileModifier.delete((repoItem.localPath+".git").tildePath)
+            _ = GitModifier.initialize(repoItem.localPath)
+            _ = GitModifier.attachRemoteRepo(repoItem.localPath,repoItem.branch)//--add new remote origin
         case (true,true,false,_):
             Swift.print("")
             _ = GitModifier.initialize(repoItem.localPath)
