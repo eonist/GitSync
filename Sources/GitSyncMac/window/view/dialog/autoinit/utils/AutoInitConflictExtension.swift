f04c132a0a8bbc593f69eac25f92ad64de21d816
Eonist <30n1st@gmail.com>
2017-08-11 12:23:08 +0200

    Files modified: 1
    Modified 1 file:Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflictExtension.swift

--- Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflictExtension.swift
+++ Sources/GitSyncMac/window/view/dialog/autoinit/utils/AutoInitConflictExtension.swift
@@ -2,7 +2,7 @@ import Foundation
 @testable import Utils
 
 extension AutoInitConflict{
-    typealias State = (pathExists:Bool,hasPathContent:Bool,isGitRepo:Bool)
+    typealias State = (pathExists:Bool,hasPathContent:Bool,isGitRepo:Bool,areRemotesEqual:Bool)
     typealias TextData = (issue:String,proposal:String)
     /**
      * Creates the text for the AutoInitPrompt
@@ -11,30 +11,23 @@ extension AutoInitConflict{
         Swift.print("AutoInitConflict.text")
         var issue:String = ""
         var proposal:String = ""
-        let state:State = (pathExists,hasPathContent,isGitRepo)
+        let state:State = (pathExists,hasPathContent,isGitRepo,areRemotesEqual)
         Swift.print("state: " + "\(state)")
         switch state {
-        case (true,true,true):
-            let curRemotePath:String = GitParser.originUrl(repoItem.localPath)
-            Swift.print("curRemotePath: " + ">\(curRemotePath)<")
-            Swift.print("repoItem.remotePath: " + ">\(repoItem.remotePath)<")
-            let isEqual = curRemotePath == repoItem.remotePath
-            Swift.print("isEqual: " + "\(isEqual)")
-            if curRemotePath != repoItem.remotePath {
-                issue = "There is already a git project in the folder: \(repoItem.local) with a different remote URL"
-                proposal = "Do you want to assign a new remote URL and start a merge wizard?"
-            }//else no need to show AutoInit Dialog, proceed
-        case (true,true,false):
+        case (true,true,true,false):
+            issue = "There is already a git project in the folder: \(repoItem.local) with a different remote URL"
+            proposal = "Do you want to assign a new remote URL and start a merge wizard?"
+        case (true,true,false,_):
             issue = "There is preExisiting files in path: " + "\(repoItem.localPath)"
             proposal = "Do you want to download from remote and start a merge wizard?"
-        case (true,false,_):
+        case (true,false,_,_):
             issue = "There is no content in the path: " + "\(repoItem.localPath)"
             proposal = "Do you want to download from remote?"
-        case (false,_,_):
+        case (false,_,_,_):
             issue = "There is nothing in the path \(repoItem.localPath)"
             proposal = "Do you want to create it and download files from remote "//\(repoItem.remotePath)
         default:
-            fatalError("Has no strategy for this scenario ")
+            fatalError("Has no strategy for this scenario \(state)")
         }
         return (issue,proposal)
     }
@@ -42,9 +35,9 @@ extension AutoInitConflict{
      * NOTE: after this you often want to : MergeUtils.manualMerge(repoItem,{})
      */
     func process(){
-        let state:State = (pathExists,hasPathContent,isGitRepo)
+        let state:State = (pathExists,hasPathContent,isGitRepo,areRemotesEqual)
         switch state {
-        case (true,true,true):
+        case (true,true,true,false):
             let curRemotePath:String = GitParser.originUrl(repoItem.localPath)
             if curRemotePath != repoItem.remotePath {//--the .git folder already has a remote repo attached
                 _ = GitModifier.detachRemoteRepo(repoItem.localPath/*branch*/)//--promt the user if he wants to use the existing remote origin, this will skip the user needing to input a remote url
@@ -54,15 +47,15 @@ extension AutoInitConflict{
             }else{//--does not have remote repo attached// GitAsserter.hasRemoteRepoAttached(localPath, branch)
                 _ = GitModifier.attachRemoteRepo(repoItem.localPath,repoItem.remotePath)//--attach remote repo
             }
-        case (true,true,false):
+        case (true,true,false,_):
             _ = GitModifier.initialize(repoItem.localPath)
             _ = GitModifier.attachRemoteRepo(repoItem.localPath,repoItem.branch)//--add new remote origin
-        case (true,false,_):
+        case (true,false,_,_):
             _ = GitModifier.clone(repoItem.remotePath,repoItem.localPath)
-        case (false,_,_):
+        case (false,_,_,_):
             _ = GitModifier.clone(repoItem.remotePath,repoItem.localPath)//--this will also create the folders if they dont exist, even nested
         default:
-            fatalError("Has no strategy for this scenario ")
+            fatalError("Has no strategy for this scenario: \(state) ")
         }
     }
 }
