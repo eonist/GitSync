3ac3b4014a00c6c89cb0bc20d698ac03d95b803f
Eonist <30n1st@gmail.com>
2017-08-12 23:22:22 +0200

    Files modified: 1
    Modified 1 file: Sources/GitSyncMac/library/utils/Refresh.swift

--- Sources/GitSyncMac/library/utils/Refresh.swift
+++ Sources/GitSyncMac/library/utils/Refresh.swift
@@ -45,12 +45,7 @@ class Refresh{
      * The final complete call
      */
     private func allRefreshesCompleted(){/*All repo items are now refreshed, the entire refresh process is finished*/
-        //Swift.print("commitDB.sortedArr.count: " + "\(commitDP!.items.count)")
-        //Swift.print("Printing sortedArr after refresh: ")
-        //commitDP!.items.forEach{
-        //Swift.print("hash: \($0["hash"]!) date: \(GitDateUtils.gitTime($0["sortableDate"]!)) repo: \($0["repo-name"]!) ")
-        //}
-        CommitDPCache.write(commitDP!)//write data to disk, we could also do this on app exit
+        CommitDPCache.write(commitDP!)/*write data to disk, we could also do this on app exit*/ 
         //Swift.print("💾 Refresh.onRefreshReposComplete() Written to disk")
         //isRefreshing = false
         Swift.print("🔄 Refresh.allRefreshesCompleted() ⏰ Time: " + "\(abs(startTime!.timeIntervalSinceNow))")/*How long did the gathering of git commit logs take?*/
@@ -58,11 +53,15 @@ class Refresh{
     }
 }
 class RefreshUtils{
+    static var totalCommitCount:Int = 0
+    static var commitCount:Int = 0
+    static var commitsCompletedCount:Int = 0
+    typealias CommitItemsComplete = (_ results:[String])->Void
+    typealias RefreshRepoComplete = ()->Void
     /**
      * Adds commit items to CommitDB if they are newer than the oldest commit in CommitDB
      * Retrieve the commit log items for this repo with the range specified
      */
-    typealias RefreshRepoComplete = ()->Void
     static func refreshRepo(_ dp:CommitDP,_ repo:RepoItem,_ onComplete:@escaping RefreshRepoComplete){
         Swift.print("RefreshUtils.refreshRepo \(repo.title) 🔄💾")
         func onCommitItemsCompleted(_ results:[String]){
@@ -78,7 +77,7 @@ class RefreshUtils{
         }
         func onCommitCountComplete(_ commitCount:Int){/*once these completes then do result, you do not want to wait until calling refreshRepo*/
 //            Swift.print("💙 RefreshUtils.refreshRepo().onCommitCountComplete \(repo.title): commitCount: " + "\(commitCount)")
-            RefreshUtils.getCommitItems(repo.local, commitCount, onCommitItemsCompleted)//🚧0~100 Git calls/*creates an array raw commit item logs, from repo*/
+            RefreshUtils.commitItems(repo.local, commitCount, onCommitItemsCompleted)//🚧0~100 Git calls/*creates an array raw commit item logs, from repo*/
         }
         commitCount(dp,repo,onCommitCountComplete)//🚪⬅️️
     }
@@ -92,7 +91,7 @@ class RefreshUtils{
         let group = DispatchGroup()
         
         group.enter()
-        bg.async {/*do some work in the background*/
+        bg.async {
             let totCommitCountStr:String = GitUtils.commitCount(repo.local)
             totCommitCount = totCommitCountStr.int//🚧1 Git call/*Get the total commitCount of this repo*/
             if totCommitCount > 0 {totCommitCount = totCommitCount - 1}//why is this?
@@ -100,7 +99,7 @@ class RefreshUtils{
         }
         group.enter()
         bg.async {
-            if !dp.items.isEmpty {/*maybe do some work*/
+            if !dp.items.isEmpty {
                 let lastDate:Int = dp.items.last!["sortableDate"]!.int/*the last date is always the furthest distant date 19:59,19:15,19:00 etc*/
                 let gitTime:String = GitDateUtils.gitTime(lastDate.string)/*converts descending date to git time*/
                 let rangeCount:Int = GitUtils.commitCount(repo.local, after: gitTime).int//🚧1 Git call /*Finds the num of commits from now until */
@@ -115,17 +114,13 @@ class RefreshUtils{
             onComplete(clippedCommitCount)/*🚪➡️️*/
         }
     }
-    static var totalCommitCount:Int = 0
-    static var commitCount:Int = 0
-    static var commitsCompletedCount:Int = 0
-    
+   
     /**
      * Basically creates an array of commit data from the latest commit until limit (limit:3 returns the 3 last commits)
      * Returns an array of commitItems at PARAM: localPath and limited with PARAM: max
      * PARAM: limit = max Items Allowed per repo
      */
-    typealias CommitItemsComplete = (_ results:[String])->Void
-    static func getCommitItems(_ localPath:String,_ limit:Int, _ onComplete:@escaping CommitItemsComplete) {
+    static func commitItems(_ localPath:String,_ limit:Int, _ onComplete:@escaping CommitItemsComplete) {
         var results:[String] = Array(repeating: "", count:limit)//basically creates an array with many empty strings
         let group = DispatchGroup()
         let formating:String = "--pretty=format:Hash:%h%nAuthor:%an%nDate:%ci%nSubject:%s%nBody:%b".encode()!//"-3 --oneline"//
@@ -137,15 +132,13 @@ class RefreshUtils{
                 let cmd:String = "head~" + "\(i) " + formating + " --no-patch"
                 let result:String = GitParser.show(localPath, cmd)/*🌵*/
                 main.async {
-//                    Swift.print("result main: " + "\(result.count)")
-                    results[i] = result//results.append(result)
+                    results[i] = result
                     group.leave()
                 }
             }
         }
         group.notify(queue: main){
             //Swift.print("🏁 Utils.commitItems() all results completed results.count: \(results.count)")
-//            Swift.print("🏁 getCommitItemsGroups completed. results: " + "\(results.count)")
             onComplete(results.reversed()) //reversed is a temp fix/*Jump back on the main thread bc: onComplete resides there*/
         }
     }
