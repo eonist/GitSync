fda8eb8ea1fd62029bb6c717735d8e238bcea4a7
Eonist <30n1st@gmail.com>
2017-08-13 15:20:11 +0200

    Moved GitCMD to standalone class
    Modified 1 file:
    Sources/GitSyncMac/utils/commit/GitCMD.swift

--- Sources/GitSyncMac/utils/commit/GitCMD.swift
+++ Sources/GitSyncMac/utils/commit/GitCMD.swift
@@ -1,5 +1,26 @@
 import Foundation
 
-class GitCMD {
-
+/**
+ * Utility methods for parsing the the "git status message"
+ * TODO: Sometimes RM shows up, figure out what that does
+ * NOTE: ' ' = unmodified, M = modified,A = added,D = deleted,R = renamed,C = copied,U = updated but unmerged
+ */
+enum GitCMD:String{
+    case M = "M"/*When a file is modified*/
+    case D = "D"/*When a file is deleted*/
+    case A = "A"/*When a file is added*/
+    case R = "R"/*When a file is renamed,*/
+    case DU = "DU"/*unmerged, deleted by us*/
+    case DD = "DD"/*unmerged, both deleted*/
+    case DM = "DM"/*deleted from index*/
+    case AA = "AA"/*unmerged, both added*/
+    case AU = "AU"/*unmerged, added by us*/
+    case AM = "AM"/*Beta, needs description, probably file added with two parents*/
+    case MM = "MM"/*There are two Ms in your example because it's a merge commit with two parents*/
+    case RM = "RM"/*When a file is renamed, new and experimental*/
+    case QQ = "??"/*untracked*/
+    case EE = "!!"/*ignored*/
+    case UU = "UU"/*unmerged, both modified*/
+    case UD = "UD"/*unmerged, deleted by them*/
+    case UA = "UA"/*unmerged, added by them*/
 }
