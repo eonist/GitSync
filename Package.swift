e46d85f6048776e8d41e780fad1845b26468f83f
Eonist <30n1st@gmail.com>
2017-08-13 11:30:22 +0200

    Files modified: 1
    Modified 1 file:Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,5 @@ import PackageDescription
 
 let package = Package(
     name: "GitSyncMac",
-    dependencies: [.Package(url: "https://github.com/eonist/Element.git", Version(0, 0, 0, prereleaseIdentifiers: ["alpha", "5"]))]
+    dependencies: [.Package(url: "https://github.com/eonist/Element.git", Version(0, 0, 0, prereleaseIdentifiers: ["alpha", "10"]))]
 )
