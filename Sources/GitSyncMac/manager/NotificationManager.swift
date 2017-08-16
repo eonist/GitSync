5877c663c6d3ec121074b07e2f350255133c6dc2
Eonist <30n1st@gmail.com>
2017-08-16 16:37:12 +0200

    Added NotificationManager
    Modified 1 file:
    Sources/GitSyncMac/manager/NotificationManager.swift

--- Sources/GitSyncMac/manager/NotificationManager.swift
+++ Sources/GitSyncMac/manager/NotificationManager.swift
@@ -1,5 +1,16 @@
 import Foundation
+@testable import Utils
 
 class NotificationManager {
-
+    /**
+     * Sends Message to NotificationCenter in MacOS about latest commit
+     */
+    static func notifyUser(message commitMessage:CommitMessage,repo repoItem:RepoItem){
+        let notification = NSUserNotification()
+        notification.title = "\(repoItem.title)"//Committed in:
+        notification.subtitle = commitMessage.title
+        notification.informativeText = commitMessage.description
+        notification.soundName = nil//NSUserNotificationDefaultSoundName
+        NSUserNotificationCenter.default.deliver(notification)
+    }
 }
