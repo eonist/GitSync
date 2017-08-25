310ba826cc317dd8fc1c2782ace43721db7b7e73
Eonist <30n1st@gmail.com>
2017-08-25 22:56:35 +0200

    Added description to prefsView UI
    Deleted 1 file:
    Sources/GitSyncMac/window/view/prefs/utils/Key.swift
    
    Modified 2 files:
    Sources/GitSyncMac/library/temp/unfoldables/Unfoldable+Section.swift
    Sources/GitSyncMac/window/view/prefs/utils/PrefsView+Extensions.swift

--- Sources/GitSyncMac/library/temp/unfoldables/Unfoldable+Section.swift
+++ Sources/GitSyncMac/library/temp/unfoldables/Unfoldable+Section.swift
@@ -1,5 +1,16 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class Unfoldable_Section {
+extension Section:UnFoldable{}
 
+extension UnFoldable where Self:Section{
+    /**
+     * New
+     */
+    static func unfold(dict:[String:Any]) -> UnFoldable{
+        let element:ElementConfig = .init(dict)
+        return Section.init(size:element.size, id:element.id)
+    }
 }
+
