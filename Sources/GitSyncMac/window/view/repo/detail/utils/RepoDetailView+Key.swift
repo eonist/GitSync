02c16ea7639e7560264aacac4cd403452ddced62
Eonist <30n1st@gmail.com>
2017-08-26 14:11:50 +0200

    Added Unfold Keys for RepoDetail
    Modified 2 files:
    Sources/GitSyncMac/window/view/repo/detail/utils/RepoDetailView+Key.swift
    Sources/GitSyncMac/window/view/repo/detail/utils/RepodetailView+Extensions.swift

--- Sources/GitSyncMac/window/view/repo/detail/utils/RepoDetailView+Key.swift
+++ Sources/GitSyncMac/window/view/repo/detail/utils/RepoDetailView+Key.swift
@@ -1,5 +1,14 @@
 import Foundation
+@testable import Utils
 
-class RepoDetailView_Key {
-
+extension RepoDetailView{
+    enum Key{
+        static let title:String = RepoType.title.rawValue
+        static let local:String = RepoType.local.rawValue
+        static let remote:String = RepoType.remote.rawValue
+        static let branch:String = RepoType.branch.rawValue
+        static let auto:String = RepoType.auto.rawValue
+        static let message:String = RepoType.message.rawValue
+        static let active:String = RepoType.active.rawValue
+    }
 }
--- Sources/GitSyncMac/window/view/repo/detail/utils/RepodetailView+Extensions.swift
+++ Sources/GitSyncMac/window/view/repo/detail/utils/RepodetailView+Extensions.swift
@@ -1,5 +1,58 @@
 import Foundation
+@testable import Utils
+@testable import Element
 
-class RepodetailView_Extensions {
-
+extension RepoDetailView{
+    /**
+     * Populates the UI elements with data from the dp item
+     * NOTE: Filters groups and items
+     */
+    func setRepoData(_ idx3d:[Int]){
+//        Swift.print("setRepoData(idx3d) ")
+        RepoView.selectedListItemIndex = idx3d
+        //TODO: Use the RepoItem on the bellow line see AutoSync class for implementation
+        if let tree:Tree = RepoView.treeDP.tree[idx3d], let repoItemDict = tree.props{//NodeParser.dataAt(treeList!.node, selectedIndex)
+            var repoItem:RepoItem
+            let hasIsOpenAttrib:Bool = TreeAsserter.hasAttribute(RepoView.treeDP.tree, idx3d, "isOpen")
+            
+            Swift.print("hasIsOpenAttrib: " + "\(hasIsOpenAttrib)")
+            if !tree.children.isEmpty  || hasIsOpenAttrib {/*Support for folders*/
+                repoItem = RepoItem()
+                if let title:String = repoItemDict[RepoType.title.rawValue] {repoItem.title = title}
+                if let active:String = repoItemDict[RepoType.active.rawValue] {repoItem.active = active.bool}
+            }else{
+                repoItem = RepoUtils.repoItem(repoItemDict)
+            }
+            setRepoData(repoItem)
+        }
+    }
+    /**
+     * Populates the UI elements with data from the dp item
+     * TODO: ⚠️️ Use the Unfold lib to set data not direct refs like this
+     */
+    private func setRepoData(_ repoItem:RepoItem){
+        Swift.print("setRepoData(repoItem)")
+        /*TextInput*/
+        //Swift.print("repoItem.title: " + "\(repoItem.title)")
+        nameText?.inputTextArea.setTextValue(repoItem.title)
+        localPathPicker?.textInput.setInputText(repoItem.local)
+        remoteText?.inputTextArea.setTextValue(repoItem.remote)
+        branchText?.inputTextArea.setTextValue(repoItem.branch)
+        /*CheckButtons*/
+        //        autoCheckBoxButton?.setChecked(repoItem.auto)
+        self.apply(["autoGroup",Key.auto], repoItem.auto)
+        self.apply(["messageGroup",Key.message], repoItem.message)
+        self.apply(["activeGroup",Key.active], repoItem.active)
+//        messageCheckBoxButton?.setChecked(repoItem.message)
+//        activeCheckBoxButton?.setChecked(repoItem.active)
+    }
+}
+extension RepoDetailView{/*Convenience*/
+    var nameText:TextInput? {return self.element(RepoType.title.rawValue)}
+    var localPathPicker:FilePicker? {return self.element(RepoType.local.rawValue)}
+    var remoteText:TextInput? {return self.element(RepoType.remote.rawValue)}
+    var branchText:TextInput? {return self.element(RepoType.branch.rawValue)}
+    //    var autoCheckBoxButton:CheckBoxButton? {return self.element(RepoType.auto.rawValue)}
+//    var messageCheckBoxButton:CheckBoxButton? {return self.element(RepoType.message.rawValue)}
+//    var activeCheckBoxButton:CheckBoxButton? {return self.element(RepoType.active.rawValue)}
 }
