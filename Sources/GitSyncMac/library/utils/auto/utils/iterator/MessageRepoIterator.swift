5ae3f95d093a3765f3bbea8fad5e0910b3d26a8f
Eonist <30n1st@gmail.com>
2017-08-31 00:30:05 +0200

    Debugging async bug with weak refs
    Modified 1 file:
    Sources/GitSyncMac/library/utils/auto/utils/iterator/MessageRepoIterator.swift

--- Sources/GitSyncMac/library/utils/auto/utils/iterator/MessageRepoIterator.swift
+++ Sources/GitSyncMac/library/utils/auto/utils/iterator/MessageRepoIterator.swift
@@ -15,11 +15,15 @@ class MessageRepoIterator:ArrayIterator<RepoItem> {
         if hasNext() {
             Swift.print("iterator.hasNext")
             let repoItem:RepoItem = next()
+            Swift.print("repoItem: " + "\(repoItem)")
             if var commitMessage:CommitMessage = CommitMessageUtils.generateCommitMessage(repoItem.local) {/*if no commit msg is generated, then no commit is needed*/
+                Swift.print("has commit message")
                 if !repoItem.template.isEmpty {commitMessage.title = repoItem.template}//add template as title if it exists
                 Nav.setView(.dialog(.commit(repoItem,commitMessage,{[weak self] in self?.iterate()})))/*this view eventually calls initCommit*/
             }else {
+                Swift.print("has no commit message")
                 MergeUtils.manualMerge(repoItem){/*nothing to commit but check if remote has updates*/
+                    Swift.print("nothing to commit, iterate")
                     self.iterate()/*nothing to commit, iterate*/
                 }
             }
